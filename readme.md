047560d1148bc50e2b1a6339240d4a885365d6a5
Deltakosh <david.catuhe@live.fr>
2013-06-27 14:07:15 -0700

    Move things to correct folders and adding website
    Former-commit-id: 4cc6c3ba9239ba1c96519af6d42aac7eb987b259

---
+++ readme.md
@@ -0,0 +1,86 @@
+﻿Babylon.js
+========
+ 
+
+Babylon.js is a webgl / javascript 3D engine. It supports the following features: 
+- Complete scene graph with lights, cameras, materials and meshes
+- Standard material is a per pixel material that supports: ◦Diffuse lightning and texture
+ - Ambient lightning and texture
+ - Specular lightning
+ - Opacity texture
+ - Reflection texture (Spheric, planar, cubic and projection)
+ - Mirror texture
+ - Emissive texture
+ - Specular texture
+ - Up to 4 lights (point or directional)
+- Render target textures
+- Dynamic textures (canvas)
+- Custom materials
+- Alpha blending
+- Alpha testing
+- Billboarding
+- Scene picking
+- Collisions engine
+- Cameras: ◦Arc rotate camera
+ - Free camera
+ - Tocuh camera
+- Frustum clipping
+- Sub-meshes clipping
+- Fullscreen mode
+- Babylon file format is a JSON file that can be produced from: ◦.OBJ
+ - .FBX
+ - .MXB
+ - Blender
+- Hardware scaling
+- Antialiasing
+- Particles Systems
+- Mesh cloning
+- Animations engine
+- Sprites and 2D layers
+
+### License
+
+Microsoft Public License (Ms-PL)
+
+This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+
+1. Definitions
+
+The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+
+A "contribution" is the original software, or any additions or changes to the software.
+
+A "contributor" is any person that distributes its contribution under this license.
+
+"Licensed patents" are a contributor's patent claims that read directly on its contribution.
+
+2. Grant of Rights
+
+(A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+
+(B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+
+3. Conditions and Limitations
+
+(A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+
+(B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+
+(C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+
+(D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+
+(E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+
+### Contributors
+
+- David Catuhe (@deltakosh)
+
+### External dependencies
+- hand.js: http://handjs.codeplex.com
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,203 @@
+﻿var CreateTestScene = function (engine) {
+    var scene = new BABYLON.Scene(engine);
+    //var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 10, BABYLON.Vector3.Zero(), scene);
+    var camera2 = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 0, -10), scene);
+    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 100, 2), scene);
+    var material = new BABYLON.StandardMaterial("kosh", scene);
+    var material2 = new BABYLON.StandardMaterial("kosh transparent", scene);
+    var planeMaterial = new BABYLON.StandardMaterial("plane material", scene);
+    var box = BABYLON.Mesh.createBox("Box", 1.0, scene);
+    var box2 = BABYLON.Mesh.createBox("Box2", 1.2, scene);
+    var box3 = BABYLON.Mesh.createBox("Box3", 0.8, scene);
+    var sphere = BABYLON.Mesh.createSphere("Sphere", 16, 3, scene);
+    var plane = BABYLON.Mesh.createPlane("plane", 3, scene);
+
+    //material.diffuseColor = new BABYLON.Color3(0, 0, 1);
+    material.diffuseTexture = new BABYLON.Texture("Assets/Tree.png", scene);
+    material.diffuseTexture.hasAlpha = true;
+    material.backFaceCulling = false;
+    material2.diffuseTexture = new BABYLON.Texture("Assets/kosh.jpg", scene);
+    material2.alpha = 0.5;
+    material2.backFaceCulling = false;
+    planeMaterial.backFaceCulling = false;
+    var planeTexture = new BABYLON.DynamicTexture("dynamic texture", 512, scene, true);
+    planeTexture.hasAlpha = true;
+    planeMaterial.diffuseTexture = planeTexture;
+    plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+
+    box.material = material;
+    box2.material = material;
+    box3.material = material2;
+    sphere.material = material;
+    plane.material = planeMaterial;
+    box2.position.x += 13;
+    box3.position.x -= 3;
+    box3.parent = sphere;
+    sphere.position.z = 3;
+    plane.position = new BABYLON.Vector3(0, 7, 0);
+
+    // Particles
+    var particleSystem = new BABYLON.ParticleSystem("particles", 4000, scene);
+    particleSystem.particleTexture = new BABYLON.Texture("Assets/Flare.png", scene);
+    particleSystem.minAngularSpeed = -0.5;
+    particleSystem.maxAngularSpeed = 0.5;
+    particleSystem.minSize = 0.5;
+    particleSystem.maxSize = 1.0;
+    particleSystem.minLifeTime = 0.5;
+    particleSystem.maxLifeTime = 1.0;
+    particleSystem.emitter = box3;
+    particleSystem.emitRate = 300;
+    particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+    particleSystem.minEmitBox = new BABYLON.Vector3(0, 0.1, 0);
+    particleSystem.maxEmitBox = new BABYLON.Vector3(0, -0.1, 0);
+    particleSystem.gravity = new BABYLON.Vector3(0, -0.5, 0);
+    particleSystem.start();
+
+    // Mirror
+    var mirror = BABYLON.Mesh.createBox("Mirror", 1.0, scene);
+    mirror.scaling = new BABYLON.Vector3(100.0, 0.01, 100.0);
+    mirror.material = new BABYLON.StandardMaterial("mirror", scene);
+    mirror.material.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
+    mirror.material.reflectionTexture = new BABYLON.MirrorTexture("mirror", 512, scene, true);
+    mirror.material.reflectionTexture.mirrorPlane = new BABYLON.Plane(0, -1.0, 0, -5.0);
+    mirror.material.reflectionTexture.renderList = [box, box2, box3, sphere];
+    mirror.material.reflectionTexture.level = 0.5;
+    mirror.position = new BABYLON.Vector3(0, -5.0, 0);
+
+    // Sprites
+    var spriteManager = new BABYLON.SpriteManager("MonsterA", "Assets/Player.png", 500, 64, scene);
+    for (var index = 0; index < 500; index++) {
+        var sprite = new BABYLON.Sprite("toto", spriteManager);
+        sprite.position.y = -4.5;
+        sprite.position.z = Math.random() * 20 - 10;
+        sprite.position.x = Math.random() * 20 - 10;
+        sprite.dir = Math.round(Math.random()) * 2 - 1;
+        sprite.invertU = (sprite.dir < 0);
+
+        sprite.playAnimation(0, 9, true, 100);
+    }
+    
+    // Backgrounds
+    var background0 = new BABYLON.Layer("back0", "Assets/Layer0_0.png", scene);
+    var background1 = new BABYLON.Layer("back1", "Assets/Layer1_0.png", scene);
+    var foreground = new BABYLON.Layer("back0", "Assets/Layer2_0.png", scene);
+    
+    // Import
+    var spaceDek;
+    BABYLON.SceneLoader.ImportMesh("Vaisseau", "Scenes/SpaceDek/", "SpaceDek.babylon", scene, function (newMeshes) {
+        spaceDek = newMeshes[0];
+        for (var index = 0; index < newMeshes.length; index++) {
+            mirror.material.reflectionTexture.renderList.push(newMeshes[index]);
+        }
+
+        spaceDek.position = new BABYLON.Vector3(0, 20, 0);
+        spaceDek.scaling = new BABYLON.Vector3(0.3, 0.3, 0.3);
+    });
+
+    var spaceDek2;
+    var spaceDek3;
+    BABYLON.SceneLoader.ImportMesh("Vaisseau", "Scenes/SpaceDek/", "SpaceDek.babylon", scene, function (newMeshes) {
+        spaceDek2 = newMeshes[0];
+        spaceDek2.name = "Vaisseau 2";
+        for (var index = 0; index < newMeshes.length; index++) {
+            mirror.material.reflectionTexture.renderList.push(newMeshes[index]);
+        }
+
+        spaceDek2.position = new BABYLON.Vector3(40, 20, 0);
+        spaceDek2.scaling = new BABYLON.Vector3(0.3, 0.3, 0.3);
+
+        // Clone
+        spaceDek3 = spaceDek2.clone("Vaisseau 3");
+        spaceDek3.position = new BABYLON.Vector3(-50, 20, 0);
+        spaceDek3.scaling = new BABYLON.Vector3(0.3, 0.3, 0.3);
+        mirror.material.reflectionTexture.renderList.push(spaceDek3);
+        var children = spaceDek3.getDescendants();
+        for (var index = 0; index < children.length; index++) {
+            mirror.material.reflectionTexture.renderList.push(children[index]);
+        }
+
+        scene.beginAnimation(spaceDek3, 0, 100, true, 1.0);
+    });
+
+    // Animations
+    var alpha = 0;
+    var frameCount = 0;
+    var reloop = 0;
+    var startDate = new Date();
+    var count = 0;
+    scene.beforeRender = function () {
+        box.rotation.y += 0.01;
+        box2.rotation.x += 0.01;
+        sphere.rotation.y += 0.02;
+        //  box3.scaling.y = 1 + Math.cos(alpha);
+        box3.rotation.z += 0.01;
+        alpha += 0.01;
+
+        if (spaceDek) {
+            spaceDek.rotation.y += 0.01;
+        }
+
+        if (spaceDek2) {
+            spaceDek2.rotation.y -= 0.01;
+        }
+
+        if (spaceDek3) {
+            spaceDek3.rotation.y -= 0.01;
+        }
+        
+        if (box3.intersectsMesh(box)) {
+            material2.alpha = 1;
+            box3.scaling = new BABYLON.Vector3(2, 2, 2);
+        } else {
+            material2.alpha = 0.5;
+            box3.scaling = new BABYLON.Vector3(1, 1, 1);
+        }
+
+        // Sprites
+        frameCount++;
+        if (frameCount > 3) {
+            frameCount = 0;
+            reloop++;
+            for (var index = 0; index < spriteManager.sprites.length; index++) {
+                var sprite = spriteManager.sprites[index];
+                sprite.position.x -= 0.1 * sprite.dir;
+
+                if (reloop > 20) {
+                    sprite.dir *= -1;
+                    sprite.invertU = !sprite.invertU;
+                }
+            }
+
+            if (reloop > 20) {
+                reloop = 0;
+            }
+        }
+        
+        // Update dynamic texture
+        var diff = (new Date() - startDate);
+        
+        if (diff > 200) {
+            startDate = new Date();
+
+            var textureContext = planeTexture.getContext();
+            var size = planeTexture.getSize();
+            var text = count.toString();
+
+            textureContext.clearRect(0, 0, size.width, size.height);
+
+            textureContext.font = "bold 120px Calibri";
+            var textSize = textureContext.measureText(text);
+            textureContext.fillStyle = "white";
+            textureContext.fillText(text, (size.width - textSize.width) / 2, (size.height - 120) / 2);
+
+            planeTexture.update();
+
+            count++;
+        }
+        
+        // Background
+        background0.texture.uOffset += 0.001;
+    };
+
+    return scene;
+};
