a8c28625d2d6a4d9c55b60becae352447747a22b
Deltakosh <david.catuhe@live.fr>
2013-07-05 20:43:32 +0200

    v1.0.2

---
+++ Babylon/Lights/babylon.hemisphericLight.js
@@ -0,0 +1,20 @@
+﻿var BABYLON = BABYLON || {};
+
+(function () {
+    BABYLON.HemisphericLight = function (name, direction, scene) {
+        this.name = name;
+        this.id = name;
+        this.direction = direction;
+        this.diffuse = new BABYLON.Color3(1.0, 1.0, 1.0);
+        this.specular = new BABYLON.Color3(1.0, 1.0, 1.0);
+        this.groundColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+        this._scene = scene;
+
+        scene.lights.push(this);
+
+        // Animations
+        this.animations = [];
+    };
+    
+    BABYLON.HemisphericLight.prototype = Object.create(BABYLON.Light.prototype);
+})();
