4b21f25b8be22cf65797abc3a3ea9b9973579f6d
Deltakosh <david.catuhe@live.fr>
2013-07-05 20:43:32 +0200

    v1.0.2
    Former-commit-id: a8c28625d2d6a4d9c55b60becae352447747a22b

---
+++ Babylon/Lights/babylon.spotLight.js
@@ -0,0 +1,22 @@
+﻿var BABYLON = BABYLON || {};
+
+(function () {
+    BABYLON.SpotLight = function (name, position, direction, angle, exponent, scene) {
+        this.name = name;
+        this.id = name;
+        this.position = position;
+        this.direction = direction;
+        this.angle = angle;
+        this.exponent = exponent;
+        this.diffuse = new BABYLON.Color3(1.0, 1.0, 1.0);
+        this.specular = new BABYLON.Color3(1.0, 1.0, 1.0);
+        this._scene = scene;
+
+        scene.lights.push(this);
+
+        // Animations
+        this.animations = [];
+    };
+    
+    BABYLON.SpotLight.prototype = Object.create(BABYLON.Light.prototype);
+})();
