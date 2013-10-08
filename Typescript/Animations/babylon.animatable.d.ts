50f1df68fbb40d69dd351ab25ee446f20f4763ca
Justin Oblak <jroblak@gmail.com>
2013-10-08 09:39:37 -0400

    Animations Definitions files updated

---
+++ Typescript/Animations/babylon.animatable.d.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../babylon.d.ts" />
+
+declare module BABYLON {
+    class _Animatable {
+        target: Object;
+        fromFrame: number;
+        toFrame: number;
+        loopAnimation: boolean;
+        animationStartDate: Date;
+        speedRatio: number;
+        onAnimationEnd: Function;
+
+        constructor(target: Object, from: number, to: number, loop: boolean, speedRatio: number, onAnimationEnd: Function);
+
+        _animate(delay: number): boolean;
+    }
+}
