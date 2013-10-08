fe70b3709d3dc837771fa569cb510818ba12b2bf
Justin Oblak <jroblak@gmail.com>
2013-10-08 09:48:19 -0400

    Begin adding Bones definition files
    Former-commit-id: 70d357cc2a770e7aa6d6c2f0bbfe63ab9af03d95

---
+++ Typescript/Bones/babylon.bone.d.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../babylon.d.ts" />
+
+declare module BABYLON {
+    class Bone {
+        name: string;
+        children: Bone[];
+        animation: Animation[];
+
+        constructor(name: string, skeleton: Skeleton; parentBone: Bone, matrix: Matrix);
+
+        getParent(): Bone;
+        getLocalMatrix: Matrix;
+        getAbsoluteMatrix: Matrix;
+        updateMatrix(matrix: Matrix): void;
+        markAsDirty(): void;
+    }
+}
---
+++ Typescript/Bones/babylon.skeleton.d.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../babylon.d.ts" />
+
+declare module BABYLON {
+    class Skeleton {
+        id: number;
+        name: string;
+        bones: Bone[];
+
+        constructor(name: string, id: number, scene: Scene);
+
+        getTransformMatrices(): Matrix[];
+        prepare(): void;
+        getAnimatables(): Animation[];
+        clone(name: string, id: number): Skeleton;
+    }
+}
