02a5957df0580a91b7b8c64a05580c1e01d2df6a
Deltakosh <david.catuhe@live.fr>
2013-10-31 19:34:27 +0100

    Adding missing files

---
+++ Exporters/FBX - OBJ/BabylonExport.Core/Exporters/OBJ/MtlHeader.cs
@@ -0,0 +1,25 @@
+﻿namespace BabylonExport.Core
+{
+    internal enum MtlHeader
+    {
+        None,
+        Comment,
+        Material,
+        DiffuseColor,
+        DiffuseTexture,
+        SpecularColor,
+        SpecularTexture,
+        SpecularPower,
+        EmissiveColor,
+        Alpha,
+        IlluminationModel,
+        BumpTexture,
+        ReflectionTexture,
+        TransparencyTexture,
+        AmbientTexture,
+        AmbientColor,
+        RefractionIndex,
+        Transparency,
+        TransmissionFiter,
+    }
+}
