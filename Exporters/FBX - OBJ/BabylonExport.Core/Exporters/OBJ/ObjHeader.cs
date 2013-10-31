6cbf460424fb188a1ea68689f988999f1f31479d
Deltakosh <david.catuhe@live.fr>
2013-10-31 19:34:27 +0100

    Adding missing files
    Former-commit-id: 02a5957df0580a91b7b8c64a05580c1e01d2df6a

---
+++ Exporters/FBX - OBJ/BabylonExport.Core/Exporters/OBJ/ObjHeader.cs
@@ -0,0 +1,18 @@
+﻿namespace BabylonExport.Core
+{
+    internal enum ObjHeader
+    {
+        None,
+        Comment,
+        Object,
+        Vertices,
+        TextureCoordinates,
+        Normals,
+        Group,
+        SmoothingGroup,
+        Line,
+        Faces,
+        MaterialLibrary,
+        Material,
+    }
+}
