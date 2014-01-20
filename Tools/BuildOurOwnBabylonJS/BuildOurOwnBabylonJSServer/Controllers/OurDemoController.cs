cb0dab4408987ff53e2852eb33ccef1ecc1aece9
Gwenaël Hagenmuller <gwenael@stereograph.fr>
2014-01-20 14:56:19 +0100

    Referenced BabylonJS demos found under /Samples Thus, it's possible to test ourBabylon.js against them.

---
+++ Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSServer/Controllers/OurDemoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildOurOwnBabylonJSServer.Controllers
+{
+    public class OurDemoController : Controller
+    {
+        public ActionResult Show(string demo)
+        {
+            return View(demo);
+        }
+    }
+}
--- Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSServer/WebViewPageExtensions.cs
+++ Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSServer/WebViewPageExtensions.cs
@@ -3,21 +3,36 @@ using System.IO;
 using System.Text;
 using System.Web;
 using BuildOurOwnBabylonJSServer.Controllers;
+using System;
 
 namespace BuildOurOwnBabylonJSServer.Extensions
 {
     public static class WebViewPageExtensions
     {
+        public static string BabylonJSFile(this WebViewPage page,
+            string relPathToBabylonJSFolder)
+        {
+            if (page == null)
+                return null;
+
+            // relPath must be the last one so filename must be appended to it
+            var url = page.Url.Action(BuildOurOwnBabylonJSController.GetFileContentActionName,
+                                      "BuildOurOwnBabylonJS",
+                                      new { rootPath = page.ViewBag.BabylonJSFolder, relPath = relPathToBabylonJSFolder },
+                                      page.Request.Url.Scheme);
+
+            return url;
+        }
+        
         public static IHtmlString BabylonJSScript(this WebViewPage page, 
             string relPathToBabylonJSFolder)
         {
             if (page == null)
                 return null;
-            
+
             var type = "text/javascript";
-            var src = page.Url.Action(BuildOurOwnBabylonJSController.GetFileContentActionName,
-                                      "BuildOurOwnBabylonJS", 
-                                      new { rootPath = page.ViewBag.BabylonJSFolder, relPath = relPathToBabylonJSFolder, type = type });
+
+            var src = page.BabylonJSFile(relPathToBabylonJSFolder);
 
             var script = new TagBuilder("script");
             script.Attributes.Add("src", src);
