c14b0c03cafa3cbf5a58c62f550d4983a529567f
Gwenaël Hagenmuller <gwenael@stereograph.fr>
2014-04-03 17:59:39 +0200

    Tags system
    Tags can be added to ANY javascript object (not necessarily reserved to BabylonJS) by calling BABYLON.Tags.AddTagsTo.
    BABYLON.Tags.MatchesQuery can be used to check if a javascript object matches a "boolean" query:
       var myObject = {};
       BABYLON.Tags.AddTagsTo(myObject, "tag1 tag2");
       BABYLON.Tags.MatchesQuery(myObject, "tag1"); // => true
       BABYLON.Tags.MatchesQuery(myObject, "tag2"); // => true
       BABYLON.Tags.MatchesQuery(myObject, "tag3"); // => false
       BABYLON.Tags.MatchesQuery(myObject, "!tag3"); // => false
       BABYLON.Tags.MatchesQuery(myObject, "tag3 || tag1"); // => true
       BABYLON.Tags.MatchesQuery(myObject, "tag1 && tag2"); // => true
       BABYLON.Tags.MatchesQuery(myObject, "tag1 && !tag2"); // => false
       BABYLON.Tags.MatchesQuery(myObject, "(tag1 && !tag2) || !tag3"); // => true
    
       BABYLON.Tags.MatchesQuery(myObject, ""); // => true since it has at least one tag
    
       var myObject2 = {};
    
       BABYLON.Tags.MatchesQuery(myObject2, ""); // => false since it has no tag
    
       BABYLON.Tags.MatchesQuery(myObject, undefined); // => true
       BABYLON.Tags.MatchesQuery(myObject2, undefined); // => true
    
    BABYLON.Tags.MatchesQuery doesn't use eval which is prohibited in win8
    
    BABYLON.Scene.prototype.getMeshesByTags uses this system
    
    This system uses AndOrNotEvaluator

---
+++ Babylon/Tools/babylon.tags.js
@@ -0,0 +1,73 @@
+﻿"use strict";
+
+var BABYLON = BABYLON || {};
+
+(function () {
+    BABYLON.Tags = BABYLON.Tags || {};
+
+    BABYLON.Tags.EnableFor = function (obj) {
+        obj._tags = obj._tags || {};
+    };
+
+    BABYLON.Tags.HasTags = function (obj) {
+        if (!obj._tags) {
+            return false;
+        }
+        return !BABYLON.Tools.IsEmpty(obj._tags);
+    };
+
+    // the tags 'true' and 'false' are reserved and cannot be used as tags
+    // a tag cannot start with '||', '&&', and '!'
+    // it cannot contain whitespaces
+    BABYLON.Tags.AddTagsTo = function (obj, tagsString) {
+        var tags = tagsString.split(" ");
+        for (var t in tags) {
+            BABYLON.Tags._AddTagTo(obj, tags[t]);
+        }
+    };
+
+    BABYLON.Tags._AddTagTo = function (obj, tag) {
+        tag = tag.trim();
+
+        if (tag === "" || tag === "true" || tag === "false") {
+            return;
+        }
+
+        if (tag.match(/[\s]/) || tag.match(/^([!]|([|]|[&]){2})/)) {
+            return;
+        }
+
+        BABYLON.Tags.EnableFor(obj);
+        obj._tags[tag] = true;
+    };
+
+    BABYLON.Tags.RemoveTagsFrom = function (obj, tagsString) {
+        if (!BABYLON.Tags.HasTags(obj)) {
+            return;
+        }
+        var tags = tagsString.split(" ");
+        for (var t in tags) {
+            BABYLON.Tags._RemoveTagFrom(obj, tags[t]);
+        }
+    };
+    
+
+    BABYLON.Tags._RemoveTagFrom = function (obj, tag) {
+        delete obj._tags[tag];
+    };
+
+    BABYLON.Tags.MatchesQuery = function (obj, tagsQuery) {
+        if (tagsQuery === undefined) {
+            return true;
+        }
+
+        if (tagsQuery === "") {
+            return BABYLON.Tags.HasTags(obj);
+        }
+
+        return BABYLON.AndOrNotEvaluator.Eval(tagsQuery, function (r) {
+            return BABYLON.Tags.HasTags(obj) && obj._tags[r];
+        });
+    };
+
+})();
