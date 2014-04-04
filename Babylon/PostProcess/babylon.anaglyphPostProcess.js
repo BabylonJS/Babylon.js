ddc56f0a7cc96301fd103e4edf711ed03f36eac3
michael-korbas <michael@stereograph.fr>
2014-04-04 15:15:41 +0200

    Adding anaglyphPostProcess Adding new anaglyphCamera
    Former-commit-id: 67547afc68c40d399b557c6eaf84201bc796b4e8

---
+++ Babylon/PostProcess/babylon.anaglyphPostProcess.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+var BABYLON = BABYLON || {};
+
+(function () {
+    BABYLON.AnaglyphPostProcess = function (name, ratio, camera, samplingMode, engine, reusable) {
+        BABYLON.PostProcess.call(this, name, "anaglyph", null, ["leftSampler", "rightSampler"], ratio, camera, samplingMode, engine, reusable);
+    };
+
+    BABYLON.AnaglyphPostProcess.prototype = Object.create(BABYLON.PostProcess.prototype);
+
+})();
