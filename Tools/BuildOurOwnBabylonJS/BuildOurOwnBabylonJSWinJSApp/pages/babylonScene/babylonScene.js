877728d60b2a251ae8239767a41bf1db10118b46
Nicolas Obré <nicolas.obre@stereograph.fr>
2014-04-08 16:02:43 +0200

    WinJS application now using listView to show available scenes. Listing all babylon demos scene in a group, and user customs scenes in an other group. Adding a back button to go back to scenes listing page.
    Former-commit-id: 0a4df8a7b5a4b711cfe2628abb69b0ffa444dcca

--- Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSWinJSApp/pages/babylonScene/babylonScene.js
+++ Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSWinJSApp/pages/babylonScene/babylonScene.js
@@ -9,17 +9,11 @@
             var canvas = element.querySelector("#babylonCanvas");
             engine = new BABYLON.Engine(canvas, true);
 
-            BABYLON.SceneLoader.Load("/BabylonJs-Demos/"+options.babylonFolder + "/", options.babylonFile, engine, function (scene) {
-                //scene = newScene;
-
+            BABYLON.SceneLoader.Load(options.babylonFolder + "/", options.babylonFile, engine, function (scene) {
                 scene.executeWhenReady(function () { 
                     if (!scene.activeCamera) {
-                        scene.activeCamera = new BABYLON.ArcRotateCamera("DefaultCamera", Math.PI / 2, 0, 10, new BABYLON.Vector3.Zero(), scene);
+                        scene.activeCamera = new BABYLON.ArcRotateCamera("DefaultCamera", -Math.PI / 2, Math.PI / 2, 10, new BABYLON.Vector3.Zero(), scene);
                         scene.activeCamera.zoomOn();
-                        /*var cube = BABYLON.Mesh.CreateSphere("test", 10, 1, scene);
-                        cube.material = new BABYLON.StandardMaterial("test", scene);
-                        cube.material.emissiveColor = new BABYLON.Color3(1.0, 0.0, 0.0);
-                        cube.position = scene.activeCamera.target;*/
                     }
                     if (scene.lights.length == 0) {
                         var light = new BABYLON.HemisphericLight("Default light", new BABYLON.Vector3(0, 1, 0), scene);
--- Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSWinJSApp/pages/home/home.js
+++ Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSWinJSApp/pages/home/home.js
@@ -1,13 +1,41 @@
 ﻿(function () {
     "use strict";
 
+    var itemsList = new WinJS.Binding.List();
+
+    function getGroupKey(dataItem){
+        return dataItem.group;
+    }
+
+    function getGroupData(dataItem){
+        return {
+            title: dataItem.group
+        };
+    }
+
+    var groupedItemsList = itemsList.createGrouped(getGroupKey, getGroupData);
+
+    WinJS.Namespace.define("home.babylonScenes", {
+        groupedItemsList: groupedItemsList
+    });
+    WinJS.Namespace.define("home.userScenes", {
+        data: new WinJS.Binding.List()
+    });
+
     WinJS.UI.Pages.define("/pages/home/home.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            Windows.ApplicationModel.Package.current.installedLocation.getFolderAsync("BabylonJS-Demos").then(function (folder) {
-                folder.getFoldersAsync().then(function (folders) {
-                    var ul = document.getElementById("babylonScenes");
+            element.querySelector("#babylonScenesListView").addEventListener("iteminvoked", function (event) {
+                event.detail.itemPromise.done(function (item) {
+                    WinJS.Navigation.navigate('/pages/babylonScene/babylonScene.html', { babylonFolder: item.data.folder, babylonFile: item.data.file });
+                });
+            });
+
+            var onSceneFolderOpenCallback = function (rootFolder) {
+                rootFolder.getFoldersAsync().then(function (folders) {
+                    var list = itemsList;
+                    var group = (rootFolder.name == "BabylonJS-Demos") ? "BabylonJS demos scenes" : "Your scenes"
                     folders.forEach(function (folder) {
                         folder.getFilesAsync().then(function (files) {
                             for (var j in files) {
@@ -19,29 +47,23 @@
                                 if (file.name.indexOf(".incremental.babylon") !== -1) {
                                     incremental = true;
                                 }
-                                var li = document.createElement("li");
-                                var button = document.createElement("button");
-                                button.className = "sceneButton";
-                                button.setAttribute("data-scene-folder", folder.name);
-                                button.setAttribute("data-scene-file", file.name);
-                                button.textContent = folder.displayName + ((incremental) ? " (incremental)" : "");
-                                li.innerHTML = button;
-                                ul.appendChild(button);
-
-                                button.addEventListener("click", onButtonClick)
+                                list.push({
+                                    title: folder.displayName.charAt(0).toUpperCase() + folder.displayName.slice(1),
+                                    text: file.name,
+                                    group: group,
+                                    folder: rootFolder.name + "/" + folder.name,
+                                    file: file.name
+                                });
                             }
                         });
                     });
                 });
-            }, function (error) {
-                var test = true;
-            });
-
-            var onButtonClick = function (event) {
-                var dataFolder = event.srcElement.getAttribute("data-scene-folder");
-                var dataFile = event.srcElement.getAttribute("data-scene-file")
-                WinJS.Navigation.navigate('/pages/babylonScene/babylonScene.html', { babylonFolder: dataFolder, babylonFile: dataFile });
             };
+
+            if (itemsList.length == 0) {
+                Windows.ApplicationModel.Package.current.installedLocation.getFolderAsync("BabylonJS-Demos").then(onSceneFolderOpenCallback);
+                Windows.ApplicationModel.Package.current.installedLocation.getFolderAsync("addToPackage").then(onSceneFolderOpenCallback);
+            }
         }
     });
 })();
