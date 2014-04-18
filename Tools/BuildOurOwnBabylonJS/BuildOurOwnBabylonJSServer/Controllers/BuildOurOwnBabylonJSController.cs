ba91db4594af0300f587da9cc10a52b2294d3de8
Gwenaël Hagenmuller <gwenael@stereograph.fr>
2014-04-18 18:02:09 +0200

    Secured BuildOurOwnBabylonJSController actions Call BuildOurOwnBabylonJS.exe once BuildOurOwnBabylonJSWinJSApp has been built
    Note: whereas ourOwnBabylon.js is regenerated when F5 is pressed for BuildOurOwnBabylonJSServer, it's not when F5 is pressed for BuildOurOwnBabylonJSWinJSApp (you have to first build the app)
    
    Former-commit-id: 1e2fc4fe93d905350d3c61da238759ba132a53fa

--- Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSServer/Controllers/BuildOurOwnBabylonJSController.cs
+++ Tools/BuildOurOwnBabylonJS/BuildOurOwnBabylonJSServer/Controllers/BuildOurOwnBabylonJSController.cs
@@ -15,44 +15,44 @@ namespace BuildOurOwnBabylonJSServer.Controllers
         [ActionName(BuildOurOwnBabylonJSController.GetFileContentActionName)]
         public ActionResult GetFileContent(string rootPath, string relPath)
         {
-            var babylonJSPath = Path.Combine(Server.MapPath("~"), rootPath);
-
-            var absPath = Path.Combine(babylonJSPath, relPath);
-
-            var type = "";
-            var extension = "";
-
-            if (!String.IsNullOrEmpty(relPath))
-                extension = Path.GetExtension(relPath).ToLower();
-
-            switch (extension)
-            {
-                case ".js":
-                case ".babylon":
-                case ".manifest":
-                    type = "text/javascript";
-                    break;
-
-                case ".png":
-                    type = "image/png";
-                    break;
-
-                case ".jpeg":
-                case ".jpg":
-                    type = "image/jpeg";
-                    break;
-
-                case ".bmp":
-                    type = "image/bmp";
-                    break;
-
-                default:
-                    type = "text/plain";
-                    break;
-            }
-
             try
             {
+                var babylonJSPath = Path.Combine(Server.MapPath("~"), rootPath);
+
+                var absPath = Path.Combine(babylonJSPath, relPath);
+
+                var type = "";
+                var extension = "";
+
+                if (!String.IsNullOrEmpty(relPath))
+                    extension = Path.GetExtension(relPath).ToLower();
+
+                switch (extension)
+                {
+                    case ".js":
+                    case ".babylon":
+                    case ".manifest":
+                        type = "text/javascript";
+                        break;
+
+                    case ".png":
+                        type = "image/png";
+                        break;
+
+                    case ".jpeg":
+                    case ".jpg":
+                        type = "image/jpeg";
+                        break;
+
+                    case ".bmp":
+                        type = "image/bmp";
+                        break;
+
+                    default:
+                        type = "text/plain";
+                        break;
+                }
+            
                 return File(new FileStream(absPath, FileMode.Open), type);
             }
             catch
@@ -64,27 +64,37 @@ namespace BuildOurOwnBabylonJSServer.Controllers
         [ActionName(BuildOurOwnBabylonJSController.GetBabylonScenesActionName)]
         public string GetBabylonScenes(string rootPath)
         {
-            var dir = new DirectoryInfo(rootPath);
-            var subDirs = dir.GetDirectories();
-            var files = new List<JObject>();
+            try
+            {
+                var dir = new DirectoryInfo(rootPath);
+                var subDirs = dir.GetDirectories();
+                var files = new List<JObject>();
 
-            foreach (var directory in subDirs) {
-                var babylonFiles = directory.GetFiles("*.babylon");
-                if (babylonFiles.Length == 0) {
-                    continue;
+                foreach (var directory in subDirs)
+                {
+                    var babylonFiles = directory.GetFiles("*.babylon");
+
+                    if (babylonFiles.Length == 0)
+                        continue;
+
+                    foreach (var file in babylonFiles)
+                    {
+                        var linkName = directory.Name + (file.Name.Contains(".incremental.babylon") ? " - Incremental" : "");
+                        files.Add(new JObject(
+                            new JProperty("url", Url.Action("Index", "BabylonJSDemo", new { demoFolderName = directory.Name, demoFile = file.Name })),
+                            new JProperty("linkName", linkName)
+                        ));
+                    }
                 }
 
-                foreach (var file in babylonFiles) {
-                    var linkName = directory.Name + (file.Name.Contains(".incremental.babylon") ? " - Incremental" : "");
-                    files.Add(new JObject(
-                        new JProperty("url", Url.Action("Index", "BabylonJSDemo", new { demoFolderName = directory.Name, demoFile = file.Name })),
-                        new JProperty("linkName", linkName)
-                    ));
-                }
+                var json = new JObject(new JProperty("files", files));
+                return json.ToString(Newtonsoft.Json.Formatting.None);
             }
-
-            var json = new JObject(new JProperty("files", files));
-            return json.ToString(Newtonsoft.Json.Formatting.None);
+            catch
+            {
+                var json = new JObject(new JProperty("files", ""));
+                return json.ToString(Newtonsoft.Json.Formatting.None);
+            }
         }
     }
 }
