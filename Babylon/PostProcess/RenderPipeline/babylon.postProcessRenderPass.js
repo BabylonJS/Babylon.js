0118ddd4beaeee0e59f5172dd9c140e325b02ab0
David Catuhe <david.catuhe@live.fr>
2014-05-05 08:59:54 -0700

    Fixing small issues
    Former-commit-id: 5c62be19839ea8437f576d1d8aedf1655c99f325

--- Babylon/PostProcess/RenderPipeline/babylon.postProcessRenderPass.js
+++ Babylon/PostProcess/RenderPipeline/babylon.postProcessRenderPass.js
@@ -1,50 +1,49 @@
-﻿"use strict";
+﻿var BABYLON;
+(function (BABYLON) {
+    var PostProcessRenderPass = (function () {
+        function PostProcessRenderPass(scene, name, size, renderList, beforeRender, afterRender) {
+            this.name = name;
+            this._enabled = true;
+            this._refCount = 0;
+            this._renderTexture = new BABYLON.RenderTargetTexture(name, size, scene);
+            this.setRenderList(renderList);
 
-var BABYLON = BABYLON || {};
+            this._renderTexture.onBeforeRender = beforeRender;
+            this._renderTexture.onAfterRender = afterRender;
 
-(function () {
-	BABYLON.PostProcessRenderPass = function PostProcessRenderPass(scene, name, size, renderList, beforeRender, afterRender) {
-		this._name = name;
-		this._enabled = true;
+            this._scene = scene;
+        }
+        PostProcessRenderPass.prototype.incRefCount = function () {
+            if (this._refCount == 0) {
+                this._scene.customRenderTargets.push(this._renderTexture);
+            }
 
-		this._renderList = renderList;
+            return ++this._refCount;
+        };
 
-		this._renderTexture = new BABYLON.RenderTargetTexture(name, size, scene);
-		this.setRenderList(renderList);
+        PostProcessRenderPass.prototype.decRefCount = function () {
+            this._refCount--;
 
-		this._renderTexture.onBeforeRender = beforeRender;
-		this._renderTexture.onAfterRender = afterRender;
+            if (this._refCount <= 0) {
+                this._scene.customRenderTargets.splice(this._scene.customRenderTargets.indexOf(this._renderTexture), 1);
+            }
 
-		this._scene = scene;
+            return this._refCount;
+        };
 
-		this._refCount = 0;
-	};
+        PostProcessRenderPass.prototype.setRenderList = function (renderList) {
+            this._renderTexture.renderList = renderList;
+        };
 
-	BABYLON.PostProcessRenderPass.prototype.incRefCount = function () {
-	    if (this._refCount == 0) {
-	        this._scene.customRenderTargets.push(this._renderTexture);
-	    }
+        PostProcessRenderPass.prototype.getRenderTexture = function () {
+            return this._renderTexture;
+        };
 
-	    this._refCount++;
-	};
-
-	BABYLON.PostProcessRenderPass.prototype.decRefCount = function () {
-	    this._refCount--;
-
-	    if (this._refCount <= 0) {
-	        this._scene.customRenderTargets.splice(this._scene.customRenderTargets.indexOf(this._renderTexture), 1);
-	    }
-	};
-
-	BABYLON.PostProcessRenderPass.prototype.setRenderList = function (renderList) {
-		this._renderTexture.renderList = renderList;
-	};
-
-	BABYLON.PostProcessRenderPass.prototype.getRenderTexture = function () {
-		return this._renderTexture;
-	};
-
-	BABYLON.PostProcessRenderPass.prototype._update = function () {
-		this.setRenderList(this._renderList);
-	};
-})();
+        PostProcessRenderPass.prototype._update = function () {
+            this.setRenderList(this._renderList);
+        };
+        return PostProcessRenderPass;
+    })();
+    BABYLON.PostProcessRenderPass = PostProcessRenderPass;
+})(BABYLON || (BABYLON = {}));
+//# sourceMappingURL=babylon.postProcessRenderPass.js.map
--- Babylon/Tools/babylon.tools.ts
+++ Babylon/Tools/babylon.tools.ts
@@ -11,6 +11,9 @@
         height: number;
     }
 
+    // Screenshots
+    var screenshotCanvas: HTMLCanvasElement;
+
     // FPS
     var fpsRange = 60;
     var previousFramesDuration = [];
@@ -83,7 +86,7 @@
             };
         }
 
-        public static MakeArray(obj, allowsNullUndefined?: boolean): Array<any> {
+        public static MakeArray(obj, allowsNullUndefined: boolean): Array<any> {
             if (allowsNullUndefined !== true && (obj === undefined || obj == null))
                 return undefined;
 
@@ -332,6 +335,110 @@
 
                 fps = 1000.0 / (sum / (length - 1));
             }
+        }
+
+        public static CreateScreenshot(engine: Engine, camera: Camera, size: any): void {
+            var width: number;
+            var height: number;
+
+            //If a zoom value is specified
+            if (size.zoom) {
+                width = Math.round(engine.getRenderWidth() * size.zoom);
+                height = Math.round(width / engine.getAspectRatio(camera));
+                size = { width: width, height: height };
+            }
+            else if (size.width && size.height) {
+                width = size.width;
+                height = size.height;
+            }
+            //If passing only width, computing height to keep display canvas ratio.
+            else if (size.width && !size.height) {
+                width = size.width;
+                height = Math.round(width / engine.getAspectRatio(camera));
+                size = { width: width, height: height };
+            }
+            //If passing only height, computing width to keep display canvas ratio.
+            else if (size.height && !size.width) {
+                height = size.height;
+                width = Math.round(height * engine.getAspectRatio(camera));
+                size = { width: width, height: height };
+            }
+            //Assuming here that "size" parameter is a number
+            else if (!isNaN(size)) {
+                height = size;
+                width = size;
+            }
+            else {
+                console.error("Invalid 'size' parameter !");
+            }
+
+            var texture = new RenderTargetTexture("screenShot", size, engine.scenes[0]);
+            texture.renderList = engine.scenes[0].meshes;
+
+            texture.onAfterRender = () => {
+                // Read the contents of the framebuffer
+                var numberOfChannelsByLine = width * 4;
+                var halfHeight = height / 2;
+
+                //Reading datas from WebGL
+                var data = engine.readPixels(0, 0, width, height);
+
+
+                //To flip image on Y axis.
+                for (var i = 0; i < halfHeight; i++) {
+                    for (var j = 0; j < numberOfChannelsByLine; j++) {
+                        var currentCell = j + i * numberOfChannelsByLine;
+                        var targetLine = height - i - 1;
+                        var targetCell = j + targetLine * numberOfChannelsByLine;;
+
+                        var temp = data[currentCell];
+                        data[currentCell] = data[targetCell];
+                        data[targetCell] = temp;
+                    }
+                }
+
+                // Create a 2D canvas to store the result
+                if (!screenshotCanvas) {
+                    screenshotCanvas = document.createElement('canvas');
+                }
+                screenshotCanvas.width = width;
+                screenshotCanvas.height = height;
+                var context = screenshotCanvas.getContext('2d');
+
+                // Copy the pixels to a 2D canvas
+                var imageData = context.createImageData(width, height);
+                imageData.data.set(data);
+                context.putImageData(imageData, 0, 0);
+
+                var base64Image = screenshotCanvas.toDataURL();
+
+                //Creating a link if the browser have the download attribute on the a tag, to automatically start download generated image.
+                if (("download" in document.createElement("a"))) {
+                    var a = window.document.createElement("a");
+                    a.href = base64Image;
+                    var date = new Date();
+                    var stringDate = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate() + "-" + date.getHours() + ":" + date.getMinutes();
+                    a.setAttribute("download", "screenshot-" + stringDate);
+
+                    window.document.body.appendChild(a);
+
+                    a.addEventListener("click", () => {
+                        a.parentElement.removeChild(a);
+                    });
+                    a.click();
+
+                    //Or opening a new tab with the image if it is not possible to automatically start download.
+                } else {
+                    var newWindow = window.open("");
+                    var img = newWindow.document.createElement("img");
+                    img.src = base64Image;
+                    newWindow.document.body.appendChild(img);
+                }
+
+            };
+
+            texture.render();
+            texture.dispose();
         }
     }
 } 
