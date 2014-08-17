ec511525039958ce7d7fc9fdf6e499ceab436671
Alex Bogartz <alex@jumpkick.pro>
2014-08-16 17:14:07 -0700

    Update babylon.followCamera.ts
    Former-commit-id: 4e90d1594c4f1466490a46d644f3e5a67c317ac9

--- Babylon/Cameras/babylon.followCamera.ts
+++ Babylon/Cameras/babylon.followCamera.ts
@@ -20,7 +20,7 @@
             if (!cameraTarget)
                 return;
 
-            var radians:number = this.getRadians(cameraTarget.rotation.y - this.rotationOffset);
+            var radians:number = this.getRadians(this.rotationOffset)+cameraTarget.rotation.y;
             var targetX:number = cameraTarget.position.x + Math.sin(radians) * this.radius;
 
             var targetZ:number = cameraTarget.position.z + Math.cos(radians) * this.radius;
