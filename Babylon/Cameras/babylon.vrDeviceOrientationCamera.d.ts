dbca6c038161077c12d8bcb17a2e9e4c00c2c902
David Catuhe <david.catuhe@live.fr>
2015-01-03 12:37:01 -0800

    .

--- Babylon/Cameras/babylon.vrDeviceOrientationCamera.d.ts
+++
@@ -1,9 +0,0 @@
-declare module BABYLON {
-    class VRDeviceOrientationCamera extends OculusCamera {
-        public _alpha: number;
-        public _beta: number;
-        public _gamma: number;
-        constructor(name: string, position: Vector3, scene: Scene);
-        public _onOrientationEvent(evt: DeviceOrientationEvent): void;
-    }
-}
