79a221e0c1c4f2cae910a15c7f3936b8222fec72
Vousk-prod <vousk@free.fr>
2015-02-03 20:13:55 +0100

    debug layer : better layout and events management

--- Babylon/Debug/babylon.debugLayer.ts
+++ Babylon/Debug/babylon.debugLayer.ts
@@ -450,6 +450,8 @@
 
             var boundingBoxesCheckbox = document.createElement("input");
             boundingBoxesCheckbox.type = "checkbox";
+            boundingBoxesCheckbox.style.pointerEvents = "auto";
+            boundingBoxesCheckbox.style.setProperty ("width", "25px", "important");
             boundingBoxesCheckbox.checked = initialState;
 
             boundingBoxesCheckbox.addEventListener("change", (evt: Event) => {
@@ -467,6 +469,8 @@
 
             var boundingBoxesRadio = document.createElement("input");
             boundingBoxesRadio.type = "radio";
+            boundingBoxesRadio.style.pointerEvents = "auto";
+            boundingBoxesRadio.style.setProperty ("width", "25px", "important");  
             boundingBoxesRadio.name = name;
             boundingBoxesRadio.checked = initialState;
 
@@ -487,7 +491,8 @@
             this._globalDiv.style.fontFamily = "Segoe UI, Arial";
             this._globalDiv.style.fontSize = "14px";
             this._globalDiv.style.color = "white";
-
+            this._globalDiv.style.pointerEvents = "none"; 
+            
             // Drawing canvas
             this._drawingCanvas = document.createElement("canvas");
             this._drawingCanvas.id = "DebugLayerDrawingCanvas";
@@ -539,6 +544,7 @@
                 this._logDiv.style.background = background;
                 this._logDiv.style.padding = "0px 0px 0px 5px";
                 this._logDiv.style.display = "none";
+                this._logDiv.style.pointerEvents = "auto";
                 this._generateheader(this._logDiv, "LOGS");
                 this._logSubsetDiv = document.createElement("div");
                 this._logSubsetDiv.style.height = "127px";
