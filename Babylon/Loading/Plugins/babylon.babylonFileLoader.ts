4725c5757a082d5cb1f0dd7658438be16cf41004
Raanan Weber <raananw+github@gmail.com>
2015-03-06 00:33:13 +0100

    Fixes for ArcRotate loading
    The file loader was also missing the function to set the target vector
    in case no target mesh id was given.
    
    
    Former-commit-id: cf77623abfbe70d0dfd5a2d88c94ecebc03c048b

--- Babylon/Loading/Plugins/babylon.babylonFileLoader.ts
+++ Babylon/Loading/Plugins/babylon.babylonFileLoader.ts
@@ -454,7 +454,12 @@
 
         // Target
         if (parsedCamera.target) {
-            camera.setTarget(BABYLON.Vector3.FromArray(parsedCamera.target));
+            if (camera.setTarget) {
+                camera.setTarget(BABYLON.Vector3.FromArray(parsedCamera.target));
+            } else {
+                //For ArcRotate
+                camera.target = BABYLON.Vector3.FromArray(parsedCamera.target);
+            }
         } else {
             camera.rotation = BABYLON.Vector3.FromArray(parsedCamera.rotation);
         }
