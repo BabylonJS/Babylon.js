1757ac21519ae1431b920964fda901621d7f6899
Vousk-prod <vousk@free.fr>
2015-03-07 04:25:17 +0100

    add collection

---
+++ Babylon/Tools/babylon.collection.ts
@@ -0,0 +1,31 @@
+﻿module BABYLON {
+    export class Collection {
+        public count: number = 0;
+        public items: any;
+
+        constructor() {
+            this.items = {};
+        }
+
+        public add(key: string, item: any): number {        
+            if (this.items[key] != undefined) {
+                return undefined;
+            }
+            this.items[key] = item;
+            return ++this.count;
+        }
+     
+        public remove(key: string): number {
+            if (this.items[key] == undefined) {
+                return undefined;
+            }
+            delete this.items[key];
+            return --this.count;
+        }
+ 
+        public item(key: string): any {
+            return this.items[key];
+        }
+
+    }
+} 
