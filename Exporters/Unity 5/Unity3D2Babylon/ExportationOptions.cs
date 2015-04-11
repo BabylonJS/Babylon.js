bab379852bfed1bacf5fe3ee69c65fb88d027de8
David Catuhe <david.catuhe@live.fr>
2015-04-11 07:52:42 -0700

    Moving to beta First release of Unity 5 exporter
    Former-commit-id: c54ced8cd0f917f230e85fd5f43b6c0f5ab98bd8

---
+++ Exporters/Unity 5/Unity3D2Babylon/ExportationOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity3D2Babylon
+{
+    public class ExportationOptions
+    {
+        public string DefaultFolder { get; set; }
+        public float ReflectionDefaultLevel { get; set; }
+        public bool ExportCollisions { get; set; }
+        public SerializableVector3 CameraEllipsoid { get; set; }
+        public SerializableVector3 Gravity { get; set; }
+
+        public ExportationOptions()
+        {
+            DefaultFolder = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            ReflectionDefaultLevel = 0.3f;
+            ExportCollisions = false;
+            CameraEllipsoid = new Vector3(0.5f, 1.0f, 0.5f);
+            Gravity = new Vector3(0, -0.9f, 0);
+        }
+    }
+}
