89df831a55773b98595470323b7b10a8eaac3f47
Raanan Weber <raananw+github@gmail.com>
2015-04-30 23:11:14 +0200

    Updating build tools with new file
    collisionCoordinator was added to both the babylon XML and the gulp
    file.

--- Tools/Gulp/gulpfile.js
+++ Tools/Gulp/gulpfile.js
@@ -78,6 +78,7 @@ gulp.task('scripts', ['shaders'] ,function() {
       '../../Babylon/Lights/babylon.directionalLight.js',
       '../../Babylon/Lights/Shadows/babylon.shadowGenerator.js',
       '../../Babylon/Collisions/babylon.collider.js',
+	  '../../Babylon/Collisions/babylon.collisionCoordinator.js',
       '../../Babylon/Cameras/babylon.camera.js',
       '../../Babylon/Cameras/babylon.targetCamera.js',
       '../../Babylon/Cameras/babylon.freeCamera.js',
