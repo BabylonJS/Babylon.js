77a42749ecd56502b6ffda0797d2cdf27c46434e
Raanan Weber <raananw+github@gmail.com>
2015-05-08 22:26:54 +0200

    Applied only if apply-gravity is on
    Gravity is only applied when gravity is on. Bug fix.
    
    
    Former-commit-id: 65e1d0f98a869c4ce92acb245061bc70984fea4b

--- Babylon/Cameras/babylon.freeCamera.ts
+++ Babylon/Cameras/babylon.freeCamera.ts
@@ -190,7 +190,9 @@
             this._collider.radius = this.ellipsoid;
 
             //add gravity to the velocity to prevent the dual-collision checking
-            velocity.addInPlace(this.getScene().gravity);
+            if(this.applyGravity) {
+                velocity.addInPlace(this.getScene().gravity);
+            }
 
             this.getScene().collisionCoordinator.getNewPosition(this._oldPosition, velocity, this._collider, 3, null, this._onCollisionPositionChange, this.uniqueId);
             
