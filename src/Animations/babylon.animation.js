3cb777f557ade6bdafdb6ffeb2f6b18d03eea232
Vousk-prod <vousk@free.fr>
2015-07-15 12:25:07 +0200

    Added easingFunction param in Animation.CreateAndStartAnimation (quick animation method)
    Former-commit-id: 22f9dda30379ac160b50f052027bf24448f4fd4b

--- src/Animations/babylon.animation.js
+++ src/Animations/babylon.animation.js
@@ -14,7 +14,7 @@ var BABYLON;
             this.dataType = dataType;
             this.loopMode = loopMode === undefined ? Animation.ANIMATIONLOOPMODE_CYCLE : loopMode;
         }
-        Animation.CreateAndStartAnimation = function (name, mesh, targetProperty, framePerSecond, totalFrame, from, to, loopMode) {
+        Animation.CreateAndStartAnimation = function (name, mesh, targetProperty, framePerSecond, totalFrame, from, to, loopMode, easingFunction) {
             var dataType = undefined;
             if (!isNaN(parseFloat(from)) && isFinite(from)) {
                 dataType = Animation.ANIMATIONTYPE_FLOAT;
@@ -39,6 +39,9 @@ var BABYLON;
             keys.push({ frame: 0, value: from });
             keys.push({ frame: totalFrame, value: to });
             animation.setKeys(keys);
+            if (easingFunction !== undefined) {
+                animation.setEasingFunction(easingFunction);
+            }
             mesh.animations.push(animation);
             return mesh.getScene().beginAnimation(mesh, 0, totalFrame, (animation.loopMode === 1));
         };
