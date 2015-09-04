660d08d36495fd7fc23b667fa9cf182eb86b9c5e
David catuhe <david.catuhe@live.fr>
2015-09-04 13:38:20 -0700

    Fixed readPixels bug
    Former-commit-id: ba47b3d22f106a13d936dae908f4dc678c255f84

--- src/babylon.engine.js
+++ src/babylon.engine.js
@@ -1803,7 +1803,7 @@ var BABYLON;
         };
         Engine.prototype.readPixels = function (x, y, width, height) {
             var data = new Uint8Array(height * width * 4);
-            this._gl.readPixels(0, 0, width, height, this._gl.RGBA, this._gl.UNSIGNED_BYTE, data);
+            this._gl.readPixels(x, y, width, height, this._gl.RGBA, this._gl.UNSIGNED_BYTE, data);
             return data;
         };
         // Dispose
