a1db7e387451eec55cef5fb1d93c74d4bc5f7668
Ben Riordan <ben@benriordan.com>
2015-10-15 15:06:01 -0700

    Update babylon.2.2.d.ts
    I'm not sure how you're generating the distribution babylon.2.2.d.ts so this may need to also be edited elsewhere, but it resolves the following error: error TS2339: Property 'uScale' does not exist on type 'BaseTexture' when it is a valid property.

--- dist/babylon.2.2.d.ts
+++ dist/babylon.2.2.d.ts
@@ -5849,6 +5849,8 @@ declare module BABYLON {
         coordinatesMode: number;
         wrapU: number;
         wrapV: number;
+        uScale: number;
+        vScale: number;
         anisotropicFilteringLevel: number;
         _cachedAnisotropicFilteringLevel: number;
         private _scene;
