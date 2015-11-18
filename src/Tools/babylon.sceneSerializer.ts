eaf46680ba6c1e1d02d4bf42dbcac5f6b7a7a07a
David <david.pe@aliceadsl.fr>
2015-11-18 23:53:32 +0100

    Update babylon.sceneSerializer.ts
    add collisionsEnabled and workerCollisions

--- src/Tools/babylon.sceneSerializer.ts
+++ src/Tools/babylon.sceneSerializer.ts
@@ -801,7 +801,9 @@
             serializationObject.clearColor = scene.clearColor.asArray();
             serializationObject.ambientColor = scene.ambientColor.asArray();
             serializationObject.gravity = scene.gravity.asArray();
-
+            serializationObject.collisionsEnabled = scene.collisionsEnabled;
+            serializationObject.workerCollisions = scene.workerCollisions;
+            
             // Fog
             if (scene.fogMode && scene.fogMode !== 0) {
                 serializationObject.fogMode = scene.fogMode;
