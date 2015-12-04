6bdf3ae5977636d3d009dda392f8af634968ba8f
KK <wyklion@qq.com>
2015-12-04 17:25:44 +0800

    Update babylon.particleSystem.ts
    Former-commit-id: ecf5d1e6ebf97ef5b347f06a795265545e66a667

--- src/Particles/babylon.particleSystem.ts
+++ src/Particles/babylon.particleSystem.ts
@@ -450,7 +450,7 @@
             var serializationObject: any = {};
 
             serializationObject.name = this.name;
-            if (this.emittor.position) {
+            if (this.emitter.position) {
                 serializationObject.emitterId = this.emitter.id;
             } else {
                 serializationObject.emitter = this.emitter.asArray();;
