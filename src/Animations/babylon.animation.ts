302a12211f5ba5e87170db16b1be293375fbaaeb
Temechon <temechon@pixelcodr.com>
2016-01-15 14:59:38 +0100

    Fixed bug when an animation event was accessing new animated value

--- src/Animations/babylon.animation.ts
+++ src/Animations/babylon.animation.ts
@@ -453,6 +453,9 @@
             var currentFrame = returnValue ? from + ratio % range : to;
             var currentValue = this._interpolate(currentFrame, repeatCount, this.loopMode, offsetValue, highLimitValue);
 
+            // Set value
+            this.setValue(currentValue);
+
             // Check events
             for (var index = 0; index < this._events.length; index++) {
                 if (currentFrame >= this._events[index].frame) {
@@ -471,9 +474,6 @@
                     this._events[index].isDone = false;
                 }
             }
-
-            // Set value
-            this.setValue(currentValue);
 
             if (!returnValue) {
                 this._stopped = true;
