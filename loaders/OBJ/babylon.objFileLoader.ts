d33d031d36057029ef19f1e626a9a0c2acd1ede0
Sébastien Vandenberghe <sevan@microsoft.com>
2016-03-19 01:11:24 +0000

    Add more obj support
    Former-commit-id: 05364c7f524f3f5808899a4304e89c6b73b59b71

--- loaders/OBJ/babylon.objFileLoader.ts
+++ loaders/OBJ/babylon.objFileLoader.ts
@@ -204,13 +204,13 @@ module BABYLON {
         // vt float float
         public uvPattern = /vt( +[\d|\.|\+|\-|e|E]+)( +[\d|\.|\+|\-|e|E]+)/;
         // f vertex vertex vertex ...
-        public facePattern1 = /f\s(([\d]{1,}[\s]?){3,})+/;
+        public facePattern1 = /f\s+(([\d]{1,}[\s]?){3,})+/;
         // f vertex/uvs vertex/uvs vertex/uvs ...
-        public facePattern2 = /f\s((([\d]{1,}\/[\d]{1,}[\s]?){3,})+)/;
+        public facePattern2 = /f\s+((([\d]{1,}\/[\d]{1,}[\s]?){3,})+)/;
         // f vertex/uvs/normal vertex/uvs/normal vertex/uvs/normal ...
-        public facePattern3 = /f\s((([\d]{1,}\/[\d]{1,}\/[\d]{1,}[\s]?){3,})+)/;
+        public facePattern3 = /f\s+((([\d]{1,}\/[\d]{1,}\/[\d]{1,}[\s]?){3,})+)/;
         // f vertex//normal vertex//normal vertex//normal ...
-        public facePattern4 = /f\s((([\d]{1,}\/\/[\d]{1,}[\s]?){3,})+)/;
+        public facePattern4 = /f\s+((([\d]{1,}\/\/[\d]{1,}[\s]?){3,})+)/;
 
 
         /**
