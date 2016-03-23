b3745055f1fd7b4e437f2a003d2d9e655a2c847f
Temechon <temechon@pixelcodr.com>
2016-03-23 15:45:15 +0100

    Merge all vr camera in one file
    Former-commit-id: 981b0f29bbbf10e9cbcec10e157b984f381b550d

--- src/Cameras/VR/babylon.vrDeviceOrientationCamera.ts
+++ src/Cameras/VR/babylon.vrDeviceOrientationCamera.ts
@@ -16,4 +16,21 @@ module BABYLON {
             return "VRDeviceOrientationFreeCamera";
         }
     }
+    
+    export class VRDeviceOrientationArcRotateCamera extends ArcRotateCamera {        
+
+        constructor(name: string, alpha: number, beta: number, radius: number, target: Vector3, scene: Scene, compensateDistortion = true) {
+            super(name, alpha, beta, radius, target, scene);
+
+            var metrics = VRCameraMetrics.GetDefault();
+            metrics.compensateDistortion = compensateDistortion;
+            this.setCameraRigMode(Camera.RIG_MODE_VR, { vrCameraMetrics: metrics });
+
+            this.inputs.addVRDeviceOrientation();
+        }
+
+        public getTypeName(): string {
+            return "VRDeviceOrientationArcRotateCamera";
+        }
+    }
 }
