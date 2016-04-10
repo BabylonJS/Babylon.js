a15ada63a56b91e9a2819153266c2c240f203b96
David Catuhe <david.catuhe@Live.fr>
2016-04-09 19:08:52 -0700

    Nightly
    Former-commit-id: e759503b099a6fa5dbb2fc68fbdeefd37d34ff35

--- src/Physics/Plugins/babylon.oimoJSPlugin.js
+++ src/Physics/Plugins/babylon.oimoJSPlugin.js
@@ -100,7 +100,8 @@ var BABYLON;
                         x: impostor.object.rotationQuaternion.x,
                         y: impostor.object.rotationQuaternion.y,
                         z: impostor.object.rotationQuaternion.z,
-                        s: impostor.object.rotationQuaternion.w });
+                        s: impostor.object.rotationQuaternion.w
+                    });
                     var extendSize = i.getObjectExtendSize();
                     if (i === impostor) {
                         var center = impostor.getObjectCenter();
@@ -311,13 +312,15 @@ var BABYLON;
             }
         };
         OimoJSPlugin.prototype.setMotor = function (joint, speed, maxForce, motorIndex) {
-            var motor = motorIndex ? joint.physicsJoint.rotationalLimitMotor2 : joint.physicsJoint.rotationalLimitMotor1 || joint.physicsJoint.limitMotor;
+            //TODO separate rotational and transational motors.
+            var motor = motorIndex ? joint.physicsJoint.rotationalLimitMotor2 : joint.physicsJoint.rotationalLimitMotor1 || joint.physicsJoint.rotationalLimitMotor || joint.physicsJoint.limitMotor;
             if (motor) {
                 motor.setMotor(speed, maxForce);
             }
         };
         OimoJSPlugin.prototype.setLimit = function (joint, upperLimit, lowerLimit, motorIndex) {
-            var motor = motorIndex ? joint.physicsJoint.rotationalLimitMotor2 : joint.physicsJoint.rotationalLimitMotor1 || joint.physicsJoint.limitMotor;
+            //TODO separate rotational and transational motors.
+            var motor = motorIndex ? joint.physicsJoint.rotationalLimitMotor2 : joint.physicsJoint.rotationalLimitMotor1 || joint.physicsJoint.rotationalLimitMotor || joint.physicsJoint.limitMotor;
             if (motor) {
                 motor.setLimit(upperLimit, lowerLimit === void 0 ? -upperLimit : lowerLimit);
             }
--- src/Physics/Plugins/babylon.oimoJSPlugin.ts
+++ src/Physics/Plugins/babylon.oimoJSPlugin.ts
@@ -25,7 +25,7 @@ module BABYLON {
 
         public executeStep(delta: number, impostors: Array<PhysicsImpostor>) {
 
-            impostors.forEach(function(impostor) {
+            impostors.forEach(function (impostor) {
                 impostor.beforeStep();
             });
 
@@ -96,8 +96,8 @@ module BABYLON {
 
                 var impostors = [impostor];
                 function addToArray(parent: IPhysicsEnabledObject) {
-                    if(!parent.getChildMeshes) return;
-                    parent.getChildMeshes().forEach(function(m) {
+                    if (!parent.getChildMeshes) return;
+                    parent.getChildMeshes().forEach(function (m) {
                         if (m.physicsImpostor) {
                             impostors.push(m.physicsImpostor);
                             m.physicsImpostor._init();
@@ -114,11 +114,12 @@ module BABYLON {
 
                     //get the correct bounding box
                     var oldQuaternion = i.object.rotationQuaternion;
-                    var rot = new OIMO.Euler().setFromQuaternion({ 
-                        x: impostor.object.rotationQuaternion.x, 
-                        y: impostor.object.rotationQuaternion.y, 
-                        z: impostor.object.rotationQuaternion.z, 
-                        s: impostor.object.rotationQuaternion.w });
+                    var rot = new OIMO.Euler().setFromQuaternion({
+                        x: impostor.object.rotationQuaternion.x,
+                        y: impostor.object.rotationQuaternion.y,
+                        z: impostor.object.rotationQuaternion.z,
+                        s: impostor.object.rotationQuaternion.w
+                    });
 
 
                     var extendSize = i.getObjectExtendSize();
@@ -283,10 +284,10 @@ module BABYLON {
             //So we will bruteforce it!
             try {
                 this.world.removeJoint(impostorJoint.joint.physicsJoint);
-            } catch(e) {
+            } catch (e) {
                 Tools.Warn(e);
             }
-            
+
         }
 
         public isSupported(): boolean {
