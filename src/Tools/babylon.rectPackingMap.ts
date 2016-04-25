10ea44291492732ef44493d69488febe6447c1ce
David Catuhe <david.catuhe@live.fr>
2016-04-25 16:36:47 -0700

    Fixed babylon.rectPackingMap declaration order
    Former-commit-id: 835aea4234887ef45ca2f2cc126416f34991f245

--- src/Tools/babylon.rectPackingMap.ts
+++ src/Tools/babylon.rectPackingMap.ts
@@ -1,46 +1,9 @@
 ﻿module BABYLON {
-    /**
-     * The purpose of this class is to pack several Rectangles into a big map, while trying to fit everything as optimaly as possible.
-     * This class is typically used to build lightmaps, sprite map or to pack several little textures into a big one.
-     * Note that this class allows allocated Rectangles to be freed: that is the map is dynamically maintained so you can add/remove rectangle based on their lifecycle.
-     */
-    export class RectPackingMap extends PackedRect {
-        /**
-         * Create an instance of the object with a dimension using the given size
-         * @param size The dimension of the rectangle that will contain all the sub ones.
-         */
-        constructor(size: Size) {
-            super(null, null, Vector2.Zero(), size);
-
-            this._root = this;
-        }
-
-        /**
-         * Add a rectangle, finding the best location to store it into the map
-         * @param size the dimension of the rectangle to store
-         * @return the Node containing the rectangle information, or null if we couldn't find a free spot
-         */
-        public addRect(size: Size): PackedRect {
-            var node = this.findAndSplitNode(size);
-            return node;
-        }
-
-        /**
-         * Return the current space free normalized between [0;1]
-         * @returns {} 
-         */
-        public get freeSpace(): number {
-            var freeSize = 0;
-            freeSize = this.evalFreeSize(freeSize);
-
-            return freeSize / (this._size.width * this._size.height);
-        }
-    }
 
     /**
-     * This class describe a rectangle that were added to the map.
-     * You have access to its coordinates either in pixel or normalized (UV)
-     */
+  * This class describe a rectangle that were added to the map.
+  * You have access to its coordinates either in pixel or normalized (UV)
+  */
     export class PackedRect {
         constructor(root: PackedRect, parent: PackedRect, pos: Vector2, size: Size) {
             this._pos = pos;
@@ -216,5 +179,41 @@
         protected _size: Size;
     }
 
+    /**
+     * The purpose of this class is to pack several Rectangles into a big map, while trying to fit everything as optimaly as possible.
+     * This class is typically used to build lightmaps, sprite map or to pack several little textures into a big one.
+     * Note that this class allows allocated Rectangles to be freed: that is the map is dynamically maintained so you can add/remove rectangle based on their lifecycle.
+     */
+    export class RectPackingMap extends PackedRect {
+        /**
+         * Create an instance of the object with a dimension using the given size
+         * @param size The dimension of the rectangle that will contain all the sub ones.
+         */
+        constructor(size: Size) {
+            super(null, null, Vector2.Zero(), size);
 
+            this._root = this;
+        }
+
+        /**
+         * Add a rectangle, finding the best location to store it into the map
+         * @param size the dimension of the rectangle to store
+         * @return the Node containing the rectangle information, or null if we couldn't find a free spot
+         */
+        public addRect(size: Size): PackedRect {
+            var node = this.findAndSplitNode(size);
+            return node;
+        }
+
+        /**
+         * Return the current space free normalized between [0;1]
+         * @returns {} 
+         */
+        public get freeSpace(): number {
+            var freeSize = 0;
+            freeSize = this.evalFreeSize(freeSize);
+
+            return freeSize / (this._size.width * this._size.height);
+        }
+    }
 }
