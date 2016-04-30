18fcc2a00bb63788a87d3bcb60db1d63c09a93ba
David Catuhe <david.catuhe@live.fr>
2016-04-30 08:24:21 -0700

    Update babylon.solidParticleSystem.ts

--- src/Particles/babylon.solidParticleSystem.ts
+++ src/Particles/babylon.solidParticleSystem.ts
@@ -382,7 +382,7 @@
 
         /**
         * Adds some particles to the SPS from the model shape. Returns the shape id.   
-        * Please read the doc : http://doc.babylonjs.com/tutorials/Solid_Particle_System#create-an-immutable-sps
+        * Please read the doc : http://doc.babylonjs.com/overviews/Solid_Particle_System#create-an-immutable-sps
         * `mesh` is any `Mesh` object that will be used as a model for the solid particles.
         * `nb` (positive integer) the number of particles to be created from this model
         * `positionFunction` is an optional javascript function to called for each particle on SPS creation. 
