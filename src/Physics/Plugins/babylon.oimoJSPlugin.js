a7cd75dc833b2643f6e1d470a822744bc496e72b
David Catuhe <david.catuhe@live.fr>
2016-06-13 09:58:56 -0700

    Nightly

--- src/Physics/Plugins/babylon.oimoJSPlugin.js
+++ src/Physics/Plugins/babylon.oimoJSPlugin.js
@@ -258,6 +258,7 @@ var BABYLON;
                     impostor.object.position.copyFrom(impostor.physicsBody.getPosition());
                 }
                 impostor.object.rotationQuaternion.copyFrom(impostor.physicsBody.getQuaternion());
+                impostor.object.rotationQuaternion.normalize();
             }
         };
         OimoJSPlugin.prototype.setPhysicsBodyTransformation = function (impostor, newPosition, newRotation) {
