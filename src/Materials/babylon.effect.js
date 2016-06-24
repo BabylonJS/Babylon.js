5889ec958ebe2b5366d43e4193bf6918ba6c034b
David Catuhe <david.catuhe@live.fr>
2016-06-23 18:09:16 -0700

    Nightly
    Former-commit-id: 2c02ef55927650964c422f25db134c28ae527e54

--- src/Materials/babylon.effect.js
+++ src/Materials/babylon.effect.js
@@ -46,7 +46,7 @@ var BABYLON;
             return currentDefines;
         };
         return EffectFallbacks;
-    })();
+    }());
     BABYLON.EffectFallbacks = EffectFallbacks;
     var Effect = (function () {
         function Effect(baseName, attributesNames, uniformsNames, samplers, engine, defines, fallbacks, onCompiled, onError, indexParameters) {
@@ -520,6 +520,6 @@ var BABYLON;
         Effect.ShadersStore = {};
         Effect.IncludesShadersStore = {};
         return Effect;
-    })();
+    }());
     BABYLON.Effect = Effect;
 })(BABYLON || (BABYLON = {}));
