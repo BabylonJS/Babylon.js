ea35b9f30f0beed1f7e7f7eef0243b141f8c6d84
= <=>
2016-07-01 23:16:43 +0100

    Add custom shadow generator support
    Former-commit-id: 1a978c61bab63b1fcea21e84eaf86b2659e9dfb7

--- src/Tools/babylon.sceneSerializer.ts
+++ src/Tools/babylon.sceneSerializer.ts
@@ -334,8 +334,10 @@
             for (index = 0; index < scene.lights.length; index++) {
                 light = scene.lights[index];
 
-                if (light.getShadowGenerator()) {
-                    serializationObject.shadowGenerators.push(light.getShadowGenerator().serialize());
+                let shadowGenerator = light.getShadowGenerator();
+                // Only support serialization for official generator so far.
+                if (shadowGenerator && shadowGenerator instanceof ShadowGenerator) {
+                    serializationObject.shadowGenerators.push(<ShadowGenerator>shadowGenerator.serialize());
                 }
             }
 
