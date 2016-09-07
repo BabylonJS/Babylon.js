5a0a8987fd23774d0794499a523a3a558dd2868a
nockawa <loic.baumann@outlook.com>
2016-09-07 18:00:22 +0200

    Move Geometry.RandomId() to Tools and keep the former as a shortcut for legacy reasons.
    Former-commit-id: 85764c2c5add40c7652cb3ca322530bf2ae0a5ed

--- src/Tools/babylon.tools.ts
+++ src/Tools/babylon.tools.ts
@@ -750,6 +750,18 @@
             return false;
         }
 
+        /**
+         * Implementation from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#answer-2117523
+         * Be aware Math.random() could cause collisions, but:
+         * "All but 6 of the 128 bits of the ID are randomly generated, which means that for any two ids, there's a 1 in 2^^122 (or 5.3x10^^36) chance they'll collide"
+         */
+        public static RandomId(): string {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+                var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+
         // Logs
         private static _NoneLogLevel = 0;
         private static _MessageLogLevel = 1;
