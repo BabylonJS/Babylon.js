818eb351d356f26f3821debb78c475c795a91567
nockawa <loic.baumann@outlook.com>
2016-09-29 00:12:02 +0200

    Revert "Canvas2D: hide WorldSpaceCanvas' mesh when Canvas is hidden"
    This reverts commit edcb4d8609998c52860381e6e5f3334b1464cb9e [formerly 653df865451b9f38fdebc9809a6c978223d5b477].
    
    
    Former-commit-id: 51040e8665bb7476efffd44f34a18590f10da412

--- src/Canvas2d/babylon.canvas2d.ts
+++ src/Canvas2d/babylon.canvas2d.ts
@@ -1688,15 +1688,7 @@
                 this._worldSpaceNode = settings.customWorldSpaceNode;
                 this.applyCachedTexture(null, null);
             }
-
-            this.propertyChanged.add((e, st) => {
-                let mesh = this._worldSpaceNode as AbstractMesh;
-                if (mesh) {
-                    mesh.isVisible = this.isVisible;
-                }
-            }, Prim2DBase.isVisibleProperty.flagId);
         }
-
     }
 
     @className("ScreenSpaceCanvas2D")
