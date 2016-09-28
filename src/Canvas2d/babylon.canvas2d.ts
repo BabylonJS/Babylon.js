edcb4d8609998c52860381e6e5f3334b1464cb9e
nockawa <loic.baumann@outlook.com>
2016-09-28 23:04:30 +0200

    Canvas2D: hide WorldSpaceCanvas' mesh when Canvas is hidden
    Former-commit-id: 653df865451b9f38fdebc9809a6c978223d5b477

--- src/Canvas2d/babylon.canvas2d.ts
+++ src/Canvas2d/babylon.canvas2d.ts
@@ -1688,7 +1688,15 @@
                 this._worldSpaceNode = settings.customWorldSpaceNode;
                 this.applyCachedTexture(null, null);
             }
+
+            this.propertyChanged.add((e, st) => {
+                let mesh = this._worldSpaceNode as AbstractMesh;
+                if (mesh) {
+                    mesh.isVisible = this.isVisible;
+                }
+            }, Prim2DBase.isVisibleProperty.flagId);
         }
+
     }
 
     @className("ScreenSpaceCanvas2D")
