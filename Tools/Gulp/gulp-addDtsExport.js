7f432c4ba85e0a81e8b0cf997e77d6c456a31d97
Ole Rehmsen <ole@rehmsen.de>
2016-09-28 22:19:54 +0200

    Add "export = BABYLON;" to the end of d.ts file so that it can be imported with an import statement.

---
+++ Tools/Gulp/gulp-addDtsExport.js
@@ -0,0 +1,29 @@
+var gutil = require('gulp-util');
+var through = require('through2');
+
+module.exports = function (varName) {
+    return through.obj(function (file, enc, cb) {
+
+        var moduleExportsAddition = '\nexport = ' + varName + ';\n';
+
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            //streams not supported, no need for now.
+            return;
+        }
+
+        try {
+            file.contents = new Buffer(String(file.contents) + moduleExportsAddition);
+            this.push(file);
+
+        } catch (err) {
+            this.emit('error', new gutil.PluginError('gulp-add-module-exports', err, { fileName: file.path }));
+        }
+        cb();
+    });
+};
+
