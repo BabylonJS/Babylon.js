20f2fe0cf8e6f1722c02ce1ef1b6f19ae679ef59
sevan <sevan@SEVANSBP>
2016-11-29 21:18:24 -0800

    Dev tools 2

---
+++ Tools/DevLoader/BabylonLoader.js
@@ -0,0 +1,128 @@
+// Old Fashion Way for IE 11 Devs. Yes, that still exists ;-)
+
+var BABYLONDEVTOOLS;
+(function (BABYLONDEVTOOLS) {
+    
+    var getJson = function(url, callback, errorCallback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                var data = JSON.parse(xhr.response);
+                callback(data)
+            } else {
+                errorCallback({
+                status: this.status,
+                statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            errorCallback({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    }
+
+    var Loader = (function () {
+        var queue;
+        var callback = null;
+
+        function Loader() {
+            queue = [];
+        }
+
+        Loader.prototype.onReady = function (callback) {
+            this.callback = callback;
+        }
+
+        Loader.prototype.dequeue = function () {
+            if (queue.length == 0) {
+                console.log('Scripts loaded');
+                BABYLON.Engine.ShadersRepository = "/src/Shaders/"; 
+                if (this.callback) {                    
+                    this.callback();
+                }
+                return;                
+            }
+
+            var url = queue.shift();
+            
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = url;
+
+            var self = this;
+            script.onload = function() {
+                self.dequeue();
+            };
+            head.appendChild(script);
+        }
+
+        Loader.prototype.loadScript = function (url) {
+            queue.push(url);
+        }
+
+        Loader.prototype.loadScripts = function (urls) {
+            for (var i = 0; i< urls.length; i++) {
+                this.loadScript(urls[i]);
+            }
+        }
+
+        Loader.prototype.loadLibrary = function (library) {
+            var i = 0;
+            for (; i < library.files.length; i++) {
+                var file = library.files[i];
+                file = file.replace('.ts', '.js');
+                this.loadScript(file);
+            }
+
+            if (library.shaderFiles && library.shaderFiles.length > 0) {
+                var shaderFile = library.shaderFiles[0];
+                var endDirectoryIndex = shaderFile.lastIndexOf('/');
+                shaderFile = shaderFile.substring(0, endDirectoryIndex + 1);
+                shaderFile += library.output.replace('.js', '.js.fx');
+                this.loadScript(shaderFile);
+            }
+        }
+
+        Loader.prototype.loadModule = function (module) {
+            for (var i = 0; i< module.libraries.length; i++) {
+                this.loadLibrary(module.libraries[i]);
+            }
+        }
+
+        Loader.prototype.loadBJSScripts = function (settings) {
+            this.loadScripts(settings.core.files);
+            this.loadScripts(settings.extras.files);
+            
+            for (var i = 0; i< settings.modules.length; i++) {
+                this.loadModule(settings[settings.modules[i]]);
+            }
+        }
+
+        Loader.prototype.load = function () {
+            var self = this;
+            getJson('/Tools/Gulp/config.json',
+                function(data) {
+                    self.loadBJSScripts(data);
+                    self.dequeue();
+                },
+                function(reason) { 
+                    console.error(reason);
+                });
+
+        };
+
+        return Loader;
+    }());    
+
+    var loader = new Loader();
+    loader.load();
+
+    BABYLONDEVTOOLS.Loader = loader;
+
+})(BABYLONDEVTOOLS || (BABYLONDEVTOOLS = {}))
---
+++ dist/preview release/proceduralTexturesLibrary/babylon.starfieldProceduralTexture.js
@@ -0,0 +1,167 @@
+/// <reference path="../../../dist/preview release/babylon.d.ts"/>
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var BABYLON;
+(function (BABYLON) {
+    var StarfieldProceduralTexture = (function (_super) {
+        __extends(StarfieldProceduralTexture, _super);
+        function StarfieldProceduralTexture(name, size, scene, fallbackTexture, generateMipMaps) {
+            _super.call(this, name, size, "starfieldProceduralTexture", scene, fallbackTexture, generateMipMaps);
+            this._time = 1;
+            this._alpha = 0.5;
+            this._beta = 0.8;
+            this._zoom = 0.8;
+            this._formuparam = 0.53;
+            this._stepsize = 0.1;
+            this._tile = 0.850;
+            this._brightness = 0.0015;
+            this._darkmatter = 0.400;
+            this._distfading = 0.730;
+            this._saturation = 0.850;
+            this.updateShaderUniforms();
+        }
+        StarfieldProceduralTexture.prototype.updateShaderUniforms = function () {
+            this.setFloat("time", this._time);
+            this.setFloat("alpha", this._alpha);
+            this.setFloat("beta", this._beta);
+            this.setFloat("zoom", this._zoom);
+            this.setFloat("formuparam", this._formuparam);
+            this.setFloat("stepsize", this._stepsize);
+            this.setFloat("tile", this._tile);
+            this.setFloat("brightness", this._brightness);
+            this.setFloat("darkmatter", this._darkmatter);
+            this.setFloat("distfading", this._distfading);
+            this.setFloat("saturation", this._saturation);
+        };
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "time", {
+            get: function () {
+                return this._time;
+            },
+            set: function (value) {
+                this._time = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "alpha", {
+            get: function () {
+                return this._alpha;
+            },
+            set: function (value) {
+                this._alpha = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "beta", {
+            get: function () {
+                return this._beta;
+            },
+            set: function (value) {
+                this._beta = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "formuparam", {
+            get: function () {
+                return this._formuparam;
+            },
+            set: function (value) {
+                this._formuparam = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "stepsize", {
+            get: function () {
+                return this._stepsize;
+            },
+            set: function (value) {
+                this._stepsize = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "zoom", {
+            get: function () {
+                return this._zoom;
+            },
+            set: function (value) {
+                this._zoom = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "tile", {
+            get: function () {
+                return this._tile;
+            },
+            set: function (value) {
+                this._tile = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "brightness", {
+            get: function () {
+                return this._brightness;
+            },
+            set: function (value) {
+                this._brightness = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "darkmatter", {
+            get: function () {
+                return this._darkmatter;
+            },
+            set: function (value) {
+                this._darkmatter = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "distfading", {
+            get: function () {
+                return this._distfading;
+            },
+            set: function (value) {
+                this._distfading = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(StarfieldProceduralTexture.prototype, "saturation", {
+            get: function () {
+                return this._saturation;
+            },
+            set: function (value) {
+                this._saturation = value;
+                this.updateShaderUniforms();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return StarfieldProceduralTexture;
+    }(BABYLON.ProceduralTexture));
+    BABYLON.StarfieldProceduralTexture = StarfieldProceduralTexture;
+})(BABYLON || (BABYLON = {}));
+
+//# sourceMappingURL=babylon.starfieldProceduralTexture.js.map
+
+BABYLON.Effect.ShadersStore['starfieldProceduralTexturePixelShader'] = "precision highp float;\n\n#define volsteps 20\n#define iterations 15\nvarying vec2 vPosition;\nvarying vec2 vUV;\nuniform float time;\nuniform float alpha;\nuniform float beta;\nuniform float zoom;\nuniform float formuparam;\nuniform float stepsize;\nuniform float tile;\nuniform float brightness;\nuniform float darkmatter;\nuniform float distfading;\nuniform float saturation;\nvoid main()\n{\nvec3 dir=vec3(vUV*zoom,1.);\nfloat localTime=time*0.0001;\n\nmat2 rot1=mat2(cos(alpha),sin(alpha),-sin(alpha),cos(alpha));\nmat2 rot2=mat2(cos(beta),sin(beta),-sin(beta),cos(beta));\ndir.xz*=rot1;\ndir.xy*=rot2;\nvec3 from=vec3(1.,.5,0.5);\nfrom+=vec3(-2.,localTime*2.,localTime);\nfrom.xz*=rot1;\nfrom.xy*=rot2;\n\nfloat s=0.1,fade=1.;\nvec3 v=vec3(0.);\nfor (int r=0; r<volsteps; r++) {\nvec3 p=from+s*dir*.5;\np=abs(vec3(tile)-mod(p,vec3(tile*2.))); \nfloat pa,a=pa=0.;\nfor (int i=0; i<iterations; i++) {\np=abs(p)/dot(p,p)-formuparam; \na+=abs(length(p)-pa); \npa=length(p);\n}\nfloat dm=max(0.,darkmatter-a*a*.001); \na*=a*a; \nif (r>6) fade*=1.-dm; \n\nv+=fade;\nv+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; \nfade*=distfading; \ns+=stepsize;\n}\nv=mix(vec3(length(v)),v,saturation); \ngl_FragColor=vec4(v*.01,1.);\n}";
