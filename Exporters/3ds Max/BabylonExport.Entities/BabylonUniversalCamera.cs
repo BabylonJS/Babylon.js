8016f466069d2374e0b989fa72d9c466ea8253f9
MackeyK24 <MackeyK24@gmail.com>
2016-12-10 20:05:49 -1000

    Added Worker Collisions And Universal Camera Support
    Enables Worker Collisions And Ortho Mode And Stereo Serialization
    Options

---
+++ Exporters/3ds Max/BabylonExport.Entities/BabylonUniversalCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+namespace BabylonExport.Entities
+{
+    [DataContract]
+    public class BabylonUniversalCamera : BabylonCamera
+    {
+        [DataMember]
+        public int mode { get; set; }
+
+        [DataMember]
+        public float? orthoLeft { get; set; }
+
+        [DataMember]
+        public float? orthoRight { get; set; }
+
+        [DataMember]
+        public float? orthoBottom { get; set; }
+
+        [DataMember]
+        public float? orthoTop { get; set; }
+
+        [DataMember]
+        public bool isStereoscopicSideBySide;
+
+        public BabylonUniversalCamera()
+        {
+            this.mode = 0;
+            this.orthoLeft = null;
+            this.orthoRight = null;
+            this.orthoBottom = null;
+            this.orthoTop = null;
+            this.type = "BABYLON.UniversalCamera";
+        }
+    }
+}
