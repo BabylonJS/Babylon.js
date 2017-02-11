a3f9f4d07a9f01d04e470d9b32c37ff4cc76c313
aWeirdo <thomasbeck@live.dk>
2017-02-11 02:18:14 +0100

    Update babylon.freeCamera.ts

--- src/Cameras/babylon.freeCamera.ts
+++ src/Cameras/babylon.freeCamera.ts
@@ -115,7 +115,7 @@
             globalPosition.subtractFromFloatsToRef(0, this.ellipsoid.y, 0, this._oldPosition);
             this._collider.radius = this.ellipsoid;
 
-            if(!this._collider.collisionFilter) this._collider.collisionFilter = this.collisionFilter ? this.collisionFilter : 1;
+	    this._collider._collisionMask = this._collisionMask ? this._collisionMask : -1;
 		
             //no need for clone, as long as gravity is not on.
             var actualVelocity = velocity;
