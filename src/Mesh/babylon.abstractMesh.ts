ae704e2d5ba65f7b7ff193ae6d137a532b8093f4
David <david.pe@aliceadsl.fr>
2017-02-25 13:56:50 +0100

    Update babylon.abstractMesh.ts

--- src/Mesh/babylon.abstractMesh.ts
+++ src/Mesh/babylon.abstractMesh.ts
@@ -644,19 +644,19 @@
          * @param {number} z
          * @param {number} maxHeight (default 200)
          */
-        public getHeightMeshAtCoordinates(x: number, z: number, maxHeight = 200): AbstractMesh {		
-		    var ray = new BABYLON.Ray(new BABYLON.Vector3(x, maxHeight, z), new BABYLON.Vector3(0, -1, 0), 2*maxHeight);
-		    var res = this.intersects(ray, true);
-		    return res.pickedPoint.y;
-	    }
+        public getHeightMeshAtCoordinates(x: number, z: number, maxHeight = 200): Vector3 {		
+            var ray = new BABYLON.Ray(new BABYLON.Vector3(x, maxHeight, z), new BABYLON.Vector3(0, -1, 0), 2*maxHeight);
+            var res = this.intersects(ray, true);
+            return res.pickedPoint.y;
+        }
          
         /**
          * Redefine updatable on a mesh
          */
-        public setUpdatable(updatable?: boolean): AbstractMesh {		
-		    this.setVerticesData(BABYLON.VertexBuffer.PositionKind, this.getVerticesData(BABYLON.VertexBuffer.PositionKind), updatable);
-		    this.setVerticesData(BABYLON.VertexBuffer.NormalKind, this.getVerticesData(BABYLON.VertexBuffer.NormalKind), updatable);		
-	    }
+        public setUpdatable(updatable?: boolean): void {		
+            this.setVerticesData(BABYLON.VertexBuffer.PositionKind, this.getVerticesData(BABYLON.VertexBuffer.PositionKind), updatable);
+            this.setVerticesData(BABYLON.VertexBuffer.NormalKind, this.getVerticesData(BABYLON.VertexBuffer.NormalKind), updatable);		
+        }
 
         // ================================== Point of View Movement =================================
         /**
