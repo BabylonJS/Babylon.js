40f1cc9d1898614606ed7381482490b50ff30923
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-04-11 22:08:23 +0200

    Fixed materials library: check if subMesh.effect is not undefined before returning true in readyForSubMesh
    Former-commit-id: adeb2f014d67a80e00811428504edeea81f96743

--- materialsLibrary/src/simple/babylon.simpleMaterial.ts
+++ materialsLibrary/src/simple/babylon.simpleMaterial.ts
@@ -76,7 +76,7 @@ module BABYLON {
             var defines = <SimpleMaterialDefines>subMesh._materialDefines;
             var scene = this.getScene();
 
-            if (!this.checkReadyOnEveryCall) {
+            if (!this.checkReadyOnEveryCall && subMesh.effect) {
                 if (this._renderId === scene.getRenderId()) {
                     return true;
                 }
