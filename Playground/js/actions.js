3d88508b84c7363f31d0b8b709528687cc334589
Temechon <temechon@gmail.com>
2017-04-16 15:30:28 +0200

    Playground - CSS theme updated
    Former-commit-id: 5084b3e54ed692ea42df864ac6885bca8f144990

---
+++ Playground/js/actions.js
@@ -0,0 +1,54 @@
+(function () {
+
+    var allSelect = document.querySelectorAll('.select');
+    var allToDisplay = document.querySelectorAll('.toDisplay');
+    var allToDisplayBig = document.querySelectorAll('.toDisplayBig');
+
+    // Remove displayed options
+    window.addEventListener('click', function () {
+        for (var a of allToDisplay) {
+            if (a.style.display == 'block') {
+                a.style.display = 'none';
+            }
+        }
+        for (var b of allToDisplayBig) {
+            if (b.style.display == 'block') {
+                b.style.display = 'none';
+            }
+        }
+    });
+
+    // Handle click on select elements
+    for (var s of allSelect) {
+        // Get child called to display
+        s.addEventListener('click', function (e) {
+            var toDisplay = this.querySelector('.toDisplay');
+            if (toDisplay) {
+                if (toDisplay.style.display == 'block') {
+                    toDisplay.style.display = 'none';
+                } else {
+                    toDisplay.style.display = 'block';
+                }
+            }
+            toDisplay = this.querySelector('.toDisplayBig');
+            if (toDisplay) {
+                if (toDisplay.style.display == 'block') {
+                    toDisplay.style.display = 'none';
+                } else {
+                    toDisplay.style.display = 'block';
+                }
+            }
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    }
+
+    document.querySelector('#safemodeToggle').addEventListener('click', function () {
+        this.classList.toggle('checked');
+        if (this.classList.contains('checked')) {
+            this.innerHTML = 'Safe mode <i class="fa fa-check-square" aria-hidden="true"></i>';
+        } else {
+            this.innerHTML = 'Safe mode <i class="fa fa-square-o" aria-hidden="true"></i>';
+        }
+    })
+})();
