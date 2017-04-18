fc2983bb77d3278dd112415aaed201dec091478d
David Catuhe <david.catuhe@live.fr>
2017-04-17 17:18:49 -0700

    PG fix
    Former-commit-id: a560805d68cf9bf99706e98a046e1f327c565755

--- Playground/js/actions.js
+++ Playground/js/actions.js
@@ -5,12 +5,14 @@
     var allToDisplayBig = document.querySelectorAll('.toDisplayBig');
 
     var removeAllOptions = function () {
-        for (var a of allToDisplay) {
+        for (var index = 0; index < allToDisplay.length; index++) {
+            var a = allToDisplay[index];
             if (a.style.display == 'block') {
                 a.style.display = 'none';
             }
         }
-        for (var b of allToDisplayBig) {
+        for (var index = 0; index < allToDisplayBig.length; index++) {
+            var b = allToDisplayBig[index];
             if (b.style.display == 'block') {
                 b.style.display = 'none';
             }
