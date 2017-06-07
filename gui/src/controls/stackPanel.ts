51170d1da30726e12b3a298afa44bd892a6f10e8
David Catuhe <david.catuhe@live.fr>
2017-06-07 09:45:42 -0700

    Fixes #2248
    Former-commit-id: a5e72c572f8441321c201c00c98c59b4ec557178

--- gui/src/controls/stackPanel.ts
+++ gui/src/controls/stackPanel.ts
@@ -3,6 +3,7 @@
 module BABYLON.GUI {
     export class StackPanel extends Container {
         private _isVertical = true;
+        private _tempMeasureStore = Measure.Empty();
 
         public get isVertical(): boolean {
             return this._isVertical;
@@ -21,34 +22,55 @@ module BABYLON.GUI {
             super(name);
         }
 
-        protected _additionalProcessing(parentMeasure: Measure, context: CanvasRenderingContext2D): void {
+        protected _preMeasure(parentMeasure: Measure, context: CanvasRenderingContext2D): void {
             var stack = 0;
             for (var child of this._children) {
+                this._tempMeasureStore.copyFrom(child._currentMeasure);
                 child._currentMeasure.copyFrom(parentMeasure);
                 child._measure();
                 
                 if (this._isVertical) {
                     child.top = stack + "px";
+                    if (!child._top.ignoreAdaptiveScaling) {
+                        child._markAsDirty();
+                    }
+                    child._top.ignoreAdaptiveScaling = true;
                     stack += child._currentMeasure.height;
                     child.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
                 } else {
                     child.left = stack + "px";
+                    if (!child._left.ignoreAdaptiveScaling) {
+                        child._markAsDirty();
+                    }                    
+                    child._left.ignoreAdaptiveScaling = true;
                     stack += child._currentMeasure.width;
                     child.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-
                 }
+
+                child._currentMeasure.copyFrom(this._tempMeasureStore);
             }
 
+            let panelChanged = false;
             if (this._isVertical) {
+                let previousHeight = this.height;
                 this.height = stack + "px";
+                
+                panelChanged = previousHeight !== this.height || !this._height.ignoreAdaptiveScaling;
+
                 this._height.ignoreAdaptiveScaling = true;
             } else {
+                let previousWidth = this.width;
                 this.width = stack + "px";
+                panelChanged = previousWidth !== this.width || !this._width.ignoreAdaptiveScaling;
+
                 this._width.ignoreAdaptiveScaling = true;
             }
-            
 
-            super._additionalProcessing(parentMeasure, context);
+            if (panelChanged) {
+                this._markAllAsDirty();
+            }
+            
+            super._preMeasure(parentMeasure, context);
         }    
     }    
 }
