d6b7ad8b3fe3aadaefa4eb173f2a1178123c9af1
David Catuhe <david.catuhe@live.fr>
2017-06-19 14:36:40 -0700

    Added separated kernel for mirror blur
    Former-commit-id: faad241492691f0fa5329359c61ced8104fba6a0

--- src/Materials/Textures/babylon.mirrorTexture.ts
+++ src/Materials/Textures/babylon.mirrorTexture.ts
@@ -10,7 +10,8 @@ module BABYLON {
 
         private _blurX: BlurPostProcess;
         private _blurY: BlurPostProcess;
-        private _blurKernel = 0;
+        private _blurKernelX = 0;
+        private _blurKernelY = 0;
         private _blurRatio = 1.0;
 
         public set blurRatio(value: number) {
@@ -27,17 +28,35 @@ module BABYLON {
         }
 
         public set blurKernel(value: number) {
-            if (this._blurKernel === value) {
+            this.blurKernelX = value;
+            this.blurKernelY = value;
+        }        
+
+        public set blurKernelX(value: number) {
+            if (this._blurKernelX === value) {
                 return;
             }
 
-            this._blurKernel = value;
+            this._blurKernelX = value;
             this._preparePostProcesses();
         }
 
-        public get blurKernel(): number {
-            return this._blurKernel;
+        public get blurKernelX(): number {
+            return this._blurKernelX;
         }        
+
+        public set blurKernelY(value: number) {
+            if (this._blurKernelY === value) {
+                return;
+            }
+
+            this._blurKernelY = value;
+            this._preparePostProcesses();
+        }
+
+        public get blurKernelY(): number {
+            return this._blurKernelY;
+        }             
 
         constructor(name: string, size: any, scene: Scene, generateMipMaps?: boolean, type: number = Engine.TEXTURETYPE_UNSIGNED_INT, samplingMode = Texture.BILINEAR_SAMPLINGMODE, generateDepthBuffer = true) {
             super(name, size, scene, generateMipMaps, true, type, false, samplingMode, generateDepthBuffer);
@@ -69,12 +88,12 @@ module BABYLON {
         private _preparePostProcesses(): void {
             this.clearPostProcesses(true);
 
-            if (this._blurKernel) {
+            if (this._blurKernelX && this._blurKernelY) {
                 var engine = this.getScene().getEngine();
 
                 var textureType = engine.getCaps().textureFloatRender ? Engine.TEXTURETYPE_FLOAT : Engine.TEXTURETYPE_HALF_FLOAT;
 
-                this._blurX = new BABYLON.BlurPostProcess("horizontal blur", new BABYLON.Vector2(1.0, 0), this._blurKernel, this._blurRatio, null, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false, textureType);
+                this._blurX = new BABYLON.BlurPostProcess("horizontal blur", new BABYLON.Vector2(1.0, 0), this._blurKernelX, this._blurRatio, null, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false, textureType);
                 this._blurX.autoClear = false;
 
                 if (this._blurRatio === 1) {
@@ -83,13 +102,9 @@ module BABYLON {
                     this._blurX.alwaysForcePOT = true;
                 }
 
-                this._blurY = new BABYLON.BlurPostProcess("vertical blur", new BABYLON.Vector2(0, 1.0), this._blurKernel, this._blurRatio, null, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false, textureType);
+                this._blurY = new BABYLON.BlurPostProcess("vertical blur", new BABYLON.Vector2(0, 1.0), this._blurKernelY, this._blurRatio, null, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false, textureType);
                 this._blurY.autoClear = false;
-                this._blurY.alwaysForcePOT = true;
-
-                if (this._blurRatio !== 1) {
-                    this._blurY.alwaysForcePOT = true;
-                }
+                this._blurY.alwaysForcePOT = this._blurRatio !== 1;
 
                 this.addPostProcess(this._blurX);
                 this.addPostProcess(this._blurY);   
