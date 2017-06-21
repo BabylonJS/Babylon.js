5a4e2d7654bca6c54018aeb4f815c72acd5f3b90
Sebastien Vandenberghe <vandenberghe.sebastien@gmail.com>
2017-06-20 20:42:03 -0700

    Quick Fix Directional

--- src/Lights/babylon.directionalLight.ts
+++ src/Lights/babylon.directionalLight.ts
@@ -169,22 +169,22 @@ module BABYLON {
          * Gets the minZ used for shadow according to both the scene and the light.
          * 
          * Values are fixed on directional lights as it relies on an ortho projection hence the need to convert being
-         * -1 and 1 to 0 and 1 doing (depth + min) / (min + max) -> (depth + 0.5) / (0.5 + 5) -> (depth + 0.5) * 0.5.
+         * -1 and 1 to 0 and 1 doing (depth + min) / (min + max) -> (depth + 1) / (1 + 1) -> (depth * 0.5) + 0.5.
          * @param activeCamera 
          */
         public getDepthMinZ(activeCamera: Camera): number {
-            return 0.5;
+            return 1;
         }
 
         /**
          * Gets the maxZ used for shadow according to both the scene and the light.
          * 
          * Values are fixed on directional lights as it relies on an ortho projection hence the need to convert being
-         * -1 and 1 to 0 and 1 doing (depth + min) / (min + max) -> (depth + 0.5) / (0.5 + 5) -> (depth + 0.5) * 0.5.
+         * -1 and 1 to 0 and 1 doing (depth + min) / (min + max) -> (depth + 1) / (1 + 1) -> (depth * 0.5) + 0.5.
          * @param activeCamera 
          */
         public getDepthMaxZ(activeCamera: Camera): number {
-             return 1.5;
+             return 1;
         }
     }
 }  
