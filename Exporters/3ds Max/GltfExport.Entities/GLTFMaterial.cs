84a726522bcd1dd28ef18da08e2456ae26d2f9d3
noalak <tambrun.fabrice@gmail.com>
2017-08-18 17:02:06 +0200

    Exporter 3ds Max > Add Node class to babylon entities | Add exporter to gltf format handling simple meshes and materials

---
+++ Exporters/3ds Max/GltfExport.Entities/GLTFMaterial.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Serialization;
+
+namespace GLTFExport.Entities
+{
+    [DataContract]
+    public class GLTFMaterial : GLTFIndexedChildRootProperty
+    {
+        public enum AlphaMode
+        {
+            OPAQUE,
+            MASK,
+            BLEND
+        }
+
+        [DataMember(EmitDefaultValue = false)]
+        public GLTFPBRMetallicRoughness pbrMetallicRoughness { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public GLTFTextureInfo normalTexture { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public GLTFTextureInfo occlusionTexture { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public GLTFTextureInfo emissiveTexture { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public float[] emissiveFactor { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string alphaMode { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public float? alphaCutoff { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public bool doubleSided { get; set; }
+
+        public string id;
+    }
+}
---
+++ Exporters/3ds Max/GltfExport.Entities/GLTFSampler.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization;
+
+namespace GLTFExport.Entities
+{
+    [DataContract]
+    public class GLTFSampler : GLTFIndexedChildRootProperty
+    {
+        public enum TextureMagFilter
+        {
+            NEAREST = 9728,
+            LINEAR = 9729
+        }
+
+        public enum TextureMinFilter
+        {
+            NEAREST = 9728,
+            LINEAR = 9729,
+            NEAREST_MIPMAP_NEAREST = 9984,
+            LINEAR_MIPMAP_NEAREST = 9985,
+            NEAREST_MIPMAP_LINEAR = 9986,
+            LINEAR_MIPMAP_LINEAR = 9987
+        }
+
+        public enum TextureWrapMode
+        {
+            CLAMP_TO_EDGE = 33071,
+            MIRRORED_REPEAT = 33648,
+            REPEAT = 10497
+        }
+
+        [DataMember(EmitDefaultValue = false)]
+        public TextureMagFilter? magFilter { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public TextureMinFilter? minFilter { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public TextureWrapMode? wrapS { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public TextureWrapMode? wrapT { get; set; }
+    }
+}
