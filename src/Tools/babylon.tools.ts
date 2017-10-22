2b4552f382550c3849ac573ab84faeab3a28ec4f
Gary Hsu <garyhsu@microsoft.com>
2017-10-22 13:01:39 -0700

    Change Tools.LoadFile to return XMLHttpRequest when available
    Former-commit-id: dcb3767bcbbb4d554e1806c3bd1c668261e405b7

--- src/Tools/babylon.tools.ts
+++ src/Tools/babylon.tools.ts
@@ -481,13 +481,15 @@
         }
 
         //ANY
-        public static LoadFile(url: string, callback: (data: any) => void, progressCallBack?: (data: any) => void, database?: Database, useArrayBuffer?: boolean, onError?: (request?: XMLHttpRequest, exception?: any) => void): void {
+        public static LoadFile(url: string, callback: (data: any) => void, progressCallBack?: (data: any) => void, database?: Database, useArrayBuffer?: boolean, onError?: (request?: XMLHttpRequest, exception?: any) => void): XMLHttpRequest {
             url = Tools.CleanUrl(url);
 
             url = Tools.PreprocessUrl(url);
 
+            var request: XMLHttpRequest;
+
             var noIndexedDB = () => {
-                var request = new XMLHttpRequest();
+                request = new XMLHttpRequest();
                 var loadUrl = Tools.BaseUrl + url;
                 request.open('GET', loadUrl, true);
 
@@ -550,6 +552,8 @@
                     noIndexedDB();
                 }
             }
+
+            return request;
         }
 
         /** 
