84784606edb834d693b8a7da3c19127b88f33477
Borut <bobalazek124@gmail.com>
2017-11-22 21:37:54 +0100

    Added updraft effect
    Former-commit-id: 17a3fe522eabd2be3acd35d17bab28b4e3587f44

--- src/Physics/babylon.physicsHelper.ts
+++ src/Physics/babylon.physicsHelper.ts
@@ -42,7 +42,6 @@ module BABYLON {
             var event = new PhysicsRadialExplosionEvent(this._scene);
 
             impostors.forEach(impostor => {
-
                 var impostorForceAndContactPoint = event.getImpostorForceAndContactPoint(impostor, origin, radius, strength, falloff);
                 if (!impostorForceAndContactPoint) {
                     return;
@@ -77,13 +76,12 @@ module BABYLON {
 
             impostors.forEach(impostor => {
                 var impostorForceAndContactPoint = event.getImpostorForceAndContactPoint(impostor, origin, radius, strength, falloff);
-
                 if (!impostorForceAndContactPoint) {
                     return;
                 }
 
                 impostor.applyForce(impostorForceAndContactPoint.force, impostorForceAndContactPoint.contactPoint);
-            })
+            });
 
             event.dispose(false);
 
@@ -113,6 +111,29 @@ module BABYLON {
 
             return event;
         }
+
+        /**
+         * @param {Vector3} origin the origin of the updraft
+         * @param {number} radius the radius of the updraft
+         * @param {number} strength the strength of the updraft
+         * @param {number} height the height of the updraft
+         */
+        public updraft(origin: Vector3, radius: number, strength: number, height: number): Nullable<PhysicsUpdraftEvent> {
+            if (!this._physicsEngine) {
+                Tools.Warn('Physics engine not enabled. Please enable the physics before you call the PhysicsHelper.');
+                return null;
+            }
+
+            if (this._physicsEngine.getImpostors().length === 0) {
+                return null;
+            }
+
+            var event = new PhysicsUpdraftEvent(this._physicsEngine, this._scene, origin, radius, strength, height);
+
+            event.dispose(false);
+
+            return event;
+        }
     }
 
     /***** Radial explosion *****/
@@ -120,7 +141,8 @@ module BABYLON {
     export class PhysicsRadialExplosionEvent {
 
         private _scene: Scene;
-        private _radialSphere: Mesh; // create a sphere, so we can get the intersecting meshes inside
+        private _sphere: Mesh; // create a sphere, so we can get the intersecting meshes inside
+        private _sphereOptions: { segments: number, diameter: number } = { segments: 32, diameter: 1 }; // TODO: make configurable
         private _rays: Array<Ray> = [];
         private _dataFetched: boolean = false; // check if the data has been fetched. If not, do cleanup
 
@@ -129,14 +151,14 @@ module BABYLON {
         }
 
         /**
-         * Returns the data related to the radial explosion event (radialSphere & rays).
+         * Returns the data related to the radial explosion event (sphere & rays).
          * @returns {PhysicsRadialExplosionEventData}
          */
         public getData(): PhysicsRadialExplosionEventData {
             this._dataFetched = true;
 
             return {
-                radialSphere: this._radialSphere,
+                sphere: this._sphere,
                 rays: this._rays,
             };
         }
@@ -155,7 +177,7 @@ module BABYLON {
                 return null;
             }
 
-            if (!this._intersectsWithRadialSphere(impostor, origin, radius)) {
+            if (!this._intersectsWithSphere(impostor, origin, radius)) {
                 return null;
             }
 
@@ -187,16 +209,16 @@ module BABYLON {
         }
 
         /**
-         * Disposes the radialSphere.
+         * Disposes the sphere.
          * @param {bolean} force
          */
         public dispose(force: boolean = true) {
             if (force) {
-                this._radialSphere.dispose();
+                this._sphere.dispose();
             } else {
                 setTimeout(() => {
                     if (!this._dataFetched) {
-                        this._radialSphere.dispose();
+                        this._sphere.dispose();
                     }
                 }, 0);
             }
@@ -204,38 +226,27 @@ module BABYLON {
 
         /*** Helpers ***/
 
-        private _prepareRadialSphere(): void {
-            if (!this._radialSphere) {
-                this._radialSphere = MeshBuilder.CreateSphere("radialSphere", { segments: 32, diameter: 1 }, this._scene);
-                this._radialSphere.isVisible = false;
+        private _prepareSphere(): void {
+            if (!this._sphere) {
+                this._sphere = MeshBuilder.CreateSphere("radialExplosionEventSphere", this._sphereOptions, this._scene);
+                this._sphere.isVisible = false;
             }
         }
 
-        private _intersectsWithRadialSphere(impostor: PhysicsImpostor, origin: Vector3, radius: number): boolean {
+        private _intersectsWithSphere(impostor: PhysicsImpostor, origin: Vector3, radius: number): boolean {
             var impostorObject = <Mesh>impostor.object;
 
-            this._prepareRadialSphere();
+            this._prepareSphere();
 
-            this._radialSphere.position = origin;
-            this._radialSphere.scaling = new Vector3(radius * 2, radius * 2, radius * 2);
-            this._radialSphere._updateBoundingInfo();
-            this._radialSphere.computeWorldMatrix(true);
+            this._sphere.position = origin;
+            this._sphere.scaling = new Vector3(radius * 2, radius * 2, radius * 2);
+            this._sphere._updateBoundingInfo();
+            this._sphere.computeWorldMatrix(true);
 
-            return this._radialSphere.intersectsMesh(impostorObject, true);
+            return this._sphere.intersectsMesh(impostorObject, true);
         }
 
     }
-
-    export interface PhysicsRadialExplosionEventData {
-        radialSphere: Mesh;
-        rays: Array<Ray>;
-    }
-
-    export interface PhysicsForceAndContactPoint {
-        force: Vector3;
-        contactPoint: Vector3;
-    }
-
 
     /***** Gravitational Field *****/
 
@@ -248,17 +259,10 @@ module BABYLON {
         private _strength: number;
         private _falloff: PhysicsRadialImpulseFallof;
         private _tickCallback: any;
-        private _radialSphere: Mesh;
+        private _sphere: Mesh;
         private _dataFetched: boolean = false; // check if the has been fetched the data. If not, do cleanup
 
-        constructor(
-            physicsHelper: PhysicsHelper,
-            scene: Scene,
-            origin: Vector3,
-            radius: number,
-            strength: number,
-            falloff: PhysicsRadialImpulseFallof = PhysicsRadialImpulseFallof.Constant
-        ) {
+        constructor(physicsHelper: PhysicsHelper, scene: Scene, origin: Vector3, radius: number, strength: number, falloff: PhysicsRadialImpulseFallof = PhysicsRadialImpulseFallof.Constant) {
             this._physicsHelper = physicsHelper;
             this._scene = scene;
             this._origin = origin;
@@ -269,14 +273,14 @@ module BABYLON {
         }
 
         /**
-         * Returns the data related to the gravitational field event (radialSphere).
+         * Returns the data related to the gravitational field event (sphere).
          * @returns {PhysicsGravitationalFieldEventData}
          */
         public getData(): PhysicsGravitationalFieldEventData {
             this._dataFetched = true;
 
             return {
-                radialSphere: this._radialSphere,
+                sphere: this._sphere,
             };
         }
 
@@ -296,16 +300,16 @@ module BABYLON {
         }
 
         /**
-         * Disposes the radialSphere.
+         * Disposes the sphere.
          * @param {bolean} force
          */
         public dispose(force: boolean = true) {
             if (force) {
-                this._radialSphere.dispose();
+                this._sphere.dispose();
             } else {
                 setTimeout(() => {
                     if (!this._dataFetched) {
-                        this._radialSphere.dispose();
+                        this._sphere.dispose();
                     }
                 }, 0);
             }
@@ -313,12 +317,12 @@ module BABYLON {
 
         private _tick() {
             // Since the params won't change, we fetch the event only once
-            if (this._radialSphere) {
+            if (this._sphere) {
                 this._physicsHelper.applyRadialExplosionForce(this._origin, this._radius, this._strength * -1, this._falloff);
             } else {
                 var radialExplosionEvent = this._physicsHelper.applyRadialExplosionForce(this._origin, this._radius, this._strength * -1, this._falloff);
                 if (radialExplosionEvent) {
-                    this._radialSphere = <Mesh>radialExplosionEvent.getData().radialSphere.clone('radialSphereClone');
+                    this._sphere = <Mesh>radialExplosionEvent.getData().sphere.clone('radialExplosionEventSphereClone');
                 }
             }
         }
@@ -326,7 +330,163 @@ module BABYLON {
     }
 
     export interface PhysicsGravitationalFieldEventData {
-        radialSphere: Mesh;
+        sphere: Mesh;
+    }
+
+    /***** Updraft *****/
+
+    export class PhysicsUpdraftEvent {
+
+        private _physicsEngine: PhysicsEngine;
+        private _scene: Scene;
+        private _origin: Vector3;
+        private _originTop: Vector3 = Vector3.Zero(); // the most upper part of the cylinder
+        private _radius: number;
+        private _strength: number;
+        private _height: number;
+        private _tickCallback: any;
+        private _cylinder: Mesh;
+        private _cylinderPosition: Vector3 = Vector3.Zero(); // to keep the cylinders position, because normally the origin is in the center and not on the bottom
+        private _rays: Array<Ray> = [];
+        private _dataFetched: boolean = false; // check if the has been fetched the data. If not, do cleanup
+ 
+        constructor(physicsEngine: PhysicsEngine, scene: Scene, origin: Vector3, radius: number, strength: number, height: number) {
+            this._physicsEngine = physicsEngine;
+            this._scene = scene;
+            this._origin = origin;
+            this._radius = radius;
+            this._strength = strength;
+            this._height = height;
+
+            this._origin.addToRef(new Vector3(0, this._height / 2, 0), this._cylinderPosition);
+            this._origin.addToRef(new Vector3(0, this._height, 0), this._originTop);
+ 
+            this._tickCallback = this._tick.bind(this);
+        }
+
+        /**
+         * Returns the data related to the updraft event (cylinder).
+         * @returns {PhysicsGravitationalFieldEventData}
+         */
+        public getData(): PhysicsUpdraftEventData {
+            this._dataFetched = true;
+
+            return {
+                cylinder: this._cylinder,
+                rays: this._rays,
+            };
+        }
+
+        /**
+         * Enables the updraft.
+         */
+        public enable() {
+            this._tickCallback.call(this);
+            this._scene.registerBeforeRender(this._tickCallback);
+        }
+
+        /**
+         * Disables the cortex.
+         */
+        public disable() {
+            this._scene.unregisterBeforeRender(this._tickCallback);
+        }
+
+        /**
+         * Disposes the sphere.
+         * @param {bolean} force
+         */
+        public dispose(force: boolean = true) {
+            if (force) {
+                this._cylinder.dispose();
+            } else {
+                setTimeout(() => {
+                    if (!this._dataFetched) {
+                        this._cylinder.dispose();
+                    }
+                }, 0);
+            }
+        }
+
+        private getImpostorForceAndContactPoint(impostor: PhysicsImpostor): Nullable<PhysicsForceAndContactPoint> {
+            if (impostor.mass === 0) {
+                return null;
+            }
+
+            if (!this._intersectsWithCylinder(impostor, this._origin, this._radius)) {
+                return null;
+            }
+
+            var impostorObject = (<Mesh>impostor.object);
+            var impostorObjectCenter = impostor.getObjectCenter();
+            var direction = impostorObjectCenter.subtract(this._originTop);
+
+            var ray = new Ray(this._originTop, direction, this._radius);
+            this._rays.push(ray);
+            var hit = ray.intersectsMesh(impostorObject);
+
+            var contactPoint = hit.pickedPoint;
+            if (!contactPoint) {
+                return null;
+            }
+
+            var multiplier = this._strength * -1;
+
+            var force = direction.multiplyByFloats(multiplier, multiplier, multiplier);
+
+            return { force: force, contactPoint: contactPoint };
+        }
+
+        private _tick() {
+            this._physicsEngine.getImpostors().forEach(impostor => {
+                var impostorForceAndContactPoint = this.getImpostorForceAndContactPoint(impostor);
+                if (!impostorForceAndContactPoint) {
+                    return;
+                }
+
+                impostor.applyForce(impostorForceAndContactPoint.force, impostorForceAndContactPoint.contactPoint);
+            });
+        }
+
+        /*** Helpers ***/
+
+        private _prepareCylinder(): void {
+            if (!this._cylinder) {
+                this._cylinder = MeshBuilder.CreateCylinder("updraftEventCylinder", {
+                    height: this._height,
+                    diameter: this._radius * 2,
+                }, this._scene);
+                this._cylinder.isVisible = false;
+            }
+        }
+
+        private _intersectsWithCylinder(impostor: PhysicsImpostor, origin: Vector3, radius: number): boolean {
+            var impostorObject = <Mesh>impostor.object;
+
+            this._prepareCylinder();
+
+            this._cylinder.position = this._cylinderPosition;
+
+            return this._cylinder.intersectsMesh(impostorObject, true);
+        }
+
+    }
+
+    /***** Data interfaces *****/
+
+    export interface PhysicsRadialExplosionEventData {
+        sphere: Mesh;
+        rays: Array<Ray>;
+    }
+
+    export interface PhysicsForceAndContactPoint {
+        force: Vector3;
+        contactPoint: Vector3;
+    }
+
+    export interface PhysicsUpdraftEventData {
+        cylinder: Mesh;
+        rays: Array<Ray>;
     }
 
 }
