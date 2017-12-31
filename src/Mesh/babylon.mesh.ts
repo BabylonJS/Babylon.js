ea4dc8038336913628faf6044a1aa863fa729e0a
Michael Schlotfeldt <michael@plaudit.com>
2017-12-31 10:42:20 -0500

    Switched getTotalVertices to be stricter if check.

--- src/Mesh/babylon.mesh.ts
+++ src/Mesh/babylon.mesh.ts
@@ -427,7 +427,7 @@
          * Returns a positive integer : the total number of vertices within the mesh geometry or zero if the mesh has no geometry.
          */
         public getTotalVertices(): number {
-            if (!this._geometry) {
+            if (this._geometry === null || this._geometry === undefined) {
                 return 0;
             }
             return this._geometry.getTotalVertices();
