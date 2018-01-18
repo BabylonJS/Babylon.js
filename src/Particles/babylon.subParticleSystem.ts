374f417260bfe7c98a583c8ec13145801162477b
Ibraheem Osama <ibraheem.osama@gmail.com>
2018-01-18 20:09:03 +1100

    Adding active sub systems
    Former-commit-id: 0bd23b4ff32c7fbd9f2f94fcb0bc8bdefb49ed48

--- src/Particles/babylon.subParticleSystem.ts
+++ src/Particles/babylon.subParticleSystem.ts
@@ -1,31 +1,36 @@
 module BABYLON {
     export class SubParticleSystem extends ParticleSystem {
-        constructor(name: string, capacity: number, scene: Scene, private _generation: number, private _parentParticleSystem: ParticleSystem, customEffect: Nullable<Effect> = null, _isAnimationSheetEnabled: boolean = false, epsilon: number = 0.01) {
+        public generationString: string;
+        constructor(name: string, capacity: number, scene: Scene, private _generation: number, private _rootParticleSystem: ParticleSystem, customEffect: Nullable<Effect> = null, _isAnimationSheetEnabled: boolean = false, epsilon: number = 0.01) {
             super(name, capacity, scene, customEffect, _isAnimationSheetEnabled, epsilon);
+            this.generationString = this._generation.toString();
         }
 
-        public stoppedEmitting(): void {
-            var generationString = this._generation.toString();
-            if (this._parentParticleSystem.stockSubSystems.contains(generationString)) {
-                (this._parentParticleSystem.stockSubSystems.get(generationString) as (Array<SubParticleSystem>)).push(this);
+        public stoppedEmitting(overrideRemove = false): void {
+
+            if (overrideRemove)
+                this._rootParticleSystem.activeSubSystems.remove(this.name);
+                
+            if (this._rootParticleSystem.stockSubSystems.contains(this.generationString)) {
+                (this._rootParticleSystem.stockSubSystems.get(this.generationString)!).push(this);
             }
             else {
                 var subSysArray = new Array<SubParticleSystem>();
                 subSysArray.push(this);
-                this._parentParticleSystem.stockSubSystems.add(generationString, subSysArray);
+                this._rootParticleSystem.stockSubSystems.add(this.generationString, subSysArray);
             }
         }
 
         public emitFromParticle(particle: Particle): void {
-            ParticleSystem.emitFromGeneration(this._parentParticleSystem, particle, this._generation + 1);
+            ParticleSystem.emitFromGeneration(this._rootParticleSystem, particle, this._generation + 1);
         }
 
         public recycleParticle(particle: Particle): void {
-            ParticleSystem.recycleParticle(this._parentParticleSystem, this, particle);
+            ParticleSystem.recycleParticle(this._rootParticleSystem, this, particle);
         }
 
         public createParticle(): Particle {
-            return ParticleSystem.createParticle(this._parentParticleSystem, this);
+            return ParticleSystem.createParticle(this._rootParticleSystem, this);
         }
     }
 }
