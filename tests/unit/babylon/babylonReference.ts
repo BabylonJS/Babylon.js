489913922e1a66c6adafcdb39c0fee0d7a67c461
Trevor Baron <trbaron@microsoft.com>
2018-01-19 17:12:51 -0800

    add unit test

--- tests/unit/babylon/babylonReference.ts
+++ tests/unit/babylon/babylonReference.ts
@@ -1,5 +1,5 @@
-/// <reference path="../../../dist/babylon.d.ts" />
-/// <reference path="../../../dist/loaders/babylon.glTF2FileLoader.d.ts" />
+/// <reference path="../../../dist/preview release/babylon.d.ts" />
+/// <reference path="../../../dist/preview release/loaders/babylon.glTF2FileLoader.d.ts" />
 /// <reference path="../../../dist/preview release/serializers/babylon.glTF2Serializer.d.ts" />
 
 /// <reference path="../node_modules/@types/chai/index.d.ts" />
