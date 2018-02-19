aac3088643131178af0c28d9bb52e40924d4cf53
Brian Zinn <github@wakeskate.com>
2018-02-19 09:41:31 -0800

    fix typedoc static analysis errors.
    Former-commit-id: 42b8ff6cea9f98536dc7769ed0cda0fe10be88b4

--- src/Gamepad/Controllers/babylon.daydreamController.ts
+++ src/Gamepad/Controllers/babylon.daydreamController.ts
@@ -3,9 +3,19 @@ module BABYLON {
      * Google Daydream controller
      */
     export class DaydreamController extends WebVRController {
-        public static MODEL_BASE_URL:string = 'https://controllers.babylonjs.com/generic/';
-        public static MODEL_FILENAME:string = 'generic.babylon';
+        /**
+         * Base Url for the controller model.
+         */
+        public static MODEL_BASE_URL: string = 'https://controllers.babylonjs.com/generic/';
 
+        /**
+         * File name for the controller model.
+         */
+        public static MODEL_FILENAME: string = 'generic.babylon';
+
+        /**
+         * Gamepad Id prefix used to identify Gear VR Controller.
+         */
         public static readonly GAMEPAD_ID_PREFIX: string = 'Daydream'; // id is 'Daydream Controller'
 
         constructor(vrGamepad: any) {
@@ -24,7 +34,7 @@ module BABYLON {
             });
         }
 
-        protected handleButtonChange(buttonIdx: number, state: ExtendedGamepadButton, changes: GamepadButtonChanges) {
+        protected _handleButtonChange(buttonIdx: number, state: ExtendedGamepadButton, changes: GamepadButtonChanges) {
             // Daydream controller only has 1 GamepadButton (on the trackpad).
             if (buttonIdx === 0) {
                 let observable = this.onTriggerStateChangedObservable;
