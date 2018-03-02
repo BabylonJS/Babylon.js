f3b77e508fbccd5edf43ee1bdf0220f4904fb484
Raanan Weber <info@raananweber.com>
2018-03-02 09:57:35 +0100

    animation configuration and model animation using both animation groups and skeleton animations

--- Viewer/src/model/viewerModel.ts
+++ Viewer/src/model/viewerModel.ts
@@ -1,12 +1,12 @@
-import { ISceneLoaderPlugin, ISceneLoaderPluginAsync, AnimationGroup, Animatable, AbstractMesh, Tools, Scene, SceneLoader, Observable, SceneLoaderProgressEvent, Tags, ParticleSystem, Skeleton } from "babylonjs";
+import { ISceneLoaderPlugin, ISceneLoaderPluginAsync, AnimationGroup, Animatable, AbstractMesh, Tools, Scene, SceneLoader, Observable, SceneLoaderProgressEvent, Tags, ParticleSystem, Skeleton, IDisposable, Nullable, Animation } from "babylonjs";
 import { IModelConfiguration } from "../configuration/configuration";
+import { IModelAnimation, GroupModelAnimation, AnimationPlayMode } from "./modelAnimation";
 
-export class ViewerModel {
+export class ViewerModel implements IDisposable {
 
     public loader: ISceneLoaderPlugin | ISceneLoaderPluginAsync;
 
-    public animationGroups: Array<AnimationGroup>;
-    public animatables: Array<Animatable>;
+    private _animations: Array<IModelAnimation>;
     public meshes: Array<AbstractMesh>;
     public particleSystems: Array<ParticleSystem>;
     public skeletons: Array<Skeleton>;
@@ -20,6 +20,8 @@ export class ViewerModel {
         this.onLoadErrorObservable = new Observable();
         this.onLoadProgressObservable = new Observable();
 
+        this._animations = [];
+
         if (!disableAutoLoad) {
             this._initLoad();
         }
@@ -30,6 +32,25 @@ export class ViewerModel {
             Tools.Error("Model was already loaded or in the process of loading.");
         } else {
             this._initLoad();
+        }
+    }
+
+    public getAnimations() {
+        return this._animations;
+    }
+
+    public getAnimationNames() {
+        return this._animations.map(a => a.name);
+    }
+
+    public getAnimationByName(name: string): Nullable<IModelAnimation> {
+        // can't use .find, noe available on IE
+        let filtered = this._animations.filter(a => a.name === name);
+        // what the next line means - if two animations have the same name, they will not be returned!
+        if (filtered.length === 1) {
+            return filtered[0];
+        } else {
+            return null;
         }
     }
 
@@ -50,21 +71,54 @@ export class ViewerModel {
             this.meshes = meshes;
             this.particleSystems = particleSystems;
             this.skeletons = skeletons;
+
+            // check if this is a gltf loader and load the animations
+            if (this.loader['_loader'] && this.loader['_loader']['_gltf'] && this.loader['_loader']['_gltf'].animations) {
+                this.loader['_loader']['_gltf'].animations.forEach(animation => {
+                    this._animations.push(new GroupModelAnimation(animation._babylonAnimationGroup));
+                });
+            } else {
+                skeletons.forEach((skeleton, idx) => {
+                    let ag = new BABYLON.AnimationGroup("animation-" + idx, this._scene);
+                    skeleton.getAnimatables().forEach(a => {
+                        if (a.animations[0]) {
+                            ag.addTargetedAnimation(a.animations[0], a);
+                        }
+                    });
+                    this._animations.push(new GroupModelAnimation(ag));
+                });
+            }
+
+            if (this._modelConfiguration.animation) {
+                if (this._modelConfiguration.animation.playOnce) {
+                    this._animations.forEach(a => {
+                        a.playMode = AnimationPlayMode.ONCE;
+                    });
+                }
+                if (this._modelConfiguration.animation.autoStart) {
+                    let animation = this.getAnimationByName(this._modelConfiguration.animation.autoStart);
+                    if (animation) {
+                        animation.start();
+                    }
+                }
+            }
+
+            console.log(this.getAnimationNames());
+
             this.onLoadedObservable.notifyObserversWithPromise(this);
         }, (progressEvent) => {
             this.onLoadProgressObservable.notifyObserversWithPromise(progressEvent);
         }, (e, m, exception) => {
             this.onLoadErrorObservable.notifyObserversWithPromise({ message: m, exception: exception });
         }, plugin)!;
+
+        this.loader['animationStartMode'] = 0;
     }
 
     public dispose() {
-        this.animatables.forEach(a => {
-            a.stop();
-        });
         this.particleSystems.forEach(ps => ps.dispose());
         this.skeletons.forEach(s => s.dispose());
-        this.animationGroups.forEach(ag => ag.dispose());
+        this._animations.forEach(ag => ag.dispose());
         this.meshes.forEach(m => m.dispose());
     }
 }
