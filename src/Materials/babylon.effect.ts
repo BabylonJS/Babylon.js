ead8ec78a581c6c2a1c0d97e31a32e129055bac6
Trevor Baron <trbaron@microsoft.com>
2018-03-13 10:55:48 -0700

    copy array to avoid modifying passed in sampler arrays when initializing an effect
    Former-commit-id: 58ed658da7c1f2126b9c523583820887764ff4c4

--- src/Materials/babylon.effect.ts
+++ src/Materials/babylon.effect.ts
@@ -259,7 +259,7 @@
 
                 this._attributesNames = options.attributes;
                 this._uniformsNames = options.uniformsNames.concat(options.samplers);
-                this._samplers = options.samplers;
+                this._samplers = options.samplers.slice();
                 this.defines = options.defines;
                 this.onError = options.onError;
                 this.onCompiled = options.onCompiled;
@@ -276,7 +276,7 @@
                 this._engine = <Engine>engine;
                 this.defines = <string>defines;
                 this._uniformsNames = (<string[]>uniformsNamesOrEngine).concat(<string[]>samplers);
-                this._samplers = <string[]>samplers;
+                this._samplers = samplers ? <string[]>samplers.slice() : [];
                 this._attributesNames = (<string[]>attributesNamesOrOptions);
 
                 this.onError = onError;
