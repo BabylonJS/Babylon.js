c52b585fc9b86ce508537ebe98b7080c8aa60674
Raanan Weber <info@raananweber.com>
2018-03-20 12:58:11 +0100

    documentation and declaration update
    Former-commit-id: 029f9d992698585a7cf34714b441d163188eb321

--- Viewer/src/eventManager.ts
+++ Viewer/src/eventManager.ts
@@ -1,6 +1,10 @@
 import { EventCallback, TemplateManager } from "./templateManager";
 
 
+/**
+ * The EventManager is in charge of registering user interctions with the viewer.
+ * It is used in the TemplateManager
+ */
 export class EventManager {
 
     private _callbacksContainer: { [key: string]: Array<{ eventType?: string, selector?: string, callback: (eventData: EventCallback) => void }> }
@@ -12,6 +16,15 @@ export class EventManager {
         })
     }
 
+    /**
+     * Register a new callback to a specific template.
+     * The best example for the usage can be found in the DefaultViewer
+     * 
+     * @param templateName the templateName to register the event to
+     * @param callback The callback to be executed
+     * @param eventType the type of event to register
+     * @param selector an optional selector. if not defined the parent object in the template will be selected
+     */
     public registerCallback(templateName: string, callback: (eventData: EventCallback) => void, eventType?: string, selector?: string) {
         if (!this._callbacksContainer[templateName]) {
             this._callbacksContainer[templateName] = [];
@@ -22,7 +35,16 @@ export class EventManager {
         });
     }
 
-    public unregisterCallback(templateName: string, callback?: (eventData: EventCallback) => void, eventType?: string, selector?: string) {
+    /**
+     * This will remove a registered event from the defined template.
+     * Each one of the variables apart from the template name are optional, but one must be provided.
+     * 
+     * @param templateName the templateName
+     * @param callback the callback to remove (optional)
+     * @param eventType the event type to remove (optional)
+     * @param selector the selector from which to remove the event (optional)
+     */
+    public unregisterCallback(templateName: string, callback: (eventData: EventCallback) => void, eventType?: string, selector?: string) {
         let callbackDefs = this._callbacksContainer[templateName] || [];
         this._callbacksContainer[templateName] = callbackDefs.filter(callbackDef => (!callbackDef.eventType || callbackDef.eventType === eventType) && (!callbackDef.selector || callbackDef.selector === selector));
     }
@@ -38,6 +60,9 @@ export class EventManager {
         });
     }
 
+    /**
+     * Dispose the event manager
+     */
     public dispose() {
         this._callbacksContainer = {};
     }
