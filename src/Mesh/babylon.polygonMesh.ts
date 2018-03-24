52634807477f800768d24220ed42c6fc664e9c42
Raanan Weber <info@raananweber.com>
2018-03-24 21:11:27 +0100

    Earcut is now an external ooptional dependency
    Former-commit-id: 84c5951d85b9e12f039a7d9b9c31153ec857c4bb

--- src/Mesh/babylon.polygonMesh.ts
+++ src/Mesh/babylon.polygonMesh.ts
@@ -1,4 +1,5 @@
 module BABYLON {
+    declare var earcut: any;
     class IndexedVector2 extends Vector2 {
         constructor(original: Vector2, public index: number) {
             super(original.x, original.y);
@@ -161,7 +162,7 @@ module BABYLON {
 
             var indices = new Array<number>();
 
-            let res = Earcut.earcut(this._epoints, this._eholes, 2);
+            let res = earcut(this._epoints, this._eholes, 2);
 
             for (let i = 0; i < res.length; i++) {
                 indices.push(res[i]);
