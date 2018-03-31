bf1fab8f2c9973a8f3df988459c7d526e88501b0
Raanan Weber <info@raananweber.com>
2018-03-31 21:49:16 +0200

    allowing to debug the viewer straight from vscode

--- Viewer/webpack.config.js
+++ Viewer/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         libraryTarget: 'umd',
         library: 'BabylonViewer',
         umdNamedDefine: true,
-        devtoolModuleFilenameTemplate: '[absolute-resource-path]'
+        devtoolModuleFilenameTemplate: '[relative-resource-path]'
     },
     resolve: {
         extensions: ['.ts', '.js'],
@@ -57,7 +57,7 @@ module.exports = {
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
-        compress: true,
+        compress: false,
         //open: true,
         port: 9000
     }
