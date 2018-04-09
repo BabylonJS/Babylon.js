869cb86b1466d467f93933a7e0cfd5cb64401c50
Raanan Weber <info@raananweber.com>
2018-04-09 20:43:44 +0200

    fixing issues with running tests PhantomJS has a bug when using fragments in ranges, and es6 doesn't support changing internal flags. Globals was introduced to control all viewers created (for webgl2 disable and disable init)

---
+++ Viewer/src/configuration/globals.ts
@@ -0,0 +1,8 @@
+export class ViewerGlobals {
+
+    public disableInit: boolean = false;
+    public disableWebGL2Support: boolean = false;
+
+}
+
+export let viewerGlobals: ViewerGlobals = new ViewerGlobals();
