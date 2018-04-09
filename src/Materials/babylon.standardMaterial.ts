0066649dfe4bf0cb88ce923e881c837edd30e3ca
David Catuhe <david.catuhe@live.fr>
2018-04-09 13:31:30 -0700

    Nightly
    Former-commit-id: 26cb2611b1f38a8ff27febcf3c6b3f22a8c2c397

--- src/Materials/babylon.standardMaterial.ts
+++ src/Materials/babylon.standardMaterial.ts
@@ -968,12 +968,19 @@ module BABYLON {
 
         public unbind(): void {
             if (this._activeEffect) {
+                let needFlag = false;
                 if (this._reflectionTexture && this._reflectionTexture.isRenderTarget) {
                     this._activeEffect.setTexture("reflection2DSampler", null);
+                    needFlag = true;
                 }
 
                 if (this._refractionTexture && this._refractionTexture.isRenderTarget) {
                     this._activeEffect.setTexture("refraction2DSampler", null);
+                    needFlag = true;
+                }
+
+                if (needFlag) {
+                    this._markAllSubMeshesAsTexturesDirty();
                 }
             }
 
