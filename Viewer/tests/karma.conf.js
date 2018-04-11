70a55621228c3362d8e74218d9e4e5f7e2084ae7
Raanan Weber <info@raananweber.com>
2018-04-11 12:16:31 +0200

    adding missing files (due to gitignore)

---
+++ Viewer/tests/karma.conf.js
@@ -0,0 +1,39 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+        captureTimeout: 3e5,
+        browserNoActivityTimeout: 3e5,
+        browserDisconnectTimeout: 3e5,
+        browserDisconnectTolerance: 3,
+        concurrency: 1,
+
+        urlRoot: '/karma/',
+
+        frameworks: ['mocha', 'chai', 'sinon'],
+
+        files: [
+            './tests/unit/build/*.js',
+            { pattern: './tests/**/*', watched: false, included: false, served: true },
+        ],
+        proxies: {
+            '/tests/': '/base/tests/'
+        },
+        client: {
+            mocha: {
+                timeout: 10000
+            }
+        },
+
+        port: 3000,
+        colors: true,
+        autoWatch: false,
+        singleRun: true,
+        browserNoActivityTimeout: 20000,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        browsers: ['PhantomJS']
+    })
+}
