a62740b7a83dd1a73ec28d27cbccd73b7fa56fab
Raanan Weber <info@raananweber.com>
2018-04-18 21:43:37 +0200

    bug fix for plugin cache
    Former-commit-id: 4fbd259744afed43fb58d28a8f775bff68365f06

--- Viewer/src/loader/plugins/index.ts
+++ Viewer/src/loader/plugins/index.ts
@@ -7,19 +7,21 @@ import { ExtendedMaterialLoaderPlugin } from './extendedMaterialLoaderPlugin';
 const pluginCache: { [key: string]: ILoaderPlugin } = {};
 
 export function getLoaderPluginByName(name: string) {
-    switch (name) {
-        case 'telemetry':
-            pluginCache[name] = new TelemetryLoaderPlugin();
-            break;
-        case 'msftLod':
-            pluginCache[name] = new MSFTLodLoaderPlugin();
-            break;
-        case 'minecraft':
-            pluginCache[name] = new MSFTLodLoaderPlugin();
-            break;
-        case 'extendedMaterial':
-            pluginCache[name] = new ExtendedMaterialLoaderPlugin();
-            break;
+    if (!pluginCache[name]) {
+        switch (name) {
+            case 'telemetry':
+                pluginCache[name] = new TelemetryLoaderPlugin();
+                break;
+            case 'msftLod':
+                pluginCache[name] = new MSFTLodLoaderPlugin();
+                break;
+            case 'minecraft':
+                pluginCache[name] = new MSFTLodLoaderPlugin();
+                break;
+            case 'extendedMaterial':
+                pluginCache[name] = new ExtendedMaterialLoaderPlugin();
+                break;
+        }
     }
 
     return pluginCache[name];
