fa22f57b8da35a89e37c8dbb8c0952542d522ad2
David Catuhe <david.catuhe@live.fr>
2018-05-01 08:11:38 -0700

    kicking 3.3 off:)

--- src/Engine/babylon.engine.ts
+++ src/Engine/babylon.engine.ts
@@ -726,7 +726,7 @@
          * Returns the current version of the framework
          */
         public static get Version(): string {
-            return "3.2.0";
+            return "3.3.0-alpha.0";
         }
 
         // Updatable statics so stick with vars here
