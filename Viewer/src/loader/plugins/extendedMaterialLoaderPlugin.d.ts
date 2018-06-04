7f57e796f8a5845efbe4f036edfcbd0227567204
Raanan Weber <info@raananweber.com>
2018-06-04 10:11:10 +0200

    cleanup

--- Viewer/src/loader/plugins/extendedMaterialLoaderPlugin.d.ts
+++
@@ -1,9 +0,0 @@
-import { ILoaderPlugin } from "./loaderPlugin";
-import { Material } from "babylonjs";
-/**
- * A (PBR) material will be extended using this function.
- * This function will hold extra default configuration for the viewer, if not implemented in Babylon itself.
- */
-export declare class ExtendedMaterialLoaderPlugin implements ILoaderPlugin {
-    onMaterialLoaded(baseMaterial: Material): void;
-}
