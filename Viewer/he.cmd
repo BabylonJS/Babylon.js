3e9c5aaeae7bc44801c0367f03e81acf1a28bf40
Raanan Weber <info@raananweber.com>
2018-06-04 10:11:10 +0200

    cleanup
    Former-commit-id: 7f8cf6afc887b9c842c9ecfdbf32ad803e84cb45

--- Viewer/he.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\he\bin\he" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\he\bin\he" %*
-)
