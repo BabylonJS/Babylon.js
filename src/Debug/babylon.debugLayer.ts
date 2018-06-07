3be09bf9801f498eec39f9591b74c4782b26f115
Jaskar <anthony.gee@live.fr>
2018-06-07 18:05:00 +0200

    Inspector : Rename "onGlobalPropertyChange" to "onPropertyChangedObservable"
    Former-commit-id: befc4331f43f0dc070011ad29e424ae1b56a9c1d

--- src/Debug/babylon.debugLayer.ts
+++ src/Debug/babylon.debugLayer.ts
@@ -12,13 +12,11 @@ module BABYLON {
 
         private BJSINSPECTOR = typeof INSPECTOR !== 'undefined' ? INSPECTOR : undefined;
 
-        public onGlobalPropertyChange: Array<Function>;
+        public onPropertyChangedObservable = new Observable<{ object: any, property: string, value: any, initialValue: any }>();
 
         constructor(scene: Scene) {
             this._scene = scene;
             // load inspector using require, if it doesn't exist on the global namespace.
-
-            this.onGlobalPropertyChange = new Array<Function>();
         }
 
         /** Creates the inspector window. */
@@ -115,12 +113,6 @@ module BABYLON {
          */
         public getActiveTab(): number {
             return this._inspector ? this._inspector.getActiveTabIndex() : -1;
-        }
-
-        public onGlobalPropertyChangeCallback(result: {}) {
-            this.onGlobalPropertyChange.forEach((callback) => {
-                callback(result);
-            });
         }
     }
 }
