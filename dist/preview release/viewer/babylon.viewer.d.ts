e806bc954be06df85f91b296b19ad4cf7ef63d36
David Catuhe <david.catuhe@live.fr>
2018-07-31 09:31:27 -0700

    Fix #4859

--- dist/preview release/viewer/babylon.viewer.d.ts
+++ dist/preview release/viewer/babylon.viewer.d.ts
@@ -1649,6 +1649,32 @@ declare module BabylonViewer {
     }
 }
 declare module BabylonViewer {
+    /**
+        * Defines an animation to be applied to a model (translation, scale or rotation).
+        */
+    export interface IModelAnimationConfiguration {
+            /**
+                * Time of animation, in seconds
+                */
+            time?: number;
+            /**
+                * Scale to apply
+                */
+            scaling?: {
+                    x: number;
+                    y: number;
+                    z: number;
+            };
+            /**
+                * Easing function to apply
+                */
+            easingFunction?: number;
+            /**
+                * An Easing mode to apply to the easing function
+                * See BABYLON.EasingFunction
+                */
+            easingMode?: number;
+    }
 }
 declare module BabylonViewer {
     export class TelemetryLoaderPlugin implements ILoaderPlugin {
