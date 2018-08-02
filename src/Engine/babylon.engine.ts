2aa89008f5a0bda473ae650aa7d87a8b011f2f20
David Catuhe <david.catuhe@live.fr>
2018-08-02 09:48:21 -0700

    3.3.0-alpha.14
    Former-commit-id: fbc48457ef63832e4e48eb8bf3a1d56eba1a78fa

--- src/Engine/babylon.engine.ts
+++ src/Engine/babylon.engine.ts
@@ -435,7 +435,7 @@
          * Returns the current version of the framework
          */
         public static get Version(): string {
-            return "3.3.0-alpha.13";
+            return "3.3.0-alpha.14";
         }
 
         // Updatable statics so stick with vars here
