6c4ceccc57c006f05e0b3c3c7d5c9a61aa222cff
Kacey Coley <kcoley@g.clemson.edu>
2018-08-28 17:17:11 -0700

    glTFSerializer: Add texture transform support

---
+++ serializers/src/glTF/babylon.glTFFileExporter.ts
@@ -0,0 +1,22 @@
+/// <reference path="../../../dist/preview release/glTF2Interface/babylon.glTF2Interface.d.ts"/>
+
+module BABYLON {
+    /**
+     * Interface for extending the exporter
+     */
+    export interface IGLTFExporterExtension {
+        /**
+         * The name of this extension
+         */
+        readonly name: string;
+        /**
+         * Defines whether this extension is enabled
+         */
+        enabled: boolean;
+
+        /**
+         * Defines whether this extension is required
+         */
+        required: boolean;
+    }
+}
