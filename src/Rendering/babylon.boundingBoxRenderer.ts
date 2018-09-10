c7d695a3740e07750af26848ca0a231a2b4b4005
David Catuhe <david.catuhe@live.fr>
2018-09-10 08:51:35 -0700

    Fix naming

--- src/Rendering/babylon.boundingBoxRenderer.ts
+++ src/Rendering/babylon.boundingBoxRenderer.ts
@@ -102,7 +102,7 @@
 
             this.scene._evaluateSubMeshStage.registerStep(SceneComponentConstants.STEP_EVALUATESUBMESH_BOUNDINGBOXRENDERER, this, this._evaluateSubMesh);
 
-            this.scene._afterRenderingGroupDrawStage.registerStep(SceneComponentConstants.STEP_AFTERCAMERADRAW_BOUNDINGBOXRENDERER, this, this.render);
+            this.scene._afterRenderingGroupDrawStage.registerStep(SceneComponentConstants.STEP_AFTERRENDERINGGROUPDRAW_BOUNDINGBOXRENDERER, this, this.render);
         }
 
         private _evaluateSubMesh(mesh: AbstractMesh, subMesh: SubMesh): void {
