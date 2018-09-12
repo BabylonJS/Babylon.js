353a84ab513fb0f818f6bd83fab1dddcf136596c
sebastien <vandenberghe.sebastien@gmail.com>
2018-09-13 01:21:30 +0200

    Comment audio

--- src/Audio/babylon.audioEngine.ts
+++ src/Audio/babylon.audioEngine.ts
@@ -211,6 +211,7 @@
 
         /**
          * Sets the global volume of your experience (sets on the master gain).
+         * @param newVloume Defines the new global volume of the application
          */
         public setGlobalVolume(newVolume: number): void {
             if (this.canUseWebAudio && this._audioContextInitialized) {
