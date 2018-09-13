a6c21ed5070a9a37f7dc23094b346319888d82a9
sebastien <vandenberghe.sebastien@gmail.com>
2018-09-13 20:51:46 +0200

    Doc Stereoscopic Camera

--- src/Cameras/Stereoscopic/babylon.stereoscopicFreeCamera.ts
+++ src/Cameras/Stereoscopic/babylon.stereoscopicFreeCamera.ts
@@ -5,6 +5,7 @@ module BABYLON {
 
     /**
      * Camera used to simulate stereoscopic rendering (based on FreeCamera)
+     * @see http://doc.babylonjs.com/features/cameras
      */    
     export class StereoscopicFreeCamera extends FreeCamera {
         /**
