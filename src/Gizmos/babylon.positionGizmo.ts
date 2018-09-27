bb193e25a6b2214ea3d263913c0692f0537f6900
Trevor Baron <trbaron@microsoft.com>
2018-09-27 10:27:38 -0700

    clear observables

--- src/Gizmos/babylon.positionGizmo.ts
+++ src/Gizmos/babylon.positionGizmo.ts
@@ -97,6 +97,8 @@ module BABYLON {
             this.xGizmo.dispose();
             this.yGizmo.dispose();
             this.zGizmo.dispose();
+            this.onDragStartObservable.clear();
+            this.onDragEndObservable.clear();
         }
 
         /**
