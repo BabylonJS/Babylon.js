4298d3225979a3ff3e0b3062387c7501f68c2830
Julien Barrois <barrois.julien@gmail.com>
2018-10-03 04:12:20 +0200

    fix comment typo

--- src/Tools/babylon.tools.ts
+++ src/Tools/babylon.tools.ts
@@ -589,7 +589,7 @@ module BABYLON {
         /**
          * Returns an array of the given size filled with element built from the given constructor and the paramters
          * @param size the number of element to construct and put in the array
-         * @param itemBuilder a callback responsable for creating new instance of item. Called once per array entry.
+         * @param itemBuilder a callback responsible for creating new instance of item. Called once per array entry.
          * @returns a new array filled with new objects
          */
         public static BuildArray<T>(size: number, itemBuilder: () => T): Array<T> {
