772a3d37094cd3de7d5173e87b16df02f8a53377
sebavan <vandenberghe@sebastien.com>
2018-11-28 15:01:26 +0100

    Fix Npm Audit Dev

--- Tools/Gulp/tasks/gulpTasks-localRun.js
+++ Tools/Gulp/tasks/gulpTasks-localRun.js
@@ -1,6 +1,6 @@
 // Import Dependencies.
 var gulp = require("gulp");
-var webserver = require("gulp-webserver");
+var connect = require("gulp-connect");
 var minimist = require("minimist");
 
 // Comand line parsing.
@@ -13,14 +13,14 @@ var commandLineOptions = minimist(process.argv.slice(2), {
  */
 gulp.task("webserver", function() {
     var options = {
+        root: "../../.",
         port: 1338,
-        livereload: false,
-
+        livereload: false
     };
 
     if (commandLineOptions.public) {
         options.host = "0.0.0.0";
     }
 
-    return gulp.src("../../.").pipe(webserver(options));
+    connect.server(options);
 });
