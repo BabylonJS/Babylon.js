ed88544fec8ef142b99a1e9c8cf2db199527f92f
sebavan <vandenberghe@sebastien.com>
2018-12-20 17:30:35 +0100

    Factorize Webpack Config
    Former-commit-id: c95f0fe12eec31305c810f7fbc34d846d6e53982

---
+++ Tools/WebpackPlugins/babylonWebpackConfig.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const babylonExternals = require('./babylonExternals');
+const hardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+var configPath = "../Gulp/config.json";
+const configFolder = path.dirname(path.resolve(__dirname, configPath));
+const config = require(configPath);
+
+module.exports = function defaultConfig(options) {
+    if (!options) {
+        throw "Options are mandatory to create the config.";
+    }
+
+    const module = options.module;
+    const settings = config[module];
+
+    const src = path.resolve(__dirname, settings.build.srcDirectory);
+    const webpackFolder = path.dirname(path.resolve(configFolder, settings.build.webpack));
+
+    options.resolveExtensions = options.resolveExtensions || [];
+
+    return {
+        context: src,
+        entry: {
+            [settings.build.processDeclaration.packageName]: path.resolve(src, settings.libraries[0].entry),
+        },
+        output: {
+            path: path.resolve(__dirname, config.build.outputDirectory) + settings.build.distOutputDirectory,
+            filename: settings.libraries[0].output,
+            libraryTarget: 'umd',
+            library: {
+                root: settings.build.processDeclaration.moduleName.split("."),
+                amd: settings.build.processDeclaration.packageName,
+                commonjs: settings.build.processDeclaration.packageName
+            },
+            umdNamedDefine: true
+        },
+        resolve: {
+            extensions: [".ts", ...options.resolveExtensions]
+        },
+        externals: [babylonExternals()],
+        devtool: "none",
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    configFileName: path.resolve(webpackFolder, './tsconfig.json'),
+                    declaration: false
+                }
+            }]
+        },
+        mode: "production",
+        performance: {
+            hints: false
+        },
+        plugins: [
+            new hardSourceWebpackPlugin(),
+            new webpack.WatchIgnorePlugin([
+                /\.js$/,
+                /\.d\.ts$/,
+                /\.fx$/
+            ])
+        ]
+    }
+};
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,47 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const babylonWebpackConfig = require('../Tools/WebpackPlugins/babylonWebpackConfig');
 
-module.exports = {
-    context: __dirname,
-    entry: {
-        'babylonjs': path.resolve(__dirname, './Legacy/legacy.ts'),
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist/preview release'),
-        filename: 'babylon.js',
-        libraryTarget: 'umd',
-        library: {
-            root: ["BABYLON"],
-            amd: "babylonjs",
-            commonjs: "babylonjs"
-        },
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts']
-    },
-    devtool: "source-map",
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-            options: {
-                configFileName: path.resolve(__dirname, './tsconfig.json'),
-                declaration: false
-            }
-        }]
-    },
-    mode: "production",
-    performance: {
-        hints: false
-    },
-    plugins: [
-        new HardSourceWebpackPlugin(),
-        new webpack.WatchIgnorePlugin([
-            /\.js$/,
-            /\.d\.ts$/,
-            /\.fx$/
-        ])
-    ]
-}
+var config = babylonWebpackConfig({
+    module: "core"
+});
+
+module.exports = config;
