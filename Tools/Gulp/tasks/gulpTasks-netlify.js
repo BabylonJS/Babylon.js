c6572402cf9371aa10d3ca26a8e7f9166e832473
sebavan <vandenberghe@sebastien.com>
2018-12-22 00:21:33 +0100

    getting ready
    Former-commit-id: 66203221e342e9ac9edd1d44ae0859652c94d900

--- Tools/Gulp/tasks/gulpTasks-netlify.js
+++ Tools/Gulp/tasks/gulpTasks-netlify.js
@@ -7,8 +7,6 @@ gulp.task("netlify-cleanup", function() {
     //set by netlify
     if (process.env.REPOSITORY_URL) {
         return del([
-            "../../../inspector/node_modules/**/*",
-            "../../../gui/node_modules/**/*",
             "../../../Viewer/node_modules/**/*"
         ], { force: true });
     }
