d58356230033606c999c6d7f79509525882da56d
sebavan <vandenberghe@sebastien.com>
2018-12-24 02:08:09 +0100

    add npm link command in dev

--- Tools/Publisher/index.js
+++ Tools/Publisher/index.js
@@ -1,4 +1,22 @@
 // Dependecies.
 const publisher = require('./tasks/main');
+const minimist = require("minimist");
 
-publisher(false);
+const commandLineOptions = minimist(process.argv.slice(2), {
+    boolean: ["es6"],
+    boolean: ["umd"]
+});
+
+if (commandLineOptions.es6) {
+    publisher(false, {
+        es6: true
+    });
+}
+else if (commandLineOptions.umd) {
+    publisher(false, {
+        umd: true
+    });
+}
+else {
+    publisher(false);
+}
