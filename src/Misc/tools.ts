67d6c064fb47722a08da0473e38a897d96f5bd5f
sebavan <vandenberghe@sebastien.com>
2018-12-27 00:04:43 +0100

    CreateScreenshot dependencies

--- src/Misc/tools.ts
+++ src/Misc/tools.ts
@@ -4,9 +4,6 @@ import { Color4, Color3, Vector2, Vector3 } from "../Maths/math";
 import { Scalar } from "../Maths/math.scalar";
 import { IOfflineProvider } from "../Offline/IOfflineProvider";
 import { Camera } from "../Cameras/camera";
-import { Texture } from "../Materials/Textures/texture";
-import { RenderTargetTexture } from "../Materials/Textures/renderTargetTexture";
-import { FxaaPostProcess } from "../PostProcesses/fxaaPostProcess";
 import { Observable } from "./observable";
 import { FilesInput } from "./filesInput";
 import { TGATools } from "./tga";
@@ -1350,64 +1347,7 @@ declare type Engine = import("../Engines/engine").Engine;
          * Check your browser for supported MIME types
          */
         public static CreateScreenshot(engine: Engine, camera: Camera, size: any, successCallback?: (data: string) => void, mimeType: string = "image/png"): void {
-            var width: number;
-            var height: number;
-
-            // If a precision value is specified
-            if (size.precision) {
-                width = Math.round(engine.getRenderWidth() * size.precision);
-                height = Math.round(width / engine.getAspectRatio(camera));
-            }
-            else if (size.width && size.height) {
-                width = size.width;
-                height = size.height;
-            }
-            //If passing only width, computing height to keep display canvas ratio.
-            else if (size.width && !size.height) {
-                width = size.width;
-                height = Math.round(width / engine.getAspectRatio(camera));
-            }
-            //If passing only height, computing width to keep display canvas ratio.
-            else if (size.height && !size.width) {
-                height = size.height;
-                width = Math.round(height * engine.getAspectRatio(camera));
-            }
-            //Assuming here that "size" parameter is a number
-            else if (!isNaN(size)) {
-                height = size;
-                width = size;
-            }
-            else {
-                Logger.Error("Invalid 'size' parameter !");
-                return;
-            }
-
-            if (!screenshotCanvas) {
-                screenshotCanvas = document.createElement('canvas');
-            }
-
-            screenshotCanvas.width = width;
-            screenshotCanvas.height = height;
-
-            var renderContext = screenshotCanvas.getContext("2d");
-
-            var ratio = engine.getRenderWidth() / engine.getRenderHeight();
-            var newWidth = width;
-            var newHeight = newWidth / ratio;
-            if (newHeight > height) {
-                newHeight = height;
-                newWidth = newHeight * ratio;
-            }
-
-            var offsetX = Math.max(0, width - newWidth) / 2;
-            var offsetY = Math.max(0, height - newHeight) / 2;
-
-            var renderingCanvas = engine.getRenderingCanvas();
-            if (renderContext && renderingCanvas) {
-                renderContext.drawImage(renderingCanvas, offsetX, offsetY, newWidth, newHeight);
-            }
-
-            Tools.EncodeScreenshotCanvasData(successCallback, mimeType);
+            throw "Import ScreenshotTools before creating screenshot.";
         }
 
         /**
@@ -1430,80 +1370,7 @@ declare type Engine = import("../Engines/engine").Engine;
          * @param fileName A name for for the downloaded file.
          */
         public static CreateScreenshotUsingRenderTarget(engine: Engine, camera: Camera, size: any, successCallback?: (data: string) => void, mimeType: string = "image/png", samples: number = 1, antialiasing: boolean = false, fileName?: string): void {
-            var width: number;
-            var height: number;
-
-            //If a precision value is specified
-            if (size.precision) {
-                width = Math.round(engine.getRenderWidth() * size.precision);
-                height = Math.round(width / engine.getAspectRatio(camera));
-                size = { width: width, height: height };
-            }
-            else if (size.width && size.height) {
-                width = size.width;
-                height = size.height;
-            }
-            //If passing only width, computing height to keep display canvas ratio.
-            else if (size.width && !size.height) {
-                width = size.width;
-                height = Math.round(width / engine.getAspectRatio(camera));
-                size = { width: width, height: height };
-            }
-            //If passing only height, computing width to keep display canvas ratio.
-            else if (size.height && !size.width) {
-                height = size.height;
-                width = Math.round(height * engine.getAspectRatio(camera));
-                size = { width: width, height: height };
-            }
-            //Assuming here that "size" parameter is a number
-            else if (!isNaN(size)) {
-                height = size;
-                width = size;
-            }
-            else {
-                Logger.Error("Invalid 'size' parameter !");
-                return;
-            }
-
-            var scene = camera.getScene();
-            var previousCamera: Nullable<Camera> = null;
-
-            if (scene.activeCamera !== camera) {
-                previousCamera = scene.activeCamera;
-                scene.activeCamera = camera;
-            }
-
-            var renderCanvas = engine.getRenderingCanvas();
-            if (!renderCanvas) {
-                Logger.Error("No rendering canvas found !");
-                return;
-            }
-
-            var originalSize = { width: renderCanvas.width, height: renderCanvas.height };
-            engine.setSize(width, height);
-            scene.render();
-
-            // At this point size can be a number, or an object (according to engine.prototype.createRenderTargetTexture method)
-            var texture = new RenderTargetTexture("screenShot", size, scene, false, false, Constants.TEXTURETYPE_UNSIGNED_INT, false, Texture.NEAREST_SAMPLINGMODE);
-            texture.renderList = null;
-            texture.samples = samples;
-            if (antialiasing) {
-                texture.addPostProcess(new FxaaPostProcess('antialiasing', 1.0, scene.activeCamera));
-            }
-            texture.onAfterRenderObservable.add(() => {
-                Tools.DumpFramebuffer(width, height, engine, successCallback, mimeType, fileName);
-            });
-
-            scene.incrementRenderId();
-            scene.resetCachedMaterial();
-            texture.render(true);
-            texture.dispose();
-
-            if (previousCamera) {
-                scene.activeCamera = previousCamera;
-            }
-            engine.setSize(originalSize.width, originalSize.height);
-            camera.getProjectionMatrix(true); // Force cache refresh;
+            throw "Import ScreenshotTools before creating screenshot.";
         }
 
         /**
