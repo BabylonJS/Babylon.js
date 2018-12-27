bd180c6356cbb5e13d6a5bbad450a6e97c6b39f5
sebavan <vandenberghe@sebastien.com>
2018-12-27 14:21:25 +0100

    fix import lint

--- Tools/Gulp/tasks/gulpTasks-importLint.js
+++ Tools/Gulp/tasks/gulpTasks-importLint.js
@@ -4,9 +4,20 @@ var filter = require('gulp-filter');
 
 // Helpers.
 var validateImports = require("../helpers/gulp-validateImports");
+var uncommentShaders = require('../helpers/gulp-removeShaderComments');
+var processShaders = require("../helpers/gulp-processShaders");
 
 // Read the full config.
 var config = require("../../Config/config.js");
+
+/**
+ * Create shader ts files.
+ */
+var buildShaders = function(settings) {
+    return gulp.src(settings.computed.shaderGlob)
+            .pipe(uncommentShaders())
+            .pipe(processShaders(settings.isCore));
+}
 
 /*
  * ImportLint all typescript files from the src directory.
@@ -23,14 +34,25 @@ var importLintLibrary = function(settings) {
 /**
  * Dynamic module linting for library (mat, post processes, ...).
  */
-config.lintModules.map(function(module) {
-    // Task will be like moduleName-importLint
-    gulp.task(module + "-importLint", function() {
-        var settings = config[module];
+var lintLibrary = function(settings) {
+    var tasks = [];
 
-        return importLintLibrary(settings, false);
-    });
+    var shaders = function() { return buildShaders(settings); };
+    var lint = function() { return importLintLibrary(settings); };
+
+    tasks.push(shaders, lint);
+
+    return gulp.series.apply(this, tasks);
+};
+
+/**
+ * Dynamic module linting for library (mat, post processes, ...).
+ */
+config.lintModules.map(function(module) {
+    const settings = config[module];
+    gulp.task(module + "-importLint", lintLibrary(settings));
 });
+
 
 /**
  * Full Librairies importLint.
