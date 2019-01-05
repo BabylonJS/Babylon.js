7b7384223435dcb620ec9ad5b81eb2f7a304497c
duncan law <mrdunk@gmail.com>
2019-01-05 19:52:49 +0000

    Fix case of paths. Gulp fails to serve many existing paths on case sensitive OS-es.

--- materialsLibrary/test/addtriplanar.js
+++ materialsLibrary/test/addtriplanar.js
@@ -1,10 +1,10 @@
 window.prepareTriPlanar = function() {
     var triPlanar = new BABYLON.TriPlanarMaterial("triplanar", scene);
-    triPlanar.diffuseTextureX = new BABYLON.Texture("/playground/textures/rock.png", scene);
-    triPlanar.diffuseTextureY = new BABYLON.Texture("/playground/textures/grass.png", scene);
+    triPlanar.diffuseTextureX = new BABYLON.Texture("/Playground/textures/rock.png", scene);
+    triPlanar.diffuseTextureY = new BABYLON.Texture("/Playground/textures/grass.png", scene);
     triPlanar.diffuseTextureZ = triPlanar.diffuseTextureX;
-    triPlanar.normalTextureX = new BABYLON.Texture("/playground/textures/rockn.png", scene);
-    triPlanar.normalTextureY = new BABYLON.Texture("/playground/textures/grassn.png", scene);
+    triPlanar.normalTextureX = new BABYLON.Texture("/Playground/textures/rockn.png", scene);
+    triPlanar.normalTextureY = new BABYLON.Texture("/Playground/textures/grassn.png", scene);
     triPlanar.normalTextureZ = triPlanar.normalTextureX;
     triPlanar.specularPower = 64;
     triPlanar.tileSize = 1.5;
