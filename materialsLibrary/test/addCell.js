00a0f8524ec3a43004a3d53765b20c8187db7507
duncan law <mrdunk@gmail.com>
2019-01-05 20:21:26 +0000

    Fix case of paths. Gulp webserver serves wrong paths on case sensitive OS-es.
    Former-commit-id: 7991ea61b1f8885be7fb740a818a82aaecba2f1b

--- materialsLibrary/test/addCell.js
+++ materialsLibrary/test/addCell.js
@@ -1,6 +1,6 @@
 window.prepareCell = function() {
     var cell = new BABYLON.CellMaterial("cell", scene);
-	cell.diffuseTexture = new BABYLON.Texture("/playground/textures/amiga.jpg", scene);
+	cell.diffuseTexture = new BABYLON.Texture("/Playground/textures/amiga.jpg", scene);
 	cell.diffuseTexture.uScale = cell.diffuseTexture.vScale = 3;
 	cell.computeHighLevel = true;
 
