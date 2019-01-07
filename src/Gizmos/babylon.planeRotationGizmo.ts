475a3d8e07c9d7b0e8c9ec7ff97f3aba693e05c3
Trevor Baron <trbaron@microsoft.com>
2019-01-07 14:09:16 -0800

    snap correctly in the negative direction
    Former-commit-id: 26c546007100f923d7f7bd25e69490f4033db77c

--- src/Gizmos/babylon.planeRotationGizmo.ts
+++ src/Gizmos/babylon.planeRotationGizmo.ts
@@ -117,7 +117,10 @@ module BABYLON {
                     if (this.snapDistance != 0) {
                         currentSnapDragDistance += angle;
                         if (Math.abs(currentSnapDragDistance) > this.snapDistance) {
-                            var dragSteps = Math.floor(currentSnapDragDistance / this.snapDistance);
+                            var dragSteps = Math.floor(Math.abs(currentSnapDragDistance) / this.snapDistance);
+                            if (currentSnapDragDistance < 0) {
+                                dragSteps *= -1;
+                            }
                             currentSnapDragDistance = currentSnapDragDistance % this.snapDistance;
                             angle = this.snapDistance * dragSteps;
                             snapped = true;
