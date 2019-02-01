d4737eca9baff233ba4439067e5f5f4f69f43ed5
Trevor Baron <trbaron@microsoft.com>
2019-02-01 14:33:23 -0800

    update webpack and package json

--- Viewer/webpack.config.js
+++ Viewer/webpack.config.js
@@ -1,52 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = /*[
-    {
-    entry: {
-        'assets': './src/assets/index.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        devtoolModuleFilenameTemplate: '[relative-resource-path]'
-    },
-    plugins: [
-        new webpack.WatchIgnorePlugin([
-            /\.d\.ts$/
-        ])
-    ],
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        loaders: [{
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/
-        },
-        {
-            test: /\.(html)$/,
-            use: {
-                loader: 'html-loader',
-                options: {
-                    minimize: true
-                }
-            }
-        },
-        {
-            test: /\.(jpe?g|png|ttf|eot|svg?)(\?[a-z0-9=&.]+)?$/,
-            use: 'base64-image-loader?limit=1000&name=[name].[ext]'
-        },
-        {
-            test: /\.(woff|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'base64-font-loader'
-        }]
-    }
-},*/
+module.exports = 
     {
         entry: {
             'viewer': './src/index.ts',
+            'renderOnlyViewer': './src/renderOnlyIndex.ts',
         },
         output: {
             path: path.resolve(__dirname, 'dist'),
@@ -59,10 +18,6 @@ module.exports = /*[
         resolve: {
             extensions: ['.ts', '.js'],
             alias: {
-                "babylonjs": __dirname + '/../dist/preview release/babylon.max.js',
-                "babylonjs-materials": __dirname + '/../dist/preview release/materialsLibrary/babylonjs.materials.js',
-                "babylonjs-loaders": __dirname + '/../dist/preview release/loaders/babylonjs.loaders.js',
-                "babylonjs-viewer-assets": __dirname + '/src/assets/index.ts'
             }
         },
         mode: "development",
@@ -103,4 +58,3 @@ module.exports = /*[
             port: 9000
         }
     }
-//]
