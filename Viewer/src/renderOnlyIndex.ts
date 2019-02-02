260356a34bca031b74bb83a279584fc7196976d0
Trevor Baron <trbaron@microsoft.com>
2019-02-01 17:01:28 -0800

    remove standard material
    Former-commit-id: c29a9c5fb45e94438df1892b27275abf89575c89

--- Viewer/src/renderOnlyIndex.ts
+++ Viewer/src/renderOnlyIndex.ts
@@ -1,15 +1,17 @@
 import { RenderOnlyViewer } from './viewer/renderOnlyViewer';
+
 // Required side effects
 import '@babylonjs/loaders/glTF/2.0';
 import "@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent"
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/core/Meshes/meshBuilder";
-// TODO bad error message without this
-import '@babylonjs/core/Loading/Plugins'
-import "@babylonjs/core/Materials/Textures/Loaders"
+import "@babylonjs/core/Materials/Textures/Loaders/ddsTextureLoader"
+import "@babylonjs/core/Materials/Textures/Loaders/envTextureLoader"
+import "@babylonjs/core/Materials/Textures/Loaders/ktxTextureLoader"
 import "@babylonjs/core/PostProcesses/RenderPipeline/postProcessRenderPipelineManagerSceneComponent";
-// This was needed at some point but no longer?
-// import "@babylonjs/core/Gamepads/gamepadSceneComponent"
 
+// Override default material factory to avoid the dependency on standard material
+import { Scene } from '@babylonjs/core/scene';
+Scene.DefaultMaterialFactory = (scene:Scene)=>{return null as any};
 
 export { RenderOnlyViewer };
