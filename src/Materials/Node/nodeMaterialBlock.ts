158a4fe01b0261a9841132b6c5e48f278e2c6385
David Catuhe <david.catuhe@live.fr>
2019-03-05 09:53:55 -0800

    First scafolding

---
+++ src/Materials/Node/nodeMaterialBlock.ts
@@ -0,0 +1,10 @@
+export class NodeMaterialBlock {
+    /**
+     * Gets or sets the name of the block
+     */
+    public name: string;
+
+    public getClassName() {
+        return "NodeMaterialBlock";
+    }
+}
