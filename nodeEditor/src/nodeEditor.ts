f8b9bac185b6f578be1e9c5cb2a3fa4bd3e4eba2
David Catuhe <david.catuhe@live.fr>
2019-03-21 15:24:17 -0700

    nodeEditor initial build
    Former-commit-id: 5b19d3fe598c20a0322be41c349c34d1611f359d

--- nodeEditor/src/nodeEditor.ts
+++ nodeEditor/src/nodeEditor.ts
@@ -1,9 +1,34 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { GlobalState } from './globalState';
+import { GraphEditor } from './components/graphEditor';
 
+/**
+ * Interface used to specify creation options for the node editor
+ */
+export interface INodeEditorOptions {
+    /**
+     * Defines the DOM element that will host the node editor
+     */
+    hostElement: HTMLDivElement
+}
+
+/**
+ * Class used to create a node editor
+ */
 export class NodeEditor {
-    public static Create() {
+    /**
+     * Show the node editor
+     * @param options defines the options to use to configure the node editor
+     */
+    public static Show(options: INodeEditorOptions) {
+        let globalState = new GlobalState();
 
+        const graphEditor = React.createElement(GraphEditor, {
+            globalState: globalState
+        });
+
+        ReactDOM.render(graphEditor, options.hostElement);
     }
 }
 
