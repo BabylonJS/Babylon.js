15b0b500780984588ce78b40260107c00fc406f6
David Catuhe <david.catuhe@live.fr>
2019-04-02 21:40:37 -0700

    .
    Former-commit-id: 17f46245564f2e4e7922e47b78a3c20fa515cff8

--- src/Culling/boundingBox.ts
+++ src/Culling/boundingBox.ts
@@ -98,8 +98,10 @@ export class BoundingBox implements ICullable {
         // OBB
         max.addToRef(min, this.center).scaleInPlace(0.5);
         max.subtractToRef(min, this.extendSize).scaleInPlace(0.5);
+        
+        this._worldMatrix = worldMatrix || Matrix.IdentityReadOnly;
 
-        this._update(worldMatrix || Matrix.IdentityReadOnly);
+        this._update(this._worldMatrix);
     }
 
     /**
