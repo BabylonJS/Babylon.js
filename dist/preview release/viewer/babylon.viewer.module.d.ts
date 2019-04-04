15c76821349eac1a5a2b335cdf60c3cafaef417c
David Catuhe <david.catuhe@live.fr>
2019-04-04 09:53:46 -0700

    Nightly

--- dist/preview release/viewer/babylon.viewer.module.d.ts
+++ dist/preview release/viewer/babylon.viewer.module.d.ts
@@ -2234,11 +2234,7 @@ declare module 'babylonjs-viewer/configuration/interfaces/lightConfiguration' {
 }
 
 declare module 'babylonjs-viewer/configuration/interfaces/observersConfiguration' {
-    export interface IObserversConfiguration {
-        onEngineInit?: string;
-        onSceneInit?: string;
-        onModelLoaded?: string;
-    }
+    
 }
 
 declare module 'babylonjs-viewer/configuration/interfaces/sceneConfiguration' {
