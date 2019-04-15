c83ad89f95939f7fbf4b9f8a8e807be8ffce3814
David Catuhe <david.catuhe@live.fr>
2019-04-15 15:35:06 -0700

    environment control in the sandbox

---
+++ sandbox/environment.js
@@ -0,0 +1,60 @@
+
+// Environments
+var dropdownContentEnv = document.getElementById("dropdownContent-env");
+var btnEnvironment = document.getElementById("btnEnvironment");
+
+var skyboxes = [
+    "https://assets.babylonjs.com/environments/environmentSpecular.env",
+    "https://assets.babylonjs.com/environments/studio.env",
+];
+
+var skyboxesNames = [
+    "Default",
+    "Studio",
+];
+
+var readLocaStorageValue = function(key, defautlValue) {
+    if (typeof (Storage) !== "undefined" && localStorage.getItem(key) !== null) {
+        return parseInt(localStorage.getItem(key));
+    }
+
+    return defautlValue;
+}
+
+var defaultSkyboxIndex = readLocaStorageValue("defaultSkyboxId", 0);
+
+function displayDropdownContentEnv(display) {
+    if (display) {
+        dropdownContentEnv.style.display = "block";
+    }
+    else {
+        dropdownContentEnv.style.display = "none";
+    }
+}
+
+btnEnvironment.addEventListener('click', function() {
+    displayDropdownContentEnv(dropdownContentEnv.style.display !== "block");
+}, false);
+
+addEnvironmentLoader = function(index) {
+    var env = document.createElement("div");
+    env.innerHTML = skyboxesNames[index];
+    env.title = skyboxesNames[index];
+    env.addEventListener("click", function() {
+        // hide the content of the dropdown
+        displayDropdownContentEnv(false);
+        if (typeof (Storage) !== "undefined") {
+            localStorage.setItem("defaultSkyboxId", index);
+        }
+        defaultSkyboxIndex = index;
+        skyboxPath = skyboxes[defaultSkyboxIndex];
+        filesInput.reload();
+    });
+
+    return env;
+}
+
+for (var index = 0; index < skyboxes.length; index++) {
+    var env = addEnvironmentLoader(index);
+    dropdownContentEnv.appendChild(env);
+}
