a332a095fccea8d5289a2baec53d6d33f428852d
David Catuhe <david.catuhe@live.fr>
2019-04-30 11:07:07 -0700

    4.0.1
    Former-commit-id: f32ce1440551a64a8c30e5daf940795ecee381c1

--- src/Engines/engine.ts
+++ src/Engines/engine.ts
@@ -491,14 +491,14 @@ export class Engine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@4.0.0";
+        return "babylonjs@4.0.1";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "4.0.0";
+        return "4.0.1";
     }
 
     /**
