88863abc68bbd25e4dcd82f828dedc2f85b4d37d
Trevor Baron <trbaron@microsoft.com>
2019-05-03 13:49:26 -0700

    remove texture
    Former-commit-id: 957abdbee9a9b8da2e23ef0956309b747aa270fe

--- nodeEditor/src/components/customDiragramNodes/texture/textureNodeModel.ts
+++
@@ -1,9 +0,0 @@
-import { NodeModel } from "storm-react-diagrams";
-import { TexturePortModel } from './texturePortModel';
-
-export class TextureNodeModel extends NodeModel {
-	constructor() {
-		super("texture");
-		this.addPort(new TexturePortModel("right"));
-	}
-}
