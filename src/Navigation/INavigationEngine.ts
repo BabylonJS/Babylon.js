b78b54a0e9c9994ff41519c68c24f5b21c08ae63
Cedric Guillemet <cedric.guillemet@microsoft.com>
2019-06-17 10:31:06 -0700

    lint
    Former-commit-id: 1eacc68207754c5b75c63bc8690059625614a451

--- src/Navigation/INavigationEngine.ts
+++ src/Navigation/INavigationEngine.ts
@@ -162,74 +162,74 @@ export class AgentParameters {
  */
 export class NavMeshParameters {
     /**
-     * The xz-plane cell size to use for fields. [Limit: > 0] [Units: wu] 
+     * The xz-plane cell size to use for fields. [Limit: > 0] [Units: wu]
      */
-	cs: number;
+    cs: number;
 
-	/**
+    /**
      * The y-axis cell size to use for fields. [Limit: > 0] [Units: wu]
      */
     ch: number;
-    
+
     /**
-     * The maximum slope that is considered walkable. [Limits: 0 <= value < 90] [Units: Degrees] 
+     * The maximum slope that is considered walkable. [Limits: 0 <= value < 90] [Units: Degrees]
      */
     walkableSlopeAngle: number;
 
     /**
-     * Minimum floor to 'ceiling' height that will still allow the floor area to 
-     * be considered walkable. [Limit: >= 3] [Units: vx] 
+     * Minimum floor to 'ceiling' height that will still allow the floor area to
+     * be considered walkable. [Limit: >= 3] [Units: vx]
      */
     walkableHeight: number;
 
     /**
-     * Maximum ledge height that is considered to still be traversable. [Limit: >=0] [Units: vx] 
+     * Maximum ledge height that is considered to still be traversable. [Limit: >=0] [Units: vx]
      */
     walkableClimb: number;
 
     /**
-     * The distance to erode/shrink the walkable area of the heightfield away from 
-     * obstructions.  [Limit: >=0] [Units: vx] 
+     * The distance to erode/shrink the walkable area of the heightfield away from
+     * obstructions.  [Limit: >=0] [Units: vx]
      */
     walkableRadius: number;
 
     /**
-     * The maximum allowed length for contour edges along the border of the mesh. [Limit: >=0] [Units: vx] 
+     * The maximum allowed length for contour edges along the border of the mesh. [Limit: >=0] [Units: vx]
      */
     maxEdgeLen: number;
 
     /**
-     * The maximum distance a simplfied contour's border edges should deviate 
+     * The maximum distance a simplfied contour's border edges should deviate
      * the original raw contour. [Limit: >=0] [Units: vx]
      */
     maxSimplificationError: number;
 
     /**
-     * The minimum number of cells allowed to form isolated island areas. [Limit: >=0] [Units: vx] 
+     * The minimum number of cells allowed to form isolated island areas. [Limit: >=0] [Units: vx]
      */
     minRegionArea: number;
 
     /**
-     * Any regions with a span count smaller than this value will, if possible, 
-     * be merged with larger regions. [Limit: >=0] [Units: vx] 
+     * Any regions with a span count smaller than this value will, if possible,
+     * be merged with larger regions. [Limit: >=0] [Units: vx]
      */
     mergeRegionArea: number;
 
     /**
-     * The maximum number of vertices allowed for polygons generated during the 
-     * contour to polygon conversion process. [Limit: >= 3] 
+     * The maximum number of vertices allowed for polygons generated during the
+     * contour to polygon conversion process. [Limit: >= 3]
      */
     maxVertsPerPoly: number;
 
     /**
      * Sets the sampling distance to use when generating the detail mesh.
-     * (For height detail only.) [Limits: 0 or >= 0.9] [Units: wu] 
+     * (For height detail only.) [Limits: 0 or >= 0.9] [Units: wu]
      */
     detailSampleDist: number;
 
     /**
      * The maximum distance the detail mesh surface should deviate from heightfield
-     * data. (For height detail only.) [Limit: >=0] [Units: wu] 
+     * data. (For height detail only.) [Limit: >=0] [Units: wu]
      */
     detailSampleMaxError: number;
 }
