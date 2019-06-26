c5a0cadfbd3932243d13ce539b4bbcbfc0ba3606
sebavan <vandenberghe.sebastien@gmail.com>
2019-06-26 14:46:19 +0200

    Fix camera spatial audio local space
    Former-commit-id: e363f6cbbfb3a452f5a822f756bf09d3c731dbde

--- src/Audio/audioSceneComponent.ts
+++ src/Audio/audioSceneComponent.ts
@@ -398,7 +398,7 @@ export class AudioSceneComponent implements ISceneSerializableComponent {
         }
 
         if (listeningCamera && audioEngine.audioContext) {
-            audioEngine.audioContext.listener.setPosition(listeningCamera.position.x, listeningCamera.position.y, listeningCamera.position.z);
+            audioEngine.audioContext.listener.setPosition(listeningCamera.globalPosition.x, listeningCamera.globalPosition.y, listeningCamera.globalPosition.z);
             // for VR cameras
             if (listeningCamera.rigCameras && listeningCamera.rigCameras.length > 0) {
                 listeningCamera = listeningCamera.rigCameras[0];
