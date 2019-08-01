8792db49c21c9132d0333b82eaa77b479df3c267
David Catuhe <david.catuhe@live.fr>
2019-08-01 10:52:17 -0700

    Associated with #6012 - Store node position when serializing

--- nodeEditor/src/globalState.ts
+++ nodeEditor/src/globalState.ts
@@ -3,6 +3,9 @@ import { Nullable } from "babylonjs/types"
 import { Observable } from 'babylonjs/Misc/observable';
 import { DefaultNodeModel } from './components/diagram/defaultNodeModel';
 import { LogEntry } from './components/log/logComponent';
+import { NodeMaterialBlock } from 'babylonjs';
+import { NodeModel } from 'storm-react-diagrams';
+import { INodeLocationInfo } from './nodeLocationInfo';
 
 export class GlobalState {
     nodeMaterial: NodeMaterial;
@@ -10,10 +13,11 @@ export class GlobalState {
     hostDocument: HTMLDocument;
     onSelectionChangedObservable = new Observable<Nullable<DefaultNodeModel>>();
     onRebuildRequiredObservable = new Observable<void>();
-    onResetRequiredObservable = new Observable<void>();
+    onResetRequiredObservable = new Observable<Nullable<INodeLocationInfo[]>>();
     onUpdateRequiredObservable = new Observable<void>();
     onZoomToFitRequiredObservable = new Observable<void>();
     onReOrganizedRequiredObservable = new Observable<void>();
     onLogRequiredObservable = new Observable<LogEntry>();
     onErrorMessageDialogRequiredObservable = new Observable<string>();
+    onGetNodeFromBlock: (block: NodeMaterialBlock) => NodeModel;
 }
