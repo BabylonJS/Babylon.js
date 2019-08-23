2d92db2938f28043145ceab0e6e9b034063ae8fc
David Catuhe <david.catuhe@live.fr>
2019-08-23 11:02:14 -0700

    reduce the lsit of versions

--- Playground/js/config_versions.js
+++ Playground/js/config_versions.js
@@ -1,6 +1,5 @@
 CONFIG_last_versions = [
     ["Latest","https://preview.babylonjs.com/babylon.js"],
     ["4.0.3","https://unpkg.com/babylonjs@4.0.3/babylon.js"],
-    ["4.0.0","https://unpkg.com/babylonjs@4.0.0/babylon.js"],
     ["3.3","https://unpkg.com/babylonjs@3.3.0/babylon.js"]
 ];
