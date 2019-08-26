fd0c0f2e98637f7f06805624702ddc01f5c65a94
Cedric Guillemet <cedric.guillemet@microsoft.com>
2019-08-26 10:12:04 -0700

    Some files used different encoding than UTF-8

--- TestApp/Source/UWP/App.cpp
+++ TestApp/Source/UWP/App.cpp
@@ -1,4 +1,4 @@
-﻿#include "App.h"
+#include "App.h"
 
 #include <pplawait.h>
 #include <winrt/Windows.ApplicationModel.h>
--- TestApp/Source/UWP/App.h
+++ TestApp/Source/UWP/App.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include <Shared/InputManager.h>
 #include <Babylon/RuntimeUWP.h>
