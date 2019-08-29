7fc7556fcd39c819f836b8e6c725722ea170ff06
Cedric Guillemet <cedricguillemet@BigParks-iMac.guest.corp.microsoft.com>
2019-08-29 13:19:54 -0700

    macOS app runs

--- Library/Source/CommonApple.cpp
+++ Library/Source/CommonApple.cpp
@@ -2,14 +2,5 @@
 
 namespace babylon
 {
-    /*std::filesystem::path GetModulePath()
-    {
-        return std::filesystem::path{ "." };
-    }
-
-    std::string GetUrlFromPath(const std::filesystem::path& path)
-    {
-        // not implemented
-        throw std::exception();
-    }*/
+    // Apple stub
 }
