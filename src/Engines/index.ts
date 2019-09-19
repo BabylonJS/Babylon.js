f81d0086550b3a3bad069dea1611a33646b23cf5
David Catuhe <david.catuhe@live.fr>
2019-09-19 08:13:19 -0700

    Revert unwanted changes for this branch
    Former-commit-id: 79a3828a74b8bf4552adc8efa7e6a34d9c1ff6a9

--- src/Engines/index.ts
+++ src/Engines/index.ts
@@ -1,6 +1,5 @@
 export * from "./constants";
 export * from "./engine";
-export * from "./engine.backwardCompatibility";
 export * from "./engineStore";
 export * from "./nullEngine";
 export * from "./Extensions/index";
