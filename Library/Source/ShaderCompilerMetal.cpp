db9e93bd3aa9af33a96ce6f9be612b7f7cf13b7f
Cedric Guillemet <cedricguillemet@BigParks-iMac.guest.corp.microsoft.com>
2019-09-24 13:26:28 -0700

    jsc naming and tabs

--- Library/Source/ShaderCompilerMetal.cpp
+++ Library/Source/ShaderCompilerMetal.cpp
@@ -10,84 +10,84 @@
 namespace glslang
 {
     void AddShader(glslang::TProgram& program, glslang::TShader& shader, std::string_view source)
+    {
+        const std::array<const char*, 1> sources{ source.data() };
+        shader.setStrings(sources.data(), gsl::narrow_cast<int>(sources.size()));
+        shader.setEnvInput(glslang::EShSourceGlsl, shader.getStage(), glslang::EShClientVulkan, 100);
+        shader.setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_0);
+        shader.setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
+
+        // TODO: Do this to avoid the work around for dFdy?
+        //shader->setInvertY(true);
+
+        if (!shader.parse(&babylon::DefaultTBuiltInResource, 450, false, EShMsgDefault))
         {
-            const std::array<const char*, 1> sources{ source.data() };
-            shader.setStrings(sources.data(), gsl::narrow_cast<int>(sources.size()));
-            shader.setEnvInput(glslang::EShSourceGlsl, shader.getStage(), glslang::EShClientVulkan, 100);
-            shader.setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_0);
-            shader.setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
-
-            // TODO: Do this to avoid the work around for dFdy?
-            //shader->setInvertY(true);
-
-            if (!shader.parse(&babylon::DefaultTBuiltInResource, 450, false, EShMsgDefault))
-            {
-                throw std::exception();//shader.getInfoDebugLog());
-            }
-
-            program.addShader(&shader);
+            throw std::exception();//shader.getInfoDebugLog());
         }
 
-        std::unique_ptr<spirv_cross::Compiler> CompileShader(glslang::TProgram& program, EShLanguage stage, std::string& shaderResult)
+        program.addShader(&shader);
+    }
+
+    std::unique_ptr<spirv_cross::Compiler> CompileShader(glslang::TProgram& program, EShLanguage stage, std::string& shaderResult)
+    {
+        std::vector<uint32_t> spirv;
+        glslang::GlslangToSpv(*program.getIntermediate(stage), spirv);
+
+        spirv_cross::Parser parser{ std::move(spirv) };
+        parser.parse();
+
+        auto compiler = std::make_unique<spirv_cross::CompilerMSL>(parser.get_parsed_ir());
+
+        shaderResult = compiler->compile();
+        
+        std::string sourceNoStruct(shaderResult.data(), shaderResult.size());
+        const std::string main0 = "main0(";
+        size_t pos = sourceNoStruct.find(main0);
+        if (pos != std::string::npos)
         {
-            std::vector<uint32_t> spirv;
-            glslang::GlslangToSpv(*program.getIntermediate(stage), spirv);
-
-            spirv_cross::Parser parser{ std::move(spirv) };
-            parser.parse();
-
-            auto compiler = std::make_unique<spirv_cross::CompilerMSL>(parser.get_parsed_ir());
-
-            shaderResult = compiler->compile();
-            
-            std::string sourceNoStruct(shaderResult.data(), shaderResult.size());
-            const std::string main0 = "main0(";
-            size_t pos = sourceNoStruct.find(main0);
+            sourceNoStruct.replace(pos, main0.size(), "xlatMtlMain(");
+        }
+        
+        // WIP for constant names, Needed by bgfx.
+        {
+            const std::string frame0 = "_22";
+            pos = sourceNoStruct.find(frame0);
             if (pos != std::string::npos)
             {
-                sourceNoStruct.replace(pos, main0.size(), "xlatMtlMain(");
-            }
-            
-            // WIP for constant names, Needed by bgfx.
-            {
-                const std::string frame0 = "_22";
-                pos = sourceNoStruct.find(frame0);
-                if (pos != std::string::npos)
+                sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
+                while(1)
                 {
-                    sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
-                    while(1)
+                    pos = sourceNoStruct.find(frame0);
+                    if (pos == std::string::npos)
                     {
-                        pos = sourceNoStruct.find(frame0);
-                        if (pos == std::string::npos)
-                        {
-                            break;
-                        }
-                        sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
+                        break;
                     }
+                    sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
                 }
             }
-            
-            {
-                const std::string frame0 = "_69";
-                pos = sourceNoStruct.find(frame0);
-                if (pos != std::string::npos)
-                {
-                    sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
-                    while(1)
-                    {
-                        pos = sourceNoStruct.find(frame0);
-                        if (pos == std::string::npos)
-                        {
-                            break;
-                        }
-                        sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
-                    }
-                }
-            }
-            
-            shaderResult = sourceNoStruct;
-            return std::move(compiler);
         }
+        
+        {
+            const std::string frame0 = "_69";
+            pos = sourceNoStruct.find(frame0);
+            if (pos != std::string::npos)
+            {
+                sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
+                while(1)
+                {
+                    pos = sourceNoStruct.find(frame0);
+                    if (pos == std::string::npos)
+                    {
+                        break;
+                    }
+                    sourceNoStruct.replace(pos, frame0.size(), "_mtl_u");
+                }
+            }
+        }
+        
+        shaderResult = sourceNoStruct;
+        return std::move(compiler);
+    }
 }
 
 namespace babylon
