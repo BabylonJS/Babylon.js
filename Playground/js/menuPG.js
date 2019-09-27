59703fd514fb62da66a53370665bb698681b3430
Jaskar <anthony.gee@live.fr>
2019-09-27 17:47:41 +0200

    Playground : Fix examples on mobile

--- Playground/js/menuPG.js
+++ Playground/js/menuPG.js
@@ -49,7 +49,8 @@ class MenuPG {
         // Handle quit of menus
         for (var i = 0; i < this.allToDisplay.length; i++) {
             this.allToDisplay[i].addEventListener('mouseleave', function () {
-                this.removeAllOptions();
+                if(this.isMobileVersion) return;
+                else this.removeAllOptions();
             }.bind(this));
         }
 
