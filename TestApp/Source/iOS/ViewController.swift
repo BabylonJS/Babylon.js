e9f16156d41eb87cc3465f6d752b2ac2877a90eb
Cedric Guillemet <cedricguillemet@BigParks-iMac.guest.corp.microsoft.com>
2019-09-27 16:16:44 -0700

    ios stub

---
+++ TestApp/Source/iOS/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        if appDelegate != nil
+        {
+            let screenBounds = UIScreen.main.bounds
+            let width = screenBounds.size.width
+            let height = screenBounds.size.height
+            //let scale = UIScreen.main.scale
+            
+            //let view = self.view as! NativeViewerView
+            
+            // Swift doesn't understand void* pointers, rather it encapsulates them in a specialized pointer class
+            let rawMetalLayerPtr: UnsafeMutableRawPointer = Unmanaged.passUnretained(self.view).toOpaque()
+            
+            appDelegate!._bridge!.init(rawMetalLayerPtr, width: Int32(width), height: Int32(height))
+        }
+    }
+}
+
