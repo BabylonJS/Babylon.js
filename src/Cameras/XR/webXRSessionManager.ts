8c92d741feefc3b95d391354a0fd04ecf97edd44
Justin Murray <33846034+syntheticmagus@users.noreply.github.com>
2019-10-14 14:38:52 -0700

    Remove circular dependency
    Removed inadvertent circular dependency created by using outdated logging mechanism.

--- src/Cameras/XR/webXRSessionManager.ts
+++ src/Cameras/XR/webXRSessionManager.ts
@@ -6,7 +6,7 @@ import { InternalTexture, InternalTextureSource } from "../../Materials/Textures
 import { RenderTargetTexture } from "../../Materials/Textures/renderTargetTexture";
 import { WebXRRenderTarget } from './webXRTypes';
 import { WebXRManagedOutputCanvas } from './webXRManagedOutputCanvas';
-import { WebXRState, Tools } from '../../Legacy/legacy';
+import { WebXRState } from '../../Legacy/legacy';
 
 interface IRenderTargetProvider {
     getRenderTargetForEye(eye: XREye): RenderTargetTexture;
@@ -118,14 +118,14 @@ export class WebXRSessionManager implements IDisposable {
         return this.session.requestReferenceSpace(referenceSpace).then((referenceSpace: XRReferenceSpace) => {
             this.referenceSpace = referenceSpace;
         }, rejectionReason => {
-            Tools.Error("XR.requestReferenceSpace failed for the following reason: ");
-            Tools.Error(rejectionReason);
-            Tools.Log("Defaulting to universally-supported \"viewer\" reference space type.");
+            Logger.Error("XR.requestReferenceSpace failed for the following reason: ");
+            Logger.Error(rejectionReason);
+            Logger.Log("Defaulting to universally-supported \"viewer\" reference space type.");
 
             return this.session.requestReferenceSpace("viewer").then((referenceSpace: XRReferenceSpace) => {
                 this.referenceSpace = referenceSpace;
             }, rejectionReason => {
-                Tools.Error(rejectionReason);
+                Logger.Error(rejectionReason);
                 throw "XR initialization failed: required \"viewer\" reference space type not supported.";
             });
         });
