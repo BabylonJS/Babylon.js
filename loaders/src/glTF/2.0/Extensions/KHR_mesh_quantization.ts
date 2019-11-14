a2073835e2832422b9c811a5778af0faedceb45f
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2019-11-13 19:50:42 -0800

    Rename KHR_quantized_geometry to KHR_mesh_quantization
    Also clean up unnecessary member variable

--- loaders/src/glTF/2.0/Extensions/KHR_mesh_quantization.ts
+++ loaders/src/glTF/2.0/Extensions/KHR_mesh_quantization.ts
@@ -1,12 +1,12 @@
 import { IGLTFLoaderExtension } from "../glTFLoaderExtension";
 import { GLTFLoader } from "../glTFLoader";
 
-const NAME = "KHR_quantized_geometry";
+const NAME = "KHR_mesh_quantization";
 
 /**
- * [Specification](https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_quantized_geometry)
+ * [Specification](https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_mesh_quantization)
  */
-export class KHR_quantized_geometry implements IGLTFLoaderExtension {
+export class KHR_mesh_quantization implements IGLTFLoaderExtension {
     /**
      * The name of this extension.
      */
@@ -17,18 +17,14 @@ export class KHR_quantized_geometry implements IGLTFLoaderExtension {
      */
     public enabled: boolean;
 
-    private _loader: GLTFLoader;
-
     /** @hidden */
     constructor(loader: GLTFLoader) {
-        this._loader = loader;
-        this.enabled = this._loader.isExtensionUsed(NAME);
+        this.enabled = loader.isExtensionUsed(NAME);
     }
 
     /** @hidden */
     public dispose() {
-        delete this._loader;
     }
 }
 
-GLTFLoader.RegisterExtension(NAME, (loader) => new KHR_quantized_geometry(loader));
+GLTFLoader.RegisterExtension(NAME, (loader) => new KHR_mesh_quantization(loader));
