d7bbdff5af2a95ca97326a7856e812240d2a0405
Guide <babylonjsguide@gmail.com>
2019-11-18 16:17:11 +0000

    Scroll Viewer with Image Bars
    Former-commit-id: 7d7ca4e207c11925a993466046f6ce80f69dd54f

--- gui/src/2D/controls/scrollViewers/scrollViewer.ts
+++ gui/src/2D/controls/scrollViewers/scrollViewer.ts
@@ -4,12 +4,14 @@ import { PointerInfo, PointerEventTypes } from "babylonjs/Events/pointerEvents";
 
 import { Rectangle } from "../rectangle";
 import { Grid } from "../grid";
+import { Image } from "../image";
 import { Control } from "../control";
 import { Container } from "../container";
 import { Measure } from "../../measure";
 import { AdvancedDynamicTexture } from "../../advancedDynamicTexture";
 import { _ScrollViewerWindow } from "./scrollViewerWindow";
 import { ScrollBar } from "../sliders/scrollBar";
+import { ImageScrollBar } from "../sliders/imageScrollBar";
 import { _TypeStore } from 'babylonjs/Misc/typeStore';
 
 /**
@@ -20,10 +22,12 @@ export class ScrollViewer extends Rectangle {
     private _horizontalBarSpace: Rectangle;
     private _verticalBarSpace: Rectangle;
     private _dragSpace: Rectangle;
-    private _horizontalBar: ScrollBar;
-    private _verticalBar: ScrollBar;
+    private _horizontalBar: ScrollBar | ImageScrollBar;
+    private _verticalBar: ScrollBar | ImageScrollBar;
     private _barColor: string;
     private _barBackground: string;
+    private _barImage: Image;
+    private _barBackgroundImage: Image;
     private _barSize: number = 20;
     private _endLeft: number;
     private _endTop: number;
@@ -33,18 +37,20 @@ export class ScrollViewer extends Rectangle {
     private _onPointerObserver: Nullable<Observer<PointerInfo>>;
     private _clientWidth: number;
     private _clientHeight: number;
+    private _useImageBar: Boolean;
+    private _thumbSize: number = 1;
 
     /**
      * Gets the horizontal scrollbar
      */
-    public get horizontalBar(): ScrollBar {
+    public get horizontalBar(): ScrollBar | ImageScrollBar {
         return this._horizontalBar;
     }
 
     /**
      * Gets the vertical scrollbar
      */
-    public get verticalBar(): ScrollBar {
+    public get verticalBar(): ScrollBar | ImageScrollBar {
         return this._verticalBar;
     }
 
@@ -88,8 +94,10 @@ export class ScrollViewer extends Rectangle {
     * Creates a new ScrollViewer
     * @param name of ScrollViewer
     */
-    constructor(name?: string) {
+    constructor(name?: string, isImageBased?: boolean) {
         super(name);
+
+        this._useImageBar = isImageBased ? isImageBased : false;
 
         this.onDirtyObservable.add(() => {
             this._horizontalBarSpace.color = this.color;
@@ -106,8 +114,14 @@ export class ScrollViewer extends Rectangle {
         });
 
         this._grid = new Grid();
-        this._horizontalBar = new ScrollBar();
-        this._verticalBar = new ScrollBar();
+        if (this._useImageBar) {
+            this._horizontalBar = new ImageScrollBar();
+            this._verticalBar = new ImageScrollBar();
+        }
+        else {
+            this._horizontalBar = new ScrollBar();
+            this._verticalBar = new ScrollBar();
+        }
 
         this._window = new _ScrollViewerWindow();
         this._window.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
@@ -121,57 +135,29 @@ export class ScrollViewer extends Rectangle {
         super.addControl(this._grid);
         this._grid.addControl(this._window, 0, 0);
 
-        this._verticalBar.paddingLeft = 0;
-        this._verticalBar.width = "100%";
-        this._verticalBar.height = "100%";
-        this._verticalBar.barOffset = 0;
-        this._verticalBar.value = 0;
-        this._verticalBar.maximum = 1;
-        this._verticalBar.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
-        this._verticalBar.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-        this._verticalBar.isVertical = true;
-        this._verticalBar.rotation = Math.PI;
-        this._verticalBar.isVisible = false;
-
         this._verticalBarSpace = new Rectangle();
         this._verticalBarSpace.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
         this._verticalBarSpace.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
         this._verticalBarSpace.thickness = 1;
         this._grid.addControl(this._verticalBarSpace, 0, 1);
-        this._verticalBarSpace.addControl(this._verticalBar);
-
-        this._verticalBar.onValueChangedObservable.add((value) => {
-            this._window.top = value * this._endTop + "px";
-        });
-
-        this._horizontalBar.paddingLeft = 0;
-        this._horizontalBar.width = "100%";
-        this._horizontalBar.height = "100%";
-        this._horizontalBar.barOffset = 0;
-        this._horizontalBar.value = 0;
-        this._horizontalBar.maximum = 1;
-        this._horizontalBar.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
-        this._horizontalBar.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-        this._horizontalBar.isVisible = false;
+        this._addBar(this._verticalBar, this._verticalBarSpace, true, Math.PI);
 
         this._horizontalBarSpace = new Rectangle();
         this._horizontalBarSpace.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
         this._horizontalBarSpace.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
         this._horizontalBarSpace.thickness = 1;
         this._grid.addControl(this._horizontalBarSpace, 1, 0);
-        this._horizontalBarSpace.addControl(this._horizontalBar);
-
-        this._horizontalBar.onValueChangedObservable.add((value) => {
-            this._window.left = value * this._endLeft + "px";
-        });
+        this._addBar(this._horizontalBar, this._horizontalBarSpace, false, 0);
 
         this._dragSpace = new Rectangle();
         this._dragSpace.thickness = 1;
         this._grid.addControl(this._dragSpace, 1, 1);
 
         // Colors
-        this.barColor = "grey";
-        this.barBackground = "transparent";
+        if (!this._useImageBar) {
+            this.barColor = "grey";
+            this.barBackground = "transparent";
+        }
     }
 
     /** Reset the scroll viewer window to initial size */
@@ -228,6 +214,19 @@ export class ScrollViewer extends Rectangle {
         this._wheelPrecision = value;
     }
 
+    /** Gets or sets the scroll bar container background color */
+    public get scrollBackground(): string {
+        return this._dragSpace.background;
+    }
+
+    public set scrollBackground(color: string) {
+        if (this._dragSpace.background === color) {
+            return;
+        }
+
+        this._dragSpace.background = color;
+    }
+
     /** Gets or sets the bar color */
     public get barColor(): string {
         return this._barColor;
@@ -241,6 +240,23 @@ export class ScrollViewer extends Rectangle {
         this._barColor = color;
         this._horizontalBar.color = color;
         this._verticalBar.color = color;
+    }
+
+    /** Gets or sets the bar image */
+    public get barImage(): Image {
+        return this._barImage;
+    }
+
+    public set barImage(value: Image) {
+        if (this._barImage === value) {
+            return;
+        }
+
+        this._barImage = value;
+        let hb = <ImageScrollBar>this.horizontalBar;
+        let vb = <ImageScrollBar>this._verticalBar;
+        hb.thumbImage = value;
+        vb.thumbImage = value;
     }
 
     /** Gets or sets the size of the bar */
@@ -264,6 +280,20 @@ export class ScrollViewer extends Rectangle {
         }
     }
 
+    /** Gets or sets the size of the thumb */
+    public get thumbSize(): number {
+        return this._thumbSize;
+    }
+
+    public set thumbSize(value: number) {
+        if (this._thumbSize === value) {
+            return;
+        }
+
+        this._thumbSize = value;
+        this._markAsDirty();
+    }
+
     /** Gets or sets the bar background */
     public get barBackground(): string {
         return this._barBackground;
@@ -275,9 +305,27 @@ export class ScrollViewer extends Rectangle {
         }
 
         this._barBackground = color;
-        this._horizontalBar.background = color;
-        this._verticalBar.background = color;
+        let hb = <ScrollBar>this._horizontalBar;
+        let vb = <ScrollBar>this._verticalBar;
+        hb.background = color;
+        vb.background = color;
         this._dragSpace.background = color;
+    }
+
+    /** Gets or sets the bar background image */
+    public get barBackgroundImage(): Image {
+        return this._barBackgroundImage;
+    }
+
+    public set barBackgroundImage(value: Image) {
+        if (this._barBackgroundImage === value) {
+        }
+
+        this._barBackgroundImage = value;
+        let hb = <ImageScrollBar>this._horizontalBar;
+        let vb = <ImageScrollBar>this._verticalBar;
+        hb.backgroundImage = value;
+        vb.backgroundImage = value;
     }
 
     /** @hidden */
@@ -326,17 +374,49 @@ export class ScrollViewer extends Rectangle {
             this._rebuildLayout = true;
         }
 
-        let horizontalMultiplicator = this._clientWidth / windowContentsWidth;
-        let verticalMultiplicator = this._clientHeight / windowContentsHeight;
+        if (this._useImageBar && this._barImage) {
+            this._horizontalBar.thumbWidth = Math.min(0.9 * this._clientWidth, this._thumbSize * this._barImage.domImage.width * this._horizontalBarSpace.heightInPixels / this._barImage.domImage.height) + "px";
+            this._verticalBar.thumbWidth = Math.min(0.9 * this._clientHeight, this._thumbSize * this._barImage.domImage.width * this._verticalBarSpace.widthInPixels / this._barImage.domImage.height) + "px";
+        }
+        else {
+            let horizontalMultiplicator = this._clientWidth / windowContentsWidth;
+            let verticalMultiplicator = this._clientHeight / windowContentsHeight;
 
-        this._horizontalBar.thumbWidth = (this._clientWidth * horizontalMultiplicator) + "px";
-        this._verticalBar.thumbWidth = (this._clientHeight * verticalMultiplicator) + "px";
+            this._horizontalBar.thumbWidth = Math.min(0.9 * this._clientWidth, this._thumbSize * (this._clientWidth * horizontalMultiplicator)) + "px";
+            this._verticalBar.thumbWidth = Math.min(0.9 * this._clientHeight, this._thumbSize * (this._clientHeight * verticalMultiplicator)) + "px";
+        }
     }
 
     public _link(host: AdvancedDynamicTexture): void {
         super._link(host);
 
         this._attachWheel();
+    }
+
+    /** @hidden */
+    private _addBar(barControl: ScrollBar | ImageScrollBar, barContainer: Rectangle, isVertical: boolean, rotation: number) {
+        barControl.paddingLeft = 0;
+        barControl.width = "100%";
+        barControl.height = "100%";
+        barControl.barOffset = 0;
+        barControl.value = 0;
+        barControl.maximum = 1;
+        barControl.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+        barControl.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+        barControl.isVertical = isVertical;
+        barControl.rotation = rotation;
+        barControl.isVisible = false;
+
+        barContainer.addControl(barControl);
+
+        barControl.onValueChangedObservable.add((value) => {
+            if (rotation > 0) {
+                this._window.top = value * this._endTop + "px";
+            }
+            else {
+                this._window.left = value * this._endLeft + "px";
+            }
+        });
     }
 
     /** @hidden */
