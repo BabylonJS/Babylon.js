d4a090cb1223c1f2b335987a41050311e42d6a0b
ladevieq <ladevie.quentin@gmail.com>
2019-11-27 16:05:29 +0100

    Set the scene render mode to intermediate before screenshot
    When doing a screenshot using render target for the first time, the scene
    intermediate rendering is set to false at the end of the
    texture.render().
    
    Next time the function is called it makes all instances being avoided
    during the render.

--- src/Misc/screenshotTools.ts
+++ src/Misc/screenshotTools.ts
@@ -151,6 +151,7 @@ export class ScreenshotTools {
         const renderToTexture = () => {
             scene.incrementRenderId();
             scene.resetCachedMaterial();
+            scene._intermediateRendering = true;
             texture.render(true);
             texture.dispose();
 
