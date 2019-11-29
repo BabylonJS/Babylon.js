18fc8cea916d60fe9956e2aeccd6f8ca7ea04399
Alex Bogartz <alex@jumpkick.pro>
2019-11-29 14:41:04 -0800

    Fix engine

--- src/Engines/engine.ts
+++ src/Engines/engine.ts
@@ -1550,7 +1550,7 @@ export class Engine extends ThinEngine {
      * Example: [".dds", ".env", "myfile.png"]
      */
     public setCompressedTextureExclusions(skippedFiles: Array<string>): void {
-        ThinEngine.ExcludedCompressedTextures = skippedFiles;
+        this.excludedCompressedTextures = skippedFiles;
     }
 
     /**
