76da57a1c187c116b0e167a8a24206a37b405f2f
Cedric Guillemet <cedric.guillemet@gmail.com>
2019-12-13 11:52:06 +0100

    discard any previous set state before touching a new view

--- Library/Source/NativeEngine.h
+++ Library/Source/NativeEngine.h
@@ -79,6 +79,8 @@ namespace Babylon
         void Update() const
         {
             bgfx::setViewClear(m_viewId, m_flags, Color(), m_depth, m_stencil);
+            // discard any previous set state 
+            bgfx::discard();
             bgfx::touch(m_viewId);
         }
 
