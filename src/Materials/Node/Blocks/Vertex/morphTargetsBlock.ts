6793c8e2c3d30818772d25e785794e23a850947f
David Catuhe <david.catuhe@live.fr>
2020-01-16 14:37:45 -0800

    Fix #7466
    Former-commit-id: 0193244ee51780571aceaf39ed572b697e572087

--- src/Materials/Node/Blocks/Vertex/morphTargetsBlock.ts
+++ src/Materials/Node/Blocks/Vertex/morphTargetsBlock.ts
@@ -20,7 +20,6 @@ import "../../../../Shaders/ShadersInclude/morphTargetsVertexGlobalDeclaration";
  */
 export class MorphTargetsBlock extends NodeMaterialBlock {
     private _repeatableContentAnchor: string;
-    private _repeatebleContentGenerated = 0;
 
     /**
      * Create a new MorphTargetsBlock
@@ -154,7 +153,7 @@ export class MorphTargetsBlock extends NodeMaterialBlock {
     }
 
     public bind(effect: Effect, nodeMaterial: NodeMaterial, mesh?: Mesh) {
-        if (mesh && this._repeatebleContentGenerated) {
+        if (mesh && mesh.morphTargetManager && mesh.morphTargetManager.numInfluencers > 0) {
             MaterialHelper.BindMorphTargetParameters(mesh, effect);
         }
     }
@@ -170,7 +169,6 @@ export class MorphTargetsBlock extends NodeMaterialBlock {
         let uvOutput = this.uvOutput;
         let state = vertexShaderState;
         let repeatCount = defines.NUM_MORPH_INFLUENCERS as number;
-        this._repeatebleContentGenerated = repeatCount;
 
         var manager = (<Mesh>mesh).morphTargetManager;
         var hasNormals = manager && manager.supportsNormals && defines["NORMAL"];
