a64b3f5f3439625525d2b7ae434438e8e1516693
Avin Grape <carcinogen75@gmail.com>
2020-01-25 11:10:53 +0300

    fix jsDoc

--- src/Maths/math.vector.ts
+++ src/Maths/math.vector.ts
@@ -853,7 +853,7 @@ export class Vector3 {
     /**
      * Negate the current Vector3 and stores the result in the given vector "result" coordinates
      * @param result defines the Vector3 object where to store the result
-     * @returns this
+     * @returns the current Vector3
      */
     public negateToRef(result: Vector3): Vector3 {
         return result.copyFromFloats(this.x * -1, this.y * -1, this.z * -1);
