b079b526796992e994875eb0b90a7043051c539d
bghgary <bghgary@users.noreply.github.com>
2020-02-23 12:29:48 -0800

    Minor fix
    Former-commit-id: 25d8a0dbc6ce5f0c25bda22adfecadd5e074807f

--- loaders/src/glTF/2.0/Extensions/KHR_texture_basisu.ts
+++ loaders/src/glTF/2.0/Extensions/KHR_texture_basisu.ts
@@ -19,13 +19,14 @@ export class KHR_texture_basisu implements IGLTFLoaderExtension {
     public readonly name = NAME;
 
     /** Defines whether this extension is enabled. */
-    public enabled = true;
+    public enabled: boolean;
 
     private _loader: GLTFLoader;
 
     /** @hidden */
     constructor(loader: GLTFLoader) {
         this._loader = loader;
+        this.enabled = loader.isExtensionUsed(NAME);
     }
 
     /** @hidden */
