49adbdf0e0e0980abd9a5f863274cbb2496e17a5
bghgary <bghgary@users.noreply.github.com>
2020-02-29 16:04:37 -0800

    Add missing typedoc
    Former-commit-id: f58ce610bacd978e93b35db4f64262ffc0860c1c

--- src/Misc/dataStorage.ts
+++ src/Misc/dataStorage.ts
@@ -29,29 +29,62 @@ export class DataStorage {
         }
     }
 
+    /**
+     * Reads a string from the data storage
+     * @param key The key to read
+     * @param defaultValue The value if the key doesn't exist
+     * @returns The string value
+     */
     public static ReadString(key: string, defaultValue: string): string {
         const value = this._Storage.getItem(key);
         return (value !== null ? value : defaultValue);
     }
 
+    /**
+     * Writes a string to the data storage
+     * @param key The key to write
+     * @param value The value to write
+     */
     public static WriteString(key: string, value: string): void {
         this._Storage.setItem(key, value);
     }
 
+    /**
+     * Reads a boolean from the data storage
+     * @param key The key to read
+     * @param defaultValue The value if the key doesn't exist
+     * @returns The boolean value
+     */
     public static ReadBoolean(key: string, defaultValue: boolean): boolean {
         const value = this._Storage.getItem(key);
         return (value !== null ? (value === "true") : defaultValue);
     }
 
+    /**
+     * Writes a boolean to the data storage
+     * @param key The key to write
+     * @param value The value to write
+     */
     public static WriteBoolean(key: string, value: boolean) {
         this._Storage.setItem(key, value ? "true" : "false");
     }
 
+    /**
+     * Reads a number from the data storage
+     * @param key The key to read
+     * @param defaultValue The value if the key doesn't exist
+     * @returns The number value
+     */
     public static ReadNumber(key: string, defaultValue: number): number {
         const value = this._Storage.getItem(key);
         return (value !== null ? parseFloat(value) : defaultValue);
     }
 
+    /**
+     * Writes a number to the data storage
+     * @param key The key to write
+     * @param value The value to write
+     */
     public static WriteNumber(key: string, value: number) {
         this._Storage.setItem(key, value.toString());
     }
