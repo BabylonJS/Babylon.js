a11a58263146dd3095c32e0f15b9d3594ad0f1a9
Raanan Weber <info@raananweber.com>
2020-04-08 16:28:47 +0200

    fir for cross eye

--- src/Cameras/RigModes/stereoscopicRigMode.ts
+++ src/Cameras/RigModes/stereoscopicRigMode.ts
@@ -3,7 +3,7 @@ import { Viewport } from '../../Maths/math.viewport';
 
 Camera._setStereoscopicRigMode = function(camera: Camera) {
     var isStereoscopicHoriz = camera.cameraRigMode === Camera.RIG_MODE_STEREOSCOPIC_SIDEBYSIDE_PARALLEL || camera.cameraRigMode === Camera.RIG_MODE_STEREOSCOPIC_SIDEBYSIDE_CROSSEYED;
-    camera._rigCameras[0].viewport = new Viewport(0, 0, isStereoscopicHoriz ? 0.5: 1.0, isStereoscopicHoriz ? 1.0: 0.5);
-    camera._rigCameras[1].viewport = new Viewport(isStereoscopicHoriz ? 0.5 : 0, isStereoscopicHoriz ? 0 : 0.5, isStereoscopicHoriz ? 0.5: 1.0, isStereoscopicHoriz ? 1.0: 0.5);
-
+    var isCrossEye = camera.cameraRigMode === Camera.RIG_MODE_STEREOSCOPIC_SIDEBYSIDE_CROSSEYED;
+    camera._rigCameras[isCrossEye ? 1 : 0].viewport = new Viewport(0, 0, isStereoscopicHoriz ? 0.5 : 1.0, isStereoscopicHoriz ? 1.0 : 0.5);
+    camera._rigCameras[isCrossEye ? 0 : 1].viewport = new Viewport(isStereoscopicHoriz ? 0.5 : 0, isStereoscopicHoriz ? 0 : 0.5, isStereoscopicHoriz ? 0.5 : 1.0, isStereoscopicHoriz ? 1.0 : 0.5);
 };
