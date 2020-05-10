541fd787fe76d57dadfca3b97c26bf5523c57cbb
Dave Solares <50599569+PolygonalSun@users.noreply.github.com>
2020-05-10 12:40:36 -0700

    Fixed import/export
    Former-commit-id: 58360f07bd3a49a54d7f4dea8144dcf49598b8ab

--- src/DeviceInput/index.ts
+++ src/DeviceInput/index.ts
@@ -1,6 +1,6 @@
 export * from "./deviceInputSystem";
 export * from "./InputDevices/deviceEnums";
-export * from "./InputDevices/deviceSourceManager";
-
-import "./InputDevices/deviceTypes";
 export * from "./InputDevices/deviceTypes";
+
+import "./InputDevices/deviceSourceManager";
+export * from "./InputDevices/deviceSourceManager";
