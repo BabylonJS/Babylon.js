74619ea3d4df9f99157b6d0a3e2f740c6e8ac8c5
David Catuhe <david.catuhe@live.fr>
2020-05-12 11:26:26 -0700

    4.2.0-alpha.16
    Former-commit-id: 60883149ef68fb74dc39e59456b684f0d61ca628

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -132,14 +132,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@4.2.0-alpha.15";
+        return "babylonjs@4.2.0-alpha.16";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "4.2.0-alpha.15";
+        return "4.2.0-alpha.16";
     }
 
     /**
