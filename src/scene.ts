e90a6d6f16182f323751c5b1d47823e8cd47e0f6
Popov72 <github@evpopov.com>
2020-05-16 23:03:30 +0200

    Rollback latest change
    Former-commit-id: 3700617c12bbd5527ecd02c28de1e37a3ddeb529

--- src/scene.ts
+++ src/scene.ts
@@ -1757,9 +1757,7 @@ export class Scene extends AbstractScene implements IAnimatable {
                 return false;
             }
 
-            let hardwareInstancedRendering = (
-                    (mesh.getClassName() === "InstancedMesh" || mesh.getClassName() === "InstancedLinesMesh") && (<Mesh>mesh).instances.length > 0 || mesh.hasThinInstances
-                ) && engine.getCaps().instancedArrays;
+            let hardwareInstancedRendering = mesh.hasThinInstances || mesh.getClassName() === "InstancedMesh" || mesh.getClassName() === "InstancedLinesMesh" || engine.getCaps().instancedArrays && (<Mesh>mesh).instances.length > 0;
             // Is Ready For Mesh
             for (let step of this._isReadyForMeshStage) {
                 if (!step.action(mesh, hardwareInstancedRendering)) {
