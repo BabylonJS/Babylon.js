208dd8f07b5c15a5424065f507240d76c308d291
Alejandro Toledo <alex@pixelspace.com>
2020-05-19 10:35:53 -0500

    value update
    Former-commit-id: 89d2d18e6fde7c8791e46bef2f7485c330ec964f

--- inspector/src/components/actionTabs/tabs/propertyGrids/animations/graphActionsBar.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/animations/graphActionsBar.tsx
@@ -22,11 +22,11 @@ export class GraphActionsBar extends React.Component<IGraphActionsBarProps>{
            <div className="actions-wrapper">
                <div className="action-input">
                <label>Frame</label>
-               <input type="number" value={this.props.currentFrame} onChange={this.props.handleFrameChange}/>
+               <input type="number" value={this.props.currentFrame} onChange={this.props.handleFrameChange} step="1"/>
                </div>
                <div className="action-input">
                <label>Value</label>
-               <input type="number" value={this.props.currentValue.toFixed(3)} onChange={this.props.handleValueChange}/>
+               <input type="number" value={this.props.currentValue.toFixed(3)} onChange={this.props.handleValueChange} step="0.001"/>
                </div>
               <ButtonLineComponent label={"Add Keyframe"} onClick={this.props.addKeyframe} />
               <ButtonLineComponent label={"Remove Keyframe"} onClick={this.props.removeKeyframe} />
