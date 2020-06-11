6b425b7db9b5783562d073708a340dd04815f5b4
Alejandro Toledo Martinez <toledoal@users.noreply.github.com>
2020-06-11 09:21:56 -0500

    Style timeline (#8353)
    * timeline 1
    
    * Styling 3
    
    * whats new
    
    * active key
    
    * formatting
    
    Co-authored-by: Alejandro Toledo <alex@pixelspace.com>
    Former-commit-id: 7391bd32b687ef1deaac3e5a268b563fdb9f5778

--- inspector/src/components/actionTabs/tabs/propertyGrids/animations/playhead.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/animations/playhead.tsx
@@ -2,24 +2,26 @@
 import * as React from "react";
 
 interface IPlayheadProps {
-   frame: number;
-   offset: number;
+    frame: number;
+    offset: number;
 }
 
-export class Playhead extends React.Component<IPlayheadProps>{ 
+export class Playhead extends React.Component<IPlayheadProps>{
     constructor(props: IPlayheadProps) {
         super(props);
     }
-     
-    render() { 
-       return (
-           <div className="playhead-wrapper" id="playhead" style={{left: `calc(${this.props.frame * (this.props.offset) }px - 13px)`}}>
-            <div className="playhead">{this.props.frame}</div>
-            <div className="playhead-triangle"></div>
-            <div className="playhead-line"></div>
-           </div>
+
+    render() {
+        return (
+            <div className="playhead-wrapper" id="playhead" style={{ left: `calc(${this.props.frame * (this.props.offset)}px - 13px)` }}>
+                <div className="playhead-line"></div>
+                <div className="playhead-handle">
+                    <div className="playhead-circle"></div>
+                    <div className="playhead">{this.props.frame}</div>
+                </div>
+            </div>
         )
     }
-} 
+}
 
 
