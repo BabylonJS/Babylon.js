7a43553604b70933cdb3711595dbc97ec5d9567a
Popov72 <github@evpopov.com>
2020-06-17 16:33:20 +0200

    Revert fix because handled in another PR

--- loaders/src/OBJ/objFileLoader.ts
+++ loaders/src/OBJ/objFileLoader.ts
@@ -449,10 +449,10 @@ export class OBJFileLoader implements ISceneLoaderPluginAsync, ISceneLoaderPlugi
                 unwrappedPositionsForBabylon.push(wrappedPositionForBabylon[l].x, wrappedPositionForBabylon[l].y, wrappedPositionForBabylon[l].z);
                 unwrappedNormalsForBabylon.push(wrappedNormalsForBabylon[l].x, wrappedNormalsForBabylon[l].y, wrappedNormalsForBabylon[l].z);
                 unwrappedUVForBabylon.push(wrappedUvsForBabylon[l].x, wrappedUvsForBabylon[l].y); //z is an optional value not supported by BABYLON
-                if (this._meshLoadOptions.ImportVertexColors === true) {
-                    //Push the r, g, b, a values of each element in the unwrapped array
-                    unwrappedColorsForBabylon.push(wrappedColorsForBabylon[l].r, wrappedColorsForBabylon[l].g, wrappedColorsForBabylon[l].b, wrappedColorsForBabylon[l].a);
-                }
+            }
+            if (this._meshLoadOptions.ImportVertexColors === true) {
+                //Push the r, g, b, a values of each element in the unwrapped array
+                unwrappedColorsForBabylon.push(wrappedColorsForBabylon[l].r, wrappedColorsForBabylon[l].g, wrappedColorsForBabylon[l].b, wrappedColorsForBabylon[l].a);
             }
             // Reset arrays for the next new meshes
             wrappedPositionForBabylon = [];
