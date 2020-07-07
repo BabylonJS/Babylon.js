9ca4da550a3a8892628dc1152743a8d84f19c802
David `Deltakosh` Catuhe <david.catuhe@live.fr>
2020-07-07 15:25:10 -0700

    Fix build

--- dist/preview release/playground/babylon.playground.module.d.ts
+++ dist/preview release/playground/babylon.playground.module.d.ts
@@ -3,12 +3,33 @@ declare module "babylonjs-playground/globalState" {
     export class GlobalState {
     }
 }
+declare module "babylonjs-playground/components/monacoComponent" {
+    import * as React from "react";
+    interface IMonacoComponentProps {
+        language: "JS" | "TS";
+    }
+    export class MonacoComponent extends React.Component<IMonacoComponentProps> {
+        private _hostReference;
+        private _editor;
+        private _definitionWorker;
+        private _deprecatedCandidates;
+        constructor(props: IMonacoComponentProps);
+        setupMonaco(): Promise<void>;
+        setupMonacoColorProvider(): void;
+        setupMonacoCompilationPipeline(libContent: string): void;
+        setupDefinitionWorker(libContent: string): void;
+        analyzeCode(): Promise<void>;
+        isDeprecatedEntry(details: any): any;
+        isDeprecatedTag(tag: any): boolean;
+        componentDidMount(): void;
+        render(): JSX.Element;
+    }
+}
 declare module "babylonjs-playground/playground" {
     import * as React from "react";
     interface IPlaygroundProps {
     }
     export class Playground extends React.Component<IPlaygroundProps, {
-        isFooterVisible: boolean;
         errorMessage: string;
     }> {
         constructor(props: IPlaygroundProps);
@@ -31,10 +52,30 @@ declare module PLAYGROUND {
     }
 }
 declare module PLAYGROUND {
+    interface IMonacoComponentProps {
+        language: "JS" | "TS";
+    }
+    export class MonacoComponent extends React.Component<IMonacoComponentProps> {
+        private _hostReference;
+        private _editor;
+        private _definitionWorker;
+        private _deprecatedCandidates;
+        constructor(props: IMonacoComponentProps);
+        setupMonaco(): Promise<void>;
+        setupMonacoColorProvider(): void;
+        setupMonacoCompilationPipeline(libContent: string): void;
+        setupDefinitionWorker(libContent: string): void;
+        analyzeCode(): Promise<void>;
+        isDeprecatedEntry(details: any): any;
+        isDeprecatedTag(tag: any): boolean;
+        componentDidMount(): void;
+        render(): JSX.Element;
+    }
+}
+declare module PLAYGROUND {
     interface IPlaygroundProps {
     }
     export class Playground extends React.Component<IPlaygroundProps, {
-        isFooterVisible: boolean;
         errorMessage: string;
     }> {
         constructor(props: IPlaygroundProps);
