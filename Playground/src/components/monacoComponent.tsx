3b2d8cbd76b3bfd9b6971042b6c21e775a569533
David `Deltakosh` Catuhe <david.catuhe@live.fr>
2020-07-08 07:52:07 -0700

    .
    Former-commit-id: 684b4c65b0d6ab665a743ee9a569e259cb7532eb

--- Playground/src/components/monacoComponent.tsx
+++ Playground/src/components/monacoComponent.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import 'monaco-editor/esm/vs/editor/editor.api'
+
+declare var monaco: any;
 
 require("../scss/monaco.scss");
 
