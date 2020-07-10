5d6d36184f4836a0099bef0bf2a82bfcdd48c690
David `Deltakosh` Catuhe <david.catuhe@live.fr>
2020-07-09 17:03:31 -0700

    create new

--- Playground/src/components/commandBarComponent.tsx
+++ Playground/src/components/commandBarComponent.tsx
@@ -18,10 +18,15 @@ export class CommandBarComponent extends React.Component<ICommandBarComponentPro
         this.props.globalState.onRunRequiredObservable.notifyObservers();
     }
 
+    onNew() {
+        this.props.globalState.onNewRequiredObservable.notifyObservers();
+    }
+
     public render() {
         return (
             <div className={"commands " + (this.props.globalState.language === "JS" ? "background-js" : "background-ts")}>
                 <CommandButtonComponent globalState={this.props.globalState} tooltip="Run" icon="play" isActive={true} onClick={()=> this.onPlay()}/>
+                <CommandButtonComponent globalState={this.props.globalState} tooltip="Create new" icon="new" isActive={false} onClick={()=> this.onNew()}/>
             </div>
         );
     }
