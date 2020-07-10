276f9b89a6f517add625bb0f8b776cf725c66814
sebavan <vandenberghe.sebastien@gmail.com>
2020-07-10 03:05:06 +0200

    One more step

---
+++ Tools/Gulp/tasks/gulpTasks-watchApps.js
@@ -0,0 +1,47 @@
+// Import Dependencies.
+var gulp = require("gulp");
+var webpack = require('webpack');
+var webpackStream = require("webpack-stream");
+var path = require("path");
+
+// Read the full config.
+var config = require("../../Config/config.js");
+
+/**
+ * Watch ts files and fire repective tasks.
+ */
+gulp.task("watchApps", function startWatch() {
+    var tasks = [];
+
+    config.apps.map(function(module) {
+        var settings = config[module].computed;
+        if (!config[module].isCore && settings) {
+            var wpConfig = require(settings.webpackConfigPath);
+
+            // watch on.
+            wpConfig.watch = true;
+            // dev mode and absolute path sourcemaps for debugging
+            wpConfig.mode = "development";
+            wpConfig.devtool = "nosources-source-map";
+
+            // Source Map Remapping for dev tools.
+            wpConfig.output.devtoolModuleFilenameTemplate = (info) => {
+                info.resourcePath = path.normalize(info.resourcePath);
+
+                if (!path.isAbsolute(info.resourcePath)) {
+                    info.resourcePath = path.join(settings.srcDirectory, info.resourcePath);
+                }
+
+                return `../../../${path.relative(config.computed.rootFolder, info.resourcePath).replace(/\\/g, "/")}`;
+            };
+
+            var outputDirectory = settings.localDevUMDDirectory;
+            tasks.push(
+                webpackStream(wpConfig, webpack)
+                    .pipe(gulp.dest(outputDirectory))
+            );
+        }
+    });
+
+    return Promise.resolve();
+});
