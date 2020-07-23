ccb6e321a5acc060d6449eda7a068db97230cb44
Darragh Burke <t-dab@microsoft.com>
2020-07-23 13:31:57 -0700

    color picker UI

--- inspector/src/components/actionTabs/tabs/propertyGrids/materials/textures/toolBar.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/materials/textures/toolBar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { SketchPicker } from 'react-color';
 
 export interface Tool {
     type: any,
@@ -18,18 +19,29 @@ interface ToolBarProps {
 
 interface ToolBarState {
     toolURL : string;
+    pickerEnabled : boolean;
 }
 
+const addTool = require('./assets/addTool.svg');
+
 export class ToolBar extends React.Component<ToolBarProps, ToolBarState> {
+    private pickerRef : React.RefObject<HTMLDivElement>;
     constructor(props : ToolBarProps) {
         super(props);
         this.state = {
             toolURL: "",
+            pickerEnabled: false
         };
+        this.pickerRef = React.createRef();
     }
+
+    computeRGBAColor() {
+        const opacityInt = Math.floor(this.props.metadata.opacity * 255);
+        const opacityHex = opacityInt.toString(16).padStart(2, '0');
+        return `${this.props.metadata.color}${opacityHex}`;
+    }
+
     render() {
-        // No need to display toolbar if there aren't any tools loaded
-        if (this.props.tools.length === 0) return <></>;
         return <div id='toolbar'>
             <div id='tools'>
                 {this.props.tools.map(
@@ -44,10 +56,26 @@ export class ToolBar extends React.Component<ToolBarProps, ToolBarState> {
                         />
                     }
                 )}
+                <img src={addTool} className='icon button' title='Add Tool' alt='Add Tool'/>
             </div>
-            <div id='color' title='Color' className='icon button'>
-                <div id='activeColor' style={{backgroundColor: this.props.metadata.color}}></div>
-            </div>
+                <div id='color' onClick={() => this.setState({pickerEnabled: !this.state.pickerEnabled})} title='Color' className='icon button'>
+                    <div id='activeColor' style={{backgroundColor: this.props.metadata.color}}></div>
+                </div>
+                {
+                    this.state.pickerEnabled &&
+                    <>
+                        <div className='color-picker-cover' onClick={evt => {
+                            if (evt.target !== this.pickerRef.current?.ownerDocument.querySelector('.color-picker-cover')) {
+                                return;
+                            }
+                            this.setState({pickerEnabled: false});
+                        }}>
+                        </div>
+                        <div className='color-picker' ref={this.pickerRef}>
+                                <SketchPicker disableAlpha={false} color={this.computeRGBAColor()}  onChangeComplete={color => this.props.setMetadata({color: color.hex, opacity: color.rgb.a})}/>
+                        </div>
+                    </>
+                }
         </div>;
     }
 }
