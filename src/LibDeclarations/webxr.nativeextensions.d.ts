6b2a96dadfa1fb89e03abeb0dc931a989cc73594
Alex Tran <altran@microsoft.com>
2020-07-29 10:22:19 -0700

    Expose feature points in BabylonJS as a native extension.
    Former-commit-id: 361f5a2686d561da22fadc3381879636b3eed0b9

---
+++ src/LibDeclarations/webxr.nativeextensions.d.ts
@@ -0,0 +1,7 @@
+// This file contains native only extensions for WebXR  These APIs are not supported in the browser yet.
+// They are intended for use with either Babylon Native https://github.com/BabylonJS/BabylonNative or
+// Babylon React Native: https://github.com/BabylonJS/BabylonReactNative
+
+interface XRFrame {
+    featurePointCloud? : Array<number>;
+}
