791bdbb9bc67b1e06ad713d1fafe0d15b1196b4c
Michael Bond <mbond@adobe.com>
2020-08-11 08:14:17 -0700

    Fix linting error
    Former-commit-id: fd9e2a0cc629d6161c15cfae5d6a6b8ae3d3461b

--- loaders/src/glTF/2.0/Extensions/KHR_materials_volume.ts
+++ loaders/src/glTF/2.0/Extensions/KHR_materials_volume.ts
@@ -79,7 +79,7 @@ export class KHR_materials_volume implements IGLTFLoaderExtension {
         pbrMaterial.subSurface.tintColorAtDistance = extension.attenuationDistance !== undefined ? extension.attenuationDistance : Number.MAX_VALUE;
         if (extension.attenuationColor !== undefined && extension.attenuationColor.length == 3) {
             pbrMaterial.subSurface.tintColor = new Color3(1.0, 1.0, 1.0);
-            pbrMaterial.subSurface.tintColor.copyFromFloats(extension.attenuationColor[0], extension.attenuationColor[1], extension.attenuationColor[2])
+            pbrMaterial.subSurface.tintColor.copyFromFloats(extension.attenuationColor[0], extension.attenuationColor[1], extension.attenuationColor[2]);
         } else {
             pbrMaterial.subSurface.tintColor = new Color3(1.0, 1.0, 1.0);
         }
