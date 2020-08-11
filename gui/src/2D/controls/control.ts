24babc81139178a8d4be4e357c77820acd923a78
David `Deltakosh` Catuhe <david.catuhe@live.fr>
2020-08-11 16:53:15 -0700

    Associated with #8695
    Former-commit-id: 63ccbcbbbd810f06d72eab49745767385c98dd0a

--- gui/src/2D/controls/control.ts
+++ gui/src/2D/controls/control.ts
@@ -1400,7 +1400,12 @@ export class Control {
         if (this._isDirty || !this._cachedParentMeasure.isEqualsTo(parentMeasure)) {
             this.host._numLayoutCalls++;
 
-            this._currentMeasure.transformToRef(this._transformMatrix, this._prevCurrentMeasureTransformedIntoGlobalSpace);
+            this._currentMeasure.addAndTransformToRef(this._transformMatrix, 
+                -this.paddingLeftInPixels | 0,
+                -this.paddingTopInPixels | 0,
+                this.paddingRightInPixels | 0,
+                this.paddingBottomInPixels | 0,
+                this._prevCurrentMeasureTransformedIntoGlobalSpace);
 
             context.save();
 
