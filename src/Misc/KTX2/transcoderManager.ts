0b73807784fa331b290042b0e29f029071b3c08c
Popov72 <github@evpopov.com>
2020-08-18 17:45:37 +0200

    Add a transcoder manager class
    Former-commit-id: da2f7007669338110d7c2eeed8bb8381210b80bc

---
+++ src/Misc/KTX2/transcoderManager.ts
@@ -0,0 +1,44 @@
+import { transcodeTarget, sourceTextureFormat, Transcoder } from './transcoder';
+import { Nullable } from '../../types';
+import { WASMMemoryManager } from './wasmMemoryManager';
+
+/**
+ * @hidden
+ */
+export class TranscoderManager {
+
+    public static _Transcoders: Array<typeof Transcoder> = [];
+
+    public static registerTranscoder(transcoder: typeof Transcoder) {
+        TranscoderManager._Transcoders.push(transcoder);
+    }
+
+    private static _transcoderInstances: { [key: string]: Transcoder } = {};
+
+    private _wasmMemoryManager: WASMMemoryManager;
+
+    public findTranscoder(src: sourceTextureFormat, dst: transcodeTarget): Nullable<Transcoder> {
+        let transcoder: Nullable<Transcoder> = null;
+
+        for (let i = 0; i < TranscoderManager._Transcoders.length; ++i) {
+            if (TranscoderManager._Transcoders[i].CanTranscode(src, dst)) {
+                const key = sourceTextureFormat[src] + "_" + transcodeTarget[dst];
+                transcoder = TranscoderManager._transcoderInstances[key];
+                if (!transcoder) {
+                    transcoder = new TranscoderManager._Transcoders[i]();
+                    transcoder!.initialize();
+                    if (transcoder!.needMemoryManager()) {
+                        if (!this._wasmMemoryManager) {
+                            this._wasmMemoryManager = new WASMMemoryManager();
+                        }
+                        transcoder!.setMemoryManager(this._wasmMemoryManager);
+                    }
+                    TranscoderManager._transcoderInstances[key] = transcoder;
+                }
+                break;
+            }
+        }
+
+        return transcoder;
+    }
+}
