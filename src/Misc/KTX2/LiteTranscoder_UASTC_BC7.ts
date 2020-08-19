aa5a2a7f3458428508cce45ceb4dee7bd5300d92
Popov72 <github@evpopov.com>
2020-08-19 14:58:11 +0200

    Nit

--- src/Misc/KTX2/LiteTranscoder_UASTC_BC7.ts
+++ src/Misc/KTX2/LiteTranscoder_UASTC_BC7.ts
@@ -5,12 +5,16 @@ import { LiteTranscoder } from './LiteTranscoder';
  * @hidden
  */
 export class LiteTranscoder_UASTC_BC7 extends LiteTranscoder {
+    /**
+     * URL to use when loading the wasm module for the transcoder
+     */
+    public static WasmModuleURL = "https://preview.babylonjs.com/ktx2Transcoders/uastc_bc7.wasm";
 
     public static CanTranscode(src: sourceTextureFormat, dst: transcodeTarget): boolean {
         return src === sourceTextureFormat.UASTC4x4 && dst === transcodeTarget.BC7_M5_RGBA;
     }
 
     public initialize(): void {
-        this.setModulePath("https://preview.babylonjs.com/ktx2Transcoders/uastc_bc7.wasm");
+        this.setModulePath(LiteTranscoder_UASTC_BC7.WasmModuleURL);
     }
 }
