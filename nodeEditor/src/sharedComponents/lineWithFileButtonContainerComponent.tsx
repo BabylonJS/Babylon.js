c0ed2d900d5a01a0223d74ff9e62e786b5f556f8
Pamela Wolf <pamela.wolf@digipen.edu>
2020-08-20 21:24:20 -0700

    adjustments to the UI for the adding

--- nodeEditor/src/sharedComponents/lineWithFileButtonContainerComponent.tsx
+++ nodeEditor/src/sharedComponents/lineWithFileButtonContainerComponent.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { DataStorage } from 'babylonjs/Misc/dataStorage';
 const addButton = require('../../imgs/add.svg');
 
 interface ILineWithFileButtonContainerComponentProps {
     title: string;
-    children: any[] | any;
     closed?: boolean;
     label: string;
     onClick: (file: File) => void;
@@ -43,57 +40,20 @@ export class LineWithFileButtonContainerComponent extends React.Component<ILineW
         this.setState({ isExpanded: newState });
     }
 
-    renderHeader() {
-        let className = this.state.isExpanded ? "collapse" : "collapse closed";
-
-        return (
-            <div className="header" onClick={() => this.switchExpandedState()}>
-                <div className="title">
-                    {this.props.title}
-                </div>
-                <div className="buttonIcon" title="Upload Custom">
-                <label htmlFor={this.props.uploadName ? this.props.uploadName : "file-upload"} className="file-upload">
-                        </label>
-                    <div className="icon">
-                    <img title="Add" className="addIcon" src={addButton}/>
-                    </div>
-                    <div className="icon">
-                    <input ref={this.uploadRef} id={this.props.uploadName ? this.props.uploadName : "file-upload"} type="file" accept={this.props.accept} onChange={evt => this.onChange(evt)} />
-                    </div>
-                    <div className={className}>
-                    <FontAwesomeIcon icon={faChevronDown} />
-                    </div>
-                </div>
-               
-            </div>
-        );
-    }
-    
     render() {
-        
-        if (!this.state.isExpanded) {
-            return (
-                <div className="paneContainer">
-                    <div className="paneContainer-content">
-                        {
-                            this.renderHeader()
-                        }
-                    </div>
-                </div>
-            );
-        }
-
         return (
-            <div className="paneContainer">
-                <div className="paneContainer-content">
-                    {
-                        this.renderHeader()
-                    }
-                    <div className="paneList">
-                        {this.props.children}
-                    </div >
+            <div className="nonDraggableLine withButton" title="add">
+                Add
+                <div className="icon" title="Upload Custom">
+                <img src={addButton}/>
                 </div>
+                <div className="buttonLine" title="Upload Custom">
+                    <label htmlFor={this.props.uploadName ? this.props.uploadName : "file-upload"} className="file-upload"/>   
+                    <input ref={this.uploadRef} id={this.props.uploadName ? this.props.uploadName : "file-upload"} type="file" accept={this.props.accept} onChange={evt => this.onChange(evt)} />
+                </div>
+                
             </div>
         );
+    
     }
 }
