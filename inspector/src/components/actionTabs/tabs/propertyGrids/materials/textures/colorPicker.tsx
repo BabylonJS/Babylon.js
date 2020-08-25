b104ef7b422118ccc154bd77b8ba92a3efca77a3
Darragh Burke <t-dab@microsoft.com>
2020-08-25 15:31:05 -0700

    remove unneeded class
    Former-commit-id: e7a09fe500253fa304b7d293cf8f3d3d7b4a87f3

--- inspector/src/components/actionTabs/tabs/propertyGrids/materials/textures/colorPicker.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { SketchPicker } from 'react-color';
-import { Color4 } from 'babylonjs';
-
-export function ColorPicker() {
-    const [color, setColor] = React.useState(new Color4());
-    return <SketchPicker color={color.toHexString(false)}  onChange={color => setColor(new Color4(color.rgb.r / 255,color.rgb.g / 255,color.rgb.b / 255,color.rgb.a))}/>;
-}
