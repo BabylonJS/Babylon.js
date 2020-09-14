9f1d1b6a80099bcad6499b8b5130811048b4cd72
David `Deltakosh` Catuhe <david.catuhe@live.fr>
2020-09-14 09:35:53 -0700

    4.2.0-beta.2

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -152,14 +152,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@4.2.0-beta.1";
+        return "babylonjs@4.2.0-beta.2";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "4.2.0-beta.1";
+        return "4.2.0-beta.2";
     }
 
     /**
