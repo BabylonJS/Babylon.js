73b3f1ac3000aefc677b827ed7a6c3f657cd6aac
David Catuhe <david.catuhe@live.fr>
2020-09-30 17:30:11 -0700

    4.2.0-beta.7
    Former-commit-id: 42e1c588592f1de34b9d87180fd2c732a32cded8

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -157,14 +157,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@4.2.0-beta.6";
+        return "babylonjs@4.2.0-beta.7";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "4.2.0-beta.6";
+        return "4.2.0-beta.7";
     }
 
     /**
