813522dda7936a9c086dd37f424cee61291b393a
Popov72 <github@evpopov.com>
2020-10-05 16:51:29 +0200

    Fix view not being created correctly when using swap&die
    Former-commit-id: 911c9896f9cf293b574f35c0240210acceb32d3b

--- src/Materials/Textures/hardwareTextureWrapper.ts
+++ src/Materials/Textures/hardwareTextureWrapper.ts
@@ -4,6 +4,7 @@ export interface HardwareTextureWrapper {
     underlyingResource: any;
 
     set(hardwareTexture: any): void;
+    setUsage(textureSource: number, generateMipMaps: boolean, isCube: boolean, width: number, height: number): void;
     reset(): void;
     release(): void;
 }
