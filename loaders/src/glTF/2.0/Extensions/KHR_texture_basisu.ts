d7cf5210eb083aa55ecff5bd38ef41672931e90f
Popov72 <github@evpopov.com>
2020-10-10 01:21:42 +0200

    nit
    Former-commit-id: 00761915a37b139c866c90b4d73701431c9caea5

--- loaders/src/glTF/2.0/Extensions/KHR_texture_basisu.ts
+++ loaders/src/glTF/2.0/Extensions/KHR_texture_basisu.ts
@@ -1,6 +1,6 @@
 import { IGLTFLoaderExtension } from "../glTFLoaderExtension";
 import { GLTFLoader, ArrayItem } from "../glTFLoader";
-import { ITexture, ITextureInfo } from "../glTFLoaderInterfaces";
+import { ITexture } from "../glTFLoaderInterfaces";
 import { BaseTexture } from "babylonjs/Materials/Textures/baseTexture";
 import { Nullable } from "babylonjs/types";
 import { IKHRTextureBasisU } from 'babylonjs-gltf2interface';
