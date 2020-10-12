a5d7c12ab21375a929cb7c51fb4de70b81e407b1
Raanan Weber <info@raananweber.com>
2020-10-12 18:35:13 +0200

    Better log

--- src/Misc/tools.ts
+++ src/Misc/tools.ts
@@ -747,7 +747,7 @@ export class Tools {
         if (typeof args[0] === "boolean") {
             return args[0];
         } else if (typeof args[1] === "boolean") {
-            Tools.Warn("Make sure you don't pass HTML element to attachControl function");
+            Tools.Warn("Don't pass HTML element to attachControl. Set engine.inputElement instead");
             return args[1];
         }
 
