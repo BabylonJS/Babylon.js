efa73f821d5c3b6343b7b4d7faf1a9377a2811a3
Popov72 <github@evpopov.com>
2020-10-23 00:31:37 +0200

    Make the inputText and inputPassword work with unicode strings
    Former-commit-id: 520dfcefa039192b368c38c1f3addc9588786922

--- gui/src/2D/controls/inputPassword.ts
+++ gui/src/2D/controls/inputPassword.ts
@@ -1,16 +1,18 @@
-import { InputText } from "./inputText";
+import { InputText, TextWrapper } from "./inputText";
 import { _TypeStore } from 'babylonjs/Misc/typeStore';
 
 /**
  * Class used to create a password control
  */
 export class InputPassword extends InputText {
-    protected _beforeRenderText(text: string): string {
+    protected _beforeRenderText(textWrapper: TextWrapper): TextWrapper {
+        const pwdTextWrapper = new TextWrapper();
         let txt = "";
-        for (let i = 0; i < text.length; i++) {
+        for (let i = 0; i < textWrapper.length; i++) {
             txt += "\u2022";
         }
-        return txt;
+        pwdTextWrapper.text = txt;
+        return pwdTextWrapper;
     }
 }
 _TypeStore.RegisteredTypes["BABYLON.GUI.InputPassword"] = InputPassword;
