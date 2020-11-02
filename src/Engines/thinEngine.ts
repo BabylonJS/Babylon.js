68f3d1e261a9f7fb2e7db1266bfd0e3d6aa00cd0
David Catuhe <david.catuhe@live.fr>
2020-11-02 13:07:19 -0800

    4.2.0-beta.22

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -158,14 +158,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@4.2.0-beta.21";
+        return "babylonjs@4.2.0-beta.22";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "4.2.0-beta.21";
+        return "4.2.0-beta.22";
     }
 
     /**
