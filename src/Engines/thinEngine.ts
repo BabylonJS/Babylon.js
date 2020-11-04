428d6adaac9cf78ddbe1a9b98903ce19d64e621e
David Catuhe <david.catuhe@live.fr>
2020-11-04 09:39:10 -0800

    4.2.0-rc.3

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -158,14 +158,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@4.2.0-rc.2";
+        return "babylonjs@4.2.0-rc.3";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "4.2.0-rc.2";
+        return "4.2.0-rc.3";
     }
 
     /**
