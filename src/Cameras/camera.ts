d3e45141b57cfe01ad21500ef9893e7861a7db71
Popov72 <github@evpopov.com>
2020-11-05 11:04:13 +0100

    Fix parsing the camera upVector from serialization

--- src/Cameras/camera.ts
+++ src/Cameras/camera.ts
@@ -1303,6 +1303,8 @@ export class Camera extends Node {
             camera._setupInputs();
         }
 
+        camera.upVector = Vector3.FromArray(parsedCamera.upVector); // need to force the upVector
+
         if ((<any>camera).setPosition) { // need to force position
             camera.position.copyFromFloats(0, 0, 0);
             (<any>camera).setPosition(Vector3.FromArray(parsedCamera.position));
