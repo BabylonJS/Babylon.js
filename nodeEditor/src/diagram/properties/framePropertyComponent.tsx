7ebbe273899da912043213404161b0e0f8a4168d
Pamela Wolf <pamela.wolf@digipen.edu>
2020-11-20 10:38:30 -0800

    exposing input blocks to closed frames

--- nodeEditor/src/diagram/properties/framePropertyComponent.tsx
+++ nodeEditor/src/diagram/properties/framePropertyComponent.tsx
@@ -8,6 +8,14 @@ import { TextInputLineComponent } from '../../sharedComponents/textInputLineComp
 import { ButtonLineComponent } from '../../sharedComponents/buttonLineComponent';
 import { Nullable } from 'babylonjs/types';
 import { Observer } from 'babylonjs/Misc/observable';
+import { InputBlock, NodeMaterialBlockConnectionPointTypes } from 'babylonjs';
+import { CheckBoxLineComponent } from '../../sharedComponents/checkBoxLineComponent';
+import { FloatLineComponent } from '../../sharedComponents/floatLineComponent';
+import { SliderLineComponent } from '../../sharedComponents/sliderLineComponent';
+import { Vector3LineComponent } from '../../sharedComponents/vector3LineComponent';
+import { Vector4LineComponent } from '../../sharedComponents/vector4LineComponent';
+import { Vector2LineComponent } from '../../sharedComponents/vector2LineComponent';
+import { Color4LineComponent } from '../../sharedComponents/color4LineComponent';
 
 export interface IFramePropertyTabComponentProps {
     globalState: GlobalState
@@ -31,7 +39,116 @@ export class FramePropertyTabComponent extends React.Component<IFramePropertyTab
             this.props.frame.onExpandStateChanged.remove(this.onFrameExpandStateChangedObserver);
             this.onFrameExpandStateChangedObserver = null;
         }
-    }    
+    }
+    
+    renderInputBlock(block: InputBlock) {
+        switch (block.type) {
+            case NodeMaterialBlockConnectionPointTypes.Float:
+                    let cantDisplaySlider = (isNaN(block.min) || isNaN(block.max) || block.min === block.max);
+                    return (
+                        <div key={block.name}>                            
+                            {
+                                block.isBoolean &&
+                                <CheckBoxLineComponent key={block.name} label={block.name} target={block} propertyName="value" onValueChanged={() => {
+                                    this.props.globalState.onUpdateRequiredObservable.notifyObservers();}} />
+                            }
+                            {
+                                !block.isBoolean && cantDisplaySlider &&
+                                <FloatLineComponent key={block.name} label={block.name} target={block} propertyName="value" 
+                                    globalState={this.props.globalState} onChange={() => {
+                                        this.props.globalState.onUpdateRequiredObservable.notifyObservers();}}
+                                />
+                            }        
+                            {
+                                !block.isBoolean && !cantDisplaySlider &&
+                                <SliderLineComponent globalState={this.props.globalState} key={block.name} 
+                                label={block.name} target={block} propertyName="value" step={(block.max - block.min) / 100.0} minimum={block.min} maximum={block.max}
+                                onChange={() => {this.props.globalState.onUpdateRequiredObservable.notifyObservers();}}/>
+                            }
+                        </div>
+                    );  
+            case NodeMaterialBlockConnectionPointTypes.Color3:
+                return (
+                    <Color3LineComponent key={block.name} label={block.name} target={block} propertyName="value" 
+                    globalState={this.props.globalState} onChange={() => {
+                        this.props.globalState.onUpdateRequiredObservable.notifyObservers();}}/>
+                )     
+            case NodeMaterialBlockConnectionPointTypes.Color4:
+                return (
+                    <Color4LineComponent key={block.name} label={block.name} target={block} propertyName="value" 
+                    globalState={this.props.globalState} onChange={() => {
+                        this.props.globalState.onUpdateRequiredObservable.notifyObservers();}}/>
+                )                         
+            case NodeMaterialBlockConnectionPointTypes.Vector2:
+                return (
+                        <Vector2LineComponent key={block.name} label={block.name} target={block} propertyName="value" 
+                        globalState={this.props.globalState}onChange={() => {
+                            this.props.globalState.onUpdateRequiredObservable.notifyObservers();}}/>
+                    )                                
+            case NodeMaterialBlockConnectionPointTypes.Vector3:
+                return (
+                    <Vector3LineComponent key={block.name} label={block.name} target={block} propertyName="value" 
+                    globalState={this.props.globalState} onChange={() => {
+                        this.props.globalState.onUpdateRequiredObservable.notifyObservers();}} />
+                )
+            case NodeMaterialBlockConnectionPointTypes.Vector4:
+                return (
+                    <Vector4LineComponent key={block.name} label={block.name} target={block} propertyName="value" 
+                    globalState={this.props.globalState} onChange={() => {
+                        this.props.globalState.onUpdateRequiredObservable.notifyObservers();}}/>
+                )
+            }
+        return null;
+    }
+    
+    renderInputValues() {
+        let configurableInputBlocks = this.props.frame.nodes.filter(node => {
+            return node.block.isInput && node.block.visibleOnFrame
+        }).sort( (a, b) => {
+            return a.name.localeCompare(b.name);
+        });
+
+        let namedGroups: string[] = [];
+        configurableInputBlocks.forEach(block => {
+            if (!(block.block as InputBlock).groupInInspector) {
+                return;
+            }
+
+            if (namedGroups.indexOf((block.block as InputBlock).groupInInspector) === -1) {
+                namedGroups.push((block.block as InputBlock).groupInInspector);
+            }
+        });
+        namedGroups.sort();
+        
+
+        let inputBlockContainer = configurableInputBlocks.length > 0 ?
+            <LineContainerComponent title="INPUTS"> {
+                configurableInputBlocks.filter(block => !(block.block as InputBlock).groupInInspector).map(block => {
+                    return this.renderInputBlock(block.block as InputBlock);
+                })
+            }
+            </LineContainerComponent> : null;
+
+        return (           
+            <>
+                {inputBlockContainer}
+                {
+                    namedGroups.map((name, i) => {
+                        return (
+                            <LineContainerComponent key={"inputValue" + i} title={name.toUpperCase()}>
+                            {
+                                configurableInputBlocks.filter(block => (block.block as InputBlock).groupInInspector === name).map(block => {
+                                    return this.renderInputBlock(block.block as InputBlock);
+                                })
+                            }
+                            </LineContainerComponent>
+                        )
+                    })   
+                }
+          </>
+        );
+    }
+
 
     render() {      
         return (
@@ -63,6 +180,7 @@ export class FramePropertyTabComponent extends React.Component<IFramePropertyTab
                                 this.props.frame!.export();
                             }} />
                 </LineContainerComponent>
+                {this.renderInputValues()}
             </div>
         </div>
         );
