fcbd17c4bca9525aaa9f8130e774a6b1942b2a27
sebavan <vandenberghe.sebastien@gmail.com>
2020-11-24 04:50:56 +0100

    Add more config
    Former-commit-id: aa2fb7edf20731c56239c5c5ca0f944323ba30ae

--- Tools/Gulp/tasks/gulpTasks-symlink.js
+++ Tools/Gulp/tasks/gulpTasks-symlink.js
@@ -3,17 +3,50 @@ var gulp = require("gulp");
 var symlinkDir = require('symlink-dir')
 var path = require('path')
 
+// Import Build Config
+var config = require("../../Config/config.js");
+
 /**
  * Generate our required symlinked for the shared components.
  */
-gulp.task("generate-symlinks", function(done) {
-    var sharedUiComponents = path.resolve(__dirname, "../../../sharedUiComponents/");
-    var inspectorSharedUiComponents = path.resolve(__dirname, "../../../inspector/src/sharedUiComponents/");
-    var nodeEditorSharedUiComponents = path.resolve(__dirname, "../../../nodeEditor/src/sharedUiComponents/");
+var generateSharedUiComponents = function(settings, done) {
+    if (!settings.build.sharedUiComponents) {
+        done();
+        return;
+    }
 
-    symlinkDir(sharedUiComponents, inspectorSharedUiComponents).then(() => {
-        symlinkDir(sharedUiComponents, nodeEditorSharedUiComponents).then(() => {
-             done();
-        });
+    var sharedUiComponents = config.computed.sharedUiComponentsSrcPath;
+    var umdSharedUiComponents = path.resolve(settings.computed.mainDirectory, settings.build.sharedUiComponents);
+
+    symlinkDir(sharedUiComponents, umdSharedUiComponents).then(() => {
+        done();
     });
+};
+
+/**
+ * Dynamic build SymLinks.
+ */
+function buildSymLinks(settings) {
+    // Creates the required tasks.
+    var tasks = [];
+
+    var sharedUiComponents = function(cb) { return generateSharedUiComponents(settings, cb); };
+
+    tasks.push(sharedUiComponents);
+
+    return gulp.series.apply(this, tasks);
+}
+
+/**
+ * Dynamic symlinks creation.
+ */
+config.modules.map(function(module) {
+    const settings = config[module];
+    gulp.task(module + "-symlinks", buildSymLinks(settings));
 });
+
+/**
+ * Build all es 6 libs.
+ */
+gulp.task("generate-symlinks", gulp.series(config.modules.map((module) => module + "-symlinks")));
+
