29ee20908254fda59bf0a03409b80b23f895610c
Cedric Guillemet <ceguille@microsoft.com>
2020-11-24 11:11:45 +0100

    less choppy movement for navigation agents
    Former-commit-id: 89c57a9e7155087c53ce64478707737d6640a498

--- src/Navigation/Plugins/recastJSPlugin.ts
+++ src/Navigation/Plugins/recastJSPlugin.ts
@@ -640,15 +640,16 @@ export class RecastJSCrowd implements ICrowd {
         if (timeStep <= Epsilon) {
             this.recastCrowd.update(deltaTime);
         } else {
-            var iterationCount = deltaTime / timeStep;
+            var iterationCount = Math.floor(deltaTime / timeStep);
             if (maxStepCount && iterationCount > maxStepCount) {
                 iterationCount = maxStepCount;
             }
             if (iterationCount < 1) {
                 iterationCount = 1;
             }
+
             for (let i = 0; i < iterationCount; i++) {
-                this.recastCrowd.update(timeStep);
+                this.recastCrowd.update(deltaTime/iterationCount);
             }
         }
 
