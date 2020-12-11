707640c4e1f7a15fd9bf8569eede4c24d0618d63
David Catuhe <david.catuhe@live.fr>
2020-12-11 09:11:44 -0800

    Let user change block's target when they are neutrals

--- nodeEditor/src/diagram/properties/genericNodePropertyComponent.tsx
+++ nodeEditor/src/diagram/properties/genericNodePropertyComponent.tsx
@@ -11,6 +11,7 @@ import { Vector2LineComponent } from '../../sharedComponents/vector2LineComponen
 import { OptionsLineComponent } from '../../sharedComponents/optionsLineComponent';
 import { InputBlock } from 'babylonjs/Materials/Node/Blocks/Input/inputBlock';
 import { PropertyTypeForEdition, IPropertyDescriptionForEdition, IEditablePropertyListOption } from 'babylonjs/Materials/Node/nodeMaterialDecorator';
+import { NodeMaterialBlockTargets } from "babylonjs";
 
 export class GenericPropertyComponent extends React.Component<IPropertyComponentProps> {
     constructor(props: IPropertyComponentProps) {
@@ -33,6 +34,13 @@ export class GeneralPropertyTabComponent extends React.Component<IPropertyCompon
     }
 
     render() {
+
+        var targetOptions = [
+            { label: "Neutral", value: NodeMaterialBlockTargets.Neutral },
+            { label: "Vertex", value: NodeMaterialBlockTargets.Vertex },
+            { label: "Fragment", value: NodeMaterialBlockTargets.Fragment },
+        ];
+
         return (
             <>
                 <LineContainerComponent title="GENERAL">
@@ -47,6 +55,20 @@ export class GeneralPropertyTabComponent extends React.Component<IPropertyCompon
                             }
                             return true;
                             }} />
+                    }
+                    {
+                        (this.props.block.target === NodeMaterialBlockTargets.Neutral || (this.props.block as any)._modifiedTarget) &&
+                        <OptionsLineComponent label="Target" options={targetOptions} target={this.props.block} propertyName="target" onSelect={(value: any) => {
+                            (this.props.block as any)._modifiedTarget = true;
+                            this.forceUpdate();
+
+                            this.props.globalState.onUpdateRequiredObservable.notifyObservers();
+                            this.props.globalState.onRebuildRequiredObservable.notifyObservers();
+                        }} />
+                    }
+                    {
+                        (this.props.block.target !== NodeMaterialBlockTargets.Neutral && !(this.props.block as any)._modifiedTarget) &&
+                        <TextLineComponent label="Type" value={NodeMaterialBlockTargets[this.props.block.target]} />
                     }
                     <TextLineComponent label="Type" value={this.props.block.getClassName()} />
                     <TextInputLineComponent globalState={this.props.globalState} label="Comments" propertyName="comments" target={this.props.block}
