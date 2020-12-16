31175d45e5f1f383b100866ec5421b83f07b76f2
msDestiny14 <pawolf@microsoft.com>
2020-12-16 11:02:41 -0500

    removing UI files
    Former-commit-id: 4f06e54a6f4a44b446ac53b1e2b058484e89b92d

--- guiEditor/src/diagram/propertyLedger.ts
+++ guiEditor/src/diagram/propertyLedger.ts
@@ -1,19 +1,10 @@
 import { ComponentClass } from 'react';
 
 import { IPropertyComponentProps } from './properties/propertyComponentProps';
-import { ButtonPropertyTabComponent } from './properties/buttonGuiPropertyComponent copy';
 import { SliderPropertyTabComponent } from './properties/sliderGuiPropertyComponent';
-import { CheckboxPropertyTabComponent } from './properties/checkboxGuiPropertyComponent';
-import { ShapePropertyTabComponent } from './properties/shapeGuiPropertyComponent';
-import { LinePropertyTabComponent } from './properties/lineGuiPropertyComponent';
 
 export class PropertyGuiLedger {
     public static RegisteredControls: {[key: string] : ComponentClass<IPropertyComponentProps>} = {};
 }
 
-PropertyGuiLedger.RegisteredControls["Button"] = ButtonPropertyTabComponent;
 PropertyGuiLedger.RegisteredControls["Slider"] = SliderPropertyTabComponent;
-PropertyGuiLedger.RegisteredControls["Checkbox"] = CheckboxPropertyTabComponent;
-PropertyGuiLedger.RegisteredControls["Rectangle"] = ShapePropertyTabComponent;
-PropertyGuiLedger.RegisteredControls["Ellipse"] = ShapePropertyTabComponent;
-PropertyGuiLedger.RegisteredControls["Line"] = LinePropertyTabComponent;
