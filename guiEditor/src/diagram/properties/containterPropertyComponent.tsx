0678fd595725acfdedfa9a4e3a3349b64312fb22
msDestiny14 <pawolf@microsoft.com>
2020-12-31 12:17:54 -0500

    parenting system beginning work

---
+++ guiEditor/src/diagram/properties/containterPropertyComponent.tsx
@@ -0,0 +1,48 @@
+
+import { Container } from "babylonjs-gui";
+import * as React from "react";
+import { LineContainerComponent } from '../../sharedComponents/lineContainerComponent';
+import { ButtonLineComponent } from "../../sharedUiComponents/lines/buttonLineComponent";
+import { TextLineComponent } from "../../sharedUiComponents/lines/textLineComponent";
+import { IContainerComponentProps} from './propertyComponentProps';
+
+export class ContainerPropertyTabComponent extends React.Component<IContainerComponentProps> {
+    constructor(props: IContainerComponentProps) {
+        super(props);
+    }
+
+    AddElement()
+    {
+        let newElementName = window.prompt("Please enter name of the element");
+
+        if (!newElementName) {
+            return;
+        }
+
+        var childGuiNode = this.props.globalState.workbench.findNodeFromGuiElementName(newElementName);
+        this.props.guiNode.addGui(childGuiNode);
+    }
+
+    
+    render() {
+
+        var guiElementMenu: JSX.Element[] = [];
+        var guiNode = this.props.guiNode.guiNode as Container;
+        guiNode.children.forEach(child => {
+            guiElementMenu.push(<TextLineComponent label={child.name}></TextLineComponent>);           
+        });
+
+        
+
+        return (
+            <>                
+                <LineContainerComponent title="CONTAINER PROPERTIES"> 
+                {guiElementMenu}               
+                <ButtonLineComponent label="Add element" onClick={() => {
+                    this.AddElement();
+                }} />
+                </LineContainerComponent>            
+            </>
+        );
+    }
+}
