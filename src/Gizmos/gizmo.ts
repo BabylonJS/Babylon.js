5702f374e7937bcd618f782939431b3e10bff170
Jason Sturges <jason@jasonsturges.com>
2021-01-01 18:25:28 -0600

    Gizmos documentation

--- src/Gizmos/gizmo.ts
+++ src/Gizmos/gizmo.ts
@@ -19,15 +19,15 @@ import { LinesMesh } from '../Meshes/linesMesh';
  * Cache built by each axis. Used for managing state between all elements of gizmo for enhanced UI
  */
 export interface GizmoAxisCache {
-    /** Mesh used to runder the Gizmo */
+    /** Mesh used to render the Gizmo */
     gizmoMeshes: Mesh[];
     /** Mesh used to detect user interaction with Gizmo */
     colliderMeshes: Mesh[];
-    /** Material used to inicate color of gizmo mesh */
+    /** Material used to indicate color of gizmo mesh */
     material: StandardMaterial;
-    /** Material used to inicate hover state of the Gizmo */
+    /** Material used to indicate hover state of the Gizmo */
     hoverMaterial: StandardMaterial;
-    /** Material used to inicate disabled state of the Gizmo */
+    /** Material used to indicate disabled state of the Gizmo */
     disableMaterial: StandardMaterial;
     /** Used to indicate Active state of the Gizmo */
     active: boolean;
