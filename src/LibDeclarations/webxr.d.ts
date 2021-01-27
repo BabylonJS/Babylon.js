53601ce29e4d361d82f133c1ece497fa254d8c67
Raanan Weber <info@raananweber.com>
2021-01-27 16:16:14 +0100

    initial Hand-API update

--- src/LibDeclarations/webxr.d.ts
+++ src/LibDeclarations/webxr.d.ts
@@ -187,7 +187,7 @@ interface XRSession {
      * canceling the callback using cancelAnimationFrame(). This method is comparable
      * to the Window.requestAnimationFrame() method.
      */
-    requestAnimationFrame: XRFrameRequestCallback;
+    requestAnimationFrame: (callback: XRFrameRequestCallback) => void;
     /**
      * Requests that a new XRReferenceSpace of the specified type be created.
      * Returns a promise which resolves with the XRReferenceSpace or
@@ -315,10 +315,45 @@ interface XRJointPose extends XRPose {
     radius: number | undefined;
 }
 
+enum XRHandJoint {
+    "wrist" = "wrist",
+
+    "thumb-metacarpal" = "thumb-metacarpal",
+    "thumb-phalanx-proximal" = "thumb-phalanx-proximal",
+    "thumb-phalanx-distal" = "thumb-phalanx-distal",
+    "thumb-tip" = "thumb-tip",
+
+    "index-finger-metacarpal" = "index-finger-metacarpal",
+    "index-finger-phalanx-proximal" = "index-finger-phalanx-proximal",
+    "index-finger-phalanx-intermediate" = "index-finger-phalanx-intermediate",
+    "index-finger-phalanx-distal" = "index-finger-phalanx-distal",
+    "index-finger-tip" = "index-finger-tip",
+
+    "middle-finger-metacarpal" = "middle-finger-metacarpal",
+    "middle-finger-phalanx-proximal" = "middle-finger-phalanx-proximal",
+    "middle-finger-phalanx-intermediate" = "middle-finger-phalanx-intermediate",
+    "middle-finger-phalanx-distal" = "middle-finger-phalanx-distal",
+    "middle-finger-tip" = "middle-finger-tip",
+
+    "ring-finger-metacarpal" = "ring-finger-metacarpal",
+    "ring-finger-phalanx-proximal" = "ring-finger-phalanx-proximal",
+    "ring-finger-phalanx-intermediate" = "ring-finger-phalanx-intermediate",
+    "ring-finger-phalanx-distal" = "ring-finger-phalanx-distal",
+    "ring-finger-tip" = "ring-finger-tip",
+
+    "pinky-finger-metacarpal" = "pinky-finger-metacarpal",
+    "pinky-finger-phalanx-proximal" = "pinky-finger-phalanx-proximal",
+    "pinky-finger-phalanx-intermediate" = "pinky-finger-phalanx-intermediate",
+    "pinky-finger-phalanx-distal" = "pinky-finger-phalanx-distal",
+    "pinky-finger-tip" = "pinky-finger-tip",
+}
+
 interface XRHand extends Iterable<XRJointSpace> {
-    readonly length: number;
+    readonly size: number;
 
     [index: number]: XRJointSpace;
+
+    get(joint: XRHandJoint): XRJointSpace;
 
     readonly WRIST: number;
 
