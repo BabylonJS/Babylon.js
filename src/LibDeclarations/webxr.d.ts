5f78a368587f4fadf1438a33ac87ec6cca91b8ab
Raanan Weber <info@raananweber.com>
2021-01-27 16:49:24 +0100

    revert changes to webxr dec

--- src/LibDeclarations/webxr.d.ts
+++ src/LibDeclarations/webxr.d.ts
@@ -315,39 +315,6 @@ interface XRJointPose extends XRPose {
     radius: number | undefined;
 }
 
-enum XRHandJoint {
-    "wrist" = "wrist",
-
-    "thumb-metacarpal" = "thumb-metacarpal",
-    "thumb-phalanx-proximal" = "thumb-phalanx-proximal",
-    "thumb-phalanx-distal" = "thumb-phalanx-distal",
-    "thumb-tip" = "thumb-tip",
-
-    "index-finger-metacarpal" = "index-finger-metacarpal",
-    "index-finger-phalanx-proximal" = "index-finger-phalanx-proximal",
-    "index-finger-phalanx-intermediate" = "index-finger-phalanx-intermediate",
-    "index-finger-phalanx-distal" = "index-finger-phalanx-distal",
-    "index-finger-tip" = "index-finger-tip",
-
-    "middle-finger-metacarpal" = "middle-finger-metacarpal",
-    "middle-finger-phalanx-proximal" = "middle-finger-phalanx-proximal",
-    "middle-finger-phalanx-intermediate" = "middle-finger-phalanx-intermediate",
-    "middle-finger-phalanx-distal" = "middle-finger-phalanx-distal",
-    "middle-finger-tip" = "middle-finger-tip",
-
-    "ring-finger-metacarpal" = "ring-finger-metacarpal",
-    "ring-finger-phalanx-proximal" = "ring-finger-phalanx-proximal",
-    "ring-finger-phalanx-intermediate" = "ring-finger-phalanx-intermediate",
-    "ring-finger-phalanx-distal" = "ring-finger-phalanx-distal",
-    "ring-finger-tip" = "ring-finger-tip",
-
-    "pinky-finger-metacarpal" = "pinky-finger-metacarpal",
-    "pinky-finger-phalanx-proximal" = "pinky-finger-phalanx-proximal",
-    "pinky-finger-phalanx-intermediate" = "pinky-finger-phalanx-intermediate",
-    "pinky-finger-phalanx-distal" = "pinky-finger-phalanx-distal",
-    "pinky-finger-tip" = "pinky-finger-tip",
-}
-
 interface XRHand extends Iterable<XRJointSpace> {
     readonly size: number;
 
