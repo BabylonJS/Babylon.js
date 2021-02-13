b9fec2a6013bbc0172c150e2f5cfe4ed294d64de
David Catuhe <david.catuhe@live.fr>
2021-02-13 14:15:12 -0800

    Moving keys!
    Former-commit-id: 26d023b13204d212105a6d6593ab6a106c7635c4

--- inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/graph/animationCurveEditorKeyPoint.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/graph/animationCurveEditorKeyPoint.tsx
@@ -12,10 +12,14 @@ interface IAnimationCurveEditorKeyPointComponentProps {
     scale: number;
     context: AnimationCurveEditorContext;
     channel: string;
+    onFrameValueChanged: (value: number) => void;
+    onKeyValueChanged: (value: number) => void;
 }
 
 interface IAnimationCurveEditorKeyPointComponentState {
-    isSelected: boolean;
+    isSelected: boolean;    
+    x: number;
+    y: number;
 }
 
 export class AnimationCurveEditorKeyPointComponent extends React.Component<
@@ -23,11 +27,14 @@ IAnimationCurveEditorKeyPointComponentProps,
 IAnimationCurveEditorKeyPointComponentState
 > {    
     private _onActiveKeyPointChangedObserver: Nullable<Observer<Nullable<{keyPoint: AnimationCurveEditorKeyPointComponent, channel: string}>>>;
+    private _pointerIsDown: boolean;
+    private _sourcePointerX: number;
+    private _sourcePointerY: number;
 
     constructor(props: IAnimationCurveEditorKeyPointComponentProps) {
         super(props);
 
-        this.state = { isSelected: false };
+        this.state = { isSelected: false, x: this.props.x, y: this.props.y };
 
         this._onActiveKeyPointChangedObserver = this.props.context.onActiveKeyPointChanged.add(data => {
             this.setState({isSelected: data?.keyPoint === this});
@@ -39,20 +46,65 @@ IAnimationCurveEditorKeyPointComponentState
             this.props.context.onActiveKeyPointChanged.remove(this._onActiveKeyPointChangedObserver);
         }
     }
+    shouldComponentUpdate(newProps: IAnimationCurveEditorKeyPointComponentProps, newState: IAnimationCurveEditorKeyPointComponentState) {
+        if (newProps.x !== this.props.x || newProps.y !== this.props.y) {
+            newState.x = newProps.x;
+            newState.y = newProps.y;
+        }
+
+        return true;
+    }
+
+    private _onPointerDown(evt: React.PointerEvent<SVGSVGElement>) {
+        this.props.context.onActiveKeyPointChanged.notifyObservers({
+            keyPoint: this,
+            channel: this.props.channel
+        });
+
+        this._pointerIsDown = true;
+        evt.currentTarget.setPointerCapture(evt.pointerId);
+        this._sourcePointerX = evt.nativeEvent.offsetX;
+        this._sourcePointerY = evt.nativeEvent.offsetY;
+
+        evt.stopPropagation();
+    }
+
+    private _onPointerMove(evt: React.PointerEvent<SVGSVGElement>) {
+        if (!this._pointerIsDown) {
+            return;
+        }
+
+        let newX = this.state.x + (evt.nativeEvent.offsetX - this._sourcePointerX) * this.props.scale;
+        let newY = this.state.y + (evt.nativeEvent.offsetY - this._sourcePointerY) * this.props.scale;
+
+        this.props.onFrameValueChanged(newX);
+        this.props.onKeyValueChanged(newY);
+              
+        this._sourcePointerX = evt.nativeEvent.offsetX;
+        this._sourcePointerY = evt.nativeEvent.offsetY;
+
+        this.setState({x: newX, y: newY});
+
+        evt.stopPropagation();
+    }
+
+    private _onPointerUp(evt: React.PointerEvent<SVGSVGElement>) {
+        this._pointerIsDown = false;
+        evt.currentTarget.releasePointerCapture(evt.pointerId);
+
+        evt.stopPropagation();
+    }
 
     public render() {
         const svgImageIcon = this.state.isSelected ? keySelected : keyInactive;
 
         return (
             <svg
-                x={this.props.x}
-                y={this.props.y}
-                onClick={() => {
-                    this.props.context.onActiveKeyPointChanged.notifyObservers({
-                        keyPoint: this,
-                        channel: this.props.channel
-                    });
-                }}
+                onPointerDown={evt => this._onPointerDown(evt)}
+                onPointerMove={evt => this._onPointerMove(evt)}
+                onPointerUp={evt => this._onPointerUp(evt)}
+                x={this.state.x}
+                y={this.state.y}
                 style={{ cursor: "pointer", overflow: "auto" }}
         >
             <image
