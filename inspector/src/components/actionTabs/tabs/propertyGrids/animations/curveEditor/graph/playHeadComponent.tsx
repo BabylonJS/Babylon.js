625c5beb9b9e390d092ddb1240968ae9c1d297c1
David Catuhe <david.catuhe@live.fr>
2021-02-16 14:37:01 -0800

    play head is alive!!
    Former-commit-id: 310bd5a238260a78de998789552dc167af456af6

--- inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/graph/playHeadComponent.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/graph/playHeadComponent.tsx
@@ -1,3 +1,6 @@
+import { Observer } from "babylonjs/Misc/observable";
+import { Scene } from "babylonjs/scene";
+import { Nullable } from "babylonjs/types";
 import * as React from "react";
 import { GlobalState } from "../../../../../../globalState";
 import { Context } from "../context";
@@ -13,30 +16,83 @@ interface IPlayHeadComponentState {
 export class PlayHeadComponent extends React.Component<
 IPlayHeadComponentProps,
 IPlayHeadComponentState
-> {
-
+> {        
+    private readonly _GraphAbsoluteWidth = 788;
+    private _playHead: React.RefObject<HTMLDivElement>;
+    private _playHeadCircle: React.RefObject<HTMLDivElement>;
+    private _playHeadArea: React.RefObject<HTMLDivElement>;
+    private _onBeforeRenderObserver: Nullable<Observer<Scene>>;
+    private _viewScale = 1;
+    private _offsetX = 0;
+    
     constructor(props: IPlayHeadComponentProps) {
         super(props);
 
         this.state = { };
+        
+        this._playHead = React.createRef();
+        this._playHeadArea = React.createRef();
+        this._playHeadCircle = React.createRef();
 
         this.props.context.onActiveAnimationChanged.add(() => {
             this.forceUpdate();
-        });        
+        });   
+        
+        this._onBeforeRenderObserver = this.props.context.scene.onBeforeRenderObservable.add(() => {
+            if (!this.props.context.activeAnimation) {
+                return;
+            }
+
+            let animation = this.props.context.activeAnimation;
+
+            if (!animation) {
+                return;
+            }
+            let runtimeAnimation = animation.runtimeAnimations[0];
+
+            if (runtimeAnimation) {
+                this._playHead.current!.style.left = this._frameToPixel(runtimeAnimation.currentFrame) + "px"
+                this._playHeadCircle.current!.innerHTML = runtimeAnimation.currentFrame.toFixed(2);
+            }
+        });
+
+        this.props.context.onGraphMoved.add(x => {
+            this._offsetX = x;
+            this.forceUpdate();
+        });
+
+        this.props.context.onGraphScaled.add(scale => {
+            this._viewScale =  1 /scale;
+            this.forceUpdate();
+        });
+    }
+
+    private _frameToPixel(frame: number) {
+        let animation = this.props.context.activeAnimation!;
+        let keys = animation.getKeys();
+        let minFrame = keys[0].frame;
+        let maxFrame = keys[keys.length - 1].frame;
+
+        return (((frame - minFrame) /  (maxFrame - minFrame)) * this._GraphAbsoluteWidth + this._offsetX) * this._viewScale;
+    }
+
+    componentWillUnmount() {
+        if (this._onBeforeRenderObserver) {
+            this.props.context.scene.onBeforeRenderObservable.remove(this._onBeforeRenderObserver);
+            this._onBeforeRenderObserver = null;
+        }
     }
 
     public render() {
-        if (this.props.context.activeAnimation === null) {
+        if (!this.props.context.activeAnimation) {
             return null;
         }
 
         return (
-            <div id="play-head-area">
-                <div id="play-head">
+            <div id="play-head-area" ref={this._playHeadArea}>
+                <div id="play-head" ref={this._playHead}>
                     <div id="play-head-bar"></div>
-                    <div id="play-head-circle">
-                        {                           
-                        }
+                    <div id="play-head-circle" ref={this._playHeadCircle}>
                     </div>
                 </div>
             </div>
