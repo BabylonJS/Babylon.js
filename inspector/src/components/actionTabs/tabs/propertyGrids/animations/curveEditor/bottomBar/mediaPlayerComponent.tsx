ebf398bcbed6f476e09c2c4d4d46266b2013d258
David Catuhe <david.catuhe@live.fr>
2021-02-18 15:14:55 -0800

    play/stop buttons

--- inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/bottomBar/mediaPlayerComponent.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/bottomBar/mediaPlayerComponent.tsx
@@ -21,6 +21,9 @@ const startKeyHoverIcon = require("../assets/animationStartHoverIcon.svg");
 const endKeyIcon = require("../assets/animationEndIcon.svg");
 const endKeyHoverIcon = require("../assets/animationEndHoverIcon.svg");
 
+const stopIcon = require("../assets/animationStopIcon.svg");
+const stopHoverIcon = require("../assets/animationStopHoverIcon.svg");
+
 interface IMediaPlayerComponentProps {
     globalState: GlobalState;
     context: Context;
@@ -38,6 +41,10 @@ IMediaPlayerComponentState
         super(props);
 
         this.state = { };
+
+        this.props.context.onAnimationStateChanged.add(() => {
+            this.forceUpdate();
+        });
     }
 
     private _onFirstKey() {
@@ -50,10 +57,12 @@ IMediaPlayerComponentState
     
     private _onRewind() {
         this.props.context.play(false);
+        this.forceUpdate();
     }
 
     private _onForward() {
         this.props.context.play(true);
+        this.forceUpdate();
     }
 
     private _onNextKey() {
@@ -64,13 +73,29 @@ IMediaPlayerComponentState
 
     }
 
+    private _onStop() {
+        this.props.context.stop();
+        this.forceUpdate();
+    }
+
     public render() {
         return (
             <div id="media-player">
                 <ControlButtonComponent id="start-key" context={this.props.context} globalState={this.props.globalState} icon={startKeyIcon}  hoverIcon={startKeyHoverIcon} onClick={() => this._onPrevKey()}/>
                 <ControlButtonComponent id="first-key" context={this.props.context} globalState={this.props.globalState} icon={firstKeyIcon} hoverIcon={firstKeyHoverIcon} onClick={() => this._onFirstKey()}/>
-                <ControlButtonComponent id="rev-key" context={this.props.context} globalState={this.props.globalState} icon={revKeyIcon} hoverIcon={revKeyHoverIcon} onClick={() => this._onRewind()}/>
-                <ControlButtonComponent id="fwd-key" context={this.props.context} globalState={this.props.globalState} icon={fwdKeyIcon} hoverIcon={fwdKeyHoverIcon} onClick={() => this._onForward()}/>
+                { (this.props.context.isPlaying && this.props.context.forwardAnimation || !this.props.context.isPlaying) && 
+                    <ControlButtonComponent id="rev-key" context={this.props.context} globalState={this.props.globalState} icon={revKeyIcon} hoverIcon={revKeyHoverIcon} onClick={() => this._onRewind()}/>
+                }
+                
+                { (this.props.context.isPlaying && !this.props.context.forwardAnimation) && 
+                    <ControlButtonComponent id="stop-key" context={this.props.context} globalState={this.props.globalState} icon={stopIcon} hoverIcon={stopHoverIcon} onClick={() => this._onStop()}/>
+                }
+                { (this.props.context.isPlaying && !this.props.context.forwardAnimation || !this.props.context.isPlaying) && 
+                    <ControlButtonComponent id="fwd-key" context={this.props.context} globalState={this.props.globalState} icon={fwdKeyIcon} hoverIcon={fwdKeyHoverIcon} onClick={() => this._onForward()}/>
+                }
+                { (this.props.context.isPlaying && this.props.context.forwardAnimation) && 
+                    <ControlButtonComponent id="stop-key" context={this.props.context} globalState={this.props.globalState} icon={stopIcon} hoverIcon={stopHoverIcon} onClick={() => this._onStop()}/>
+                }
                 <ControlButtonComponent id="next-key" context={this.props.context} globalState={this.props.globalState} icon={nextKeyIcon} hoverIcon={nextKeyHoverIcon} onClick={() => this._onNextKey()}/>
                 <ControlButtonComponent id="end-key" context={this.props.context} globalState={this.props.globalState} icon={endKeyIcon}  hoverIcon={endKeyHoverIcon} onClick={() => this._onEndKey()}/>
             </div>
