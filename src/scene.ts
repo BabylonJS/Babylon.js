8db0fb1e6010b0e1e4593d9087415767313bb827
David Catuhe <david.catuhe@live.fr>
2021-02-24 09:13:50 -0800

    ts 4.2.2
    Former-commit-id: 332eb757dd1dffab4ea6b5c66f323e39bc30a2dc

--- src/scene.ts
+++ src/scene.ts
@@ -1256,7 +1256,7 @@ export class Scene extends AbstractScene implements IAnimatable, IClipPlanesHold
         this._components.push(component);
         this._transientComponents.push(component);
 
-        const serializableComponent = component as ISceneSerializableComponent;
+        const serializableComponent = component as any;
         if (serializableComponent.addFromContainer && serializableComponent.serialize) {
             this._serializableComponents.push(serializableComponent);
         }
