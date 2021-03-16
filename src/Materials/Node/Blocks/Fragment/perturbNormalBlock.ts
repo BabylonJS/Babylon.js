9605f948ee464f9aefd950faba2e399e6cc971b1
Popov72 <github@evpopov.com>
2021-03-16 17:54:51 +0100

    linting
    Former-commit-id: 46425f28f1d99be30017a82cd6e844980c3f4938

--- src/Materials/Node/Blocks/Fragment/perturbNormalBlock.ts
+++ src/Materials/Node/Blocks/Fragment/perturbNormalBlock.ts
@@ -151,7 +151,7 @@ export class PerturbNormalBlock extends NodeMaterialBlock {
 
         state._emitUniformFromString(this._tangentSpaceParameterName, "vec2");
 
-        let replaceForBumpInfos = this.strength.isConnectedToInputBlock && this.strength.connectInputBlock!.isConstant ? 
+        let replaceForBumpInfos = this.strength.isConnectedToInputBlock && this.strength.connectInputBlock!.isConstant ?
             `\r\n#if !defined(NORMALXYSCALE)\r\n1.0/\r\n#endif\r\n${state._emitFloat(this.strength.connectInputBlock!.value)}` :
             `\r\n#if !defined(NORMALXYSCALE)\r\n1.0/\r\n#endif\r\n${this.strength.associatedVariableName}`;
 
