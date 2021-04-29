0a24a97cacfa382f0cefd19190ec7800df6200bf
Benjamin Guignabert <bguignabert@wanadev.fr>
2021-04-29 12:52:20 +0200

    interpolation OK

--- src/Behaviors/Meshes/followBehavior.ts
+++ src/Behaviors/Meshes/followBehavior.ts
@@ -7,7 +7,7 @@ import { Camera } from "../../Cameras/camera";
 import { Matrix, Quaternion, Vector3 } from "../../Maths/math.vector";
 import { Scalar } from "../../Maths/math.scalar";
 
-const EPSILON = 1e-8;
+const EPSILON = 1e-5;
 
 /**
  * A behavior that when attached to a mesh will allow the mesh to fade in and out
@@ -21,9 +21,16 @@ export class FollowBehavior implements Behavior<AbstractMesh> {
 
     public attachedNode: Nullable<AbstractMesh>;
     public followDistance = 5;
+    public lerpTime = 100;
 
     private _followedCamera: Nullable<Camera>;
     private _onFollowedCameraMatrixChanged: Nullable<Observer<Camera>>;
+    private _onBeforeRender: Nullable<Observer<Scene>>;
+
+    private _workingPosition: Vector3 = new Vector3();
+    private _workingPivot: Vector3 = new Vector3();
+    private _workingQuaternion: Quaternion = new Quaternion();
+    private _lastTick: number = -1;
 
     public get followedCamera(): Nullable<Camera> {
         return this._followedCamera;
@@ -214,39 +221,129 @@ export class FollowBehavior implements Behavior<AbstractMesh> {
         }
         Vector3.CrossToRef(right, toFollowed, up);
         Quaternion.FromLookDirectionLHToRef(toFollowed, up, rotationQuaternion);
+        rotationQuaternion.normalize();
+    }
+
+    private _vectorSlerpToRef(vector1: Vector3, vector2: Vector3, slerp: number, result: Vector3) {
+        slerp = Scalar.Clamp(slerp, 0, 1);
+        const vector1Dir = this._tmpVectors[0];
+        const vector2Dir = this._tmpVectors[1];
+        let vector1Length;
+        let vector2Length;
+
+        vector1Dir.copyFrom(vector1);
+        vector1Length = vector1Dir.length();
+        vector1Dir.normalizeFromLength(vector1Length);
+
+        vector2Dir.copyFrom(vector2);
+        vector2Length = vector2Dir.length();
+        vector2Dir.normalizeFromLength(vector2Length);
+
+        const dot = Vector3.Dot(vector1Dir, vector2Dir);
+
+        let scale1;
+        let scale2;
+
+        if (Math.abs(dot) < 1 - EPSILON) {
+            const omega = Math.acos(dot);
+            const invSin = 1 / Math.sin(omega);
+            scale1 = Math.sin((1 - slerp) * omega) * invSin;
+            scale2 = Math.sin(slerp * omega) * invSin;
+        } else {
+            // Use linear interpolation
+            scale1 = 1 - slerp;
+            scale2 = slerp;
+        }
+
+        vector1Dir.scaleInPlace(scale1);
+        vector2Dir.scaleInPlace(scale2);
+        result.copyFrom(vector1Dir).addInPlace(vector2Dir);
+        result.scaleInPlace(Scalar.Lerp(vector1Length, vector2Length, slerp));
+    }
+
+    private _vectorSmoothToRef(source: Vector3, goal: Vector3, deltaTime: number, lerpTime: number, result: Vector3) {
+        return this._vectorSlerpToRef(source, goal, lerpTime === 0 ? 1 : deltaTime / lerpTime, result);
+    }
+
+    private _quaternionSmoothToRef(source: Quaternion, goal: Quaternion, deltaTime: number, lerpTime: number, result: Quaternion) {
+        let slerp = lerpTime === 0 ? 1 : deltaTime / lerpTime;
+        slerp = Scalar.Clamp(slerp, 0, 1);
+
+        return Quaternion.SlerpToRef(source, goal, slerp, result);
+    }
+
+    private _updateLeashing(camera: Camera) {
+        if (this.attachedNode) {
+            let oldParent = this.attachedNode.parent;
+            this.attachedNode.setParent(null);
+
+            const worldMatrix = this.attachedNode.computeWorldMatrix(true);
+            const currentToTarget = this._tmpVector;
+            const rotationQuaternion = this._workingQuaternion;
+            const pivot = this.attachedNode.getPivotPoint();
+            this._workingPivot.copyFrom(pivot);
+            Vector3.TransformCoordinatesToRef(pivot, worldMatrix, currentToTarget);
+            currentToTarget.subtractInPlace(camera.globalPosition);
+
+            this._angularClamp(this.attachedNode, camera, currentToTarget);
+            this._distanceClamp(this.attachedNode, camera, currentToTarget);
+            this._orientationClamp(this.attachedNode, camera, currentToTarget, rotationQuaternion);
+
+            this._workingPosition.copyFrom(currentToTarget).subtractInPlace(pivot);
+
+            this.attachedNode.setParent(oldParent);
+            this.attachedNode.computeWorldMatrix(true);
+        }
+    }
+
+    private _updateTransformToGoal(elapsed: number) {
+        if (!this.attachedNode || !this.followedCamera) {
+            return;
+        }
+
+        if (!this.attachedNode.rotationQuaternion) {
+            this.attachedNode.rotationQuaternion = Quaternion.Identity();
+        }
+
+        const oldParent = this.attachedNode.parent;
+        this.attachedNode.setParent(null);
+
+        // position
+        const currentDirection = new Vector3();
+        currentDirection.copyFrom(this.attachedNode.position).subtractInPlace(this.followedCamera.globalPosition);
+        this._vectorSmoothToRef(currentDirection, this._workingPosition, elapsed, this.lerpTime, currentDirection);
+        currentDirection.addInPlace(this.followedCamera.globalPosition);
+        this.attachedNode.position.copyFrom(currentDirection);
+
+        // rotation
+        const currentRotation = new Quaternion();
+        currentRotation.copyFrom(this.attachedNode.rotationQuaternion);
+        this._quaternionSmoothToRef(currentRotation, this._workingQuaternion, elapsed, this.lerpTime, this.attachedNode.rotationQuaternion);
+        this.attachedNode.setParent(oldParent);
     }
 
     private _addObservables(followedCamera: Camera) {
-        this._onFollowedCameraMatrixChanged = followedCamera.onViewMatrixChangedObservable.add((camera: Camera) => {
-            if (this.attachedNode) {
-                let oldParent = this.attachedNode.parent;
-                this.attachedNode.setParent(null);
-                const worldMatrix = this.attachedNode.computeWorldMatrix(true);
-                const currentToTarget = this._tmpVector;
-                const rotationQuaternion = this.attachedNode.rotationQuaternion || Quaternion.Identity();
-                const pivot = this.attachedNode.getPivotPoint();
-                Vector3.TransformCoordinatesToRef(pivot, worldMatrix, currentToTarget);
-                currentToTarget.subtractInPlace(camera.globalPosition);
-
-                this._angularClamp(this.attachedNode, camera, currentToTarget);
-                this._distanceClamp(this.attachedNode, camera, currentToTarget);
-                this._orientationClamp(this.attachedNode, camera, currentToTarget, rotationQuaternion);
-                rotationQuaternion.normalize();
-                if (!this.attachedNode.rotationQuaternion) {
-                    this.attachedNode.rotationQuaternion = rotationQuaternion;
-                }
-
-                this.attachedNode.position.copyFrom(camera.globalPosition).addInPlace(currentToTarget).subtractInPlace(pivot);
-
-                this.attachedNode.setParent(oldParent);
-                this.attachedNode.computeWorldMatrix(true);
+        this._lastTick = Date.now();
+        // this._onFollowedCameraMatrixChanged = followedCamera.onViewMatrixChangedObservable.add((camera: Camera) => this._updateLeashing(camera));
+        this._onBeforeRender = this._scene.onBeforeRenderObservable.add(() => {
+            if (!this.followedCamera) {
+                return;
             }
+
+            const tick = Date.now();
+            this._updateLeashing(this.followedCamera);
+            this._updateTransformToGoal(tick - this._lastTick);
+            this._lastTick = tick;
         });
     }
 
     private _removeObservables(followedCamera: Camera) {
         if (this._onFollowedCameraMatrixChanged) {
             followedCamera.onViewMatrixChangedObservable.remove(this._onFollowedCameraMatrixChanged);
+        }
+
+        if (this._onBeforeRender) {
+            this._scene.onBeforeRenderObservable.remove(this._onBeforeRender);
         }
     }
 }
