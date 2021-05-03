ac663165ed924d22bc0db22718eab1d50d3cc257
Pamela Wolf <pamela.wolf@digipen.edu>
2021-05-03 08:25:27 -0700

    scene explorer

--- guiEditor/src/components/sceneExplorer/entities/gui/controlTreeItemComponent.tsx
+++ guiEditor/src/components/sceneExplorer/entities/gui/controlTreeItemComponent.tsx
@@ -1,10 +1,8 @@
-import { faObjectGroup, faHighlighter, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { IExplorerExtensibilityGroup } from "babylonjs/Debug/debugLayer";
 import { Control } from "babylonjs-gui/2D/controls/control";
 import { TreeItemLabelComponent } from "../../treeItemLabelComponent";
 import { ExtensionsComponent } from "../../extensionsComponent";
 import * as React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface IControlTreeItemComponentProps {
     control: Control;
@@ -12,12 +10,12 @@ interface IControlTreeItemComponentProps {
     onClick: () => void;
 }
 
-export class ControlTreeItemComponent extends React.Component<IControlTreeItemComponentProps, { isActive: boolean, isVisible: boolean }> {
+export class ControlTreeItemComponent extends React.Component<IControlTreeItemComponentProps, { isActive: boolean, isVisible: boolean, isHovered: boolean}> {
     constructor(props: IControlTreeItemComponentProps) {
         super(props);
 
         const control = this.props.control;
-        this.state = { isActive: control.isHighlighted, isVisible: control.isVisible };
+        this.state = { isActive: control.isHighlighted, isVisible: control.isVisible, isHovered: false };
     }
 
     highlight() {
@@ -31,25 +29,36 @@ export class ControlTreeItemComponent extends React.Component<IControlTreeItemCo
         const newState = !this.state.isVisible;
         this.setState({ isVisible: newState });
         this.props.control.isVisible = newState;
+
     }
 
     render() {
         const control = this.props.control;
         const name = (control.name || "No name") + ` [${control.getClassName()}]`;
-        const isActiveElement = this.state.isActive ? <FontAwesomeIcon icon={faHighlighter} /> : <FontAwesomeIcon icon={faHighlighter} className="isNotActive" />;
-        const visibilityElement = this.state.isVisible ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} className="isNotActive" />;
+        //const isActiveElement = this.state.isActive ? <FontAwesomeIcon icon={faHighlighter} /> : <FontAwesomeIcon icon={faHighlighter} className="isNotActive" />;
+        //const visibilityElement = this.state.isVisible ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} className="isNotActive" />;
 
-        return (
-            <div className="controlTools">
-                <TreeItemLabelComponent label={name} onClick={() => this.props.onClick()} icon={faObjectGroup} color="greenyellow" />
-                <div className="highlight icon" onClick={() => this.highlight()} title="Highlight this control">
-                    {isActiveElement}
+        if (true) {
+            return (
+                <div className="controlTools" onMouseOut={ () =>  this.setState({ isHovered: false})}>
+                    <TreeItemLabelComponent label={name} onClick={() => this.props.onClick()} color="greenyellow" />
+                    <div className="addComponent icon" onClick={() => this.highlight()} title="Add component (Not Implemented)">
+                        <img src={"../imgs/makeComponentIcon.svg"} color="black" className=""/>
+                    </div>
+                    <div className="visibility icon" onClick={() => this.switchVisibility()} title="Show/Hide control">
+                        <img src={"../imgs/visibilityIcon.svg"} color="black" className=""/>
+                    </div>
+                    <ExtensionsComponent target={control} extensibilityGroups={this.props.extensibilityGroups} />
                 </div>
-                <div className="visibility icon" onClick={() => this.switchVisibility()} title="Show/Hide control">
-                    {visibilityElement}
+            );
+        }
+        else {
+            return (
+                <div className="controlTools" onMouseEnter={ () =>  this.setState({ isHovered: true})}>
+                    <TreeItemLabelComponent label={name} onClick={() => this.props.onClick()} color="greenyellow" />
+                    <ExtensionsComponent target={control} extensibilityGroups={this.props.extensibilityGroups} />
                 </div>
-                <ExtensionsComponent target={control} extensibilityGroups={this.props.extensibilityGroups} />
-            </div>
-        );
+            );
+        }
     }
 }
