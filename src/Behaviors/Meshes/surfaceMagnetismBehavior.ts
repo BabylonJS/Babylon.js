a2e97774238cc5328957f7ea234f1c8e6780e9e8
Benjamin Guignabert <bguignabert@wanadev.fr>
2021-05-11 12:53:00 +0200

    no interpolation position-only snap

--- src/Behaviors/Meshes/surfaceMagnetismBehavior.ts
+++ src/Behaviors/Meshes/surfaceMagnetismBehavior.ts
@@ -1,6 +1,11 @@
-import { Ray } from "../../Culling";
+import { PickingInfo } from "../../Collisions";
+import { PointerEventTypes, PointerInfo } from "../../Events/pointerEvents";
 import { Matrix, TmpVectors, Vector3 } from "../../Maths/math.vector";
-import { Mesh } from "../../Meshes";
+import { AbstractMesh } from "../../Meshes";
+import { Mesh } from "../../Meshes/mesh";
+import { Observer } from "../../Misc/observable";
+import { UtilityLayerRenderer } from "../../Rendering";
+import { Scene } from "../../scene";
 import { Nullable } from "../../types";
 import { Behavior } from "../behavior";
 
@@ -8,13 +13,16 @@ import { Behavior } from "../behavior";
  * A behavior that allows a transform node to stick to a surface position/orientation
  */
 export class SurfaceMagnetismBehavior implements Behavior<Mesh> {
+    private _scene: Scene;
     private _attachedMesh: Nullable<Mesh>;
     private _attachPointLocalOffset: Vector3;
+    private _pointerObserver: Nullable<Observer<PointerInfo>>;
+    private _utilityLayer: UtilityLayerRenderer;
 
     /**
      * Distance offset from the hit point to place the target at, along the hit normal.
      */
-    public hitNormalOffset: number = 0.1;
+    public hitNormalOffset: number = 0;
 
     /**
      * Name of the behavior
@@ -26,7 +34,7 @@ export class SurfaceMagnetismBehavior implements Behavior<Mesh> {
     /**
      * Spatial mapping meshes to collide with
      */
-    public meshes: Mesh[];
+    public meshes: AbstractMesh[];
 
     /**
      * Function called when the behavior needs to be initialized (after attaching it to a target)
@@ -37,31 +45,38 @@ export class SurfaceMagnetismBehavior implements Behavior<Mesh> {
      * Attaches the behavior to a transform node
      * @param target defines the target where the behavior is attached to
      */
-    public attach(target: Mesh): void {
+    public attach(target: Mesh, scene?: Scene, utilityLayer?: UtilityLayerRenderer): void {
         this._attachedMesh = target;
+        this._scene = scene || target.getScene();
         this.updateAttachPoint();
+        if (utilityLayer) {
+            this._utilityLayer = utilityLayer;
+            utilityLayer.pickingEnabled = false;
+        }
+        this._addObservables();
     }
-
+    
     /**
      * Detaches the behavior
      */
     public detach(): void {
         this._attachedMesh = null;
+        this._removeObservables();
+        this._utilityLayer.pickingEnabled = true;
     }
 
     /**
      * Collide the attached mesh with meshes of spatial mapping
      * @param ray The ray to collide meshes with
      */
-    public collide(ray: Ray) : Nullable<Vector3> {
+    public collide(pickingInfo: PickingInfo): Nullable<Vector3> {
         if (!this._attachedMesh) {
             return null;
         }
 
-        const pickingInfo = ray.intersectsMeshes(this.meshes);
-        if (pickingInfo[0] && pickingInfo[0].hit) {
-            const pickedNormal = pickingInfo[0].getNormal(true, true);
-            const pickedPoint = pickingInfo[0].pickedPoint;
+        if (pickingInfo && pickingInfo.hit) {
+            const pickedNormal = pickingInfo.getNormal(true, true);
+            const pickedPoint = pickingInfo.pickedPoint;
 
             if (!pickedNormal || !pickedPoint) {
                 return null;
@@ -107,5 +122,39 @@ export class SurfaceMagnetismBehavior implements Behavior<Mesh> {
         const centerOffset = Vector3.TransformCoordinates(center, Matrix.Invert(this._attachedMesh.getWorldMatrix()));
 
         return centerOffset;
+    }
+
+    private _addObservables() {
+        const pickPredicate = (mesh: AbstractMesh) => {
+            return this.meshes.indexOf(mesh) !== -1;
+        };
+
+        this._pointerObserver = this._scene.onPointerObservable.add((pointerInfo) => {
+            if (pointerInfo.type == PointerEventTypes.POINTERMOVE) {
+                if (
+                    this._attachedMesh &&
+                    pointerInfo.pickInfo &&
+                    pointerInfo.pickInfo.hit &&
+                    pointerInfo.pickInfo.pickedMesh &&
+                    pointerInfo.pickInfo.ray &&
+                    pickPredicate(pointerInfo.pickInfo.pickedMesh)
+                ) {
+                    const pos = this.collide(pointerInfo.pickInfo);
+                    if (pos) {
+                        this._attachedMesh.position.copyFrom(pos);
+                    }
+                }
+            }
+
+            if (pointerInfo.type == PointerEventTypes.POINTERDOWN) {
+                // Release the mesh
+                this.detach();
+            }
+        });
+    }
+
+    private _removeObservables() {
+        this._scene.onPointerObservable.remove(this._pointerObserver);
+        this._pointerObserver = null;
     }
 }
