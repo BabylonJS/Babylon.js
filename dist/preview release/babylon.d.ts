e46f4c797c2493e2a1a1e546e76040ed18e90464
David Catuhe <david.catuhe@live.fr>
2021-05-19 08:51:38 -0700

    Nightly

--- dist/preview release/babylon.d.ts
+++ dist/preview release/babylon.d.ts
@@ -82147,6 +82147,29 @@ declare module BABYLON {
 }
 declare module BABYLON {
     /**
+     * Class used to connect with the reflector zone of the sandbox via the reflector bridge
+     */
+    export class Reflector {
+        private static readonly SERVER_PREFIX;
+        private _scene;
+        private _webSocket;
+        /**
+         * Constructs a reflector object.
+         * @param scene The scene to use
+         * @param hostname The hostname of the reflector bridge
+         * @param port The port of the reflector bridge
+         */
+        constructor(scene: Scene, hostname: string, port: number);
+        /**
+         * Closes the reflector connection
+         */
+        close(): void;
+        private _handleServerMessage;
+        private _handleClientMessage;
+    }
+}
+declare module BABYLON {
+    /**
      * An interface for all Hit test features
      */
     export interface IWebXRHitTestFeature<T extends IWebXRLegacyHitResult> extends IWebXRFeature {
