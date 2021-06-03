536dea49ae51083550241266921775647cb5942a
Popov72 <github@evpopov.com>
2021-06-03 20:01:07 +0200

    Add a needShaderCodeInlining feature to the engine
    Former-commit-id: 7a5203d7e9dc3f32b3e15888f114daa2f20dc3e7

--- src/Engines/nativeEngine.ts
+++ src/Engines/nativeEngine.ts
@@ -28,7 +28,6 @@ import { DepthTextureCreationOptions } from '../Engines/depthTextureCreationOpti
 import { IMaterialContext } from "./IMaterialContext";
 import { IDrawContext } from "./IDrawContext";
 import { IStencilState } from "../States/IStencilState";
-import { ShaderProcessor } from "./Processors/shaderProcessor";
 
 interface INativeCamera {
     createVideo(constraints: MediaTrackConstraints): any;
@@ -208,7 +207,6 @@ class NativePipelineContext implements IPipelineContext {
 
     constructor(engine: NativeEngine) {
         this.engine = engine;
-        ShaderProcessor.EnableShaderCodeInliner = true;
     }
 
     public _fillEffectInformation(effect: Effect, uniformBuffersNames: { [key: string]: number }, uniformsNames: string[], uniforms: { [key: string]: Nullable<WebGLUniformLocation> }, samplerList: string[], samplers: { [key: string]: number }, attributesNames: string[], attributes: number[]) {
@@ -847,6 +845,7 @@ export class NativeEngine extends Engine {
             supportSyncTextureRead: false,
             needsInvertingBitmap: true,
             useUBOBindingCache: true,
+            needShaderCodeInlining: true,
             _collectUbosUpdatedInFrame: false,
         };
 
