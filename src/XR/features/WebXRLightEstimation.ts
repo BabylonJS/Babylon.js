4c9fb31b95fb58e1b4968ef52093e18830ec396e
Brian Zinn <github@wakeskate.com>
2021-06-03 23:09:23 -0700

    Able to retrieve: - LightEstimate   - Primary Light     - Direction     - Intensity - Reflection Cube Map.
    Former-commit-id: 5fc46bfdfc5ae2e0ee8a37f232e4d7ffce2ea30b

--- src/XR/features/WebXRLightEstimation.ts
+++ src/XR/features/WebXRLightEstimation.ts
@@ -21,16 +21,10 @@ export interface IWebXRLightEstimationOptions {
  */
 export class WebXRLightEstimation extends WebXRAbstractFeature {
 
-    /**
-     * While the estimated cube map is expected to update over time to better reflect the user's environment as they move around those changes are unlikely to happen with every XRFrame.
-     * Since creating and processing the cube map is potentially expensive, especially if mip maps are needed, pages can listen to the reflectionchange event on the XRLightProbe to
-     * determine when an updated cube map needs to be retrieved.
-     *
-     * Event Listener to for "reflectionchange" events.
-     */
-    private _reflectionChangeListener: Nullable<EventListenerOrEventListenerObject> = null;
-
     private _xrLightProbe: Nullable<XRLightProbe> = null;
+    private _xrLightEstimate: Nullable<XRLightEstimate> = null;
+    private _xrWebGLBinding: Nullable<XRWebGLBinding> = null;
+    private _reflectionCubeMap: Nullable<WebGLTexture> = null;
 
     /**
      * The module's name
@@ -44,7 +38,7 @@ export class WebXRLightEstimation extends WebXRAbstractFeature {
     public static readonly Version = 1;
 
     /**
-    * Creates a new instance of the light-estimation feature
+    * Creates a new instance of the light estimation feature
     * @param _xrSessionManager an instance of WebXRSessionManager
     * @param options options to use when constructing this feature
     */
@@ -77,30 +71,82 @@ export class WebXRLightEstimation extends WebXRAbstractFeature {
             reflectionFormat: this.options.reflectionFormatSRGBA8 ? "srgba8" : this._xrSessionManager.session.preferredReflectionFormat ?? "srgba8"
         }).then((value: XRLightProbe) => {
             this._xrLightProbe = value;
-        })
+        });
         return true;
     }
 
+    private _getXRGLBinding() : XRWebGLBinding {
+        if (this._xrWebGLBinding === null) {
+            // TODO: find a way that looks less hacky to get webgl context.  try webgl2 and fallback to webgl.
+            const canvas: HTMLCanvasElement = this._xrSessionManager.scene.getEngine().getRenderingCanvas()!;
+            let context: WebGLRenderingContext | WebGL2RenderingContext = <any>canvas.getContext("webgl2");
+            if (!context) {
+                context = <any>canvas.getContext("webgl");
+            }
+            this._xrWebGLBinding = new XRWebGLBinding(this._xrSessionManager.session, context);
+        }
+        return this._xrWebGLBinding;
+    }
+
     /**
-     * The XRLightProbe object created during attach (may not be available for a few frames).
+     * While the estimated cube map is expected to update over time to better reflect the user's environment as they move around those changes are unlikely to happen with every XRFrame.
+     * Since creating and processing the cube map is potentially expensive, especially if mip maps are needed, pages can listen to the reflectionchange event on the XRLightProbe to
+     * determine when an updated cube map needs to be retrieved.
+     *
+     * Event Listener to for "reflectionchange" events.
+     */
+    private _updateReflectionCubeMap() : void {
+        this._reflectionCubeMap = this._getXRGLBinding().getReflectionCubeMap(this._xrLightProbe!);
+    }
+
+    /**
+     * An estimated cube map representing the users environment.
+     */
+    public get reflectionCubeMap(): Nullable<WebGLTexture> {
+        if (this._xrLightProbe === null) {
+            return null;
+        }
+
+        if (this._reflectionCubeMap = null) {
+            this._reflectionCubeMap = this._getXRGLBinding().getReflectionCubeMap(this._xrLightProbe);
+            this._xrLightProbe.addEventListener('reflectionchange', this._updateReflectionCubeMap);
+        }
+
+        return this._reflectionCubeMap;
+    }
+
+    /**
+     * The XRLightProbe object created during attach (may not be available for a few frames depending on device).
+     *
+     * The XRLightProbe itself contains no lighting values, but is used to retrieve the current lighting state with each XRFrame.
      */
     public get xrLightProbe(): Nullable<XRLightProbe> {
         return this._xrLightProbe;
     }
 
     /**
+     * The most recent light estimate.  Available starting on the first frame where the device provides a light probe ()
+     */
+     public get xrLightingEstimate(): Nullable<XRLightEstimate> {
+        return this._xrLightEstimate;
+    }
+
+    /**
      * Dispose this feature and all of the resources attached
-     * 
-     * TODO: add code to attach the event listener!!
      */
     public dispose(): void {
         super.dispose();
-        if (this._xrLightProbe !== null && this._reflectionChangeListener !== null) {
-            this._xrLightProbe.removeEventListener('reflectionchange', this._reflectionChangeListener);
+        if (this._xrLightProbe !== null && this._reflectionCubeMap !== null) {
+            this._xrLightProbe.removeEventListener('reflectionchange', this._updateReflectionCubeMap);
         }
+        this._xrLightProbe = null;
     }
 
-    protected _onXRFrame(_xrFrame: XRFrame): void { /* empty */ }
+    protected _onXRFrame(_xrFrame: XRFrame): void {
+        if (this._xrLightProbe !== null) {
+            this._xrLightEstimate = _xrFrame.getLightEstimate(this._xrLightProbe);
+        }
+    }
 }
 
 // register the plugin
