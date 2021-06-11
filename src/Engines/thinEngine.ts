006a5419ca8f4f0f3aeb14d7c98a89f8d746cbb2
David Catuhe <david.catuhe@live.fr>
2021-06-11 09:34:37 -0700

    5.0.0-alpha.25
    Former-commit-id: d1cad1098601d3f80d995ebe5d094a066834bdd1

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -172,14 +172,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@5.0.0-alpha.24";
+        return "babylonjs@5.0.0-alpha.25";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "5.0.0-alpha.24";
+        return "5.0.0-alpha.25";
     }
 
     /**
