546ea7c5c468976f2bb29b90ec854f7b2e5f1474
Benjamin Guignabert <bguignabert@wanadev.fr>
2021-06-16 15:40:46 +0200

    #1 LOD screen coverage proposal
    Former-commit-id: 1fd7d0643219d4d168427f3332300fa75dae01f8

--- src/Meshes/meshLODLevel.ts
+++ src/Meshes/meshLODLevel.ts
@@ -8,13 +8,16 @@ import { Nullable } from '../types';
 export class MeshLODLevel {
     /**
      * Creates a new LOD level
-     * @param distance defines the distance where this level should star being displayed
+     * @param distanceOrScreenCoverage defines either the distance or the screen coverage where this level should start being displayed
      * @param mesh defines the mesh to use to render this level
+     * @param screenCoverage if specified, will use screen coverage (surface on the screen) to select LOD level
      */
     constructor(
-        /** Defines the distance where this level should start being displayed */
-        public distance: number,
+        /** Either distance from the center of the object to show this level or the screen coverage if `useScreenCoverage` is set to `true` */
+        public distanceOrScreenCoverage: number,
         /** Defines the mesh to use to render this level */
-        public mesh: Nullable<Mesh>) {
+        public mesh: Nullable<Mesh>,
+        /** Alternative definition of LOD level, using screen coverage instead of distance */
+        public useScreenCoverage: boolean = false) {
     }
 }
