6ece8b4c922cdc067b758cc8fa76a41238751c69
sebavan <vandenberghe.sebastien@gmail.com>
2021-06-19 00:57:18 +0200

    5.0.0-alpha.27
    Former-commit-id: 0557b950a6d0f8973bfcc3b5c6d5b2db0e8ac917

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -178,14 +178,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@5.0.0-alpha.26";
+        return "babylonjs@5.0.0-alpha.27";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "5.0.0-alpha.26";
+        return "5.0.0-alpha.27";
     }
 
     /**
