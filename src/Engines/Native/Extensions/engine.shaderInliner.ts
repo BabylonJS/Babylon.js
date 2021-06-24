d4fd841b8c52fc54f539d914bc48ddbe06734b58
Popov72 <github@evpopov.com>
2021-06-24 17:03:15 +0200

    Simplify code

--- src/Engines/Native/Extensions/engine.shaderInliner.ts
+++
@@ -1,9 +0,0 @@
-import { ShaderCodeInliner } from "../../Processors/shaderCodeInliner";
-import { NativeEngine } from "../../nativeEngine";
-
-NativeEngine.prototype.inlineShaderCode = function(code: string) {
-    const sci = new ShaderCodeInliner(code);
-    sci.debug = false;
-    sci.processCode();
-    return sci.code;
-};
--- src/Engines/Native/Extensions/index.ts
+++
@@ -1,1 +0,0 @@
-import "./engine.shaderInliner";
