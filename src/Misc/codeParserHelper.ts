c891ea4cbacbb602ade254d306f9338a0d2d1001
Popov72 <github@evpopov.com>
2021-06-30 19:38:44 +0200

    Fix doc

--- src/Misc/codeParserHelper.ts
+++ src/Misc/codeParserHelper.ts
@@ -1,7 +1,7 @@
 import { Nullable } from "../types";
 
 /**
- * Helper class used to parse shader code
+ * Helper class used to parse code
 */
 export abstract class CodeParserHelper {
 
@@ -17,14 +17,14 @@ export abstract class CodeParserHelper {
 
     /**
      * Initializes the parser
-     * @param sourceCode shader code source
+     * @param sourceCode source code
      */
     constructor(sourceCode: string) {
         this._sourceCode = sourceCode;
     }
 
     /**
-     * Start the processing of the shader code
+     * Starts the processing of the code
      */
     public processCode() {
         if (this.debug) {
@@ -38,6 +38,15 @@ export abstract class CodeParserHelper {
 
     protected abstract _internalProcess(): void;
 
+    /**
+     * Extracts some code between two markers. Note that you should call this method with the markerOpen in the first position of "block" and a corresponding markerClose somewhere in the block (not necessarily at the end)!
+     * For eg, you can call it with "(a+3*(4-c))+23" with markerOpen="(" and markerClose=")". It will return "a+3*(4-c)"
+     * @param markerOpen open marker
+     * @param markerClose close marker
+     * @param block block of code to parse
+     * @param startIndex starting index in "block"
+     * @returns the extracted code
+     */
     public static ExtractBetweenMarkers(markerOpen: string, markerClose: string, block: string, startIndex: number): number {
         let currPos = startIndex,
             openMarkers = 0,
@@ -94,6 +103,12 @@ export abstract class CodeParserHelper {
         return openMarkers === 0 ? currPos - 1 : -1;
     }
 
+    /**
+     * Skips all whitespaces in a string and return the index of the first non-whitespace character
+     * @param s string to parse
+     * @param index starting index in "s"
+     * @returns the index of the first non-whitespace character in s. If not found, returns s.length
+     */
     public static SkipWhitespaces(s: string, index: number): number {
         while (index < s.length) {
             const c = s[index];
@@ -106,6 +121,11 @@ export abstract class CodeParserHelper {
         return index;
     }
 
+    /**
+     * Checks if a string can be an identifier name (meaning, it contains only 0-9, A-Z, a-z and _ characters)
+     * @param c string to check
+     * @returns true if the string can be an identifier name, else false
+     */
     public static IsIdentifierChar(c: string): boolean {
         const v = c.charCodeAt(0);
         return (v >= 48 && v <= 57) || // 0-9
@@ -114,6 +134,11 @@ export abstract class CodeParserHelper {
             (v == 95); // _
     }
 
+    /**
+     * Removes the comments from a block of code. The comments are either \/* ... *\/ or \/\/ ...
+     * @param block code to parse
+     * @returns the code without the comments
+     */
     public static RemoveComments(block: string): string {
         let currPos = 0,
             waitForChar = '',
@@ -174,8 +199,14 @@ export abstract class CodeParserHelper {
         return s.join('');
     }
 
-    // this function can split the parameter list used in a function call at ',' boundaries by taking care of potential parenthesis like in:
-    //      myfunc(a, vec2(1., 0.), 4.)
+    /**
+     * Splits a list of parameters separated by a comma.
+     * This function can split the parameter list used in a function call at ',' boundaries by taking care of potential parenthesis like in:
+     *      myfunc(a, vec2(1., 0.), 4.)
+     * The extracted parameters will be ["a", "vec2(1., 0.)", "4."]
+     * @param s the string to parse
+     * @returns An array with the extracted parameters
+     */
     public static SplitParameters(s: string): Nullable<string[]> {
         const parameters = [];
         let curIdx = 0, startParamIdx = 0;
@@ -201,6 +232,13 @@ export abstract class CodeParserHelper {
         return parameters;
     }
 
+    /**
+     * Scans a string backward until a specific character is found
+     * @param s string to scan
+     * @param index starting index in "s" to look for the character
+     * @param c character to look for
+     * @returns the index in "s" where "c" has been found. Returns -1 if the character could not be found
+     */
     public static FindBackward(s: string, index: number, c: string): number {
         while (index >= 0 && s.charAt(index) !== c) {
             index--;
@@ -209,6 +247,11 @@ export abstract class CodeParserHelper {
         return index;
     }
 
+    /**
+     * Escapes a string so that it can be used as a regular expression
+     * @param s string to escape
+     * @returns escaped string
+     */
     public static EscapeRegExp(s: string): string {
         return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
