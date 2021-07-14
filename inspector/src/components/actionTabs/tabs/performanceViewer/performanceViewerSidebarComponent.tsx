0a1989a25d20f0a22e04b29762ef0fcf5a6f7de2
Sahil Tara <sahiltara@hotmail.com>
2021-07-14 18:11:29 -0400

    Added some css and colour picker

--- inspector/src/components/actionTabs/tabs/performanceViewer/performanceViewerSidebarComponent.tsx
+++ inspector/src/components/actionTabs/tabs/performanceViewer/performanceViewerSidebarComponent.tsx
@@ -1,23 +1,28 @@
-import { Observable } from 'babylonjs';
+import { Color3, Observable } from 'babylonjs';
 import { IPerfDataset } from 'babylonjs/Misc/interfaces/iPerfViewer';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
+import { ColorPickerLineComponent } from '../../../../sharedUiComponents/lines/colorPickerComponent';
 
 interface IPerformanceViewerSidebarComponentProps {
     datasetObservable: Observable<IPerfDataset[]>
     onToggleVisibility: (id: string, value: boolean) => void;
+    onColorChanged: (id: string, value: string) => void;
 }
 
-
 export const PerformanceViewerSidebarComponent = (props: IPerformanceViewerSidebarComponentProps) => {
-    const {onToggleVisibility, datasetObservable} = props;
-    const [content, setContent] = useState<JSX.Element>(<></>);
+    const {onToggleVisibility, datasetObservable, onColorChanged} = props;
+    // TODO: Refactor to store only metadata once data layer is implemented.
+    const [datasets, setDatasets] = useState<IPerfDataset[]>([]);
 
     useEffect(() => {
+        const onUpdateDatasets = (datasets: IPerfDataset[]) => {
+            setDatasets(datasets);
+        }
+
         datasetObservable.add(onUpdateDatasets);
         return () => {
             datasetObservable.removeCallback(onUpdateDatasets);
-            console.log(datasetObservable.hasObservers());
         }
     }, []);
 
@@ -25,30 +30,21 @@ export const PerformanceViewerSidebarComponent = (props: IPerformanceViewerSideb
         onToggleVisibility(id, !event.currentTarget.checked);
     }
 
-    const onUpdateDatasets = (datasets: IPerfDataset[]) => {
-
-        const htmlContent: JSX.Element = (
-            <>
-                {
-                    datasets.map((dataset: IPerfDataset) => {
-                        return (
-                            <div key={`perf-sidebar-item-${dataset.id}`} className={"group"}>
-                                <input type="checkbox" checked={!dataset.hidden} onChange={onCheckChange(dataset.id)} />
-                                <span>{dataset.id}</span>
-                            </div>
-                        );
-                    })
-                }
-            </>
-        )
-        
-        setContent(htmlContent);
+    const onColorChange = (id: string) => (color: string) => {
+        onColorChanged(id, color);
     }
 
     return (
-        <div id="performanceViewerSidebar">
+        <div id="performance-viewer-sidebar">
             {
-                content
+                datasets.map((dataset: IPerfDataset) => (
+                        <div key={`perf-sidebar-item-${dataset.id}`} className="sidebar-item">
+                            <input type="checkbox" checked={!dataset.hidden} onChange={onCheckChange(dataset.id)} />
+                            <span className="sidebar-item-label">{dataset.id}</span>
+                            <ColorPickerLineComponent value={Color3.FromHexString(dataset.color ?? "#000")} onColorChanged={onColorChange(dataset.id)} shouldPopRight />
+                        </div>
+                    )
+                )
             }
         </div>
     )
