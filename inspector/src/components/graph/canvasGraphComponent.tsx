c5381d8e6768a25d6216bbb3cffee0cc84db4fac
Sahil Tara <sahiltara@hotmail.com>
2021-07-15 19:44:36 -0400

    Added resizability
    Former-commit-id: 71dbb72ca976480587efbf4daf725b0013bc3f92

--- inspector/src/components/graph/canvasGraphComponent.tsx
+++ inspector/src/components/graph/canvasGraphComponent.tsx
@@ -1,34 +1,53 @@
+import { Observable } from 'babylonjs/Misc/observable';
 import * as React from 'react';
 import { useEffect, useRef } from 'react';
 import { CanvasGraphService } from './canvasGraphService';
+import { IPerfLayoutSize } from './graphSupportingTypes';
 
 interface ICanvasGraphComponentProps {
     id: string;
     canvasServiceCallback: (canvasService: CanvasGraphService) => void;
+    layoutObservable?: Observable<IPerfLayoutSize>;
 }
 
 export const CanvasGraphComponent: React.FC<ICanvasGraphComponentProps> = (props: ICanvasGraphComponentProps) => {
-    const { id, canvasServiceCallback } = props;
+    const { id, canvasServiceCallback, layoutObservable } = props;
     const canvasRef: React.MutableRefObject<HTMLCanvasElement | null>  = useRef(null);
+
     useEffect(() => {
         if (!canvasRef.current) {
             return;
         }
         
-        let canvasGraphService: CanvasGraphService | undefined;
+        let cs: CanvasGraphService | undefined;
 
         // temporarily set empty array, will eventually be passed by props!
         try {
-            canvasGraphService = new CanvasGraphService(canvasRef.current, {datasets: []});
-            canvasServiceCallback(canvasGraphService);
+            cs = new CanvasGraphService(canvasRef.current, {datasets: []});
+            canvasServiceCallback(cs);
         } catch (error) {
             console.error(error);
             return;
         }
 
-        return () => canvasGraphService?.destroy();
-    }, [canvasRef])
-   
+        const layoutUpdated = (newSize: IPerfLayoutSize) => {
+            if (!canvasRef.current) {
+                return;
+            }
+            const {left, top} = canvasRef.current.getBoundingClientRect();
+            const width = newSize.width - left;
+            const height = newSize.height - top;
+            cs?.resize(width, height);
+        };
+
+        layoutObservable?.add(layoutUpdated);
+
+        return () => {
+            cs?.destroy();
+            layoutObservable?.removeCallback(layoutUpdated);
+        };
+    }, [canvasRef]);
+    
     return (
         <canvas id={id} ref={canvasRef}>
 
