58b2105c7cf71257d16605c0e42ea53582844f9b
Darragh Burke <darraghjburke@gmail.com>
2021-07-15 11:40:07 -0700

    remove extra whitespace
    Former-commit-id: b2a7c417438979c9614156dafe004817d1e51330

--- serializers/src/glTF/2.0/glTFExporter.ts
+++ serializers/src/glTF/2.0/glTFExporter.ts
@@ -1080,7 +1080,7 @@ export class _Exporter {
             jsonChunkBufferView.setUint32(0, jsonLength + jsonPadding, true);
             jsonChunkBufferView.setUint32(4, 0x4E4F534A, true);
 
-            //json chunk bytes 
+            //json chunk bytes
             const jsonData = new Uint8Array(jsonChunkBuffer, chunkLengthPrefix);
             // if TextEncoder was available, we can simply copy the encoded array
             if (encodedJsonText) {
