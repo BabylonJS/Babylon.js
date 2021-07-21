0df85c17cd71a71338ad6f2a7d71240fa22f9d7c
rickfromwork <msft_richard@outlook.com>
2021-07-21 16:02:28 -0700

    cleanup
    Former-commit-id: 6a93b1865d56e529bb0e1a191cbe71bff598b6dc

--- src/XR/features/WebXREyeTracking.ts
+++ src/XR/features/WebXREyeTracking.ts
@@ -1,47 +1,17 @@
 import { WebXRFeaturesManager, WebXRFeatureName } from '../webXRFeaturesManager';
 import { WebXRAbstractFeature } from './WebXRAbstractFeature';
 import { WebXRSessionManager } from '../webXRSessionManager';
-import { TransformNode } from '../../Meshes/transformNode';
 import { Observable } from "../../Misc/observable";
 import { Vector3, Quaternion } from "../../Maths/math.vector";
 import { Ray } from "../../Culling/ray";
 import { Nullable } from "../../types";
 
 /**
- * Options used in the mesh detector module
- */
-export interface IWebXREyeTrackingOptions {
-    /**
-     * The node to use to transform the local results to world coordinates
-     */
-    worldParentNode?: TransformNode;
-    /**
-     * If set to true a reference of the created meshes will be kept until the next session starts
-     * If not defined, meshes will be removed from the array when the feature is detached or the session ended.
-     */
-    doNotRemoveMeshesOnSessionEnded?: boolean;
-    /**
-     * Preferred detector configuration, not all preferred options will be supported by all platforms.
-     */
-    preferredDetectorOptions?: XRGeometryDetectorOptions;
-    /**
-     * If set to true, WebXREyeTracking will convert coordinate systems for meshes.
-     * If not defined, mesh conversions from right handed to left handed coordinate systems won't be conducted.
-     * Right handed mesh data will be available through IWebXRVertexData.xrMesh.
-     */
-    convertCoordinateSystems?: boolean;
-}
-
-
-//let meshIdProvider = 0;
-
-/**
- * The mesh detector is used to detect meshes in the real world when in AR
+ * The eye tracker grabs eye data from the device and provides it in an easy-access format.
+ * Currently only enabled for BabylonNative applications, developers intending to use this must enable the Gaze Input capability in their appx package manifest for data to flow.
  */
 export class WebXREyeTracking extends WebXRAbstractFeature {
-//    private _detectedMeshes: Map<XRMesh, IWebXRVertexData> = new Map<XRMesh, IWebXRVertexData>();
     private _latestEyeSpace: Nullable<XRSpace>;
-
     private _gazeRay: Nullable<Ray>;
 
     private _tmpQuat: Quaternion = new Quaternion();
@@ -57,26 +27,20 @@ export class WebXREyeTracking extends WebXRAbstractFeature {
      */
     public static readonly Version = 1;
 
-
-
     /**
-     * Observers registered here will be executed when a new mesh was added to the session
+     * This observable will notify registered observers when eye tracking starts
      */
-  //  public onMeshAddedObservable: Observable<IWebXRVertexData> = new Observable();
-    /**
-     * Observers registered here will be executed when a mesh is no longer detected in the session
-     */
-  //  public onMeshRemovedObservable: Observable<IWebXRVertexData> = new Observable();
-    /**
-     * Observers registered here will be executed when an existing mesh updates
-     */
-  //  public onMeshUpdatedObservable: Observable<IWebXRVertexData> = new Observable();
     public onEyeTrackingStartedObservable: Observable<Ray> = new Observable();
+    /**
+     * This observable will notify registered observers when eye tracking ends
+     */
     public onEyeTrackingEndedObservable: Observable<void> = new Observable();
+    /**
+     * This observable will notify registered observers on each frame that has valid tracking
+     */
     public onEyeTrackingFrameUpdateObservable: Observable<Ray> = new Observable();
 
-
-    constructor(_xrSessionManager: WebXRSessionManager, private _options: IWebXREyeTrackingOptions = {}) {
+    constructor(_xrSessionManager: WebXRSessionManager) {
         super(_xrSessionManager);
         this.xrNativeFeatureName = "eye-tracking";
         if (this._xrSessionManager.session) {
@@ -122,10 +86,6 @@ export class WebXREyeTracking extends WebXRAbstractFeature {
         if (!this.attached || !frame) {
             return;
         }
-        if (this._options.convertCoordinateSystems) {
-            //satisfy the compiler for now
-            return;
-        }
 
         if (this._latestEyeSpace && this._gazeRay) {
             let pose = frame.getPose(this._latestEyeSpace, this._xrSessionManager.referenceSpace);
@@ -139,11 +99,10 @@ export class WebXREyeTracking extends WebXRAbstractFeature {
                     this._tmpQuat.z *= -1;
                     this._tmpQuat.w *= -1;
                 }
-                
+
                 Vector3.Forward().rotateByQuaternionToRef(this._tmpQuat, this._gazeRay.direction);
                 this.onEyeTrackingFrameUpdateObservable.notifyObservers(this._gazeRay);
             }
-
         }
     }
 
@@ -171,7 +130,7 @@ export class WebXREyeTracking extends WebXRAbstractFeature {
 WebXRFeaturesManager.AddWebXRFeature(
     WebXREyeTracking.Name,
     (xrSessionManager, options) => {
-        return () => new WebXREyeTracking(xrSessionManager, options);
+        return () => new WebXREyeTracking(xrSessionManager);
     },
     WebXREyeTracking.Version,
     false
