bb2e15fe41de8e0c49f606f725aa2ee058f6b5fc
Sahil Tara <sahiltara@hotmail.com>
2021-07-22 19:41:54 -0400

    Changed to enum string instead of bitfield enum
    Former-commit-id: 4a5f011feb59763842dbb853c8f319bd581a472f

--- src/Misc/PerformanceViewer/performanceViewerCollectionStrategies.ts
+++ src/Misc/PerformanceViewer/performanceViewerCollectionStrategies.ts
@@ -7,10 +7,6 @@ import { PerfCollectionType } from "./perfCollectionType";
  */
 export interface IPerfViewerCollectionStrategy {
     /**
-     * The id the collection uses, used for mapping purposes.
-     */
-    id: string;
-    /**
      * Function which gets the data for the strategy.
      */
     getData: () => number;
@@ -31,122 +27,98 @@ const defaultGetDataImpl = () => 0;
  */
 export function collectionTypeToPredefinedStrategies(scene: Scene, engineInstrumentation: EngineInstrumentation): Record<PerfCollectionType, IPerfViewerCollectionStrategy> {
     const fpsStrategy: IPerfViewerCollectionStrategy = {
-        id: "fps",
         getData: () => scene.getEngine().getFps(),
     };
 
     const cpuStrategy: IPerfViewerCollectionStrategy = {
-        id: "cpu usage",
         getData: defaultGetDataImpl,
     };
 
     const totalMeshesStrategy: IPerfViewerCollectionStrategy = {
-        id: "total meshes",
         getData: defaultGetDataImpl,
     };
 
     const activeMeshesStrategy: IPerfViewerCollectionStrategy = {
-        id: "active meshes",
         getData: defaultGetDataImpl,
     };
 
     const activeIndiciesStrategy: IPerfViewerCollectionStrategy = {
-        id: "active indices",
         getData: defaultGetDataImpl,
     };
 
     const activeFacesStrategy: IPerfViewerCollectionStrategy = {
-        id: "active faces",
         getData: defaultGetDataImpl,
     };
 
     const activeBonesStrategy: IPerfViewerCollectionStrategy = {
-        id: "active bones",
         getData: defaultGetDataImpl,
     };
 
     const activeParticlesStrategy: IPerfViewerCollectionStrategy = {
-        id: "active particles",
         getData: defaultGetDataImpl,
     };
 
     const drawCallsStrategy: IPerfViewerCollectionStrategy = {
-        id: "draw calls",
         getData: defaultGetDataImpl,
     };
 
     const totalLightsStrategy: IPerfViewerCollectionStrategy = {
-        id: "total lights",
         getData: defaultGetDataImpl,
     };
 
     const totalVerticesStrategy: IPerfViewerCollectionStrategy = {
-        id: "total vertices",
         getData: defaultGetDataImpl,
     };
 
     const totalMaterialsStrategy: IPerfViewerCollectionStrategy = {
-        id: "total materials",
         getData: defaultGetDataImpl,
     };
 
     const totalTexturesStrategy: IPerfViewerCollectionStrategy = {
-        id: "total textures",
         getData: defaultGetDataImpl,
     };
 
     const absoluteFpsStrategy: IPerfViewerCollectionStrategy = {
-        id: "absolute fps",
         getData: defaultGetDataImpl,
     };
 
     const meshesSelectionStrategy: IPerfViewerCollectionStrategy = {
-        id: "meshes selection time",
         getData: defaultGetDataImpl,
     };
 
     const renderTargetsStrategy: IPerfViewerCollectionStrategy = {
-        id: "render targets time",
         getData: defaultGetDataImpl,
     };
 
     const particlesStrategy: IPerfViewerCollectionStrategy = {
-        id: "particles time",
         getData: defaultGetDataImpl,
     };
 
     const spritesStrategy: IPerfViewerCollectionStrategy = {
-        id: "sprites time",
         getData: defaultGetDataImpl,
     };
 
     const animationsStrategy: IPerfViewerCollectionStrategy = {
-        id: "animations time",
         getData: defaultGetDataImpl,
     };
 
     const physicsStrategy: IPerfViewerCollectionStrategy = {
-        id: "physics time",
         getData: defaultGetDataImpl,
     };
 
     const renderStrategy: IPerfViewerCollectionStrategy = {
-        id: "render time",
         getData: defaultGetDataImpl,
     };
 
     const frameTotalStrategy: IPerfViewerCollectionStrategy = {
-        id: "total frame time",
         getData: defaultGetDataImpl,
     };
 
     const interFrameStrategy: IPerfViewerCollectionStrategy = {
-        id: "inter-frame time",
         getData: defaultGetDataImpl,
     };
 
     const gpuFrameTimeStrategy: IPerfViewerCollectionStrategy = {
-        id: "gpu frame time",
         getData: () => Math.max(engineInstrumentation.gpuFrameTimeCounter.lastSecAverage * 0.000001, 0),
         initialization:  () => {
             engineInstrumentation.captureGPUFrameTime = true;
