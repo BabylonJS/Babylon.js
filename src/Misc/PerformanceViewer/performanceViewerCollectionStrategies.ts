84f0bde521e2d8c1d0af4c7f996ec748729869bd
Sahil Tara <sahiltara@hotmail.com>
2021-07-23 13:04:25 -0400

    Removed enum and changed to class based approach
    Former-commit-id: 1e1b903c3871db49bb0554d5f29d882ce981b3f3

--- src/Misc/PerformanceViewer/performanceViewerCollectionStrategies.ts
+++ src/Misc/PerformanceViewer/performanceViewerCollectionStrategies.ts
@@ -1,11 +1,14 @@
 import { EngineInstrumentation } from "../../Instrumentation/engineInstrumentation";
 import { Scene } from "../../scene";
-import { PerfCollectionType } from "./perfCollectionType";
 
 /**
  * Defines the general structure of what is necessary for a collection strategy.
  */
 export interface IPerfViewerCollectionStrategy {
+    /**
+     * The id of the strategy.
+     */
+    id: string;
     /**
      * Function which gets the data for the strategy.
      */
@@ -20,135 +23,227 @@ export interface IPerfViewerCollectionStrategy {
 const defaultGetDataImpl = () => 0;
 
 /**
- * Function to get a mapping of PerfCollectionType to strategy.
- * @param scene the scene to collect data on.
- * @param engineInstrumentation engineInstrumentation which is initialized from the scene, also used to collect data on.
- * @returns a mapping of PerfCollectionType to strategy.
+ * Defines the predefined strategies used in the performance viewer.
  */
-export function collectionTypeToPredefinedStrategies(scene: Scene, engineInstrumentation: EngineInstrumentation): Record<PerfCollectionType, IPerfViewerCollectionStrategy> {
-    const fpsStrategy: IPerfViewerCollectionStrategy = {
+ export class PerfCollectionStrategy {
+    /**
+     * Gets the strategy used for collection of fps metrics
+     * @param scene the scene used for collection of metrics.
+     * @returns the fps strategy
+     */
+    public static FpsStrategy = (scene: Scene): IPerfViewerCollectionStrategy => ({
+        id: "fps",
         getData: () => scene.getEngine().getFps(),
-    };
+    });
 
-    const cpuStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of cpu utilization metrics.
+     * @returns the cpu utilization strategy
+     */
+    public static CpuStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "cpu utilization",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const totalMeshesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of total meshes metrics.
+     * @returns the total meshes strategy
+     */
+    public static TotalMeshesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "total meshes",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const activeMeshesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of active meshes metrics.
+     * @returns the active meshes strategy
+     */
+    public static ActiveMeshesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "active meshes",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const activeIndiciesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of active indices metrics.
+     * @returns the active indices strategy
+     */
+    public static ActiveIndiciesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "active indices",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const activeFacesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of active faces metrics.
+     * @returns the active faces strategy
+     */
+    public static ActiveFacesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "active faces",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const activeBonesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of active bones metrics.
+     * @returns the active bones strategy
+     */
+    public static ActiveBonesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "active bones",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const activeParticlesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of active particles metrics.
+     * @returns the active particles strategy
+     */
+    public static ActiveParticlesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "active particles",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const drawCallsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of draw calls metrics.
+     * @returns the draw calls strategy
+     */
+    public static DrawCallsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "draw calls",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const totalLightsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of total lights metrics.
+     * @returns the total lights strategy
+     */
+    public static TotalLightsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "total lights",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const totalVerticesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of total vertices metrics.
+     * @returns the total vertices strategy
+     */
+    public static TotalVerticesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "total vertices",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const totalMaterialsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of total materials metrics.
+     * @returns the total materials strategy
+     */
+    public static TotalMaterialsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "total materials",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const totalTexturesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of total textures metrics.
+     * @returns the total textures strategy
+     */
+    public static TotalTexturesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "total textures",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const absoluteFpsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of absolute fps metrics.
+     * @returns the absolute fps strategy
+     */
+    public static AbsoluteFpsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "absolute fps",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const meshesSelectionStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of meshes selection time metrics.
+     * @returns the meshes selection time strategy
+     */
+    public static MeshesSelectionStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "meshes selection time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const renderTargetsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of render targets time metrics.
+     * @returns the render targets time strategy
+     */
+    public static RenderTargetsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "render targets time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const particlesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of particles time metrics.
+     * @returns the particles time strategy
+     */
+    public static ParticlesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "particles time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const spritesStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of sprites time metrics.
+     * @returns the sprites time strategy
+     */
+    public static SpritesStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "sprites time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const animationsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of animations time metrics.
+     * @returns the animations time strategy
+     */
+    public static AnimationsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "animations time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const physicsStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of physics time metrics.
+     * @returns the physics time strategy
+     */
+    public static PhysicsStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "physics time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const renderStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of render time metrics.
+     * @returns the render time strategy
+     */
+    public static RenderStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "render time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const frameTotalStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of total frame time metrics.
+     * @returns the total frame time strategy
+     */
+    public static FrameTotalStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "total frame time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const interFrameStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of inter-frame time metrics.
+     * @returns the inter-frame time strategy
+     */
+    public static InterFrameStrategy = (): IPerfViewerCollectionStrategy => ({
+        id: "inter-frame time",
         getData: defaultGetDataImpl,
-    };
+    });
 
-    const gpuFrameTimeStrategy: IPerfViewerCollectionStrategy = {
+    /**
+     * Gets the strategy used for collection of gpu frame time metrics.
+     * @param engineInstrumentation engine instrumentation object used for collection of metrics.
+     * @returns the gpu frame time strategy
+     */
+    public static GpuFrameTimeStrategy = (engineInstrumentation: EngineInstrumentation): IPerfViewerCollectionStrategy => ({
+        id:  "gpu frame time",
         getData: () => Math.max(engineInstrumentation.gpuFrameTimeCounter.lastSecAverage * 0.000001, 0),
         initialization:  () => {
             engineInstrumentation.captureGPUFrameTime = true;
         }
-    };
-
-    return {
-        [PerfCollectionType.Fps]: fpsStrategy,
-        [PerfCollectionType.Cpu]: cpuStrategy,
-        [PerfCollectionType.TotalMeshes]: totalMeshesStrategy,
-        [PerfCollectionType.ActiveMeshes]: activeMeshesStrategy,
-        [PerfCollectionType.ActiveIndicies]: activeIndiciesStrategy,
-        [PerfCollectionType.ActiveFaces]: activeFacesStrategy,
-        [PerfCollectionType.ActiveBones]: activeBonesStrategy,
-        [PerfCollectionType.ActiveParticles]: activeParticlesStrategy,
-        [PerfCollectionType.DrawCalls]: drawCallsStrategy,
-        [PerfCollectionType.TotalLights]: totalLightsStrategy,
-        [PerfCollectionType.TotalVertices]: totalVerticesStrategy,
-        [PerfCollectionType.TotalMaterials]: totalMaterialsStrategy,
-        [PerfCollectionType.TotalTextures]: totalTexturesStrategy,
-        [PerfCollectionType.AbsoluteFps]: absoluteFpsStrategy,
-        [PerfCollectionType.MeshesSelection]: meshesSelectionStrategy,
-        [PerfCollectionType.RenderTargets]: renderTargetsStrategy,
-        [PerfCollectionType.Particles]: particlesStrategy,
-        [PerfCollectionType.Sprites]: spritesStrategy,
-        [PerfCollectionType.Animations]: animationsStrategy,
-        [PerfCollectionType.Physics]: physicsStrategy,
-        [PerfCollectionType.Render]: renderStrategy,
-        [PerfCollectionType.FrameTotal]: frameTotalStrategy,
-        [PerfCollectionType.InterFrame]: interFrameStrategy,
-        [PerfCollectionType.GpuFrameTime]: gpuFrameTimeStrategy,
-    };
+    });
 }
