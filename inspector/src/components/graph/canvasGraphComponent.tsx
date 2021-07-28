bc65567db9a5262d09dd3c561865913d9c09eabe
Sahil Tara <sahiltara@hotmail.com>
2021-07-28 15:00:11 -0400

    Used observables instead of callbacks, and began using performanceviewercollector in neccessary components.
    Former-commit-id: c958b94213ae720aa2c6f1dd7caf7818dda8182d

--- inspector/src/components/graph/canvasGraphComponent.tsx
+++ inspector/src/components/graph/canvasGraphComponent.tsx
@@ -1,17 +1,21 @@
+import { PerformanceViewerCollector } from 'babylonjs/Misc/PerformanceViewer/performanceViewerCollector';
 import { Observable } from 'babylonjs/Misc/observable';
 import * as React from 'react';
 import { useEffect, useRef } from 'react';
 import { CanvasGraphService } from './canvasGraphService';
 import { IPerfLayoutSize } from './graphSupportingTypes';
+import { IPerfMetadata } from 'babylonjs/Misc/interfaces/iPerfViewer';
+import { Scene } from 'babylonjs/scene';
 
 interface ICanvasGraphComponentProps {
     id: string;
-    canvasServiceCallback: (canvasService: CanvasGraphService) => void;
+    scene: Scene;
+    collector: PerformanceViewerCollector
     layoutObservable?: Observable<IPerfLayoutSize>;
 }
 
 export const CanvasGraphComponent: React.FC<ICanvasGraphComponentProps> = (props: ICanvasGraphComponentProps) => {
-    const { id, canvasServiceCallback, layoutObservable } = props;
+    const { id, collector, scene, layoutObservable } = props;
     const canvasRef: React.MutableRefObject<HTMLCanvasElement | null>  = useRef(null);
 
     useEffect(() => {
@@ -21,10 +25,9 @@ export const CanvasGraphComponent: React.FC<ICanvasGraphComponentProps> = (props
         
         let cs: CanvasGraphService | undefined;
 
-        // temporarily set empty array, will eventually be passed by props!
+        // TODO: SET datasets as collector.datasets once canvas graph service pr is up.
         try {
             cs = new CanvasGraphService(canvasRef.current, {datasets: []});
-            canvasServiceCallback(cs);
         } catch (error) {
             console.error(error);
             return;
@@ -40,11 +43,28 @@ export const CanvasGraphComponent: React.FC<ICanvasGraphComponentProps> = (props
             cs?.resize(newSize);
         };
 
+        const dataUpdated = () => {
+            cs?.update();
+        };
+
+        const metaUpdated = (_: Map<string, IPerfMetadata>) => {
+            if (!cs) {
+                return;
+            }
+            // TODO: add this line once canvas graph service pr is up. cs.metadata = meta;
+            cs.update();
+        };
+        
+        scene.onAfterRenderObservable.add(dataUpdated);
+        collector.metadataObservable.add(metaUpdated);
+
         layoutObservable?.add(layoutUpdated);
 
         return () => {
             cs?.destroy();
             layoutObservable?.removeCallback(layoutUpdated);
+            collector.datasetObservable.removeCallback(dataUpdated);
+            collector.metadataObservable.removeCallback(metaUpdated);
         };
     }, [canvasRef]);
     
