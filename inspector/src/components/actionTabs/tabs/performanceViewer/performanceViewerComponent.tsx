40c6dec8fc69c5a1c7e83e84d6963aedc5d52661
Sahil Tara <sahiltara@hotmail.com>
2021-07-28 15:00:11 -0400

    Used observables instead of callbacks, and began using performanceviewercollector in neccessary components.

--- inspector/src/components/actionTabs/tabs/performanceViewer/performanceViewerComponent.tsx
+++ inspector/src/components/actionTabs/tabs/performanceViewer/performanceViewerComponent.tsx
@@ -1,10 +1,16 @@
+import { Observable } from "babylonjs";
 import { Scene } from "babylonjs/scene";
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ButtonLineComponent } from "../../../../sharedUiComponents/lines/buttonLineComponent";
 import { CanvasGraphComponent } from "../../../graph/canvasGraphComponent";
-import { CanvasGraphService } from "../../../graph/canvasGraphService";
+import { IPerfLayoutSize } from "../../../graph/graphSupportingTypes";
 import { PopupComponent } from "../../../popupComponent";
+import { PerformanceViewerSidebarComponent } from "./performanceViewerSidebarComponent";
+import { PerformanceViewerCollector } from "babylonjs/Misc/PerformanceViewer/performanceViewerCollector";
+import { PerfCollectionStrategy } from "babylonjs/Misc/PerformanceViewer/performanceViewerCollectionStrategies";
+
+require('./scss/performanceViewer.scss');
 
 interface IPerformanceViewerComponentProps {
     scene: Scene;
@@ -14,10 +20,14 @@ interface IPerformanceViewerComponentProps {
 const initialWindowSize = { width: 1024, height: 512 };
 
 // Note this should be false when committed until the feature is fully working.
-const isEnabled = false;
+const isEnabled = true;
 
 export const PerformanceViewerComponent: React.FC<IPerformanceViewerComponentProps> = (props: IPerformanceViewerComponentProps) => {
+    const { scene } = props;
     const [isOpen, setIsOpen] = useState(false);
+    const [ performanceCollector, setPerformanceCollector ] = useState<PerformanceViewerCollector | undefined>();
+    const [layoutObservable] = useState(new Observable<IPerfLayoutSize>());
+    const popupRef = useRef<PopupComponent | null>(null);
 
     // do cleanup when the window is closed
     const onClosePerformanceViewer = (window: Window | null) => {
@@ -32,12 +42,28 @@ export const PerformanceViewerComponent: React.FC<IPerformanceViewerComponentPro
     }
 
     const onResize = () => {
-        // do nothing for now.
+        if (!popupRef.current) {
+            return;
+        }
+        const window = popupRef.current.getWindow();
+        const width = window?.innerWidth ?? 0;
+        const height = window?.innerHeight ?? 0;
+        layoutObservable.notifyObservers({width, height});
     }
 
-    const canvasServiceCallback = (canvasService: CanvasGraphService) => {
-        canvasService.update();
-    };
+    useEffect(() => {
+        let perfCollector: PerformanceViewerCollector | undefined;
+        if (isOpen) {
+            perfCollector = new PerformanceViewerCollector(scene, [PerfCollectionStrategy.GpuFrameTimeStrategy(), PerfCollectionStrategy.FpsStrategy()]);
+            perfCollector.start();
+            setPerformanceCollector(perfCollector);
+        }
+
+        return () => {
+            perfCollector?.stop();
+            setPerformanceCollector(undefined);
+        }
+    }, [isOpen]);
 
     return (
         <>
@@ -51,13 +77,15 @@ export const PerformanceViewerComponent: React.FC<IPerformanceViewerComponentPro
                     id="perf-viewer"
                     title="Performance Viewer"
                     size={initialWindowSize}
+                    ref={popupRef}
                     onResize={onResize}
                     onClose={onClosePerformanceViewer}
                 >
                     <div id="performance-viewer">
-                        <>
-                            <CanvasGraphComponent id="myChart" canvasServiceCallback={canvasServiceCallback} />
-                        </>
+                        {performanceCollector && <>
+                            <PerformanceViewerSidebarComponent collector={performanceCollector} />
+                            <CanvasGraphComponent id="performance-viewer-graph" layoutObservable={layoutObservable} scene={scene} collector={performanceCollector} />
+                        </>}
                     </div>
                 </PopupComponent>
         }
