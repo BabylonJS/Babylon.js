e85ed8aa23336af02a667f8573479c240fff78ca
Sahil Tara <sahiltara@hotmail.com>
2021-07-28 18:05:02 -0400

    Changed method name to getCurrentSlice, used spread operator, removed /index
    Former-commit-id: 3b9629d4534d3beaf13d01edae49ee9083dd41b7

--- src/Misc/index.ts
+++ src/Misc/index.ts
@@ -55,4 +55,4 @@ export * from './timer';
 export * from "./copyTools";
 export * from "./reflector";
 export * from "./domManagement";
-export * from "./PerformanceViewer/index";
+export * from "./PerformanceViewer/";
