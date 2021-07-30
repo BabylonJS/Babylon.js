4659337354c1360ebe9c78f3b489dedb6c249b07
Popov72 <github@evpopov.com>
2021-07-30 12:24:58 +0200

    Fix bakeTransformIntoVertices not working when using rotationQuaternion

--- src/Meshes/transformNode.ts
+++ src/Meshes/transformNode.ts
@@ -1204,7 +1204,7 @@ export class TransformNode extends Node {
                     let tmpRotationQuaternion = TmpVectors.Quaternion[0];
                     bakedMatrix.decompose(child.scaling, tmpRotationQuaternion, child.position);
                     if (child.rotationQuaternion) {
-                        child.rotationQuaternion = tmpRotationQuaternion;
+                        child.rotationQuaternion = tmpRotationQuaternion.clone();
                     } else {
                         tmpRotationQuaternion.toEulerAnglesToRef(child.rotation);
                     }
