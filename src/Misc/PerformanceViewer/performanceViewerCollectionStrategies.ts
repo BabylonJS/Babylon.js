9113fd8b7b55da9628ab7990fe78e6a06a39db4d
Sahil Tara <sahiltara@hotmail.com>
2021-08-06 12:34:18 -0400

    Added initial version of CPU Strategy

--- src/Misc/PerformanceViewer/performanceViewerCollectionStrategies.ts
+++ src/Misc/PerformanceViewer/performanceViewerCollectionStrategies.ts
@@ -1,7 +1,7 @@
 import { EngineInstrumentation } from "../../Instrumentation/engineInstrumentation";
 import { Scene } from "../../scene";
 import { PrecisionDate } from "../precisionDate";
-
+declare var ComputePressureObserver: any;
 /**
  * Defines the general structure of what is necessary for a collection strategy.
  */
@@ -21,9 +21,6 @@ export interface IPerfViewerCollectionStrategy {
 }
 // Dispose which does nothing.
 const defaultDisposeImpl = () => {};
-
-// Temporary until implemented all getDatas.
-const defaultGetDataImpl = () => 0;
 
 /**
  * Initializer callback for a strategy
@@ -53,11 +50,26 @@ export class PerfCollectionStrategy {
      * @returns the initializer for the cpu utilization strategy
      */
     public static CpuStrategy(): PerfStrategyInitialization {
+        let observer: any;
+        let value: number = 0;
+        function callback(update: any) {
+            value = update.cpuUtilization;
+        }
+          
+          
+        if ('ComputePressureObserver' in window) {
+            observer = new ComputePressureObserver(callback, {
+                // Thresholds divide the interval [0.0 .. 1.0] into ranges.
+                cpuUtilizationThresholds: [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
+            });
+            observer.observe && observer.observe();
+        }
+
         return () => {
             return {
                 id: "cpu utilization",
-                getData: defaultGetDataImpl,
-                dispose: defaultDisposeImpl,
+                getData: () => value,
+                dispose: () => observer?.unobserve && observer?.unobserve(),
             };
         };
     }
