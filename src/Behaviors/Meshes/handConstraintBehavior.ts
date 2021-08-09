f03f0574a6b092676d4d0aff55bf86da3b373100
rickfromwork <msft_richard@outlook.com>
2021-08-09 14:55:10 -0700

    Adding eye tracking to hand constraint
    Former-commit-id: 3cfd46f7aebd14e4ac7afe9beb3dabdf97667f3e

--- src/Behaviors/Meshes/handConstraintBehavior.ts
+++ src/Behaviors/Meshes/handConstraintBehavior.ts
@@ -1,12 +1,14 @@
 import { TransformNode } from "../../Meshes/transformNode";
 import { Nullable } from "../../types";
 import { WebXRFeatureName } from "../../XR/webXRFeaturesManager";
+import { WebXREyeTracking } from "../../XR/features/WebXREyeTracking";
 import { WebXRHandTracking, XRHandJoint } from "../../XR/features/WebXRHandTracking";
 import { WebXRExperienceHelper } from "../../XR/webXRExperienceHelper";
 import { Behavior } from "../behavior";
 import { Observer } from "../../Misc/observable";
 import { Scene } from "../../scene";
 import { Quaternion, TmpVectors, Vector3 } from "../../Maths/math.vector";
+import { Ray } from "../../Culling/ray";
 
 /**
  * Zones around the hand
@@ -45,15 +47,56 @@ export enum HandConstraintOrientation {
 }
 
 /**
+ * Orientations for the hand zones and for the attached node
+ */
+export enum HandConstraintVisibility {
+    /**
+     * Constraint is always visible
+     */
+    ALWAYS_VISIBLE,
+    /**
+     * Constraint is only visible when the palm is up
+     */
+    PALM_UP,
+    /**
+     * Constraint is only visible when the user is looking at the constraint.
+     * Uses XR Eye Tracking if enabled/available, otherwise uses camera direction
+     */
+    GAZE_FOCUS,
+    /**
+     * Constraint is only visible when the palm is up and the user is looking at it
+     */
+    PALM_AND_GAZE,
+}
+
+/**
  * Hand constraint behavior that makes the attached `TransformNode` follow hands in XR experiences.
  * @since 5.0.0
  */
 export class HandConstraintBehavior implements Behavior<TransformNode> {
     private _scene: Scene;
     private _node: TransformNode;
+    private _eyeTracking: Nullable<WebXREyeTracking>;
     private _handTracking: Nullable<WebXRHandTracking>;
     private _sceneRenderObserver: Nullable<Observer<Scene>> = null;
     private _zoneAxis: { [id: number]: Vector3 } = {};
+
+    /**
+     * Sets the HandConstraintVisibility level for the hand constraint
+     */
+    public handConstraintVisibility: HandConstraintVisibility = HandConstraintVisibility.ALWAYS_VISIBLE;
+
+    /**
+     * A number from -1 to 1 that determines how 'upright' the palm must be before the attached node is enabled.
+     * Used with HandConstraintVisibility.PALM_UP
+     */
+    public palmUpStrictness: number = 0.9;
+
+    /**
+     * The radius around the TransformNode the user must look wiithin for the attached node to be enabled.
+     * Used with HandConstraintVisibility.GAZE_FOCUS
+     */
+    public gazeProximity: number = 1;
 
     /**
      * Offset distance from the hand in meters
@@ -99,6 +142,16 @@ export class HandConstraintBehavior implements Behavior<TransformNode> {
     /** gets or sets behavior's name */
     public get name() {
         return "HandConstraint";
+    }
+
+    /** Enable the behavior */
+    public enable() {
+        this._node.setEnabled(true);
+    }
+
+    /** Disable the behavior */
+    public disable() {
+        this._node.setEnabled(false);
     }
 
     private _getHandPose() {
@@ -162,7 +215,7 @@ export class HandConstraintBehavior implements Behavior<TransformNode> {
         }
 
         let lastTick = Date.now();
-        this._scene.onBeforeRenderObservable.add(() => {
+        this._sceneRenderObserver = this._scene.onBeforeRenderObservable.add(() => {
             const pose = this._getHandPose();
 
             this._node.reservedDataStore = this._node.reservedDataStore || {};
@@ -214,8 +267,58 @@ export class HandConstraintBehavior implements Behavior<TransformNode> {
                 this._node.reservedDataStore.nearInteraction.excludedControllerId = pose.controllerId;
             }
 
+            this._setVisibility();
+
             lastTick = Date.now();
         });
+    }
+
+    private _setVisibility() {
+        let palmVisible = true;
+        let gazeVisible = true;
+
+        if (this.handConstraintVisibility === HandConstraintVisibility.GAZE_FOCUS ||
+            this.handConstraintVisibility === HandConstraintVisibility.PALM_AND_GAZE) {
+            gazeVisible = false;
+            let gaze: Ray | undefined;
+            if (this._eyeTracking) {
+                gaze = this._eyeTracking.getEyeGaze()!;
+            }
+            if (!gaze) {
+                const camera = this._scene.activeCamera;
+                gaze = camera?.getForwardRay();
+            }
+
+            if (gaze) {
+                const behaviorPosition = this._node.position;
+                const rayPos = gaze.origin;
+                const rayPos2 = gaze.origin.add(gaze.direction);
+                const rayPosToPoint = rayPos.subtract(behaviorPosition);
+                const rayPos2ToPoint = rayPos2.subtract(behaviorPosition);
+                const distance = rayPosToPoint.cross(rayPos2ToPoint).length();
+
+                if (distance < this.gazeProximity) {
+                    gazeVisible = true;
+                }
+            }
+        }
+
+        if (this.handConstraintVisibility === HandConstraintVisibility.PALM_UP ||
+            this.handConstraintVisibility === HandConstraintVisibility.PALM_AND_GAZE) {
+            palmVisible = false;
+
+            const pose = this._getHandPose();
+            if (pose) {
+                const palmDirection = Vector3.Up();
+                palmDirection.rotateByQuaternionToRef(pose.quaternion, palmDirection);
+
+                if (Vector3.Dot(palmDirection, Vector3.UpReadOnly) > this.palmUpStrictness) {
+                    palmVisible = true;
+                }
+            }
+        }
+
+        this._node.setEnabled(palmVisible && gazeVisible);
     }
 
     /**
@@ -230,6 +333,7 @@ export class HandConstraintBehavior implements Behavior<TransformNode> {
      * @param xr xr experience
      */
     public linkToXRExperience(xr: WebXRExperienceHelper) {
+        this._eyeTracking = xr.featuresManager.getEnabledFeature(WebXRFeatureName.EYE_TRACKING) as WebXREyeTracking;
         this._handTracking = xr.featuresManager.getEnabledFeature(WebXRFeatureName.HAND_TRACKING) as WebXRHandTracking;
     }
 }
