4abde5e7c0d499774b0503744d5ed0aafbe94a78
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2021-08-25 05:05:06 -0700

    Added awaitability, allowing coroutines to pause themselves.

--- src/Misc/observableCoroutine.ts
+++ src/Misc/observableCoroutine.ts
@@ -1,19 +1,21 @@
 import { Nullable } from "../types";
 import { Observable } from "./observable";
 
+type Coroutine = Iterator<void | Promise<void>, void, void>;
+
 declare module "./observable" {
     export interface Observable<T> {
         /**
          * Internal list of iterators and promise resolvers associated with coroutines.
          */
-        coroutineIterators: Nullable<Array<{ iterator: Iterator<void, void, void>, resolver: () => void, rejecter: () => void }>>;
+        coroutineIterators: Nullable<Array<{ iterator: Coroutine, resolver: () => void, rejecter: () => void, paused: boolean }>>;
 
         /**
          * Runs a coroutine asynchronously on this observable
          * @param coroutineIterator the iterator resulting from having started the coroutine
          * @returns a promise which will be resolved when the coroutine finishes or rejected if the coroutine is cancelled
          */
-        runCoroutineAsync(coroutineIterator: Iterator<void, void, void>): Promise<void>;
+        runCoroutineAsync(coroutineIterator: Coroutine): Promise<void>;
 
         /**
          * Cancels all coroutines currently running on this observable
@@ -22,13 +24,27 @@ declare module "./observable" {
     }
 }
 
-Observable.prototype.runCoroutineAsync = function (coroutineIterator: Iterator<void, void, void>): Promise<void> {
+Observable.prototype.runCoroutineAsync = function (coroutineIterator: Coroutine): Promise<void> {
     if (!this.coroutineIterators) {
         this.coroutineIterators = [];
 
         this.add(() => {
             for (let idx = this.coroutineIterators!.length - 1; idx >= 0; --idx) {
-                if (this.coroutineIterators![idx].iterator.next().done) {
+                if (this.coroutineIterators![idx].paused) {
+                    continue;
+                }
+
+                const next = this.coroutineIterators![idx].iterator.next();
+
+                if (next.value) {
+                    const coroutine = this.coroutineIterators![idx];
+                    coroutine.paused = true;
+                    next.value.then(() => {
+                        coroutine.paused = false;
+                    });
+                }
+
+                if (next.done) {
                     this.coroutineIterators![idx].resolver();
                     this.coroutineIterators!.splice(idx, 1);
                 }
@@ -40,7 +56,8 @@ Observable.prototype.runCoroutineAsync = function (coroutineIterator: Iterator<v
         this.coroutineIterators?.push({
             iterator: coroutineIterator,
             resolver: resolver,
-            rejecter: rejecter
+            rejecter: rejecter,
+            paused: false
         });
     });
 };
