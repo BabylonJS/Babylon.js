8e9c123cdf4a0d51b7909fef07217f26fa63358c
Popov72 <github@evpopov.com>
2021-09-03 13:39:01 +0200

    Implement setSampler in WebGPU

--- src/Engines/WebGPU/Extensions/engine.sampler.ts
+++ src/Engines/WebGPU/Extensions/engine.sampler.ts
@@ -4,5 +4,5 @@ import { Nullable } from "../../../types";
 declare type Sampler = import("../../../Materials/Textures/sampler").Sampler;
 
 WebGPUEngine.prototype.setSampler = function (name: string, sampler: Nullable<Sampler>): void {
-
+    this._currentMaterialContext?.setSampler(name, sampler);
 };
