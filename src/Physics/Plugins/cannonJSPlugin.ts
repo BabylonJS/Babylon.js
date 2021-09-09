edb492e79ced5d1cfe66349748a7694b18707a5e
Cedric Guillemet <ceguille@microsoft.com>
2021-09-09 15:28:45 +0200

    Fix heightmap creation for cannonJS

--- src/Physics/Plugins/cannonJSPlugin.ts
+++ src/Physics/Plugins/cannonJSPlugin.ts
@@ -525,7 +525,7 @@ export class CannonJSPlugin implements IPhysicsEnginePlugin {
             }
 
             //calculate the new center using a pivot (since this.BJSCANNON.js doesn't center height maps)
-            const p = Matrix.Translation(boundingInfo.boundingBox.extendSizeWorld.x, 0, -boundingInfo.boundingBox.extendSizeWorld.z);
+            const p = Matrix.Translation(-boundingInfo.boundingBox.extendSizeWorld.x, 0, -boundingInfo.boundingBox.extendSizeWorld.z);
             mesh.setPreTransformMatrix(p);
             mesh.computeWorldMatrix(true);
 
