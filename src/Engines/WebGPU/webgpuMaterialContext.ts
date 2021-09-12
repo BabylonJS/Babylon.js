1b584616d0cb815633d3cce078c4ff28a479fb6b
Popov72 <github@evpopov.com>
2021-09-12 18:42:05 +0200

    Don't lookup bind group cache if material context is not dirty

--- src/Engines/WebGPU/webgpuMaterialContext.ts
+++ src/Engines/WebGPU/webgpuMaterialContext.ts
@@ -25,52 +25,79 @@ export class WebGPUMaterialContext implements IMaterialContext {
     private static _Counter = 0;
 
     public uniqueId: number;
-    public numFloatTextures: number;
-    public numExternalTextures: number;
+    public isDirty: boolean;
     public samplers: { [name: string]: Nullable<IWebGPUMaterialContextSamplerCache> };
     public textures: { [name: string]: Nullable<IWebGPUMaterialContextTextureCache> };
     public uniformBuffers: { [name: string]: WebGPUDataBuffer };
 
+    public bindGroups: GPUBindGroup[]; // cache of the bind groups. Will be reused for the next draw if isDirty=false
+
+    public get forceBindGroupCreation() {
+        // If there is at least one external texture to bind, we must recreate the bind groups each time
+        // because we need to retrieve a new texture each frame (by calling device.importExternalTexture)
+        return this._numExternalTextures > 0;
+    }
+
+    public get hasFloatTextures() {
+        return this._numFloatTextures > 0;
+    }
+
+    protected _numFloatTextures: number;
+    protected _numExternalTextures: number;
+
     constructor() {
+        this.uniqueId = WebGPUMaterialContext._Counter++;
+        this.reset();
+    }
+
+    public reset(): void {
         this.samplers = {};
         this.textures = {};
         this.uniformBuffers = {};
-        this.uniqueId = WebGPUMaterialContext._Counter++;
-        this.numFloatTextures = 0;
-        this.numExternalTextures = 0;
+        this.isDirty = true;
+        this._numFloatTextures = 0;
+        this._numExternalTextures = 0;
     }
 
     public setSampler(name: string, sampler: Nullable<Sampler>): void {
         let samplerCache = this.samplers[name];
+        let currentHashCode = -1;
         if (!samplerCache) {
             this.samplers[name] = samplerCache = { sampler, hashCode: 0 };
+        } else {
+            currentHashCode = samplerCache.hashCode;
         }
 
         samplerCache.sampler = sampler;
         samplerCache.hashCode = sampler ? WebGPUCacheSampler.GetSamplerHashCode(sampler) : 0;
+
+        this.isDirty ||= currentHashCode !== samplerCache.hashCode;
     }
 
     public setTexture(name: string, texture: Nullable<InternalTexture | ExternalTexture>): void {
         let textureCache = this.textures[name];
+        let currentTextureId = -1;
         if (!textureCache) {
             this.textures[name] = textureCache = { texture, isFloatTexture: false, isExternalTexture: false };
+        } else {
+            currentTextureId = textureCache.texture?.uniqueId ?? -1;
         }
 
         if (textureCache.isExternalTexture) {
-            this.numExternalTextures--;
+            this._numExternalTextures--;
         }
         if (textureCache.isFloatTexture) {
-            this.numFloatTextures--;
+            this._numFloatTextures--;
         }
 
         if (texture) {
             textureCache.isFloatTexture = texture.type === Constants.TEXTURETYPE_FLOAT;
             textureCache.isExternalTexture = ExternalTexture.IsExternalTexture(texture);
             if (textureCache.isFloatTexture) {
-                this.numFloatTextures++;
+                this._numFloatTextures++;
             }
             if (textureCache.isExternalTexture) {
-                this.numExternalTextures++;
+                this._numExternalTextures++;
             }
         } else {
             textureCache.isFloatTexture = false;
@@ -78,9 +105,13 @@ export class WebGPUMaterialContext implements IMaterialContext {
         }
 
         textureCache.texture = texture;
+
+        this.isDirty ||= currentTextureId !== (texture?.uniqueId ?? -1);
     }
 
     public setUniformBuffer(name: string, buffer: WebGPUDataBuffer): void {
+        this.isDirty ||= buffer.uniqueId !== this.uniformBuffers[name]?.uniqueId;
+
         this.uniformBuffers[name] = buffer;
     }
 }
