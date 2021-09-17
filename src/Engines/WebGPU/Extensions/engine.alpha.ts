13fe1019bdef94dea4c1ba7b8de3865b9e35a1b0
Popov72 <github@evpopov.com>
2021-09-17 15:37:00 +0200

    Add missing alpha modes to WebGPU
    Former-commit-id: c1df80b3c276896dd36436fb850868861456acb2

--- src/Engines/WebGPU/Extensions/engine.alpha.ts
+++ src/Engines/WebGPU/Extensions/engine.alpha.ts
@@ -1,3 +1,4 @@
+import { Constants } from "../../constants";
 import { Engine } from "../../engine";
 import { WebGPUEngine } from "../../webgpuEngine";
 
@@ -7,57 +8,87 @@ WebGPUEngine.prototype.setAlphaMode = function (mode: number, noDepthWriteChange
     }
 
     switch (mode) {
-        case Engine.ALPHA_DISABLE:
+        case Constants.ALPHA_DISABLE:
             this._alphaState.alphaBlend = false;
             break;
-        case Engine.ALPHA_PREMULTIPLIED:
+        case Constants.ALPHA_PREMULTIPLIED:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE_MINUS_SRC_ALPHA, this._gl.ONE, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(1, 0x0303, 1, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_PREMULTIPLIED_PORTERDUFF:
+        case Constants.ALPHA_PREMULTIPLIED_PORTERDUFF:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE_MINUS_SRC_ALPHA, this._gl.ONE, this._gl.ONE_MINUS_SRC_ALPHA);
             this._alphaState.setAlphaBlendFunctionParameters(1, 0x0303, 1, 0x0303);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_COMBINE:
+        case Constants.ALPHA_COMBINE:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.SRC_ALPHA, this._gl.ONE_MINUS_SRC_ALPHA, this._gl.ONE, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(0x0302, 0x0303, 1, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_ONEONE:
+        case Constants.ALPHA_ONEONE:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE, this._gl.ZERO, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(1, 1, 0, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_ADD:
+        case Constants.ALPHA_ADD:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.SRC_ALPHA, this._gl.ONE, this._gl.ZERO, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(0x0302, 1, 0, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_SUBTRACT:
+        case Constants.ALPHA_SUBTRACT:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.ZERO, this._gl.ONE_MINUS_SRC_COLOR, this._gl.ONE, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(0, 0x0301, 1, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_MULTIPLY:
+        case Constants.ALPHA_MULTIPLY:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.DST_COLOR, this._gl.ZERO, this._gl.ONE, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(0x0306, 0, 1, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_MAXIMIZED:
+        case Constants.ALPHA_MAXIMIZED:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.SRC_ALPHA, this._gl.ONE_MINUS_SRC_COLOR, this._gl.ONE, this._gl.ONE);
             this._alphaState.setAlphaBlendFunctionParameters(0x0302, 0x0301, 1, 1);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_INTERPOLATE:
+        case Constants.ALPHA_INTERPOLATE:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.CONSTANT_COLOR, this._gl.ONE_MINUS_CONSTANT_COLOR, this._gl.CONSTANT_ALPHA, this._gl.ONE_MINUS_CONSTANT_ALPHA);
             this._alphaState.setAlphaBlendFunctionParameters(0x8001, 0x8002, 0x8003, 0x8004);
             this._alphaState.alphaBlend = true;
             break;
-        case Engine.ALPHA_SCREENMODE:
+        case Constants.ALPHA_SCREENMODE:
             // this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE_MINUS_SRC_COLOR, this._gl.ONE, this._gl.ONE_MINUS_SRC_ALPHA);
             this._alphaState.setAlphaBlendFunctionParameters(1, 0x0301, 1, 0x0303);
+            this._alphaState.alphaBlend = true;
+            break;
+        case Constants.ALPHA_ONEONE_ONEONE:
+            //this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE, this._gl.ONE, this._gl.ONE);
+            this._alphaState.setAlphaBlendFunctionParameters(1, 1, 1, 1);
+            this._alphaState.alphaBlend = true;
+            break;
+        case Constants.ALPHA_ALPHATOCOLOR:
+            //this._alphaState.setAlphaBlendFunctionParameters(this._gl.DST_ALPHA, this._gl.ONE, this._gl.ZERO, this._gl.ZERO);
+            this._alphaState.setAlphaBlendFunctionParameters(0x0304, 1, 0, 0);
+            this._alphaState.alphaBlend = true;
+            break;
+        case Constants.ALPHA_REVERSEONEMINUS:
+            //this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE_MINUS_DST_COLOR, this._gl.ONE_MINUS_SRC_COLOR, this._gl.ONE_MINUS_DST_ALPHA, this._gl.ONE_MINUS_SRC_ALPHA);
+            this._alphaState.setAlphaBlendFunctionParameters(0x0307, 0x0301, 0x0305, 0x0303);
+            this._alphaState.alphaBlend = true;
+            break;
+        case Constants.ALPHA_SRC_DSTONEMINUSSRCALPHA:
+            //this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE_MINUS_SRC_ALPHA, this._gl.ONE, this._gl.ONE_MINUS_SRC_ALPHA);
+            this._alphaState.setAlphaBlendFunctionParameters(1, 0x0303, 1, 0x0303);
+            this._alphaState.alphaBlend = true;
+            break;
+        case Constants.ALPHA_ONEONE_ONEZERO:
+            //this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE, this._gl.ONE, this._gl.ONE, this._gl.ZERO);
+            this._alphaState.setAlphaBlendFunctionParameters(1, 1, 1, 0);
+            this._alphaState.alphaBlend = true;
+            break;
+        case Constants.ALPHA_EXCLUSION:
+            //this._alphaState.setAlphaBlendFunctionParameters(this._gl.ONE_MINUS_DST_COLOR, this._gl.ONE_MINUS_SRC_COLOR, this._gl.ZERO, this._gl.ONE);
+            this._alphaState.setAlphaBlendFunctionParameters(0x0307, 0x0301, 0, 1);
             this._alphaState.alphaBlend = true;
             break;
     }
