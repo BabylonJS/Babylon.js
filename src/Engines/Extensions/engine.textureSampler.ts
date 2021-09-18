9630abad169c18894ab249c2abcb5c8c78e1c6b6
Popov72 <github@evpopov.com>
2021-09-18 14:03:44 +0200

    Fix TextureSampler naming
    Former-commit-id: 0085996a09ab61e9676f0328cbd351024282827f

--- src/Engines/Extensions/engine.textureSampler.ts
+++ src/Engines/Extensions/engine.textureSampler.ts
@@ -1,19 +1,19 @@
 import { ThinEngine } from "../../Engines/thinEngine";
 import { Nullable } from "../../types";
 
-declare type Sampler = import("../../Materials/Textures/textureSampler").TextureSampler;
+declare type TextureSampler = import("../../Materials/Textures/textureSampler").TextureSampler;
 
 declare module "../../Engines/thinEngine" {
     export interface ThinEngine {
         /**
-         * Sets a sampler to the according uniform.
+         * Sets a texture sampler to the according uniform.
          * @param name The name of the uniform in the effect
          * @param sampler The sampler to apply
          */
-        setSampler(name: string, sampler: Nullable<Sampler>): void;
+        setTextureSampler(name: string, sampler: Nullable<TextureSampler>): void;
     }
 }
 
-ThinEngine.prototype.setSampler = function (name: string, sampler: Nullable<Sampler>): void {
-    throw new Error("setSampler: This engine does not support separate sampler objects!");
+ThinEngine.prototype.setTextureSampler = function (name: string, sampler: Nullable<TextureSampler>): void {
+    throw new Error("setTextureSampler: This engine does not support separate texture sampler objects!");
 };
