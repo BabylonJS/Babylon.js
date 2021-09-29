34e265a7f0719c0dd01fed9e16cd016b27027ee0
Raanan Weber <1381702+RaananW@users.noreply.github.com>
2021-09-29 11:54:23 +0200

    Provide the preprocessed URL to the error handler
    If the URL was changed by the preprocessing function the old URL will be delivered when the error occurs.
    
    Former-commit-id: d6aa5d86587dcd25b1250e033cba2d8cc79df6dd

--- src/Misc/fileTools.ts
+++ src/Misc/fileTools.ts
@@ -205,8 +205,8 @@ export class FileTools {
             img.removeEventListener("error", errorHandler);
 
             if (onError) {
-                const inputText = input.toString();
-                onError("Error while trying to load image: " + (inputText.length < 32 ? inputText : inputText.slice(0, 32) + "..."), err);
+                const inputText = url || input.toString();
+                onError(`Error while trying to load image: ${inputText}`, err);
             }
 
             if (usingObjectURL && img.src) {
