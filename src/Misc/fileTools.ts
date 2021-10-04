50592b424e5e36175c52d9645914bb7c2fa36ca3
Raanan Weber <raweber@microsoft.com>
2021-10-04 15:49:12 +0200

    reinstate fileTools

--- src/Misc/fileTools.ts
+++ src/Misc/fileTools.ts
@@ -590,7 +590,61 @@ const initSideEffects = () => {
     ShaderProcessor._FileToolsLoadFile = LoadFile;
 };
 
-initSideEffects();
+/**
+ * Backwards compatibility.
+ * @hidden
+ * @deprecated
+ */
+export const FileTools = {
+    DecodeBase64UrlToBinary,
+    DecodeBase64UrlToString,
+    DefaultRetryStrategy: FileToolsOptions.DefaultRetryStrategy,
+    BaseUrl: FileToolsOptions.BaseUrl,
+    CorsBehavior: FileToolsOptions.CorsBehavior,
+    PreprocessUrl: FileToolsOptions.PreprocessUrl,
+    IsBase64DataUrl,
+    IsFileURL,
+    LoadFile,
+    LoadImage,
+    ReadFile,
+    RequestFile,
+    SetCorsBehavior,
+};
 
-import { _injectLTS } from './fileTools.lts';
-_injectLTS();
+Object.defineProperty(FileTools, "DefaultRetryStrategy", {
+    get: function (this: null) {
+        return FileToolsOptions.DefaultRetryStrategy;
+    },
+    set: function (this: null, value: (url: string, request: WebRequest, retryIndex: number) => number) {
+        FileToolsOptions.DefaultRetryStrategy = value;
+    }
+});
+
+Object.defineProperty(FileTools, "BaseUrl", {
+    get: function (this: null) {
+        return FileToolsOptions.BaseUrl;
+    },
+    set: function (this: null, value: string) {
+        FileToolsOptions.BaseUrl = value;
+    }
+});
+
+Object.defineProperty(FileTools, "PreprocessUrl", {
+    get: function (this: null) {
+        return FileToolsOptions.PreprocessUrl;
+    },
+    set: function (this: null, value: (url: string) => string) {
+        FileToolsOptions.PreprocessUrl = value;
+    }
+});
+
+Object.defineProperty(FileTools, "CorsBehavior", {
+    get: function (this: null) {
+        return FileToolsOptions.CorsBehavior;
+    },
+    set: function (this: null, value: string | ((url: string | string[]) => string)) {
+        FileToolsOptions.CorsBehavior = value;
+    }
+});
+
+initSideEffects();
