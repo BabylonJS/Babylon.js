028a7e75423ec4226bd28e3be80fb7175129607a
Pamela Wolf <pawolf@microsoft.com>
2021-10-05 09:24:45 -0700

    grid improvements

--- guiEditor/src/guiNodeTools.ts
+++ guiEditor/src/guiNodeTools.ts
@@ -114,8 +114,8 @@ export class GUINodeTools {
             case "Grid":
                 element = new Grid("Grid");
                 element.isHighlighted = true;
-                element.addColumnDefinition(0.5);
-                element.addRowDefinition(0.5);
+                element.addColumnDefinition(1.0);
+                element.addRowDefinition(1.0);
                 element.isPointerBlocker = true;
                 return element;
             case "DisplayGrid":
