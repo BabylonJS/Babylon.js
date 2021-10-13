b6a1343b68e34283b6f510dc7a6e75947fca34a2
Darragh Burke <darraghjburke@gmail.com>
2021-10-13 13:23:09 -0700

    set URL to null if texture edited+ base64 encode

--- src/Materials/Textures/texture.ts
+++ src/Materials/Textures/texture.ts
@@ -770,7 +770,7 @@ export class Texture extends BaseTexture {
                 serializationObject.name = serializationObject.name.replace("data:", "");
             } else if (this.url && StartsWith(this.url, "data:") && this._buffer instanceof Uint8Array) {
                 serializationObject.base64String = "data:image/png;base64," + EncodeArrayBufferToBase64(this._buffer);
-            } else if (Texture.ForceSerializeBuffers || (this.url && StartsWith(this.url, "blob:"))) {
+            } else if (Texture.ForceSerializeBuffers || (this.url && StartsWith(this.url, "blob:")) || !this.url) {
                 serializationObject.base64String = !this._engine || this._engine._features.supportSyncTextureRead ? GenerateBase64StringFromTexture(this) : GenerateBase64StringFromTextureAsync(this);
             }
         }
