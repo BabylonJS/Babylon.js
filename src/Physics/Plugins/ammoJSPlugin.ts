47eee8a99a77c368e97871b0b581c05a0a0fcf0d
Cedric Guillemet <ceguille@microsoft.com>
2021-10-27 17:58:58 +0200

    Fix deltaPositon with ammoJS

--- src/Physics/Plugins/ammoJSPlugin.ts
+++ src/Physics/Plugins/ammoJSPlugin.ts
@@ -57,6 +57,7 @@ export class AmmoJSPlugin implements IPhysicsEnginePlugin {
     private _tmpAmmoVectorRCB: any;
     private _raycastResult: PhysicsRaycastResult;
     private _tmpContactPoint = new Vector3();
+    private _tmpVec3 = new Vector3();
 
     private static readonly DISABLE_COLLISION_FLAG = 4;
     private static readonly KINEMATIC_FLAG = 2;
@@ -460,6 +461,16 @@ export class AmmoJSPlugin implements IPhysicsEnginePlugin {
                 if (impostor.type == PhysicsImpostor.NoImpostor && !colShape.getChildShape) {
                     body.setCollisionFlags(body.getCollisionFlags() | AmmoJSPlugin.DISABLE_COLLISION_FLAG);
                 }
+
+                // compute delta position: compensate the difference between shape center and mesh origin
+                impostor.object.computeWorldMatrix(true);
+                const boundingInfo = impostor.object.getBoundingInfo();
+                this._tmpVec3.copyFrom(impostor.object.getAbsolutePosition());
+                this._tmpVec3.subtractInPlace(boundingInfo.boundingBox.centerWorld);
+                this._tmpVec3.x /= impostor.object.scaling.x;
+                this._tmpVec3.y /= impostor.object.scaling.y;
+                this._tmpVec3.z /= impostor.object.scaling.z;
+                impostor.setDeltaPosition(this._tmpVec3);
 
                 let group = impostor.getParam("group");
                 let mask = impostor.getParam("mask");
