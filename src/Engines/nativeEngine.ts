b5544844f9e9d56d47cc1b232e0c884f23169971
Pamela Wolf <pawolf@microsoft.com>
2021-11-02 12:57:20 -0700

    removing from native

--- src/Engines/nativeEngine.ts
+++ src/Engines/nativeEngine.ts
@@ -2842,10 +2842,4 @@ export class NativeEngine extends Engine {
                 throw new Error(`Unsupported attribute type: ${type}.`);
         }
     }
-
-    public getFontOffset(font: string): { ascent: number, height: number, descent: number } {
-        // TODO
-        var result = { ascent: 0, height: 0, descent: 0 };
-        return result;
-    }
 }
