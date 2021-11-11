81ae9ce8220274864fe5a6874966e89c29fabee3
Carolina Herbster <carolhmj@gmail.com>
2021-11-11 14:47:59 -0300

    * Validate Enter keypress as a change in the clip length value.
    Former-commit-id: 4eb6ffd8589c4220673af3b332d87babaac9b459

--- inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/controls/textInputComponent.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/animations/curveEditor/controls/textInputComponent.tsx
@@ -19,22 +19,19 @@ interface ITextInputComponentState {
     isFocused: boolean;
 }
 
-export class TextInputComponent extends React.Component<
-ITextInputComponentProps,
-ITextInputComponentState
-> {
+export class TextInputComponent extends React.Component<ITextInputComponentProps, ITextInputComponentState> {
     private _lastKnownGoodValue: string;
 
     constructor(props: ITextInputComponentProps) {
         super(props);
 
-        this.state = { value: this.props.value, isFocused: false};
+        this.state = { value: this.props.value, isFocused: false };
     }
 
     private _onChange(value: string) {
         if (this.props.isNumber) {
             let valueAsNumber = parseFloat(value);
-            
+
             if (!isNaN(valueAsNumber)) {
                 if (this.props.onValueAsNumberChanged) {
                     this.props.onValueAsNumberChanged(valueAsNumber, true);
@@ -46,7 +43,7 @@ ITextInputComponentState
         }
 
         this._lastKnownGoodValue = value;
-        this.setState({value: value});
+        this.setState({ value: value });
     }
 
     private _onBlur() {
@@ -59,41 +56,54 @@ ITextInputComponentState
                 if (this.props.onValueAsNumberChanged) {
                     this.props.onValueAsNumberChanged(valueAsNumber, false);
                 }
-                this.setState({value: valueAsNumber.toString(), isFocused: false});
+                this.setState({ value: valueAsNumber.toString(), isFocused: false });
             } else {
-                this.setState({value: this._lastKnownGoodValue, isFocused: false});
+                this.setState({ value: this._lastKnownGoodValue, isFocused: false });
             }
             return;
         }
 
-        this.setState({isFocused: false});
+        this.setState({ isFocused: false });
     }
 
     private _onFocus() {
         this.props.context.focusedInput = true;
-        this.setState({isFocused: true});
+        this.setState({ isFocused: true });
     }
 
     shouldComponentUpdate(newProps: ITextInputComponentProps, newState: ITextInputComponentState) {
         if (newProps !== this.props) {
             newState.value = newProps.value;
         }
-        
+
         return true;
+    }
+
+    private _onKeyPress(evt: React.KeyboardEvent<HTMLInputElement>) {
+        if (evt.key === "Enter") {
+            let valueAsNumber = parseFloat(this.state.value);
+
+            if (!isNaN(valueAsNumber)) {
+                if (this.props.onValueAsNumberChanged) {
+                    this.props.onValueAsNumberChanged(valueAsNumber, false);
+                }
+            }
+        }
     }
 
     public render() {
         return (
-            <input 
+            <input
                 type="text"
                 title={this.props.tooltip}
                 onFocus={() => this._onFocus()}
                 onBlur={() => this._onBlur()}
-                className={"text-input" + (this.props.className ? " " + this.props.className : "")} 
-                onChange={evt => this._onChange(this.props.complement ? evt.target.value.replace(this.props.complement, "") : evt.target.value)}
+                className={"text-input" + (this.props.className ? " " + this.props.className : "")}
+                onChange={(evt) => this._onChange(this.props.complement ? evt.target.value.replace(this.props.complement, "") : evt.target.value)}
+                onKeyPress={(evt) => this._onKeyPress(evt)}
                 value={(this.state.value || "") + (!this.state.isFocused && this.props.complement ? this.props.complement : "")}
-                id={this.props.id}>
-            </input>
+                id={this.props.id}
+            ></input>
         );
     }
 }
