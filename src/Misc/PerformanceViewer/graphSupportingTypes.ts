e6430a986f661231b751d5bf5dc6abaab44fbf2b
Carolina Herbster <carolhmj@gmail.com>
2021-11-17 17:56:35 -0300

    * Linting and documentation.

--- src/Misc/PerformanceViewer/graphSupportingTypes.ts
+++ src/Misc/PerformanceViewer/graphSupportingTypes.ts
@@ -4,7 +4,13 @@ import { IPerfDatasets } from "../interfaces/iPerfViewer";
  * Defines a structure to hold max and min.
  */
 export interface IPerfMinMax {
+    /**
+     * min
+     */
     min: number;
+    /**
+     * max
+     */
     max: number;
 }
 
@@ -12,7 +18,13 @@ export interface IPerfMinMax {
  * Defines structure of the object which contains information related to panning.
  */
 export interface IPerfMousePanningPosition {
+    /**
+     * Starting x position of pan
+     */
     xPos: number;
+    /**
+     * Pan delta on the x axis
+     */
     delta: number;
 }
 
@@ -20,12 +32,24 @@ export interface IPerfMousePanningPosition {
  * Defines structure of the object which contains information regarding the bounds of each dataset we want to consider.
  */
 export interface IPerfIndexBounds {
+    /**
+     * starting bound of the dataset
+     */
     start: number;
+    /**
+     * max bound of the dataset
+     */
     end: number;
 }
 
 export interface IPerfLayoutSize {
+    /**
+     * layout width
+     */
     width: number;
+    /**
+     * layout height
+     */
     height: number;
 }
 
@@ -33,7 +57,13 @@ export interface IPerfLayoutSize {
  * Defines the structure of the meta object for the tooltip that appears when hovering over a performance graph!
  */
 export interface IPerfTooltip {
+    /**
+     * tooltip text
+     */
     text: string;
+    /**
+     * tooltip color
+     */
     color: string;
 }
 
@@ -41,7 +71,13 @@ export interface IPerfTooltip {
  * Defines the structure of a cache object used to store the result of measureText().
  */
 export interface IPerfTextMeasureCache {
+    /**
+     * longest text we've seen so far
+     */
     text: string;
+    /**
+     * width of that longest text
+     */
     width: number;
 }
 
@@ -49,9 +85,21 @@ export interface IPerfTextMeasureCache {
  * Defines a structure defining the available space in a drawable area.
  */
 export interface IGraphDrawableArea {
+    /**
+     * top
+     */
     top: number;
+    /**
+     * left
+     */
     left: number;
+    /**
+     * bottom
+     */
     bottom: number;
+    /**
+     * right
+     */
     right: number;
 }
 
@@ -59,7 +107,13 @@ export interface IGraphDrawableArea {
  * Defines the structure representing necessary ticker information.
  */
 export interface IPerfTicker extends IPerfMinMax {
+    /**
+     * ticker id
+     */
     id: string;
+    /**
+     * ticker text
+     */
     text: string;
 }
 
@@ -67,6 +121,9 @@ export interface IPerfTicker extends IPerfMinMax {
  * Defines what settings our canvas graphing service accepts
  */
 export interface ICanvasGraphServiceSettings {
+    /**
+     * from where we extract data
+     */
     datasets: IPerfDatasets;
 }
 
@@ -74,14 +131,32 @@ export interface ICanvasGraphServiceSettings {
  * Defines the structure representing the preprocessable tooltip information.
  */
 export interface ITooltipPreprocessedInformation {
+    /**
+     * pixel coordinates of a number on the x axis
+     */
     xForActualTimestamp: number;
+    /**
+     * number of tooltip items
+     */
     numberOfTooltipItems: number;
+    /**
+     * longest text we've seen so far
+     */
     longestText: string;
+    /**
+     * focused id
+     */
     focusedId: string;
 }
 
 export interface IPerfTooltipHoverPosition {
+    /**
+     * x hover position
+     */
     xPos: number;
+    /**
+     * y hover position
+     */
     yPos: number;
 }
 
@@ -89,8 +164,20 @@ export interface IPerfTooltipHoverPosition {
  * Defines the supported timestamp units.
  */
 export enum TimestampUnit {
+    /**
+     * Milliseconds
+     */
     Milliseconds = 0,
+    /**
+     * Seconds
+     */
     Seconds = 1,
+    /**
+     * Minutes
+     */
     Minutes = 2,
+    /**
+     * Hours
+     */
     Hours = 3,
 }
--- src/Misc/PerformanceViewer/performanceViewerSceneExtension.ts
+++ src/Misc/PerformanceViewer/performanceViewerSceneExtension.ts
@@ -12,19 +12,19 @@ declare module "../../scene" {
         /**
          * Standalone performance viewer element with a simplified view of perfomance independent of the inspector.
          */
-        _standalonePerfViewerCanvas : HTMLCanvasElement;
+        _standalonePerfViewerCanvas: HTMLCanvasElement;
         /**
          * Service that draws the performance graphs on the canvas
          */
-        _standalonePerfViewerService : CanvasGraphService;
+        _standalonePerfViewerService: CanvasGraphService;
         /**
          * Asks the service to update after the scene is rendered
          */
-        _standalonePerfViewerSceneObserver : Nullable<Observer<Scene>>;
+        _standalonePerfViewerSceneObserver: Nullable<Observer<Scene>>;
     }
 }
 
-Scene.prototype.getPerfCollector = function(this: Scene): PerformanceViewerCollector {
+Scene.prototype.getPerfCollector = function (this: Scene): PerformanceViewerCollector {
     if (!this._perfCollector) {
         this._perfCollector = new PerformanceViewerCollector(this);
     }
@@ -36,7 +36,7 @@ Scene.prototype.getPerfCollector = function(this: Scene): PerformanceViewerColle
  * Add the standalone performance viewer as a child to element parentElement
  * @param parentElement the HTML element to which the perf viewer will be appended.
  */
-Scene.prototype.addPerfViewer = function(parentElement : HTMLElement) {
+Scene.prototype.addPerfViewer = function (parentElement: HTMLElement) {
     if (!isEnabled) {
         Tools.Warn("This feature is not enabled yet.");
         return;
@@ -44,32 +44,32 @@ Scene.prototype.addPerfViewer = function(parentElement : HTMLElement) {
     this.getPerfCollector().start();
 
     this._standalonePerfViewerCanvas = document.createElement("canvas");
-    
+
     parentElement.appendChild(this._standalonePerfViewerCanvas);
 
-    this._standalonePerfViewerService = new CanvasGraphService(this._standalonePerfViewerCanvas, {datasets: this.getPerfCollector().datasets});
+    this._standalonePerfViewerService = new CanvasGraphService(this._standalonePerfViewerCanvas, { datasets: this.getPerfCollector().datasets });
     this._standalonePerfViewerSceneObserver = this.onAfterRenderObservable.add(() => {
         this._standalonePerfViewerService.update();
     });
 
     // Place the standalone perf viewer in the top left corner of the parent, but this should be
     // dependent on options
-    this._standalonePerfViewerCanvas.style.position = 'absolute';
-    this._standalonePerfViewerCanvas.style.top = '0px';
-    this._standalonePerfViewerCanvas.style.left = '0px';
-    this._standalonePerfViewerCanvas.style.fontSize = '10px';
+    this._standalonePerfViewerCanvas.style.position = "absolute";
+    this._standalonePerfViewerCanvas.style.top = "0px";
+    this._standalonePerfViewerCanvas.style.left = "0px";
+    this._standalonePerfViewerCanvas.style.fontSize = "10px";
 };
 
 /**
  * Remove performance viewer from parent and dispose of any of its resources.
  */
-Scene.prototype.removePerfViewer = function() {
+Scene.prototype.removePerfViewer = function () {
     if (!isEnabled) {
         Tools.Warn("This feature is not enabled yet.");
         return;
     }
     this.getPerfCollector().stop();
-    
+
     this.onAfterRenderObservable.remove(this._standalonePerfViewerSceneObserver);
     this._standalonePerfViewerSceneObserver = null;
 
