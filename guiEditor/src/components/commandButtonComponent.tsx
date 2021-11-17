396aab836338478facfa12a201294db9bcd936f3
Pamela Wolf <pawolf@microsoft.com>
2021-11-17 08:07:42 -0800

    fixing buttons

--- guiEditor/src/components/commandButtonComponent.tsx
+++ guiEditor/src/components/commandButtonComponent.tsx
@@ -7,6 +7,7 @@ interface ICommandButtonComponentProps {
     iconLabel? : string;
     isActive: boolean;
     onClick: () => void;
+    altStyle? : boolean;
 }
 
 export class CommandButtonComponent extends React.Component<ICommandButtonComponentProps> {    
@@ -16,10 +17,18 @@ export class CommandButtonComponent extends React.Component<ICommandButtonCompon
     }    
 
     public render() {
+
+        const divClassName = this.props.altStyle ? 
+        `command-button-alt ${this.props.isActive ? "active" : ""}` :
+        `command-button ${this.props.isActive ? "active" : ""}`
+
+        const iconClassName =
+        `command-button-icon ${this.props.isActive ? "active" : ""}`;
+
         return (
-            <div className={`command-button ${this.props.isActive ? "active" : ""}`} onClick={this.props.onClick} title={`${this.props.tooltip} ${this.props.shortcut ? " (" + this.props.shortcut + ")" : ""}`}>
-                <div className={`command-button-icon ${this.props.isActive ? "active" : ""}`}>
-                    <img src={this.props.icon} title={this.props.iconLabel} alt={this.props.iconLabel}  color="white" className={this.props.isActive ? "active" : ""}/>
+            <div className={divClassName} onClick={this.props.onClick} title={`${this.props.tooltip} ${this.props.shortcut ? " (" + this.props.shortcut + ")" : ""}`}>
+                <div className={iconClassName}>
+                    <img src={this.props.icon} title={this.props.iconLabel} alt={this.props.iconLabel} color="white" className={this.props.isActive ? "active" : ""}/>
                 </div>
                 <div className="command-label">
                     {this.props.tooltip}
