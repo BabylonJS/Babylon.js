e050f2d319f3c30b0dd7d5827213c7a6d4f0693e
Brian Zinn <github@wakeskate.com>
2021-12-01 22:09:52 -0800

    grid.dispose() - clean up observers and clear data structures
    Former-commit-id: 5da1ca324e1c8c13b9282dfe78281e1ddeaf92d5

--- tests/unit/babylon/gui/2D/controls/grid.tests.ts
+++ tests/unit/babylon/gui/2D/controls/grid.tests.ts
@@ -1,7 +1,7 @@
 /**
   * Tests grid state and behavior
   */
- describe('Grid', () => {
+describe('Grid', () => {
 
     const getFakeContext = (): BABYLON.ICanvasRenderingContext => {
         const fakeContext = sinon.stub() as any;
@@ -13,11 +13,12 @@
         fakeContext.beginPath = sinon.stub();
         fakeContext.rect = sinon.stub();
         fakeContext.clip = sinon.stub();
-        fakeContext.restore = () => {};
+        fakeContext.restore = () => { };
+
         return fakeContext;
     }
 
-    const getFakeAdvancedDynamicTexture = () : BABYLON.GUI.AdvancedDynamicTexture => {
+    const getFakeAdvancedDynamicTexture = (): BABYLON.GUI.AdvancedDynamicTexture => {
         const fakeADT = sinon.stub() as any;
         fakeADT.markAsDirty = sinon.stub();
         return fakeADT;
@@ -41,5 +42,19 @@
         // when changing a row definition the grid will mark itself as dirty
         valueAndUnit.value = 0.7;
         expect(subject.isDirty).to.be.true;
+    });
+
+    it('When grid is disposed it should clear rows/columns', () => {
+        const subject = new BABYLON.GUI.Grid();
+
+        subject.addRowDefinition(0.5);
+        subject.addColumnDefinition(0.5);
+
+        expect(subject.rowCount).equals(1);
+        expect(subject.columnCount).equal(1);
+
+        subject.dispose();
+        expect(subject.rowCount).equals(0, 'should have no rows after dispose');
+        expect(subject.columnCount).equals(0, 'should have no columns after dispose');
     });
 });
