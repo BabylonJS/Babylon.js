e1f2fa8290e6e3114285c9994733d789e12d8300
rogerdis <rogerdis@microsoft.com>
2021-12-15 16:52:53 -0800

    PR feedback

--- src/XR/native/nativeXRPlugin.ts
+++ src/XR/native/nativeXRPlugin.ts
@@ -1,13 +1,18 @@
 import { INative } from "../../Engines/Native/nativeInterfaces";
+import { Nullable } from "../../types";
 
-declare let _native: INative;
+declare const _native: INative;
 
 /** @hidden */
 export class NativeXRPlugin {
     private static _RegisteredTypes = new Map<string, any>();
+    private static _nativeObject: Nullable<INative> = null;
 
-    public static RegisterType<Type>(typeName: string, objectType: Type) {
-        this._RegisteredTypes.set(typeName, objectType);
+    public static RegisterType<Type>(typeName: string, constructor: Type) {
+        this._RegisteredTypes.set(typeName, constructor);
+        if (this._nativeObject) {
+            (this._nativeObject as any)[typeName] = constructor;
+        }
     }
 
     /**
@@ -15,8 +20,12 @@ export class NativeXRPlugin {
      * The goal is to improve performance by decreasing the number of jumps across the JS-native boundary and by using more performant types for transferrring data across the boundary.
      */
     public static ExtendNativeObject() {
+        if (!_native) {
+            throw Error("ExtendNativeObject should only be called once _native is defined.");
+        }
+        this._nativeObject = _native;
         this._RegisteredTypes.forEach((constructor, typeName) => {
-            (_native as any)[typeName] = constructor;
+            (this._nativeObject as any)[typeName] = constructor;
         });
     }
 }
