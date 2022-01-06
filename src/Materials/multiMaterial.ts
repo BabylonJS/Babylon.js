cb9bd6f9e641e52ec554d5d9240b7323e7233cbe
Darragh Burke <darraghjburke@gmail.com>
2022-01-06 15:24:16 -0800

    fix array name
    Former-commit-id: 29af42e808687629972725bf410d79b3030004a8

--- src/Materials/multiMaterial.ts
+++ src/Materials/multiMaterial.ts
@@ -264,7 +264,7 @@ export class MultiMaterial extends Material {
                 }
             });
         } else {
-            parsedMultiMaterial.materialsUniqueIds.forEach((subMatId: any) => {
+            parsedMultiMaterial.materials.forEach((subMatId: any) => {
                 if (subMatId) {
                     // If the same multimaterial is loaded twice, the 2nd multimaterial needs to reference the latest material by that id which
                     // is why this lookup should use getLastMaterialByID instead of getMaterialByID
