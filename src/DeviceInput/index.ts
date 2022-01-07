873d82c36515cd97a1aa40af9695d4536d07d5c6
Dave Solares <dasolare@microsoft.com>
2022-01-07 12:14:51 -0800

    PR Feedback

--- src/DeviceInput/index.ts
+++ src/DeviceInput/index.ts
@@ -5,7 +5,7 @@ export * from "./Helpers/eventFactory";
 export * from "./InputDevices/nativeDeviceInputSystem";
 export * from "./InputDevices/webDeviceInputSystem";
 
-export * from "./InputDevices/deviceSource"
+export * from "./InputDevices/deviceSource";
 export * from "./InputDevices/internalDeviceSourceManager";
 
 import "./InputDevices/deviceSourceManager";
