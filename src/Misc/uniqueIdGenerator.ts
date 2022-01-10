bb61125ace3ce87a94da159878f4f65f6b03fe92
Darragh Burke <darraghjburke@gmail.com>
2022-01-10 14:46:40 -0800

    undo change on wrong branch
    Former-commit-id: f8b66b23dd82ef8732d1cfe0a6f74d788524ed5b

--- src/Misc/uniqueIdGenerator.ts
+++ src/Misc/uniqueIdGenerator.ts
@@ -3,7 +3,7 @@
  */
 export class UniqueIdGenerator {
     // Statics
-    private static _UniqueIdCounter = 1;
+    private static _UniqueIdCounter = 0;
 
     /**
      * Gets an unique (relatively to the current scene) Id
