883e5c1719e88f471aeebb900888a501a0bf0a5b
Raanan Weber <Raanan.Weber@microsoft.com>
2022-02-01 13:18:32 +0100

    Mark VR classes as deprecated (not yet removing them)

--- src/Cameras/VR/webVRCamera.ts
+++ src/Cameras/VR/webVRCamera.ts
@@ -153,6 +153,7 @@ export interface WebVROptions {
 /**
  * This represents a WebVR camera.
  * The WebVR camera is Babylon's simple interface to interaction with Windows Mixed Reality, HTC Vive and Oculus Rift.
+ * @deprecated Use WebXR instead - https://doc.babylonjs.com/divingDeeper/webXR
  * @example https://doc.babylonjs.com/how_to/webvr_camera
  */
 export class WebVRFreeCamera extends FreeCamera implements PoseControlled {
