aeb20a11c79affe0031d5d0868c5092747f63973
Darragh Burke <darraghburke@microsoft.com>
2022-02-02 18:27:21 -0800

    color refactoring
    Former-commit-id: 2fc63816ca630e8630005d96e17beb027f475fa9

--- inspector/src/components/actionTabs/tabs/propertyGrids/materials/nodeMaterialPropertyGridComponent.tsx
+++ inspector/src/components/actionTabs/tabs/propertyGrids/materials/nodeMaterialPropertyGridComponent.tsx
@@ -124,7 +124,7 @@ export class NodeMaterialPropertyGridComponent extends React.Component<INodeMate
                     <Color4LineComponent
                         key={block.name}
                         label={block.name}
-                        target={block}
+                        targets={[block]}
                         propertyName="value"
                         onPropertyChangedObservable={this.props.onPropertyChangedObservable}
                     />
---
+++ sharedUiComponents/lines/targetsProxy.tsx
@@ -0,0 +1,40 @@
+import { PropertyChangedEvent } from "../propertyChangedEvent";
+import { Observable } from "babylonjs/Misc/observable";
+
+/**
+ * 
+ * @param propertyName the property that the input changes
+ * @param targets a list of selected targets
+ * @param defaultValue the value that should be returned when two targets have conflicting values
+ * @param setter an optional setter function to override the default setter behavior
+ * @returns a proxy object that can be passed as a target into the input
+ */
+export function makeTargetsProxy(targets: any[], defaultValue: any, onPropertyChangedObservable?: Observable<PropertyChangedEvent>) {
+    return new Proxy({}, {
+        get(_, name) {
+            if (targets.length === 0) return defaultValue;
+            const firstValue = targets[0][name];
+            for (const target of targets) {
+                if (target[name] !== firstValue) {
+                    return defaultValue;
+                }
+            }
+            return firstValue;
+        },
+        set(_, name, value, __) {
+            for(const target of targets) {
+                const initialValue = target[name];
+                target[name] = value;
+                if (onPropertyChangedObservable) {
+                    onPropertyChangedObservable.notifyObservers({
+                        object: target,
+                        property: name as string,
+                        value: target[name],
+                        initialValue
+                    });
+                }
+            }
+            return true;
+        }
+    })
+}
