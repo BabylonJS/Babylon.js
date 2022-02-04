8de5c1874d7b26606cb72067fff1fdd30a5fbf64
Cedric Guillemet <ceguille@microsoft.com>
2022-02-04 16:20:30 +0100

    updated breaking change edge case for instances & colorInstances

--- src/Materials/materialHelper.ts
+++ src/Materials/materialHelper.ts
@@ -163,6 +163,12 @@ export class MaterialHelper {
             changed = true;
         }
 
+        // ensure defines.INSTANCESCOLOR is not out of sync with instances
+        if (defines["INSTANCESCOLOR"] && !defines["INSTANCES"]) {
+            defines["INSTANCESCOLOR"] = false;
+            changed = true;
+        }
+
         if (defines["THIN_INSTANCES"] !== useThinInstances) {
             defines["THIN_INSTANCES"] = useThinInstances;
             changed = true;
