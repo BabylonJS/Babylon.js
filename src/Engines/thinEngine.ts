e74df5c28177cfbabbe46c202ce7dba24654566c
Raanan Weber <Raanan.Weber@microsoft.com>
2022-02-28 21:11:22 +0100

    5.0.0-rc.0
    Former-commit-id: 9ca9e30f7576cbd725bb9b770740a715ecd18b2b

--- src/Engines/thinEngine.ts
+++ src/Engines/thinEngine.ts
@@ -190,14 +190,14 @@ export class ThinEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@5.0.0-beta.12";
+        return "babylonjs@5.0.0-rc.0";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "5.0.0-beta.12";
+        return "5.0.0-rc.0";
     }
 
     /**
