4bd154db566ee43e282e80b4fdeb32d042d148bd
Raanan Weber <Raanan.Weber@microsoft.com>
2022-03-16 17:12:57 +0100

    reflector package
    Former-commit-id: b930f77d470c9eafc361ee4679d1f30d0afb40bb

--- packages/tools/reflector/src/index.ts
+++ packages/tools/reflector/src/index.ts
@@ -1,4 +1,7 @@
-import * as WebSocket from 'ws';
+import { WebSocketServer } from "ws";
+import { readFileSync } from "fs";
+import { createServer } from "https";
+import { resolve } from "path";
 
 interface WebSocketInfo extends WebSocket {
     _id?: number;
@@ -7,31 +10,38 @@ interface WebSocketInfo extends WebSocket {
 
 class Server {
     private _nextClientId = 0;
-    private _server: WebSocket.Server;
+    private _server: WebSocketServer;
 
-    public start(port: number): void {
-        console.log(`Starting reflector on port ${port}`);
-        this._server = new WebSocket.Server({ port: port });
+    public start(port: number, ssl?: boolean): void {
+        console.log(`Starting reflector on port ${port}, ssl: ${ssl}`);
+        const server =
+            ssl &&
+            createServer({
+                cert: readFileSync(resolve("./cert/cert.pem")),
+                key: readFileSync(resolve("./cert/key.pem")),
+            }).listen(1234) || undefined;
 
-        this._server.on("connection", (client: WebSocketInfo, request) => {
+        this._server = ssl ? new WebSocketServer({ server }) : new WebSocketServer({ port });
+
+        this._server.on("connection", (client: WebSocketInfo) => {
             client._id = this._nextClientId++;
             console.log(`Client ${client._id} connected`);
 
             this._checkClients();
 
-            client.on("message", (message: string) => {
-                console.log(`Received message from client ${client._id}: ${message.substr(0, 64)}`);
+            this._server.on("message", (message: string) => {
+                console.log(`Received message from client ${client._id}: ${message.substring(0, 64)}`);
 
                 if (!client._other) {
                     client.close(4001, "received message without two clients connected");
                     return;
                 }
 
-                console.log(`Sending message to client ${client._other._id}: ${message.substr(0, 64)}`);
+                console.log(`Sending message to client ${client._other._id}: ${message.substring(0, 64)}`);
                 client._other.send(message);
             });
 
-            client.on("close", (code, reason) => {
+            this._server.on("close", (code: any, reason: any) => {
                 console.log(`Client ${client._id} disconnected: ${code} ${reason}`);
                 if (client._other) {
                     delete client._other._other;
@@ -45,7 +55,7 @@ class Server {
 
     private _checkClients(): void {
         if (this._server.clients.size == 2) {
-            const clients = Array.from(this._server.clients.values()) as Array<WebSocketInfo>;
+            const clients = Array.from(this._server.clients.values()) as any as Array<WebSocketInfo>;
             clients[0]._other = clients[1];
             clients[1]._other = clients[0];
             this._broadcast("connected");
@@ -63,4 +73,5 @@ class Server {
 
 const server = new Server();
 const port = process.env.npm_config_port ? +process.env.npm_config_port : 1234;
-server.start(port);
+const ssl = process.env.reflector_ssl || process.argv[2] === "--ssl" ? true : false;
+server.start(port, ssl);
