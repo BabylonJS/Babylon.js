6e955d7f514d0207959b3be4e098212ccc3a0c9c
Raanan Weber <Raanan.Weber@microsoft.com>
2022-03-16 15:12:18 +0100

    remove legacy
    Former-commit-id: 8945b7b7d64e2be2de36d0fe37154a5bcdfa597b

--- packages/dev/loaders/src/legacy/legacy.ts
+++
@@ -1,6 +0,0 @@
-import "../index";
-export * from "./legacy-glTF";
-export * from "./legacy-glTF1";
-export * from "./legacy-glTF2";
-export * from "./legacy-objFileLoader";
-export * from "./legacy-stlFileLoader";
