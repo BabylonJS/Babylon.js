dd792a6e06dc11404edec7d6218d768761abb6ef
Darragh Burke <darraghburke@microsoft.com>
2022-03-28 14:46:40 -0700

    update inputs on unit changed

--- packages/dev/sharedUiComponents/src/lines/textInputLineComponent.tsx
+++ packages/dev/sharedUiComponents/src/lines/textInputLineComponent.tsx
@@ -63,7 +63,9 @@ export class TextInputLineComponent extends React.Component<ITextInputLineCompon
             return true;
         }
 
-        if (nextState.dragging != this.state.dragging) return true;
+        if (nextState.dragging != this.state.dragging || nextProps.unit !== this.props.unit || nextProps.unitLocked !== this.props.unitLocked) {
+            return true;
+        ]
 
         return false;
     }
