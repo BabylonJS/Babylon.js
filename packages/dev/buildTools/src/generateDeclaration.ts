d2c29a21976ce84f12173842c28f801e65e7f144
Raanan Weber <Raanan.Weber@microsoft.com>
2022-03-28 18:54:15 +0200

    fixes

--- packages/dev/buildTools/src/generateDeclaration.ts
+++ packages/dev/buildTools/src/generateDeclaration.ts
@@ -385,6 +385,8 @@ ${looseDeclarationsString || ""}
                 );
                 // check documentation flags
                 if (config.addToDocumentation) {
+                    // make sure snapshot directory exists
+                    checkDirectorySync(path.join(rootDir, "packages", ".snapshot"));
                     const documentationFile = path.join(rootDir, ".snapshot", "documentation.d.ts");
                     const originalFile = fs.existsSync(documentationFile) && !config.initDocumentation ? fs.readFileSync(documentationFile, "utf8") : "";
                     fs.writeFileSync(
