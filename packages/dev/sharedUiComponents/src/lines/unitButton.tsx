80589ca10cad6888cbfaa5020a036d184a82ee27
Darragh Burke <darraghburke@microsoft.com>
2022-03-29 15:56:26 -0700

    add units to slider

---
+++ packages/dev/sharedUiComponents/src/lines/unitButton.tsx
@@ -0,0 +1,17 @@
+interface IUnitButtonProps {
+    unit: string;
+    locked?: boolean;
+    onClick?: (unit: string) => void;
+}
+
+export function UnitButton(props: IUnitButtonProps) {
+    return <button
+            className={props.locked ? "unit disabled" : "unit"}
+            onClick={() => {
+                if (props.onClick && !props.locked) props.onClick(props.unit || "");
+            }}
+            disabled={props.locked}
+        >
+            {props.unit}
+        </button>;
+}
