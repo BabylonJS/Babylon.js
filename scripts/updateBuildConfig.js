ddd8bed48cba8960b607af1a2de389fec46b8ae3
Raanan Weber <Raanan.Weber@microsoft.com>
2022-03-31 14:04:38 +0200

    update build config script

---
+++ scripts/updateBuildConfig.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+
+/**
+ * How to use:
+ * 
+ * From the command line run the following:
+ * node ./scripts/updateBuildConfig.js versionType
+ * versionType can be either "major", "minor", "patch", or "prerelease"
+ * When prerelease the second argument is the prerelease id (alpha, beta, rc). keep empty to keep the same one.
+ * 
+ * This will be triggered by the CI when running the right pipeline (publish minor, publish major, publish patch)
+ */
+
+
+const config = require(path.resolve("./.build/config.json"));
+const updateType = process.argv[2] || "minor";
+const preid = process.argv[3] || config.preid;
+
+config.versionDefinition = updateType;
+config.nonce++;
+config.preid = preid;
+
+fs.writeFileSync(path.resolve("./.build/config.json"), JSON.stringify(config, null, 2));
