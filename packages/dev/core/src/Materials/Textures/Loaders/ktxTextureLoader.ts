8d909740b5d7a30883a0b78a60bf20fedaf44de0
Jasper St. Pierre <jstpierre@mecheye.net>
2022-04-07 14:29:28 -0700

    ktxTextureLoader: Mark _useSRGBBuffers when loading an SRGB-enabled texture format
    Also set texture.format to the correct value,
    since it seems the code elsewhere, like in the WebGPU engine, would
    like a "base" texture format.

--- packages/dev/core/src/Materials/Textures/Loaders/ktxTextureLoader.ts
+++ packages/dev/core/src/Materials/Textures/Loaders/ktxTextureLoader.ts
@@ -6,6 +6,7 @@ import type { InternalTexture } from "../../../Materials/Textures/internalTextur
 import type { IInternalTextureLoader } from "../../../Materials/Textures/internalTextureLoader";
 import { EndsWith } from "../../../Misc/stringTools";
 import { Logger } from "../../../Misc/logger";
+import { Constants } from "../../../Engines/constants";
 
 /**
  * Implementation of the KTX Texture Loader.
@@ -82,6 +83,56 @@ export class _KTXTextureLoader implements IInternalTextureLoader {
             // Need to invert vScale as invertY via UNPACK_FLIP_Y_WEBGL is not supported by compressed texture
             texture._invertVScale = !texture.invertY;
             const ktx = new KhronosTextureContainer(data, 1);
+
+            switch (ktx.glInternalFormat) {
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_S3TC_DXT1:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT1:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT3:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT5:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_BPTC_SIGNED_FLOAT:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_ASTC_4x4:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_ETC1_WEBGL:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGB8_ETC2:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA8_ETC2_EAC:
+                case Constants.TEXTUREFORMAT_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+                    texture.format = ktx.glInternalFormat;
+                    // Leave texture._useSRGBBuffer alone.
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_S3TC_DXT1_EXT:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGB_S3TC_DXT1;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT3;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT5;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ETC2:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGB8_ETC2;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA8_ETC2_EAC;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_ASTC_4x4;
+                    texture._useSRGBBuffer = true;
+                    break;
+                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_BPTC_UNORM:
+                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_BPTC_UNORM;
+                    texture._useSRGBBuffer = true;
+                    break;
+            }
+
             callback(
                 ktx.pixelWidth,
                 ktx.pixelHeight,
