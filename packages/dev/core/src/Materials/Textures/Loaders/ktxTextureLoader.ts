b6e057bc6b45b3d74fee1947e71fe361a8f0ad5d
Gary Hsu <bghgary@users.noreply.github.com>
2022-04-11 16:43:55 -0700

    Fix srgb format handling in KTX 1 files

--- packages/dev/core/src/Materials/Textures/Loaders/ktxTextureLoader.ts
+++ packages/dev/core/src/Materials/Textures/Loaders/ktxTextureLoader.ts
@@ -8,6 +8,29 @@ import { EndsWith } from "../../../Misc/stringTools";
 import { Logger } from "../../../Misc/logger";
 import { Constants } from "../../../Engines/constants";
 
+function mapSRGBToLinear(format: number): Nullable<number> {
+    switch (format) {
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_S3TC_DXT1_EXT:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGB_S3TC_DXT1;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT3;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT5;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ETC2:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGB8_ETC2;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGBA8_ETC2_EAC;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGBA_ASTC_4x4;
+        case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_BPTC_UNORM:
+            return Constants.TEXTUREFORMAT_COMPRESSED_RGBA_BPTC_UNORM;
+    }
+
+    return null;
+}
+
 /**
  * Implementation of the KTX Texture Loader.
  * @hidden
@@ -84,53 +107,13 @@ export class _KTXTextureLoader implements IInternalTextureLoader {
             texture._invertVScale = !texture.invertY;
             const ktx = new KhronosTextureContainer(data, 1);
 
-            switch (ktx.glInternalFormat) {
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_S3TC_DXT1:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT1:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT3:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT5:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_BPTC_SIGNED_FLOAT:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA_ASTC_4x4:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGB_ETC1_WEBGL:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGB8_ETC2:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGBA8_ETC2_EAC:
-                case Constants.TEXTUREFORMAT_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
-                    texture.format = ktx.glInternalFormat;
-                    // Leave texture._useSRGBBuffer alone.
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_S3TC_DXT1_EXT:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGB_S3TC_DXT1;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT3;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_S3TC_DXT5;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ETC2:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGB8_ETC2;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA8_ETC2_EAC;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_ASTC_4x4;
-                    texture._useSRGBBuffer = true;
-                    break;
-                case Constants.TEXTUREFORMAT_COMPRESSED_SRGB_ALPHA_BPTC_UNORM:
-                    texture.format = Constants.TEXTUREFORMAT_COMPRESSED_RGBA_BPTC_UNORM;
-                    texture._useSRGBBuffer = true;
-                    break;
+            const mappedFormat = mapSRGBToLinear(ktx.glInternalFormat);
+            if (mappedFormat) {
+                texture.format = mappedFormat;
+                texture._useSRGBBuffer = texture.getEngine()._getUseSRGBBuffer(true, texture.generateMipMaps);
+                texture._gammaSpace = true;
+            } else {
+                texture.format = ktx.glInternalFormat;
             }
 
             callback(
