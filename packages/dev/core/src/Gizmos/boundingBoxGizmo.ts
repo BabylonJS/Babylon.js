c3514a8434108ee83aae8e3a2fbab52f4c58e204
Gong Siyi <gongsiyi198825@gmail.com>
2022-05-10 04:30:33 +0800

    Fix BoundingBoxGizmo rotate incorrectly when using RightHand system.

--- packages/dev/core/src/Gizmos/boundingBoxGizmo.ts
+++ packages/dev/core/src/Gizmos/boundingBoxGizmo.ts
@@ -358,6 +358,11 @@ export class BoundingBoxGizmo extends Gizmo {
 
                         // Rotate around center of bounding box
                         this._anchorMesh.addChild(this.attachedMesh, Gizmo.PreserveScaling);
+                        if (this._anchorMesh.getScene().useRightHandedSystem) {
+                            this._tmpQuaternion.x *= -1;
+                            this._tmpQuaternion.y *= -1;
+                            this._tmpQuaternion.z *= -1;
+                        }
                         this._anchorMesh.rotationQuaternion!.multiplyToRef(this._tmpQuaternion, this._anchorMesh.rotationQuaternion!);
                         this._anchorMesh.removeChild(this.attachedMesh, Gizmo.PreserveScaling);
                         this.attachedMesh.setParent(originalParent, Gizmo.PreserveScaling);
