9c9fb4cd4fe5a22a2f9deec38e1225e6d017c07b
Gong Siyi <gongsiyi198825@gmail.com>
2022-05-10 17:37:27 +0800

    Use Quaternion.conjugateInPlace() method to conjugate the BoundingBoxGizmo_tmpQuaternion when scene.useRightHandedSystem = true

--- packages/dev/core/src/Gizmos/boundingBoxGizmo.ts
+++ packages/dev/core/src/Gizmos/boundingBoxGizmo.ts
@@ -359,9 +359,7 @@ export class BoundingBoxGizmo extends Gizmo {
                         // Rotate around center of bounding box
                         this._anchorMesh.addChild(this.attachedMesh, Gizmo.PreserveScaling);
                         if (this._anchorMesh.getScene().useRightHandedSystem) {
-                            this._tmpQuaternion.x *= -1;
-                            this._tmpQuaternion.y *= -1;
-                            this._tmpQuaternion.z *= -1;
+                            this._tmpQuaternion.conjugateInPlace();
                         }
                         this._anchorMesh.rotationQuaternion!.multiplyToRef(this._tmpQuaternion, this._anchorMesh.rotationQuaternion!);
                         this._anchorMesh.removeChild(this.attachedMesh, Gizmo.PreserveScaling);
