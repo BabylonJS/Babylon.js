eba2dc39d5e5227a677c476786ca2d184331c468
Gary Hsu <bghgary@users.noreply.github.com>
2022-05-24 13:03:37 -0700

    Formatting

--- packages/dev/loaders/src/glTF/glTFFileLoader.ts
+++ packages/dev/loaders/src/glTF/glTFFileLoader.ts
@@ -316,24 +316,24 @@ export class GLTFFileLoader implements IDisposable, ISceneLoaderPluginAsync, ISc
      * @param node - the transform node that corresponds to the original glTF skin node used for animations
      * @param skinnedNode - the transform node that is the skinned mesh itself or the parent of the skinned meshes
      */
-     public readonly onSkinLoadedObservable = new Observable<{ node: TransformNode, skinnedNode: TransformNode }>();
+    public readonly onSkinLoadedObservable = new Observable<{ node: TransformNode; skinnedNode: TransformNode }>();
 
-     private _onSkinLoadedObserver: Nullable<Observer<{ node: TransformNode, skinnedNode: TransformNode }>>;
+    private _onSkinLoadedObserver: Nullable<Observer<{ node: TransformNode; skinnedNode: TransformNode }>>;
 
-     /**
-      * Callback raised when the loader creates a mesh after parsing the glTF properties of the mesh.
-      * Note that the callback is called as soon as the mesh object is created, meaning some data may not have been setup yet for this mesh (vertex data, morph targets, material, ...)
-      */
-     public set onSkinLoaded(callback: (node: TransformNode, skinnedNode: TransformNode) => void) {
-         if (this._onSkinLoadedObserver) {
-             this.onSkinLoadedObservable.remove(this._onSkinLoadedObserver);
-         }
-         this._onSkinLoadedObserver = this.onSkinLoadedObservable.add((data) => {
-             callback(data.node, data.skinnedNode);
-         });
-     }
- 
-     /**
+    /**
+     * Callback raised when the loader creates a mesh after parsing the glTF properties of the mesh.
+     * Note that the callback is called as soon as the mesh object is created, meaning some data may not have been setup yet for this mesh (vertex data, morph targets, material, ...)
+     */
+    public set onSkinLoaded(callback: (node: TransformNode, skinnedNode: TransformNode) => void) {
+        if (this._onSkinLoadedObserver) {
+            this.onSkinLoadedObservable.remove(this._onSkinLoadedObserver);
+        }
+        this._onSkinLoadedObserver = this.onSkinLoadedObservable.add((data) => {
+            callback(data.node, data.skinnedNode);
+        });
+    }
+
+    /**
      * Observable raised when the loader creates a texture after parsing the glTF properties of the texture.
      */
     public readonly onTextureLoadedObservable = new Observable<BaseTexture>();
