ce2b146f44b154a980a07c2522423a1e6f95f33d
Gary Hsu <bghgary@users.noreply.github.com>
2022-05-25 10:13:40 -0700

    Add missing interpolation to glTF animation key (#12584)
    Former-commit-id: 13a1b1359fd5942a00faf900e6a8c6a723678f4a

--- packages/dev/loaders/src/glTF/2.0/glTFLoader.ts
+++ packages/dev/loaders/src/glTF/2.0/glTFLoader.ts
@@ -1673,6 +1673,7 @@ export class GLTFLoader implements IGLTFLoader {
                             inTangent: key.inTangent ? key.inTangent[targetIndex] : undefined,
                             value: key.value[targetIndex],
                             outTangent: key.outTangent ? key.outTangent[targetIndex] : undefined,
+                            interpolation: key.interpolation,
                         }))
                     );
 
