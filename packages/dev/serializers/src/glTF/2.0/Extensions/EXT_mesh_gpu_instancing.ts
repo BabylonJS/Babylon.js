3a30892900e5394a6952626413d0f7a316eb9cd7
Raanan Weber <Raanan.Weber@microsoft.com>
2022-05-30 21:29:59 +0200

    fix import
    Former-commit-id: 09c58b0d1d3fc0fafc4fccc9a8554626a8d8b884

--- packages/dev/serializers/src/glTF/2.0/Extensions/EXT_mesh_gpu_instancing.ts
+++ packages/dev/serializers/src/glTF/2.0/Extensions/EXT_mesh_gpu_instancing.ts
@@ -4,7 +4,7 @@ import type { IGLTFExporterExtensionV2 } from "../glTFExporterExtension";
 import { _Exporter, _BinaryWriter } from "../glTFExporter";
 import type { Nullable } from "core/types";
 import type { Node } from "core/node";
-import { Mesh } from "core/Meshes";
+import { Mesh } from "core/Meshes/mesh";
 import { TmpVectors, Quaternion, Vector3 } from "core/Maths/math.vector";
 import { VertexBuffer } from "core/Buffers/buffer";
 
