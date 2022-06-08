2d9087c4637f737441522c91f48d5f159435dc11
Raanan Weber <Raanan.Weber@microsoft.com>
2022-06-08 17:58:58 +0200

    fix formatting
    Former-commit-id: 900162dfe685cc773286a8a27219f737daa2d31b

--- packages/dev/core/src/Misc/computePressure.ts
+++ packages/dev/core/src/Misc/computePressure.ts
@@ -27,9 +27,10 @@ export class ComputePressureObserverWrapper {
      * Method that must be called to begin observing changes, and triggering callbacks.
      */
     observe(): void {
-        this._observer?.observe && this._observer?.observe().catch(() => {
-            // Ignore error
-        });
+        this._observer?.observe &&
+            this._observer?.observe().catch(() => {
+                // Ignore error
+            });
     }
 
     /**
