c98cf68c87679f5dc2765ef4af3a9b06fab3c2fa
Carolina <carolhmj@gmail.com>
2022-06-21 08:46:43 -0300

    Format.

--- packages/dev/core/src/Misc/tools.ts
+++ packages/dev/core/src/Misc/tools.ts
@@ -773,10 +773,10 @@ export class Tools {
     }
 
     /**
-     * Download a Blob object 
+     * Download a Blob object
      * @param blob the Blob object
      * @param fileName the file name to download
-     * @returns 
+     * @returns
      */
     static DownloadBlob(blob: Blob, fileName?: string) {
         //Creating a link if the browser have the download attribute on the a tag, to automatically start download generated image.
