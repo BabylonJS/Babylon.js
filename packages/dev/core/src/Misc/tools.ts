d28c79184f48135974ca1ab41c7d44bcd992530b
Carolina <carolhmj@gmail.com>
2022-06-21 09:34:18 -0300

    Force render with just that camera on CreateScreenshot (#12656)
    * Force render with just that camera on CreateScreenshot
    
    * Add comment.

--- packages/dev/core/src/Misc/tools.ts
+++ packages/dev/core/src/Misc/tools.ts
@@ -773,6 +773,41 @@ export class Tools {
     }
 
     /**
+     * Download a Blob object
+     * @param blob the Blob object
+     * @param fileName the file name to download
+     * @returns
+     */
+    static DownloadBlob(blob: Blob, fileName?: string) {
+        //Creating a link if the browser have the download attribute on the a tag, to automatically start download generated image.
+        if ("download" in document.createElement("a")) {
+            if (!fileName) {
+                const date = new Date();
+                const stringDate =
+                    (date.getFullYear() + "-" + (date.getMonth() + 1)).slice(2) + "-" + date.getDate() + "_" + date.getHours() + "-" + ("0" + date.getMinutes()).slice(-2);
+                fileName = "screenshot_" + stringDate + ".png";
+            }
+            Tools.Download(blob, fileName);
+        } else {
+            if (blob) {
+                const url = URL.createObjectURL(blob);
+
+                const newWindow = window.open("");
+                if (!newWindow) {
+                    return;
+                }
+                const img = newWindow.document.createElement("img");
+                img.onload = function () {
+                    // no longer need to read the blob so it's revoked
+                    URL.revokeObjectURL(url);
+                };
+                img.src = url;
+                newWindow.document.body.appendChild(img);
+            }
+        }
+    }
+
+    /**
      * Encodes the canvas data to base 64 or automatically download the result if filename is defined
      * @param successCallback defines the callback triggered once the data are available
      * @param mimeType defines the mime type of the result
@@ -794,37 +829,8 @@ export class Tools {
             this.ToBlob(
                 canvas ?? Tools._ScreenshotCanvas,
                 function (blob) {
-                    //Creating a link if the browser have the download attribute on the a tag, to automatically start download generated image.
-                    if ("download" in document.createElement("a")) {
-                        if (!fileName) {
-                            const date = new Date();
-                            const stringDate =
-                                (date.getFullYear() + "-" + (date.getMonth() + 1)).slice(2) +
-                                "-" +
-                                date.getDate() +
-                                "_" +
-                                date.getHours() +
-                                "-" +
-                                ("0" + date.getMinutes()).slice(-2);
-                            fileName = "screenshot_" + stringDate + ".png";
-                        }
-                        Tools.Download(blob!, fileName);
-                    } else {
-                        if (blob) {
-                            const url = URL.createObjectURL(blob);
-
-                            const newWindow = window.open("");
-                            if (!newWindow) {
-                                return;
-                            }
-                            const img = newWindow.document.createElement("img");
-                            img.onload = function () {
-                                // no longer need to read the blob so it's revoked
-                                URL.revokeObjectURL(url);
-                            };
-                            img.src = url;
-                            newWindow.document.body.appendChild(img);
-                        }
+                    if (blob) {
+                        Tools.DownloadBlob(blob, fileName);
                     }
                 },
                 mimeType,
