0b857995ad0bc9b39708a27ea9b97e5ce9378d0d
Carolina <carolhmj@gmail.com>
2022-06-28 19:53:02 -0300

    Fix serialization when objects have InstancedMesh parents (#12690)
    * Properly serialize meshes whose parents are instanced meshes
    
    * Add a fix and tests.
    
    * Update visualization test.
    
    * Fix linting.

--- packages/dev/core/src/Cameras/camera.ts
+++ packages/dev/core/src/Cameras/camera.ts
@@ -1265,7 +1265,7 @@ export class Camera extends Node {
 
         // Parent
         if (this.parent) {
-            serializationObject.parentId = this.parent.uniqueId;
+            this.parent._serializeAsParent(serializationObject);
         }
 
         if (this.inputs) {
@@ -1378,6 +1378,11 @@ export class Camera extends Node {
         // Parent
         if (parsedCamera.parentId !== undefined) {
             camera._waitingParentId = parsedCamera.parentId;
+        }
+
+        // Parent instance index
+        if (parsedCamera.parentInstanceIndex !== undefined) {
+            camera._waitingParentInstanceIndex = parsedCamera.parentInstanceIndex;
         }
 
         //If camera has an input manager, let it parse inputs settings
