baf4c465527af60a4b9aa7e38283f724a9ac8a68
amritanarasimhan <amrita.pln@hotmail.com>
2022-07-19 14:28:09 -0700

    issue in progress
    Former-commit-id: 0d9b0d4d162c599f9fdb2b0084e5054aff1180cc

--- packages/tools/guiEditor/src/globalState.ts
+++ packages/tools/guiEditor/src/globalState.ts
@@ -96,6 +96,7 @@ export class GlobalState {
     onCopyObservable = new Observable<(content: string) => void>();
     onCutObservable = new Observable<(content: string) => void>();
     onPasteObservable = new Observable<string>();
+    //onPasteObservable = new Observable<(content: string) => void>();
     isSaving = false;
     public lockObject = new LockObject();
     storeEditorData: (serializationObject: any) => void;
@@ -134,7 +135,8 @@ export class GlobalState {
         });
         this.hostDocument.addEventListener("paste", (event) => {
             if (!isElementEditable(event.target as HTMLElement)) {
-                this.onPasteObservable.notifyObservers(event.clipboardData?.getData("text") || "");
+                this.onPasteObservable.notifyObservers(event.clipboardData?.getData("text") || "")
+                //this.onPasteObservable.notifyObservers((content) => event.clipboardData?.setData("text/plain", content));
                 event.preventDefault();
             }
         });
