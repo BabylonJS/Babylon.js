31fb8c7c32025b7a474029db604be9a183b1fd1b
amritanarasimhan <amrita.pln@hotmail.com>
2022-07-22 10:44:34 -0700

    fixing unnecessary file

--- packages/tools/guiEditor/src/diagram/workbench.tsx
+++ packages/tools/guiEditor/src/diagram/workbench.tsx
@@ -286,8 +286,6 @@ export class WorkbenchComponent extends React.Component<IWorkbenchComponentProps
                 for (const control of parsed.controls) {
                     newSelection.push(this.appendBlock(Control.Parse(control, this.props.globalState.guiTexture)));
                 }
-                newSelection[0].leftInPixels = newSelection[0].leftInPixels + 10;
-                newSelection[0].topInPixels = newSelection[0].topInPixels + 10;
                 this.props.globalState.setSelection(newSelection);
                 return true;
             }
