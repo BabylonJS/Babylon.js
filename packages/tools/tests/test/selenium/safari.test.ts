87cc35ac9e70d0f3c347434e10e826a5db84b01c
Dave Solares <>
2022-08-01 16:55:08 -0700

    PR Feedback
    Former-commit-id: 89bf7510d4bfb1de7932ea258c52b1590918ec68

--- packages/tools/tests/test/selenium/safari.test.ts
+++ packages/tools/tests/test/selenium/safari.test.ts
@@ -1,44 +1,43 @@
-import { getGlobalConfig, SafariSeleniumEnvironment } from "@tools/test-tools";
-import { Button, Key } from "selenium-webdriver";
+import { CheckTestSuccessStatus, getGlobalConfig, LoadPlayground } from "@tools/test-tools";
+import { Builder, Button, By, Key } from "selenium-webdriver";
 
 jest.setTimeout(60000);
 
 describe("safari", () => {
-    const testEnv = new SafariSeleniumEnvironment();
+    const driver = new Builder().forBrowser("safari").build();
+
+    beforeAll(async () => {
+        await driver.get(getGlobalConfig().baseUrl + `/empty.html`);
+    });
 
     afterAll(async () => {
-        await testEnv.quit();
+        await driver.quit();
     });
 
     // Check if allowMouse logic for camera touch input is validating correctly
     // PG: https://playground.babylonjs.com/#ITQ2NZ#9
     it("check isMouseEvent", async () => {
-        await testEnv.get(getGlobalConfig().baseUrl + `/empty.html`).then(async () => {
-            await testEnv.loadPG("#ITQ2NZ#9", getGlobalConfig(), 1000);
-        });
-        const el = await testEnv.findElementById("babylon-canvas");
+        await LoadPlayground(driver, "#ITQ2NZ#9", getGlobalConfig(), 1000);
+        const el = await driver.findElement(By.id("babylon-canvas"));
 
         // With allowMouse = true, touch controls should move camera forward if isMouseEvent is true
-        await testEnv.getDriver().actions({ bridge: true }).move({ origin: el }).press().move({ x: 0, y: -200, origin: el }).release().perform();
-        await testEnv.getDriver().sleep(1000);
+        await driver.actions({ bridge: true }).move({ origin: el }).press().move({ x: 0, y: -200, origin: el }).release().perform();
+        await driver.sleep(1000);
 
         // Set allowMouse = false (handled in PG) and verify that camera uses mouse controls
-        await testEnv.getDriver().actions({ bridge: true }).move({ origin: el }).press().move({ x: 0, y: 200, origin: el }).release().perform();
-        await testEnv.getDriver().sleep(1000);
+        await driver.actions({ bridge: true }).move({ origin: el }).press().move({ x: 0, y: 200, origin: el }).release().perform();
+        await driver.sleep(1000);
 
         // Check status after actions
-        const testStatus = await testEnv.checkTestSuccessStatus();
+        const testStatus = await CheckTestSuccessStatus(driver);
         expect(testStatus).toBe(true);
     });
 
     it("can process InputManager pointer events", async () => {
-        await testEnv.get(getGlobalConfig().baseUrl + `/empty.html`).then(async () => {
-            await testEnv.loadPG("#YQUTAY#12", getGlobalConfig());
-        });
+        await LoadPlayground(driver, "#YQUTAY#12", getGlobalConfig());
+        const el = await driver.findElement(By.id("babylon-canvas"));
 
-        const el = await testEnv.findElementById("babylon-canvas");
-        await testEnv
-            .getDriver()
+        await driver
             .actions()
             .move({ origin: el })
             .press(Button.LEFT)
@@ -49,14 +48,12 @@ describe("safari", () => {
             .release(Button.MIDDLE)
             .perform();
 
-        await testEnv.getDriver().sleep(1000);
+        await driver.sleep(1000);
 
-        await testEnv
-            .getDriver()
+        await driver
             .executeScript("BABYLON.Scene.DoubleClickDelay = 500;")
             .then(async () => {
-                await testEnv
-                    .getDriver()
+                await driver
                     .actions()
                     .move({ origin: el })
                     .press(Button.LEFT)
@@ -74,7 +71,7 @@ describe("safari", () => {
                     .perform();
             });
 
-        const testStatus = await testEnv.checkTestSuccessStatus();
+        const testStatus = await CheckTestSuccessStatus(driver);
         expect(testStatus).toBe(true);
     });
 
@@ -83,84 +80,31 @@ describe("safari", () => {
     // pointer capture functions (eg. hasPointerCapture)
     // PG: https://playground.babylonjs.com/#5NMCCT
     it("check pointerCapture", async () => {
-        await testEnv.get(getGlobalConfig().baseUrl + `/empty.html`).then(async () => {
-            await testEnv.loadPG("#5NMCCT", getGlobalConfig(), 1000);
-        });
-        const el = await testEnv.findElementById("babylon-canvas");
+        await LoadPlayground(driver, "#5NMCCT", getGlobalConfig(), 1000);
+        const el = await driver.findElement(By.id("babylon-canvas"));
 
         // With allowMouse = true, touch controls should move camera forward if isMouseEvent is true
-        await testEnv.getDriver().actions({ bridge: true }).move({ origin: el }).press().move({ x: 200, y: 0, origin: el }).release().perform();
-        await testEnv.getDriver().sleep(1000);
+        await driver.actions({ bridge: true }).move({ origin: el }).press().move({ x: 200, y: 0, origin: el }).release().perform();
+        await driver.sleep(1000);
 
         // Check status after actions
-        const testStatus = await testEnv.checkTestSuccessStatus();
+        const testStatus = await CheckTestSuccessStatus(driver);
         expect(testStatus).toBe(true);
     });
-
-    //#NL54AT#4
-    /*it("check pointerLock", async () => {
-        await testEnv.get(getGlobalConfig().baseUrl + `/empty.html`).then(async () => {
-            await testEnv.loadPG("#NL54AT#8", getGlobalConfig(), 1000);
-        });
-        const el = await testEnv.findElementById("babylon-canvas");
-        await testEnv.getDriver().sleep(5000);
-
-        // With allowMouse = true, touch controls should move camera forward if isMouseEvent is true
-        await testEnv
-            .getDriver()
-            .actions({ bridge: true })
-            .click(el)
-            .perform();
-            await testEnv.getDriver().sleep(5000);
-
-            await testEnv
-            .getDriver()
-            .actions({ bridge: true })
-            .sendKeys(Key.ESCAPE)
-            .perform();
-            await testEnv.getDriver().sleep(1000);
-
-        await testEnv
-            .getDriver()
-            .actions({ bridge: true })
-            .press()
-            .move({ duration: 500, x: -200, y: 0, origin: el })
-            .move({ duration: 500, x: 0, y: -200, origin: el })
-            .move({ duration: 500, x: 200, y: 0, origin: el })
-            .release()
-            .perform();
-        await testEnv.getDriver().sleep(1000);
-
-        
-
-            await testEnv
-            .getDriver()
-            .actions({ bridge: true })
-            .sendKeys(Key.ESCAPE)
-            .perform();
-
-            console.log(await testEnv.getDriver().executeScript("return globalThis.testLog;"));
-
-        // Check status after actions
-        const testStatus = await testEnv.checkTestSuccessStatus();
-        expect(testStatus).toBe(true);
-    });*/
 
     // Check if Meta key (Cmd) is not interfering with keys being released properly
     // PG: https://playground.babylonjs.com/#Y4YWCD#9
     it("check Meta key allowing keyup", async () => {
-        await testEnv.get(getGlobalConfig().baseUrl + `/empty.html`).then(async () => {
-            await testEnv.loadPG("#Y4YWCD#9", getGlobalConfig(), 1000);
-        });
-        const el = await testEnv.findElementById("babylon-canvas");
+        await LoadPlayground(driver, "#Y4YWCD#9", getGlobalConfig(), 1000);
+        const el = await driver.findElement(By.id("babylon-canvas"));
 
         // With allowMouse = true, touch controls should move camera forward if isMouseEvent is true
-        await testEnv.getDriver().actions({ bridge: true }).move({ origin: el }).click().keyDown(Key.META).sendKeys("c").keyUp(Key.META).perform();
-        await testEnv.getDriver().sleep(1000);
-        await testEnv.getDriver().actions({ bridge: true }).move({ origin: el }).click().sendKeys(Key.META).perform();
+        await driver.actions({ bridge: true }).move({ origin: el }).click().keyDown(Key.META).sendKeys("c").keyUp(Key.META).perform();
+        await driver.sleep(1000);
+        await driver.actions({ bridge: true }).move({ origin: el }).click().sendKeys(Key.META).perform();
 
         // Check status after actions
-        const testStatus = await testEnv.checkTestSuccessStatus();
+        const testStatus = await CheckTestSuccessStatus(driver);
         expect(testStatus).toBe(true);
     });
 });
