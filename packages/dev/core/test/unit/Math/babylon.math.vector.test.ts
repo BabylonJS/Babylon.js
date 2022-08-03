2c886c0e631ba05525bfb39ff13b2a24a6bc373f
sebavan <vandenberghe.sebastien@gmail.com>
2022-08-03 20:54:52 +0200

    Format

--- packages/dev/core/test/unit/Math/babylon.math.vector.test.ts
+++ packages/dev/core/test/unit/Math/babylon.math.vector.test.ts
@@ -1,4 +1,4 @@
-import { Plane, Vector3 } from 'core/Maths'
+import { Plane, Vector3 } from "core/Maths";
 
 /**
  * Describes the test suite.
@@ -7,10 +7,7 @@ describe("Babylon Vectors", () => {
     describe("#Vector3", () => {
         it("can project from an origin onto a plane", () => {
             // A ground plane at origin
-            const simplePlane = Plane.FromPositionAndNormal(
-                Vector3.Zero(),
-                Vector3.Up(),
-            );
+            const simplePlane = Plane.FromPositionAndNormal(Vector3.Zero(), Vector3.Up());
 
             const rayOrigin = new Vector3(0, 10, 0);
             const rayGoingThrough = new Vector3(1, 8, 0);
@@ -18,15 +15,12 @@ describe("Babylon Vectors", () => {
             // Going left 1 unit for each 2 units downs
             const expected = new Vector3(5, 0, 0);
 
-            expect(rayGoingThrough.projectOnPlane(simplePlane, rayOrigin)).toEqual(expected)
-        })
+            expect(rayGoingThrough.projectOnPlane(simplePlane, rayOrigin)).toEqual(expected);
+        });
 
         it("can project from an origin onto an offset plane", () => {
             // A ground plane 10 units below origin
-            const simplePlane = Plane.FromPositionAndNormal(
-                new Vector3(0, -10, 0),
-                Vector3.Up(),
-            );
+            const simplePlane = Plane.FromPositionAndNormal(new Vector3(0, -10, 0), Vector3.Up());
 
             const rayOrigin = new Vector3(0, 10, 0);
             const rayGoingThrough = new Vector3(1, 8, 0);
@@ -34,7 +28,7 @@ describe("Babylon Vectors", () => {
             // Going left 1 unit for each 2 units downs
             const expected = new Vector3(10, -10, 0);
 
-            expect(rayGoingThrough.projectOnPlane(simplePlane, rayOrigin)).toEqual(expected)
-        })
-    })
-})
+            expect(rayGoingThrough.projectOnPlane(simplePlane, rayOrigin)).toEqual(expected);
+        });
+    });
+});
