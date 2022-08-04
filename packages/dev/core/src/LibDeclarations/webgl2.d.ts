da24ba72800d4ef94e1ac2498301e6e14a4915ff
sebavan <vandenberghe.sebastien@gmail.com>
2022-08-04 16:54:32 +0200

    Formatting

--- packages/dev/core/src/LibDeclarations/webgl2.d.ts
+++ packages/dev/core/src/LibDeclarations/webgl2.d.ts
@@ -1,9 +1,6 @@
-// /* eslint-disable no-var */
-// /* eslint-disable @typescript-eslint/naming-convention */
-// // Type definitions for WebGL 2, Editor's Draft Fri Feb 24 16:10:18 2017 -0800
-// // Project: https://www.khronos.org/registry/webgl/specs/latest/2.0/
-// // Definitions by: Nico Kemnitz <https://github.com/nkemnitz/>
-// // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/naming-convention */
+// Type definitions for WebGL 2 extended with Babylon specific types
 
 interface WebGL2RenderingContext extends WebGL2RenderingContextBase {
     HALF_FLOAT_OES: number;
