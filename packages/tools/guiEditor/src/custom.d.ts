5e3a8b19cb653d032a19cc87640de05166c7b881
Raanan Weber <info@raananweber.com>
2022-08-04 15:04:04 +0200

    Support for scss modules (#12832)

--- packages/tools/guiEditor/src/custom.d.ts
+++ packages/tools/guiEditor/src/custom.d.ts
@@ -2,3 +2,8 @@ declare module "*.svg" {
     const content: string;
     export default content;
 }
+
+declare module "*.modules.scss" {
+    const content: Record<string, string>;
+    export default content;
+}
