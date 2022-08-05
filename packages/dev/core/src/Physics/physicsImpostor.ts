f8a48b285db809baec7c93a5e9e24973f7ad423f
Cedric Guillemet <cedric.guillemet@gmail.com>
2022-08-05 10:41:38 +0200

    Fix getHeightAtCoordinates with Impostors and out of bound

--- packages/dev/core/src/Physics/physicsImpostor.ts
+++ packages/dev/core/src/Physics/physicsImpostor.ts
@@ -893,9 +893,14 @@ export class PhysicsImpostor {
         }
         // take the position set and make it the absolute position of this object.
         this.object.setAbsolutePosition(this.object.position);
-        this._deltaRotation && this.object.rotationQuaternion && this.object.rotationQuaternion.multiplyToRef(this._deltaRotation, this.object.rotationQuaternion);
+        if (this._deltaRotation) {
+            this.object.rotationQuaternion && this.object.rotationQuaternion.multiplyToRef(this._deltaRotation, this.object.rotationQuaternion);
+            this._deltaPosition.applyRotationQuaternionToRef(this._deltaRotation, PhysicsImpostor._TmpVecs[0]);
+            this.object.translate(PhysicsImpostor._TmpVecs[0], 1);
+        } else {
+            this.object.translate(this._deltaPosition, 1);
+        }
         this.object.computeWorldMatrix(true);
-        this.object.translate(this._deltaPosition, 1);
     };
 
     /**
