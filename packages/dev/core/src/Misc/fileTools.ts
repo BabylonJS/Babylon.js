d40304a23fe4813c7438b3ec81e7fbae02697f80
Carolina <carolhmj@gmail.com>
2022-08-30 06:38:36 -0300

    Add type on blob creation (#12912)
    * Add type on blob creation
    
    * Review.
    
    Former-commit-id: 58b6dccc6f0a7505c3ecb6087217a74ede3409b5

--- packages/dev/core/src/Misc/fileTools.ts
+++ packages/dev/core/src/Misc/fileTools.ts
@@ -247,8 +247,9 @@ export const LoadImage = (
     const noOfflineSupport = () => {
         LoadFile(
             url,
-            (data) => {
-                const blob = new Blob([data]);
+            (data, _, contentType) => {
+                const type = !mimeType && contentType ? contentType : mimeType;
+                const blob = new Blob([data], { type });
                 const url = URL.createObjectURL(blob);
                 img.src = url;
             },
@@ -357,7 +358,7 @@ export const ReadFile = (
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const LoadFile = (
     fileOrUrl: File | string,
-    onSuccess: (data: string | ArrayBuffer, responseURL?: string) => void,
+    onSuccess: (data: string | ArrayBuffer, responseURL?: string, contentType?: Nullable<string>) => void,
     onProgress?: (ev: ProgressEvent) => void,
     offlineProvider?: IOfflineProvider,
     useArrayBuffer?: boolean,
@@ -419,7 +420,7 @@ export const LoadFile = (
     return RequestFile(
         url,
         (data, request) => {
-            onSuccess(data, request ? request.responseURL : undefined);
+            onSuccess(data, request?.responseURL, request?.getResponseHeader("content-type"));
         },
         onProgress,
         offlineProvider,
