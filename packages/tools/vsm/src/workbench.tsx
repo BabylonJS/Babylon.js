f82bf824b68501f178ceb4015138bbf92ebd982d
Carolina Herbster <carolhmj@gmail.com>
2022-09-27 13:19:30 -0300

    More basic declarations and styling.

--- packages/tools/vsm/src/workbench.tsx
+++ packages/tools/vsm/src/workbench.tsx
@@ -1,10 +1,13 @@
 import { CommandBarComponent } from "shared-ui-components/components/bars/CommandBarComponent";
+import style from "./workbench.modules.scss";
+
 export type WorkbenchProps = {};
 
 export const Workbench: React.FC<WorkbenchProps> = () => {
     return (
-        <div>
+        <div className={style.workbenchContainer}>
             <CommandBarComponent />
+            <div className={style.workArea}></div>
         </div>
     );
 };
