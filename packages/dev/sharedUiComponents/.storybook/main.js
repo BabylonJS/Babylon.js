c82765aaee2ae2cc478b1c08ad14e775dcafc497
Carolina <carolhmj@gmail.com>
2022-09-27 14:01:15 -0300

    Update Storybook to 7 (#13033)

--- packages/dev/sharedUiComponents/.storybook/main.js
+++ packages/dev/sharedUiComponents/.storybook/main.js
@@ -3,16 +3,23 @@ const path = require("path");
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
     addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-    framework: "@storybook/react",
-    core: {
-        builder: "webpack5",
-    },
+    framework: "@storybook/react-webpack5",
     webpackFinal: async (config, { configType }) => {
         // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
         // You can change the configuration based on that.
         // 'PRODUCTION' is used when building the static version of storybook.
 
         // Make whatever fine-grained changes you need
+        config.module.rules.push({
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+            sideEffects: true,
+            options: {
+                configFile: "tsconfig.build.json",
+            },
+        });
+
         config.module.rules.push({
             test: /\.scss$/,
             use: [
@@ -32,3 +39,4 @@ module.exports = {
         return config;
     },
 };
+
