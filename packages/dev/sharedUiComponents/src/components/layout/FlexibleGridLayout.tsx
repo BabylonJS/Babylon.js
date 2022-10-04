c515b077fc85649e7a5bec10c97d3c8302b0d961
Carolina Herbster <carolhmj@gmail.com>
2022-10-04 17:19:55 -0300

    reorganize drag processing
    Former-commit-id: 9c51088e649e0d663a391737c93ff6af4e05c2dc

--- packages/dev/sharedUiComponents/src/components/layout/FlexibleGridLayout.tsx
+++ packages/dev/sharedUiComponents/src/components/layout/FlexibleGridLayout.tsx
@@ -1,4 +1,4 @@
-import type { FC, MutableRefObject } from "react";
+import type { FC } from "react";
 import { useState, useRef } from "react";
 import { FlexibleColumn } from "./FlexibleColumn";
 import style from "./FlexibleGridLayout.modules.scss";
@@ -6,7 +6,7 @@ import { FlexibleTabsContainer } from "./FlexibleTabsContainer";
 import { Vector2 } from "core/Maths/math";
 import { FlexibleDropZone } from "./FlexibleDropZone";
 import type { Nullable } from "core/types";
-import { DRAGCLASS } from "./constants";
+import { DRAGCLASS, DragOperationTypes } from "./constants";
 import { addPercentages } from "./unitTools";
 
 export interface IFlexibleGridLayoutProps {
@@ -16,12 +16,11 @@ export interface IFlexibleGridLayoutProps {
 export const FlexibleGridLayout: FC<IFlexibleGridLayoutProps> = (props) => {
     const [layout, setLayout] = useState(props.layoutDefinition);
     const containerDiv = useRef<Nullable<HTMLDivElement>>(null);
-    // DRAGGING INFORMATION
-    const initialPointerPos = useRef(new Vector2(0, 0));
-    const startDragRow: MutableRefObject<Nullable<number>> = useRef(null);
-    const startDragColumn: MutableRefObject<Nullable<number>> = useRef(null);
-    const hasClicked = useRef(false);
+    // CLICK/DRAG INFORMATION
+    const pointerPos = useRef(new Vector2(0, 0));
+    const isPointerDown = useRef(false);
     const isDragging = useRef(false);
+    const operationInformation = useRef<{ purpose?: string; args?: any }>({});
 
     const getPosInLayout = (row: number, column: number) => {
         const columnLayout = layout.columns[column];
@@ -31,30 +30,34 @@ export const FlexibleGridLayout: FC<IFlexibleGridLayoutProps> = (props) => {
         return columnLayout.rows[row];
     };
 
+    const processResizeRow = (pos: Vector2, args: { row: number; column: number }) => {
+        // Check y axis difference
+        const yDiff = pos.y - pointerPos.current.y;
+
+        // Get layout rows
+        const layoutRow0 = getPosInLayout(args.row, args.column);
+        const layoutRow1 = getPosInLayout(args.row + 1, args.column);
+        console.log("layout row 0", layoutRow0, "layout row 1", layoutRow1);
+
+        if (layoutRow0 && layoutRow1 && containerDiv.current) {
+            // Convert y diff to percentage values
+            const totalHeight = containerDiv.current.clientHeight;
+            const percHeight = (yDiff / totalHeight) * 100;
+
+            layoutRow0.height = addPercentages(layoutRow0.height, percHeight + "%");
+            layoutRow1.height = addPercentages(layoutRow1.height, -percHeight + "%");
+
+            setLayout({ ...layout });
+        }
+    };
+
     const onDragMove = (pos: Vector2) => {
         // Vertical drag
-        if (startDragRow.current !== null && startDragColumn.current != null) {
-            // Check y axis difference
-            const yDiff = pos.y - initialPointerPos.current.y;
-
-            // Get layout rows
-            const layoutRow0 = getPosInLayout(startDragRow.current, startDragColumn.current);
-            const layoutRow1 = getPosInLayout(startDragRow.current + 1, startDragColumn.current);
-            console.log("layout row 0", layoutRow0, "layout row 1", layoutRow1);
-
-            if (layoutRow0 && layoutRow1 && containerDiv.current) {
-                // Convert y diff to percentage values
-                const totalHeight = containerDiv.current.clientHeight;
-                const percHeight = (yDiff / totalHeight) * 100;
-
-                layoutRow0.height = addPercentages(layoutRow0.height, percHeight + "%");
-                layoutRow1.height = addPercentages(layoutRow1.height, -percHeight + "%");
-
-                setLayout({ ...layout });
-            }
+        if (operationInformation.current.purpose === DragOperationTypes.RESIZE_ROW) {
+            processResizeRow(pos, operationInformation.current.args);
         }
 
-        initialPointerPos.current = pos;
+        pointerPos.current = pos;
     };
 
     const columns = layout.columns.map((column: any, columnIdx: number) => {
@@ -75,31 +78,46 @@ export const FlexibleGridLayout: FC<IFlexibleGridLayoutProps> = (props) => {
 
     const pointerDownHandler = (event: React.PointerEvent<HTMLDivElement>) => {
         const target = event.target;
+        // Check if we're clicking on an element that can be dragged
         if (target instanceof HTMLElement && target.classList.contains(DRAGCLASS)) {
-            hasClicked.current = true;
-            // Store initial pointer pos
-            initialPointerPos.current.x = event.clientX;
-            initialPointerPos.current.y = event.clientY;
-            const rowNumber = target.getAttribute("data-row-number");
-            // Store possible row of where drag happened
-            startDragRow.current = rowNumber ? parseInt(rowNumber) : null;
-            const columnNumber = target.getAttribute("data-column-number");
-            startDragColumn.current = columnNumber ? parseInt(columnNumber) : null;
-            console.log("clicked on divider of row number", rowNumber, "and column number", columnNumber);
+            isPointerDown.current = true;
+
+            pointerPos.current.x = event.clientX;
+            pointerPos.current.y = event.clientY;
+
+            // Figure out the purpose of the element through its "data-drag-type" attribute
+            const purpose = target.getAttribute("data-drag-type");
+            if (purpose === DragOperationTypes.RESIZE_ROW || purpose === DragOperationTypes.RESIZE_COLUMN) {
+                const rowNumber = target.getAttribute("data-row-number");
+                // Store possible row of where drag happened
+                const row = rowNumber ? parseInt(rowNumber) : undefined;
+                const columnNumber = target.getAttribute("data-column-number");
+                const column = columnNumber ? parseInt(columnNumber) : undefined;
+                console.log("clicked on divider of row number", rowNumber, "and column number", columnNumber);
+                if (row !== undefined && column !== undefined) {
+                    operationInformation.current = {
+                        purpose,
+                        args: {
+                            column,
+                            row,
+                        },
+                    };
+                }
+            }
         }
     };
 
     const pointerMoveHandler = (event: React.PointerEvent<HTMLDivElement>) => {
         // Check pointer distance moved
         const newPos = new Vector2(event.clientX, event.clientY);
-        if (hasClicked.current && !isDragging.current) {
-            const distance = newPos.subtract(initialPointerPos.current).length();
+        if (isPointerDown.current && !isDragging.current) {
+            const distance = newPos.subtract(pointerPos.current).length();
             // If distance greater than a threshold, start drag
             if (distance > 3) {
                 isDragging.current = true;
                 onDragMove(newPos);
             }
-        } else if (hasClicked.current) {
+        } else if (isPointerDown.current) {
             onDragMove(newPos);
         }
     };
@@ -111,7 +129,7 @@ export const FlexibleGridLayout: FC<IFlexibleGridLayoutProps> = (props) => {
         } else {
             clickFn();
         }
-        hasClicked.current = false;
+        isPointerDown.current = false;
     };
 
     return (
