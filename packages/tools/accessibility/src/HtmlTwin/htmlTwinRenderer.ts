e837a94f5a040cdc2c175b86512c1462cd2d2d3a
Sunny Zhang <yayzhang@microsoft.com>
2022-10-05 10:41:32 -0700

    Render html twin inside canvas.

--- packages/tools/accessibility/src/HtmlTwin/htmlTwinRenderer.ts
+++ packages/tools/accessibility/src/HtmlTwin/htmlTwinRenderer.ts
@@ -5,19 +5,9 @@ import { HTMLTwinHostComponent } from "./htmlTwinHostComponent";
 
 export class HTMLTwinRenderer {
     public static Render(scene: Scene) {
-        const wrapper = document.createElement("div");
-        wrapper.id = "accessibility-host-wrapper";
-        wrapper.style.position = "absolute";
-        wrapper.style.left = "-999px";
-        wrapper.style.width = "900px";
-        scene.getEngine().getRenderingCanvas()?.after(wrapper);
-        scene.onDisposeObservable.add(() => {
-            wrapper.remove();
-        });
-
         const htmlTwinHost = React.createElement(HTMLTwinHostComponent, {
             scene,
         });
-        ReactDOM.render(htmlTwinHost, wrapper);
+        ReactDOM.render(htmlTwinHost, scene.getEngine().getRenderingCanvas());
     }
 }
