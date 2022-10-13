b8b4cd522ad4ca275360f1a1d1f4a4020cf17c01
Carolina Herbster <carolhmj@gmail.com>
2022-10-13 16:49:29 -0300

    Extract initial layout to separate file.

--- packages/tools/vsm/src/workbench.tsx
+++ packages/tools/vsm/src/workbench.tsx
@@ -2,8 +2,8 @@ import type { FC } from "react";
 import { useState } from "react";
 import { CommandBarComponent } from "shared-ui-components/components/bars/CommandBarComponent";
 import { FlexibleGridLayout } from "shared-ui-components/components/layout/FlexibleGridLayout";
-import { TestComponent } from "./testComponent";
 import style from "./workbench.modules.scss";
+import { initialLayout } from "./initialLayout";
 
 export type WorkbenchProps = {};
 
@@ -20,46 +20,7 @@ export const Workbench: FC<WorkbenchProps> = () => {
                 onArtboardColorChanged={(newColor) => setWorkAreaColor(newColor)}
             />
             <div className={style.workArea} style={{ backgroundColor: workAreaColor }}>
-                <FlexibleGridLayout
-                    layoutDefinition={{
-                        columns: [
-                            {
-                                id: "column1",
-                                width: "25%",
-                                rows: [
-                                    { id: "row1", selectedTab: "c1", height: "30%", tabs: [{ id: "c1", component: <TestComponent name="c1" /> }] },
-                                    {
-                                        id: "row2",
-                                        height: "20%",
-                                        selectedTab: "c3",
-                                        tabs: [
-                                            { id: "c2", component: <TestComponent name="c2" /> },
-                                            { id: "c3", component: <TestComponent name="c3" /> },
-                                            { id: "c4", component: <TestComponent name="c4" /> },
-                                        ],
-                                    },
-                                    { id: "row3", selectedTab: "c5", height: "50%", tabs: [{ id: "c5", component: <TestComponent name="c5" /> }] },
-                                ],
-                            },
-                            {
-                                id: "column2",
-                                width: "50%",
-                                rows: [
-                                    { id: "row4", selectedTab: "c6", height: "70%", tabs: [{ id: "c6", component: <TestComponent name="c6" /> }] },
-                                    { id: "row5", selectedTab: "c7", height: "30%", tabs: [{ id: "c7", component: <TestComponent name="c7" /> }] },
-                                ],
-                            },
-                            {
-                                id: "column3",
-                                width: "25%",
-                                rows: [
-                                    { id: "row6", selectedTab: "c8", height: "50%", tabs: [{ id: "c8", component: <TestComponent name="c8" /> }] },
-                                    { id: "row7", selectedTab: "c9", height: "50%", tabs: [{ id: "c9", component: <TestComponent name="c9" /> }] },
-                                ],
-                            },
-                        ],
-                    }}
-                />
+                <FlexibleGridLayout layoutDefinition={initialLayout} />
             </div>
         </div>
     );
