1f709fbdda50fd80b4e9d39dd53d9a9ae58845bb
Carolina Herbster <carolhmj@gmail.com>
2022-10-13 16:46:33 -0300

    Start work on components.
    Former-commit-id: 4c22db70d166056ebbec4f72da6f3cb72ac2afdc

--- packages/tools/vsm/src/workbench.tsx
+++ packages/tools/vsm/src/workbench.tsx
@@ -1,9 +1,14 @@
+import type { Scene } from "core/scene";
+import type { Nullable } from "core/types";
 import type { FC } from "react";
 import { useState } from "react";
 import { CommandBarComponent } from "shared-ui-components/components/bars/CommandBarComponent";
 import { FlexibleGridLayout } from "shared-ui-components/components/layout/FlexibleGridLayout";
+import { SceneContext } from "./SceneContext";
 import { TestComponent } from "./testComponent";
+import { SceneRendererComponent } from "./components/SceneRendererComponent";
 import style from "./workbench.modules.scss";
+import { StatesViewComponent } from "./components/StatesViewComponent";
 
 export type WorkbenchProps = {};
 
@@ -12,55 +17,48 @@ const INITIAL_WORKBENCH_COLOR = "#AAAAAA";
 
 export const Workbench: FC<WorkbenchProps> = () => {
     const [workAreaColor, setWorkAreaColor] = useState(INITIAL_WORKBENCH_COLOR);
+    const [scene, setScene] = useState<Nullable<Scene>>(null);
+
     return (
-        <div className={style.workbenchContainer}>
-            <CommandBarComponent
-                artboardColor={workAreaColor}
-                artboardColorPickerColor={INITIAL_WORKBENCH_COLOR}
-                onArtboardColorChanged={(newColor) => setWorkAreaColor(newColor)}
-            />
-            <div className={style.workArea} style={{ backgroundColor: workAreaColor }}>
-                <FlexibleGridLayout
-                    layoutDefinition={{
-                        columns: [
-                            {
-                                id: "column1",
-                                width: "25%",
-                                rows: [
-                                    { id: "row1", selectedTab: "c1", height: "30%", tabs: [{ id: "c1", component: <TestComponent name="c1" /> }] },
-                                    {
-                                        id: "row2",
-                                        height: "20%",
-                                        selectedTab: "c3",
-                                        tabs: [
-                                            { id: "c2", component: <TestComponent name="c2" /> },
-                                            { id: "c3", component: <TestComponent name="c3" /> },
-                                            { id: "c4", component: <TestComponent name="c4" /> },
-                                        ],
-                                    },
-                                    { id: "row3", selectedTab: "c5", height: "50%", tabs: [{ id: "c5", component: <TestComponent name="c5" /> }] },
-                                ],
-                            },
-                            {
-                                id: "column2",
-                                width: "50%",
-                                rows: [
-                                    { id: "row4", selectedTab: "c6", height: "70%", tabs: [{ id: "c6", component: <TestComponent name="c6" /> }] },
-                                    { id: "row5", selectedTab: "c7", height: "30%", tabs: [{ id: "c7", component: <TestComponent name="c7" /> }] },
-                                ],
-                            },
-                            {
-                                id: "column3",
-                                width: "25%",
-                                rows: [
-                                    { id: "row6", selectedTab: "c8", height: "50%", tabs: [{ id: "c8", component: <TestComponent name="c8" /> }] },
-                                    { id: "row7", selectedTab: "c9", height: "50%", tabs: [{ id: "c9", component: <TestComponent name="c9" /> }] },
-                                ],
-                            },
-                        ],
-                    }}
+        <SceneContext.Provider value={{ scene, setScene }}>
+            <div className={style.workbenchContainer}>
+                <CommandBarComponent
+                    artboardColor={workAreaColor}
+                    artboardColorPickerColor={INITIAL_WORKBENCH_COLOR}
+                    onArtboardColorChanged={(newColor) => setWorkAreaColor(newColor)}
                 />
+                <div className={style.workArea} style={{ backgroundColor: workAreaColor }}>
+                    <FlexibleGridLayout
+                        layoutDefinition={{
+                            columns: [
+                                {
+                                    id: "column1",
+                                    width: "30%",
+                                    rows: [
+                                        {
+                                            id: "scene",
+                                            height: "100%",
+                                            selectedTab: "sceneTab",
+                                            tabs: [
+                                                { id: "sceneTab", title: "Scene", component: <SceneRendererComponent /> },
+                                                { id: "test", title: "Test", component: <TestComponent name="test" /> },
+                                            ],
+                                        },
+                                    ],
+                                },
+                                {
+                                    id: "column2",
+                                    width: "70%",
+                                    rows: [
+                                        { id: "row4", selectedTab: "statesView", height: "70%", tabs: [{ id: "statesView", component: <StatesViewComponent /> }] },
+                                        { id: "row5", selectedTab: "c7", height: "30%", tabs: [{ id: "c7", component: <TestComponent name="c7" /> }] },
+                                    ],
+                                },
+                            ],
+                        }}
+                    />
+                </div>
             </div>
-        </div>
+        </SceneContext.Provider>
     );
 };
