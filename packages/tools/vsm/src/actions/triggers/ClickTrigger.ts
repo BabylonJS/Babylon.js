4b723b20b58d1d432f0b4b6493d2000709510fab
Carolina <carolhmj@gmail.com>
2022-10-24 17:30:39 -0300

    Update hardcoded values
    Former-commit-id: 07d299e7ec127823baf5f537f1900b630b2c030c

--- packages/tools/vsm/src/actions/triggers/ClickTrigger.ts
+++ packages/tools/vsm/src/actions/triggers/ClickTrigger.ts
@@ -1,33 +1,17 @@
 import { PointerEventTypes } from "core/Events/pointerEvents";
 import type { AbstractMesh } from "core/Meshes/abstractMesh";
-import type { Scene } from "core/scene";
 import { BaseTrigger } from "./BaseTrigger";
 
 export class ClickTrigger extends BaseTrigger {
     private _object: AbstractMesh;
-    private _clickDuration = 500;
-    private _clicked = false;
 
     constructor(object: AbstractMesh) {
         super();
         this._object = object;
         this._object.getScene().onPointerObservable.add((pointerInfo) => {
             if (pointerInfo.pickInfo?.hit && pointerInfo.pickInfo.pickedMesh === this._object) {
-                this._clicked = true;
-                setTimeout(() => {
-                    this._clicked = false;
-                }, this._clickDuration);
-            } else {
-                this._clicked = false;
+                this._triggerOn();
             }
         }, PointerEventTypes.POINTERDOWN);
-    }
-
-    set clickDuration(duration: number) {
-        this._clickDuration = duration;
-    }
-
-    public condition(scene: Scene): boolean {
-        return this._clicked;
     }
 }
