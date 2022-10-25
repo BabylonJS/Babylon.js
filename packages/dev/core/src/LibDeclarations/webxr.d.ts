abec96dbd7e688903e367cea66a4747edffaf6a6
drumath <2237k.drumath@gmail.com>
2022-10-26 03:03:54 +0900

    fix linting issues

--- packages/dev/core/src/LibDeclarations/webxr.d.ts
+++ packages/dev/core/src/LibDeclarations/webxr.d.ts
@@ -1184,7 +1184,7 @@ interface XRCPUDepthInformation extends XRDepthInformation {
 }
 
 interface XRFrame {
-    getDepthInformation(view: XRView): XRCPUDepthInformation?;
+    getDepthInformation(view: XRView): ?XRCPUDepthInformation;
 }
 
 interface XRWebGLDepthInformation extends XRDepthInformation {
@@ -1192,7 +1192,7 @@ interface XRWebGLDepthInformation extends XRDepthInformation {
 }
 
 interface XRWebGLBinding {
-    getDepthInformation(view: XRView): XRWebGLDepthInformation?;
+    getDepthInformation(view: XRView): ?XRWebGLDepthInformation;
 }
 
 /**
