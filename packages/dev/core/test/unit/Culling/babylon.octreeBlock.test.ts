9cbdcdc6577a069be88b23dbd99810b096a214bb
Oleg Postoev <dok@artlebedev.ru>
2022-10-29 20:38:11 +0300

    (test) OctreeBlock.intersectsRay(). Add basic test case
    Former-commit-id: 2791e533371f85af3b1cc294954fb790aa70b626

--- packages/dev/core/test/unit/Culling/babylon.octreeBlock.test.ts
+++ packages/dev/core/test/unit/Culling/babylon.octreeBlock.test.ts
@@ -1,9 +1,12 @@
 import { ArcRotateCamera } from "core/Cameras";
+import type { Ray } from "core/Culling";
 import { OctreeSceneComponent } from "core/Culling";
 import type { Engine } from "core/Engines";
 import { NullEngine } from "core/Engines";
 import { Vector3 } from "core/Maths";
+import type { AbstractMesh } from "core/Meshes";
 import { MeshBuilder } from "core/Meshes";
+import { SmartArrayNoDuplicate } from "core/Misc";
 import { Scene } from "core/scene";
 
 describe("OctreeBlock", function () {
@@ -54,15 +57,42 @@ describe("OctreeBlock", function () {
         scene = new Scene(engine);
 
         new ArcRotateCamera("Camera", 0, Math.PI / 2, 5, new Vector3(0, 0, 0), scene);
+
+        boxPositions.forEach((pos, i) => {
+            MeshBuilder.CreateBox(`box_${i}`, { size: 1 }, scene).position.set(...pos);
+        });
+        scene.render();
+    });
+
+    describe("intersectsRay", () => {
+        it("should set selection with block entries", () => {
+            // Create octree
+            const component = new OctreeSceneComponent(scene);
+            scene.createOrUpdateSelectionOctree(4);
+
+            // Find first octree block with entry
+            const blockWithEntries = scene.selectionOctree.blocks[4];
+            expect(blockWithEntries).toBeDefined();
+            expect(blockWithEntries.entries.length).toEqual(1);
+
+            // Call intersectsRay
+            const ray = { intersectsBoxMinMax: (_min, _max) => true } as Ray;
+            const rayIntersectsBoxMinMaxSpy = jest.spyOn(ray, "intersectsBoxMinMax");
+            const selection = new SmartArrayNoDuplicate<AbstractMesh>(128);
+            blockWithEntries!.intersectsRay(ray, selection);
+
+            // Ray intersects should be called once and with actual vectors
+            expect(rayIntersectsBoxMinMaxSpy).toHaveBeenCalledTimes(1);
+            expect(rayIntersectsBoxMinMaxSpy).toHaveBeenCalledWith(new Vector3(2, -9.5, -8.5), new Vector3(8.5, 0, 0));
+
+            // Selection should contain the mesh from the block
+            expect(selection.length).toEqual(1);
+            expect(selection.data[0].name).toEqual("box_17");
+        });
     });
 
     describe("createInnerBlocks", () => {
         it("should clean block when after subdivide", () => {
-            boxPositions.forEach((pos, i) => {
-                MeshBuilder.CreateBox(`box_${i}`, { size: 1 }, scene).position.set(...pos);
-            });
-            scene.render();
-
             // Create octree
             const component = new OctreeSceneComponent(scene);
             scene.createOrUpdateSelectionOctree(4);
