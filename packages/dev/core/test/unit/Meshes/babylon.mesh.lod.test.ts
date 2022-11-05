221778b0943cb9d4c44e932bb13a298f0727e8eb
Oleg Postoev <dok@artlebedev.ru>
2022-11-05 14:44:46 +0300

    (test) Mesh.getLOD(). Test to check screen coverage

--- packages/dev/core/test/unit/Meshes/babylon.mesh.lod.test.ts
+++ packages/dev/core/test/unit/Meshes/babylon.mesh.lod.test.ts
@@ -61,112 +61,138 @@ describe('Babylon Mesh Levels of Details', () => {
                 p: 2,
                 q: 3,
             }, scene);
-
-            knot0.addLODLevel(10, knot1);
-            knot0.addLODLevel(20, knot2);
         });
 
-        it('should select lod with correct distance', () => {
-            expect(knot0.getLOD(cameraArc)).not.toBeNull();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
+        describe('check LOD by distance', () => {
+            beforeEach(() => {
+                knot0.addLODLevel(10, knot1);
+                knot0.addLODLevel(20, knot2);
+            });
 
-            cameraArc.radius = 15;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot1');
+            it('should select lod with correct distance', () => {
+                expect(knot0.getLOD(cameraArc)).not.toBeNull();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
 
-            cameraArc.radius = 25;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot2');
+                cameraArc.radius = 15;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot1');
+
+                cameraArc.radius = 25;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot2');
+            });
+
+            it('should select lod with correct distance for orthographic camera', () => {
+                expect(knot0.getLOD(cameraOrthographic)).not.toBeNull();
+                expect(knot0.getLOD(cameraOrthographic)!.name).toEqual('Knot0');
+
+                cameraOrthographic.minZ = 15;
+                scene.render();
+                expect(knot0.getLOD(cameraOrthographic)!.name).toEqual('Knot1');
+
+                cameraOrthographic.minZ = 25;
+                scene.render();
+                expect(knot0.getLOD(cameraOrthographic)!.name).toEqual('Knot2');
+            });
+
+            it('should select loaded mesh while target lod mesh is not loaded yet', () => {
+                // not loaded yet
+                knot1.delayLoadState = Constants.DELAYLOADSTATE_NOTLOADED;
+                knot2.delayLoadState = Constants.DELAYLOADSTATE_NOTLOADED;
+
+                cameraArc.radius = 15;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
+
+                cameraArc.radius = 25;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
+
+                // while loading
+                knot1.delayLoadState = Constants.DELAYLOADSTATE_LOADING;
+                knot2.delayLoadState = Constants.DELAYLOADSTATE_LOADING;
+
+                cameraArc.radius = 15;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
+
+                cameraArc.radius = 25;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
+
+                // after loaded
+                knot1.delayLoadState = Constants.DELAYLOADSTATE_LOADED;
+                knot2.delayLoadState = Constants.DELAYLOADSTATE_LOADED;
+
+                cameraArc.radius = 15;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot1');
+
+                cameraArc.radius = 25;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot2');
+            });
+
+            it('should call user function with selected LOD', () => {
+                const onLODLevelSelectionArgs: {
+                    distance?: number;
+                    meshName?: string;
+                    selectedLevel?: string | null;
+                } = {};
+                knot0.onLODLevelSelection = (_distance, _mesh, _selectedLevel) => {
+                    onLODLevelSelectionArgs.distance = _distance;
+                    onLODLevelSelectionArgs.meshName = _mesh.name;
+                    onLODLevelSelectionArgs.selectedLevel = _selectedLevel?.name;
+                    return null;
+                };
+
+                const registerSpy = jest.spyOn(knot0, "onLODLevelSelection");
+
+                expect(registerSpy).toBeCalledTimes(0);
+
+                scene.render();
+                expect(registerSpy).toBeCalledTimes(1);
+                expect(onLODLevelSelectionArgs.distance).toBeCloseTo(5.23, 2);
+                expect(onLODLevelSelectionArgs.meshName).toEqual('Knot0');
+                expect(onLODLevelSelectionArgs.selectedLevel).toEqual('Knot0');
+
+                cameraArc.radius = 15;
+                scene.render();
+                expect(registerSpy).toBeCalledTimes(2);
+                expect(onLODLevelSelectionArgs.distance).toBeCloseTo(15.07, 2);
+                expect(onLODLevelSelectionArgs.meshName).toEqual('Knot0');
+                expect(onLODLevelSelectionArgs.selectedLevel).toEqual('Knot1');
+
+                cameraArc.radius = 25;
+                scene.render();
+                expect(registerSpy).toBeCalledTimes(3);
+                expect(onLODLevelSelectionArgs.distance).toBeCloseTo(25.03, 2);
+                expect(onLODLevelSelectionArgs.meshName).toEqual('Knot0');
+                expect(onLODLevelSelectionArgs.selectedLevel).toEqual('Knot2');
+            });
         });
 
-        it('should select lod with correct distance for orthographic camera', () => {
-            expect(knot0.getLOD(cameraOrthographic)).not.toBeNull();
-            expect(knot0.getLOD(cameraOrthographic)!.name).toEqual('Knot0');
+        describe('check LOD by screen coverage', () => {
+            beforeEach(() => {
+                knot0.useLODScreenCoverage = true;
+                knot0.addLODLevel(0.5, knot1);
+                knot0.addLODLevel(0.2, knot2);
+                knot0.addLODLevel(0.02, null);
+            });
 
-            cameraOrthographic.minZ = 15;
-            scene.render();
-            expect(knot0.getLOD(cameraOrthographic)!.name).toEqual('Knot1');
+            it('should select lod with correct screen coverage', () => {
+                cameraArc.radius = 80;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot1');
 
-            cameraOrthographic.minZ = 25;
-            scene.render();
-            expect(knot0.getLOD(cameraOrthographic)!.name).toEqual('Knot2');
+                cameraArc.radius = 120;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot2');
+
+                cameraArc.radius = 380;
+                scene.render();
+                expect(knot0.getLOD(cameraArc)).toBeNull();
+            });
         });
-
-        it('should select loaded mesh while target lod mesh is not loaded yet', () => {
-            // not loaded yet
-            knot1.delayLoadState = Constants.DELAYLOADSTATE_NOTLOADED;
-            knot2.delayLoadState = Constants.DELAYLOADSTATE_NOTLOADED;
-
-            cameraArc.radius = 15;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
-
-            cameraArc.radius = 25;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
-
-            // while loading
-            knot1.delayLoadState = Constants.DELAYLOADSTATE_LOADING;
-            knot2.delayLoadState = Constants.DELAYLOADSTATE_LOADING;
-
-            cameraArc.radius = 15;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
-
-            cameraArc.radius = 25;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot0');
-
-            // after loaded
-            knot1.delayLoadState = Constants.DELAYLOADSTATE_LOADED;
-            knot2.delayLoadState = Constants.DELAYLOADSTATE_LOADED;
-
-            cameraArc.radius = 15;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot1');
-
-            cameraArc.radius = 25;
-            scene.render();
-            expect(knot0.getLOD(cameraArc)!.name).toEqual('Knot2');
-        });
-
-        it('should call user function with selected LOD', () => {
-            const onLODLevelSelectionArgs: {
-                distance?: number;
-                meshName?: string;
-                selectedLevel?: string | null;
-            } = {};
-            knot0.onLODLevelSelection = (_distance, _mesh, _selectedLevel) => {
-                onLODLevelSelectionArgs.distance = _distance;
-                onLODLevelSelectionArgs.meshName = _mesh.name;
-                onLODLevelSelectionArgs.selectedLevel = _selectedLevel?.name;
-                return null;
-            };
-
-            const registerSpy = jest.spyOn(knot0, "onLODLevelSelection");
-
-            expect(registerSpy).toBeCalledTimes(0);
-
-            scene.render();
-            expect(registerSpy).toBeCalledTimes(1);
-            expect(onLODLevelSelectionArgs.distance).toBeCloseTo(5.23, 2);
-            expect(onLODLevelSelectionArgs.meshName).toEqual('Knot0');
-            expect(onLODLevelSelectionArgs.selectedLevel).toEqual('Knot0');
-
-            cameraArc.radius = 15;
-            scene.render();
-            expect(registerSpy).toBeCalledTimes(2);
-            expect(onLODLevelSelectionArgs.distance).toBeCloseTo(15.07, 2);
-            expect(onLODLevelSelectionArgs.meshName).toEqual('Knot0');
-            expect(onLODLevelSelectionArgs.selectedLevel).toEqual('Knot1');
-
-            cameraArc.radius = 25;
-            scene.render();
-            expect(registerSpy).toBeCalledTimes(3);
-            expect(onLODLevelSelectionArgs.distance).toBeCloseTo(25.03, 2);
-            expect(onLODLevelSelectionArgs.meshName).toEqual('Knot0');
-            expect(onLODLevelSelectionArgs.selectedLevel).toEqual('Knot2');
-        });
-
     });
 });
