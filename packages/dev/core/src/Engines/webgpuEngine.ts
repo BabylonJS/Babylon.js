1ac2f379718b7e1bea660b34b3def386683de87c
Raanan Weber <Raanan.Weber@microsoft.com>
2022-11-10 14:27:00 +0100

    redirects

--- packages/dev/core/src/Engines/webgpuEngine.ts
+++ packages/dev/core/src/Engines/webgpuEngine.ts
@@ -481,7 +481,7 @@ export class WebGPUEngine extends Engine {
     /**
      * (WebGPU only) True (default) to be in compatibility mode, meaning rendering all existing scenes without artifacts (same rendering than WebGL).
      * Setting the property to false will improve performances but may not work in some scenes if some precautions are not taken.
-     * See https://doc.babylonjs.com/advanced_topics/webGPU/webGPUOptimization/webGPUNonCompatibilityMode for more details
+     * See https://doc.babylonjs.com/setup/support/webGPU/webGPUOptimization/webGPUNonCompatibilityMode for more details
      */
     public get compatibilityMode() {
         return this._compatibilityMode;
