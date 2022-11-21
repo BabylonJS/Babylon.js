adadd447679687a95469bb28b9090a71f4a108ce
Evgeni Popov <github@evpopov.com>
2022-11-21 18:12:36 +0100

    Fix duplicate variable names
    Former-commit-id: 3bba594edfa8e6a538fdc6eb07ad6eaf016e5080

--- packages/dev/core/src/Materials/Node/Blocks/biPlanarBlock.ts
+++ packages/dev/core/src/Materials/Node/Blocks/biPlanarBlock.ts
@@ -27,39 +27,49 @@ export class BiPlanarBlock extends TriPlanarBlock {
 
         const sharpness = this.sharpness.isConnected ? this.sharpness.associatedVariableName : "1.0";
 
+        const dpdx = state._getFreeVariableName("dpdx");
+        const dpdy = state._getFreeVariableName("dpdy");
+        const n = state._getFreeVariableName("n");
+        const ma = state._getFreeVariableName("ma");
+        const mi = state._getFreeVariableName("mi");
+        const me = state._getFreeVariableName("me");
+        const x = state._getFreeVariableName("x");
+        const y = state._getFreeVariableName("y");
+        const w = state._getFreeVariableName("y");
+
         state.compilationString += `
             // grab coord derivatives for texturing
-            vec3 dpdx = dFdx(${this.position.associatedVariableName}.xyz);
-            vec3 dpdy = dFdy(${this.position.associatedVariableName}.xyz);
-            vec3 n = abs(${this.normal.associatedVariableName}.xyz);
+            vec3 ${dpdx} = dFdx(${this.position.associatedVariableName}.xyz);
+            vec3 ${dpdy} = dFdy(${this.position.associatedVariableName}.xyz);
+            vec3 ${n} = abs(${this.normal.associatedVariableName}.xyz);
         
             // determine major axis (in x; yz are following axis)
-            ivec3 ma = (n.x>n.y && n.x>n.z) ? ivec3(0,1,2) :
-                    (n.y>n.z)            ? ivec3(1,2,0) :
+            ivec3 ${ma} = (${n}.x>${n}.y && ${n}.x>${n}.z) ? ivec3(0,1,2) :
+                    (${n}.y>${n}.z)            ? ivec3(1,2,0) :
                                             ivec3(2,0,1) ;
             // determine minor axis (in x; yz are following axis)
-            ivec3 mi = (n.x<n.y && n.x<n.z) ? ivec3(0,1,2) :
-                    (n.y<n.z)            ? ivec3(1,2,0) :
+            ivec3 ${mi} = (${n}.x<${n}.y && ${n}.x<${n}.z) ? ivec3(0,1,2) :
+                    (${n}.y<${n}.z)            ? ivec3(1,2,0) :
                                             ivec3(2,0,1) ;
             // determine median axis (in x;  yz are following axis)
-            ivec3 me = ivec3(3) - mi - ma;
+            ivec3 ${me} = ivec3(3) - ${mi} - ${ma};
             
             // project+fetch
-            vec4 x = textureGrad( ${samplerName}, vec2(   ${this.position.associatedVariableName}[ma.y],   ${this.position.associatedVariableName}[ma.z]), 
-                                    vec2(dpdx[ma.y],dpdx[ma.z]), 
-                                    vec2(dpdy[ma.y],dpdy[ma.z]) );
-            vec4 y = textureGrad( ${samplerName}, vec2(   ${this.position.associatedVariableName}[me.y],   ${this.position.associatedVariableName}[me.z]), 
-                                    vec2(dpdx[me.y],dpdx[me.z]),
-                                    vec2(dpdy[me.y],dpdy[me.z]) );
+            vec4 ${x} = textureGrad( ${samplerName}, vec2(   ${this.position.associatedVariableName}[${ma}.y],   ${this.position.associatedVariableName}[${ma}.z]), 
+                                    vec2(${dpdx}[${ma}.y],${dpdx}[${ma}.z]), 
+                                    vec2(${dpdy}[${ma}.y],${dpdy}[${ma}.z]) );
+            vec4 ${y} = textureGrad( ${samplerName}, vec2(   ${this.position.associatedVariableName}[${me}.y],   ${this.position.associatedVariableName}[${me}.z]), 
+                                    vec2(${dpdx}[${me}.y],${dpdx}[${me}.z]),
+                                    vec2(${dpdy}[${me}.y],${dpdy}[${me}.z]) );
             
             // blend factors
-            vec2 w = vec2(n[ma.x],n[me.x]);
+            vec2 ${w} = vec2(${n}[${ma}.x],${n}[${me}.x]);
             // make local support
-            w = clamp( (w-0.5773)/(1.0-0.5773), 0.0, 1.0 );
+            ${w} = clamp( (${w}-0.5773)/(1.0-0.5773), 0.0, 1.0 );
             // shape transition
-            w = pow( w, vec2(${sharpness}/8.0) );
+            ${w} = pow( ${w}, vec2(${sharpness}/8.0) );
             // blend and return
-            vec4 ${this._tempTextureRead} = (x*w.x + y*w.y) / (w.x + w.y);
+            vec4 ${this._tempTextureRead} = (${x}*${w}.x + ${y}*${w}.y) / (${w}.x + ${w}.y);
         `;
     }
 }
