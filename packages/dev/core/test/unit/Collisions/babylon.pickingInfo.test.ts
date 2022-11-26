27718d3bbd654b7c6f4001c15bd8dc41a548efa1
Oleg Postoev <dok@artlebedev.ru>
2022-11-26 18:49:49 +0300

    (test) pickingInfo.getNormal. Add test case to check useVerticesNormals
    Former-commit-id: 21d3b9a87509555fb903952859ed00e1c5ed51d4

--- packages/dev/core/test/unit/Collisions/babylon.pickingInfo.test.ts
+++ packages/dev/core/test/unit/Collisions/babylon.pickingInfo.test.ts
@@ -1,6 +1,7 @@
 import { PickingInfo } from "core/Collisions";
 import type { Engine } from "core/Engines";
 import { NullEngine } from "core/Engines";
+import { Vector3 } from "core/Maths";
 import type { Mesh } from "core/Meshes";
 import { MeshBuilder } from "core/Meshes";
 import { Scene } from "core/scene";
@@ -9,6 +10,7 @@ describe("PickingInfo", () => {
     let subject: Engine;
     let scene: Scene;
     let box: Mesh;
+    let torusKnot: Mesh;
 
     beforeEach(() => {
         subject = new NullEngine({
@@ -19,6 +21,19 @@ describe("PickingInfo", () => {
             lockstepMaxSteps: 1,
         });
         scene = new Scene(subject);
+
+        torusKnot = MeshBuilder.CreateTorusKnot(
+            "Knot",
+            {
+                radius: 10,
+                tube: 3,
+                radialSegments: 32,
+                tubularSegments: 8,
+                p: 2,
+                q: 3,
+            },
+            scene
+        );
 
         box = MeshBuilder.CreateBox("Box", { size: 1 }, scene);
     });
@@ -47,6 +62,58 @@ describe("PickingInfo", () => {
             pickingInfo.pickedMesh = box;
 
             expect(pickingInfo.getNormal()).toBeNull();
+        });
+
+        it("should return normal when useVerticesNormals is true", () => {
+            const pickingInfo = new PickingInfo();
+            pickingInfo.pickedMesh = box;
+            pickingInfo.faceId = 0;
+            pickingInfo.bu = 0.5;
+            pickingInfo.bv = 0.5;
+
+            const normalBox = pickingInfo.getNormal(false, true);
+
+            expect(normalBox).toBeInstanceOf(Vector3);
+            expect(normalBox!.x).toBeCloseTo(0);
+            expect(normalBox!.y).toBeCloseTo(0);
+            expect(normalBox!.z).toBeCloseTo(1);
+
+            // And test with the knot
+
+            pickingInfo.pickedMesh = torusKnot;
+
+            const normal = pickingInfo.getNormal(false, true);
+
+            expect(normal).toBeInstanceOf(Vector3);
+            expect(normal!.x).toBeCloseTo(-0.84);
+            expect(normal!.y).toBeCloseTo(-0.24);
+            expect(normal!.z).toBeCloseTo(-0.48);
+        });
+
+        it("should return normal when useVerticesNormals is false", () => {
+            const pickingInfo = new PickingInfo();
+            pickingInfo.pickedMesh = box;
+            pickingInfo.faceId = 0;
+            pickingInfo.bu = 0.5;
+            pickingInfo.bv = 0.5;
+
+            const normalBox = pickingInfo.getNormal(false, false);
+
+            expect(normalBox).toBeInstanceOf(Vector3);
+            expect(normalBox!.x).toBeCloseTo(0);
+            expect(normalBox!.y).toBeCloseTo(0);
+            expect(normalBox!.z).toBeCloseTo(1);
+
+            // And test with the knot
+
+            pickingInfo.pickedMesh = torusKnot;
+
+            const normal = pickingInfo.getNormal(false, false);
+
+            expect(normal).toBeInstanceOf(Vector3);
+            expect(normal!.x).toBeCloseTo(-0.89);
+            expect(normal!.y).toBeCloseTo(-0.08);
+            expect(normal!.z).toBeCloseTo(-0.45);
         });
     });
 });
