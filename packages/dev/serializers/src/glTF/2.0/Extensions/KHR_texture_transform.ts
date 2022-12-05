5d7bf2392d70b6650ed2e87af38f4d48a6cf3832
Carolina <carolhmj@gmail.com>
2022-12-05 12:17:46 -0300

    Address review.

--- packages/dev/serializers/src/glTF/2.0/Extensions/KHR_texture_transform.ts
+++ packages/dev/serializers/src/glTF/2.0/Extensions/KHR_texture_transform.ts
@@ -83,15 +83,14 @@ export class KHR_texture_transform implements IGLTFExporterExtensionV2 {
                 return;
             }
 
-            if (babylonTexture.uAng !== 0 || babylonTexture.vAng !== 0) {
-                Tools.Warn(`${context}: Texture ${babylonTexture.name} with rotation in the u or v axis is not supported in glTF.`);
-                resolve(null);
-            } else 
             /*
              * The KHR_texture_transform schema only supports w rotation around the origin.
              * See https://github.com/KhronosGroup/glTF/tree/main/extensions/2.0/Khronos/KHR_texture_transform#gltf-schema-updates.
              */
-            if (babylonTexture.wAng !== 0 && (babylonTexture.uRotationCenter !== 0 || babylonTexture.vRotationCenter !== 0)) {
+            if (babylonTexture.uAng !== 0 || babylonTexture.vAng !== 0) {
+                Tools.Warn(`${context}: Texture ${babylonTexture.name} with rotation in the u or v axis is not supported in glTF.`);
+                resolve(null);
+            } else if (babylonTexture.wAng !== 0 && (babylonTexture.uRotationCenter !== 0 || babylonTexture.vRotationCenter !== 0)) {
                 Tools.Warn(`${context}: Texture ${babylonTexture.name} with rotation not centered at the origin cannot be exported with ${NAME}`);
                 resolve(null);
             } else {
