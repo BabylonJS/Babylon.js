6d63ce73005f72a2fdccacd6ad64fd50b31428d9
Tommy Boggs <tboggs300@gmail.com>
2022-12-05 20:33:15 -0500

    Support KHR_materials_emissive_stength in glTF export (#13303)
    Former-commit-id: 03eab929bc36ebcaf742493e9c0f320be4e3e4d8

---
+++ packages/dev/serializers/src/glTF/2.0/Extensions/KHR_materials_emissive_strength.ts
@@ -0,0 +1,62 @@
+import type { IGLTFExporterExtensionV2 } from "../glTFExporterExtension";
+import { _Exporter } from "../glTFExporter";
+import type { Material } from "core/Materials/material";
+import { PBRMaterial } from "core/Materials/PBR/pbrMaterial";
+import type { IMaterial, IKHRMaterialsEmissiveStrength } from "babylonjs-gltf2interface";
+
+const NAME = "KHR_materials_emissive_strength";
+
+/**
+ * [Specification](https://github.com/KhronosGroup/glTF/blob/main/extensions/2.0/Khronos/KHR_materials_emissive_strength/README.md)
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export class KHR_materials_emissive_strength implements IGLTFExporterExtensionV2 {
+    /** Name of this extension */
+    public readonly name = NAME;
+
+    /** Defines whether this extension is enabled */
+    public enabled = true;
+
+    /** Defines whether this extension is required */
+    public required = false;
+
+    private _wasUsed = false;
+
+    public dispose() {}
+
+    /** @interal */
+    public get wasUsed() {
+        return this._wasUsed;
+    }
+
+    public postExportMaterialAsync(context: string, node: IMaterial, babylonMaterial: Material): Promise<IMaterial> {
+        return new Promise((resolve) => {
+            if (!(babylonMaterial instanceof PBRMaterial)) {
+                return resolve(node);
+            }
+
+            const emissiveColor = babylonMaterial.emissiveColor.asArray();
+            const tempEmissiveStrength = Math.max(...emissiveColor);
+
+            if (tempEmissiveStrength > 1) {
+                this._wasUsed = true;
+
+                node.extensions ||= {};
+
+                const emissiveStrengthInfo: IKHRMaterialsEmissiveStrength = {
+                    emissiveStrength: tempEmissiveStrength,
+                };
+
+                // Normalize each value of the emissive factor to have a max value of 1
+                const newEmissiveFactor = babylonMaterial.emissiveColor.scale(1 / emissiveStrengthInfo.emissiveStrength);
+
+                node.emissiveFactor = newEmissiveFactor.asArray();
+                node.extensions[NAME] = emissiveStrengthInfo;
+            }
+
+            return resolve(node);
+        });
+    }
+}
+
+_Exporter.RegisterExtension(NAME, (exporter) => new KHR_materials_emissive_strength());
