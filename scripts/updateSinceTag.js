2fa17d8ce395dcf0da46cad3f63f9c05341bb43a
Raanan Weber <info@raananweber.com>
2022-12-15 19:26:24 +0100

    Allow adding a dynamic @since tag that adjusts automatically (#13348)
    * Allow adding a dynamic @since tag that adjusts automatically
    Fixes #13209
    
    * stay safe
    
    * Fix formatting
    
    * cleanup

---
+++ scripts/updateSinceTag.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require("fs");
+const glob = require("glob");
+
+const baseDirectory = path.resolve(".");
+
+const getNewVersion = () => {
+    // get @dev/core package.json
+    const rawdata = fs.readFileSync(path.join(baseDirectory, "packages", "public", "umd", "babylonjs", "package.json"), "utf-8");
+    const packageJson = JSON.parse(rawdata);
+    const version = packageJson.version;
+    return version;
+};
+
+const updateSinceTag = (version) => {
+    // get all typescript files in the dev folder
+    const files = glob.sync(path.join(baseDirectory, "packages", "dev", "**", "*.ts"));
+    files.forEach((file) => {
+        try {
+            // check if file contains @since\n
+            const data = fs.readFileSync(file, "utf-8").replace(/\r/gm, "");
+            if (data.indexOf("* @since\n") !== -1) {
+                console.log(`Updating @since tag in ${file} to ${version}`);
+                // replace @since with @since version
+                const newData = data.replace(
+                    /\* @since\n/gm,
+                    `* @since ${version}
+`
+                );
+
+                // write file
+                fs.writeFileSync(file, newData);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    });
+};
+
+const version = getNewVersion();
+updateSinceTag(version);
