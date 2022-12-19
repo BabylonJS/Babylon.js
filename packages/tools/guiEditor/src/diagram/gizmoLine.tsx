22eb08acb4c56edf649ba3b0cf42f0bbf83c361a
Carolina Herbster <carolhmj@gmail.com>
2022-12-19 16:38:46 -0300

    Fix calculation of coordinates

--- packages/tools/guiEditor/src/diagram/gizmoLine.tsx
+++ packages/tools/guiEditor/src/diagram/gizmoLine.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import type { GlobalState } from "../globalState";
 import type { IScalePoint } from "./gizmoScalePoint";
 import { GizmoScalePoint, ScalePointPosition } from "./gizmoScalePoint";
-import { CoordinateHelper } from "./coordinateHelper";
 import { Vector2 } from "core/Maths/math";
 import type { Line } from "gui/2D/controls/line";
 
@@ -15,9 +14,9 @@ export function GizmoLine(props: IGizmoLineProps) {
     const { control, globalState } = props;
 
     const [scalePoints, setScalePoints] = React.useState<IScalePoint[]>([
-        { position: new Vector2(), horizontalPosition: ScalePointPosition.Left, verticalPosition: ScalePointPosition.Center, rotation: 0, isPivot: false, defaultRotation: 0 },
+        { position: new Vector2(), horizontalPosition: ScalePointPosition.Left, verticalPosition: ScalePointPosition.Top, rotation: 0, isPivot: false, defaultRotation: 0 },
         { position: new Vector2(), horizontalPosition: ScalePointPosition.Center, verticalPosition: ScalePointPosition.Center, rotation: 0, isPivot: true, defaultRotation: 0 },
-        { position: new Vector2(), horizontalPosition: ScalePointPosition.Right, verticalPosition: ScalePointPosition.Center, rotation: 0, isPivot: false, defaultRotation: 0 },
+        { position: new Vector2(), horizontalPosition: ScalePointPosition.Right, verticalPosition: ScalePointPosition.Bottom, rotation: 0, isPivot: false, defaultRotation: 0 },
     ]);
 
     React.useEffect(() => {
@@ -32,27 +31,34 @@ export function GizmoLine(props: IGizmoLineProps) {
     }, []);
 
     const update = () => {
-        const line = control as any;
-        const offsetX = line._cachedParentMeasure.width * 0.5;
-        const offsetY = line._cachedParentMeasure.height * 0.5;
-        const x1 = line._x1.getValue(control.host) - offsetX;
-        const y1 = line._y1.getValue(control.host) - offsetY;
-        const x2 = line._effectiveX2 - offsetX;
-        const y2 = line._effectiveY2 - offsetY;
+        const x1 = control._currentMeasure.left;
+        const y1 = control._currentMeasure.top;
+        const x2 = control._currentMeasure.width + x1;
+        const y2 = control._currentMeasure.height + y1;
 
-        const positions = [
-            new Vector2(x1, y1),
-            new Vector2((control.transformCenterX - 0.5) * control.widthInPixels, (control.transformCenterY - 0.5) * control.heightInPixels),
-            new Vector2(x2, y2),
-        ];
+        const v1 = new Vector2(x1, y1);
+        const v2 = new Vector2(x2, y2);
+
+        const matrix = control._transformMatrix;
+
+        const p1 = new Vector2();
+        const p2 = new Vector2();
+
+        matrix.transformCoordinates(v1.x, v1.y, p1);
+        matrix.transformCoordinates(v2.x, v2.y, p2);
+
+        // Get middle
+        const xm = (p1.x + p2.x) * 0.5;
+        const ym = (p1.y + p2.y) * 0.5;
+
+        const positions = [new Vector2(p1.x, p1.y), new Vector2(xm, ym), new Vector2(p2.x, p2.y)];
+
         setScalePoints(
             scalePoints.map((point, index) => {
                 const position = positions[index];
-                const rtt = CoordinateHelper.NodeToRTTSpace(control, position.x, position.y);
-                const canvas = CoordinateHelper.RttToCanvasSpace(rtt.x, rtt.y);
                 return {
                     ...point,
-                    position: canvas,
+                    position,
                     rotation: control.rotation,
                 };
             })
@@ -61,16 +67,7 @@ export function GizmoLine(props: IGizmoLineProps) {
     return (
         <div className="gizmo">
             {scalePoints.map((point, index) => (
-                <GizmoScalePoint
-                    scalePoint={point}
-                    clickable={true}
-                    onDrag={() => {}}
-                    onRotate={() => {}}
-                    onUp={() => {}}
-                    key={index}
-                    overrideCursor="not-allowed"
-                    canRotate={false}
-                />
+                <GizmoScalePoint scalePoint={point} clickable={true} onDrag={() => {}} onRotate={() => {}} onUp={() => {}} key={index} canRotate={true} />
             ))}
         </div>
     );
