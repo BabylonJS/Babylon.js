c83fdfebcb7f1e6dc5d702fcdbe8bca61ee24967
drumath2237 <2237k.drumath@gmail.com>
2023-01-08 17:51:38 +0900

    add: WebXR Depth Sensing Feature class implementation

---
+++ packages/dev/core/src/XR/features/WebXRDepthSensing.ts
@@ -0,0 +1,166 @@
+import { WebXRFeatureName, WebXRFeaturesManager } from "./../webXRFeaturesManager";
+import { Observable } from "../../Misc/observable";
+import type { WebXRSessionManager } from "../webXRSessionManager";
+import { WebXRAbstractFeature } from "./WebXRAbstractFeature";
+import { Tools } from "../../Misc/tools";
+
+/**
+ * Options for Depth Sensing feature
+ */
+export interface IWebXRDepthSensingOptions {
+    /**
+     *  The desired depth sensing usage for the session
+     */
+    usagePreference: XRDepthUsage[];
+    /**
+     * The desired depth sensing data format for the session
+     */
+    dataFormatPreference: XRDepthDataFormat[];
+}
+
+/**
+ * WebXR Feature for WebXR Depth Sensing Module
+ */
+export class WebXRDepthSensing extends WebXRAbstractFeature {
+    /**
+     * An observable which triggered when the CPU depth information is acquired from XRFrame
+     */
+    public onCPUDepthInformationObservable: Observable<XRCPUDepthInformation> = new Observable();
+
+    /**
+     *An observable which triggered when the WebGL depth information is acquired from XRFrame
+     */
+    public onWebGLDepthInformationObservable: Observable<XRWebGLDepthInformation> = new Observable();
+
+    /**
+     * XRWebGLBinding which is used for acquiring WebGLDepthInformation
+     */
+    private _glBinding?: XRWebGLBinding;
+
+    /**
+     * The module's name
+     */
+    public static readonly Name = WebXRFeatureName.DEPTH_SENSING;
+
+    /**
+     * The (Babylon) version of this module.
+     * This is an integer representing the implementation version.
+     * This number does not correspond to the WebXR specs version
+     */
+    public static readonly Version = 1;
+
+    /**
+     * Creates a new instance of the depth sensing feature
+     * @param _xrSessionManager the WebXRSessionManager
+     * @param options options for WebXR Depth Sensing Feature
+     */
+    constructor(_xrSessionManager: WebXRSessionManager, public readonly options: IWebXRDepthSensingOptions) {
+        super(_xrSessionManager);
+        this.xrNativeFeatureName = "depth-sensing";
+
+        // https://immersive-web.github.io/depth-sensing/
+        Tools.Warn("depth-sensing is an experimental and unstable feature.");
+    }
+
+    /**
+     * attach this feature
+     * Will usually be called by the features manager
+     *
+     * @returns true if successful.
+     */
+    public attach(force?: boolean | undefined): boolean {
+        if (!super.attach(force)) {
+            return false;
+        }
+
+        const isDepthUsageAndFormatNull = this._xrSessionManager.session.depthDataFormat == null || this._xrSessionManager.session.depthUsage == null;
+        if (isDepthUsageAndFormatNull) {
+            return false;
+        }
+
+        this._glBinding = new XRWebGLBinding(this._xrSessionManager.session, this._xrSessionManager.scene.getEngine()._gl);
+
+        return true;
+    }
+
+    /**
+     * Dispose this feature and all of the resources attached
+     */
+    public dispose(): void {
+        this.onCPUDepthInformationObservable.clear();
+        this.onWebGLDepthInformationObservable.clear();
+    }
+
+    protected _onXRFrame(_xrFrame: XRFrame): void {
+        const referenceSPace = this._xrSessionManager.referenceSpace;
+
+        const pose = _xrFrame.getViewerPose(referenceSPace);
+        if (pose == null) {
+            return;
+        }
+
+        for (const view of pose.views) {
+            const depthUsage = this._xrSessionManager.session.depthUsage;
+            switch (depthUsage) {
+                case "cpu-optimized":
+                    this._getAndNotifyCPUDepthInformation(_xrFrame, view);
+                    break;
+
+                case "gpu-optimized":
+                    if (this._glBinding == null) {
+                        const context = this._xrSessionManager.scene.getEngine()._gl;
+                        this._glBinding = new XRWebGLBinding(this._xrSessionManager.session, context);
+                    }
+
+                    this._getAndNotifyWebGLDepthInformation(_xrFrame, view, this._glBinding);
+                    break;
+
+                default:
+                    Tools.Error("Unknown depth usage");
+            }
+        }
+    }
+
+    private _getAndNotifyCPUDepthInformation(frame: XRFrame, view: XRView): void {
+        const cpuDepthInfo = frame.getDepthInformation(view);
+        if (cpuDepthInfo == null) {
+            return;
+        }
+
+        this.onCPUDepthInformationObservable.notifyObservers(cpuDepthInfo);
+    }
+
+    private _getAndNotifyWebGLDepthInformation(frame: XRFrame, view: XRView, glBinding: XRWebGLBinding): void {
+        const webglDepthInfo = glBinding.getDepthInformation(view);
+        if (webglDepthInfo == null) {
+            return;
+        }
+
+        this.onWebGLDepthInformationObservable.notifyObservers(webglDepthInfo);
+    }
+
+    /**
+     * Extends the session init object if needed
+     * @returns augmentation object for the xr session init object.
+     */
+    public getXRSessionInitExtension(): Promise<Partial<XRSessionInit>> {
+        const isDepthUsageDeclared = this.options.usagePreference != null && this.options.usagePreference.length !== 0;
+        const isDataFormatDeclared = this.options.dataFormatPreference != null && this.options.dataFormatPreference.length !== 0;
+        return new Promise((resolve) => {
+            if (isDepthUsageDeclared && isDataFormatDeclared) {
+                resolve({ depthSensing: this.options });
+            } else {
+                resolve({});
+            }
+        });
+    }
+}
+
+WebXRFeaturesManager.AddWebXRFeature(
+    WebXRDepthSensing.Name,
+    (xrSessionManager, options) => {
+        return () => new WebXRDepthSensing(xrSessionManager, options);
+    },
+    WebXRDepthSensing.Version,
+    false
+);
