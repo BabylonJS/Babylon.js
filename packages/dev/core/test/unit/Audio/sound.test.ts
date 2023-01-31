bf93b32785640d0a8db5cf7df35feb8cac3dd966
Andy Fillebrown <afillebrown@microsoft.com>
2023-01-30 22:44:18 -0500

    Cleanup

--- packages/dev/core/test/unit/Audio/sound.test.ts
+++ packages/dev/core/test/unit/Audio/sound.test.ts
@@ -1,10 +1,7 @@
 import { AudioEngine, Sound } from "core/Audio";
-import type { DeviceType } from "core/DeviceInput";
 import { Engine, NullEngine } from "core/Engines";
-import type { IUIEvent } from "core/Events";
 import { Scene } from "core/scene";
 import type { Nullable } from "core/types";
-import { TestDeviceInputSystem } from "../DeviceInput/testDeviceInputSystem";
 
 const AudioContext = jest.fn().mockName("AudioContext").mockImplementation(() => {
     return {
@@ -49,9 +46,9 @@ const AudioContext = jest.fn().mockName("AudioContext").mockImplementation(() =>
     };
 });
 
-// Mock document.createElement so the AudioEngine sets itself up correctly.
 const Document = jest.fn().mockName("Document").mockImplementation(() => {
     return {
+        // Mock document.createElement so the AudioEngine sets itself up correctly.
         createElement: jest.fn().mockName("createElement").mockImplementation((type: string) => {
             if (type === "audio") {
                 return {
@@ -71,48 +68,42 @@ const Document = jest.fn().mockName("Document").mockImplementation(() => {
             }
             return {};
         }),
+
+        // Mock window.removeEventListener so NullEngine can be disposed correctly.
         removeEventListener: jest.fn().mockName("removeEventListener")
     };
 });
 
-// Mock window.AudioContext so the AudioEngine sets itself up correctly.
+const Navigator = jest.fn().mockName("Navigator").mockImplementation(() => {
+    return {
+        // Mock navigator.platform so WebDeviceInputSystem sets itself up correctly.
+        platform: ""
+    }
+});
+
 const Window = jest.fn().mockName("Window").mockImplementation(() => {
     return {
+        // Mock window.AudioContext so the AudioEngine sets itself up correctly.
         // eslint-disable-next-line @typescript-eslint/naming-convention
         AudioContext: AudioContext,
+
+        // Mock window.removeEventListener so NullEngine can be disposed correctly.
         removeEventListener: jest.fn().mockName("removeEventListener")
     };
 });
+
+global.document = new Document;
+global.navigator = new Navigator;
+global.window = new Window;
 
 // Required for timers (eg. setTimeout) to work
 jest.useFakeTimers();
-
-// Mock the WebDeviceInputSystem class since it relies on parts of the window object we're not mocking, like `navigator.platform`.
-jest.mock("core/DeviceInput/webDeviceInputSystem", () => {
-    return {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        WebDeviceInputSystem: jest
-            .fn()
-            .mockImplementation(
-                (
-                    engine: Engine,
-                    onDeviceConnected: (deviceType: DeviceType, deviceSlot: number) => void,
-                    onDeviceDisconnected: (deviceType: DeviceType, deviceSlot: number) => void,
-                    onInputChanged: (deviceType: DeviceType, deviceSlot: number, eventData: IUIEvent) => void
-                ) => {
-                    return new TestDeviceInputSystem(engine, onDeviceConnected, onDeviceDisconnected, onInputChanged);
-                }
-            ),
-    };
-});
 
 describe("Sound", () => {
     let engine: Nullable<NullEngine> = null;
     let scene: Nullable<Scene> = null;
 
     beforeEach(() => {
-        global.document = new Document;
-        global.window = new Window;
         engine = new NullEngine;
         scene = new Scene(engine);
         Engine.audioEngine = new AudioEngine(null, new AudioContext, null);
