eca2fc87a7e354c674029ed02d7213f82b0217e1
Raanan Weber <info@raananweber.com>
2023-02-23 12:53:16 +0100

    fix tag assignment when copying (#13555)
    * fix tag assignment when copying
    
    * unneeded condition

--- packages/dev/core/src/Misc/decorators.ts
+++ packages/dev/core/src/Misc/decorators.ts
@@ -22,8 +22,8 @@ const _copySource = function <T>(creationFunction: () => T, source: T, instancia
     const destination = creationFunction();
 
     // Tags
-    if (Tags) {
-        Tags.AddTagsTo(destination, (<any>source).tags);
+    if (Tags && Tags.HasTags(source)) {
+        Tags.AddTagsTo(destination, Tags.GetTags(source, true));
     }
 
     const classStore = getMergedStore(destination);
