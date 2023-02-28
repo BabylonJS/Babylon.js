ba3401949a75db2b57ea1cf349ba307989ae30c9
Popov72 <github@evpopov.com>
2023-02-28 14:12:45 +0100

    Try to fix build error

--- packages/dev/core/src/Materials/Textures/thinTexture.ts
+++ packages/dev/core/src/Materials/Textures/thinTexture.ts
@@ -157,7 +157,7 @@ export class ThinTexture {
      * This can be used as an internal texture wrapper in ThinEngine to benefit from the cache
      * @param internalTexture Define the internalTexture to wrap. You can also pass a RenderTargetWrapper, in which case the texture will be the render target's texture
      */
-    constructor(internalTexture: Nullable<InternalTexture> | Nullable<RenderTargetWrapper>) {
+    constructor(internalTexture: Nullable<InternalTexture | RenderTargetWrapper>) {
         this._texture = ThinTexture._IsRenderTargetWrapper(internalTexture) ? internalTexture.texture : internalTexture;
         this._texture = internalTexture;
         if (this._texture) {
