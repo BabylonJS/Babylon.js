fbad832738b334858540cce265095cf4d0214151
Carolina Herbster <carolhmj@gmail.com>
2023-03-29 10:48:12 -0300

    Review comments

--- packages/dev/core/src/Animations/animatable.ts
+++ packages/dev/core/src/Animations/animatable.ts
@@ -866,8 +866,8 @@ Scene.prototype.stopAnimation = function (target: any, animationName?: string, t
  */
 Scene.prototype.stopAllAnimations = function (): void {
     if (this._activeAnimatables) {
-        for (let i = this._activeAnimatables.length - 1; i >= 0; i--) {
-            this._activeAnimatables[i].stop();
+        for (let i = 0; i < this._activeAnimatables.length; i++) {
+            this._activeAnimatables[i].stop(undefined, undefined, true);
         }
         this._activeAnimatables.length = 0;
     }
