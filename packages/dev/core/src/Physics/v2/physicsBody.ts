dcc8694548981158fa1f58f4cb5551fd5623af05
Carolina <carolhmj@gmail.com>
2023-04-28 08:41:43 -0300

    Physics: Fix aggregate size calculation and allow passing box rotation as an aggregate option. Also add a basic explanation of motion types on PhysicsBody constructor. (#13778)

--- packages/dev/core/src/Physics/v2/physicsBody.ts
+++ packages/dev/core/src/Physics/v2/physicsBody.ts
@@ -59,7 +59,11 @@ export class PhysicsBody {
     /**
      * Constructs a new physics body for the given node.
      * @param transformNode - The Transform Node to construct the physics body for.
-     * @param motionType - The motion type of the physics body.
+     * @param motionType - The motion type of the physics body. The options are:
+     *  - PhysicsMotionType.STATIC - Static bodies are not moving and unaffected by forces or collisions. They are good for level boundaries or terrain.
+     *  - PhysicsMotionType.DYNAMIC - Dynamic bodies are fully simulated. They can move and collide with other objects.
+     *  - PhysicsMotionType.ANIMATED - They behave like dynamic bodies, but they won't be affected by other bodies, but still push other bodies out of the way.
+     * @param startsAsleep - Whether the physics body should start in a sleeping state (not a guarantee). Defaults to false.
      * @param scene - The scene containing the physics engine.
      *
      * This code is useful for creating a physics body for a given Transform Node in a scene.
