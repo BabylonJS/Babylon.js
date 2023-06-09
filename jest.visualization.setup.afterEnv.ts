7c6c8977367f05945483b15dbc4cf32eea8460b4
Raanan Weber <Raanan.Weber@microsoft.com>
2023-06-09 19:51:53 +0200

    remove commented code

--- jest.visualization.setup.afterEnv.ts
+++ jest.visualization.setup.afterEnv.ts
@@ -1,11 +1,5 @@
-// import { setupJestScreenshot } from "jest-screenshot";
-// setupJestScreenshot();
-const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
+const { configureToMatchImageSnapshot } = require("jest-image-snapshot");
 
-// const customConfig = { threshold: 0 };
-const toMatchImageSnapshot = configureToMatchImageSnapshot({
-//   customDiffConfig: customConfig,
-//   noColors: true,
-});
+const toMatchImageSnapshot = configureToMatchImageSnapshot({});
 expect.extend({ toMatchImageSnapshot });
 jest.setTimeout(12000000);
