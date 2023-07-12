d0809754121e1939d972cc44eb07d5305b0a7753
Raanan Weber <Raanan.Weber@microsoft.com>
2023-07-12 13:01:17 +0200

    addressing review

--- packages/dev/core/src/Cameras/freeCamera.ts
+++ packages/dev/core/src/Cameras/freeCamera.ts
@@ -347,7 +347,7 @@ export class FreeCamera extends TargetCamera {
     /**
      * @internal
      */
-    public _collideWithWorld(displacement: Vector3, defferOnly?: boolean): void {
+    public _collideWithWorld(displacement: Vector3): void {
         let globalPosition: Vector3;
 
         if (this.parent) {
@@ -386,11 +386,12 @@ export class FreeCamera extends TargetCamera {
 
         if (this._diffPosition.length() > Engine.CollisionsEpsilon) {
             this.position.addToRef(this._diffPosition, this._deferredPositionUpdate);
+            // call onCollide, if defined. Note that in case of deferred update, the actual position change might happen in the next frame.
+            if (this.onCollide && collidedMesh) {
+                this.onCollide(collidedMesh);
+            }
             if (!this._deferOnly) {
                 this.position.copyFrom(this._deferredPositionUpdate);
-                if (this.onCollide && collidedMesh) {
-                    this.onCollide(collidedMesh);
-                }
             } else {
                 this._deferredUpdated = true;
             }
