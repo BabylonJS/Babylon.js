3d71f31e433be4e120e1538e5483de065fe0aa65
Popov72 <github@evpopov.com>
2023-07-18 17:43:54 +0200

    lint

--- packages/dev/core/src/Animations/animationGroup.ts
+++ packages/dev/core/src/Animations/animationGroup.ts
@@ -529,7 +529,7 @@ export class AnimationGroup implements IDisposable {
 
     /**
      * Set animation weight for all animatables
-     * 
+     *
      * @since
      *  You can pass the weight to the AnimationGroup constructor, or use the weight property to set it after the group has been created,
      *  making it easier to define the overall animation weight than calling setWeightForAllAnimatables() after the animation group has been started
