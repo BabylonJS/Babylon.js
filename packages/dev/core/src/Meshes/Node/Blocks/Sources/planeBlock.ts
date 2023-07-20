769ad9d6519c9122fdc88b09443d94eefe5bdff3
Deltakosh <david.catuhe@live.fr>
2023-07-20 15:43:53 -0700

    planeBlock

--- packages/dev/core/src/Meshes/Node/Blocks/Sources/planeBlock.ts
+++ packages/dev/core/src/Meshes/Node/Blocks/Sources/planeBlock.ts
@@ -3,4 +3,6 @@ import { NodeGeometryBlock } from "../../nodeGeometryBlock";
 /**
  * Defines a block used to generate plane geometry data
  */
-export class PlaneBlock extends NodeGeometryBlock {}
+export class PlaneBlock extends NodeGeometryBlock {
+    
+}
