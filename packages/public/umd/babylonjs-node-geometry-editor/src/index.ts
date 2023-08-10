23c3f031b9421edc671d624fea9f3174a44581e7
Deltakosh <david.catuhe@live.fr>
2023-08-10 09:09:32 -0700

    Oupsie

---
+++ packages/public/umd/babylonjs-node-geometry-editor/src/index.ts
@@ -0,0 +1,3 @@
+import * as nodeEditor from "node-editor/legacy/legacy";
+export { nodeEditor };
+export default nodeEditor;
--- packages/public/umd/babylonjs-node-geometry-editor/webpack.config.js
+++ packages/public/umd/babylonjs-node-geometry-editor/webpack.config.js
@@ -4,9 +4,9 @@ const commonConfigGenerator = require("@dev/build-tools").webpackTools.commonUMD
 module.exports = (env) => {
     const commonConfig = commonConfigGenerator({
         mode: env.production ? "production" : "development",
-        devPackageName: "node-editor",
-        devPackageAliasPath: `../../../tools/nodeEditor/dist`,
-        namespace: "NODEEDITOR",
+        devPackageName: "node-geometry-editor",
+        devPackageAliasPath: `../../../tools/nodeGeometryEditor/dist`,
+        namespace: "NODEGEOMETRYEDITOR",
         maxMode: true,
         outputPath: path.resolve(__dirname),
         alias: {
