73dc5944494105c0b2fc2c877a0d8381e21f7566
Raanan Weber <raananw+github@gmail.com>
2023-09-15 14:56:11 +0200

    Update packages/tools/viewer/src/model/viewerModel.ts

--- packages/tools/viewer/src/model/viewerModel.ts
+++ packages/tools/viewer/src/model/viewerModel.ts
@@ -392,7 +392,6 @@ export class ViewerModel implements IDisposable {
     protected _getAnimationByName(name: string): Nullable<IModelAnimation> {
         // can't use .find, noe available on IE
         const filtered = this._animations.filter((a) => a.name === name.trim());
-        // what the next line means - if two animations have the same name, they will not be returned!
         if (filtered.length >= 1) {
             return filtered[0];
         } else {
