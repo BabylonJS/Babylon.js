93f70c0fc57eddd865a6d10175381fbc97a6c369
Carolina Herbster <carolhmj@gmail.com>
2023-09-26 09:01:03 -0300

    add state management to flow graph

--- packages/dev/core/src/FlowGraph/flowGraphEngine.ts
+++ packages/dev/core/src/FlowGraph/flowGraphEngine.ts
@@ -2,7 +2,14 @@ import type { Scene } from "../scene";
 import { FlowGraph } from "./flowGraph";
 import { FlowGraphEventCoordinator } from "./flowGraphEventCoordinator";
 
+/**
+ * @experimental
+ * Parameters used to create a flow graph engine.
+ */
 export class IFlowGraphEngineConfiguration {
+    /**
+     * The scene that the flow graph engine belongs to.
+     */
     scene: Scene;
 }
 /**
@@ -10,8 +17,8 @@ export class IFlowGraphEngineConfiguration {
  * It also handles communication between them through an Event Coordinator
  */
 export class FlowGraphEngine {
-    private _eventCoordinator: FlowGraphEventCoordinator;
-    private _flowGraphs: FlowGraph[] = [];
+    private readonly _eventCoordinator: FlowGraphEventCoordinator;
+    private readonly _flowGraphs: FlowGraph[] = [];
 
     constructor(private _config: IFlowGraphEngineConfiguration) {
         this._eventCoordinator = new FlowGraphEventCoordinator();
@@ -28,9 +35,29 @@ export class FlowGraphEngine {
     }
 
     /**
+     * Removes a flow graph from the list of existing flow graphs and disposes it
+     * @param graph the graph to remove
+     */
+    removeGraph(graph: FlowGraph) {
+        const index = this._flowGraphs.indexOf(graph);
+        if (index !== -1) {
+            graph.dispose();
+            this._flowGraphs.splice(index, 1);
+        }
+    }
+
+    /**
      * Starts all graphs
      */
     start() {
         this._flowGraphs.forEach((graph) => graph.start());
+    }
+
+    /**
+     * Disposes all graphs
+     */
+    dispose() {
+        this._flowGraphs.forEach((graph) => graph.dispose());
+        this._flowGraphs.length = 0;
     }
 }
