cb610fde366298d5d270136a9b00ed687aeeabf9
Edison <383612104@qq.com>
2023-10-06 15:41:33 +0800

    Update previewManager.ts

--- packages/tools/nodeEditor/src/components/preview/previewManager.ts
+++ packages/tools/nodeEditor/src/components/preview/previewManager.ts
@@ -362,7 +362,7 @@ export class PreviewManager {
                 this._hdrTexture = CubeTexture.CreateFromPrefilteredData("textures/room.env", this._scene);
                 this._prepareBackgroundHDR();
                 break;
-            case PreviewType.Custom:{
+            case PreviewType.Custom: {
                 const blob = new Blob([this._globalState.envFile], { type: "octet/stream" });
                 const reader = new FileReader();
                 reader.onload = (evt) => {
