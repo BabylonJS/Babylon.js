71c7e988f586ea36964d60694556e0abff3c02f8
Carolina <carolhmj@gmail.com>
2023-10-16 12:59:31 -0300

    visibility changed for control

--- packages/tools/accessibility/src/HtmlTwin/htmlTwinNodeAdapter.tsx
+++ packages/tools/accessibility/src/HtmlTwin/htmlTwinNodeAdapter.tsx
@@ -5,11 +5,15 @@ import { HTMLTwinAccessibilityLeaf } from "./htmlTwinAccessibilityLeaf";
 import { useContext, useEffect, useState } from "react";
 import { SceneContext } from "./htmlTwinSceneContext";
 import { HTMLTwinAccessibilityItem } from "./htmlTwinAccessibilityItem";
+import { Container } from "gui/2D/controls/container";
+import { Control } from "gui/2D/controls/control";
 
 export function HTMLTwinAccessibilityAdaptor(props: { node: AccessibilityEntity }) {
     const [isVisibleState, setIsVisibleState] = useState(isVisible(props.node));
     const [description, setDescription] = useState(getDescriptionFromNode(props.node));
+    const [children, setChildren] = useState(getDirectChildrenOf(props.node));
     const sceneContext = useContext(SceneContext);
+    const {node} = props;
     useEffect(() => {
         const observable = (props.node as any).onEnabledStateChangedObservable;
         const observer = observable.add((value: boolean) => {
@@ -36,10 +40,45 @@ export function HTMLTwinAccessibilityAdaptor(props: { node: AccessibilityEntity 
         return () => {
             observable.remove(observer);
         };
-    }, [])
+    }, []);
+    if (node instanceof Container) {
+        useEffect(() => {
+            const observable = node.onControlAddedObservable;
+            const observer = observable.add(() => {
+                // console.log("control added to container", node.name);
+                setChildren([...getDirectChildrenOf(props.node)]);
+            });
+            return () => {
+                observable.remove(observer);
+            };
+        }, []);
+        useEffect(() => {
+            const observable = node.onControlRemovedObservable;
+            const observer = observable.add(() => {
+                // console.log("control removed from container", node.name);
+                setChildren([...getDirectChildrenOf(props.node)]);
+            });
+            return () => {
+                observable.remove(observer);
+            };
+        }, []);
+    }
+    if (node instanceof Control) {
+        useEffect(() => {
+            const observable = node.onIsVisibleChangedObservable;
+            // console.log("adding visible observer for container", node.name);
+            const observer = observable.add(() => {
+                // console.log("visibility changed for container", node.name);
+                setIsVisibleState(isVisible(props.node));
+            });
+            return () => {
+                observable.remove(observer);
+            };
+        }, []);
+    }
+    // console.log('adaptor for node', node.name);
     // console.log("node", props.node.name, "has update scene", sceneContext.updateScene);
-    const childrenOf = getDirectChildrenOf(props.node);
-    console.log('childrenof', childrenOf);
+    // console.log('childrenof', children);
     if (isVisibleState) {
         const accessibleTexture = getAccessibleTexture(props.node);
         if (accessibleTexture) {
@@ -48,8 +87,8 @@ export function HTMLTwinAccessibilityAdaptor(props: { node: AccessibilityEntity 
             return <HTMLTwinAccessibilityItem 
                         description={description} 
                         isClickable={isClickable(props.node)}>
-                            {childrenOf.map((child: AccessibilityEntity) => (
-                                <HTMLTwinAccessibilityAdaptor node={child} />
+                            {children.map((child: AccessibilityEntity) => (
+                                <HTMLTwinAccessibilityAdaptor node={child} key={child.uniqueId} />
                             ))}
                     </HTMLTwinAccessibilityItem>;
         }
