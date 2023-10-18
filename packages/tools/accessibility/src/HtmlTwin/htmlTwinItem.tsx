4d13793e07af03a57acd2e739022a1d5ecce7aff
Carolina Herbster <carolhmj@gmail.com>
2023-10-18 16:24:10 -0300

    remove more unused code

--- packages/tools/accessibility/src/HtmlTwin/htmlTwinItem.tsx
+++ packages/tools/accessibility/src/HtmlTwin/htmlTwinItem.tsx
@@ -2,29 +2,13 @@ import { StandardMaterial } from "core/Materials/standardMaterial";
 import { AbstractMesh } from "core/Meshes/abstractMesh";
 import { Node } from "core/node";
 import type { Scene } from "core/scene";
-import type { Nullable } from "core/types";
 import { AdvancedDynamicTexture } from "gui/2D/advancedDynamicTexture";
 import { Button } from "gui/2D/controls/button";
 import { Container } from "gui/2D/controls/container";
 import { Control } from "gui/2D/controls/control";
-import { Image } from "gui/2D/controls/image";
-import { TextBlock } from "gui/2D/controls/textBlock";
-import type { ReactElement } from "react";
 import type { IHTMLTwinRendererOptions } from "./htmlTwinRenderer";
 
 export type AccessibilityEntity = Node | Control; // only these types have their html twin
-
-// items are clickable if they are:
-// a) buttons
-// b) meshes with pick triggers
-export function isClickable(item: AccessibilityEntity): boolean {
-    if (item instanceof Button) {
-        return true;
-    } else if (item instanceof AbstractMesh) {
-        return item.actionManager?.hasPickTriggers ?? false;
-    }
-    return false;
-}
 
 export function getAccessibleTexture(item: AccessibilityEntity): AdvancedDynamicTexture | undefined {
     if (item instanceof AbstractMesh && item.material instanceof StandardMaterial) {
@@ -37,34 +21,6 @@ export function getAccessibleTexture(item: AccessibilityEntity): AdvancedDynamic
         }
     }
     return undefined;
-}
-
-export function isAccessibleElement(item: AccessibilityEntity): boolean {
-    const accessibleDescription = getDescriptionFromNode(item);
-    if (accessibleDescription) {
-        return true;
-    }
-    const accessibleTexture = getAccessibleTexture(item);
-    if (accessibleTexture) {
-        return true;
-    }
-    return false;
-}
-
-export function hasAccessibleElement(item: AccessibilityEntity): boolean {
-    let result = false;
-    const directChildren = getDirectChildrenOf(item);
-    // console.log("children of node", item.name, directChildren);
-    for (const child of directChildren) {
-        result ||= hasAccessibleElement(child);
-    }
-    result ||= !!isAccessibleElement(item);
-    // console.log("node", item.name, "has accessible element", result);
-    return result;
-}
-
-export function hasChildren(item: AccessibilityEntity) {
-    return getDirectChildrenOf(item).length > 0;
 }
 
 export function getDirectChildrenOf(item: AccessibilityEntity): AccessibilityEntity[] {
@@ -83,41 +39,6 @@ export function isVisible(item: AccessibilityEntity): boolean {
         return item.isEnabled && item.isVisible;
     }
     return false;
-}
-
-export function getDescriptionFromNode(node: AccessibilityEntity): string | undefined {
-    const accessibleDescription = node.accessibilityTag?.description;
-    if (accessibleDescription) {
-        return accessibleDescription;
-    }
-    // check for text blocks
-    if (node instanceof TextBlock) {
-        return node.text;
-    }
-    if (node instanceof Image) {
-        return node.alt;
-    }
-    if (node instanceof Button) {
-        return node.textBlock?.text;
-    }
-}
-
-/**
- *
- */
-export function getAccessibleItemFromNode(node: AccessibilityEntity): Nullable<ReactElement> {
-    const isAccessible = isAccessibleElement(node);
-    const accessibleDescription = getDescriptionFromNode(node);
-    const isClickableNode = isClickable(node);
-    if (isAccessible) {
-        if (isClickableNode) {
-            return <button>{accessibleDescription}</button>;
-        } else {
-            return <div>{accessibleDescription}</div>;
-        }
-    } else {
-        return null;
-    }
 }
 
 /**
