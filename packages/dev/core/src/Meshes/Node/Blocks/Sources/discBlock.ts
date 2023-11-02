dd7b60f6298e2de515698b065f4bb2dda70bcc88
David Catuhe <david.catuhe@live.fr>
2023-11-02 15:56:05 -0700

    Better eval context evaluation (#14488)

--- packages/dev/core/src/Meshes/Node/Blocks/Sources/discBlock.ts
+++ packages/dev/core/src/Meshes/Node/Blocks/Sources/discBlock.ts
@@ -98,8 +98,9 @@ export class DiscBlock extends NodeGeometryBlock {
         if (this.evaluateContext) {
             this.geometry._storedFunction = func;
         } else {
+            const value = func(state);
             this.geometry._storedFunction = () => {
-                return func(state).clone();
+                return value.clone();
             };
         }
     }
