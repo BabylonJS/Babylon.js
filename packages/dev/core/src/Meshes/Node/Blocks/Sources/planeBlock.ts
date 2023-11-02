172162c16ba8612082e47d91048b9164e5cf1619
Deltakosh <david.catuhe@live.fr>
2023-11-02 15:22:12 -0700

    Better eval context evaluation

--- packages/dev/core/src/Meshes/Node/Blocks/Sources/planeBlock.ts
+++ packages/dev/core/src/Meshes/Node/Blocks/Sources/planeBlock.ts
@@ -108,8 +108,9 @@ export class PlaneBlock extends NodeGeometryBlock {
         if (this.evaluateContext) {
             this.geometry._storedFunction = func;
         } else {
+            const value = func(state);
             this.geometry._storedFunction = () => {
-                return func(state).clone();
+                return value.clone();
             };
         }
     }
