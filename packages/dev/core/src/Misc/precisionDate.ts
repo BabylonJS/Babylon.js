f04ccea3ea2b08fea391edc842ba7d31aa0dd065
Dave Solares <50599569+PolygonalSun@users.noreply.github.com>
2023-11-06 12:20:21 -0800

    Feedback

--- packages/dev/core/src/Misc/precisionDate.ts
+++ packages/dev/core/src/Misc/precisionDate.ts
@@ -1,5 +1,7 @@
 import { IsWindowObjectExist } from "./domManagement";
 
+const now = IsWindowObjectExist() && window.performance && window.performance.now ? () => window.performance.now() : () => Date.now();
+
 /**
  * Class containing a set of static utilities functions for precision date
  */
@@ -8,11 +10,6 @@ export class PrecisionDate {
      * Gets either window.performance.now() if supported or Date.now() else
      */
     public static get Now(): number {
-        return PrecisionDate._Now();
+        return now();
     }
-
-    /**
-     * Since Now is a property, we're using a private variable with a lambda to check which now function to use once
-     */
-    private static readonly _Now = IsWindowObjectExist() && window.performance && window.performance.now ? () => window.performance.now() : () => Date.now();
 }
