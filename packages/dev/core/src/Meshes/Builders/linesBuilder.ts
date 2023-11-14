fd8e50524497d47b444666cbcecf8d72415b91d0
郭斌勇 <guobinyong@qq.com>
2023-11-14 18:48:54 +0800

    refactor: optimize CreateLineSystemVertexData

--- packages/dev/core/src/Meshes/Builders/linesBuilder.ts
+++ packages/dev/core/src/Meshes/Builders/linesBuilder.ts
@@ -31,10 +31,12 @@ export function CreateLineSystemVertexData(options: { lines: Vector3[][]; colors
     for (let l = 0; l < lines.length; l++) {
         const points = lines[l];
         for (let index = 0; index < points.length; index++) {
-            positions.push(points[index].x, points[index].y, points[index].z);
+            const { x, y, z } = points[index];
+            positions.push(x, y, z);
             if (colors) {
                 const color = colors[l];
-                vertexColors.push(color[index].r, color[index].g, color[index].b, color[index].a);
+                const { r, g, b, a } = color[index];
+                vertexColors.push(r, g, b, a);
             }
             if (index > 0) {
                 indices.push(idx - 1);
