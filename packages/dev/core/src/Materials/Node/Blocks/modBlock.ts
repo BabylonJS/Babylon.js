c153503d90b58e6ec38cd9c3d2393675d80d40b9
Starryi <starryi@foxmail.com>
2023-11-26 23:43:10 +0800

    Allow Mod Node to use float as second operand

--- packages/dev/core/src/Materials/Node/Blocks/modBlock.ts
+++ packages/dev/core/src/Materials/Node/Blocks/modBlock.ts
@@ -21,6 +21,8 @@ export class ModBlock extends NodeMaterialBlock {
 
         this._outputs[0]._typeConnectionSource = this._inputs[0];
         this._linkConnectionTypes(0, 1);
+
+        this._inputs[1].acceptedConnectionPointTypes.push(NodeMaterialBlockConnectionPointTypes.Float);
     }
 
     /**
