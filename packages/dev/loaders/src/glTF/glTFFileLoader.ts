8e6b4cd4d4364a32e72b986c9d4519148d969306
Gary Hsu <bghgary@users.noreply.github.com>
2023-11-28 16:29:41 -0800

    Fix incorrect range check when reading glTF buffer

--- packages/dev/loaders/src/glTF/glTFFileLoader.ts
+++ packages/dev/loaders/src/glTF/glTFFileLoader.ts
@@ -48,8 +48,12 @@ function readAsync(arrayBuffer: ArrayBuffer, byteOffset: number, byteLength: num
 
 function readViewAsync(arrayBufferView: ArrayBufferView, byteOffset: number, byteLength: number): Promise<Uint8Array> {
     try {
-        if (arrayBufferView.byteOffset + byteOffset + byteLength > arrayBufferView.buffer.byteLength) {
-            throw new Error("Array length out of bounds.");
+        if (byteOffset < 0 || byteOffset >= arrayBufferView.byteLength) {
+            throw new RangeError("Offset is out of range.");
+        }
+
+        if (byteOffset + byteLength > arrayBufferView.byteLength) {
+            throw new RangeError("Length is out of range.");
         }
 
         return Promise.resolve(new Uint8Array(arrayBufferView.buffer, arrayBufferView.byteOffset + byteOffset, byteLength));
@@ -669,10 +673,7 @@ export class GLTFFileLoader implements IDisposable, ISceneLoaderPluginAsync, ISc
         );
     }
 
-    /**
-     * @internal
-     */
-    _loadBinary(
+    private _loadBinary(
         scene: Scene,
         data: ArrayBufferView,
         rootUrl: string,
