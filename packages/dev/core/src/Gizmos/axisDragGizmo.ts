205a0fe5683038f24e1cdc9b01423388fd3b669c
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2023-12-01 11:48:53 +0100

    signed distance for axisDragGizmo snap event

--- packages/dev/core/src/Gizmos/axisDragGizmo.ts
+++ packages/dev/core/src/Gizmos/axisDragGizmo.ts
@@ -230,7 +230,7 @@ export class AxisDragGizmo extends Gizmo implements IAxisDragGizmo {
                         if (this.dragBehavior.validateDrag(TmpVectors.Vector3[2])) {
                             this.attachedNode.getWorldMatrix().addTranslationFromFloats(TmpVectors.Vector3[1].x, TmpVectors.Vector3[1].y, TmpVectors.Vector3[1].z);
                             this.attachedNode.updateCache();
-                            tmpSnapEvent.snapDistance = this.snapDistance * dragSteps;
+                            tmpSnapEvent.snapDistance = this.snapDistance * dragSteps * Math.sign(currentSnapDragDistance);
                             this.onSnapObservable.notifyObservers(tmpSnapEvent);
                             matrixChanged = true;
                         }
