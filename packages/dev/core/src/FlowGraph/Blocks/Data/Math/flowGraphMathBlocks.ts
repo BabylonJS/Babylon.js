233a816b8f61f5764409386576251949e83b0c83
Carolina <carolhmj@gmail.com>
2023-12-04 13:24:06 -0300

    fix formatting

--- packages/dev/core/src/FlowGraph/Blocks/Data/Math/flowGraphMathBlocks.ts
+++ packages/dev/core/src/FlowGraph/Blocks/Data/Math/flowGraphMathBlocks.ts
@@ -29,7 +29,6 @@ function _areSameVectorClass(className: string, className2: string) {
     return (className === "Vector2" && className2 === "Vector2") || (className === "Vector3" && className2 === "Vector3") || (className === "Vector4" && className2 === "Vector4");
 }
 
-
 /**
  * @experimental
  * Polymorphic add block.
