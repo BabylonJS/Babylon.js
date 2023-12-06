d82c284dac0b6a404bbdbd7922afe5d46b252329
David Catuhe <david.catuhe@live.fr>
2023-12-06 14:01:48 -0800

    Fix reentrance of  updated vertex data

--- packages/dev/core/src/Meshes/Node/Blocks/Set/setTangentsBlock.ts
+++ packages/dev/core/src/Meshes/Node/Blocks/Set/setTangentsBlock.ts
@@ -93,6 +93,11 @@ export class SetTangentsBlock extends NodeGeometryBlock implements INodeGeometry
             state.pushExecutionContext(this);
 
             this._vertexData = this.geometry.getConnectedValue(state);
+
+            if (this._vertexData) {
+                this._vertexData = this._vertexData.clone(); // Preserve source data
+            }
+
             state.pushGeometryContext(this._vertexData);
 
             if (!this._vertexData || !this._vertexData.positions) {
