970cc50850e01458350058523f5334a596ecf225
Carolina Herbster <carolhmj@gmail.com>
2023-12-14 09:58:04 -0300

    add documentation

--- packages/dev/core/src/ObjectModel/objectModelInterfaces.ts
+++ packages/dev/core/src/ObjectModel/objectModelInterfaces.ts
@@ -15,9 +15,22 @@ export interface IObjectAccessor {
      * The type of the object that is converted
      */
     type: string;
+    /**
+     * Get a property value from the object.
+     * @param args any necessary arguments to get the property value
+     */
     get(...args: any[]): any;
+    /**
+     * Set a property value on the object.
+     * @param value the value to set
+     * @param args any necessary arguments to set the property value
+     */
     set(value: any, ...args: any[]): void;
-    getObject(obj: any): any;
+    /**
+     * Get the original object
+     * @param args any necessary arguments to get the original object
+     */
+    getObject(...args: any[]): any;
 }
 
 /**
