1f6d488cb6ce9359ddc30d002572a95437f47226
Raanan Weber <info@raananweber.com>
2024-02-20 16:41:17 +0100

    Fix declaration issue (#14792)

--- packages/dev/core/src/Culling/ray.ts
+++ packages/dev/core/src/Culling/ray.ts
@@ -558,7 +558,7 @@ export class Ray {
      */
     public static CreateNewFromTo(origin: Vector3, end: Vector3, world: DeepImmutable<Matrix> = Matrix.IdentityReadOnly): Ray {
         const result = new Ray(new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-        return Ray.CreateFromToToRef(origin, end, world, result);
+        return Ray.CreateFromToToRef(origin, end, result, world);
     }
 
     /**
@@ -566,11 +566,11 @@ export class Ray {
      * transformed to the given world matrix.
      * @param origin The origin point
      * @param end The end point
-     * @param world a matrix to transform the ray to. Default is the identity matrix.
      * @param result the object to store the result
+     * @param world a matrix to transform the ray to. Default is the identity matrix.
      * @returns the ref ray
      */
-    public static CreateFromToToRef(origin: Vector3, end: Vector3, world: DeepImmutable<Matrix> = Matrix.IdentityReadOnly, result: Ray): Ray {
+    public static CreateFromToToRef(origin: Vector3, end: Vector3, result: Ray, world: DeepImmutable<Matrix> = Matrix.IdentityReadOnly): Ray {
         result.origin.copyFrom(origin);
         const direction = end.subtractToRef(origin, result.direction);
         const length = Math.sqrt(direction.x * direction.x + direction.y * direction.y + direction.z * direction.z);
