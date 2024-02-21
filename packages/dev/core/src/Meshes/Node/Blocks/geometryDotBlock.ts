6381aae3234b53d831086774388be35effc46fd3
David Catuhe <david.catuhe@live.fr>
2024-02-21 13:10:53 -0800

    Nge nodes2 (#14801)
    * Associated with #14595
    
    * formatting
    
    * Associated with #14595

---
+++ packages/dev/core/src/Meshes/Node/Blocks/geometryDotBlock.ts
@@ -0,0 +1,75 @@
+import { RegisterClass } from "../../../Misc/typeStore";
+import { NodeGeometryBlockConnectionPointTypes } from "../Enums/nodeGeometryConnectionPointTypes";
+import { NodeGeometryBlock } from "../nodeGeometryBlock";
+import type { NodeGeometryConnectionPoint } from "../nodeGeometryBlockConnectionPoint";
+
+/**
+ * Block used to apply a dot product between 2 vectors
+ */
+export class GeometryDotBlock extends NodeGeometryBlock {
+    /**
+     * Creates a new GeometryDotBlock
+     * @param name defines the block name
+     */
+    public constructor(name: string) {
+        super(name);
+
+        this.registerInput("left", NodeGeometryBlockConnectionPointTypes.AutoDetect);
+        this.registerInput("right", NodeGeometryBlockConnectionPointTypes.AutoDetect);
+        this.registerOutput("output", NodeGeometryBlockConnectionPointTypes.Float);
+
+        this._linkConnectionTypes(0, 1);
+        this._inputs[0].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Float);
+        this._inputs[0].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Matrix);
+        this._inputs[1].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Float);
+        this._inputs[1].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Matrix);
+    }
+
+    /**
+     * Gets the current class name
+     * @returns the class name
+     */
+    public getClassName() {
+        return "GeometryDotBlock";
+    }
+
+    /**
+     * Gets the left operand input component
+     */
+    public get left(): NodeGeometryConnectionPoint {
+        return this._inputs[0];
+    }
+
+    /**
+     * Gets the right operand input component
+     */
+    public get right(): NodeGeometryConnectionPoint {
+        return this._inputs[1];
+    }
+
+    /**
+     * Gets the output component
+     */
+    public get output(): NodeGeometryConnectionPoint {
+        return this._outputs[0];
+    }
+
+    protected _buildBlock() {
+        if (!this.left.isConnected || !this.right.isConnected) {
+            this.output._storedFunction = null;
+            this.output._storedValue = null;
+            return;
+        }
+
+        this.output._storedFunction = (state) => {
+            const left = this.left.getConnectedValue(state);
+            const right = this.right.getConnectedValue(state);
+
+            return left.dot(right);
+        };
+
+        return this;
+    }
+}
+
+RegisterClass("BABYLON.GeometryDotBlock", GeometryDotBlock);
---
+++ packages/dev/core/src/Meshes/Node/Blocks/geometryReplaceColorBlock.ts
@@ -0,0 +1,101 @@
+import { RegisterClass } from "../../../Misc/typeStore";
+import { NodeGeometryBlockConnectionPointTypes } from "../Enums/nodeGeometryConnectionPointTypes";
+import { NodeGeometryBlock } from "../nodeGeometryBlock";
+import type { NodeGeometryConnectionPoint } from "../nodeGeometryBlockConnectionPoint";
+/**
+ * Block used to replace a color by another one
+ */
+export class GeometryReplaceColorBlock extends NodeGeometryBlock {
+    /**
+     * Creates a new GeometryReplaceColorBlock
+     * @param name defines the block name
+     */
+    public constructor(name: string) {
+        super(name);
+
+        this.registerInput("value", NodeGeometryBlockConnectionPointTypes.AutoDetect);
+        this.registerInput("reference", NodeGeometryBlockConnectionPointTypes.AutoDetect);
+        this.registerInput("distance", NodeGeometryBlockConnectionPointTypes.Float);
+        this.registerInput("replacement", NodeGeometryBlockConnectionPointTypes.AutoDetect);
+        this.registerOutput("output", NodeGeometryBlockConnectionPointTypes.BasedOnInput);
+
+        this._outputs[0]._typeConnectionSource = this._inputs[0];
+        this._linkConnectionTypes(0, 1);
+        this._linkConnectionTypes(0, 3);
+
+        this._inputs[0].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Float);
+        this._inputs[0].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Matrix);
+        this._inputs[1].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Float);
+        this._inputs[1].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Matrix);
+        this._inputs[3].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Float);
+        this._inputs[3].excludedConnectionPointTypes.push(NodeGeometryBlockConnectionPointTypes.Matrix);
+    }
+
+    /**
+     * Gets the current class name
+     * @returns the class name
+     */
+    public getClassName() {
+        return "GeometryReplaceColorBlock";
+    }
+
+    /**
+     * Gets the value input component
+     */
+    public get value(): NodeGeometryConnectionPoint {
+        return this._inputs[0];
+    }
+
+    /**
+     * Gets the reference input component
+     */
+    public get reference(): NodeGeometryConnectionPoint {
+        return this._inputs[1];
+    }
+
+    /**
+     * Gets the distance input component
+     */
+    public get distance(): NodeGeometryConnectionPoint {
+        return this._inputs[2];
+    }
+
+    /**
+     * Gets the replacement input component
+     */
+    public get replacement(): NodeGeometryConnectionPoint {
+        return this._inputs[3];
+    }
+
+    /**
+     * Gets the output component
+     */
+    public get output(): NodeGeometryConnectionPoint {
+        return this._outputs[0];
+    }
+
+    protected _buildBlock() {
+        if (!this.value.isConnected || !this.reference.isConnected || !this.distance.isConnected || !this.replacement.isConnected) {
+            this.output._storedFunction = null;
+            this.output._storedValue = null;
+            return;
+        }
+
+        this.output._storedFunction = (state) => {
+            const value = this.value.getConnectedValue(state);
+            const reference = this.reference.getConnectedValue(state);
+            const distance = this.distance.getConnectedValue(state);
+            const replacement = this.replacement.getConnectedValue(state);
+
+            if (value.subtract(reference).length() < distance) {
+                return replacement;
+            } else {
+                return value;
+            }
+        };
+
+        return this;
+    }
+}
+
+RegisterClass("BABYLON.GeometryReplaceColorBlock", GeometryReplaceColorBlock);
