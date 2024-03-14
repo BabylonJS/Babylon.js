2941253842a8fcba69481b99ec8ac62afc07f6c8
Raanan Weber <raananw+github@gmail.com>
2024-03-14 17:21:26 +0100

    update test-project

--- packages/public/umd/babylonjs-testporject/src/index.ts
+++ packages/public/umd/babylonjs-testporject/src/index.ts
@@ -1,10 +1,11 @@
+/// <reference path="../../babylonjs/babylon.module.d.ts" />
+/// <reference path="../../babylonjs-gui/babylon.gui.module.d.ts" />
+/// <reference path="../../babylonjs-loaders/babylonjs.loaders.module.d.ts" />
+/// <reference path="../../babylonjs-materials/babylonjs.materials.module.d.ts" />
+/// <reference path="../../babylonjs-serializers/babylonjs.serializers.module.d.ts" />
+
 import * as BABYLON from "babylonjs";
 import * as BABYLONGUI from "babylonjs-gui";
-import "babylonjs-loaders";
-import "babylonjs-materials";
-import "babylonjs-serializers";
-
-const engine = new BABYLON.NullEngine();
-engine.dispose();
-
-BABYLONGUI.AdvancedDynamicTexture.CreateFullscreenUI("test");
+import * as LOADERS from "babylonjs-loaders";
+import * as MATERIALS from "babylonjs-materials";
+import * as SERIALIZERS from "babylonjs-serializers";
