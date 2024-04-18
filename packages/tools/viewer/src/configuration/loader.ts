41bc488b01b6c33473feefe12793f8acf0b4b6e8
Ryan Tremblay <ryantrem@msn.com>
2024-04-18 13:01:58 -0700

    Fix a few files that are not part of the regular dev build

--- packages/tools/viewer/src/configuration/loader.ts
+++ packages/tools/viewer/src/configuration/loader.ts
@@ -4,7 +4,7 @@ import { getConfigurationType } from "./types/index";
 
 export class ConfigurationLoader extends RenderOnlyConfigurationLoader {
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    protected getExtendedConfig(type: string | undefined) {
+    protected override getExtendedConfig(type: string | undefined) {
         return getConfigurationType(type || "extended");
     }
 }
