36b858b039f72f70ffa7e69a6dc38b1ed4375182
Popov72 <github@evpopov.com>
2024-05-15 20:04:06 +0200

    Move code to external file

--- packages/dev/core/src/Engines/abstractEngine.ts
+++ packages/dev/core/src/Engines/abstractEngine.ts
@@ -34,7 +34,7 @@ import type { IMaterialContext } from "./IMaterialContext";
 import type { IStencilState } from "../States/IStencilState";
 import type { DrawWrapper } from "../Materials/drawWrapper";
 import type { IDrawContext } from "./IDrawContext";
-import { VertexBuffer } from "../Meshes/buffer";
+import type { VertexBuffer } from "../Meshes/buffer";
 import type { IAudioEngine } from "../Audio/Interfaces/IAudioEngine";
 import type { WebRequest } from "core/Misc/webRequest";
 import type { PerformanceMonitor } from "core/Misc/performanceMonitor";
@@ -193,24 +193,6 @@ export type PrepareTextureFunction = (
     processFunction: PrepareTextureProcessFunction,
     samplingMode: number
 ) => void;
-
-const vertexBufferKindForNonFloatProcessing: { [kind: string]: boolean } = {
-    [VertexBuffer.PositionKind]: true,
-    [VertexBuffer.NormalKind]: true,
-    [VertexBuffer.TangentKind]: true,
-    [VertexBuffer.UVKind]: true,
-    [VertexBuffer.UV2Kind]: true,
-    [VertexBuffer.UV3Kind]: true,
-    [VertexBuffer.UV4Kind]: true,
-    [VertexBuffer.UV5Kind]: true,
-    [VertexBuffer.UV6Kind]: true,
-    [VertexBuffer.ColorKind]: true,
-    [VertexBuffer.ColorInstanceKind]: true,
-    [VertexBuffer.MatricesIndicesKind]: true,
-    [VertexBuffer.MatricesWeightsKind]: true,
-    [VertexBuffer.MatricesIndicesExtraKind]: true,
-    [VertexBuffer.MatricesWeightsExtraKind]: true,
-};
 
 /**
  * The parent class for specialized engines (WebGL, WebGPU)
@@ -1297,59 +1279,6 @@ export abstract class AbstractEngine {
         effect: Effect,
         overrideVertexBuffers?: { [kind: string]: Nullable<VertexBuffer> }
     ): void;
-
-    /**
-     * Checks whether some vertex buffers that should be of type float are of a different type (int, byte...).
-     * If so, trigger a shader recompilation to give the shader processor the opportunity to update the code accordingly.
-     * @param vertexBuffers List of vertex buffers to check
-     * @param effect The effect (shaders) that should be recompiled if needed
-     */
-    public checkNonFloatVertexBuffers(vertexBuffers: { [key: string]: Nullable<VertexBuffer> }, effect: Effect) {
-        const pipelineContext = effect._pipelineContext;
-
-        if (!pipelineContext?.vertexBufferKindToType) {
-            return;
-        }
-
-        let shaderProcessingContext: Nullable<ShaderProcessingContext> = null;
-
-        for (const kind in vertexBuffers) {
-            const currentVertexBuffer = vertexBuffers[kind];
-
-            if (!currentVertexBuffer || !vertexBufferKindForNonFloatProcessing[kind]) {
-                continue;
-            }
-
-            const currentVertexBufferType = currentVertexBuffer.normalized ? VertexBuffer.FLOAT : currentVertexBuffer.type;
-            const vertexBufferType = pipelineContext.vertexBufferKindToType[kind];
-
-            if (
-                (currentVertexBufferType !== VertexBuffer.FLOAT && vertexBufferType === undefined) ||
-                (vertexBufferType !== undefined && vertexBufferType !== currentVertexBufferType)
-            ) {
-                if (!shaderProcessingContext) {
-                    shaderProcessingContext = this._getShaderProcessingContext(effect.shaderLanguage)!;
-                }
-                pipelineContext.vertexBufferKindToType[kind] = currentVertexBufferType;
-                if (currentVertexBufferType !== VertexBuffer.FLOAT) {
-                    shaderProcessingContext.vertexBufferKindToNumberOfComponents![kind] = VertexBuffer.DeduceStride(kind);
-                    if (VertexBuffer.IsSignedType(currentVertexBufferType)) {
-                        shaderProcessingContext.vertexBufferKindToNumberOfComponents![kind] *= -1;
-                    }
-                }
-            }
-        }
-
-        if (shaderProcessingContext) {
-            // We temporarily disable parallel compilation of shaders because we want new shaders to be compiled after the _processShaderCode call, so that they are in effect for the rest of the frame.
-            const parallelShaderCompile = this._caps.parallelShaderCompile;
-            this._caps.parallelShaderCompile = undefined;
-
-            effect._processShaderCode(null, this._features.checkNonFloatVertexBuffersDontRecreatePipelineContext, shaderProcessingContext);
-
-            this._caps.parallelShaderCompile = parallelShaderCompile;
-        }
-    }
 
     /**
      * @internal
