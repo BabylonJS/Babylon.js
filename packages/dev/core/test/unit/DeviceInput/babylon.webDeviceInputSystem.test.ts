9a711132e6b4019026add0efef5b287d0dafae6a
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2024-05-20 15:16:46 -0700

    Add webDeviceInputSystem unit tests to cover pointermove before pointerdown cases

---
+++ packages/dev/core/test/unit/DeviceInput/babylon.webDeviceInputSystem.test.ts
@@ -0,0 +1,140 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { WebDeviceInputSystem } from "core/DeviceInput/webDeviceInputSystem";
+import { NullEngine, NullEngineOptions } from "core/Engines/nullEngine";
+import { Logger } from "core/Misc/logger";
+
+describe("WebDeviceInputSystem", () => {
+    let engine: NullEngine;
+    let wdis: WebDeviceInputSystem;
+    let mockOnInputChanged: jest.Mock<any, any>;
+    let mockOnDeviceConnected: jest.Mock<any, any>;
+    let mockOnDeviceDisconnected: jest.Mock<any, any>;
+    let renderElement: HTMLCanvasElement;
+    let addEventListenerSpy: jest.SpyInstance;
+    let raiseOnPointerDown = (evt: any) => {};
+    let raiseOnPointerMove = (evt: any) => {};
+    let raiseOnPointerUp = (evt: any) => {};
+
+    beforeEach(() => {
+        // So that GetPointerPrefix knows we are going to simulate pointer events
+        window.PointerEvent = jest.fn() as any;
+
+        renderElement = document.createElement("canvas");
+        addEventListenerSpy = jest.spyOn(renderElement, "addEventListener");
+        addEventListenerSpy.mockImplementation((type, listener) => {
+            switch (type) {
+                case "pointerdown":
+                    raiseOnPointerDown = listener;
+                    break;
+                case "pointermove":
+                    raiseOnPointerMove = listener;
+                    break;
+                case "pointerup":
+                    raiseOnPointerUp = listener;
+                    break;
+            }
+        });
+
+        mockOnInputChanged = jest.fn();
+        mockOnDeviceConnected = jest.fn();
+        mockOnDeviceDisconnected = jest.fn();
+
+        const nullEngineOptions = new NullEngineOptions();
+        nullEngineOptions.renderingCanvas = renderElement;
+        engine = new NullEngine(nullEngineOptions);
+        wdis = new WebDeviceInputSystem(engine, mockOnDeviceConnected, mockOnDeviceDisconnected, mockOnInputChanged);
+
+        Logger.Warn = jest.fn();
+    });
+
+    afterEach(() => {
+        engine.dispose();
+        wdis.dispose();
+        window.PointerEvent = undefined;
+    });
+
+    describe("when pointerdown comes before pointermove", () => {
+        describe("initial pointerdown", () => {
+            it("should raise deviceConnected", () => {
+                // Act
+                raiseOnPointerDown({ pointerType: "touch", button: 0, pointerId: 1 });
+
+                // Assert
+                expect(mockOnDeviceConnected).toHaveBeenCalled();
+                expect(Logger.Warn).not.toHaveBeenCalled();
+            });
+        });
+        describe("many pointerdown, pointermove, pointerup cycles", () => {
+            it("should work and raise no warnings", () => {
+                for (let pointerId = 0; pointerId < 25; pointerId++) {
+                    // Arrange
+                    mockOnInputChanged.mockClear();
+                    mockOnDeviceConnected.mockClear();
+                    mockOnDeviceDisconnected.mockClear();
+
+                    // Act
+                    raiseOnPointerDown({ pointerType: "touch", button: 0, pointerId });
+                    raiseOnPointerMove({ pointerType: "touch", button: -1, pointerId });
+                    raiseOnPointerUp({ pointerType: "touch", button: 0, pointerId });
+
+                    // Assert
+                    expect(Logger.Warn).not.toHaveBeenCalled();
+                    expect(mockOnDeviceConnected).toHaveBeenCalled();
+                    expect(mockOnInputChanged).toHaveBeenCalled();
+                    expect(mockOnDeviceDisconnected).toHaveBeenCalled();
+                }
+            });
+        });
+    });
+
+    describe("when pointermove comes before pointerdown", () => {
+        describe("initial pointermove", () => {
+            it("should raise deviceConnected", () => {
+                // Act
+                raiseOnPointerMove({ pointerType: "touch", button: -1, pointerId: 1 });
+
+                // Assert
+                expect(mockOnDeviceConnected).toHaveBeenCalled();
+                expect(Logger.Warn).not.toHaveBeenCalled();
+            });
+        });
+        describe("subsequent pointermove", () => {
+            it("should not raise deviceConnected again", () => {
+                // Arrange
+                raiseOnPointerMove({ pointerType: "touch", button: -1, pointerId: 1 });
+                mockOnDeviceConnected.mockReset();
+
+                // Act
+                raiseOnPointerMove({ pointerType: "touch", button: -1, pointerId: 1 });
+
+                // Assert
+                expect(mockOnDeviceConnected).not.toHaveBeenCalled();
+                expect(Logger.Warn).not.toHaveBeenCalled();
+            });
+        });
+        describe("many pointermove, pointerdown, pointerup cycles", () => {
+            it("should work and raise no warnings", () => {
+                for (let pointerId = 0; pointerId < 25; pointerId++) {
+                    // Arrange
+                    mockOnInputChanged.mockClear();
+                    mockOnDeviceConnected.mockClear();
+                    mockOnDeviceDisconnected.mockClear();
+
+                    // Act
+                    raiseOnPointerMove({ pointerType: "touch", button: -1, pointerId });
+                    raiseOnPointerDown({ pointerType: "touch", button: 0, pointerId });
+                    raiseOnPointerUp({ pointerType: "touch", button: 0, pointerId });
+
+                    // Assert
+                    expect(Logger.Warn).not.toHaveBeenCalled();
+                    expect(mockOnDeviceConnected).toHaveBeenCalled();
+                    expect(mockOnInputChanged).toHaveBeenCalled();
+                    expect(mockOnDeviceDisconnected).toHaveBeenCalled();
+                }
+            });
+        });
+    });
+});
