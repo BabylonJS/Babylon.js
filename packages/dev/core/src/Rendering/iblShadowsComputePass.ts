b66d01e6ef1c6733c0457196b9f8d2cb87e6caa1
Michael Bond <mbond@adobe.com>
2024-05-24 13:58:55 -0700

    Generate custom voxel mips

--- packages/dev/core/src/Rendering/iblShadowsComputePass.ts
+++ packages/dev/core/src/Rendering/iblShadowsComputePass.ts
@@ -191,7 +191,7 @@ export class IblShadowsComputePass {
         this._outputPT.setTexture("voxelGridSampler", voxelGrid);
         this._outputPT.setTexture("icdfySampler", this._scene.iblShadowsRenderer!.getIcdfyTexture());
         this._outputPT.setTexture("icdfxSampler", this._scene.iblShadowsRenderer!.getIcdfxTexture());
-        this._outputPT.defines = "#define VOXEL_MARCHING_NUM_MIPS " + Math.log(voxelGrid!.getSize().width).toFixed(0) + "u\n";
+        this._outputPT.defines = "#define VOXEL_MARCHING_NUM_MIPS " + Math.log2(voxelGrid!.getSize().width).toFixed(0) + "u\n";
 
         const prePassRenderer = this._scene.prePassRenderer;
         if (prePassRenderer) {
