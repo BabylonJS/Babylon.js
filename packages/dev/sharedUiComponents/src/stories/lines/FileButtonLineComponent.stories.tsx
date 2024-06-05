f2c8520e328d9969a3ebe9c8d1f36e33edf938bb
Raanan Weber <raananw+github@gmail.com>
2024-06-05 17:26:59 +0200

    removing the stories

--- packages/dev/sharedUiComponents/src/stories/lines/FileButtonLineComponent.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { FileButtonLineComponent } from "../../components/lines/FileButtonLineComponent";
-import type { StoryObj } from "@storybook/react";
-
-export default {
-    component: FileButtonLineComponent,
-};
-
-export const Default: StoryObj<typeof FileButtonLineComponent> = {};
