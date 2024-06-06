f6b6eddf0df8a1cbc2357473cfb3b01fcb0dbfe5
sebavan <vandenberghe.sebastien@gmail.com>
2024-06-06 19:13:41 +0200

    Fix colored layer (#15169)

--- packages/dev/core/src/Layers/layer.ts
+++ packages/dev/core/src/Layers/layer.ts
@@ -253,7 +253,7 @@ export class Layer {
 
         const currentEffect = this._drawWrapper.effect;
 
-        return currentEffect?.isReady() && this.texture?.isReady();
+        return currentEffect?.isReady() && (!this.texture || this.texture.isReady());
     }
 
     /**
@@ -280,8 +280,10 @@ export class Layer {
         engine.setState(false);
 
         // Texture
-        currentEffect.setTexture("textureSampler", this.texture);
-        currentEffect.setMatrix("textureMatrix", this.texture!.getTextureMatrix());
+        if (this.texture) {
+            currentEffect.setTexture("textureSampler", this.texture);
+            currentEffect.setMatrix("textureMatrix", this.texture.getTextureMatrix());
+        }
 
         // Color
         currentEffect.setFloat4("color", this.color.r, this.color.g, this.color.b, this.color.a);
