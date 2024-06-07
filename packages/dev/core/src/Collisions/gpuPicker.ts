79c3929f3a8d2560741bd37c70e9033a56b4feea
David Catuhe <david.catuhe@live.fr>
2024-06-07 16:33:33 -0700

    Address feedback (#15176)
    * Address feedback
    
    * Add IGPUPickingInfo to be future proof
    
    * .

--- packages/dev/core/src/Collisions/gpuPicker.ts
+++ packages/dev/core/src/Collisions/gpuPicker.ts
@@ -14,6 +14,16 @@ import "../Shaders/picking.fragment";
 import "../Shaders/picking.vertex";
 
 /**
+ * Class used to store the result of a GPU picking operation
+ */
+export interface IGPUPickingInfo {
+    /**
+     * Picked mesh
+     */
+    mesh: AbstractMesh;
+}
+
+/**
  * Class used to perform a picking operation using GPU
  * Please note that GPUPIcker cannot pick instances, only meshes
  */
@@ -26,9 +36,12 @@ export class GPUPicker {
     private _pickableMeshes: Array<AbstractMesh>;
     private _readbuffer: Uint8Array;
     private _meshRenderingCount: number = 0;
-    private _attributeName = "instanceMeshID";
+    private readonly _attributeName = "instanceMeshID";
 
     private _createRenderTarget(scene: Scene, width: number, height: number) {
+        if (this._pickingTexure) {
+            this._pickingTexure.dispose();
+        }
         this._pickingTexure = new RenderTargetTexture(
             "pickingTexure",
             { width: width, height: height },
@@ -88,7 +101,7 @@ export class GPUPicker {
                     (mesh as Mesh).removeVerticesData(this._attributeName);
                 }
                 if (this._pickingTexure) {
-                    this._pickingTexure!.setMaterialForRendering(mesh, undefined);
+                    this._pickingTexure.setMaterialForRendering(mesh, undefined);
                 }
             }
             this._pickableMeshes.length = 0;
@@ -114,7 +127,6 @@ export class GPUPicker {
             const size = this._pickingTexure.getSize();
 
             if (size.width !== rttSizeW || size.height !== rttSizeH || this._cachedScene !== scene) {
-                this._pickingTexure.dispose();
                 this._createRenderTarget(scene, rttSizeW, rttSizeH);
             }
         }
@@ -181,7 +193,7 @@ export class GPUPicker {
      * @param disposeWhenDone defines a boolean indicating we do not want to keep resources alive (false by default)
      * @returns A promise with the picking results
      */
-    public pickAsync(x: number, y: number, disposeWhenDone = false): Promise<Nullable<AbstractMesh>> {
+    public pickAsync(x: number, y: number, disposeWhenDone = false): Promise<Nullable<IGPUPickingInfo>> {
         if (!this._pickableMeshes || this._pickableMeshes.length === 0) {
             return Promise.resolve(null);
         }
@@ -199,7 +211,6 @@ export class GPUPicker {
         const size = this._pickingTexure!.getSize();
 
         if (size.width !== rttSizeW || size.height !== rttSizeH) {
-            this._pickingTexure!.dispose();
             this._createRenderTarget(scene, rttSizeW, rttSizeH);
 
             this._pickingTexure!.renderList = [];
@@ -271,7 +282,11 @@ export class GPUPicker {
                     if (disposeWhenDone) {
                         this.dispose();
                     }
-                    resolve(pickedMesh);
+                    if (pickedMesh) {
+                        resolve({ mesh: pickedMesh });
+                    } else {
+                        resolve(null);
+                    }
                 }
             };
         });
