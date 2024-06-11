c244cbd84ae7f85ac9d647e80c4204b89f3d0a2b
David Catuhe <david.catuhe@live.fr>
2024-06-11 08:01:09 -0700

    Fix svg image (#15185)
    * Fix build
    
    * Remove engine

--- packages/dev/gui/src/2D/controls/image.ts
+++ packages/dev/gui/src/2D/controls/image.ts
@@ -530,8 +530,10 @@ export class Image extends Control {
             value.timesUsed -= 1;
 
             // Remove from DOM
-            const engine = this._host?.getScene()?.getEngine() || EngineStore.LastCreatedEngine;
-            engine?.getRenderingCanvas()?.parentNode?.removeChild(value.img as HTMLImageElement);
+            const htmlElement = value.img as HTMLImageElement;
+            if (htmlElement.parentNode) {
+                htmlElement.parentNode.removeChild(htmlElement);
+            }
 
             // Since the image isn't being used anymore, we can clean it from the cache
             if (value.timesUsed === 0) {
