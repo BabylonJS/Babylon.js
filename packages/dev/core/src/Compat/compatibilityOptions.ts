57728a71a7dede24eccae72d220c440666b94e0e
James Prevett <jp@jamespre.dev>
2024-08-07 18:53:02 -0500

    Backward compatibility

--- packages/dev/core/src/Compat/compatibilityOptions.ts
+++ packages/dev/core/src/Compat/compatibilityOptions.ts
@@ -4,3 +4,11 @@
  * Defines if the system should use OpenGL convention for UVs when creating geometry or loading .babylon files (false by default)
  */
 export const useOpenGLOrientationForUV = false;
+
+/**
+ * @deprecated use compatibility options variables
+ */
+export const CompatibilityOptions = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    UseOpenGLOrientationForUV: useOpenGLOrientationForUV,
+};
