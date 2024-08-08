6bab25cb644e8f3e38f19ce648aeb4893f6fc9e1
James Prevett <jp@jamespre.dev>
2024-08-08 09:10:53 -0500

    Migrate `EngineFactory` from a static class to functions

--- packages/dev/core/src/Engines/engineFactory.ts
+++ packages/dev/core/src/Engines/engineFactory.ts
@@ -4,23 +4,25 @@ import { NullEngine } from "./nullEngine";
 import { WebGPUEngine } from "./webgpuEngine";
 
 /**
- * Helper class to create the best engine depending on the current hardware
+ * Creates an engine based on the capabilities of the underlying hardware
+ * @param canvas Defines the canvas to use to display the result
+ * @param options Defines the options passed to the engine to create the context dependencies
+ * @returns a promise that resolves with the created engine
  */
-export class EngineFactory {
-    /**
-     * Creates an engine based on the capabilities of the underlying hardware
-     * @param canvas Defines the canvas to use to display the result
-     * @param options Defines the options passed to the engine to create the context dependencies
-     * @returns a promise that resolves with the created engine
-     */
-    public static async CreateAsync(canvas: HTMLCanvasElement, options: any): Promise<AbstractEngine> {
-        const supported = await WebGPUEngine.IsSupportedAsync;
-        if (supported) {
-            return WebGPUEngine.CreateAsync(canvas, options);
-        }
-        if (Engine.IsSupported) {
-            return new Engine(canvas, undefined, options);
-        }
-        return new NullEngine(options);
+export async function CreateEngineAsync(canvas: HTMLCanvasElement, options: any): Promise<AbstractEngine> {
+    const supported = await WebGPUEngine.IsSupportedAsync;
+    if (supported) {
+        return WebGPUEngine.CreateAsync(canvas, options);
     }
+    if (Engine.IsSupported) {
+        return new Engine(canvas, undefined, options);
+    }
+    return new NullEngine(options);
 }
+
+/**
+ * Helper to create the best engine depending on the current hardware
+ * @deprecated use `CreateEngineAsync`
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const EngineFactory = { CreateAsync: CreateEngineAsync };
