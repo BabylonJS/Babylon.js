627f3b1a7dd38591e1abac65f491e29af4f80e66
Raanan Weber <raananw+github@gmail.com>
2024-08-08 18:16:46 +0200

    Wait for effect creation, if the effect is not available yet

--- packages/dev/core/src/PostProcesses/postProcess.ts
+++ packages/dev/core/src/PostProcesses/postProcess.ts
@@ -439,6 +439,12 @@ export class PostProcess {
         return this._fragmentUrl;
     }
 
+    /**
+     * Executed when the effect was created
+     * @returns effect that was created for this post process
+     */
+    public onEffectCreatedObservable = new Observable<Effect>();
+
     // Events
 
     /**
@@ -832,6 +838,7 @@ export class PostProcess {
             },
             this._engine
         );
+        this.onEffectCreatedObservable.notifyObservers(this._drawWrapper.effect);
     }
 
     /**
