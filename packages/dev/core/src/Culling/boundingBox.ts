b9f55ac46a3c6087f533716ae927720eb3c50faf
James Prevett <jp@jamespre.dev>
2024-08-08 07:11:35 -0500

    Migrate `ArrayTools`

--- packages/dev/core/src/Culling/boundingBox.ts
+++ packages/dev/core/src/Culling/boundingBox.ts
@@ -1,5 +1,5 @@
 import type { DeepImmutable, Nullable } from "../types";
-import { ArrayTools } from "../Misc/arrayTools";
+import { BuildArray } from "../Misc/arrayTools";
 import { Matrix, Vector3 } from "../Maths/math.vector";
 import type { BoundingSphere } from "../Culling/boundingSphere";
 
@@ -16,7 +16,7 @@ export class BoundingBox implements ICullable {
     /**
      * Gets the 8 vectors representing the bounding box in local space
      */
-    public readonly vectors: Vector3[] = ArrayTools.BuildArray(8, Vector3.Zero);
+    public readonly vectors: Vector3[] = BuildArray(8, Vector3.Zero);
     /**
      * Gets the center of the bounding box in local space
      */
@@ -36,11 +36,11 @@ export class BoundingBox implements ICullable {
     /**
      * Gets the OBB (object bounding box) directions
      */
-    public readonly directions: Vector3[] = ArrayTools.BuildArray(3, Vector3.Zero);
+    public readonly directions: Vector3[] = BuildArray(3, Vector3.Zero);
     /**
      * Gets the 8 vectors representing the bounding box in world space
      */
-    public readonly vectorsWorld: Vector3[] = ArrayTools.BuildArray(8, Vector3.Zero);
+    public readonly vectorsWorld: Vector3[] = BuildArray(8, Vector3.Zero);
     /**
      * Gets the minimum vector in world space
      */
@@ -59,7 +59,7 @@ export class BoundingBox implements ICullable {
     public readonly maximum: Vector3 = Vector3.Zero();
 
     private _worldMatrix: DeepImmutable<Matrix>;
-    private static readonly _TmpVector3 = ArrayTools.BuildArray(3, Vector3.Zero);
+    private static readonly _TmpVector3 = BuildArray(3, Vector3.Zero);
 
     /**
      * @internal
