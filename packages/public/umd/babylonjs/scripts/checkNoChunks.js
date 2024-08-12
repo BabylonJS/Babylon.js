d8767e3f6252547bbca0eb846a22e004e759d3aa
Raanan Weber <info@raananweber.com>
2024-08-12 17:24:30 +0200

    Better chunk error messages for the CI (#15407)
    * Better chunk error messages for the CI
    
    * Back to normal

--- packages/public/umd/babylonjs/scripts/checkNoChunks.js
+++ packages/public/umd/babylonjs/scripts/checkNoChunks.js
@@ -7,15 +7,19 @@ const files = fs.readdirSync(directoryPath);
 
 // check there are no files that start with number or dev_core
 let hasError = false;
-files.some((file) => {
+files.forEach((file) => {
     if (file.startsWith("dev_core") || !isNaN(file.charAt(0))) {
-        // eslint-disable-next-line no-console
-        console.error(`#error Chunks were introduced to UMD in this build.`);
+        if (file.startsWith("dev_core")) {
+            // eslint-disable-next-line no-console
+            console.error(`##[error] Chunk was generated from the following file/directory: ${file.replace(/_/g, "/").replace(".babylon.max.js", "")}`);
+        }
         hasError = true;
     }
     return hasError;
 });
 
 if (hasError) {
+    // eslint-disable-next-line no-console
+    console.error(`##[error] Chunks were introduced to UMD in this build.`);
     process.exit(1);
 }
