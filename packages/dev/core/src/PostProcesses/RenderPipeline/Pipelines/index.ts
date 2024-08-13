715c6964f32d53e4e285fe37f645766c8ba30807
David Catuhe <david.catuhe@live.fr>
2024-08-13 12:11:18 -0700

    SSR to webgpu (#15420)
    * SSR to WebGPU
    
    * SSR to webgpu

--- packages/dev/core/src/PostProcesses/RenderPipeline/Pipelines/index.ts
+++ packages/dev/core/src/PostProcesses/RenderPipeline/Pipelines/index.ts
@@ -11,3 +11,11 @@ export * from "../../../Shaders/ssao2.fragment";
 export * from "../../../Shaders/ssaoCombine.fragment";
 export * from "../../../ShadersWGSL/ssao2.fragment";
 export * from "../../../ShadersWGSL/ssaoCombine.fragment";
+
+// SSR
+export * from "../../../Shaders/screenSpaceReflection2.fragment";
+export * from "../../../Shaders/screenSpaceReflection2Blur.fragment";
+export * from "../../../Shaders/screenSpaceReflection2BlurCombiner.fragment";
+export * from "../../../ShadersWGSL/screenSpaceReflection2.fragment";
+export * from "../../../ShadersWGSL/screenSpaceReflection2Blur.fragment";
+export * from "../../../ShadersWGSL/screenSpaceReflection2BlurCombiner.fragment";
