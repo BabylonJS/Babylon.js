0001dd9c0c9dbf83592b0be3454fae48545f3523
Deltakosh <david.catuhe@live.fr>
2024-08-20 11:17:25 -0700

    rename

--- packages/dev/core/src/Materials/Textures/Loaders/index.ts
+++ packages/dev/core/src/Materials/Textures/Loaders/index.ts
@@ -7,4 +7,4 @@ export * from "./hdrTextureLoader";
 export * from "./basisTextureLoader";
 export * from "./exrTextureLoader";
 export * from "./textureLoaderManager";
-export * from "./toto";
+export * from "./EXR";
