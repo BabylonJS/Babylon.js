011c9f3095ba4cb0a731f9aaae55fae3ba4a98c4
David Catuhe <david.catuhe@live.fr>
2024-09-30 13:19:14 -0700

    Fix #15642 (#15652)

--- packages/dev/core/src/Meshes/Node/Blocks/geometryOptimizeBlock.ts
+++ packages/dev/core/src/Meshes/Node/Blocks/geometryOptimizeBlock.ts
@@ -26,6 +26,12 @@ export class GeometryOptimizeBlock extends NodeGeometryBlock {
     public epsilon = Epsilon;
 
     /**
+     * Optimize faces (by removing duplicates)
+     */
+    @editableInPropertyPage("Optimize faces", PropertyTypeForEdition.Boolean, "ADVANCED", { notifiers: { rebuild: true } })
+    public optimizeFaces = false;
+
+    /**
      * Creates a new GeometryOptimizeBlock
      * @param name defines the block name
      */
@@ -67,6 +73,7 @@ export class GeometryOptimizeBlock extends NodeGeometryBlock {
             const newPositions: FloatArray = [];
             const newIndicesMap: { [key: number]: number } = {};
 
+            // Optimize positions
             for (let index = 0; index < vertexData.positions.length; index += 3) {
                 const x = vertexData.positions[index];
                 const y = vertexData.positions[index + 1];
@@ -93,7 +100,48 @@ export class GeometryOptimizeBlock extends NodeGeometryBlock {
             }
             const newVertexData = new VertexData();
             newVertexData.positions = newPositions;
-            newVertexData.indices = vertexData.indices.map((index: number) => newIndicesMap[index]);
+            const indices: number[] = vertexData.indices.map((index: number) => newIndicesMap[index]);
+            const newIndices: number[] = [];
+
+            if (this.optimizeFaces) {
+                // Optimize indices
+                for (let index = 0; index < indices.length; index += 3) {
+                    const a = indices[index];
+                    const b = indices[index + 1];
+                    const c = indices[index + 2];
+
+                    if (a === b || b == c || c === a) {
+                        continue;
+                    }
+
+                    // check if we already have it
+                    let found = false;
+                    for (let checkIndex = 0; checkIndex < newIndices.length; checkIndex += 3) {
+                        if (a === newIndices[checkIndex] && b === newIndices[checkIndex + 1] && c === newIndices[checkIndex + 2]) {
+                            found = true;
+                            continue;
+                        }
+
+                        if (a === newIndices[checkIndex + 1] && b === newIndices[checkIndex + 2] && c === newIndices[checkIndex]) {
+                            found = true;
+                            continue;
+                        }
+
+                        if (a === newIndices[checkIndex + 2] && b === newIndices[checkIndex] && c === newIndices[checkIndex + 1]) {
+                            found = true;
+                            continue;
+                        }
+                    }
+
+                    if (!found) {
+                        newIndices.push(a, b, c);
+                    }
+                }
+
+                newVertexData.indices = newIndices;
+            } else {
+                newVertexData.indices = indices;
+            }
 
             return newVertexData;
         };
@@ -109,6 +157,7 @@ export class GeometryOptimizeBlock extends NodeGeometryBlock {
     protected override _dumpPropertiesCode() {
         let codeString = super._dumpPropertiesCode() + `${this._codeVariableName}.evaluateContext = ${this.evaluateContext ? "true" : "false"};\n`;
         codeString += `${this._codeVariableName}.epsilon = ${this.epsilon};\n`;
+        codeString += `${this._codeVariableName}.optimizeFaces = ${this.optimizeFaces ? "true" : "false"};\n`;
         return codeString;
     }
 
@@ -121,6 +170,7 @@ export class GeometryOptimizeBlock extends NodeGeometryBlock {
 
         serializationObject.evaluateContext = this.evaluateContext;
         serializationObject.epsilon = this.epsilon;
+        serializationObject.optimizeFaces = this.optimizeFaces;
 
         return serializationObject;
     }
@@ -130,6 +180,7 @@ export class GeometryOptimizeBlock extends NodeGeometryBlock {
 
         this.evaluateContext = serializationObject.evaluateContext;
         this.epsilon = serializationObject.epsilon;
+        this.optimizeFaces = serializationObject.optimizeFaces;
     }
 }
 
