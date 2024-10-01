8b8f8c46a9487c48dff715460bf5725b94d095df
Raanan Weber <raananw+github@gmail.com>
2024-10-01 18:18:28 +0200

    make sure we are not disposing the wrong animation

--- packages/dev/core/src/FlowGraph/Blocks/Execution/Animation/flowGraphPlayAnimationGroupBlock.ts
+++ packages/dev/core/src/FlowGraph/Blocks/Execution/Animation/flowGraphPlayAnimationGroupBlock.ts
@@ -107,7 +107,7 @@ export class FlowGraphPlayAnimationBlock extends FlowGraphAsyncExecutionBlock {
             // if an animation group was created, dispose it and create a new one
             // TODO - is it possible to be sure this aniamtionGroup can be reused?
             const currentAnimationGroup = this.currentAnimationGroup.getValue(context);
-            if (currentAnimationGroup) {
+            if (currentAnimationGroup && currentAnimationGroup !== ag) {
                 currentAnimationGroup.dispose();
             }
             let animationGroupToUse = ag;
