1a769ea42395c4d0298140b334433031a35eab73
Andy Fillebrown <afillebrown@microsoft.com>
2024-10-04 10:23:43 -0400

    Make AbstractAUdioNodeParent children public readonly

--- packages/dev/core/src/Audio/v2/abstractAudioNodeParent.ts
+++ packages/dev/core/src/Audio/v2/abstractAudioNodeParent.ts
@@ -2,25 +2,17 @@
 /* eslint-disable jsdoc/require-jsdoc */
 
 import type { AbstractAudioNode } from "./abstractAudioNode";
-import type { Nullable } from "../../types";
 import type { IDisposable } from "../../scene";
 
 export class AbstractAudioNodeParent implements IDisposable {
-    private _children: Nullable<Set<AbstractAudioNode>>;
+    public readonly children = new Set<AbstractAudioNode>();
 
     public dispose(): void {
-        if (this._children) {
-            for (const node of this._children) {
+        if (this.children) {
+            for (const node of this.children) {
                 node.dispose();
             }
-            this._children.clear();
+            this.children.clear();
         }
-    }
-
-    public get children(): Set<AbstractAudioNode> {
-        if (!this._children) {
-            this._children = new Set();
-        }
-        return this._children;
     }
 }
