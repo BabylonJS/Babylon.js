a49658f51fa06e4ce31736865d1b1205977486c1
David Catuhe <david.catuhe@live.fr>
2024-10-07 14:34:09 -0700

    Allow user to overload picking mechanism

--- packages/dev/core/src/Culling/ray.core.ts
+++ packages/dev/core/src/Culling/ray.core.ts
@@ -25,6 +25,25 @@ export type MeshPredicate = (mesh: AbstractMesh, thinInstanceIndex: number) => b
 export type TrianglePickingPredicate = (p0: Vector3, p1: Vector3, p2: Vector3, ray: Ray, i0: number, i1: number, i2: number) => boolean;
 
 /**
+ * This class allows user to customize internal picking mechanism
+ */
+export class PickingCustomization {
+    /**
+     * Predicate to select faces when a mesh intersection is detected
+     */
+    public static InternalPickerForMesh?: (
+        pickingInfo: Nullable<PickingInfo>,
+        rayFunction: (world: Matrix, enableDistantPicking: boolean) => Ray,
+        mesh: AbstractMesh,
+        world: Matrix,
+        fastCheck?: boolean,
+        onlyBoundingInfo?: boolean,
+        trianglePredicate?: TrianglePickingPredicate,
+        skipBoundingInfo?: boolean
+    ) => PickingInfo;
+}
+
+/**
  * Class representing a ray with position and direction
  */
 export class Ray {
@@ -826,6 +845,7 @@ function InternalPick(
 
     const computeWorldMatrixForCamera = !!(scene.activeCameras && scene.activeCameras.length > 1 && scene.cameraToUseForPointers !== scene.activeCamera);
     const currentCamera = scene.cameraToUseForPointers || scene.activeCamera;
+    const picker = PickingCustomization.InternalPickerForMesh || InternalPickForMesh;
 
     for (let meshIndex = 0; meshIndex < scene.meshes.length; meshIndex++) {
         const mesh = scene.meshes[meshIndex];
@@ -843,7 +863,7 @@ function InternalPick(
 
         if (mesh.hasThinInstances && (mesh as Mesh).thinInstanceEnablePicking) {
             // first check if the ray intersects the whole bounding box/sphere of the mesh
-            const result = InternalPickForMesh(pickingInfo, rayFunction, mesh, world, true, true, trianglePredicate);
+            const result = picker(pickingInfo, rayFunction, mesh, world, true, true, trianglePredicate);
             if (result) {
                 if (onlyBoundingInfo) {
                     // the user only asked for a bounding info check so we can return
@@ -857,7 +877,7 @@ function InternalPick(
                     }
                     const thinMatrix = thinMatrices[index];
                     thinMatrix.multiplyToRef(world, tmpMatrix);
-                    const result = InternalPickForMesh(pickingInfo, rayFunction, mesh, tmpMatrix, fastCheck, onlyBoundingInfo, trianglePredicate, true);
+                    const result = picker(pickingInfo, rayFunction, mesh, tmpMatrix, fastCheck, onlyBoundingInfo, trianglePredicate, true);
 
                     if (result) {
                         pickingInfo = result;
@@ -870,7 +890,7 @@ function InternalPick(
                 }
             }
         } else {
-            const result = InternalPickForMesh(pickingInfo, rayFunction, mesh, world, fastCheck, onlyBoundingInfo, trianglePredicate);
+            const result = picker(pickingInfo, rayFunction, mesh, world, fastCheck, onlyBoundingInfo, trianglePredicate);
 
             if (result) {
                 pickingInfo = result;
@@ -897,6 +917,7 @@ function InternalMultiPick(
     const pickingInfos: PickingInfo[] = [];
     const computeWorldMatrixForCamera = !!(scene.activeCameras && scene.activeCameras.length > 1 && scene.cameraToUseForPointers !== scene.activeCamera);
     const currentCamera = scene.cameraToUseForPointers || scene.activeCamera;
+    const picker = PickingCustomization.InternalPickerForMesh || InternalPickForMesh;
 
     for (let meshIndex = 0; meshIndex < scene.meshes.length; meshIndex++) {
         const mesh = scene.meshes[meshIndex];
@@ -913,7 +934,7 @@ function InternalMultiPick(
         const world = mesh.computeWorldMatrix(forceCompute, currentCamera);
 
         if (mesh.hasThinInstances && (mesh as Mesh).thinInstanceEnablePicking) {
-            const result = InternalPickForMesh(null, rayFunction, mesh, world, true, true, trianglePredicate);
+            const result = picker(null, rayFunction, mesh, world, true, true, trianglePredicate);
             if (result) {
                 const tmpMatrix = TmpVectors.Matrix[1];
                 const thinMatrices = (mesh as Mesh).thinInstanceGetWorldMatrices();
@@ -923,7 +944,7 @@ function InternalMultiPick(
                     }
                     const thinMatrix = thinMatrices[index];
                     thinMatrix.multiplyToRef(world, tmpMatrix);
-                    const result = InternalPickForMesh(null, rayFunction, mesh, tmpMatrix, false, false, trianglePredicate, true);
+                    const result = picker(null, rayFunction, mesh, tmpMatrix, false, false, trianglePredicate, true);
 
                     if (result) {
                         result.thinInstanceIndex = index;
@@ -932,7 +953,7 @@ function InternalMultiPick(
                 }
             }
         } else {
-            const result = InternalPickForMesh(null, rayFunction, mesh, world, false, false, trianglePredicate);
+            const result = picker(null, rayFunction, mesh, world, false, false, trianglePredicate);
 
             if (result) {
                 pickingInfos.push(result);
