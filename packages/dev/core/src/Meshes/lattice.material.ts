8b5fed48e03d1788ecb636e0f26facd7787fe5ea
David Catuhe <david.catuhe@live.fr>
2024-10-14 12:59:59 -0700

    Fix es6

--- packages/dev/core/src/Meshes/lattice.material.ts
+++ packages/dev/core/src/Meshes/lattice.material.ts
@@ -1,10 +1,11 @@
-import type { UniformBuffer } from "core/Materials";
-import { ShaderLanguage, RawTexture3D } from "core/Materials";
+import { RawTexture3D } from "core/Materials/Textures/rawTexture3D";
 import type { Material } from "core/Materials/material";
 import { MaterialPluginBase } from "core/Materials/materialPluginBase";
 import type { Lattice } from "./lattice";
 import type { Nullable } from "core/types";
 import { Constants } from "core/Engines/constants";
+import { ShaderLanguage } from "core/Materials/shaderLanguage";
+import type { UniformBuffer } from "core/Materials/uniformBuffer";
 
 /**
  * Material plugin to add hardware accelerated lattice support
