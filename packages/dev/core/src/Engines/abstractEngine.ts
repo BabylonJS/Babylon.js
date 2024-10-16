c3130779aa27d961f8b29fa5479a4c4bf5574b37
urus-cibot <urus-cibot@users.noreply.github.com>
2024-10-16 05:54:21 +0000

    Version update 7.15.4

--- packages/dev/core/src/Engines/abstractEngine.ts
+++ packages/dev/core/src/Engines/abstractEngine.ts
@@ -1790,14 +1790,14 @@ export abstract class AbstractEngine {
      */
     // Not mixed with Version for tooling purpose.
     public static get NpmPackage(): string {
-        return "babylonjs@7.15.3";
+        return "babylonjs@7.15.4";
     }
 
     /**
      * Returns the current version of the framework
      */
     public static get Version(): string {
-        return "7.15.3";
+        return "7.15.4";
     }
 
     /**
