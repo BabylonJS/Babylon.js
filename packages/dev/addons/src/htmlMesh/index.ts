c0e4238a1d550a7afa53be927af126d7cb3db9ef
Deltakosh <david.catuhe@live.fr>
2024-11-05 15:33:11 -0800

    initial push

---
+++ packages/dev/addons/src/htmlMesh/index.ts
@@ -0,0 +1,7 @@
+import { HtmlMeshRenderer } from "./html-mesh-renderer";
+import { HtmlMesh } from "./html-mesh";
+import { PointerEventsCaptureBehavior } from "./pointer-events-capture-behavior";
+import { FitStrategy } from "./fit-strategy";
+
+// Export public classes and functions
+export { HtmlMeshRenderer, HtmlMesh, PointerEventsCaptureBehavior, FitStrategy };
