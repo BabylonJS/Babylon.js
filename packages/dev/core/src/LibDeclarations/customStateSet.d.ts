50c74bed4c9d10654e782e9003e97eb2ea157a67
Ryan Tremblay <ryan.tremblay@microsoft.com>
2024-11-15 09:55:42 -0800

    Attempt to add declarations for CustomStateSet

---
+++ packages/dev/core/src/LibDeclarations/customStateSet.d.ts
@@ -0,0 +1,15 @@
+/**
+ * TODO: remove this file when we upgrade to TypeScript 5.5
+ */
+
+interface CustomStateSet {
+    readonly size: number;
+    add(state: string): void;
+    clear(): void;
+    delete(state: string): void;
+    has(state: string): boolean;
+}
+
+interface ElementInternals {
+    readonly states: CustomStateSet; // TODO: Make this optional?
+}
