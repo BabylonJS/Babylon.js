775a6e6490cde0122fa57356f901adfda8bbcbbe
Raanan Weber <raananw+github@gmail.com>
2024-11-19 21:15:19 +0100

    support interpolation better

--- packages/dev/core/src/FlowGraph/Blocks/Execution/Animation/flowGraphPlayAnimationBlock.ts
+++ packages/dev/core/src/FlowGraph/Blocks/Execution/Animation/flowGraphPlayAnimationBlock.ts
@@ -96,13 +96,13 @@ export class FlowGraphPlayAnimationBlock extends FlowGraphAsyncExecutionBlock {
             return;
         } else {
             // if an animation group was created, dispose it and create a new one
-            // TODO - is it possible to be sure this aniamtionGroup can be reused?
+            // TODO - is it possible to be sure this animationGroup can be reused?
             const currentAnimationGroup = this.currentAnimationGroup.getValue(context);
             if (currentAnimationGroup && currentAnimationGroup !== ag) {
                 currentAnimationGroup.dispose();
             }
             let animationGroupToUse = ag;
-            // check which animation to use
+            // check which animation to use. If no animationGroup was defined and an animation was provided, use the animation
             if (animation && !animationGroupToUse) {
                 const target = this.object.getValue(context);
                 if (!target) {
@@ -111,12 +111,23 @@ export class FlowGraphPlayAnimationBlock extends FlowGraphAsyncExecutionBlock {
                 }
                 const name = Array.isArray(animation) ? animation[0].name : animation.name;
                 animationGroupToUse = new AnimationGroup("flowGraphAnimationGroup-" + name + "-" + target.name, context.configuration.scene);
+                let isInterpolation = false;
+                const interpolationAnimations = context._getGlobalContextVariable("interpolationAnimations", []) as number[];
                 if (Array.isArray(animation)) {
                     for (const anim of animation) {
                         animationGroupToUse.addTargetedAnimation(anim, target);
+                        if (interpolationAnimations.indexOf(anim.uniqueId) !== -1) {
+                            isInterpolation = true;
+                        }
                     }
                 } else {
                     animationGroupToUse.addTargetedAnimation(animation, target);
+                    if (interpolationAnimations.indexOf(animation.uniqueId) !== -1) {
+                        isInterpolation = true;
+                    }
+                }
+                if (isInterpolation) {
+                    this._checkInterpolationDuplications(context, animation, target);
                 }
             }
             // not accepting 0
@@ -160,8 +171,54 @@ export class FlowGraphPlayAnimationBlock extends FlowGraphAsyncExecutionBlock {
     }
 
     private _onAnimationGroupEnd(context: FlowGraphContext) {
-        context._removePendingBlock(this);
+        const currentlyRunningAnimationGroups = context._getGlobalContextVariable("currentlyRunningAnimationGroups", []) as number[];
+        const index = currentlyRunningAnimationGroups.indexOf(this.currentAnimationGroup.getValue(context).uniqueId);
+        if (index !== -1) {
+            currentlyRunningAnimationGroups.splice(index, 1);
+            context._setGlobalContextVariable("currentlyRunningAnimationGroups", currentlyRunningAnimationGroups);
+        }
+        this._resetAfterCanceled(context);
         this.done._activateSignal(context);
+    }
+
+    /**
+     * The idea behind this function is to check every running animation group and check if the targeted animations it uses are interpolation animations.
+     * If they are, we want to see that they don't collide with the current interpolation animations that are starting to play.
+     * If they do, we want to stop the already-running animation group.
+     * @internal
+     */
+    private _checkInterpolationDuplications(context: FlowGraphContext, animation: Animation | Animation[], target: any) {
+        const currentlyRunningAnimationGroups = context._getGlobalContextVariable("currentlyRunningAnimationGroups", []) as number[];
+        for (const uniqueId of currentlyRunningAnimationGroups) {
+            const ag = context.assetsContext.animationGroups.find((ag) => ag.uniqueId === uniqueId);
+            if (ag) {
+                for (const anim of ag.targetedAnimations) {
+                    if (Array.isArray(animation)) {
+                        for (const animToCheck of animation) {
+                            if (anim.animation.targetProperty === animToCheck.targetProperty && anim.target === target) {
+                                this._stopAnimationGroup(context, ag);
+                            }
+                        }
+                    } else {
+                        if (anim.animation.targetProperty === animation.targetProperty && anim.target === target) {
+                            this._stopAnimationGroup(context, ag);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private _stopAnimationGroup(context: FlowGraphContext, animationGroup: AnimationGroup) {
+        animationGroup.stop();
+        animationGroup.dispose();
+        const currentlyRunningAnimationGroups = context._getGlobalContextVariable("currentlyRunningAnimationGroups", []) as number[];
+        // check if it already running
+        const idx = currentlyRunningAnimationGroups.indexOf(animationGroup.uniqueId);
+        if (idx !== -1) {
+            currentlyRunningAnimationGroups.splice(idx, 1);
+            context._setGlobalContextVariable("currentlyRunningAnimationGroups", currentlyRunningAnimationGroups);
+        }
     }
 
     /**
@@ -171,13 +228,7 @@ export class FlowGraphPlayAnimationBlock extends FlowGraphAsyncExecutionBlock {
     public _cancelPendingTasks(context: FlowGraphContext): void {
         const ag = this.currentAnimationGroup.getValue(context);
         if (ag) {
-            ag.stop();
-            const currentlyRunningAnimationGroups = context._getGlobalContextVariable("currentlyRunningAnimationGroups", []) as number[];
-            // check if it already running
-            if (currentlyRunningAnimationGroups.indexOf(ag.uniqueId) !== -1) {
-                currentlyRunningAnimationGroups.splice(currentlyRunningAnimationGroups.indexOf(ag.uniqueId), 1);
-                context._setGlobalContextVariable("currentlyRunningAnimationGroups", currentlyRunningAnimationGroups);
-            }
+            this._stopAnimationGroup(context, ag);
         }
     }
 
