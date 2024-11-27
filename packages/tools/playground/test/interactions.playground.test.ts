83b4bf25adfffb18649ca2c666161c6816caadb5
Raanan Weber <info@raananweber.com>
2024-11-27 15:55:20 +0100

    Allow overriding the playground URL for testing (#15901)
    * Allow overriding the playground URL for testing
    
    * fix async calls
    
    * update snapshot

--- packages/tools/playground/test/interactions.playground.test.ts
+++ packages/tools/playground/test/interactions.playground.test.ts
@@ -1,43 +1,45 @@
 import { test, expect } from "@playwright/test";
 
+const url = process.env.PLAYGROUND_BASE_URL || "https://playground.babylonjs.com/";
+
 test("Playground is loaded (Desktop)", async ({ page }) => {
-    await page.goto("https://playground.babylonjs.com/", {
+    await page.goto(url, {
         waitUntil: "networkidle",
     });
-    page.setViewportSize({
+    await page.setViewportSize({
         width: 1920,
         height: 1080,
     });
     // check visibility of both canvas AND the editor
-    expect(page.locator("#canvasZone")).toBeVisible();
-    expect(page.locator("#monacoHost")).toBeVisible();
-    expect(page.locator("#pg-header")).toBeVisible();
+    await expect(page.locator("#canvasZone")).toBeVisible();
+    await expect(page.locator("#monacoHost")).toBeVisible();
+    await expect(page.locator("#pg-header")).toBeVisible();
 });
 
 test("Playground is loaded (Mobile)", async ({ page }) => {
-    await page.goto("https://playground.babylonjs.com/", {
+    await page.goto(url, {
         waitUntil: "networkidle",
     });
-    page.setViewportSize({
+    await page.setViewportSize({
         width: 800,
         height: 1080,
     });
     // check visibility of both canvas AND the editor
-    expect(page.locator("#canvasZone")).toBeVisible();
-    expect(page.locator("#monacoHost")).not.toBeVisible();
-    expect(page.locator(".hamburger-button")).toBeVisible();
+    await expect(page.locator("#canvasZone")).toBeVisible();
+    await expect(page.locator("#monacoHost")).not.toBeVisible();
+    await expect(page.locator(".hamburger-button")).toBeVisible();
 
     // click the "Switch to code" link
     await page.getByTitle("Switch to code").click();
-    expect(page.locator("#canvasZone")).not.toBeVisible();
-    expect(page.locator("#monacoHost")).toBeVisible();
+    await expect(page.locator("#canvasZone")).not.toBeVisible();
+    await expect(page.locator("#monacoHost")).toBeVisible();
 });
 
 test("Examples can be loaded", async ({ page }) => {
-    await page.goto("https://playground.babylonjs.com/", {
+    await page.goto(url, {
         waitUntil: "networkidle",
     });
-    page.setViewportSize({
+    await page.setViewportSize({
         width: 1920,
         height: 1080,
     });
@@ -45,7 +47,7 @@ test("Examples can be loaded", async ({ page }) => {
     // click the "Examples" button
     await page.getByTitle("Examples").click();
 
-    expect(page.locator("#examples")).toBeVisible();
+    await expect(page.locator("#examples")).toBeVisible();
 
     await page.locator(".example").nth(3).click();
 
@@ -53,17 +55,17 @@ test("Examples can be loaded", async ({ page }) => {
 });
 
 test("User can interact with the playground", async ({ page }) => {
-    await page.goto("https://playground.babylonjs.com/", {
+    await page.goto(url, {
         waitUntil: "networkidle",
     });
-    page.setViewportSize({
+    await page.setViewportSize({
         width: 1920,
         height: 1080,
     });
 
     await page.locator(".view-lines > div:nth-child(16)").click();
     await page.keyboard.type("camera", { delay: 50 });
-    expect(page.locator(".editor-widget")).toBeVisible();
+    await expect(page.locator(".editor-widget")).toBeVisible();
     await page.waitForTimeout(100);
     await page.getByLabel("camera", { exact: true }).locator("span").filter({ hasText: "camera" }).first().click();
 
