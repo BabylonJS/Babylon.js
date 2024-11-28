3e8370f9b71c79acadb0a41e5f3ae54d4753334e
Raanan Weber <raananw+github@gmail.com>
2024-11-28 21:06:39 +0100

    Added sandbox tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -85,6 +85,11 @@ export default defineConfig({
             testMatch: "**/*.playground.test.ts",
             use: getUseDefinition("Playground"),
         },
+        {
+            name: "sandbox",
+            testMatch: "**/*.sandbox.test.ts",
+            use: getUseDefinition("Sandbox"),
+        },
     ],
 
     snapshotPathTemplate: "packages/tools/tests/test/visualization/ReferenceImages/{arg}{ext}",
