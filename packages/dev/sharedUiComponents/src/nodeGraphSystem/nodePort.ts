435122a60c7fffc3ffff9339ede0bc444ef02cc9
Raanan Weber <info@raananweber.com>
2024-11-28 13:56:09 +0100

    SCSS - run an update of dependencies, move to modern API - v2 (#15884)
    * Revert "Revert "SCSS - run an update of dependencies, move to modern API""
    
    * Fix the css issue
    
    * fix build issue after merge from master

--- packages/dev/sharedUiComponents/src/nodeGraphSystem/nodePort.ts
+++ packages/dev/sharedUiComponents/src/nodeGraphSystem/nodePort.ts
@@ -7,8 +7,8 @@ import type { ISelectionChangedOptions } from "./interfaces/selectionChangedOpti
 import type { FrameNodePort } from "./frameNodePort";
 import type { IDisplayManager } from "./interfaces/displayManager";
 import { PortDirectValueTypes, type IPortData } from "./interfaces/portData";
-import commonStyles from "./common.module.scss";
-import localStyles from "./nodePort.module.scss";
+import * as commonStyles from "./common.module.scss";
+import * as localStyles from "./nodePort.module.scss";
 import { BuildFloatUI } from "./tools";
 
 export class NodePort {
