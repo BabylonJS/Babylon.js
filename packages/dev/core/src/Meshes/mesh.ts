3bddd4fe1d410eaaf156bef4df8ad9e94f9c9192
Deltakosh <david.catuhe@live.fr>
2024-11-28 16:19:16 -0800

    .

--- packages/dev/core/src/Meshes/mesh.ts
+++ packages/dev/core/src/Meshes/mesh.ts
@@ -1534,7 +1534,7 @@ export class Mesh extends AbstractMesh implements IGetSetVerticesData {
         }
 
         // We do not want to wipe the shader cache here
-        // The shader will be reused or dispose if a new one is required
+        // The shader will be reused or disposed if a new one is required
         this.releaseSubMeshes(false);
         return new SubMesh(0, 0, totalVertices, 0, this.getTotalIndices(), this);
     }
