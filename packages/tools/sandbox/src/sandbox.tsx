7e83ce7e11c8f3dcdd4e9cd71b9d1133915e0ec3
Gary Hsu <bghgary@users.noreply.github.com>
2024-12-03 16:30:43 -0800

    Add tone mapping parameter to sandbox

--- packages/tools/sandbox/src/sandbox.tsx
+++ packages/tools/sandbox/src/sandbox.tsx
@@ -15,6 +15,7 @@ import { Color3, Color4 } from "core/Maths/math";
 import "./scss/main.scss";
 import fullScreenLogo from "./img/logo-fullscreen.svg";
 import type { AbstractEngine } from "core/Engines/abstractEngine";
+import { ImageProcessingConfiguration } from "core/Materials/imageProcessingConfiguration";
 
 interface ISandboxProps {}
 
@@ -38,6 +39,7 @@ export class Sandbox extends React.Component<
     private _assetUrl?: string;
     private _autoRotate?: boolean;
     private _cameraPosition?: Vector3;
+    private _toneMapping?: number;
     private _logoRef: React.RefObject<HTMLImageElement>;
     private _dropTextRef: React.RefObject<HTMLDivElement>;
     private _clickInterceptorRef: React.RefObject<HTMLDivElement>;
@@ -168,16 +170,16 @@ export class Sandbox extends React.Component<
                 const split = param.split("=", 2);
                 const name = split[0];
                 const value = split[1];
-                switch (name) {
-                    case "assetUrl": {
+                switch (name.toLowerCase()) {
+                    case "asseturl": {
                         this._assetUrl = value;
                         break;
                     }
-                    case "autoRotate": {
+                    case "autorotate": {
                         this._autoRotate = value === "true" ? true : false;
                         break;
                     }
-                    case "cameraPosition": {
+                    case "cameraposition": {
                         this._cameraPosition = Vector3.FromArray(
                             value.split(",").map(function (component) {
                                 return +component;
@@ -217,13 +219,26 @@ export class Sandbox extends React.Component<
                         this._globalState.skybox = value === "true" ? true : false;
                         break;
                     }
-                    case "clearColor": {
+                    case "clearcolor": {
                         this._clearColor = value;
                         break;
                     }
                     case "camera": {
                         this._camera = +value;
                         break;
+                    }
+                    case "tonemapping": {
+                        switch (value.toLowerCase()) {
+                            case "standard":
+                                this._toneMapping = ImageProcessingConfiguration.TONEMAPPING_STANDARD;
+                                break;
+                            case "aces":
+                                this._toneMapping = ImageProcessingConfiguration.TONEMAPPING_ACES;
+                                break;
+                            case "khr_pbr_neutral":
+                                this._toneMapping = ImageProcessingConfiguration.TONEMAPPING_KHR_PBR_NEUTRAL;
+                                break;
+                        }
                     }
                 }
             }
@@ -250,6 +265,7 @@ export class Sandbox extends React.Component<
                             assetUrl={this._assetUrl}
                             autoRotate={this._autoRotate}
                             cameraPosition={this._cameraPosition}
+                            toneMapping={this._toneMapping}
                             expanded={!this.state.isFooterVisible}
                             onEngineCreated={this.onEngineCreated}
                         />
