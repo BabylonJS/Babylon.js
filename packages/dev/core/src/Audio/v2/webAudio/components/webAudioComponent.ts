ff23c09439dc8e6ebeb52fd33cbd4506f906b711
Andy Fillebrown <andy@docEdub.com>
2024-12-12 11:33:02 -0500

    Remove unused `WebAudioComponent` interface

--- packages/dev/core/src/Audio/v2/webAudio/components/webAudioComponent.ts
+++
@@ -1,7 +0,0 @@
-// import type { AbstractAudioComponent } from "../../components/abstractAudioComponent";
-
-// /** @internal */
-// export interface IWebAudioComponent extends AbstractAudioComponent {
-//     /** @internal */
-//     readonly node: AudioNode;
-// }
