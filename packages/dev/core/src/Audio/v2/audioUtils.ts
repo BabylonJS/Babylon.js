3c5cf268bf4c78788456d71a858ba61421d409aa
Andy Fillebrown <andy@docEdub.com>
2024-12-19 03:49:29 -0500

    Remove unused audio utils module

--- packages/dev/core/src/Audio/v2/audioUtils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Converts a pitch interval in cents to a playback rate.
- * @param cents - The pitch interval in cents.
- * @returns The playback rate.
- */
-export function centsToPlaybackRate(cents: number): number {
-    return Math.pow(2, cents / 1200);
-}
