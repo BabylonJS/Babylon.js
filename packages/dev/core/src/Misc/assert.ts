1fb6c1b1e5efd085643f1a65adcd16822a7fecc4
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-01-06 11:00:28 -0800

    Upper case and add use case

--- packages/dev/core/src/Misc/assert.ts
+++ packages/dev/core/src/Misc/assert.ts
@@ -2,7 +2,7 @@
  * Asserts that the given value is truthy.
  * @param value The value to check.
  */
-export function assert(value: unknown): asserts value {
+export function Assert(value: unknown): asserts value {
     if (!value) {
         throw new Error("assertion failed");
     }
