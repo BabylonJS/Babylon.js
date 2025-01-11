d611b660f2a5489e6208bc04d5034d42ee317ecc
Andy Fillebrown <andy@docEdub.com>
2025-01-11 01:26:23 -0500

    Improve mainAudioOut.ts docs

--- packages/dev/core/src/Audio/v2/mainAudioOut.ts
+++ packages/dev/core/src/Audio/v2/mainAudioOut.ts
@@ -3,6 +3,11 @@ import type { AudioEngineV2 } from "./audioEngineV2";
 
 /**
  * Abstract class for the main audio output node.
+ *
+ * A main audio output is the last audio node in the audio graph before the audio is sent to the speakers.
+ *
+ * @see {@link AudioEngineV2.mainOut}
+ * @internal
  */
 export abstract class MainAudioOut extends AbstractAudioNode {
     protected constructor(engine: AudioEngineV2) {
