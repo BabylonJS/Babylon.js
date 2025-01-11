1ec3ab2aad87506a31974270e0654dd020b5cdcb
Andy Fillebrown <andy@docEdub.com>
2025-01-11 01:21:48 -0500

    Improve mainAudioBus.ts docs

--- packages/dev/core/src/Audio/v2/mainAudioBus.ts
+++ packages/dev/core/src/Audio/v2/mainAudioBus.ts
@@ -8,10 +8,11 @@ export interface IMainAudioBusOptions extends IVolumeAudioOptions {}
 /**
  * Abstract class representing a main audio bus.
  *
- * Main audio buses are the last bus in the audio graph before connecting to the audio engine's main output node.
+ * Main audio buses are the last bus in the audio graph.
  *
- * Unlike an {@link AudioBus}, a `MainAudioBus` instance have no spatial audio and stereo output capabilities, and they
- * cannot be connected downstream to another audio bus.
+ * Unlike {@link AudioBus} instances, `MainAudioBus` instances have no spatial audio and stereo output capabilities,
+ * and they cannot be connected downstream to another audio bus. They only connect downstream to the audio engine's
+ * main output.
  */
 export abstract class MainAudioBus extends AbstractAudioBus {
     protected constructor(name: string, engine: AudioEngineV2) {
