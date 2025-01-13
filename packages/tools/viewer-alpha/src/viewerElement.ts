7681d6d1ec7ce5ce5c0037a4456253aa08735c6a
cournoll <cournoll@adobe.com>
2025-01-13 11:29:31 +0100

    feat: type styles to avoid error on ts on override

--- packages/tools/viewer-alpha/src/viewerElement.ts
+++ packages/tools/viewer-alpha/src/viewerElement.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-internal-modules
 import type { ArcRotateCamera, Nullable, Observable } from "core/index";
 
-import type { PropertyValues, TemplateResult } from "lit";
+import type { CSSResultGroup, PropertyValues, TemplateResult } from "lit";
 import type { EnvironmentOptions, ToneMapping, ViewerDetails, ViewerHotSpotQuery } from "./viewer";
 import type { CanvasViewerOptions } from "./viewerFactory";
 
@@ -160,7 +160,7 @@ export abstract class ViewerElement<ViewerClass extends Viewer = Viewer> extends
 
     /** @internal */
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    public static override styles = css`
+    public static override styles: CSSResultGroup = css`
         :host {
             --ui-foreground-color: white;
             --ui-background-hue: 233;
