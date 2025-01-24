1b6ede77c53e471b6358bda5f69a4d99ffb47810
Raanan Weber <raananw+github@gmail.com>
2025-01-24 13:01:26 +0100

    Add snapshot URL to tools snapshots

--- packages/tools/sandbox/test/interaction.sandbox.test.ts
+++ packages/tools/sandbox/test/interaction.sandbox.test.ts
@@ -7,7 +7,9 @@ test.beforeAll(async () => {
     test.setTimeout(30000);
 });
 
-const url = process.env.SANDBOX_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.SANDBOX_PORT || ":1339");
+// if running in the CI we need to use the babylon snapshot when loading the tools
+const snapshot = process.env.SNAPSHOT ? "?snapshot=" + process.env.SNAPSHOT : "";
+const url = process.env.SANDBOX_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.SANDBOX_PORT || ":1339") + snapshot;
 
 test("Sandbox is loaded (Desktop)", async ({ page }) => {
     await page.goto(url, {
--- packages/tools/tests/test/playwright/interaction.tools.test.ts
+++ packages/tools/tests/test/playwright/interaction.tools.test.ts
@@ -1,10 +1,12 @@
 import { getGlobalConfig } from "@tools/test-tools";
 import { test, expect } from "@playwright/test";
 
-const nmeUrl = process.env.NME_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.NME_PORT || ":1340");
-const ngeUrl = process.env.NGE_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.NGE_PORT || ":1343");
-const guiUrl = process.env.GUIEDITOR_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.GUIEDITOR_PORT || ":1341");
-const nrgeUrl = process.env.NRGE_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.NRGE_PORT || ":1344");
+// if running in the CI we need to use the babylon snapshot when loading the tools
+const snapshot = process.env.SNAPSHOT ? "?snapshot=" + process.env.SNAPSHOT : "";
+const nmeUrl = process.env.NME_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.NME_PORT || ":1340") + snapshot;
+const ngeUrl = process.env.NGE_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.NGE_PORT || ":1343") + snapshot;
+const guiUrl = process.env.GUIEDITOR_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.GUIEDITOR_PORT || ":1341") + snapshot;
+const nrgeUrl = process.env.NRGE_BASE_URL || getGlobalConfig().baseUrl.replace(":1337", process.env.NRGE_PORT || ":1344") + snapshot;
 
 test.beforeAll(async () => {
     // Set timeout for this hook.
