2e9bcb736a028532c0c9eb3c1093e6761a51801a
David Catuhe <david@catuhe.com>
2025-01-31 16:10:59 -0800

    Fix #14496

--- packages/tools/nodeEditor/src/components/preview/previewManager.ts
+++ packages/tools/nodeEditor/src/components/preview/previewManager.ts
@@ -221,9 +221,12 @@ export class PreviewManager {
         this._refreshPreviewMesh();
 
         // Adding a rtt to read from
-        this._globalState.previewTexture = new RenderTargetTexture("rtt", 256, this._scene, false, false, Constants.TEXTURETYPE_FLOAT);
+        this._globalState.previewTexture = new RenderTargetTexture("rtt", 256, this._scene, false);
+        this._globalState.pickingTexture = new RenderTargetTexture("rtt2", 256, this._scene, false, true, Constants.TEXTURETYPE_FLOAT);
         this._globalState.previewTexture.renderList = null;
+        this._globalState.pickingTexture.renderList = null;
         this._scene.customRenderTargets.push(this._globalState.previewTexture);
+        this._scene.customRenderTargets.push(this._globalState.pickingTexture);
 
         this._scene.onAfterRenderObservable.add(() => {
             this._globalState.onPreviewSceneAfterRenderObservable.notifyObservers();
@@ -763,6 +766,11 @@ export class PreviewManager {
         if (this._globalState.previewTexture) {
             this._globalState.previewTexture.dispose();
             this._globalState.previewTexture = null;
+        }
+
+        if (this._globalState.pickingTexture) {
+            this._globalState.pickingTexture.dispose();
+            this._globalState.pickingTexture = null;
         }
 
         if (this._material) {
