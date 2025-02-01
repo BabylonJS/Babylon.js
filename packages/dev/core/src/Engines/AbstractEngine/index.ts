4885fdbde746866c756ac993a39bc2be4f8896e9
sebavan <vandenberghe.sebastien@gmail.com>
2025-02-01 01:08:31 +0100

    move timer stats to thinEngine (#16135)

--- packages/dev/core/src/Engines/AbstractEngine/index.ts
+++ packages/dev/core/src/Engines/AbstractEngine/index.ts
@@ -3,6 +3,7 @@ export * from "./abstractEngine.cubeTexture";
 export * from "./abstractEngine.loadingScreen";
 export * from "./abstractEngine.dom";
 export * from "./abstractEngine.states";
+export * from "./abstractEngine.timeQuery";
 export * from "./abstractEngine.query";
 export * from "./abstractEngine.renderPass";
 export * from "./abstractEngine.texture";
