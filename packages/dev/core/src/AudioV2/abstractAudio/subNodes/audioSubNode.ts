7f6eed41012ce2304afe6b3348fae2452e591db9
Andy Fillebrown <afillebrown@microsoft.com>
2025-02-04 13:51:49 -0500

    Change `_AudioSubNode` to a const enum

--- packages/dev/core/src/AudioV2/abstractAudio/subNodes/audioSubNode.ts
+++ packages/dev/core/src/AudioV2/abstractAudio/subNodes/audioSubNode.ts
@@ -1,5 +1,6 @@
-export const _AudioSubNode = {
-    STEREO: "Stereo",
-    SPATIAL: "Spatial",
-    VOLUME: "Volume",
-};
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const enum _AudioSubNode {
+    STEREO = "Stereo",
+    SPATIAL = "Spatial",
+    VOLUME = "Volume",
+}
