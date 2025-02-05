74488a3dade0a32d173223d721c2c8cc44ba39e5
Raanan Weber <raananw+github@gmail.com>
2025-02-05 17:21:53 +0100

    stop value interpolation when setting a variable

--- packages/dev/core/src/FlowGraph/Blocks/Data/flowGraphSetVariableBlock.ts
+++ packages/dev/core/src/FlowGraph/Blocks/Data/flowGraphSetVariableBlock.ts
@@ -36,6 +36,25 @@ export class FlowGraphSetVariableBlock<T> extends FlowGraphExecutionBlockWithOut
     }
 
     public override _execute(context: FlowGraphContext, _callingSignal: FlowGraphSignalConnection): void {
+        // check if there is an animation(group) running on this variable. If there is, stop the animation - a value was force-set.
+        const currentlyRunningAnimationGroups = context._getGlobalContextVariable("currentlyRunningAnimationGroups", []) as number[];
+        for (const animationUniqueId of currentlyRunningAnimationGroups) {
+            const animation = context.assetsContext.animationGroups[animationUniqueId];
+            // check if there is a target animation that has the target set to be the context
+            for (const targetAnimation of animation.targetedAnimations) {
+                if (targetAnimation.target === context) {
+                    // check if the target property is the variable we are setting
+                    if (targetAnimation.target === context) {
+                        // check the variable name
+                        if (targetAnimation.animation.targetProperty === this.config.variable) {
+                            // stop the animation
+                            animation.stop();
+                            // TODO - should it now be removed from the animation groups array?
+                        }
+                    }
+                }
+            }
+        }
         context.setVariable(this.config.variable, this.value.getValue(context));
         this.out._activateSignal(context);
     }
