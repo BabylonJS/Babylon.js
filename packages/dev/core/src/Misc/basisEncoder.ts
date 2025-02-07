750298690dca65808bcb30075bf5d1baf81b596d
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2025-02-07 12:49:55 -0500

    Make more stateful to avoid memory leaks

--- packages/dev/core/src/Misc/basisEncoder.ts
+++ packages/dev/core/src/Misc/basisEncoder.ts
@@ -12,14 +12,8 @@ import { _GetDefaultNumWorkers, _IsWasmConfigurationAvailable } from "./workerUt
 
 declare let BASIS: any; // FUTURE TODO: Create TS declaration file for the Basis Universal API
 
-let _module: Nullable<any> = null;
-let _workerPool: Nullable<WorkerPool> = null;
-
-/**
- * Supported Basis Universal formats for encoding.
- * For best results, use ETC1S with color data and UASTC_LDR_4x4 with non-color data.
- */
-export type BasisFormat = "ETC1S" | "UASTC4x4";
+let _modulePromise: Nullable<Promise<any>> = null;
+let _workerPoolPromise: Nullable<Promise<WorkerPool>> = null;
 
 type IBasisEncoderConfiguration = {
     /**
@@ -37,6 +31,12 @@ type IBasisEncoderConfiguration = {
 };
 
 /**
+ * Supported Basis Universal formats for encoding.
+ * For best results, use ETC1S with color data and UASTC_LDR_4x4 with non-color data.
+ */
+export type BasisFormat = "ETC1S" | "UASTC4x4";
+
+/**
  * Default configuration for the Basis Universal encoder. Defaults to the following:
  * - numWorkers: 50% of the available logical processors, capped to 4. If no logical processors are available, defaults to 1.
  * - wasmUrl: `"https://cdn.babylonjs.com/basis_encoder.js"`
@@ -49,7 +49,43 @@ export const BasisEncoderConfiguration: IBasisEncoderConfiguration = {
 };
 
 /**
- * Encodes non-HDR, non-cube texture data to a KTX v2 image with Basis Universal supercompression.
+ * Initialize resources for the Basis Universal encoder.
+ * @returns a promise that resolves when the Basis Universal encoder resources are initialized
+ */
+export async function InitializeBasisEncoderAsync(): Promise<void> {
+    InitializeBasisEncoder();
+    if (_modulePromise) {
+        await _modulePromise;
+        return;
+    }
+    if (_workerPoolPromise) {
+        await _workerPoolPromise;
+    }
+}
+
+/**
+ * Dispose of resources for the Basis Universal encoder.
+ */
+export function DisposeBasisEncoder(): void {
+    if (_workerPoolPromise) {
+        _workerPoolPromise.then((workerPool) => {
+            workerPool.dispose();
+        });
+    }
+    _workerPoolPromise = null;
+    _modulePromise = null;
+}
+
+/**
+ * Encodes non-HDR, non-cube texture data to a KTX v2 image with Basis Universal supercompression. Example:
+ * ```typescript
+ * InitializeBasisEncoderAsync();
+ * const texture = new Texture("texture.png", scene);
+ * texture.onLoadObservable.addOnce(async () => {
+ *      const ktx2Data = await EncodeTextureToBasisAsync(texture, { basisFormat: "UASTC4x4" });
+ *      DisposeBasisEncoder();
+ * });
+ * ```
  * @param babylonTexture the Babylon texture to encode
  * @param options additional options for encoding
  * - `basisFormat` - the desired encoding format. Defaults to UASTC4x4. It is recommended
@@ -80,6 +116,43 @@ export async function EncodeTextureToBasisAsync(babylonTexture: BaseTexture, opt
 }
 
 async function EncodeDataAsync(slicedSourceImage: Uint8Array, parameters: BasisEncoderParameters): Promise<Uint8Array> {
+    if (_modulePromise) {
+        const module = await _modulePromise;
+        return EncodeImageData(module, slicedSourceImage, parameters);
+    }
+
+    if (_workerPoolPromise) {
+        const workerPool = await _workerPoolPromise;
+        return new Promise<Uint8Array>((resolve, reject) => {
+            workerPool.push((worker, onComplete) => {
+                const onError = (error: ErrorEvent) => {
+                    worker.removeEventListener("error", onError);
+                    worker.removeEventListener("message", onMessage);
+                    reject(error);
+                    onComplete();
+                };
+
+                const onMessage = (msg: MessageEvent) => {
+                    if (msg.data.id === "encodeDone") {
+                        worker.removeEventListener("message", onMessage);
+                        worker.removeEventListener("error", onError);
+                        resolve(msg.data.encodedImageData);
+                        onComplete();
+                    }
+                };
+
+                worker.addEventListener("message", onMessage);
+                worker.addEventListener("error", onError);
+
+                worker.postMessage({ id: "encode", imageData: slicedSourceImage, params: parameters }, [slicedSourceImage.buffer]);
+            });
+        });
+    }
+
+    throw new Error("Basis encoder resources are not initialized.");
+}
+
+function InitializeBasisEncoder(): void {
     const config = BasisEncoderConfiguration;
     if (!_IsWasmConfigurationAvailable(config.wasmUrl, config.wasmBinaryUrl)) {
         throw new Error("Cannot use Basis Encoder configuration. Check configuration and verify environment WebAssembly support.");
@@ -88,40 +161,15 @@ async function EncodeDataAsync(slicedSourceImage: Uint8Array, parameters: BasisE
     // Use main thread if no workers are available
     const workerSupported = typeof Worker === "function" && typeof URL === "function" && typeof URL.createObjectURL === "function";
     if (!workerSupported || BasisEncoderConfiguration.numWorkers === 0) {
-        if (!_module) {
-            _module = await CreateModuleAsync();
+        if (!_modulePromise) {
+            _modulePromise = CreateModuleAsync();
         }
-        return EncodeImageData(_module, slicedSourceImage, parameters);
+        return;
     }
 
-    if (!_workerPool) {
-        _workerPool = await CreateWorkerPoolAsync();
+    if (!_workerPoolPromise) {
+        _workerPoolPromise = CreateWorkerPoolAsync();
     }
-
-    return new Promise<Uint8Array>((resolve, reject) => {
-        _workerPool!.push((worker, onComplete) => {
-            const onError = (error: ErrorEvent) => {
-                worker.removeEventListener("error", onError);
-                worker.removeEventListener("message", onMessage);
-                reject(error);
-                onComplete();
-            };
-
-            const onMessage = (msg: MessageEvent) => {
-                if (msg.data.id === "encodeDone") {
-                    worker.removeEventListener("message", onMessage);
-                    worker.removeEventListener("error", onError);
-                    resolve(msg.data.encodedImageData);
-                    onComplete();
-                }
-            };
-
-            worker.addEventListener("message", onMessage);
-            worker.addEventListener("error", onError);
-
-            worker.postMessage({ id: "encode", imageData: slicedSourceImage, params: parameters }, [slicedSourceImage.buffer]);
-        });
-    });
 }
 
 async function CreateWorkerPoolAsync(): Promise<WorkerPool> {
