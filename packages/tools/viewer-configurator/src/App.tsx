9acedd17743e87937f3b89e5404d0c3f7a9a2568
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-02-28 11:42:14 -0800

    Configurator props required

--- packages/tools/viewer-configurator/src/App.tsx
+++ packages/tools/viewer-configurator/src/App.tsx
@@ -2,28 +2,31 @@ import type { FunctionComponent } from "react";
 import type { ViewerElement } from "viewer/viewerElement";
 
 import * as styles from "./App.module.scss";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { SplitContainer } from "shared-ui-components/split/splitContainer";
 import { ControlledSize, SplitDirection } from "shared-ui-components/split/splitContext";
 import { Splitter } from "shared-ui-components/split/splitter";
 import { Viewer } from "./components/babylonViewer/viewer";
 import { Configurator } from "./components/configurator/configurator";
+import { useEventfulState } from "./hooks/observableHooks";
 
 export const App: FunctionComponent = () => {
     const [viewerElement, setViewerElement] = useState<ViewerElement>();
+    const viewerDetails = useEventfulState(() => viewerElement?.viewerDetails, viewerElement, "viewerready");
+    const viewer = useMemo(() => viewerDetails?.viewer, [viewerDetails]);
 
     return (
         <SplitContainer className={styles["VerticalContainer"]} direction={SplitDirection.Vertical}>
             <div className={styles["HeaderContainer"]}>
-                <img src="https://www.babylonjs.com/Assets/logo-babylonjs-social-twitter.png" />
-                <div>BABYLON VIEWER CONFIGURATOR</div>
+                <img src="https://cdn.jsdelivr.net/gh/BabylonJS/Brand-Toolkit/babylonjs_identity/fullColor/babylonjs_identity_color_dark.svg" />
+                <div>Viewer Configurator</div>
             </div>
             <Splitter size={8} minSize={50} initialSize={50} maxSize={100} controlledSide={ControlledSize.First} />
             <SplitContainer className={styles["HorizontalContainer"]} direction={SplitDirection.Horizontal}>
                 <Viewer onViewerCreated={setViewerElement} />
                 <Splitter size={8} minSize={250} initialSize={300} maxSize={500} controlledSide={ControlledSize.Second} />
                 <div className={styles["ConfiguratorContainer"]}>
-                    <Configurator viewerElement={viewerElement} />
+                    {viewerElement && viewerDetails && viewer && <Configurator viewerElement={viewerElement} viewerDetails={viewerDetails} viewer={viewer} />}
                 </div>
             </SplitContainer>
         </SplitContainer>
--- packages/tools/viewer-configurator/src/components/configurator/configurator.tsx
+++ packages/tools/viewer-configurator/src/components/configurator/configurator.tsx
@@ -1,6 +1,6 @@
-import type { FunctionComponent } from "react";
+import { useMemo, type FunctionComponent } from "react";
 // eslint-disable-next-line import/no-internal-modules
-import type { ViewerElement } from "viewer/index";
+import type { ViewerElement, ViewerDetails, Viewer } from "viewer/index";
 
 import * as styles from "./configurator.module.scss";
 import { LineContainerComponent } from "shared-ui-components/lines/lineContainerComponent";
@@ -9,33 +9,28 @@ import { ButtonLineComponent } from "shared-ui-components/lines/buttonLineCompon
 import { SliderLineComponent } from "shared-ui-components/lines/sliderLineComponent";
 import { CheckBoxLineComponent } from "shared-ui-components/lines/checkBoxLineComponent";
 import { LockObject } from "shared-ui-components/tabs/propertyGrids/lockObject";
-import { useEventfulState } from "../../hooks/observableHooks";
 
-export const Configurator: FunctionComponent<{ viewerElement?: ViewerElement }> = (props) => {
-    const viewerElement = props.viewerElement;
-    const viewerDetails = useEventfulState(() => viewerElement?.viewerDetails, viewerElement, "viewerready");
+export const Configurator: FunctionComponent<{ viewerElement: ViewerElement; viewerDetails: ViewerDetails; viewer: Viewer }> = (props) => {
+    const { viewerElement, viewerDetails, viewer } = props;
+    const lockObject = useMemo(() => new LockObject(), []);
 
     return (
-        <>
-            {viewerElement && viewerDetails && (
-                <div className={styles["ConfiguratorContainer"]}>
-                    <LineContainerComponent title="SECTION 1">
-                        <TextInputLineComponent label="Text Input, single" value="Value" />
-                        <TextInputLineComponent label="Text Input, multiline" multilines={true} value="Value" />
-                        <ButtonLineComponent label="Button" onClick={() => {}} />
-                    </LineContainerComponent>
-                    <LineContainerComponent title="SECTION 2">
-                        <CheckBoxLineComponent
-                            label="Checkbox"
-                            isSelected={() => {
-                                return true;
-                            }}
-                            onSelect={(value: boolean) => {}}
-                        />
-                        <SliderLineComponent label="Slider" minimum={0} maximum={1} step={0.05} decimalCount={0} target={viewerDetails.scene} lockObject={new LockObject()} />
-                    </LineContainerComponent>
-                </div>
-            )}
-        </>
+        <div className={styles["ConfiguratorContainer"]}>
+            <LineContainerComponent title="SECTION 1">
+                <TextInputLineComponent label="Text Input, single" value="Value" />
+                <TextInputLineComponent label="Text Input, multiline" multilines={true} value="Value" />
+                <ButtonLineComponent label="Button" onClick={() => {}} />
+            </LineContainerComponent>
+            <LineContainerComponent title="SECTION 2">
+                <CheckBoxLineComponent
+                    label="Checkbox"
+                    isSelected={() => {
+                        return true;
+                    }}
+                    onSelect={(value: boolean) => {}}
+                />
+                <SliderLineComponent label="Slider" minimum={0} maximum={1} step={0.05} decimalCount={2} target={viewerDetails.scene} lockObject={lockObject} />
+            </LineContainerComponent>
+        </div>
     );
 };
