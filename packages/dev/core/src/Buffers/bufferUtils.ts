edda2f64f1adb762e535276833b4ba78e12eb059
Popov72 <github@evpopov.com>
2025-02-28 14:04:06 +0100

    Fix ts

--- packages/dev/core/src/Buffers/bufferUtils.ts
+++ packages/dev/core/src/Buffers/bufferUtils.ts
@@ -1,6 +1,6 @@
 import { Constants } from "../Engines/constants";
 import { Logger } from "../Misc/logger";
-import type { DataArray, FloatArray, IndicesArray, Nullable, TypedArray, TypedArrayConstructor } from "../types";
+import type { DataArray, FloatArray, IndicesArray, TypedArray, TypedArrayConstructor } from "../types";
 
 /**
  * Union of TypedArrays that can be used for vertex data.
