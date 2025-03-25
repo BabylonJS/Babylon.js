504287e533b4d573264c47cb11612817b5d745d6
Popov72 <github@evpopov.com>
2025-03-25 19:21:59 +0100

    Trigger the CI again

--- packages/dev/core/src/Collisions/pickingInfo.ts
+++ packages/dev/core/src/Collisions/pickingInfo.ts
@@ -125,6 +125,7 @@ export class PickingInfo {
         const transformNormalToWorld = (pickedMesh: AbstractMesh, n: Vector3) => {
             if (this.thinInstanceIndex !== -1) {
                 const tm = (pickedMesh as Mesh).thinInstanceGetWorldMatrices()[this.thinInstanceIndex];
+
                 if (tm) {
                     Vector3.TransformNormalToRef(n, tm, n);
                 }
