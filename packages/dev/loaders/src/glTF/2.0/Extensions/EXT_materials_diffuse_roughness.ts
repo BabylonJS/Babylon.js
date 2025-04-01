fd4fba9aca2d0ddd560621be83ee74dbd719cfac
Mike Bond <mdcbond@gmail.com>
2025-04-01 09:30:41 -0700

    Add EXT_materials_diffuse_roughness loader

---
+++ packages/dev/loaders/src/glTF/2.0/Extensions/EXT_materials_diffuse_roughness.ts
@@ -0,0 +1,102 @@
+import type { Nullable } from "core/types";
+import { PBRMaterial } from "core/Materials/PBR/pbrMaterial";
+import type { Material } from "core/Materials/material";
+
+import type { IMaterial } from "../glTFLoaderInterfaces";
+import type { IGLTFLoaderExtension } from "../glTFLoaderExtension";
+import { GLTFLoader } from "../glTFLoader";
+import type { IKHRMaterialsDiffuseRoughness } from "babylonjs-gltf2interface";
+import { registerGLTFExtension, unregisterGLTFExtension } from "../glTFLoaderExtensionRegistry";
+import { Constants } from "core/Engines";
+
+const NAME = "EXT_materials_diffuse_roughness";
+
+declare module "../../glTFFileLoader" {
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    export interface GLTFLoaderExtensionOptions {
+        /**
+         * Defines options for the EXT_materials_diffuse_roughness extension.
+         */
+        // NOTE: Don't use NAME here as it will break the UMD type declarations.
+        ["EXT_materials_diffuse_roughness"]: {};
+    }
+}
+
+/**
+ * [Specification](https://github.com/KhronosGroup/glTF/blob/main/extensions/2.0/Khronos/EXT_materials_diffuse_roughness/README.md)
+ * [Playground Sample](https://www.babylonjs-playground.com/frame.html#7F7PN6#8)
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export class EXT_materials_diffuse_roughness implements IGLTFLoaderExtension {
+    /**
+     * The name of this extension.
+     */
+    public readonly name = NAME;
+
+    /**
+     * Defines whether this extension is enabled.
+     */
+    public enabled: boolean;
+
+    /**
+     * Defines a number that determines the order the extensions are applied.
+     */
+    public order = 190;
+
+    private _loader: GLTFLoader;
+
+    /**
+     * @internal
+     */
+    constructor(loader: GLTFLoader) {
+        this._loader = loader;
+        this.enabled = this._loader.isExtensionUsed(NAME);
+    }
+
+    /** @internal */
+    public dispose() {
+        (this._loader as any) = null;
+    }
+
+    /**
+     * @internal
+     */
+    public loadMaterialPropertiesAsync(context: string, material: IMaterial, babylonMaterial: Material): Nullable<Promise<void>> {
+        return GLTFLoader.LoadExtensionAsync<IKHRMaterialsDiffuseRoughness>(context, material, this.name, (extensionContext, extension) => {
+            const promises = new Array<Promise<any>>();
+            promises.push(this._loader.loadMaterialPropertiesAsync(context, material, babylonMaterial));
+            promises.push(this._loadDiffuseRoughnessPropertiesAsync(extensionContext, extension, babylonMaterial));
+            return Promise.all(promises).then(() => {});
+        });
+    }
+
+    private _loadDiffuseRoughnessPropertiesAsync(context: string, properties: IKHRMaterialsDiffuseRoughness, babylonMaterial: Material): Promise<void> {
+        if (!(babylonMaterial instanceof PBRMaterial)) {
+            throw new Error(`${context}: Material type not supported`);
+        }
+
+        const promises = new Array<Promise<any>>();
+
+        babylonMaterial.baseDiffuseRoughnessModel = Constants.MATERIAL_DIFFUSE_ROUGHNESS_OPENPBR;
+
+        if (properties.diffuseRoughnessFactor != undefined) {
+            babylonMaterial.baseDiffuseRoughness = properties.diffuseRoughnessFactor;
+        } else {
+            babylonMaterial.baseDiffuseRoughness = 0;
+        }
+
+        if (properties.diffuseRoughnessTexture) {
+            promises.push(
+                this._loader.loadTextureInfoAsync(`${context}/diffuseRoughnessTexture`, properties.diffuseRoughnessTexture, (texture) => {
+                    texture.name = `${babylonMaterial.name} (Diffuse Roughness)`;
+                    babylonMaterial.baseDiffuseRoughnessTexture = texture;
+                })
+            );
+        }
+
+        return Promise.all(promises).then(() => {});
+    }
+}
+
+unregisterGLTFExtension(NAME);
+registerGLTFExtension(NAME, true, (loader) => new EXT_materials_diffuse_roughness(loader));
