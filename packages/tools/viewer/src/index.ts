57525abf48bfb7fc9a620138df8575f3edbe9c3f
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-04-01 20:58:14 -0700

    Fix type export

--- packages/tools/viewer/src/index.ts
+++ packages/tools/viewer/src/index.ts
@@ -1,8 +1,19 @@
-export type { CameraAutoOrbit, EnvironmentOptions, LoadModelOptions, Model, PostProcessing, ToneMapping, ViewerDetails, ViewerHotSpotQuery, ViewerOptions } from "./viewer";
+export type {
+    CameraAutoOrbit,
+    EnvironmentOptions,
+    HotSpot,
+    LoadModelOptions,
+    Model,
+    PostProcessing,
+    ToneMapping,
+    ViewerDetails,
+    ViewerHotSpotQuery,
+    ViewerOptions,
+} from "./viewer";
 export type { CanvasViewerOptions } from "./viewerFactory";
 export type { ViewerElementEventMap } from "./viewerElement";
 
-export { CreateHotSpotFromCamera, DefaultViewerOptions, HotSpot, Viewer, ViewerHotSpotResult } from "./viewer";
+export { CreateHotSpotFromCamera, DefaultViewerOptions, Viewer, ViewerHotSpotResult } from "./viewer";
 export { HTML3DElement, ViewerElement } from "./viewerElement";
 export { CreateViewerForCanvas as createViewerForCanvas } from "./viewerFactory";
 export { HTML3DAnnotationElement } from "./viewerAnnotationElement";
