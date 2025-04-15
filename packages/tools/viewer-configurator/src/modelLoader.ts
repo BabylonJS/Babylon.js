53e89b01f655b3c76d7836aaa0ffa667f691c4b8
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-04-15 11:37:56 -0700

    Some small fixes

--- packages/tools/viewer-configurator/src/modelLoader.ts
+++ packages/tools/viewer-configurator/src/modelLoader.ts
@@ -44,9 +44,7 @@ async function pickFile(): Promise<Nullable<Iterable<File>>> {
 
 export async function LoadModel(viewerElement: ViewerElement, source: string | File | Iterable<File> | ArrayBufferView, options?: LoadModelOptions, abortSignal?: AbortSignal) {
     try {
-        const defaultOptions: LoadModelOptions = {
-            animationAutoPlay: viewerElement.animationAutoPlay,
-        };
+        const defaultOptions: LoadModelOptions = {};
 
         if (typeof source === "object" && !ArrayBuffer.isView(source) && Symbol.iterator in source) {
             const registeredSceneLoaderPlugins = GetRegisteredSceneLoaderPluginMetadata();
