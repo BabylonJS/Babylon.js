11a6e338d2b466b0db2cfbd4ca6aed8a832c4844
MiiBond <MiiBond@users.noreply.github.com>
2025-04-17 10:02:46 -0700

    Update packages/dev/core/src/Materials/PBR/pbrBaseMaterial.ts
    Co-authored-by: Popov72 <github@evpopov.com>

--- packages/dev/core/src/Materials/PBR/pbrBaseMaterial.ts
+++ packages/dev/core/src/Materials/PBR/pbrBaseMaterial.ts
@@ -2404,7 +2404,7 @@ export abstract class PBRBaseMaterial extends PushMaterial {
                 }
 
                 ubo.updateFloat("baseWeight", this._baseWeight);
-                ubo.updateFloat("baseDiffuseRoughness", this._baseDiffuseRoughness ?? 0.0);
+                ubo.updateFloat("baseDiffuseRoughness", this._baseDiffuseRoughness || 0.0);
 
                 // Misc
                 this._lightingInfos.x = this._directIntensity;
