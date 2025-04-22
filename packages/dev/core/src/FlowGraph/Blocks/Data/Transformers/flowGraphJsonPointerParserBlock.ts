ba5539af8d5774c17f9ad9c9adaa1d0c9500a71a
David Catuhe <david@catuhe.com>
2025-04-22 14:17:23 -0700

    Fix build

--- packages/dev/core/src/FlowGraph/Blocks/Data/Transformers/flowGraphJsonPointerParserBlock.ts
+++ packages/dev/core/src/FlowGraph/Blocks/Data/Transformers/flowGraphJsonPointerParserBlock.ts
@@ -132,29 +132,28 @@ export class FlowGraphJsonPointerParserBlock<P extends any, O extends FlowGraphA
                     };
                 });
             }
-            if (accessorContainer.info.interpolation) {
-                for (const [index, info] of accessorContainer.info.interpolation.entries()) {
-                    const name = accessorContainer.info.getPropertyName?.[index](accessorContainer.object) || "Animation-interpolation-" + index;
-                    // generate the keys based on interpolation info
-                    let newKeys: any[] = keys;
-                    if (animationType !== info.type) {
-                        // convert the keys to the right type
-                        newKeys = keys.map((key) => {
-                            return {
-                                frame: key.frame,
-                                value: info.getValue(undefined, key.value.asArray ? key.value.asArray() : [key.value], 0, 1),
-                            };
-                        });
-                    }
-                    const animationData = info.buildAnimations(accessorContainer.object, name, 60, newKeys);
-                    for (const animation of animationData) {
-                        if (easingFunction) {
-                            animation.babylonAnimation.setEasingFunction(easingFunction);
-                        }
-                        animations.push(animation.babylonAnimation);
-                    }
+            // eslint-disable-next-line github/array-foreach
+            accessorContainer.info.interpolation?.forEach((info, index) => {
+                const name = accessorContainer.info.getPropertyName?.[index](accessorContainer.object) || "Animation-interpolation-" + index;
+                // generate the keys based on interpolation info
+                let newKeys: any[] = keys;
+                if (animationType !== info.type) {
+                    // convert the keys to the right type
+                    newKeys = keys.map((key) => {
+                        return {
+                            frame: key.frame,
+                            value: info.getValue(undefined, key.value.asArray ? key.value.asArray() : [key.value], 0, 1),
+                        };
+                    });
                 }
-            }
+                const animationData = info.buildAnimations(accessorContainer.object, name, 60, newKeys);
+                for (const animation of animationData) {
+                    if (easingFunction) {
+                        animation.babylonAnimation.setEasingFunction(easingFunction);
+                    }
+                    animations.push(animation.babylonAnimation);
+                }
+            });
 
             return animations;
         };
