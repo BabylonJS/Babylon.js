e23c44d0d7cf5e1a2541b1821286ed47cb248203
David Catuhe <david@catuhe.com>
2025-04-28 16:00:26 -0700

    Adding attractors for cpu particle system

---
+++ packages/dev/core/src/Particles/attractor.ts
@@ -0,0 +1,33 @@
+import { Vector3 } from "core/Maths/math.vector";
+import type { Particle } from "./particle";
+import type { ThinParticleSystem } from "./thinParticleSystem";
+
+const toAttractor: Vector3 = Vector3.Zero();
+const force: Vector3 = Vector3.Zero();
+const scaledForce: Vector3 = Vector3.Zero();
+
+/**
+ * Class representing an attractor in a particle system.
+ */
+export class Attractor {
+    /**
+     * Gets or sets the strength of the attractor.
+     * A positive value attracts particles, while a negative value repels them.
+     */
+    public strength = 0.0;
+
+    /**
+     * Gets or sets the position of the attractor in 3D space.
+     */
+    public position = Vector3.Zero();
+
+    /** @internal */
+    public _processParticle(particle: Particle, system: ThinParticleSystem) {
+        this.position.subtractToRef(particle.position, toAttractor);
+        const distanceSquared = toAttractor.lengthSquared() + 0.0001; // Avoid division by zero
+        toAttractor.normalize().scaleToRef(this.strength / distanceSquared, force);
+        force.scaleToRef(system._tempScaledUpdateSpeed, scaledForce);
+
+        particle.direction.addInPlace(scaledForce); // Update particle velocity
+    }
+}
