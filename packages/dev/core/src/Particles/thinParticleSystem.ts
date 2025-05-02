131c97eddfdbc6576c96a65f444caef98add64e4
David Catuhe <david@catuhe.com>
2025-05-02 11:17:44 -0700

    Fix webgpu

--- packages/dev/core/src/Particles/thinParticleSystem.ts
+++ packages/dev/core/src/Particles/thinParticleSystem.ts
@@ -542,22 +542,6 @@ export class ThinParticleSystem extends BaseParticleSystem implements IDisposabl
 
         this._initShaderSourceAsync();
 
-        // Setup the default processing configuration to the scene.
-        this._attachImageProcessingConfiguration(null);
-
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        this._customWrappers = { 0: new DrawWrapper(this._engine) };
-        this._customWrappers[0]!.effect = customEffect;
-
-        this._drawWrappers = [];
-        this._useInstancing = this._engine.getCaps().instancedArrays;
-
-        this._createIndexBuffer();
-        this._createVertexBuffers();
-
-        // Default emitter type
-        this.particleEmitterType = new BoxParticleEmitter();
-
         // Creation queue
         this._lifeTimeCreation = {
             process: _CreateLifetimeData,
@@ -648,6 +632,22 @@ export class ThinParticleSystem extends BaseParticleSystem implements IDisposabl
         this._updateQueueStart = this._colorProcessing;
 
         this._isAnimationSheetEnabled = isAnimationSheetEnabled;
+
+        // Setup the default processing configuration to the scene.
+        this._attachImageProcessingConfiguration(null);
+
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        this._customWrappers = { 0: new DrawWrapper(this._engine) };
+        this._customWrappers[0]!.effect = customEffect;
+
+        this._drawWrappers = [];
+        this._useInstancing = this._engine.getCaps().instancedArrays;
+
+        this._createIndexBuffer();
+        this._createVertexBuffers();
+
+        // Default emitter type
+        this.particleEmitterType = new BoxParticleEmitter();
 
         // Update
         this.updateFunction = (particles: Particle[]): void => {
