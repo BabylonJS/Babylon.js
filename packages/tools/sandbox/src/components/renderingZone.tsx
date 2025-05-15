a0f9b0107584dc7e440ed11f175a39f0087c4031
David Catuhe <david.catuhe@live.fr>
2025-05-15 11:27:30 -0700

    Remove no AA for 3dCommerce (#16610)
    Co-authored-by: David Catuhe <david@catuhe.com>

--- packages/tools/sandbox/src/components/renderingZone.tsx
+++ packages/tools/sandbox/src/components/renderingZone.tsx
@@ -69,7 +69,7 @@ export class RenderingZone extends React.Component<IRenderingZoneProps> {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     async initEngine() {
         const useWebGPU = location.href.indexOf("webgpu") !== -1 && !!(navigator as any).gpu;
-        const antialias = !this.props.globalState.commerceMode;
+        const antialias = true;
 
         this._canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
         if (useWebGPU) {
