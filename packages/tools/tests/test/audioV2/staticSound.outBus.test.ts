9870b7b3de95ef83c32ae7b4827a2ada27f66adc
Andy Fillebrown <andy@docEdub.com>
2025-05-22 16:36:36 -0400

    Add tests for audio source node `defaultToMainBus` option

---
+++ packages/tools/tests/test/audioV2/staticSound.outBus.test.ts
@@ -0,0 +1,5 @@
+import { AddSharedAbstractSoundSourceOutBusTests } from "./shared/abstractSoundSource.outBus";
+import { InitAudioV2Tests } from "./utils/audioV2.utils";
+
+InitAudioV2Tests();
+AddSharedAbstractSoundSourceOutBusTests("StaticSound");
