e086c477daf472905ba64ad769ea9da1c65a05bc
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-05-30 15:50:07 -0700

    Remove BooleanProperty (bound component) for now

--- packages/dev/inspector-v2/src/services/panes/properties/mesh/meshGeneralProperties.tsx
+++ packages/dev/inspector-v2/src/services/panes/properties/mesh/meshGeneralProperties.tsx
@@ -3,17 +3,10 @@ import type { AbstractMesh } from "core/index";
 
 import type { FunctionComponent } from "react";
 
-import { BooleanProperty } from "../../../../components/booleanProperty";
-
 export const MeshGeneralProperties: FunctionComponent<{ entity: AbstractMesh }> = ({ entity: mesh }) => {
     return (
-        <BooleanProperty
-            key="MeshIsEnabled"
-            label="Is enabled"
-            description="Determines whether a mesh is enabled within the scene"
-            accessor={() => mesh.isEnabled(false)}
-            mutator={(value) => mesh.setEnabled(value)}
-            observable={mesh.onEnabledStateChangedObservable}
-        />
+        <>
+            <div key="MeshIsEnabled">Is enabled: {mesh.isEnabled(false).toString()}</div>
+        </>
     );
 };
