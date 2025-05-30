f206d5d37795a1c0ac692b0f8df9ae1899f2e3a0
MiiBond <MiiBond@users.noreply.github.com>
2025-05-30 08:19:08 -0700

    Fix compile error for WebGPU and animated meshes (#16687)
    Animated meshes with WebGPU previously caused a shader compile error in
    the GeometryBufferRenderer. This PR fixes the issue.

--- packages/tools/viewer/src/viewer.ts
+++ packages/tools/viewer/src/viewer.ts
@@ -1551,16 +1551,8 @@ export class Viewer implements IDisposable {
                 if (this._shadowQuality === "normal") {
                     await this._updateShadowMap(abortController.signal);
                 } else if (this._shadowQuality === "high") {
-                    const isWebGPU = this._scene.getEngine().isWebGPU;
-                    // there is some issue with meshes with indices, so disable environment shadows for now
-                    const hasAnyAnimationOrIndices = this._loadedModelsBacking.some(
-                        (model) => model.assetContainer.animationGroups.length > 0 && model.assetContainer.meshes.some((mesh) => mesh.getIndices() !== null)
-                    );
-
-                    if (this._loadedModelsBacking.length > 0 && !(isWebGPU && hasAnyAnimationOrIndices)) {
+                    if (this._loadedModelsBacking.length > 0) {
                         await this._updateEnvShadow(abortController.signal);
-                    } else {
-                        this._log("Environment shadows are not supported in WebGPU with animated meshes.");
                     }
                 }
             }
