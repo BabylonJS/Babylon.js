fd1c395daa1152aede492fb0cd1e2e37e19718e1
Andy Fillebrown <andy@docEdub.com>
2025-06-03 00:30:02 -0400

    Refactor common bus and sound code into `AbstractAudioOutNode` base class

---
+++ packages/dev/core/src/AudioV2/abstractAudio/abstractAudioOutNode.ts
@@ -0,0 +1,62 @@
+import type { Nullable } from "../../types";
+import type { AudioNodeType } from "./abstractAudioNode";
+import { AbstractNamedAudioNode } from "./abstractAudioNode";
+import type { AudioEngineV2 } from "./audioEngineV2";
+import type { _AbstractAudioSubGraph } from "./subNodes/abstractAudioSubGraph";
+import type { IVolumeAudioOptions } from "./subNodes/volumeAudioSubNode";
+import { _GetVolumeAudioProperty, _GetVolumeAudioSubNode } from "./subNodes/volumeAudioSubNode";
+import type { AbstractAudioAnalyzer, IAudioAnalyzerOptions } from "./subProperties/abstractAudioAnalyzer";
+import { _AudioAnalyzer } from "./subProperties/audioAnalyzer";
+
+/** @internal */
+export interface IAbstractAudioOutNodeOptions extends IAudioAnalyzerOptions, IVolumeAudioOptions {}
+
+/**
+ * Abstract class representing and audio output node with volume control.
+ */
+export abstract class AbstractAudioOutNode extends AbstractNamedAudioNode {
+    private _analyzer: Nullable<AbstractAudioAnalyzer> = null;
+
+    protected abstract _subGraph: _AbstractAudioSubGraph;
+
+    protected constructor(name: string, engine: AudioEngineV2, nodeType: AudioNodeType) {
+        super(name, engine, nodeType);
+    }
+
+    /**
+     * The analyzer features of the bus.
+     */
+    public get analyzer(): AbstractAudioAnalyzer {
+        return this._analyzer ?? (this._analyzer = new _AudioAnalyzer(this._subGraph));
+    }
+
+    /**
+     * The audio output volume.
+     */
+
+    public get volume(): number {
+        return _GetVolumeAudioProperty(this._subGraph, "volume");
+    }
+
+    public set volume(value: number) {
+        // The volume subnode is created on initialization and should always exist.
+        const node = _GetVolumeAudioSubNode(this._subGraph);
+        if (!node) {
+            throw new Error("No volume subnode");
+        }
+
+        node.volume = value;
+    }
+
+    /**
+     * Releases associated resources.
+     */
+    public override dispose(): void {
+        super.dispose();
+
+        this._analyzer?.dispose();
+        this._analyzer = null;
+
+        this._subGraph.dispose();
+    }
+}
--- packages/dev/core/src/AudioV2/abstractAudio/abstractSoundSource.ts
+++ packages/dev/core/src/AudioV2/abstractAudio/abstractSoundSource.ts
@@ -1,18 +1,15 @@
 import type { Nullable } from "../../types";
-import { AbstractNamedAudioNode, AudioNodeType } from "./abstractAudioNode";
+import { AudioNodeType } from "./abstractAudioNode";
+import { AbstractAudioOutNode, type IAbstractAudioOutNodeOptions } from "./abstractAudioOutNode";
 import type { PrimaryAudioBus } from "./audioBus";
 import type { AudioEngineV2 } from "./audioEngineV2";
-import type { _AbstractAudioSubGraph } from "./subNodes/abstractAudioSubGraph";
-import { _GetVolumeAudioProperty, _GetVolumeAudioSubNode } from "./subNodes/volumeAudioSubNode";
-import type { AbstractAudioAnalyzer, IAudioAnalyzerOptions } from "./subProperties/abstractAudioAnalyzer";
 import type { AbstractSpatialAudio, ISpatialAudioOptions } from "./subProperties/abstractSpatialAudio";
 import type { AbstractStereoAudio, IStereoAudioOptions } from "./subProperties/abstractStereoAudio";
-import { _AudioAnalyzer } from "./subProperties/audioAnalyzer";
 
 /**
  * Options for creating a sound source.
  */
-export interface ISoundSourceOptions extends IAudioAnalyzerOptions, ISpatialAudioOptions, IStereoAudioOptions {
+export interface ISoundSourceOptions extends IAbstractAudioOutNodeOptions, ISpatialAudioOptions, IStereoAudioOptions {
     /**
      * The output bus for the sound source. Defaults to `null`.
      * - If not set or `null`, and `outBusAutoDefault` is `true`, then the sound source is automatically connected to the audio engine's default main bus.
@@ -29,21 +26,11 @@ export interface ISoundSourceOptions extends IAudioAnalyzerOptions, ISpatialAudi
 /**
  * Abstract class representing a sound in the audio engine.
  */
-export abstract class AbstractSoundSource extends AbstractNamedAudioNode {
-    private _analyzer: Nullable<AbstractAudioAnalyzer> = null;
+export abstract class AbstractSoundSource extends AbstractAudioOutNode {
     private _outBus: Nullable<PrimaryAudioBus> = null;
-
-    protected abstract _subGraph: _AbstractAudioSubGraph;
 
     protected constructor(name: string, engine: AudioEngineV2, nodeType: AudioNodeType = AudioNodeType.HAS_OUTPUTS) {
         super(name, engine, nodeType);
-    }
-
-    /**
-     * The analyzer features of the sound.
-     */
-    public get analyzer(): AbstractAudioAnalyzer {
-        return this._analyzer ?? (this._analyzer = new _AudioAnalyzer(this._subGraph));
     }
 
     /**
@@ -87,30 +74,10 @@ export abstract class AbstractSoundSource extends AbstractNamedAudioNode {
     public abstract stereo: AbstractStereoAudio;
 
     /**
-     * The output volume of the sound.
-     */
-    public get volume(): number {
-        return _GetVolumeAudioProperty(this._subGraph, "volume");
-    }
-
-    public set volume(value: number) {
-        // The volume subnode is created on initialization and should always exist.
-        const node = _GetVolumeAudioSubNode(this._subGraph);
-        if (!node) {
-            throw new Error("No volume subnode");
-        }
-
-        node.volume = value;
-    }
-
-    /**
      * Releases associated resources.
      */
     public override dispose(): void {
         super.dispose();
-
-        this._analyzer?.dispose();
-        this._analyzer = null;
 
         this._outBus = null;
     }
