aff191611a0a1063dc03cb90e078a44b730f9413
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-06-05 16:46:29 -0700

    Now updating version.ts

--- packages/dev/smartFilters/src/version.ts
+++ packages/dev/smartFilters/src/version.ts
@@ -1,4 +1,4 @@
 /**
- * The version of the SmartFilter core. During publish, this file is overwritten by versionUp.ts with the same version that is used for the NPM publish.
+ * The version of the SmartFilter core. During publish, this file is overwritten by recordVersionNumber.ts with the same version that is used for the NPM publish.
  */
 export const SmartFilterCoreVersion = "locally built";
