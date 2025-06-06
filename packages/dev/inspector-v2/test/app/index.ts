40c617d80340d149ecf91a2e8394173d9984c5d3
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-06-06 09:39:08 -0700

    Add drag & drop

--- packages/dev/inspector-v2/test/app/index.ts
+++ packages/dev/inspector-v2/test/app/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-internal-modules
-import type { ArcRotateCamera } from "core/index";
+import type { ArcRotateCamera, Nullable } from "core/index";
 
 import { Engine } from "core/Engines/engine";
 import { LoadAssetContainerAsync } from "core/Loading/sceneLoader";
@@ -22,15 +22,45 @@ const engine = new Engine(canvas, true, {
 
 const scene = new Scene(engine);
 
-(async () => {
-    const assetContainer = await LoadAssetContainerAsync("https://assets.babylonjs.com/meshes/alien.glb", scene);
-    assetContainer.addAllToScene();
+let camera: Nullable<ArcRotateCamera> = null;
+
+function createCamera() {
+    camera?.dispose();
     scene.createDefaultCameraOrLight(true, true, true);
-    const camera = scene.activeCamera as ArcRotateCamera;
+    camera = scene.activeCamera as ArcRotateCamera;
     camera.alpha = Math.PI / 2;
+}
+
+(async () => {
+    let assetContainer = await LoadAssetContainerAsync("https://assets.babylonjs.com/meshes/alien.glb", scene);
+    assetContainer.addAllToScene();
+    createCamera();
 
     engine.runRenderLoop(() => {
         scene.render();
+    });
+
+    canvas.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+
+    let isDropping = false;
+    canvas.addEventListener("drop", async (event) => {
+        if (!isDropping) {
+            const file = event.dataTransfer?.files[0];
+            if (file) {
+                event.preventDefault();
+                isDropping = true;
+                try {
+                    assetContainer.removeAllFromScene();
+                    assetContainer = await LoadAssetContainerAsync(file, scene);
+                    assetContainer.addAllToScene();
+                    createCamera();
+                } finally {
+                    isDropping = false;
+                }
+            }
+        }
     });
 })();
 
