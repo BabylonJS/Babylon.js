8be2964b07445f436757dadf6e02a7f077b0247d
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-06-05 17:09:24 -0700

    Use commonDevWebpackConfiguration

--- packages/dev/inspector-v2/webpack.config.js
+++ packages/dev/inspector-v2/webpack.config.js
@@ -1,46 +1,49 @@
-const path = require("path");
+const webpackTools = require("@dev/build-tools").webpackTools;
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-module.exports = {
-    entry: "./test/app/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-    },
-    module: {
-        rules: [
+module.exports = (env) => {
+    return {
+        entry: "./test/app/index.tsx",
+
+        ...webpackTools.commonDevWebpackConfiguration(
             {
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            targets: "defaults",
-                            presets: [["@babel/preset-env"], ["@babel/preset-react"], ["@babel/preset-typescript"]],
-                        },
-                    },
-                ],
-                exclude: /node_modules/,
+                ...env,
+                outputFilename: "bundle.js",
+                dirName: __dirname,
+                enableHotReload: true,
             },
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean),
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "public"),
+                static: ["public"],
+                port: process.env.INSPECTOR_TEST_PORT || 9001,
+            }
+        ),
+
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
         },
-        hot: true,
-        compress: true,
-        port: 9001,
-        historyApiFallback: true,
-    },
-    mode: "development",
+
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                targets: "defaults",
+                                presets: [["@babel/preset-env"], ["@babel/preset-react"], ["@babel/preset-typescript"]],
+                            },
+                        },
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+
+        plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean),
+    };
 };
