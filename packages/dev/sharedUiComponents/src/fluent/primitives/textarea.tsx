6de41da771012e6b4a31ed657961e195fcd76e58
Georgina <gehalper@microsoft.com>
2025-06-09 15:40:12 -0400

    update textarea import to include keyboard/change event

--- packages/dev/sharedUiComponents/src/fluent/primitives/textarea.tsx
+++ packages/dev/sharedUiComponents/src/fluent/primitives/textarea.tsx
@@ -1,6 +1,6 @@
 import type { TextareaProps as FluentTextareaProps } from "@fluentui/react-components";
 import { Textarea as FluentTextarea, makeStyles } from "@fluentui/react-components";
-import type { FunctionComponent } from "react";
+import type { FunctionComponent, KeyboardEvent, ChangeEvent } from "react";
 
 const useInputStyles = makeStyles({
     textarea: {},
@@ -13,14 +13,14 @@ const useInputStyles = makeStyles({
  */
 export const Textarea: FunctionComponent<FluentTextareaProps> = (props) => {
     const classes = useInputStyles();
-    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>, data: any) => {
+    const handleChange = (event: ChangeEvent<HTMLTextAreaElement>, data: any) => {
         event.stopPropagation(); // Prevent event propagation
         if (props.onChange) {
             props.onChange(event, data); // Call the original onChange handler passed as prop
         }
     };
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
         event.stopPropagation(); // Prevent event propagation
         if (props.onKeyDown) {
             props.onKeyDown(event); // Call the original onKeyDown handler passed as prop
