6d0bdeabe6e787951a72d5027ddbbedfb1605f27
Raanan Weber <raananw+github@gmail.com>
2025-06-11 21:20:56 +0200

    fix build

--- packages/dev/core/src/Particles/Node/Blocks/Triggers/triggerTools.ts
+++ packages/dev/core/src/Particles/Node/Blocks/Triggers/triggerTools.ts
@@ -2,12 +2,13 @@ import type { Scene } from "core/scene";
 import type { Vector3 } from "core/Maths/math.vector";
 import type { SystemBlock } from "../systemBlock";
 import { NodeParticleBuildState } from "../../nodeParticleBuildState";
+import type { ParticleSystem } from "core/Particles/particleSystem";
 
 /**
  * @internal
  * Tools for managing particle triggers and sub-emitter systems.
  */
-export function _TriggerSubEmitter(template: SystemBlock, scene: Scene, location: Vector3) {
+export function _TriggerSubEmitter(template: SystemBlock, scene: Scene, location: Vector3): ParticleSystem {
     const newState = new NodeParticleBuildState();
     newState.scene = scene;
     const clone = template.createSystem(newState);
