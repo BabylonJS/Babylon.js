5552c0dc25a084a9155c934167211ed0378ce43c
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-06-12 10:51:58 -0700

    Inspector v2: Switch some Observable to IReadonlyObservable (#16742)
    This PR just switches some contracts to expose `IReadonlyObservable`
    instead of `Observable` (for API clarity, and also to simplify some code
    since `IReadonlyObservable<T>` is covariant).
    Also small change to `IReadonlyObservable<T>` to make `T = unknown` by
    default (so for cases where you don't care about the payload, you can
    just use `IReadonlyObservable` without any type params).

--- packages/dev/inspector-v2/src/modularity/serviceDefinition.ts
+++ packages/dev/inspector-v2/src/modularity/serviceDefinition.ts
@@ -22,12 +22,16 @@ export interface IService<ContractIdentity extends symbol> {
     readonly [Contract]?: ContractIdentity;
 }
 
+// This utility type extracts the service identity (unique symbol type) from a service contract. That is, it extracts T from IService<T>.
 type ExtractContractIdentity<ServiceContract extends IService<symbol>> = ServiceContract extends IService<infer ContractIdentity> ? ContractIdentity : never;
 
+// This utility type extracts the contract identities from an array of service contracts. That is, it extracts [T1, T2, ...] from [IService<T1>, IService<T2>, ...].
 type ExtractContractIdentities<ServiceContracts extends IService<symbol>[]> = {
     [Index in keyof ServiceContracts]: ExtractContractIdentity<ServiceContracts[Index]>;
 };
 
+// This utility type converts a union of types into an intersection of types. That is, it converts T1 | T2 | ... into T1 & T2 & ...
+// This is specifically used to determine the type that a service factory function returns when it produces multiple services.
 type UnionToIntersection<Union> = (Union extends any ? (k: Union) => void : never) extends (k: infer Intersection) => void ? Intersection : never;
 
 type MaybePromise<T> = T | Promise<T>;
@@ -35,7 +39,8 @@ type MaybePromise<T> = T | Promise<T>;
 /**
  * A factory function responsible for creating a service instance.
  * Consumed services are passed as arguments to the factory function.
- * The returned value must implement all produced services.
+ * The returned value must implement all produced services, and may IDisposable.
+ * If not services are produced, the returned value may implement IDisposable, otherwise it may return void.
  */
 export type ServiceFactory<Produces extends IService<symbol>[], Consumes extends IService<symbol>[]> = (
     ...dependencies: [...Consumes, abortSignal?: AbortSignal]
