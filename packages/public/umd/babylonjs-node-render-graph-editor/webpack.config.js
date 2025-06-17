d7d46fe95758391055ef8e95ac9301b2e3fd37fd
Georgina <gehalper@microsoft.com>
2025-06-17 13:30:18 -0400

    remove dev step from tooling build, copy minified file to a max.js file for back compat

--- packages/public/umd/babylonjs-node-render-graph-editor/webpack.config.js
+++ packages/public/umd/babylonjs-node-render-graph-editor/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = (env) => {
         devPackageAliasPath: `../../../tools/nodeRenderGraphEditor/dist`,
         namespace: "NODERENDERGRAPHEDITOR",
         maxMode: true,
+        minToMax: true,
         outputPath: path.resolve(__dirname),
         alias: {
             "shared-ui-components": path.resolve("../../../dev/sharedUiComponents/dist"),
