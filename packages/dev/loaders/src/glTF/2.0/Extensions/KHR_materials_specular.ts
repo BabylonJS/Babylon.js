8053c2dc47da9a717a88c426c35f872383b0d869
Mike Bond <mdcbond@gmail.com>
2025-06-19 13:20:41 -0700

    Added PBR2 material and option to load using glTF

--- packages/dev/loaders/src/glTF/2.0/Extensions/KHR_materials_specular.ts
+++ packages/dev/loaders/src/glTF/2.0/Extensions/KHR_materials_specular.ts
@@ -1,5 +1,6 @@
 import type { Nullable } from "core/types";
 import { PBRMaterial } from "core/Materials/PBR/pbrMaterial";
+import { PBRMaterial2 } from "core/Materials/PBR/pbrMaterial2";
 import type { Material } from "core/Materials/material";
 
 import type { IMaterial, ITextureInfo } from "../glTFLoaderInterfaces";
@@ -83,7 +84,7 @@ export class KHR_materials_specular implements IGLTFLoaderExtension {
 
     // eslint-disable-next-line @typescript-eslint/promise-function-async, no-restricted-syntax
     private _loadSpecularPropertiesAsync(context: string, properties: IKHRMaterialsSpecular, babylonMaterial: Material): Promise<void> {
-        if (!(babylonMaterial instanceof PBRMaterial)) {
+        if (!(babylonMaterial instanceof PBRMaterial) && !(babylonMaterial instanceof PBRMaterial2)) {
             throw new Error(`${context}: Material type not supported`);
         }
 
