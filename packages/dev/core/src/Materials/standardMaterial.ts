51cae8475abca776c46d0a76130c257e10a5057c
Mike Bond <mdcbond@gmail.com>
2025-06-20 14:06:51 -0700

    Add mixin for imageProcessing support in materials

--- packages/dev/core/src/Materials/standardMaterial.ts
+++ packages/dev/core/src/Materials/standardMaterial.ts
@@ -62,6 +62,7 @@ import { SerializationHelper } from "../Misc/decorators.serialization";
 import { ShaderLanguage } from "./shaderLanguage";
 import { MaterialHelperGeometryRendering } from "./materialHelper.geometryrendering";
 import { UVDefinesMixin } from "./uv.defines";
+import { ImageProcessingMixin } from "./imageProcessing";
 
 const onCreatedEffectParameters = { effect: null as unknown as Effect, subMesh: null as unknown as Nullable<SubMesh> };
 
@@ -249,12 +250,13 @@ export class StandardMaterialDefines extends ImageProcessingDefinesMixin(Standar
     }
 }
 
+class StandardMaterialBase extends ImageProcessingMixin(PushMaterial) {}
 /**
  * This is the default material used in Babylon. It is the best trade off between quality
  * and performances.
  * @see https://doc.babylonjs.com/features/featuresDeepDive/materials/using/materials_introduction
  */
-export class StandardMaterial extends PushMaterial {
+export class StandardMaterial extends StandardMaterialBase {
     /**
      * Force all the standard materials to compile to glsl even on WebGPU engines.
      * False by default. This is mostly meant for backward compatibility.
@@ -601,64 +603,6 @@ export class StandardMaterial extends PushMaterial {
     @expandToProperty("_markAllSubMeshesAsMiscDirty")
     public applyDecalMapAfterDetailMap: boolean;
 
-    /**
-     * Default configuration related to image processing available in the standard Material.
-     */
-    protected _imageProcessingConfiguration: ImageProcessingConfiguration;
-
-    /**
-     * Gets the image processing configuration used either in this material.
-     */
-    public get imageProcessingConfiguration(): ImageProcessingConfiguration {
-        return this._imageProcessingConfiguration;
-    }
-
-    /**
-     * Sets the Default image processing configuration used either in the this material.
-     *
-     * If sets to null, the scene one is in use.
-     */
-    public set imageProcessingConfiguration(value: ImageProcessingConfiguration) {
-        this._attachImageProcessingConfiguration(value);
-
-        // Ensure the effect will be rebuilt.
-        this._markAllSubMeshesAsImageProcessingDirty();
-    }
-
-    /**
-     * Keep track of the image processing observer to allow dispose and replace.
-     */
-    private _imageProcessingObserver: Nullable<Observer<ImageProcessingConfiguration>>;
-
-    /**
-     * Attaches a new image processing configuration to the Standard Material.
-     * @param configuration
-     */
-    protected _attachImageProcessingConfiguration(configuration: Nullable<ImageProcessingConfiguration>): void {
-        if (configuration === this._imageProcessingConfiguration) {
-            return;
-        }
-
-        // Detaches observer
-        if (this._imageProcessingConfiguration && this._imageProcessingObserver) {
-            this._imageProcessingConfiguration.onUpdateParameters.remove(this._imageProcessingObserver);
-        }
-
-        // Pick the scene configuration if needed
-        if (!configuration) {
-            this._imageProcessingConfiguration = this.getScene().imageProcessingConfiguration;
-        } else {
-            this._imageProcessingConfiguration = configuration;
-        }
-
-        // Attaches observer
-        if (this._imageProcessingConfiguration) {
-            this._imageProcessingObserver = this._imageProcessingConfiguration.onUpdateParameters.add(() => {
-                this._markAllSubMeshesAsImageProcessingDirty();
-            });
-        }
-    }
-
     private _shadersLoaded = false;
 
     /**
@@ -671,108 +615,6 @@ export class StandardMaterial extends PushMaterial {
      */
     public override get isPrePassCapable(): boolean {
         return !this.disableDepthWrite;
-    }
-
-    /**
-     * Gets whether the color curves effect is enabled.
-     */
-    public get cameraColorCurvesEnabled(): boolean {
-        return this.imageProcessingConfiguration.colorCurvesEnabled;
-    }
-    /**
-     * Sets whether the color curves effect is enabled.
-     */
-    public set cameraColorCurvesEnabled(value: boolean) {
-        this.imageProcessingConfiguration.colorCurvesEnabled = value;
-    }
-
-    /**
-     * Gets whether the color grading effect is enabled.
-     */
-    public get cameraColorGradingEnabled(): boolean {
-        return this.imageProcessingConfiguration.colorGradingEnabled;
-    }
-    /**
-     * Gets whether the color grading effect is enabled.
-     */
-    public set cameraColorGradingEnabled(value: boolean) {
-        this.imageProcessingConfiguration.colorGradingEnabled = value;
-    }
-
-    /**
-     * Gets whether tonemapping is enabled or not.
-     */
-    public get cameraToneMappingEnabled(): boolean {
-        return this._imageProcessingConfiguration.toneMappingEnabled;
-    }
-    /**
-     * Sets whether tonemapping is enabled or not
-     */
-    public set cameraToneMappingEnabled(value: boolean) {
-        this._imageProcessingConfiguration.toneMappingEnabled = value;
-    }
-
-    /**
-     * The camera exposure used on this material.
-     * This property is here and not in the camera to allow controlling exposure without full screen post process.
-     * This corresponds to a photographic exposure.
-     */
-    public get cameraExposure(): number {
-        return this._imageProcessingConfiguration.exposure;
-    }
-    /**
-     * The camera exposure used on this material.
-     * This property is here and not in the camera to allow controlling exposure without full screen post process.
-     * This corresponds to a photographic exposure.
-     */
-    public set cameraExposure(value: number) {
-        this._imageProcessingConfiguration.exposure = value;
-    }
-
-    /**
-     * Gets The camera contrast used on this material.
-     */
-    public get cameraContrast(): number {
-        return this._imageProcessingConfiguration.contrast;
-    }
-
-    /**
-     * Sets The camera contrast used on this material.
-     */
-    public set cameraContrast(value: number) {
-        this._imageProcessingConfiguration.contrast = value;
-    }
-
-    /**
-     * Gets the Color Grading 2D Lookup Texture.
-     */
-    public get cameraColorGradingTexture(): Nullable<BaseTexture> {
-        return this._imageProcessingConfiguration.colorGradingTexture;
-    }
-    /**
-     * Sets the Color Grading 2D Lookup Texture.
-     */
-    public set cameraColorGradingTexture(value: Nullable<BaseTexture>) {
-        this._imageProcessingConfiguration.colorGradingTexture = value;
-    }
-
-    /**
-     * The color grading curves provide additional color adjustmnent that is applied after any color grading transform (3D LUT).
-     * They allow basic adjustment of saturation and small exposure adjustments, along with color filter tinting to provide white balance adjustment or more stylistic effects.
-     * These are similar to controls found in many professional imaging or colorist software. The global controls are applied to the entire image. For advanced tuning, extra controls are provided to adjust the shadow, midtone and highlight areas of the image;
-     * corresponding to low luminance, medium luminance, and high luminance areas respectively.
-     */
-    public get cameraColorCurves(): Nullable<ColorCurves> {
-        return this._imageProcessingConfiguration.colorCurves;
-    }
-    /**
-     * The color grading curves provide additional color adjustment that is applied after any color grading transform (3D LUT).
-     * They allow basic adjustment of saturation and small exposure adjustments, along with color filter tinting to provide white balance adjustment or more stylistic effects.
-     * These are similar to controls found in many professional imaging or colorist software. The global controls are applied to the entire image. For advanced tuning, extra controls are provided to adjust the shadow, midtone and highlight areas of the image;
-     * corresponding to low luminance, medium luminance, and high luminance areas respectively.
-     */
-    public set cameraColorCurves(value: Nullable<ColorCurves>) {
-        this._imageProcessingConfiguration.colorCurves = value;
     }
 
     /**
--- packages/dev/loaders/src/glTF/2.0/Extensions/KHR_materials_pbrSpecularGlossiness.ts
+++ packages/dev/loaders/src/glTF/2.0/Extensions/KHR_materials_pbrSpecularGlossiness.ts
@@ -1,7 +1,7 @@
 import type { Nullable } from "core/types";
 import { Color3 } from "core/Maths/math.color";
 import { PBRMaterial } from "core/Materials/PBR/pbrMaterial";
-import { PBRMaterial2 } from "core/Materials/PBR/pbrMaterial2";
+import { PBR2Material } from "core/Materials/PBR/pbr2Material";
 import type { Material } from "core/Materials/material";
 
 import type { IMaterial } from "../glTFLoaderInterfaces";
@@ -75,7 +75,7 @@ export class KHR_materials_pbrSpecularGlossiness implements IGLTFLoaderExtension
 
     // eslint-disable-next-line @typescript-eslint/promise-function-async, no-restricted-syntax
     private _loadSpecularGlossinessPropertiesAsync(context: string, properties: IKHRMaterialsPbrSpecularGlossiness, babylonMaterial: Material): Promise<void> {
-        if (!(babylonMaterial instanceof PBRMaterial) && !(babylonMaterial instanceof PBRMaterial2)) {
+        if (!(babylonMaterial instanceof PBRMaterial) && !(babylonMaterial instanceof PBR2Material)) {
             throw new Error(`${context}: Material type not supported`);
         }
 
