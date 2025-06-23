90e5ccfe2ddf40a267f309a6e7f00d8eff7eceb5
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-06-23 15:22:40 -0700

    Finished frame step duration pane

--- packages/dev/inspector-v2/src/components/stats/countStats.tsx
+++ packages/dev/inspector-v2/src/components/stats/countStats.tsx
@@ -1,17 +1,63 @@
 // eslint-disable-next-line import/no-internal-modules
 import type { Scene } from "core/index";
-
-import type { FunctionComponent } from "react";
-
+import { TextPropertyLine } from "shared-ui-components/fluent/hoc/textPropertyLine";
+import { useEffect, useRef, type FunctionComponent } from "react";
 import { useObservableState } from "../../hooks/observableHooks";
+import { usePollingObservable } from "../../hooks/pollingHooks";
+// import { EngineInstrumentation } from "core/Instrumentation/engineInstrumentation";
+import { SceneInstrumentation } from "core/Instrumentation/sceneInstrumentation";
 
 export const CountStats: FunctionComponent<{ context: Scene }> = ({ context: scene }) => {
+    const pollingObservable = usePollingObservable(500);
+    const sceneInstrumentationRef = useRef<SceneInstrumentation>(new SceneInstrumentation(scene));
+
+    useEffect(() => {
+        let sceneInstrumentation = sceneInstrumentationRef.current;
+
+        if (sceneInstrumentation.scene !== scene) {
+            sceneInstrumentation.dispose();
+            sceneInstrumentation = new SceneInstrumentation(scene);
+            sceneInstrumentationRef.current = sceneInstrumentation;
+        }
+        sceneInstrumentation.captureActiveMeshesEvaluationTime = true;
+        sceneInstrumentation.captureRenderTargetsRenderTime = true;
+        sceneInstrumentation.captureFrameTime = true;
+        sceneInstrumentation.captureRenderTime = true;
+        sceneInstrumentation.captureInterFrameTime = true;
+        sceneInstrumentation.captureParticlesRenderTime = true;
+        sceneInstrumentation.captureSpritesRenderTime = true;
+        sceneInstrumentation.capturePhysicsTime = true;
+        sceneInstrumentation.captureAnimationsTime = true;
+
+        return () => {
+            sceneInstrumentation.dispose();
+        };
+    }, [scene]);
+
     const totalMeshes = useObservableState(() => scene.meshes.length, scene.onNewMeshAddedObservable, scene.onMeshRemovedObservable);
+    const activeMeshes = useObservableState(() => scene.getActiveMeshes().length, pollingObservable);
+    const activeIndices = useObservableState(() => scene.getActiveIndices(), pollingObservable);
+    const activeBones = useObservableState(() => scene.getActiveBones(), pollingObservable);
+    const activeParticles = useObservableState(() => scene.getActiveParticles(), pollingObservable);
+    const drawCalls = useObservableState(() => sceneInstrumentationRef.current.drawCallsCounter.current, pollingObservable);
+    const totalLights = useObservableState(() => scene.lights.length, pollingObservable);
+    const totalVertices = useObservableState(() => scene.getTotalVertices(), pollingObservable);
+    const totalMaterials = useObservableState(() => scene.materials.length, pollingObservable);
+    const totalTextures = useObservableState(() => scene.textures.length, pollingObservable);
 
     return (
-        // TODO: Use the new Fluent property line shared components.
         <>
-            <div key="TotalMeshes">TotalMeshes: {totalMeshes}</div>
+            <TextPropertyLine key="TotalMeshes" label="Total meshes" value={totalMeshes.toLocaleString()} />
+            <TextPropertyLine key="ActiveMeshes" label="Active meshes" value={activeMeshes.toLocaleString()} />
+            <TextPropertyLine key="ActiveIndices" label="Active indeces" value={activeIndices.toLocaleString()} />
+            <TextPropertyLine key="ActiveFaces" label="Active faces" value={(activeIndices / 3).toLocaleString()} />
+            <TextPropertyLine key="ActiveBones" label="Active bones" value={activeBones.toLocaleString()} />
+            <TextPropertyLine key="ActiveParticles" label="Active particles" value={activeParticles.toLocaleString()} />
+            <TextPropertyLine key="DrawCalls" label="Draw calls" value={drawCalls.toLocaleString()} />
+            <TextPropertyLine key="TotalLights" label="Total lights" value={totalLights.toLocaleString()} />
+            <TextPropertyLine key="TotalVertices" label="Total vertices" value={totalVertices.toLocaleString()} />
+            <TextPropertyLine key="TotalMaterials" label="Total materials" value={totalMaterials.toLocaleString()} />
+            <TextPropertyLine key="TotalTextures" label="Total textures" value={totalTextures.toLocaleString()} />
         </>
     );
 };
--- packages/dev/inspector-v2/src/components/stats/frameStepStats.tsx
+++ packages/dev/inspector-v2/src/components/stats/frameStepStats.tsx
@@ -1,27 +1,23 @@
 // eslint-disable-next-line import/no-internal-modules
 import type { Scene } from "core/index";
-
+import { TextPropertyLine } from "shared-ui-components/fluent/hoc/textPropertyLine";
 import type { FunctionComponent } from "react";
-
 import { useEffect, useRef } from "react";
-
 import { EngineInstrumentation } from "core/Instrumentation/engineInstrumentation";
 import { SceneInstrumentation } from "core/Instrumentation/sceneInstrumentation";
 import "core/Engines/AbstractEngine/abstractEngine.timeQuery";
-
+import "core/Engines/Extensions/engine.query";
 import { useObservableState } from "../../hooks/observableHooks";
 import { usePollingObservable } from "../../hooks/pollingHooks";
 
 export const FrameStepsStats: FunctionComponent<{ context: Scene }> = ({ context: scene }) => {
-    const engine = scene.getEngine();
-
     const pollingObservable = usePollingObservable(500);
-
     const sceneInstrumentationRef = useRef<SceneInstrumentation>(new SceneInstrumentation(scene));
-    const engineInstrumentationRef = useRef<EngineInstrumentation>(new EngineInstrumentation(engine));
+    const engineInstrumentationRef = useRef<EngineInstrumentation>(new EngineInstrumentation(scene.getEngine()));
 
     useEffect(() => {
         let sceneInstrumentation = sceneInstrumentationRef.current;
+
         if (sceneInstrumentation.scene !== scene) {
             sceneInstrumentation.dispose();
             sceneInstrumentation = new SceneInstrumentation(scene);
@@ -44,6 +40,7 @@ export const FrameStepsStats: FunctionComponent<{ context: Scene }> = ({ context
 
     useEffect(() => {
         let engineInstrumentation = engineInstrumentationRef.current;
+        const engine = scene.getEngine();
         if (engineInstrumentation.engine !== engine) {
             engineInstrumentation.dispose();
             engineInstrumentation = new EngineInstrumentation(engine);
@@ -54,17 +51,31 @@ export const FrameStepsStats: FunctionComponent<{ context: Scene }> = ({ context
         return () => {
             engineInstrumentation.dispose();
         };
-    }, [engine]);
+    }, [scene]);
 
-    const absoluteFPS = useObservableState(() => (1000.0 / sceneInstrumentationRef.current.frameTimeCounter.lastSecAverage).toFixed(0), pollingObservable);
-    // TODO: Dynamically import the right engine.query module based on the type of engine?
-    const gpuFrameTime = useObservableState(() => (engineInstrumentationRef.current.gpuFrameTimeCounter.lastSecAverage * 0.000001).toFixed(2), pollingObservable);
+    const absoluteFPS = useObservableState(() => Math.floor(1000.0 / sceneInstrumentationRef.current.frameTimeCounter.lastSecAverage), pollingObservable);
+    const meshesSelection = useObservableState(() => sceneInstrumentationRef.current.activeMeshesEvaluationTimeCounter.lastSecAverage, pollingObservable);
+    const renderTargets = useObservableState(() => sceneInstrumentationRef.current.renderTargetsRenderTimeCounter.lastSecAverage, pollingObservable);
+    const particles = useObservableState(() => sceneInstrumentationRef.current.particlesRenderTimeCounter.lastSecAverage, pollingObservable);
+    const sprites = useObservableState(() => sceneInstrumentationRef.current.spritesRenderTimeCounter.lastSecAverage, pollingObservable);
+    const animations = useObservableState(() => sceneInstrumentationRef.current.animationsTimeCounter.lastSecAverage, pollingObservable);
+    const physics = useObservableState(() => sceneInstrumentationRef.current.physicsTimeCounter.lastSecAverage, pollingObservable);
+    const interFrameTime = useObservableState(() => sceneInstrumentationRef.current.interFrameTimeCounter.lastSecAverage, pollingObservable);
+    const gpuFrameTime = useObservableState(() => engineInstrumentationRef.current.gpuFrameTimeCounter.lastSecAverage * 0.000001, pollingObservable);
+    const gpuFrameTimeAverage = useObservableState(() => engineInstrumentationRef.current.gpuFrameTimeCounter.average * 0.000001, pollingObservable);
 
     return (
-        // TODO: Use the new Fluent property line shared components.
         <>
-            <div key="AbsoluteFPS">AbsoluteFPS: {absoluteFPS}</div>
-            <div key="GPUFrameTime">GPU Frame Time: {gpuFrameTime}ms</div>
+            <TextPropertyLine key="AbsoluteFPS" label="Absolute FPS" value={absoluteFPS.toLocaleString()} />
+            <TextPropertyLine key="MeshesSelection" label="Meshes selection" value={meshesSelection.toFixed(2) + " ms"} />
+            <TextPropertyLine key="RenderTargets" label="Render targets" value={renderTargets.toFixed(2) + " ms"} />
+            <TextPropertyLine key="Particles" label="Particles" value={particles.toFixed(2) + " ms"} />
+            <TextPropertyLine key="Sprites" label="Sprites" value={sprites.toFixed(2) + " ms"} />
+            <TextPropertyLine key="Animations" label="Animations" value={animations.toFixed(2) + " ms"} />
+            <TextPropertyLine key="Physics" label="Physics" value={physics.toFixed(2) + " ms"} />
+            <TextPropertyLine key="InterFrameTime" label="Inter-frame time" value={interFrameTime.toFixed(2) + " ms"} />
+            <TextPropertyLine key="GPUFrameTime" label="GPU frame time" value={gpuFrameTime.toFixed(2) + " ms"} />
+            <TextPropertyLine key="GPUFrameTimeAverage" label="GPU frame time (average)" value={gpuFrameTimeAverage.toFixed(2) + " ms"} />
         </>
     );
 };
