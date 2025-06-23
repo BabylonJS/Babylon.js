29148d2af65fcd555a5dbeeaacaa3b1e31c78a20
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2025-06-23 14:46:08 -0300

    Added base implementation for hemisphericLight

--- packages/dev/inspector-v2/src/components/properties/lights/hemisphericLight/hemisphericLightGeneralProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/lights/hemisphericLight/hemisphericLightGeneralProperties.tsx
@@ -1,19 +1,12 @@
 // eslint-disable-next-line import/no-internal-modules
 import type { HemisphericLight } from "core/index";
-
 import type { FunctionComponent } from "react";
 
-import { useInterceptObservable } from "../../../../hooks/instrumentationHooks";
-import { useObservableState } from "../../../../hooks/observableHooks";
-
 export const HemisphericLightGeneralProperties: FunctionComponent<{ context: HemisphericLight }> = ({ context: hemisphericLight }) => {
-    // There is no observable for computeBonesUsingShaders, so we use an interceptor to listen for changes.
-    const direction = useObservableState(() => hemisphericLight.direction, useInterceptObservable("property", hemisphericLight, "direction"));
-
     return (
         // TODO: Use the new Fluent property line shared components.
         <>
-            <VectorPropertyLine label="Direction" vector={direction} onCopy={() => {}} />;
+            <div key="HemisphericLightIsEnabled">Is enabled: {hemisphericLight.isEnabled(false).toString()}</div>
         </>
     );
 };
