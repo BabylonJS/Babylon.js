0dac6d329db7b3c6563a16742aba10a418774b1a
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-06-26 14:21:43 -0700

    Scale slider min/max to avoid tick marks

--- packages/dev/sharedUiComponents/src/fluent/primitives/syncedSlider.tsx
+++ packages/dev/sharedUiComponents/src/fluent/primitives/syncedSlider.tsx
@@ -37,12 +37,19 @@ export const SyncedSliderInput: FunctionComponent<SyncedSliderProps> = (props) =
     const classes = useSyncedSliderStyles();
     const [value, setValue] = useState<number>(props.value);
 
+    // NOTE: The Fluent slider will add tick marks if the step prop is anything other than undefined.
+    // To avoid this, we scale the min/max based on the step so we can always make step undefined.
+    // The actual step size in the Fluent slider is 1 when it is ste to undefined.
+    const min = props.min ?? 0;
+    const max = props.max ?? 100;
+    const step = props.step ?? 1;
+
     useEffect(() => {
         setValue(props.value ?? ""); // Update local state when props.value changes
     }, [props.value]);
 
     const handleSliderChange = (_: ChangeEvent<HTMLInputElement>, data: SliderOnChangeData) => {
-        const value = data.value;
+        const value = data.value * step;
         setValue(value);
         props.onChange(value); // Notify parent
     };
@@ -57,8 +64,10 @@ export const SyncedSliderInput: FunctionComponent<SyncedSliderProps> = (props) =
 
     return (
         <div className={classes.syncedSlider}>
-            {props.min !== undefined && props.max !== undefined && <Slider {...props} size="small" className={classes.slider} value={value} onChange={handleSliderChange} />}
-            <Input {...props} className={classes.input} value={value} onChange={handleInputChange} step={props.step} />
+            {props.min !== undefined && props.max !== undefined && (
+                <Slider {...props} size="small" className={classes.slider} min={min / step} max={max / step} step={undefined} value={value / step} onChange={handleSliderChange} />
+            )}
+            <Input {...props} className={classes.input} value={Math.round(value / step) * step} onChange={handleInputChange} step={step} />
         </div>
     );
 };
