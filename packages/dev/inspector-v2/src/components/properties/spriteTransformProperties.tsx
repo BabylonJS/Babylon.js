8deb99cc68e63bc216212602c7d680aee09ac4de
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-06-30 15:54:12 -0700

    Add optional converter functions to BoundProperty

--- packages/dev/inspector-v2/src/components/properties/spriteTransformProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/spriteTransformProperties.tsx
@@ -4,7 +4,7 @@ import type { Sprite } from "core/index";
 
 import { SyncedSliderLine } from "shared-ui-components/fluent/hoc/syncedSliderLine";
 import { Vector3PropertyLine } from "shared-ui-components/fluent/hoc/vectorPropertyLine";
-import { useProperty, useVector3Property } from "../../hooks/compoundPropertyHooks";
+import { useVector3Property } from "../../hooks/compoundPropertyHooks";
 import { useObservableState } from "../../hooks/observableHooks";
 import type { ISettingsContext } from "../../services/settingsContext";
 import { BoundProperty } from "./boundProperty";
@@ -14,26 +14,26 @@ const RadiansToDegrees = 180 / Math.PI;
 export const SpriteTransformProperties: FunctionComponent<{ sprite: Sprite; settings: ISettingsContext }> = (props) => {
     const { sprite, settings } = props;
 
-    const angle = useProperty(sprite, "angle");
     const position = useVector3Property(sprite, "position");
 
     const useDegrees = useObservableState(() => settings.useDegrees, settings.settingsChangedObservable);
     const angleMultiplier = useDegrees ? RadiansToDegrees : 1;
-    const max = Math.PI * 2 * angleMultiplier;
-    const step = useDegrees ? 1 : 0.01;
 
     return (
         <>
             <Vector3PropertyLine key="PositionTransform" label="Position" value={position} onChange={(val) => (sprite.position = val)} />
-            <SyncedSliderLine
-                key="Angle"
+            <BoundProperty
+                component={SyncedSliderLine}
+                key="Angle2"
                 label="Angle"
                 description="Rotation angle of the sprite"
-                value={angle * angleMultiplier}
                 min={0}
-                max={max}
-                step={step}
-                onChange={(angle) => (sprite.angle = angle / angleMultiplier)}
+                max={Math.PI * 2 * angleMultiplier}
+                step={useDegrees ? 1 : 0.01}
+                target={sprite}
+                propertyKey="angle"
+                convertTo={(angle) => angle * angleMultiplier}
+                convertFrom={(angle) => angle / angleMultiplier}
             />
             <BoundProperty component={SyncedSliderLine} key="Width" label="Width" description="Width of the sprite (in world space units)" target={sprite} propertyKey="width" />
             <BoundProperty
