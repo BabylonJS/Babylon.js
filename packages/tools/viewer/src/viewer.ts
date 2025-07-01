25b93f758d6e76d1692d3089d63bfdf0cabfcced
Alexandre Mottet <alexandremottet.dev@gmail.com>
2025-07-01 16:33:53 +0200

    merge 2 for loop in one

--- packages/tools/viewer/src/viewer.ts
+++ packages/tools/viewer/src/viewer.ts
@@ -1851,12 +1851,8 @@ export class Viewer implements IDisposable {
         normal.light.position = position;
 
         for (const model of this._loadedModelsBacking) {
-            // Add all root meshes to the shadow generator.
             for (const mesh of model.assetContainer.meshes) {
-                normal.generator.addShadowCaster(mesh);
-            }
-            // Set all meshes to receive shadows.
-            for (const mesh of model.assetContainer.meshes) {
+                normal.generator.addShadowCaster(mesh, false);
                 mesh.receiveShadows = true;
             }
         }
