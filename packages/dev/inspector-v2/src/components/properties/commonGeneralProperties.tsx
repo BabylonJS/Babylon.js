8a6cbed4310071f5083a6aa0cdd945dbaac11b34
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-07-09 15:43:51 -0700

    Rendering pipelines in scene explorer

--- packages/dev/inspector-v2/src/components/properties/commonGeneralProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/commonGeneralProperties.tsx
@@ -1,8 +1,11 @@
 import type { FunctionComponent } from "react";
 
+import { useMemo } from "react";
+
 import { TextInputPropertyLine } from "shared-ui-components/fluent/hoc/inputPropertyLine";
 import { TextPropertyLine } from "shared-ui-components/fluent/hoc/textPropertyLine";
 import { useProperty } from "../../hooks/compoundPropertyHooks";
+import { GetPropertyDescriptor, IsPropertyReadonly } from "../../instrumentation/propertyInstrumentation";
 
 type CommonEntity = {
     readonly id?: number;
@@ -15,14 +18,20 @@ export const CommonGeneralProperties: FunctionComponent<{ commonEntity: CommonEn
     const { commonEntity } = props;
 
     const name = useProperty(commonEntity, "name");
+    const namePropertyDescriptor = useMemo(() => GetPropertyDescriptor(commonEntity, "name")?.[1], [commonEntity]);
+    const isNameReadonly = !namePropertyDescriptor || IsPropertyReadonly(namePropertyDescriptor);
+
     const className = commonEntity.constructor?.name || commonEntity.getClassName?.();
 
     return (
         <>
             {commonEntity.id !== undefined && <TextPropertyLine key="EntityId" label="ID" description="The id of the node." value={commonEntity.id.toString()} />}
-            {name !== undefined && (
-                <TextInputPropertyLine key="EntityName" label="Name" description="The name of the node." value={name} onChange={(newName) => (commonEntity.name = newName)} />
-            )}
+            {name !== undefined &&
+                (isNameReadonly ? (
+                    <TextPropertyLine key="EntityName" label="Name" description="The name of the node." value={name} />
+                ) : (
+                    <TextInputPropertyLine key="EntityName" label="Name" description="The name of the node." value={name} onChange={(newName) => (commonEntity.name = newName)} />
+                ))}
             {commonEntity.uniqueId !== undefined && (
                 <TextPropertyLine key="EntityUniqueId" label="Unique ID" description="The unique id of the node." value={commonEntity.uniqueId.toString()} />
             )}
