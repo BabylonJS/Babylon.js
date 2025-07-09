c8e322d4f0be7243bebf4f3ca27dc753d8dede06
Georgina <gehalper@microsoft.com>
2025-07-09 16:31:21 -0400

    fix rerenders that were breaking NME logic

--- packages/dev/gui/src/2D/controls/sliders/baseSlider.ts
+++ packages/dev/gui/src/2D/controls/sliders/baseSlider.ts
@@ -18,6 +18,7 @@ export class BaseSlider extends Control {
     private _isVertical = false;
     protected _barOffset = new ValueAndUnit(5, ValueAndUnit.UNITMODE_PIXEL, false);
     private _isThumbClamped = false;
+    private _dragOnly = false;
     protected _displayThumb = true;
     private _step = 0;
 
@@ -190,6 +191,21 @@ export class BaseSlider extends Control {
         this._markAsDirty();
     }
 
+    /** Gets or sets a value indicating whether the slider disables picking */
+    @serialize()
+    public get dragOnly(): boolean {
+        return this._dragOnly;
+    }
+
+    public set dragOnly(value: boolean) {
+        if (this._dragOnly === value) {
+            return;
+        }
+
+        this._dragOnly = value;
+        this._markAsDirty();
+    }
+
     /**
      * Creates a new BaseSlider
      * @param name defines the control name
@@ -310,10 +326,11 @@ export class BaseSlider extends Control {
         }
 
         this._pointerIsDown = true;
-
-        this._updateValueFromPointer(coordinates.x, coordinates.y);
-        this._host._capturingControl[pointerId] = this;
-        this._lastPointerDownId = pointerId;
+        if (!this._dragOnly) {
+            this._updateValueFromPointer(coordinates.x, coordinates.y);
+            this._host._capturingControl[pointerId] = this;
+            this._lastPointerDownId = pointerId;
+        }
         return true;
     }
 
