ce03c25b829c2b7cc85d928173419f4c3ccb2e69
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-07-10 11:28:27 -0700

    Add add/remove observables for missing types in scene
    Have scene explorer sections consume scene context directly
    
    Fix up particle systems in scene explorer

--- packages/dev/inspector-v2/src/services/panes/scene/particleSystemExplorerService.tsx
+++ packages/dev/inspector-v2/src/services/panes/scene/particleSystemExplorerService.tsx
@@ -1,22 +1,29 @@
+import type { IParticleSystem } from "core/index";
 import type { ServiceDefinition } from "../../../modularity/serviceDefinition";
+import type { ISceneContext } from "../../sceneContext";
 import type { ISceneExplorerService } from "./sceneExplorerService";
 
 import { DropRegular } from "@fluentui/react-icons";
 
 import { Observable } from "core/Misc";
 import { InterceptProperty } from "../../../instrumentation/propertyInstrumentation";
+import { SceneContextIdentity } from "../../sceneContext";
 import { SceneExplorerServiceIdentity } from "./sceneExplorerService";
-import { ParticleSystem } from "core/Particles";
 
-export const ParticleSystemExplorerServiceDefinition: ServiceDefinition<[], [ISceneExplorerService]> = {
+export const ParticleSystemExplorerServiceDefinition: ServiceDefinition<[], [ISceneExplorerService, ISceneContext]> = {
     friendlyName: "Particle System Hierarchy",
-    consumes: [SceneExplorerServiceIdentity],
-    factory: (sceneExplorerService) => {
+    consumes: [SceneExplorerServiceIdentity, SceneContextIdentity],
+    factory: (sceneExplorerService, sceneContext) => {
+        const scene = sceneContext.currentScene;
+        if (!scene) {
+            return void 0;
+        }
+
         const sectionRegistration = sceneExplorerService.addSection({
             displayName: "Particle Systems",
-            order: 4,
-            predicate: (entity) => entity instanceof ParticleSystem, //  TODO-iv2: Implement a more robust predicate to filter for IParticleSystems (perhaps checking if contained in scene.particleSystems)
-            getRootEntities: (scene) => scene.particleSystems.map((ps) => ps as ParticleSystem),
+            order: 600,
+            predicate: (entity): entity is IParticleSystem => scene.particleSystems.includes(entity as IParticleSystem),
+            getRootEntities: () => scene.particleSystems,
             getEntityDisplayInfo: (particleSystem) => {
                 const onChangeObservable = new Observable<void>();
 
@@ -38,8 +45,8 @@ export const ParticleSystemExplorerServiceDefinition: ServiceDefinition<[], [ISc
                 };
             },
             entityIcon: () => <DropRegular />,
-            getEntityAddedObservables: (scene) => [], // TODO-iv2: Implement scene-level observables for particle system additions/removals
-            getEntityRemovedObservables: (scene) => [],
+            getEntityAddedObservables: () => [scene.onNewParticleSystemAddedObservable],
+            getEntityRemovedObservables: () => [scene.onParticleSystemRemovedObservable],
         });
 
         return {
