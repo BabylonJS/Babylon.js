05409e92cf662a636ff7c0241dccdcc55b04469a
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-07-10 20:00:21 -0700

    Rework gui explorer service to only use types and not require a dynamic import

--- packages/dev/inspector-v2/src/services/panes/scene/guiExplorerService.tsx
+++ packages/dev/inspector-v2/src/services/panes/scene/guiExplorerService.tsx
@@ -1,5 +1,4 @@
-import type { IDisposable, IObserver, Nullable } from "core/index";
-
+import type { AdvancedDynamicTexture } from "gui/index";
 import type { ServiceDefinition } from "../../../modularity/serviceDefinition";
 import type { ISceneContext } from "../../sceneContext";
 import type { ISceneExplorerService } from "./sceneExplorerService";
@@ -11,6 +10,10 @@ import { InterceptProperty } from "../../../instrumentation/propertyInstrumentat
 import { SceneContextIdentity } from "../../sceneContext";
 import { SceneExplorerServiceIdentity } from "./sceneExplorerService";
 
+function IsAdvancedDynamicTexture(entity: unknown): entity is AdvancedDynamicTexture {
+    return (entity as AdvancedDynamicTexture)?.getClassName() === "AdvancedDynamicTexture";
+}
+
 export const GuiExplorerServiceDefinition: ServiceDefinition<[], [ISceneExplorerService, ISceneContext]> = {
     friendlyName: "GUI Hierarchy",
     consumes: [SceneExplorerServiceIdentity, SceneContextIdentity],
@@ -20,66 +23,39 @@ export const GuiExplorerServiceDefinition: ServiceDefinition<[], [ISceneExplorer
             return undefined;
         }
 
-        let disposed = false;
-        let sectionRegistration: Nullable<IDisposable> = null;
-        let textureAddedObserver: Nullable<IObserver> = null;
+        const sectionRegistration = sceneExplorerService.addSection({
+            displayName: "GUI",
+            order: 900,
+            predicate: IsAdvancedDynamicTexture,
+            getRootEntities: () => scene.textures.filter(IsAdvancedDynamicTexture),
+            getEntityDisplayInfo: (texture) => {
+                const onChangeObservable = new Observable<void>();
 
-        const addSectionAsync = async () => {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            const { AdvancedDynamicTexture } = await import("gui/2D/advancedDynamicTexture");
-            if (disposed || sectionRegistration) {
-                return;
-            }
+                const nameHookToken = InterceptProperty(texture, "name", {
+                    afterSet: () => {
+                        onChangeObservable.notifyObservers();
+                    },
+                });
 
-            sectionRegistration = sceneExplorerService.addSection({
-                displayName: "GUI",
-                order: 900,
-                predicate: (entity) => entity instanceof AdvancedDynamicTexture,
-                getRootEntities: () => scene.textures.filter((texture) => texture instanceof AdvancedDynamicTexture),
-                getEntityDisplayInfo: (advancedDynamicTexture) => {
-                    const onChangeObservable = new Observable<void>();
-
-                    const nameHookToken = InterceptProperty(advancedDynamicTexture, "name", {
-                        afterSet: () => {
-                            onChangeObservable.notifyObservers();
-                        },
-                    });
-
-                    return {
-                        get name() {
-                            return advancedDynamicTexture.name;
-                        },
-                        onChange: onChangeObservable,
-                        dispose: () => {
-                            nameHookToken.dispose();
-                            onChangeObservable.clear();
-                        },
-                    };
-                },
-                entityIcon: () => <AppGenericRegular />,
-                getEntityAddedObservables: () => [scene.onNewTextureAddedObservable],
-                getEntityRemovedObservables: () => [scene.onTextureRemovedObservable],
-            });
-        };
-
-        if (scene.textures.some((texture) => texture.getClassName() === "AdvancedDynamicTexture")) {
-            // If an AdvancedDynamicTexture is already present, we can register the section immediately.
-            void addSectionAsync();
-        } else {
-            // Otherwise, we defer the registration until an AdvancedDynamicTexture is added.
-            textureAddedObserver = scene.onNewTextureAddedObservable.add((texture) => {
-                if (texture.getClassName() === "AdvancedDynamicTexture") {
-                    textureAddedObserver?.remove();
-                    void addSectionAsync();
-                }
-            });
-        }
+                return {
+                    get name() {
+                        return texture.name;
+                    },
+                    onChange: onChangeObservable,
+                    dispose: () => {
+                        nameHookToken.dispose();
+                        onChangeObservable.clear();
+                    },
+                };
+            },
+            entityIcon: () => <AppGenericRegular />,
+            getEntityAddedObservables: () => [scene.onNewTextureAddedObservable],
+            getEntityRemovedObservables: () => [scene.onTextureRemovedObservable],
+        });
 
         return {
             dispose: () => {
-                disposed = true;
-                textureAddedObserver?.remove();
-                sectionRegistration?.dispose();
+                sectionRegistration.dispose();
             },
         };
     },
