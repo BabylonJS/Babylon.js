332372831dc3ad5985640a683288c4ff8d04ebcd
Mike Bond <mdcbond@gmail.com>
2025-07-15 09:00:10 -0700

    Revert "Finish glTFLoader dynamic material load"
    This reverts commit c1c7c59d745dd061af842a005998f50bce588fa7.

--- packages/dev/loaders/src/glTF/2.0/Extensions/MSFT_sRGBFactors.ts
+++ packages/dev/loaders/src/glTF/2.0/Extensions/MSFT_sRGBFactors.ts
@@ -1,7 +1,7 @@
 import type { Nullable } from "core/types";
 import type { Material } from "core/Materials/material";
-import type { PBRMaterial } from "core/Materials/PBR/pbrMaterial";
-import type { OpenPBRMaterial } from "core/Materials/PBR/openPbrMaterial";
+import { PBRMaterial } from "core/Materials/PBR/pbrMaterial";
+import { OpenPBRMaterial } from "core/Materials/PBR/openPbrMaterial";
 
 import type { IMaterial } from "../glTFLoaderInterfaces";
 import type { IGLTFLoaderExtension } from "../glTFLoaderExtension";
@@ -20,8 +20,6 @@ declare module "../../glTFFileLoader" {
         ["MSFT_sRGBFactors"]: {};
     }
 }
-
-let PBRMaterialClass: typeof PBRMaterial | typeof OpenPBRMaterial;
 
 /** @internal */
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -47,30 +45,23 @@ export class MSFT_sRGBFactors implements IGLTFLoaderExtension {
 
     /** @internal*/
     // eslint-disable-next-line no-restricted-syntax
-    public loadMaterialPropertiesAsync(context: string, material: IMaterial, babylonMaterial: Material, useOpenPBR: boolean = false): Nullable<Promise<void>> {
+    public loadMaterialPropertiesAsync(context: string, material: IMaterial, babylonMaterial: Material): Nullable<Promise<void>> {
         return GLTFLoader.LoadExtraAsync<boolean>(context, material, this.name, async (extraContext, extra) => {
-            if (useOpenPBR) {
-                const mod = await import("core/Materials/PBR/openPbrMaterial");
-                PBRMaterialClass = mod.OpenPBRMaterial;
-            } else {
-                const mod = await import("core/Materials/PBR/pbrMaterial");
-                PBRMaterialClass = mod.PBRMaterial;
-            }
             if (extra) {
-                if (!(babylonMaterial instanceof PBRMaterialClass)) {
+                if (!(babylonMaterial instanceof PBRMaterial) && !(babylonMaterial instanceof OpenPBRMaterial)) {
                     throw new Error(`${extraContext}: Material type not supported`);
                 }
 
                 const promise = this._loader.loadMaterialPropertiesAsync(context, material, babylonMaterial);
 
                 const useExactSrgbConversions = babylonMaterial.getScene().getEngine().useExactSrgbConversions;
-                if (useOpenPBR) {
-                    if (!(babylonMaterial as OpenPBRMaterial).baseColorTexture) {
-                        (babylonMaterial as OpenPBRMaterial).baseColor.toLinearSpaceToRef((babylonMaterial as OpenPBRMaterial).baseColor, useExactSrgbConversions);
+                if (babylonMaterial instanceof OpenPBRMaterial) {
+                    if (!babylonMaterial.baseColorTexture) {
+                        babylonMaterial.baseColor.toLinearSpaceToRef(babylonMaterial.baseColor, useExactSrgbConversions);
                     }
                 } else {
-                    if (!(babylonMaterial as PBRMaterial).albedoTexture) {
-                        (babylonMaterial as PBRMaterial).albedoColor.toLinearSpaceToRef((babylonMaterial as PBRMaterial).albedoColor, useExactSrgbConversions);
+                    if (!babylonMaterial.albedoTexture) {
+                        babylonMaterial.albedoColor.toLinearSpaceToRef(babylonMaterial.albedoColor, useExactSrgbConversions);
                     }
                 }
 
