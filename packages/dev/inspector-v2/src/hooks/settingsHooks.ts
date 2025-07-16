c8b4ff068bb29333f96ac4c83a3d9954eb6d9316
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-07-16 13:01:15 -0700

    Add angle wrapping to converter functions

--- packages/dev/inspector-v2/src/hooks/settingsHooks.ts
+++ packages/dev/inspector-v2/src/hooks/settingsHooks.ts
@@ -6,6 +6,14 @@ import { useObservableState } from "./observableHooks";
 
 const RadiansToDegrees = 180 / Math.PI;
 
+function WrapAngle(angle: number) {
+    angle %= Math.PI * 2;
+    if (angle < 0) {
+        angle += Math.PI * 2;
+    }
+    return angle;
+}
+
 /**
  * Gets functions used to convert to/from display values for angles based on the current settings.
  * @param settings The settings context to use for determining if angles should be displayed in degrees or radians.
@@ -15,15 +23,22 @@ export function useAngleConverters(settings: ISettingsContext) {
     const useDegrees = useObservableState(() => settings.useDegrees, settings.settingsChangedObservable);
 
     const toDisplayValue = useCallback(
-        (angle: number) => {
+        (angle: number, wrap = false) => {
+            if (wrap) {
+                angle = WrapAngle(angle);
+            }
             return useDegrees ? angle * RadiansToDegrees : angle;
         },
         [useDegrees]
     );
 
     const fromDisplayValue = useCallback(
-        (angle: number) => {
-            return useDegrees ? angle / RadiansToDegrees : angle;
+        (angle: number, wrap = false) => {
+            angle = useDegrees ? angle / RadiansToDegrees : angle;
+            if (wrap) {
+                angle = WrapAngle(angle);
+            }
+            return angle;
         },
         [useDegrees]
     );
