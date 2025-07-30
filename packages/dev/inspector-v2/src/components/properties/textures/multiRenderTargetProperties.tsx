e37f32cd1085bfcde0c259af4457887cc3372978
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-07-30 14:22:16 -0700

    Use BoundProperty where possible

--- packages/dev/inspector-v2/src/components/properties/textures/multiRenderTargetProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/textures/multiRenderTargetProperties.tsx
@@ -3,16 +3,14 @@ import type { MultiRenderTarget } from "core/index";
 import type { FunctionComponent } from "react";
 
 import { StringifiedPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/stringifiedPropertyLine";
-import { useProperty } from "../../../hooks/compoundPropertyHooks";
+import { BoundProperty } from "../boundProperty";
 
 export const MultiRenderTargetGeneralProperties: FunctionComponent<{ texture: MultiRenderTarget }> = (props) => {
     const { texture } = props;
 
-    const count = useProperty(texture, "count");
-
     return (
         <>
-            <StringifiedPropertyLine label="Count" description="The number of render target textures." value={count} />
+            <BoundProperty component={StringifiedPropertyLine} label="Count" description="The number of render target textures." target={texture} propertyKey="count" />
         </>
     );
 };
