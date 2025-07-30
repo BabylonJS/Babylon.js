bb2d02faf563dec0d815cb587437df5ebb903aca
Georgina <gehalper@microsoft.com>
2025-07-29 20:29:09 -0400

    remove converter

--- packages/dev/sharedUiComponents/src/fluent/hoc/propertyLines/stringifiedPropertyLine.tsx
+++ packages/dev/sharedUiComponents/src/fluent/hoc/propertyLines/stringifiedPropertyLine.tsx
@@ -7,16 +7,16 @@ type StringifiedPropertyLineProps = PropertyLineProps<number> &
     ImmutablePrimitiveProps<number> & {
         precision?: number;
         units?: string;
-        converter?: (value: number) => string;
     };
 
 /**
  * Expects a numerical value and converts it to a fixed string
- * Can pass optional precision, units, and converter function (default uses value.toString())
+ * Can pass optional precision, units, and converter function (default uses value.toLocaleString())
  * @param props
  * @returns
  */
 export const StringifiedPropertyLine: FunctionComponent<StringifiedPropertyLineProps> = (props) => {
-    const value = props.precision !== undefined ? props.value.toFixed(props.precision) : props.value;
-    return <TextPropertyLine {...props} nullable={false} value={`${value.toString()} ${props.units}`} />;
+    const value = props.precision !== undefined ? props.value.toFixed(props.precision) : props.value.toLocaleString();
+    const withUnits = props.units ? `${value} ${props.units}` : value;
+    return <TextPropertyLine {...props} nullable={false} value={withUnits} />;
 };
