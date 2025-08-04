c229d21e62d0e67d9c741b59543da7aecfab5bb2
David Catuhe <david@catuhe.com>
2025-08-04 11:26:01 -0700

    Add animationGroup to inspector v2

--- packages/dev/inspector-v2/src/components/properties/animation/targetedAnimationProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/animation/targetedAnimationProperties.tsx
@@ -1,7 +1,20 @@
 import type { FunctionComponent } from "react";
 
 import type { TargetedAnimation } from "core/index";
+import { LinkPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/linkPropertyLine";
+import type { ISelectionService } from "../../../services/selectionService";
 
-export const TargetedAnimationGeneralProperties: FunctionComponent<{ targetedAnimation: TargetedAnimation }> = (props) => {
-    return <></>;
+export const TargetedAnimationGeneralProperties: FunctionComponent<{ targetedAnimation: TargetedAnimation; selectionService: ISelectionService }> = (props) => {
+    const { selectionService } = props;
+
+    return (
+        <>
+            <LinkPropertyLine
+                label="Target"
+                description={`The entity animated by this animation.`}
+                value={props.targetedAnimation.target.name}
+                onLink={() => (selectionService.selectedEntity = props.targetedAnimation.target)}
+            />
+        </>
+    );
 };
