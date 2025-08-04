9792af2cbb4ec343bc0f8348d3e69bc2e17f8d03
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2025-08-04 13:55:59 -0300

    Created first implementation for frame graph UI

--- packages/dev/inspector-v2/src/services/panes/properties/frameGraphPropertiesService.tsx
+++ packages/dev/inspector-v2/src/services/panes/properties/frameGraphPropertiesService.tsx
@@ -1,17 +1,41 @@
+import { FrameGraph } from "core/FrameGraph/frameGraph";
 import type { ServiceDefinition } from "../../../modularity/serviceDefinition";
 import type { IPropertiesService } from "./propertiesService";
 
 import { PropertiesServiceIdentity } from "./propertiesService";
+import { FrameGraphGeneralProperties } from "../../../components/properties/frameGraph/frameGraphGeneralProperties";
+import { FrameGraphTaskProperties } from "../../../components/properties/frameGraph/frameGraphTasksProperties";
 
 export const FrameGraphPropertiesServiceDefinition: ServiceDefinition<[], [IPropertiesService]> = {
     friendlyName: "Frame Graph Properties",
     consumes: [PropertiesServiceIdentity],
     factory: (propertiesService) => {
-        // TODO: Add content registrations for each section and for each type in the FrameGraph class hierarchy.
+        const frameGraphGeneralContentRegistration = propertiesService.addSectionContent({
+            key: "Frame Graph General Properties",
+            predicate: (entity: unknown) => entity instanceof FrameGraph,
+            content: [
+                {
+                    section: "General",
+                    component: ({ context }) => <FrameGraphGeneralProperties frameGraph={context} />,
+                },
+            ],
+        });
+
+        const frameGraphTasksContentRegistration = propertiesService.addSectionContent({
+            key: "Frame Graph Tasks Properties",
+            predicate: (entity: unknown) => entity instanceof FrameGraph,
+            content: [
+                {
+                    section: "Tasks",
+                    component: ({ context }) => <FrameGraphTaskProperties frameGraph={context} />,
+                },
+            ],
+        });
 
         return {
             dispose: () => {
-                // TODO: Dispose content registrations.
+                frameGraphGeneralContentRegistration.dispose();
+                frameGraphTasksContentRegistration.dispose();
             },
         };
     },
