d9a76c83b8dce8f635cc0749e5adc1451e719353
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-08-04 09:39:18 -0700

    Inspector v2: Stub out animation groups properties (#16950)

---
+++ packages/dev/inspector-v2/src/components/properties/animation/targetedAnimationProperties.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent } from "react";
+
+import type { TargetedAnimation } from "core/index";
+
+export const TargetedAnimationGeneralProperties: FunctionComponent<{ targetedAnimation: TargetedAnimation }> = (props) => {
+    return <></>;
+};
