939cae353ab596a14b585e16164885251fb04188
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-08-07 10:00:28 -0700

    Small fixes

--- packages/dev/inspector-v2/src/components/properties/materials/multiMaterialProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/materials/multiMaterialProperties.tsx
@@ -2,16 +2,19 @@ import type { FunctionComponent } from "react";
 
 import type { MultiMaterial } from "core/Materials/multiMaterial";
 import type { ISelectionService } from "../../../services/selectionService";
-import { LinkPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/linkPropertyLine";
+
+import { LinkToEntityPropertyLine } from "../linkToEntityPropertyLine";
 
 export const MultiMaterialChildrenProperties: FunctionComponent<{ multiMaterial: MultiMaterial; selectionService: ISelectionService }> = (props) => {
     const { multiMaterial, selectionService } = props;
 
     return (
         <>
-            {multiMaterial.subMaterials.map((material, index) => (
-                <LinkPropertyLine label={`Material #${index + 1}`} value={material!.name} onLink={() => (selectionService.selectedEntity = material)} />
-            ))}
+            {multiMaterial.subMaterials
+                .filter((material) => !!material)
+                .map((material, index) => (
+                    <LinkToEntityPropertyLine key={material.uniqueId} label={`Material #${index + 1}`} entity={material} selectionService={selectionService} />
+                ))}
         </>
     );
 };
