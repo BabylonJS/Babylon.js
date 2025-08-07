e14ef05040a06f21e21e4c9ad51a6bd4ed7f0e6e
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-08-07 08:40:07 -0700

    Remove unnecessary conditional components

--- packages/dev/inspector-v2/src/components/properties/materials/standardMaterialProperties.tsx
+++ packages/dev/inspector-v2/src/components/properties/materials/standardMaterialProperties.tsx
@@ -1,11 +1,13 @@
 import type { FunctionComponent } from "react";
 
-import { BoundProperty } from "../boundProperty";
 import type { StandardMaterial } from "core/Materials/standardMaterial";
-import { useProperty } from "../../../hooks/compoundPropertyHooks";
-import { SyncedSliderPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/syncedSliderPropertyLine";
+
 import { Color3PropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/colorPropertyLine";
 import { SwitchPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/switchPropertyLine";
+import { SyncedSliderPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/syncedSliderPropertyLine";
+import { Collapse } from "shared-ui-components/fluent/primitives/collapse";
+import { useProperty } from "../../../hooks/compoundPropertyHooks";
+import { BoundProperty } from "../boundProperty";
 
 /**
  * Displays the levels properties of a standard material.
@@ -15,60 +17,55 @@ import { SwitchPropertyLine } from "shared-ui-components/fluent/hoc/propertyLine
 export const StandardMaterialLevelsProperties: FunctionComponent<{ standardMaterial: StandardMaterial }> = (props) => {
     const { standardMaterial } = props;
 
-    const diffuseTexture = useProperty(standardMaterial, "diffuseTexture");
-    const specularTexture = useProperty(standardMaterial, "specularTexture");
-    const reflectionTexture = useProperty(standardMaterial, "reflectionTexture");
-    const refractionTexture = useProperty(standardMaterial, "refractionTexture");
-    const emissiveTexture = useProperty(standardMaterial, "emissiveTexture");
-    const bumpTexture = useProperty(standardMaterial, "bumpTexture");
-    const opacityTexture = useProperty(standardMaterial, "opacityTexture");
-    const ambientTexture = useProperty(standardMaterial, "ambientTexture");
-    const lightmapTexture = useProperty(standardMaterial, "lightmapTexture");
     const isDetailMapEnabled = useProperty(standardMaterial.detailMap, "isEnabled");
 
     return (
         <>
-            {diffuseTexture && <BoundProperty component={SyncedSliderPropertyLine} label="Diffuse Level" target={diffuseTexture} propertyKey="level" min={0} max={2} step={0.01} />}
-            {specularTexture && (
-                <BoundProperty component={SyncedSliderPropertyLine} label="Specular Level" target={specularTexture} propertyKey="level" min={0} max={2} step={0.01} />
-            )}
-            {reflectionTexture && (
-                <BoundProperty component={SyncedSliderPropertyLine} label="Reflection Level" target={reflectionTexture} propertyKey="level" min={0} max={2} step={0.01} />
-            )}
-            {refractionTexture && (
-                <BoundProperty component={SyncedSliderPropertyLine} label="Refraction Level" target={refractionTexture} propertyKey="level" min={0} max={2} step={0.01} />
-            )}
-            {emissiveTexture && (
-                <BoundProperty component={SyncedSliderPropertyLine} label="Emissive Level" target={emissiveTexture} propertyKey="level" min={0} max={2} step={0.01} />
-            )}
-            {bumpTexture && <BoundProperty component={SyncedSliderPropertyLine} label="Bump Level" target={bumpTexture} propertyKey="level" min={0} max={2} step={0.01} />}
-            {opacityTexture && <BoundProperty component={SyncedSliderPropertyLine} label="Opacity Level" target={opacityTexture} propertyKey="level" min={0} max={2} step={0.01} />}
-            {ambientTexture && <BoundProperty component={SyncedSliderPropertyLine} label="Ambient Level" target={ambientTexture} propertyKey="level" min={0} max={2} step={0.01} />}
-            {lightmapTexture && (
-                <BoundProperty component={SyncedSliderPropertyLine} label="Lightmap Level" target={lightmapTexture} propertyKey="level" min={0} max={2} step={0.01} />
-            )}
-            {isDetailMapEnabled && (
-                <>
-                    <BoundProperty
-                        component={SyncedSliderPropertyLine}
-                        label="Detailmap Diffuse"
-                        target={standardMaterial.detailMap}
-                        propertyKey="diffuseBlendLevel"
-                        min={0}
-                        max={1}
-                        step={0.01}
-                    />
-                    <BoundProperty
-                        component={SyncedSliderPropertyLine}
-                        label="Detailmap Bump"
-                        target={standardMaterial.detailMap}
-                        propertyKey="bumpLevel"
-                        min={0}
-                        max={1}
-                        step={0.01}
-                    />
-                </>
-            )}
+            <BoundProperty component={SyncedSliderPropertyLine} label="Diffuse Level" target={standardMaterial.diffuseTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <BoundProperty component={SyncedSliderPropertyLine} label="Specular Level" target={standardMaterial.specularTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <BoundProperty
+                component={SyncedSliderPropertyLine}
+                label="Reflection Level"
+                target={standardMaterial.reflectionTexture}
+                propertyKey="level"
+                min={0}
+                max={2}
+                step={0.01}
+            />
+            <BoundProperty
+                component={SyncedSliderPropertyLine}
+                label="Refraction Level"
+                target={standardMaterial.refractionTexture}
+                propertyKey="level"
+                min={0}
+                max={2}
+                step={0.01}
+            />
+            <BoundProperty component={SyncedSliderPropertyLine} label="Emissive Level" target={standardMaterial.emissiveTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <BoundProperty component={SyncedSliderPropertyLine} label="Bump Level" target={standardMaterial.bumpTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <BoundProperty component={SyncedSliderPropertyLine} label="Opacity Level" target={standardMaterial.opacityTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <BoundProperty component={SyncedSliderPropertyLine} label="Ambient Level" target={standardMaterial.ambientTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <BoundProperty component={SyncedSliderPropertyLine} label="Lightmap Level" target={standardMaterial.lightmapTexture} propertyKey="level" min={0} max={2} step={0.01} />
+            <Collapse visible={isDetailMapEnabled}>
+                <BoundProperty
+                    component={SyncedSliderPropertyLine}
+                    label="Detailmap Diffuse"
+                    target={standardMaterial.detailMap}
+                    propertyKey="diffuseBlendLevel"
+                    min={0}
+                    max={1}
+                    step={0.01}
+                />
+                <BoundProperty
+                    component={SyncedSliderPropertyLine}
+                    label="Detailmap Bump"
+                    target={standardMaterial.detailMap}
+                    propertyKey="bumpLevel"
+                    min={0}
+                    max={1}
+                    step={0.01}
+                />
+            </Collapse>
         </>
     );
 };
@@ -101,14 +98,12 @@ export const StandardMaterialLightingAndColorProperties: FunctionComponent<{ sta
 export const StandardMaterialTexturesProperties: FunctionComponent<{ standardMaterial: StandardMaterial }> = (props) => {
     const { standardMaterial } = props;
 
-    const decalMap = useProperty(standardMaterial, "decalMap");
-
     // TODO: Add buttons and links for adding the textures themselves
     return (
         <>
             <BoundProperty component={SwitchPropertyLine} label="Use Lightmap as Shadowmap" target={standardMaterial} propertyKey="useLightmapAsShadowmap" />
             <BoundProperty component={SwitchPropertyLine} label="Use Detailmap" target={standardMaterial.detailMap} propertyKey="isEnabled" />
-            {decalMap && <BoundProperty component={SwitchPropertyLine} label="Use Decalmap" target={decalMap} propertyKey="isEnabled" />}
+            <BoundProperty component={SwitchPropertyLine} label="Use Decalmap" target={standardMaterial.decalMap} propertyKey="isEnabled" />
         </>
     );
 };
