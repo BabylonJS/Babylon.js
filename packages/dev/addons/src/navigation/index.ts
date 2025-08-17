89f9a5d889a78df2e0841bfbd84b7743b457e78b
Roland Csibrei <roland.csibrei@hqo.co>
2025-08-17 10:00:50 +0200

    fixed export paths

--- packages/dev/addons/src/navigation/index.ts
+++ packages/dev/addons/src/navigation/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-restricted-imports */
-export * from "./plugin";
-export * from "./factory";
-export * from "./common";
-export * from "./generator";
-export { NavigationDebugger } from "./debug";
+export * from "./plugin/index";
+export * from "./factory/index";
+export * from "./common/index";
+export * from "./generator/index";
+export { NavigationDebugger } from "./debug/index";
 
 export * as Recast from "recast-navigation";
