10c0a30d5ff549a6969ff92202388b4bcd0e9ac7
Michael Bond <mbond@adobe.com>
2025-08-21 12:40:05 -0700

    possible fix

--- packages/dev/core/src/Materials/imageProcessing.ts
+++ packages/dev/core/src/Materials/imageProcessing.ts
@@ -22,8 +22,7 @@ export function ImageProcessingMixin<Tbase extends ImageProcessingMixinConstruct
             super(...args);
             // Decorators don't work on this annonymous class
             // so I'm setting this up manually.
-            const fn = serializeAsImageProcessingConfiguration();
-            fn.call(this, this, "_imageProcessingConfiguration");
+            serializeAsImageProcessingConfiguration.call(this, this, "_imageProcessingConfiguration");
         }
         /**
          * Default configuration related to image processing available in the standard Material.
