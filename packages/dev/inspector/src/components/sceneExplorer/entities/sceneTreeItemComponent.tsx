8a132d9686ed0dd9fb5b771a0206a6547dfc9e88
Amir Tadros <amirt@meyersound.com>
2025-08-22 09:15:09 -0700

    update inspector observer types

--- packages/dev/inspector/src/components/sceneExplorer/entities/sceneTreeItemComponent.tsx
+++ packages/dev/inspector/src/components/sceneExplorer/entities/sceneTreeItemComponent.tsx
@@ -23,6 +23,7 @@ import type { Bone } from "core/Bones/bone";
 
 import { setDebugNode } from "../treeNodeDebugger";
 import { FrameGraphUtils } from "core/FrameGraph/frameGraphUtils";
+import type { PointerDragBehavior } from "core/Behaviors";
 
 interface ISceneTreeItemComponentProps {
     scene: Scene;
@@ -43,9 +44,9 @@ export class SceneTreeItemComponent extends React.Component<
     private _onSelectionChangeObserver: Nullable<Observer<any>>;
     private _selectedEntity: any;
 
-    private _posDragEnd: Nullable<Observer<PropertyChangedEvent>> = null;
-    private _scaleDragEnd: Nullable<Observer<PropertyChangedEvent>> = null;
-    private _rotateDragEnd: Nullable<Observer<PropertyChangedEvent>> = null;
+    private _posDragEnd: Nullable<ReturnType<PointerDragBehavior["onDragEndObservable"]["add"]>> = null;
+    private _scaleDragEnd: Nullable<ReturnType<PointerDragBehavior["onDragEndObservable"]["add"]>> = null;
+    private _rotateDragEnd: Nullable<ReturnType<PointerDragBehavior["onDragEndObservable"]["add"]>> = null;
 
     constructor(props: ISceneTreeItemComponentProps) {
         super(props);
