b4d3adab93b75f3cf0e50b7c19e8681c4b3adc0a
Vicente Cartas <vicente.cartas@outlook.com>
2025-08-25 15:05:47 -0700

    Adding more lottie support

--- packages/tools/devHost/src/lottie/main.ts
+++ packages/tools/devHost/src/lottie/main.ts
@@ -1,8 +1,33 @@
-import { Player } from "lottie/Player";
+import type { AnimationConfiguration } from "lottie-player/animationConfiguration";
+import { LocalPlayer } from "lottie-player/LocalPlayer";
 
 /** Main entry point for the default scene for lottie-player */
-export function Main(): void {
-    const div = document.getElementById("wrapper") as HTMLDivElement; // Get the canvas element
-    const player = new Player(div, "./a1-fre.json", new Map<string, string>(), { loopAnimation: true });
-    player.playAnimation();
+export async function Main(): Promise<void> {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // You can also pass a local file that you are serving from the devhost public folder to test: const fileUrl = './myLottieFile.json'
+    const filename = searchParams.get("file") || "triangles_noParents_noCross.json";
+    const fileUrl = `https://assets.babylonjs.com/lottie/'${filename}'`;
+
+    // These are variables that will be replaced in the lottie file if it contains them. You can replace with them text strings and text fill colors
+    const variables = new Map<string, string>();
+
+    // This is the configuration for the player, you can pass as much or as little as you want, the rest will be defaulted
+    const configuration: AnimationConfiguration = {
+        loopAnimation: false, // By default do not loop animations
+        spriteAtlasWidth: 4096, // Size of the texture atlas
+        spriteAtlasHeight: 4096, // Size of the texture atlas
+        gapSize: 5, // Gap around the sprites in the atlas
+        spritesCapacity: 64, // Maximum number of sprites the renderer can handle at once
+        backgroundColor: { r: 1, g: 1, b: 1, a: 1 }, // Background color for the animation canvas
+        scaleMultiplier: 5, // Minimum scale factor to prevent too small sprites,
+        devicePixelRatio: 1, // Scale factor,
+        easingSteps: 4, // Number of steps to sample easing functions for animations - Less than 4 causes issues with some interpolations
+        ignoreOpacityAnimations: true, // Whether to ignore opacity animations for performance
+        supportDeviceLost: false, // Whether to support device lost events for WebGL contexts,
+    };
+
+    const div = document.getElementById("main-div") as HTMLDivElement; // Get the canvas element
+    const player = new LocalPlayer(div, fileUrl, variables, configuration);
+    await player.playAnimationAsync();
 }
