2c060d8be13f70c48ea05addda1b0914e5062fd1
Vicente Cartas <vicente.cartas@outlook.com>
2025-08-27 18:27:07 -0700

    Adding support for the player to take a json object (#17078)
    The current API of the player only accepted the URL of a file to load.
    This change also adds supporting a JSON object as the input instead of
    the URL, as in certain scenarios the object may have already been
    parsed, and we don't need to parse it ourselves.
    
    Also added support for the devHost to test this scenario with the QSP
    useUrl=boolean. This QSP will tell the devhost to either pass the URL to
    the player or to download the file and parse the content and pass the
    json instead. Default is using the URL.

--- packages/dev/lottiePlayer/src/worker.ts
+++ packages/dev/lottiePlayer/src/worker.ts
@@ -2,14 +2,12 @@
 import type { Nullable } from "core/types";
 import type { AnimationConfiguration } from "./animationConfiguration";
 import type { RawLottieAnimation } from "./parsing/rawTypes";
+import type { AnimationController } from "./rendering/animationController";
 import type { Message, AnimationSizeMessage, AnimationUrlMessagePayload, StartAnimationMessagePayload, ContainerResizeMessagePayload } from "./messageTypes";
 
 let RawAnimation: Nullable<RawLottieAnimation> = null;
-// Use any type to avoid importing the class at module scope
-// eslint-disable @typescript-eslint/no-explicit-any
-let Controller: Nullable<any> = null;
+let Controller: Nullable<AnimationController> = null;
 let AnimationPromises: any = null;
-// eslint-enable @typescript-eslint/no-explicit-any
 
 onmessage = async function (evt) {
     const message = evt.data as Message;
@@ -38,13 +36,13 @@ onmessage = async function (evt) {
             break;
         }
         case "startAnimation": {
-            if (RawAnimation === null) {
-                return;
+            if (AnimationPromises === null) {
+                AnimationPromises = Promise.all([import("./animationConfiguration"), import("./rendering/animationController")]);
             }
 
-            const payload = message.payload as StartAnimationMessagePayload;
             const [{ DefaultConfiguration }, { AnimationController }] = await AnimationPromises;
 
+            const payload = message.payload as StartAnimationMessagePayload;
             const canvas = payload.canvas;
             const scaleFactor = payload.scaleFactor;
             const variables = payload.variables ?? new Map<string, string>();
@@ -54,8 +52,17 @@ onmessage = async function (evt) {
                 ...originalConfig,
             };
 
-            Controller = new AnimationController(canvas, RawAnimation, scaleFactor, variables, finalConfig);
-            Controller.playAnimation();
+            if (RawAnimation === null && payload.animationData) {
+                RawAnimation = payload.animationData;
+            }
+
+            if (RawAnimation === null) {
+                return;
+            }
+
+            const controller = new AnimationController(canvas, RawAnimation, scaleFactor, variables, finalConfig);
+            controller.playAnimation();
+            Controller = controller;
             break;
         }
         case "containerResize": {
