BabylonJS/Babylon.js

commit 3dc355967856da60fe1ab54d05118b2fa1ea1084

WebGPU: fix fast snapshot rendering mode + add support for it to frame graphs (#17064)

packages/dev/core/src/Rendering/objectRenderer.ts
@@ -1,4 +1,17 @@
-import type { Nullable, Immutable, Camera, Scene, AbstractMesh, SubMesh, Material, IParticleSystem, InstancedMesh, BoundingBox, BoundingBoxRenderer } from "core/index";
+import type {
+    Nullable,
+    Immutable,
+    Camera,
+    Scene,
+    AbstractMesh,
+    SubMesh,
+    Material,
+    IParticleSystem,
+    InstancedMesh,
+    BoundingBox,
+    BoundingBoxRenderer,
+    UniformBuffer,
+} from "core/index";
 import { Observable } from "../Misc/observable";
 import { RenderingManager } from "../Rendering/renderingManager";
 import { Constants } from "../Engines/constants";
@@ -206,6 +219,9 @@ export class ObjectRenderer {
     protected _currentApplyByPostProcessSetting = false;
     protected _activeMeshes = new SmartArray<AbstractMesh>(256);
     protected _activeBoundingBoxes = new SmartArray<BoundingBox>(32);
+    protected _useUBO: boolean;
+    protected _sceneUBO: UniformBuffer;
+    protected _currentSceneUBO: UniformBuffer;
 
     /**
      * The options used by the object renderer
@@ -226,6 +242,9 @@ export class ObjectRenderer {
         }
 
         this._name = value;
+        if (this._sceneUBO) {
+            this._sceneUBO.name = `Scene ubo for ${this.name}`;
+        }
 
         if (!this._scene) {
             return;
@@ -350,6 +369,10 @@ export class ObjectRenderer {
     constructor(name: string, scene: Scene, options?: ObjectRendererOptions) {
         this.name = name;
         this._scene = scene;
+        this._useUBO = this._scene.getEngine().supportsUniformBuffers;
+        if (this._useUBO) {
+            this._sceneUBO = this._scene.createSceneUniformBuffer(`Scene ubo for ${this.name}`, false);
+        }
 
         this.renderList = [] as AbstractMesh[];
         this._renderPassIds = [];
@@ -509,12 +532,20 @@ export class ObjectRenderer {
 
         this._currentSceneCamera = this._scene.activeCamera;
 
+        if (this._useUBO) {
+            this._currentSceneUBO = this._scene.getSceneUniformBuffer();
+            this._currentSceneUBO.unbindEffect();
+            this._scene.setSceneUniformBuffer(this._sceneUBO);
+        }
+
         if (camera) {
-            if (camera !== this._scene.activeCamera) {
-                this._scene.setTransformMatrix(camera.getViewMatrix(), camera.getProjectionMatrix(true));
-                this._scene.activeCamera = camera;
-            }
+            this._scene.setTransformMatrix(camera.getViewMatrix(), camera.getProjectionMatrix(true));
+            this._scene.activeCamera = camera;
             engine.setViewport(camera.rigParent ? camera.rigParent.viewport : camera.viewport, viewportWidth, viewportHeight);
+        }
+
+        if (this._useUBO) {
+            this._scene.finalizeSceneUbo();
         }
 
         this._defaultRenderListPrepared = false;
@@ -525,6 +556,10 @@ export class ObjectRenderer {
      */
     public finishRender() {
         const scene = this._scene;
+
+        if (this._useUBO) {
+            this._scene.setSceneUniformBuffer(this._currentSceneUBO);
+        }
 
         if (this._disableImageProcessing) {
             scene.imageProcessingConfiguration._applyByPostProcess = this._currentApplyByPostProcessSetting;
@@ -896,6 +931,8 @@ export class ObjectRenderer {
         this._releaseRenderPassId();
 
         this.renderList = null;
+        this._sceneUBO?.dispose();
+        this._sceneUBO = undefined as any;
 
         this._scene.removeObjectRenderer(this);
     }

packages/dev/core/src/Engines/Extensions/engine.multiview.ts
@@ -183,8 +183,8 @@ declare module "../../scene" {
     }
 }
 
-function CreateMultiviewUbo(engine: AbstractEngine, name?: string) {
-    const ubo = new UniformBuffer(engine, undefined, true, name);
+function CreateMultiviewUbo(engine: AbstractEngine, name?: string, trackUBOsInFrame?: boolean) {
+    const ubo = new UniformBuffer(engine, undefined, true, name, undefined, trackUBOsInFrame);
     ubo.addUniform("viewProjection", 16);
     ubo.addUniform("viewProjectionR", 16);
     ubo.addUniform("view", 16);
@@ -200,11 +200,11 @@ Scene.prototype._multiviewSceneUbo = null;
 Scene.prototype._createMultiviewUbo = function () {
     this._multiviewSceneUbo = CreateMultiviewUbo(this.getEngine(), "scene_multiview");
 };
-Scene.prototype.createSceneUniformBuffer = function (name?: string): UniformBuffer {
+Scene.prototype.createSceneUniformBuffer = function (name?: string, trackUBOsInFrame?: boolean): UniformBuffer {
     if (this._multiviewSceneUbo) {
-        return CreateMultiviewUbo(this.getEngine(), name);
+        return CreateMultiviewUbo(this.getEngine(), name, trackUBOsInFrame);
     }
-    return CurrentCreateSceneUniformBuffer.bind(this)(name);
+    return CurrentCreateSceneUniformBuffer.bind(this)(name, trackUBOsInFrame);
 };
 Scene.prototype._updateMultiviewUbo = function (viewR?: Matrix, projectionR?: Matrix) {
     if (viewR && projectionR) {

packages/dev/core/src/Engines/webgpuEngine.ts
@@ -3444,6 +3444,11 @@ export class WebGPUEngine extends ThinWebGPUEngine {
         }
 
         // We don't create the render pass just now, we do a lazy creation of the render pass, hoping the render pass will be created by a call to clear()...
+        // However, if snapshot rendering is enabled, we need to create the render pass immediately, to be sure currentRenderPass is not null when _endCurrentRenderPass() is called.
+        // (as in snapshot rendering mode, we may not have a call to clear() before _endCurrentRenderPass(), so lazy creation would not work)
+        if (this._snapshotRendering.play || this._snapshotRendering.record) {
+            this._startRenderTargetRenderPass(this._currentRenderTarget, false, null, false, false);
+        }
 
         if (this._cachedViewport && !forceFullscreenViewport) {
             this.setViewport(this._cachedViewport, requiredWidth, requiredHeight);

packages/dev/core/src/FrameGraph/Tasks/Layers/baseLayerTask.ts
@@ -109,6 +109,13 @@ export class FrameGraphBaseLayerTask extends FrameGraphTask {
         }
     }
 
+    /**
+     * Gets the object renderer used to render the layer.
+     */
+    public get objectRendererForLayer() {
+        return this._objectRendererForLayer;
+    }
+
     protected readonly _scene: Scene;
     protected readonly _engine: AbstractEngine;
     protected readonly _clearLayerTextures: FrameGraphClearTextureTask;

packages/dev/core/src/Lights/Shadows/cascadedShadowGenerator.ts
@@ -804,6 +804,7 @@ export class CascadedShadowGenerator extends ShadowGenerator {
 
     protected override _createTargetRenderTexture(): void {
         const engine = this._scene.getEngine();
+        this._shadowMap?.dispose();
         const size = { width: this._mapSize, height: this._mapSize, layers: this.numCascades };
         this._shadowMap = new RenderTargetTexture(
             this._light.name + "_CSMShadowMap",

packages/dev/core/src/Lights/Shadows/shadowGenerator.ts
@@ -951,6 +951,7 @@ export class ShadowGenerator implements IShadowGenerator {
 
     protected _createTargetRenderTexture(): void {
         const engine = this._scene.getEngine();
+        this._shadowMap?.dispose();
         if (engine._features.supportDepthStencilTexture) {
             this._shadowMap = new RenderTargetTexture(
                 this._light.name + "_shadowMap",

packages/dev/core/src/Materials/uniformBuffer.ts
@@ -40,6 +40,7 @@ export class UniformBuffer {
     private _currentEffectName: string;
     private _name: string;
     private _currentFrameId: number;
+    private _trackUBOsInFrame: boolean;
 
     // Pool for avoiding memory leaks
     private static _MAX_UNIFORM_SIZE = 256;
@@ -236,8 +237,9 @@ export class UniformBuffer {
      * @param dynamic Define if the buffer is updatable
      * @param name to assign to the buffer (debugging purpose)
      * @param forceNoUniformBuffer define that this object must not rely on UBO objects
+     * @param trackUBOsInFrame define if the UBOs should be tracked in the frame (default: undefined - will use the value from Engine._features.trackUbosInFrame)
      */
-    constructor(engine: AbstractEngine, data?: number[], dynamic = false, name?: string, forceNoUniformBuffer = false) {
+    constructor(engine: AbstractEngine, data?: number[], dynamic = false, name?: string, forceNoUniformBuffer = false, trackUBOsInFrame?: boolean) {
         this._engine = engine;
         this._noUBO = !engine.supportsUniformBuffers || forceNoUniformBuffer;
         this._dynamic = dynamic;
@@ -250,12 +252,14 @@ export class UniformBuffer {
         this._uniformArraySizes = {};
         this._uniformLocationPointer = 0;
         this._needSync = false;
+        this._trackUBOsInFrame = false;
 
-        if (this._engine._features.trackUbosInFrame) {
+        if ((trackUBOsInFrame === undefined && this._engine._features.trackUbosInFrame) || trackUBOsInFrame === true) {
             this._buffers = [];
             this._bufferIndex = -1;
             this._createBufferOnWrite = false;
             this._currentFrameId = 0;
+            this._trackUBOsInFrame = true;
         }
 
         if (this._noUBO) {
@@ -587,7 +591,7 @@ export class UniformBuffer {
             this._buffer = this._engine.createUniformBuffer(this._bufferData, this._name + "_UniformList:" + this._getNames());
         }
 
-        if (this._engine._features.trackUbosInFrame) {
+        if (this._trackUBOsInFrame) {
             this._buffers.push([this._buffer, this._engine._features.checkUbosContentBeforeUpload ? this._bufferData.slice() : undefined]);
             this._bufferIndex = this._buffers.length - 1;
             this._createBufferOnWrite = false;
@@ -596,7 +600,7 @@ export class UniformBuffer {
 
     /** @internal */
     public _rebuildAfterContextLost(): void {
-        if (this._engine._features.trackUbosInFrame) {
+        if (this._trackUBOsInFrame) {
             this._buffers = [];
             this._currentFrameId = 0;
         }
@@ -613,9 +617,13 @@ export class UniformBuffer {
         return this._bufferIndex;
     }
 
-    /** Gets the name of this buffer */
+    /** Gets or sets the name of this buffer */
     public get name(): string {
         return this._name;
+    }
+
+    public set name(value: string) {
+        this._name = value;
     }
 
     /** Gets the current effect */
@@ -656,14 +664,14 @@ export class UniformBuffer {
         }
 
         if (!this._dynamic && !this._needSync) {
-            this._createBufferOnWrite = this._engine._features.trackUbosInFrame;
+            this._createBufferOnWrite = this._trackUBOsInFrame;
             return;
         }
 
         if (this._buffers && this._buffers.length > 1 && this._buffers[this._bufferIndex][1]) {
             if (this._buffersEqual(this._bufferData, this._buffers[this._bufferIndex][1]!)) {
                 this._needSync = false;
-                this._createBufferOnWrite = this._engine._features.trackUbosInFrame;
+                this._createBufferOnWrite = this._trackUBOsInFrame;
                 return;
             } else {
                 this._copyBuffer(this._bufferData, this._buffers[this._bufferIndex][1]!);
@@ -680,7 +688,7 @@ export class UniformBuffer {
         }
 
         this._needSync = false;
-        this._createBufferOnWrite = this._engine._features.trackUbosInFrame;
+        this._createBufferOnWrite = this._trackUBOsInFrame;
     }
 
     private _createNewBuffer() {
@@ -695,7 +703,7 @@ export class UniformBuffer {
     }
 
     private _checkNewFrame(): void {
-        if (this._engine._features.trackUbosInFrame && this._currentFrameId !== this._engine.frameId) {
+        if (this._trackUBOsInFrame && this._currentFrameId !== this._engine.frameId) {
             this._currentFrameId = this._engine.frameId;
             this._createBufferOnWrite = false;
             if (this._buffers && this._buffers.length > 0) {
@@ -1230,7 +1238,7 @@ export class UniformBuffer {
             uniformBuffers.pop();
         }
 
-        if (this._engine._features.trackUbosInFrame && this._buffers) {
+        if (this._trackUBOsInFrame && this._buffers) {
             for (let i = 0; i < this._buffers.length; ++i) {
                 const buffer = this._buffers[i][0];
                 this._engine._releaseBuffer(buffer);

packages/dev/core/src/Misc/snapshotRenderingHelper.ts
@@ -16,6 +16,7 @@ import { Constants } from "core/Engines/constants";
 import { BindMorphTargetParameters } from "core/Materials/materialHelper.functions";
 import { ScenePerformancePriority } from "core/scene";
 import { Logger } from "core/Misc/logger";
+import { FrameGraphBaseLayerTask } from "../FrameGraph/Tasks/Layers/baseLayerTask";
 
 /**
  * Options for the snapshot rendering helper
@@ -314,15 +315,15 @@ export class SnapshotRenderingHelper {
 
     /**
      * Update the meshes used in an effect layer to ensure that snapshot rendering works correctly for these meshes in this layer.
-     * @param effectLayer The effect layer
-     * @param autoUpdate If true, the helper will automatically update the effect layer meshes with each frame. If false, you'll need to call this method manually when the camera or layer meshes move or rotate.
+     * @param layer The effect layer or frame graph layer
+     * @param autoUpdate If true, the helper will automatically update the meshes of the layer with each frame. If false, you'll need to call this method manually when the camera or layer meshes move or rotate.
      */
-    public updateMeshesForEffectLayer(effectLayer: EffectLayer, autoUpdate = true) {
+    public updateMeshesForEffectLayer(layer: EffectLayer | FrameGraphBaseLayerTask, autoUpdate = true) {
         if (!this._engine.isWebGPU) {
             return;
         }
 
-        const renderPassId = effectLayer.mainTexture.renderPassId;
+        const renderPassId = layer instanceof FrameGraphBaseLayerTask ? layer.objectRendererForLayer.objectRenderer.renderPassId : layer.mainTexture.renderPassId;
 
         if (autoUpdate) {
             this._onBeforeRenderObserverUpdateLayer = this._scene.onBeforeRenderObservable.add(() => {
@@ -355,7 +356,8 @@ export class SnapshotRenderingHelper {
             return;
         }
 
-        const sceneTransformationMatrix = this._scene.getTransformMatrix();
+        const sceneTransformationMatrix =
+            this._scene.objectRenderers.find((renderer) => renderer.renderPassId === renderPassId)?.activeCamera?.getTransformationMatrix() ?? this._scene.getTransformMatrix();
 
         for (let i = 0; i < this._scene.meshes.length; ++i) {
             const mesh = this._scene.meshes[i];

commit dfa0906f1b50873c0c945e85712ce23496878128

FrameGraph: Fix TAA (broken by #17064) (#17091)
I have also added a visual test for TAA to detect these errors in the
future.

packages/dev/core/src/FrameGraph/Node/Blocks/Rendering/objectRendererBlock.ts
@@ -35,6 +35,10 @@ export class NodeRenderGraphObjectRendererBlock extends NodeRenderGraphBaseObjec
         const depthWrite = this.depthWrite;
         const disableShadows = this.disableShadows;
         const renderInLinearSpace = this.renderInLinearSpace;
+        const renderParticles = this.renderParticles;
+        const renderSprites = this.renderSprites;
+        const forceLayerMaskCheck = this.forceLayerMaskCheck;
+        const enableBoundingBoxRendering = this.enableBoundingBoxRendering;
 
         this._frameGraphTask.dispose();
         this._frameGraphTask = new FrameGraphObjectRendererTask(this.name, this._frameGraph, this._scene, { doNotChangeAspectRatio: value });
@@ -44,6 +48,10 @@ export class NodeRenderGraphObjectRendererBlock extends NodeRenderGraphBaseObjec
         this.depthWrite = depthWrite;
         this.disableShadows = disableShadows;
         this.renderInLinearSpace = renderInLinearSpace;
+        this.renderParticles = renderParticles;
+        this.renderSprites = renderSprites;
+        this.forceLayerMaskCheck = forceLayerMaskCheck;
+        this.enableBoundingBoxRendering = enableBoundingBoxRendering;
         this._frameGraphTask.disabled = disabled;
     }
 

packages/dev/core/src/FrameGraph/Node/Blocks/Rendering/taaObjectRendererBlock.ts
@@ -40,11 +40,27 @@ export class NodeRenderGraphTAAObjectRendererBlock extends NodeRenderGraphBaseOb
 
     public set doNotChangeAspectRatio(value: boolean) {
         const disabled = this._frameGraphTask.disabled;
+        const depthTest = this.depthTest;
+        const depthWrite = this.depthWrite;
+        const disableShadows = this.disableShadows;
+        const renderInLinearSpace = this.renderInLinearSpace;
+        const renderParticles = this.renderParticles;
+        const renderSprites = this.renderSprites;
+        const forceLayerMaskCheck = this.forceLayerMaskCheck;
+        const enableBoundingBoxRendering = this.enableBoundingBoxRendering;
 
         this._frameGraphTask.dispose();
         this._frameGraphTask = new FrameGraphTAAObjectRendererTask(this.name, this._frameGraph, this._scene, { doNotChangeAspectRatio: value });
         this._additionalConstructionParameters = [value];
 
+        this.depthTest = depthTest;
+        this.depthWrite = depthWrite;
+        this.disableShadows = disableShadows;
+        this.renderInLinearSpace = renderInLinearSpace;
+        this.renderParticles = renderParticles;
+        this.renderSprites = renderSprites;
+        this.forceLayerMaskCheck = forceLayerMaskCheck;
+        this.enableBoundingBoxRendering = enableBoundingBoxRendering;
         this._frameGraphTask.disabled = disabled;
     }
 

packages/dev/core/src/FrameGraph/Node/Blocks/inputBlock.ts
@@ -92,6 +92,7 @@ export class NodeRenderGraphInputBlock extends NodeRenderGraphBlock {
                         useSRGBBuffers: [false],
                     },
                     sizeIsPercentage: true,
+                    isHistoryTexture: false,
                 };
                 this.creationOptions = options;
                 break;
@@ -108,6 +109,7 @@ export class NodeRenderGraphInputBlock extends NodeRenderGraphBlock {
                         samples: 1,
                     },
                     sizeIsPercentage: true,
+                    isHistoryTexture: false,
                 };
                 this.creationOptions = options;
                 break;

packages/dev/core/src/FrameGraph/Tasks/Rendering/taaObjectRendererTask.ts
@@ -1,4 +1,14 @@
-import type { FrameGraph, Scene, DrawWrapper, FrameGraphTextureCreationOptions, ObjectRendererOptions, FrameGraphRenderTarget, FrameGraphRenderPass } from "core/index";
+import type {
+    FrameGraph,
+    Scene,
+    DrawWrapper,
+    FrameGraphTextureCreationOptions,
+    ObjectRendererOptions,
+    FrameGraphRenderTarget,
+    FrameGraphRenderPass,
+    Observer,
+    ObjectRenderer,
+} from "core/index";
 import { backbufferColorTextureHandle, backbufferDepthStencilTextureHandle } from "../../frameGraphTypes";
 import { FrameGraphObjectRendererTask } from "./objectRendererTask";
 import { ThinTAAPostProcess } from "core/PostProcesses/thinTAAPostProcess";
@@ -14,6 +24,7 @@ export class FrameGraphTAAObjectRendererTask extends FrameGraphObjectRendererTas
     public readonly postProcess: ThinTAAPostProcess;
 
     protected readonly _postProcessDrawWrapper: DrawWrapper;
+    protected _initRenderingObserver: Observer<ObjectRenderer>;
 
     /**
      * Constructs a new TAA object renderer task.
@@ -27,6 +38,7 @@ export class FrameGraphTAAObjectRendererTask extends FrameGraphObjectRendererTas
 
         this.postProcess = new ThinTAAPostProcess(`${name} post-process`, scene.getEngine());
         this._postProcessDrawWrapper = this.postProcess.drawWrapper;
+        this._renderer.dontSetTransformationMatrix = true;
     }
 
     public override record(): FrameGraphRenderPass {
@@ -90,6 +102,13 @@ export class FrameGraphTAAObjectRendererTask extends FrameGraphObjectRendererTas
 
         this._setLightsForShadow();
 
+        this._renderer.onInitRenderingObservable.remove(this._initRenderingObserver);
+
+        this._initRenderingObserver = this._renderer.onInitRenderingObservable.add(() => {
+            // We pass false to this.camera.getProjectionMatrix() when TAA is enabled to avoid overwriting the projection matrix calculated by the call to this.postProcess.updateProjectionMatrix()
+            this._scene.setTransformMatrix(this.camera.getViewMatrix(), this.camera.getProjectionMatrix(this.postProcess.disabled));
+        });
+
         const pass = this._frameGraph.addRenderPass(this.name);
 
         pass.setRenderTarget(this.targetTexture);
@@ -97,18 +116,10 @@ export class FrameGraphTAAObjectRendererTask extends FrameGraphObjectRendererTas
         pass.setExecuteFunc((context) => {
             this._renderer.renderList = this.objectList.meshes;
             this._renderer.particleSystemList = this.objectList.particleSystems;
-            this._renderer.disableImageProcessing = this.disableImageProcessing;
 
             this.postProcess.updateProjectionMatrix();
 
             context.setDepthStates(this.depthTest && depthEnabled, this.depthWrite && depthEnabled);
-
-            // We define the active camera and transformation matrices ourselves, otherwise this will be done by calling context.render, in which case
-            // getProjectionMatrix will be called with a "true" parameter, forcing recalculation of the projection matrix and losing our changes.
-            if (!this.postProcess.disabled) {
-                this._scene.activeCamera = this.camera;
-                this._scene.setTransformMatrix(this.camera.getViewMatrix(), this.camera.getProjectionMatrix());
-            }
 
             context.render(this._renderer, this._textureWidth, this._textureHeight);
 
@@ -138,5 +149,10 @@ export class FrameGraphTAAObjectRendererTask extends FrameGraphObjectRendererTas
         });
 
         return pass;
+    }
+
+    public override dispose() {
+        this._renderer.onInitRenderingObservable.remove(this._initRenderingObserver);
+        super.dispose();
     }
 }

packages/dev/core/src/FrameGraph/frameGraphTextureManager.ts
@@ -462,6 +462,7 @@ export class FrameGraphTextureManager {
                     const refEntry = this._textures.get(entry.refHandle)!;
 
                     entry.texture = refEntry.texture;
+                    entry.texture?.incrementReferences();
 
                     if (refEntry.refHandle === backbufferColorTextureHandle) {
                         entry.refHandle = backbufferColorTextureHandle;
@@ -544,6 +545,9 @@ export class FrameGraphTextureManager {
                 return;
             }
 
+            // We dispose of "Graph" and "Task" textures:
+            // - "Graph" textures will be recreated by _allocateTextures because the entry still exists in this._textures, but entry.texture is null
+            // - "Task" textures will be re-added to this._textures when the task is recorded (by a call to FrameGraph.build): that's why we delete the entry from this._textures below
             entry.texture?.dispose();
             entry.texture = null;
 

packages/dev/core/src/Rendering/objectRenderer.ts
@@ -160,6 +160,14 @@ export class ObjectRenderer {
     }
 
     /**
+     * If true, the object renderer will not set the view/projection/transformation matrices for the active camera (default: false).
+     * By default, the view/projection/transformation matrices are set from the active camera (either ObjectRenderer.activeCamera or scene.activeCamera).
+     * Sets this property to true if you want to define your own transformation matrices (use the onInitRenderingObservable observable
+     * to set your own matrices, to be sure they will be correctly taken into account)
+     */
+    public dontSetTransformationMatrix = false;
+
+    /**
      * Override the mesh isReady function with your own one.
      */
     public customIsReadyFunction: (mesh: AbstractMesh, refreshRate: number, preWarm?: boolean) => boolean;
@@ -528,8 +536,6 @@ export class ObjectRenderer {
         const engine = this._scene.getEngine();
         const camera: Nullable<Camera> = this.activeCamera ?? this._scene.activeCamera;
 
-        this.onInitRenderingObservable.notifyObservers(this);
-
         this._currentSceneCamera = this._scene.activeCamera;
 
         if (this._useUBO) {
@@ -538,8 +544,12 @@ export class ObjectRenderer {
             this._scene.setSceneUniformBuffer(this._sceneUBO);
         }
 
+        this.onInitRenderingObservable.notifyObservers(this);
+
         if (camera) {
-            this._scene.setTransformMatrix(camera.getViewMatrix(), camera.getProjectionMatrix(true));
+            if (!this.dontSetTransformationMatrix) {
+                this._scene.setTransformMatrix(camera.getViewMatrix(), camera.getProjectionMatrix(true));
+            }
             this._scene.activeCamera = camera;
             engine.setViewport(camera.rigParent ? camera.rigParent.viewport : camera.viewport, viewportWidth, viewportHeight);
         }

packages/tools/nodeRenderGraphEditor/src/graphSystem/properties/inputNodePropertyComponent.tsx
@@ -135,6 +135,16 @@ export class InputPropertyTabComponent extends React.Component<IPropertyComponen
                                     }}
                                     extractValue={() => creationOptions.options.useSRGBBuffers![0]}
                                 />
+                                <CheckBoxLineComponent
+                                    label="History texture"
+                                    target={creationOptions}
+                                    propertyName=""
+                                    onSelect={(value: boolean) => {
+                                        creationOptions.isHistoryTexture = value;
+                                        this.props.stateManager.onRebuildRequiredObservable.notifyObservers();
+                                    }}
+                                    extractValue={() => creationOptions.isHistoryTexture!}
+                                />
                             </>
                         )}
                     </>