19f3082b85d4900834d0c037d7241329c8bb4fd0
Georgina Halpern <gehalper@microsoft.com>
2025-09-22 17:34:19 -0400

    fix bug where usage of tempMat for all functions was causing overriding

--- packages/dev/core/src/Materials/floatingOriginMatrixOverrides.ts
+++ packages/dev/core/src/Materials/floatingOriginMatrixOverrides.ts
@@ -1,70 +1,98 @@
 import { Effect } from "../Materials/effect";
-import type { IMatrixLike, IVector3Like } from "../Maths";
-import { InvertMatrixToArray, MultiplyMatricesToArray } from "../Maths";
+import { TmpVectors } from "../Maths/math.vector";
+import type { Matrix } from "../Maths/math.vector";
+import type { IMatrixLike, IVector3Like } from "../Maths/math.like";
+import { InvertMatrixToRef, MultiplyMatricesToRef } from "../Maths/ThinMaths/thinMath.matrix.functions";
 import type { Scene } from "../scene";
-import type { DeepImmutable, Tuple } from "../types";
+import type { DeepImmutable } from "../types";
 import { UniformBuffer } from "./uniformBuffer";
 
-const TempMatArray = new Array(16).fill(0) as Tuple<number, 16>;
-const TempAsArray = () => TempMatArray;
-const TempMat: IMatrixLike = {
-    asArray: TempAsArray,
-    updateFlag: 0,
-};
+const TempFinalMat: Matrix = TmpVectors.Matrix[4];
+const TempMat1: Matrix = TmpVectors.Matrix[5];
+const TempMat2: Matrix = TmpVectors.Matrix[6];
 
-function ApplyWorldOffset(offset: IVector3Like, world: DeepImmutable<IMatrixLike>): DeepImmutable<IMatrixLike> {
+function OffsetWorldToRef(offset: IVector3Like, world: DeepImmutable<IMatrixLike>, ref: Matrix): DeepImmutable<IMatrixLike> {
+    const refArray = ref.asArray();
+    const worldArray = world.asArray();
     for (let i = 0; i < 16; i++) {
-        TempMatArray[i] = world.asArray()[i];
+        refArray[i] = worldArray[i];
     }
-    TempMatArray[12] -= offset.x;
-    TempMatArray[13] -= offset.y;
-    TempMatArray[14] -= offset.z;
-    return TempMat;
+    refArray[12] -= offset.x;
+    refArray[13] -= offset.y;
+    refArray[14] -= offset.z;
+    return ref;
 }
 
-function ApplyViewOffset(view: DeepImmutable<IMatrixLike>): DeepImmutable<IMatrixLike> {
+function OffsetViewToRef(view: DeepImmutable<IMatrixLike>, ref: Matrix): DeepImmutable<IMatrixLike> {
+    const refArray = ref.asArray();
+    const viewArray = view.asArray();
     for (let i = 0; i < 16; i++) {
-        TempMatArray[i] = view.asArray()[i];
+        refArray[i] = viewArray[i];
     }
-    TempMatArray[12] = 0;
-    TempMatArray[13] = 0;
-    TempMatArray[14] = 0;
-    return TempMat;
+    refArray[12] = 0;
+    refArray[13] = 0;
+    refArray[14] = 0;
+    return ref;
 }
 
-function ApplyViewProjectionOffset(view: DeepImmutable<IMatrixLike>, projection: DeepImmutable<IMatrixLike>): DeepImmutable<IMatrixLike> {
-    MultiplyMatricesToArray(ApplyViewOffset(view), projection, TempMatArray);
-    return TempMat;
+function OffsetViewProjectionToRef(view: DeepImmutable<IMatrixLike>, projection: DeepImmutable<IMatrixLike>, ref: Matrix): DeepImmutable<IMatrixLike> {
+    MultiplyMatricesToRef(OffsetViewToRef(view, ref), projection, ref);
+    return ref;
 }
 
-function ApplyWorldViewOffset(offset: IVector3Like, worldView: DeepImmutable<IMatrixLike>, view: DeepImmutable<IMatrixLike>): DeepImmutable<IMatrixLike> {
+function OffsetWorldViewToRef(offset: IVector3Like, worldView: DeepImmutable<IMatrixLike>, view: DeepImmutable<IMatrixLike>, ref: Matrix): DeepImmutable<IMatrixLike> {
     // ( world * view ) * inverse ( view ) = world
-    InvertMatrixToArray(view, TempMatArray); // TempMat.toArray (TempMatArray) now contains inverseView
-    MultiplyMatricesToArray(worldView, TempMat, TempMatArray); // TempMat.toArray (TempMatArray) now contains world
+    InvertMatrixToRef(view, TempMat1); // TempMat1 = inverseView
+    MultiplyMatricesToRef(worldView, TempMat1, TempMat2); // TempMat2 = world, TempMat1 can be reused
 
     // ( offsetWorld * offsetView ) = offsetWorldView
-    MultiplyMatricesToArray(ApplyWorldOffset(offset, TempMat), ApplyViewOffset(view), TempMatArray);
+    OffsetWorldToRef(offset, TempMat2, TempMat1); // TempMat1 = offsetWorld
+    OffsetViewToRef(view, TempMat2); // TempMat2 = offsetView
+    MultiplyMatricesToRef(TempMat1, TempMat2, ref);
 
-    return TempMat;
+    return ref;
 }
 
-function ApplyWorldViewProjectionOffset(
+function OffsetWorldViewProjectionToRef(
     offset: IVector3Like,
     worldViewProjection: DeepImmutable<IMatrixLike>,
     viewProjection: DeepImmutable<IMatrixLike>,
     view: DeepImmutable<IMatrixLike>,
-    projection: DeepImmutable<IMatrixLike>
+    projection: DeepImmutable<IMatrixLike>,
+    ref: IMatrixLike
 ): DeepImmutable<IMatrixLike> {
+    // ( world * view * projection ) * inverse(projection) * inverse(view) = world
     // ( world * view * projection ) * inverse (view * projection) = world
-    InvertMatrixToArray(viewProjection, TempMatArray); // TempMat.toArray (TempMatArray) now contains inverse (view * projection)
-    MultiplyMatricesToArray(worldViewProjection, TempMat, TempMatArray); // TempMat.toArray (TempMatArray) now contains world
+    InvertMatrixToRef(viewProjection, TempMat1); // TempMat1 = inverse (view * projection)
+    MultiplyMatricesToRef(worldViewProjection, TempMat1, TempMat2); // TempMat2 = world, TempMat1 can be reused
 
     // ( offsetWorld * offsetViewProjection)  = offsetWorldViewProjection
-    MultiplyMatricesToArray(ApplyWorldOffset(offset, TempMat), ApplyViewProjectionOffset(view, projection), TempMatArray);
+    OffsetWorldToRef(offset, TempMat2, TempMat1); // TempMat1 = offsetWorld
+    OffsetViewProjectionToRef(view, projection, TempMat2); // TempMat2 = offsetViewProjection
+    MultiplyMatricesToRef(TempMat1, TempMat2, ref);
 
-    return TempMat;
+    return ref;
 }
 
+function GetOffsetMatrix(uniformName: string, mat: IMatrixLike, scene: Scene): IMatrixLike {
+    TempFinalMat.updateFlag = mat.updateFlag;
+    switch (uniformName) {
+        case "world":
+            return OffsetWorldToRef(scene.floatingOriginOffset, mat, TempFinalMat);
+        case "view":
+            return OffsetViewToRef(mat, TempFinalMat);
+        case "worldView":
+            return OffsetWorldViewToRef(scene.floatingOriginOffset, mat, scene.getViewMatrix(), TempFinalMat);
+        case "viewProjection":
+            return OffsetViewProjectionToRef(scene.getViewMatrix(), scene.getProjectionMatrix(), TempFinalMat);
+        case "worldViewProjection":
+            return OffsetWorldViewProjectionToRef(scene.floatingOriginOffset, mat, scene.getTransformMatrix(), scene.getViewMatrix(), scene.getProjectionMatrix(), TempFinalMat);
+        default:
+            return mat;
+    }
+}
+
+// ---- Overriding the prototypes of effect and uniformBuffer's setMatrix functions ----
 const UniformBufferInternal = UniformBuffer as any;
 const OriginalUpdateMatrixForUniform = UniformBufferInternal.prototype._updateMatrixForUniform;
 const OriginalSetMatrix = Effect.prototype.setMatrix;
@@ -83,22 +111,4 @@ export function OverrideMatrixFunctions(scene: Scene) {
     UniformBufferInternal.prototype._updateMatrixForUniform = function (uniformName: string, matrix: IMatrixLike) {
         this._updateMatrixForUniformOverride(uniformName, GetOffsetMatrix(uniformName, matrix, scene));
     };
-}
-
-function GetOffsetMatrix(uniformName: string, mat: IMatrixLike, scene: Scene): IMatrixLike {
-    TempMat.updateFlag = mat.updateFlag;
-    switch (uniformName) {
-        case "world":
-            return ApplyWorldOffset(scene.floatingOriginOffset, mat);
-        case "view":
-            return ApplyViewOffset(mat);
-        case "worldView":
-            return ApplyWorldViewOffset(scene.floatingOriginOffset, mat, scene.getViewMatrix());
-        case "viewProjection":
-            return ApplyViewProjectionOffset(scene.getViewMatrix(), scene.getProjectionMatrix());
-        case "worldViewProjection":
-            return ApplyWorldViewProjectionOffset(scene.floatingOriginOffset, mat, scene.getTransformMatrix(), scene.getViewMatrix(), scene.getProjectionMatrix());
-        default:
-            return mat;
-    }
 }
