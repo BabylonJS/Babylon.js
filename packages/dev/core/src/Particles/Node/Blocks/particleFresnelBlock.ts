87fc49fa996a2a1ab1aabf659016b4c4edbd248e
David Catuhe <david@catuhe.com>
2025-10-03 10:35:53 -0700

    oupsie

--- packages/dev/core/src/Particles/Node/Blocks/particleFresnelBlock.ts
+++ packages/dev/core/src/Particles/Node/Blocks/particleFresnelBlock.ts
@@ -1,9 +1,8 @@
 import { RegisterClass } from "../../../Misc/typeStore";
-import { Vector2, Vector3 } from "core/Maths/math.vector";
+import { Vector3 } from "core/Maths/math.vector";
 import { NodeParticleBlock } from "../nodeParticleBlock";
 import { NodeParticleBlockConnectionPointTypes } from "../Enums/nodeParticleBlockConnectionPointTypes";
 import type { NodeParticleConnectionPoint } from "../nodeParticleBlockConnectionPoint";
-import { Color4 } from "core/Maths/math.color";
 
 /**
  * Block used to compute the fresnel term
