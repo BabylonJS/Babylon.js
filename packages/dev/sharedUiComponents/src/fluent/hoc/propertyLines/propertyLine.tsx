f6db460e5705e7646371538bed2f1cb3b8c86cab
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-10-06 16:40:28 -0700

    Allow property line value side to be less than 50% so label can use available space
    Ensure that when an info label is truncated, the info button remains visible

--- packages/dev/sharedUiComponents/src/fluent/hoc/propertyLines/propertyLine.tsx
+++ packages/dev/sharedUiComponents/src/fluent/hoc/propertyLines/propertyLine.tsx
@@ -23,22 +23,26 @@ const usePropertyLineStyles = makeStyles({
         justifyContent: "flex-start",
         width: "100%",
     },
-    label: {
+    infoLabel: {
+        display: "flex",
         flex: "1 1 0", // grow=1, shrink =1, basis = 0 initial size before
         minWidth: CustomTokens.labelMinWidth,
         textAlign: "left",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
+    },
+    labelSlot: {
+        display: "flex",
+        minWidth: 0,
     },
     labelText: {
         whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
     },
     rightContent: {
         flex: "0 1 auto",
         display: "flex",
         alignItems: "center",
         justifyContent: "flex-end",
-        minWidth: "50%",
     },
     infoPopup: {
         whiteSpace: "normal",
@@ -144,7 +148,12 @@ export const PropertyLine = forwardRef<HTMLDivElement, PropsWithChildren<Propert
     return (
         <LineContainer ref={ref}>
             <div className={classes.baseLine}>
-                <InfoLabel className={classes.label} info={description ? <div className={classes.infoPopup}>{description}</div> : undefined} title={label}>
+                <InfoLabel
+                    className={classes.infoLabel}
+                    label={{ className: classes.labelSlot }}
+                    info={description ? <div className={classes.infoPopup}>{description}</div> : undefined}
+                    title={label}
+                >
                     <Body1Strong className={classes.labelText}>{label}</Body1Strong>
                 </InfoLabel>
                 <div className={classes.rightContent}>
