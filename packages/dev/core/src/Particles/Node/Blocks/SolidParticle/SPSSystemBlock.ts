5dc1e1d82663452fba4667d5e9669d6646841336
Mikalai Lazitski <soullnik@gmail.com>
2025-10-20 09:59:31 +0300

    Refactor SPSSystemBlock to streamline particle initialization and update logic
    This commit simplifies the particle initialization and update processes within the SPSSystemBlock. The logic for creating and managing connected points has been optimized, allowing for a more efficient retrieval of connected values. Additionally, the updateParticle method has been enhanced to directly utilize shape IDs for better performance and clarity. These changes improve the overall functionality and maintainability of the Solid Particle System.

--- packages/dev/core/src/Particles/Node/Blocks/SolidParticle/SPSSystemBlock.ts
+++ packages/dev/core/src/Particles/Node/Blocks/SolidParticle/SPSSystemBlock.ts
@@ -6,6 +6,7 @@ import type { NodeParticleBuildState } from "../../nodeParticleBuildState";
 import { editableInPropertyPage, PropertyTypeForEdition } from "core/Decorators/nodeDecorator";
 import { SolidParticleSystem } from "core/Particles/solidParticleSystem";
 import type { ISPSCreateData } from "./ISPSData";
+import { SolidParticle } from "../../../solidParticle";
 
 /**
  * Block used to create SolidParticleSystem and collect all Create blocks
@@ -64,161 +65,41 @@ export class SPSSystemBlock extends NodeParticleBlock {
         sps.billboard = this.billboard;
         sps.name = this.name;
 
-        const createBlocks: ISPSCreateData[] = [];
-
-        if (this.solidParticle.endpoints.length > 0) {
-            for (const endpoint of this.solidParticle.endpoints) {
-                const createBlock = endpoint.getConnectedValue(state);
-                if (createBlock) {
-                    createBlocks.push(createBlock);
-                }
-            }
-        } else if (this.solidParticle.isConnected) {
-            const createBlock = this.solidParticle.getConnectedValue(state);
-            if (createBlock) {
-                createBlocks.push(createBlock);
-            }
-        }
-
-        if (createBlocks.length === 0 && this.solidParticle.allowMultipleConnections && this.solidParticle._connectedPoint) {
-            const connectedPoint = this.solidParticle._connectedPoint;
-            if (connectedPoint.endpoints && connectedPoint.endpoints.length > 0) {
-                for (const endpoint of connectedPoint.endpoints) {
-                    const createBlock = endpoint.getConnectedValue(state);
-                    if (createBlock) {
-                        createBlocks.push(createBlock);
-                    }
-                }
-            }
-        }
+        const createBlocks: ISPSCreateData[] = this.solidParticle.connectedPoints.map((connectedPoint) => connectedPoint._storedValue);
 
         for (const createBlock of createBlocks) {
             if (createBlock.mesh && createBlock.count) {
-                sps.addShape(createBlock.mesh, createBlock.count);
-                createBlock.mesh.dispose();
+                createBlock.shapeId = sps.addShape(createBlock.mesh, createBlock.count);
+                createBlock.mesh.isVisible = false;
             }
         }
 
         sps.initParticles = () => {
             for (const createBlock of createBlocks) {
-                if (createBlock.initBlock) {
-                    let startIndex = 0;
-                    for (let i = 0; i < createBlocks.indexOf(createBlock); i++) {
-                        startIndex += createBlocks[i].count;
-                    }
-                    const endIndex = startIndex + createBlock.count - 1;
+                if (createBlock.initBlock && createBlock.shapeId !== undefined) {
+                    const particles = sps.getParticlesByShapeId(createBlock.shapeId);
 
-                    for (let p = startIndex; p <= endIndex && p < sps.nbParticles; p++) {
-                        const particle = sps.particles[p];
-
-                        if (createBlock.initBlock?.position) {
-                            const particleContext = {
-                                id: p,
-                                position: particle.position,
-                                velocity: particle.velocity,
-                                color: particle.color,
-                                scaling: particle.scaling,
-                                rotation: particle.rotation,
-                            };
-                            state.particleContext = particleContext as any;
-
-                            const positionValue = createBlock.initBlock.position;
-                            if (typeof positionValue === "function") {
-                                particle.position.copyFrom(positionValue());
-                            } else {
-                                particle.position.copyFrom(positionValue);
-                            }
+                    particles.forEach((particle) => {
+                        if (createBlock.initBlock) {
+                            particle.position.copyFrom(createBlock.initBlock.position());
+                            particle.velocity.copyFrom(createBlock.initBlock.velocity());
+                            particle.color?.copyFrom(createBlock.initBlock.color());
+                            particle.scaling.copyFrom(createBlock.initBlock.scaling());
+                            particle.rotation.copyFrom(createBlock.initBlock.rotation());
                         }
-                        if (createBlock.initBlock?.velocity) {
-                            const velocityValue = createBlock.initBlock.velocity;
-                            if (typeof velocityValue === "function") {
-                                particle.velocity.copyFrom(velocityValue());
-                            } else {
-                                particle.velocity.copyFrom(velocityValue);
-                            }
-                        }
-                        if (createBlock.initBlock?.color) {
-                            const colorValue = createBlock.initBlock.color;
-                            if (typeof colorValue === "function") {
-                                particle.color?.copyFrom(colorValue());
-                            } else {
-                                particle.color?.copyFrom(colorValue);
-                            }
-                        }
-                        if (createBlock.initBlock?.scaling) {
-                            const scalingValue = createBlock.initBlock.scaling;
-                            if (typeof scalingValue === "function") {
-                                particle.scaling.copyFrom(scalingValue());
-                            } else {
-                                particle.scaling.copyFrom(scalingValue);
-                            }
-                        }
-                        if (createBlock.initBlock?.rotation) {
-                            const rotationValue = createBlock.initBlock.rotation;
-                            if (typeof rotationValue === "function") {
-                                particle.rotation.copyFrom(rotationValue());
-                            } else {
-                                particle.rotation.copyFrom(rotationValue);
-                            }
-                        }
-                    }
+                    });
                 }
             }
         };
 
-        sps.updateParticle = (particle: any) => {
-            let currentParticleIndex = 0;
-            let targetCreateBlock = null;
-
-            for (const createBlock of createBlocks) {
-                if (particle.idx >= currentParticleIndex && particle.idx < currentParticleIndex + createBlock.count) {
-                    targetCreateBlock = createBlock;
-                    break;
-                }
-                currentParticleIndex += createBlock.count;
-            }
-
-            if (targetCreateBlock && targetCreateBlock.updateBlock) {
-                if (targetCreateBlock.updateBlock.position) {
-                    const positionValue = targetCreateBlock.updateBlock.position;
-                    if (typeof positionValue === "function") {
-                        particle.position.copyFrom(positionValue());
-                    } else {
-                        particle.position.copyFrom(positionValue);
-                    }
-                }
-                if (targetCreateBlock.updateBlock.velocity) {
-                    const velocityValue = targetCreateBlock.updateBlock.velocity;
-                    if (typeof velocityValue === "function") {
-                        particle.velocity.copyFrom(velocityValue());
-                    } else {
-                        particle.velocity.copyFrom(velocityValue);
-                    }
-                }
-                if (targetCreateBlock.updateBlock.color) {
-                    const colorValue = targetCreateBlock.updateBlock.color;
-                    if (typeof colorValue === "function") {
-                        particle.color?.copyFrom(colorValue());
-                    } else {
-                        particle.color?.copyFrom(colorValue);
-                    }
-                }
-                if (targetCreateBlock.updateBlock.scaling) {
-                    const scalingValue = targetCreateBlock.updateBlock.scaling;
-                    if (typeof scalingValue === "function") {
-                        particle.scaling.copyFrom(scalingValue());
-                    } else {
-                        particle.scaling.copyFrom(scalingValue);
-                    }
-                }
-                if (targetCreateBlock.updateBlock.rotation) {
-                    const rotationValue = targetCreateBlock.updateBlock.rotation;
-                    if (typeof rotationValue === "function") {
-                        particle.rotation.copyFrom(rotationValue());
-                    } else {
-                        particle.rotation.copyFrom(rotationValue);
-                    }
-                }
+        sps.updateParticle = (particle: SolidParticle) => {
+            const createBlock = createBlocks.find((createBlock) => createBlock.shapeId === particle.shapeId);
+            if (createBlock && createBlock.updateBlock) {
+                particle.position.copyFrom(createBlock.updateBlock.position());
+                particle.velocity.copyFrom(createBlock.updateBlock.velocity());
+                particle.color?.copyFrom(createBlock.updateBlock.color());
+                particle.scaling.copyFrom(createBlock.updateBlock.scaling());
+                particle.rotation.copyFrom(createBlock.updateBlock.rotation());
             }
             return particle;
         };
