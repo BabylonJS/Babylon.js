227365ccc8da4ab9ee0c79e9ece46c43ea038350
Georgie <72631815+georginahalpern@users.noreply.github.com>
2025-10-22 13:17:02 -0400

    Store FloatingOrigin in local var vs copying to temp vect (#17332)
    Even better-- avoid copying offset to new vect and just store in local
    var :)
    
    ---------
    
    Co-authored-by: Georgina Halpern <gehalper@microsoft.com>

--- packages/dev/core/src/Rendering/boundingBoxRenderer.ts
+++ packages/dev/core/src/Rendering/boundingBoxRenderer.ts
@@ -629,6 +629,7 @@ export class BoundingBoxRenderer implements ISceneComponent {
         this.onBeforeBoxRenderingObservable.notifyObservers(DummyBoundingBox);
 
         let instancesCount = 0;
+        const floatingOriginOffset = this.scene.floatingOriginOffset;
 
         for (let boundingBoxIndex = 0; boundingBoxIndex < this.renderList.length; boundingBoxIndex++) {
             const boundingBox = this.renderList.data[boundingBoxIndex];
@@ -657,9 +658,9 @@ export class BoundingBoxRenderer implements ISceneComponent {
             const offset = instancesCount * 16;
             TempMatrix.multiplyToArray(boundingBox.getWorldMatrix(), matrices, offset);
 
-            matrices[offset + 12] -= this.scene.floatingOriginOffset.x;
-            matrices[offset + 13] -= this.scene.floatingOriginOffset.y;
-            matrices[offset + 14] -= this.scene.floatingOriginOffset.z;
+            matrices[offset + 12] -= floatingOriginOffset.x;
+            matrices[offset + 13] -= floatingOriginOffset.y;
+            matrices[offset + 14] -= floatingOriginOffset.z;
             instancesCount++;
         }
 
