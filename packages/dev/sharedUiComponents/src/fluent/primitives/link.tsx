cd7ea1183aa3781da4cff0db6aa58fb520a2e9fd
Georgina Halpern <gehalper@microsoft.com>
2025-10-24 15:00:57 -0400

    simplify link props

--- packages/dev/sharedUiComponents/src/fluent/primitives/link.tsx
+++ packages/dev/sharedUiComponents/src/fluent/primitives/link.tsx
@@ -3,14 +3,24 @@ import { Body1, Link as FluentLink } from "@fluentui/react-components";
 import type { ImmutablePrimitiveProps } from "./primitive";
 
 export type LinkProps = ImmutablePrimitiveProps<string> & {
+    /**
+     * Used if you want to handle the link click yourself
+     */
     onLink?: () => void;
+    /**
+     * The URL the link points to
+     */
     url?: string;
-    target?: string;
-    rel?: string;
+    /**
+     * Defines whether to open the link in current tab or new tab. Default is new
+     */
+    target?: "current" | "new";
 };
+
 export const Link: FunctionComponent<PropsWithChildren<LinkProps>> = (props) => {
+    const { target, url, onLink, ...rest } = props;
     return (
-        <FluentLink inline target="_blank" rel="noopener noreferrer" onClick={() => props.onLink?.()} href={props.url} {...props}>
+        <FluentLink inline target={target === "current" ? "_self" : "_blank"} rel="noopener noreferrer" href={url} onClick={onLink ?? undefined} {...rest}>
             {props.children}
             <Body1>{props.value}</Body1>
         </FluentLink>
