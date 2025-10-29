7ca893985a4eba3152f37fb56c472c0387207853
MiiBond <MiiBond@users.noreply.github.com>
2025-10-29 14:11:27 -0700

    EXT_lights_area updates and export support (#17312)
    Modifications to the area lights extension (replacing "shape" with
    "type" property, etc.) and adding export support.

--- packages/dev/loaders/src/glTF/2.0/Extensions/dynamic.ts
+++ packages/dev/loaders/src/glTF/2.0/Extensions/dynamic.ts
@@ -56,6 +56,11 @@ export function registerBuiltInGLTFExtensions() {
         return new KHR_lights(loader);
     });
 
+    registerGLTFExtension("EXT_lights_area", true, async (loader) => {
+        const { EXT_lights_area } = await import("./EXT_lights_area");
+        return new EXT_lights_area(loader);
+    });
+
     registerGLTFExtension("EXT_lights_ies", true, async (loader) => {
         const { EXT_lights_ies } = await import("./EXT_lights_ies");
         return new EXT_lights_ies(loader);
