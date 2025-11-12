059b5b8659951333634c4c77404b5b30181b2083
Gary Hsu <bghgary@users.noreply.github.com>
2025-11-12 14:45:37 -0800

    Remove dependency on concrete vector objects from math.vector.functions

--- packages/dev/core/src/Maths/math.like.ts
+++ packages/dev/core/src/Maths/math.like.ts
@@ -1,83 +1,121 @@
 import type { float, int, Tuple } from "../types";
 
 /**
- * @internal
+ * Interface representing a color with RGB components
  */
 export interface IColor3Like {
+    /** Red component */
     r: float;
+    /** Green component */
     g: float;
+    /** Blue component */
     b: float;
 }
 
 /**
- * @internal
+ * Interface representing a color with RGBA components
  */
 export interface IColor4Like extends IColor3Like {
+    /** Alpha component */
     a: float;
 }
 
 /**
- * @internal
+ * Interface representing a 2D vector
  */
 export interface IVector2Like {
+    /** X coordinate */
     x: float;
+    /** Y coordinate */
     y: float;
 }
 
 /**
- * @internal
+ * Interface representing a 3D vector
  */
 export interface IVector3Like extends IVector2Like {
+    /** Z coordinate */
     z: float;
-}
 
-/**
- * @internal
- */
-export interface IVector3LikeInternal {
+    /** @internal */
     _x: number;
+
+    /** @internal */
     _y: number;
+
+    /** @internal */
     _z: number;
+
+    /** @internal */
     _isDirty?: boolean;
 }
 
 /**
- * @internal
+ * Interface representing a 4D vector
  */
 export interface IVector4Like extends IVector3Like {
+    /** W coordinate */
     w: float;
+
+    /** @internal */
+    _w: number;
 }
 
 /**
- * @internal
+ * Interface representing a quaternion
  */
 export interface IQuaternionLike extends IVector3Like {
+    /** W component */
     w: float;
+
+    /** @internal */
+    _x: number;
+
+    /** @internal */
+    _y: number;
+
+    /** @internal */
+    _z: number;
+
+    /** @internal */
+    _w: number;
+
+    /** @internal */
+    _isDirty?: boolean;
 }
 
 /**
- * @internal
+ * Interface representing a plane
  */
 export interface IPlaneLike {
+    /** The normal vector of the plane */
     normal: IVector3Like;
+    /** The distance from the origin */
     d: float;
+    /** Normalizes the plane */
     normalize(): void;
 }
 
 /**
- * @internal
+ * Interface representing a matrix
  */
 export interface IMatrixLike {
+    /** Converts the matrix to an array */
     asArray(): Tuple<number, 16>;
+    /** Update flag for the matrix */
     updateFlag: int;
 }
 
 /**
- * @internal
+ * Interface representing a viewport
  */
 export interface IViewportLike {
+    /** X position of the viewport */
     x: float;
+    /** Y position of the viewport */
     y: float;
+    /** Width of the viewport */
     width: float;
+    /** Height of the viewport */
     height: float;
 }
