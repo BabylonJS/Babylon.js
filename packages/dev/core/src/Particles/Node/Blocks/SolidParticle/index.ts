43ac6acf304895fb76dd7e070c66f39b4ba42484
Mikalai Lazitski <soullnik@gmail.com>
2025-11-25 09:21:36 +0300

    Refactor SolidParticle exports in Node Particle Editor
    - Updated the exports in `index.ts` to include individual SolidParticle block files, enhancing modularity and organization.
    - Removed the obsolete `SolidParticle/index.ts` file to streamline the codebase.
    
    These changes improve the structure and maintainability of the SolidParticle components within the node particle editor.

--- packages/dev/core/src/Particles/Node/Blocks/SolidParticle/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./ISolidParticleData";
-export * from "./meshSourceBlock";
-export * from "./initSolidParticleBlock";
-export * from "./solidParticleSystemBlock";
-export * from "./updateSolidParticleBlock";
-export * from "./createSolidParticleBlock";
-export * from "./nodeMaterialSourceBlock";
