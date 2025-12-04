79fbe92eac98f975373abf90fa69cb6c08d25a8f
Popov72 <github@evpopov.com>
2025-12-04 17:11:41 +0100

    Allow smaller texture sizes for the lighting volume texture

--- packages/dev/core/src/FrameGraph/Tasks/PostProcesses/volumetricLightingTask.ts
+++ packages/dev/core/src/FrameGraph/Tasks/PostProcesses/volumetricLightingTask.ts
@@ -158,7 +158,7 @@ export class FrameGraphVolumetricLightingTask extends FrameGraphTask {
         this._renderLightingVolumeMaterial = new ShaderMaterial(`${name} - render lighting volume`, this._frameGraph.scene, "volumetricLightingRenderVolume", {
             attributes: ["position"],
             uniformBuffers: ["Scene", "Mesh"],
-            uniforms: ["world", "viewProjection", "vEyePosition", "lightDir", "invViewProjection", "outputTextureSize", "extinctionPhaseG", "lightPower"],
+            uniforms: ["world", "viewProjection", "vEyePosition", "lightDir", "invViewProjection", "outputTextureSize", "extinctionPhaseG", "lightPower", "textureRatio"],
             samplers: ["depthTexture"],
             defines: enableExtinction ? ["USE_EXTINCTION"] : [],
             shaderLanguage: isWebGPU ? ShaderLanguage.WGSL : ShaderLanguage.GLSL,
@@ -170,9 +170,9 @@ export class FrameGraphVolumetricLightingTask extends FrameGraphTask {
             this._renderLightingVolumeMaterial.bindEyePosition(this._renderLightingVolumeMaterial.getEffect());
         });
 
-        this._clearLightingVolumeTextureTask = new FrameGraphClearTextureTask(`${name} - clear lighting volume texture`, frameGraph);
-        this._renderLightingVolumeTask = new FrameGraphObjectRendererTask(`${name} - render lighting volume`, frameGraph, frameGraph.scene);
-        this._blendLightingVolumeTask = new FrameGraphVolumetricLightingBlendVolumeTask(`${name} - blend lighting volume`, frameGraph, enableExtinction);
+        this._clearLightingVolumeTextureTask = new FrameGraphClearTextureTask(`clear lighting volume texture`, frameGraph);
+        this._renderLightingVolumeTask = new FrameGraphObjectRendererTask(`render lighting volume`, frameGraph, frameGraph.scene);
+        this._blendLightingVolumeTask = new FrameGraphVolumetricLightingBlendVolumeTask(`blend lighting volume texture`, frameGraph, enableExtinction);
 
         this.onTexturesAllocatedObservable.add(() => {
             this._renderLightingVolumeMaterial.setInternalTexture("depthTexture", frameGraph.textureManager.getTextureFromHandle(this.depthTexture)!);
@@ -193,6 +193,10 @@ export class FrameGraphVolumetricLightingTask extends FrameGraphTask {
             this._renderLightingVolumeMaterial.isReady() &&
             this._blendLightingVolumeTask.isReady()
         );
+    }
+
+    public override getClassName(): string {
+        return "FrameGraphVolumetricLightingTask";
     }
 
     public override record(skipCreationOfDisabledPasses = false) {
@@ -219,13 +223,18 @@ export class FrameGraphVolumetricLightingTask extends FrameGraphTask {
 
         this.lightingVolumeMesh.meshes[0].material = this._renderLightingVolumeMaterial;
 
+        const targetTextureSize = textureManager.getTextureAbsoluteDimensions(this.targetTexture);
         const volumeTextureSize = textureManager.getTextureAbsoluteDimensions(lightingVolumeTexture);
 
+        this._renderLightingVolumeMaterial.setVector2(
+            "textureRatio",
+            new Vector2(targetTextureSize.width / volumeTextureSize.width, targetTextureSize.height / volumeTextureSize.height)
+        );
         this._renderLightingVolumeMaterial.setVector2("outputTextureSize", new Vector2(volumeTextureSize.width, volumeTextureSize.height));
 
-        const pass = this._frameGraph.addPass(this.name);
+        const passUpdateMaterial = this._frameGraph.addPass(this.name);
 
-        pass.setExecuteFunc(() => {
+        passUpdateMaterial.setExecuteFunc(() => {
             this.camera.getTransformationMatrix().invertToRef(InvViewProjectionMatrix);
 
             this._renderLightingVolumeMaterial.setMatrix("invViewProjection", InvViewProjectionMatrix);
