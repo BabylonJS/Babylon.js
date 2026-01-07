2c509b56ef12a3ef355bb2db33e09b246528012d
Georgina Halpern <gehalper@microsoft.com>
2026-01-07 14:55:56 -0500

    reset zoom logic to master

--- packages/dev/core/src/Cameras/geospatialCamera.ts
+++ packages/dev/core/src/Cameras/geospatialCamera.ts
@@ -433,14 +433,16 @@ export class GeospatialCamera extends Camera {
 
         if (zoomDelta > 0) {
             // Zooming IN - respect radiusMin as distance to surface
+            let maxZoomIn = this._radius - this.limits.radiusMin;
+
             if (pickedPoint) {
                 const pickDistance = Vector3Distance(this._position, pickedPoint);
                 // Don't zoom past the picked surface point + radiusMin
                 const maxZoomToSurface = pickDistance - this.limits.radiusMin;
-                return Math.min(zoomDelta, Math.max(0, maxZoomToSurface));
+                maxZoomIn = Math.min(maxZoomIn, Math.max(0, maxZoomToSurface));
             }
 
-            return zoomDelta;
+            return Math.min(zoomDelta, Math.max(0, maxZoomIn));
         } else {
             // Zooming OUT - respect radiusMax
             const maxZoomOut = this.limits.radiusMax - this._radius;
