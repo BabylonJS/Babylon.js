889af888729e694a895877c82e718af05e4a93ed
sebavan <vandenberghe.sebastien@gmail.com>
2026-01-09 17:27:06 +0100

    Let s try smthg different

--- packages/dev/core/src/Engines/nativeEngine.ts
+++ packages/dev/core/src/Engines/nativeEngine.ts
@@ -1,25 +1,31 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
 import type { InternalTexture } from "../Materials/Textures/internalTexture";
-import { ThinNativeEngine, type ThinNativeEngineOptions } from "./thinNativeEngine";
+import { Engine } from "./engine";
 import { Tools } from "../Misc/tools";
 
 import "./AbstractEngine/abstractEngine.loadFile";
 import "./AbstractEngine/abstractEngine.textureLoaders";
 import "./Native/Extensions/nativeEngine.cubeTexture";
+import { ThinNativeEngine } from "./thinNativeEngine";
 
 /**
  * Options to create the Native engine
  */
-export interface NativeEngineOptions extends ThinNativeEngineOptions {}
+export interface NativeEngineOptions {
+    /**
+     * defines whether to adapt to the device's viewport characteristics (default: false)
+     */
+    adaptToDeviceRatio?: boolean;
+}
 
 /** @internal */
-export class NativeEngine extends ThinNativeEngine {
+export class NativeEngine extends Engine {
     /**
      * @internal
      */
     public constructor(options: NativeEngineOptions = {}) {
-        super(options);
+        super(null, false, undefined, options.adaptToDeviceRatio);
 
         Tools.LoadScript = function (scriptUrl, onSuccess, onError) {
             Tools.LoadFile(
@@ -45,14 +51,24 @@ export class NativeEngine extends ThinNativeEngine {
     /**
      * @internal
      */
-    public wrapWebGLTexture(): InternalTexture {
+    public override wrapWebGLTexture(): InternalTexture {
         throw new Error("wrapWebGLTexture is not supported, use wrapNativeTexture instead.");
     }
 
     /**
      * @internal
      */
-    public _uploadImageToTexture(texture: InternalTexture, image: HTMLImageElement, _faceIndex: number = 0, _lod: number = 0) {
+    public override _uploadImageToTexture(texture: InternalTexture, image: HTMLImageElement, _faceIndex: number = 0, _lod: number = 0) {
         throw new Error("_uploadArrayBufferViewToTexture not implemented.");
     }
 }
+
+function applyMixins(derivedCtor: any, constructors: any[]) {
+    constructors.forEach((baseCtor) => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+            Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null));
+        });
+    });
+}
+
+applyMixins(NativeEngine, [ThinNativeEngine]);
