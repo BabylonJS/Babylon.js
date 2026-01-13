13e451b362a4d8834993d6082bcbfb0d1082518a
Andy Fillebrown <andy@docEdub.com>
2026-01-13 12:03:46 -0500

    Fix TypeScript lexical environment issues in path transform build tool

--- packages/dev/buildTools/src/pathTransform.ts
+++ packages/dev/buildTools/src/pathTransform.ts
@@ -145,6 +145,11 @@ function TransformerFactory<T extends TransformerNode>(context: ts.Transformatio
             if (ts.isStringLiteral(node)) {
                 return getResolvedPathNode(node) || node;
             }
+            // Skip type literals - they can't contain dynamic imports and their
+            // get/set accessor signatures cause lexical environment issues in TS 5.9
+            if (ts.isTypeLiteralNode(node)) {
+                return node;
+            }
             return ts.visitEachChild(node, pathReplacer, context);
         }
 
@@ -188,6 +193,12 @@ function TransformerFactory<T extends TransformerNode>(context: ts.Transformatio
              */
             if (ts.isModuleDeclaration(node)) {
                 return ts.visitEachChild(node, pathReplacer, context);
+            }
+
+            // Skip type literals - they can't contain dynamic imports and their
+            // get/set accessor signatures cause lexical environment issues in TS 5.9
+            if (ts.isTypeLiteralNode(node)) {
+                return node;
             }
 
             return ts.visitEachChild(node, visitor, context);
