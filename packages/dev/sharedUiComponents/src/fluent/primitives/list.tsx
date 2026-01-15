8461d900a7eda8231a794abf4e70e2b172badb48
Ryan Tremblay <ryan.tremblay@microsoft.com>
2026-01-15 12:52:05 -0800

    Inspector v2: Node Material input properties (#17685)
    This PR adds support for `InputBock`s in the properties of a
    `NodeMaterial`.
    - Updated `List` to pass through the original (before sorting) index of
    the item being removed or rendered. This makes it much easier to manage
    updates to the source list (unsorted).
    - Plumbed through the index passing in the various gradient list
    components.
    - Added various property lines for a NodeMaterial's InputBlocks as well
    as GradientBlocks (similar to inspector v1).
    
    <img width="341" height="479" alt="image"
    src="https://github.com/user-attachments/assets/4099f1e5-d8cf-4820-ad23-f54b03e35c48"
    />
    
    <img width="334" height="139" alt="image"
    src="https://github.com/user-attachments/assets/a0b61d65-c382-4e6a-ae77-4d3b334eb337"
    />

--- packages/dev/sharedUiComponents/src/fluent/primitives/list.tsx
+++ packages/dev/sharedUiComponents/src/fluent/primitives/list.tsx
@@ -1,7 +1,10 @@
-import { CopyRegular, DeleteRegular } from "@fluentui/react-icons";
-import type { ReactNode, ReactElement } from "react";
-import { ButtonLine } from "../hoc/buttonLine";
+import type { ReactElement, ReactNode } from "react";
+
 import { Body1Strong, makeStyles, tokens } from "@fluentui/react-components";
+import { AddRegular, CopyRegular, DeleteRegular } from "@fluentui/react-icons";
+import { useMemo } from "react";
+
+import { ButtonLine } from "../hoc/buttonLine";
 
 const useListStyles = makeStyles({
     item: {
@@ -59,23 +62,23 @@ export function List<T>(props: ListProps<T>): ReactElement {
     const { items, renderItem, onDelete, onAdd, addButtonLabel = "Add new item" } = props;
     const classes = useListStyles();
 
+    const sortedItems = useMemo(() => [...items].sort((a, b) => a.sortBy - b.sortBy), [items]);
+
     return (
         <div>
-            <ButtonLine label={addButtonLabel} onClick={() => props.onAdd()} />
+            <ButtonLine label={addButtonLabel} icon={AddRegular} onClick={() => props.onAdd()} />
 
             <div className={classes.list}>
-                {items
-                    .sort((a, b) => a.sortBy - b.sortBy)
-                    .map((item: ListItem<T>, index: number) => (
-                        <div key={item.id} className={classes.item}>
-                            <Body1Strong className={classes.itemId}>#{index}</Body1Strong>
-                            <div className={classes.itemContent}>{renderItem(item, index)}</div>
-                            <div className={classes.iconContainer}>
-                                <CopyRegular onClick={() => onAdd(item)} />
-                                <DeleteRegular onClick={() => onDelete(item, index)} />
-                            </div>
+                {sortedItems.map((item: ListItem<T>, index: number) => (
+                    <div key={item.id} className={classes.item}>
+                        <Body1Strong className={classes.itemId}>#{index}</Body1Strong>
+                        <div className={classes.itemContent}>{renderItem(item, items.indexOf(sortedItems[index]))}</div>
+                        <div className={classes.iconContainer}>
+                            <CopyRegular onClick={() => onAdd(item)} />
+                            <DeleteRegular onClick={() => onDelete(item, items.indexOf(sortedItems[index]))} />
                         </div>
-                    ))}
+                    </div>
+                ))}
             </div>
         </div>
     );
