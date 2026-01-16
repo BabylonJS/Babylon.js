38a6c1c5fadf027f2deebe396075e3af6581a5a7
Vicente Cartas Espinel <vicente.cartas@outlook.com>
2026-01-16 12:58:48 -0800

    Missing files

---
+++ packages/dev/inspector-v2/src/components/properties/particles/attractorProperties.tsx
@@ -0,0 +1,33 @@
+import type { Attractor } from "core/Particles/attractor";
+import type { ParticleSystem } from "core/Particles/particleSystem";
+import type { FunctionComponent } from "react";
+
+import { useCallback } from "react";
+
+import { MessageBar } from "shared-ui-components/fluent/primitives/messageBar";
+import { AttractorList } from "./attractorList";
+import { useObservableArray } from "./useObservableArray";
+
+/**
+ * Display attractor-related properties for a particle system.
+ * @param props Component props.
+ * @returns Render property lines.
+ */
+export const ParticleSystemAttractorProperties: FunctionComponent<{ particleSystem: ParticleSystem }> = (props) => {
+    const { particleSystem: system } = props;
+
+    const attractorsGetter = useCallback(() => system.attractors ?? [], [system]);
+    const attractors = useObservableArray<ParticleSystem, Attractor>(system, attractorsGetter, "addAttractor", "removeAttractor");
+    const scene = system.getScene();
+
+    return (
+        <>
+            {scene ? (
+                <AttractorList attractors={attractors} scene={scene} system={system} />
+            ) : (
+                // Handle missing scene defensively.
+                <MessageBar intent="info" title="No Scene Available" message="Cannot display attractors without a scene" />
+            )}
+        </>
+    );
+};
