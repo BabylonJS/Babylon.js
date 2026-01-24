12f2d6ecafd5135e7232fffaeb5824bfd99b25c1
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2026-01-23 22:52:54 -0500

    Loop through extensionOptions to create components

--- packages/dev/inspector-v2/src/components/tools/import/gltfLoaderOptionsTool.tsx
+++ packages/dev/inspector-v2/src/components/tools/import/gltfLoaderOptionsTool.tsx
@@ -71,102 +71,32 @@ export const GLTFExtensionOptionsTool: FunctionComponent<{
             indentExpandedContent={true}
             expandedContent={
                 <>
-                    <BoundProperty
-                        label="EXT_lights_image_based"
-                        component={SwitchPropertyLine}
-                        key="EXT_lights_image_based_enabled"
-                        target={extensionOptions["EXT_lights_image_based"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty
-                        label="EXT_mesh_gpu_instancing"
-                        component={SwitchPropertyLine}
-                        key="EXT_mesh_gpu_instancing_enabled"
-                        target={extensionOptions["EXT_mesh_gpu_instancing"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty component={SwitchPropertyLine} label="EXT_texture_webp" target={extensionOptions["EXT_texture_webp"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="EXT_texture_avif" target={extensionOptions["EXT_texture_avif"]} propertyKey="enabled" />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        label="KHR_draco_mesh_compression"
-                        target={extensionOptions["KHR_draco_mesh_compression"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        label="KHR_materials_pbrSpecularGlossiness"
-                        target={extensionOptions["KHR_materials_pbrSpecularGlossiness"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_clearcoat" target={extensionOptions["KHR_materials_clearcoat"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_iridescence" target={extensionOptions["KHR_materials_iridescence"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_anisotropy" target={extensionOptions["KHR_materials_anisotropy"]} propertyKey="enabled" />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        label="KHR_materials_emissive_strength"
-                        target={extensionOptions["KHR_materials_emissive_strength"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_ior" target={extensionOptions["KHR_materials_ior"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_sheen" target={extensionOptions["KHR_materials_sheen"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_specular" target={extensionOptions["KHR_materials_specular"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_unlit" target={extensionOptions["KHR_materials_unlit"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_materials_variants" target={extensionOptions["KHR_materials_variants"]} propertyKey="enabled" />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        key="KHR_materials_transmission"
-                        label="KHR_materials_transmission"
-                        target={extensionOptions["KHR_materials_transmission"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        label="KHR_materials_diffuse_transmission"
-                        target={extensionOptions["KHR_materials_diffuse_transmission"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        key="KHR_materials_volume"
-                        label="KHR_materials_volume"
-                        target={extensionOptions["KHR_materials_volume"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        key="KHR_materials_dispersion"
-                        label="KHR_materials_dispersion"
-                        target={extensionOptions["KHR_materials_dispersion"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty
-                        component={SwitchPropertyLine}
-                        label="KHR_materials_diffuse_roughness"
-                        target={extensionOptions["KHR_materials_diffuse_roughness"]}
-                        propertyKey="enabled"
-                    />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_mesh_quantization" target={extensionOptions["KHR_mesh_quantization"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_lights_punctual" target={extensionOptions["KHR_lights_punctual"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="EXT_lights_area" target={extensionOptions["EXT_lights_area"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_texture_basisu" target={extensionOptions["KHR_texture_basisu"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_texture_transform" target={extensionOptions["KHR_texture_transform"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="KHR_xmp_json_ld" target={extensionOptions["KHR_xmp_json_ld"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="MSFT_lod" target={extensionOptions["MSFT_lod"]} propertyKey="enabled" />
-                    <div style={{ paddingLeft: tokens.spacingHorizontalM }}>
-                        <BoundProperty
-                            component={SyncedSliderPropertyLine}
-                            label="Maximum LODs"
-                            target={extensionOptions["MSFT_lod"]}
-                            propertyKey="maxLODsToLoad"
-                            min={1}
-                            max={10}
-                            step={1}
-                        />
-                    </div>
-                    <BoundProperty component={SwitchPropertyLine} label="MSFT_minecraftMesh" target={extensionOptions["MSFT_minecraftMesh"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="MSFT_sRGBFactors" target={extensionOptions["MSFT_sRGBFactors"]} propertyKey="enabled" />
-                    <BoundProperty component={SwitchPropertyLine} label="MSFT_audio_emitter" target={extensionOptions["MSFT_audio_emitter"]} propertyKey="enabled" />
+                    {Object.entries(extensionOptions).map(([extensionName, options]) => {
+                        return (
+                            <BoundProperty
+                                key={extensionName}
+                                component={SwitchPropertyLine}
+                                label={extensionName}
+                                target={options}
+                                propertyKey="enabled"
+                                expandedContent={
+                                    (extensionName === "MSFT_lod" && (
+                                        <BoundProperty
+                                            key={extensionName + "_maxLODsToLoad"}
+                                            component={SyncedSliderPropertyLine}
+                                            label="Maximum LODs"
+                                            target={extensionOptions[extensionName]} // TS can't infer that value ~ extensionOptions[extensionName]
+                                            propertyKey="maxLODsToLoad"
+                                            min={1}
+                                            max={10}
+                                            step={1}
+                                        />
+                                    )) ||
+                                    undefined
+                                }
+                            />
+                        );
+                    })}
                 </>
             }
         />
