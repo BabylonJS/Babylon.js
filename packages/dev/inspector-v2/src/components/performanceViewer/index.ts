dbc759b5a9786e4675ef9f6933b22b644397a0ea
Georgina Halpern <gehalper@microsoft.com>
2026-01-26 15:49:06 -0500

    delete index

--- packages/dev/inspector-v2/src/components/performanceViewer/index.ts
+++
@@ -1,5 +0,0 @@
-export { CanvasGraph } from "./canvasGraph";
-export { CanvasGraphService } from "./canvasGraphService";
-export { PerformanceSidebar } from "./performanceSidebar";
-export { PerformanceViewerPopup } from "./performanceViewerPopup";
-export * from "./graphSupportingTypes";
