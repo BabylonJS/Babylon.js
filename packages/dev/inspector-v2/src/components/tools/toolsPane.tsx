022b21bafb60523a2e7266161c53cd92e5e3d39a
Georgina Halpern <gehalper@microsoft.com>
2026-01-26 15:26:06 -0500

    toolspane separate PR'

--- packages/dev/inspector-v2/src/components/tools/toolsPane.tsx
+++ packages/dev/inspector-v2/src/components/tools/toolsPane.tsx
@@ -1,16 +1,6 @@
 import { ExtensibleAccordion } from "../extensibleAccordion";
 import type { Scene } from "core/scene";
-import { MessageBar } from "shared-ui-components/fluent/primitives/messageBar";
 
 export const ToolsPane: typeof ExtensibleAccordion<Scene> = (props) => {
-    return (
-        <>
-            <MessageBar
-                intent="info"
-                title="Looking for more tools?"
-                message="Enable additional tools like Export, Capture, Import, and Reflector from the 'Manage Extensions' option in the toolbar."
-            />
-            <ExtensibleAccordion {...props} />
-        </>
-    );
+    return <ExtensibleAccordion {...props} />;
 };
