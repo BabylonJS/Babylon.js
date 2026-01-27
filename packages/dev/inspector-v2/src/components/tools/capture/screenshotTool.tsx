24bd99ecfe51fe829087cadea5ed5d96756efa26
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2026-01-27 18:26:17 -0500

    Just hide custom size behind toggle

--- packages/dev/inspector-v2/src/components/tools/capture/screenshotTool.tsx
+++ packages/dev/inspector-v2/src/components/tools/capture/screenshotTool.tsx
@@ -3,63 +3,44 @@ import { useState, useCallback } from "react";
 import type { FunctionComponent } from "react";
 import type { Scene } from "core/scene";
 import { SyncedSliderPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/syncedSliderPropertyLine";
-import type { IScreenshotSize } from "core/Misc/interfaces/screenshotSize";
 import { CameraRegular } from "@fluentui/react-icons";
 import { FrameGraphUtils } from "core/FrameGraph/frameGraphUtils";
 import { CreateScreenshotAsync } from "core/Misc/screenshotTools";
-import type { Nullable } from "core/types";
+import { SwitchPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/switchPropertyLine";
+import { Collapse } from "shared-ui-components/fluent/primitives/collapse";
 
 export const ScreenshotTool: FunctionComponent<{ scene: Scene }> = ({ scene }) => {
-    const [precision, setPrecision] = useState<Nullable<number>>(1);
-    const [width, setWidth] = useState<Nullable<number>>(null);
-    const [height, setHeight] = useState<Nullable<number>>(null);
+    const [precision, setPrecision] = useState<number>(1);
+    const [useCustomSize, setUseCustomSize] = useState<boolean>(false);
+    const [width, setWidth] = useState<number>(512);
+    const [height, setHeight] = useState<number>(512);
 
     const captureScreenshot = useCallback(async () => {
         const camera = scene.frameGraph ? FrameGraphUtils.FindMainCamera(scene.frameGraph) : scene.activeCamera;
-        const sizeToUse: IScreenshotSize = { precision: precision ?? undefined, width: width ?? undefined, height: height ?? undefined };
+        const screenshotSize = useCustomSize ? { width, height, precision } : { precision };
 
         if (camera) {
-            await CreateScreenshotAsync(scene.getEngine(), camera, sizeToUse, undefined, undefined, undefined, undefined, true);
+            await CreateScreenshotAsync(scene.getEngine(), camera, screenshotSize, undefined, undefined, undefined, undefined, true);
         }
-    }, [precision, width, height, scene]);
+    }, [useCustomSize, precision, width, height, scene]);
 
     return (
         <>
             <ButtonLine label="Capture Screenshot" icon={CameraRegular} onClick={captureScreenshot} />
             <SyncedSliderPropertyLine
                 label="Precision"
-                description="A scale factor for the resolution. Multiplies the width and height of the screenshot."
-                value={precision ?? 1}
-                nullable
-                ignoreNullable={false}
+                description="A multiplier allowing capture at a higher or lower resolution."
+                value={precision}
                 onChange={setPrecision}
-                defaultValue={1}
                 min={0.1}
                 max={10}
                 step={0.1}
             />
-            <SyncedSliderPropertyLine
-                label="Width"
-                description="The width of the screenshot in pixels. "
-                value={width ?? 512}
-                nullable
-                ignoreNullable={false}
-                onChange={(value) => setWidth(value !== null ? Math.floor(value) : null)}
-                defaultValue={512}
-                min={1}
-                step={1}
-            />
-            <SyncedSliderPropertyLine
-                label="Height"
-                description="The height of the screenshot in pixels."
-                value={height ?? 512}
-                nullable
-                ignoreNullable={false}
-                onChange={(value) => setHeight(value !== null ? Math.floor(value) : null)}
-                defaultValue={512}
-                min={1}
-                step={1}
-            />
+            <SwitchPropertyLine label="Use Custom Size" value={useCustomSize} onChange={setUseCustomSize} />
+            <Collapse visible={useCustomSize}>
+                <SyncedSliderPropertyLine label="Width" description="The width of the screenshot in pixels. " value={width} onChange={setWidth} min={1} step={1} />
+                <SyncedSliderPropertyLine label="Height" description="The height of the screenshot in pixels." value={height} onChange={setHeight} min={1} step={1} />
+            </Collapse>
         </>
     );
 };
