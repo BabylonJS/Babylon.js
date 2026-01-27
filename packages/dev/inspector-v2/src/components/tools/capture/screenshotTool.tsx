d1b7de0c27d88f11b0486ad712f5e354133cce2c
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2026-01-27 16:13:27 -0500

    Get rid of screenshotSizeProxy

--- packages/dev/inspector-v2/src/components/tools/capture/screenshotTool.tsx
+++ packages/dev/inspector-v2/src/components/tools/capture/screenshotTool.tsx
@@ -1,5 +1,5 @@
 import { ButtonLine } from "shared-ui-components/fluent/hoc/buttonLine";
-import { useState, useCallback, useMemo } from "react";
+import { useState, useCallback } from "react";
 import type { FunctionComponent } from "react";
 import type { Scene } from "core/scene";
 import { SyncedSliderPropertyLine } from "shared-ui-components/fluent/hoc/propertyLines/syncedSliderPropertyLine";
@@ -7,63 +7,55 @@ import type { IScreenshotSize } from "core/Misc/interfaces/screenshotSize";
 import { CameraRegular } from "@fluentui/react-icons";
 import { FrameGraphUtils } from "core/FrameGraph/frameGraphUtils";
 import { CreateScreenshotAsync } from "core/Misc/screenshotTools";
-import { BoundProperty } from "../../properties/boundProperty";
+import type { Nullable } from "core/types";
 
 export const ScreenshotTool: FunctionComponent<{ scene: Scene }> = ({ scene }) => {
-    const [screenshotSize, setScreenshotSize] = useState<IScreenshotSize>({ precision: 1, width: undefined, height: undefined });
-
-    // Create a proxy object for screenshotSize for convenient use of BoundProperty
-    const screenshotSizeProxy = useMemo(() => {
-        return new Proxy(screenshotSize, {
-            set(target, prop: keyof IScreenshotSize, value) {
-                setScreenshotSize({ ...target, [prop]: value });
-                return true;
-            },
-        });
-    }, [screenshotSize]);
+    const [precision, setPrecision] = useState<Nullable<number>>(1);
+    const [width, setWidth] = useState<Nullable<number>>(null);
+    const [height, setHeight] = useState<Nullable<number>>(null);
 
     const captureScreenshot = useCallback(async () => {
         const camera = scene.frameGraph ? FrameGraphUtils.FindMainCamera(scene.frameGraph) : scene.activeCamera;
-        const sizeToUse: IScreenshotSize = { ...screenshotSize };
+        const sizeToUse: IScreenshotSize = { precision: precision ?? undefined, width: width ?? undefined, height: height ?? undefined };
 
         if (camera) {
             await CreateScreenshotAsync(scene.getEngine(), camera, sizeToUse, undefined, undefined, undefined, undefined, true);
         }
-    }, [scene, screenshotSize]);
+    }, [precision, width, height, scene]);
 
     return (
         <>
             <ButtonLine label="Capture Screenshot" icon={CameraRegular} onClick={captureScreenshot} />
-            <BoundProperty
+            <SyncedSliderPropertyLine
                 label="Precision"
                 description="A scale factor for the resolution. Multiplies the width and height of the screenshot."
-                component={SyncedSliderPropertyLine}
-                target={screenshotSizeProxy}
-                propertyKey="precision"
+                value={precision ?? 1}
                 nullable
+                ignoreNullable={false}
+                onChange={setPrecision}
                 defaultValue={1}
                 min={0.1}
                 max={10}
                 step={0.1}
             />
-            <BoundProperty
+            <SyncedSliderPropertyLine
                 label="Width"
                 description="The width of the screenshot in pixels. "
-                component={SyncedSliderPropertyLine}
-                target={screenshotSizeProxy}
-                propertyKey="width"
+                value={width ?? 512}
                 nullable
+                ignoreNullable={false}
+                onChange={(value) => setWidth(value !== null ? Math.floor(value) : null)}
                 defaultValue={512}
                 min={1}
                 step={1}
             />
-            <BoundProperty
+            <SyncedSliderPropertyLine
                 label="Height"
                 description="The height of the screenshot in pixels."
-                component={SyncedSliderPropertyLine}
-                target={screenshotSizeProxy}
-                propertyKey="height"
+                value={height ?? 512}
                 nullable
+                ignoreNullable={false}
+                onChange={(value) => setHeight(value !== null ? Math.floor(value) : null)}
                 defaultValue={512}
                 min={1}
                 step={1}
