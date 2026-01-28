81932c39928a6f3e521ca98e608cae86afffef32
Ryan Tremblay <ryan.tremblay@microsoft.com>
2026-01-28 09:15:33 -0800

    Switch tools to be always enabled (not optional extensions)

--- packages/dev/inspector-v2/src/services/panes/tools/import/importService.tsx
+++
@@ -1,7 +0,0 @@
-import { GLTFLoaderOptionsServiceDefinition } from "./gltfLoaderOptionsService";
-import { GLTFValidationServiceDefinition } from "./gltfValidationService";
-import { GLTFAnimationImportServiceDefinition } from "./gltfAnimationImportService";
-
-export default {
-    serviceDefinitions: [GLTFAnimationImportServiceDefinition, GLTFLoaderOptionsServiceDefinition, GLTFValidationServiceDefinition],
-} as const;
