972f4898d86b72415308d0f88ed94988ad7bf909
Ryan Tremblay <ryan.tremblay@microsoft.com>
2026-01-28 14:55:34 -0800

    Make the changes :)

--- packages/dev/buildTools/src/packageMapping.ts
+++ packages/dev/buildTools/src/packageMapping.ts
@@ -15,8 +15,8 @@ export const bundledESPackages: DevPackageName[] = [
     "node-render-graph-editor",
     "node-particle-editor",
     "gui-editor",
+    "inspector-legacy",
     "inspector",
-    "inspector-v2",
 ];
 
 export type DevPackageName =
@@ -25,8 +25,8 @@ export type DevPackageName =
     | "materials"
     | "loaders"
     | "serializers"
+    | "inspector-legacy"
     | "inspector"
-    | "inspector-v2"
     | "post-processes"
     | "procedural-textures"
     | "node-editor"
@@ -48,8 +48,8 @@ export type UMDPackageName =
     | "babylonjs-loaders"
     | "babylonjs-materials"
     | "babylonjs-procedural-textures"
+    | "babylonjs-inspector-legacy"
     | "babylonjs-inspector"
-    | "babylonjs-inspector-v2"
     | "babylonjs-node-editor"
     | "babylonjs-node-geometry-editor"
     | "babylonjs-node-render-graph-editor"
@@ -96,6 +96,7 @@ export type ES6PackageName =
     | "@babylonjs/loaders"
     | "@babylonjs/serializers"
     | "@babylonjs/procedural-textures"
+    | "@babylonjs/inspector-legacy"
     | "@babylonjs/inspector"
     | "@babylonjs/node-editor"
     | "@babylonjs/node-geometry-editor"
@@ -136,12 +137,12 @@ export const umdPackageMapping: { [key in UMDPackageName]: { baseDir: string; ba
         baseDir: "proceduralTexturesLibrary",
         baseFilename: "babylonjs.proceduralTextures",
     },
-    "babylonjs-inspector": {
+    "babylonjs-inspector-legacy": {
         baseDir: "inspector",
         baseFilename: "babylon.inspector",
         isBundle: true,
     },
-    "babylonjs-inspector-v2": {
+    "babylonjs-inspector": {
         baseDir: "inspector",
         baseFilename: "babylon.inspector-v2",
         isBundle: true,
@@ -218,8 +219,8 @@ const packageMapping: {
         materials: "babylonjs-materials",
         loaders: "babylonjs-loaders",
         serializers: "babylonjs-serializers",
+        "inspector-legacy": "babylonjs-inspector-legacy",
         inspector: "babylonjs-inspector",
-        "inspector-v2": "babylonjs-inspector-v2",
         "node-editor": (_filePath?: string) => {
             // if (filePath && filePath.indexOf("sharedUiComponents") !== -1) {
             //     return "babylonjs-shared-ui-components";
@@ -266,8 +267,8 @@ const packageMapping: {
         materials: "@babylonjs/materials",
         loaders: "@babylonjs/loaders",
         serializers: "@babylonjs/serializers",
+        "inspector-legacy": "@babylonjs/inspector-legacy",
         inspector: "@babylonjs/inspector",
-        "inspector-v2": "@babylonjs/inspector",
         "node-editor": "@babylonjs/node-editor",
         "node-geometry-editor": "@babylonjs/node-geometry-editor",
         "node-render-graph-editor": "@babylonjs/node-render-graph-editor",
@@ -289,8 +290,8 @@ const packageMapping: {
         materials: "@babylonjs/esm",
         loaders: "@babylonjs/esm",
         serializers: "@babylonjs/esm",
+        "inspector-legacy": "@babylonjs/esm",
         inspector: "@babylonjs/esm",
-        "inspector-v2": "@babylonjs/esm",
         "node-editor": "@babylonjs/esm",
         "node-geometry-editor": "@babylonjs/esm",
         "node-render-graph-editor": "@babylonjs/esm",
@@ -346,7 +347,7 @@ const packageMapping: {
             return "BABYLON";
         },
         serializers: "BABYLON",
-        inspector: (filePath?: string) => {
+        "inspector-legacy": (filePath?: string) => {
             filePath = filePath?.replaceAll("\\", "/");
             if (filePath) {
                 if (filePath.includes("shared-ui-components/") || filePath.includes("/sharedUiComponents/")) {
@@ -358,7 +359,7 @@ const packageMapping: {
             }
             return "INSPECTOR";
         },
-        "inspector-v2": (filePath?: string) => {
+        inspector: (filePath?: string) => {
             filePath = filePath?.replaceAll("\\", "/");
             if (filePath) {
                 if (filePath.includes("shared-ui-components/") || filePath.includes("/sharedUiComponents/")) {
