7e69ae53ef3940981e66043724225404e34cd72d
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2026-01-27 19:24:35 -0500

    Inspector V2: Capture Tools updates (#17713)
    <img width="465" height="642" alt="image"
    src="https://github.com/user-attachments/assets/3dc0d199-49ef-4ed0-a059-12e6c659ea19"
    />
    
    ### Changes
    - Add GIF recorder
    - Add scene replay 
    - Some aesthetics... let me know what you think.
    - Separate capture screenshot, capture equirectangular, and record video
    into their own sections.
      - Always show width/height of screenshot
    - Marry capture screenshot with capture RTT (for a UI, they seem
    functionally equivalent, right?)
    
    ### Notes
    *State in components vs. service*: I've kept the functionality in the
    components for now, instead of in a service, both because it was simpler
    and because these components don't really need to persist state outside
    of the Inspector's lifetime. (Unlike the glTF loader options and glTF
    validator in the Import Tools...)
    
    *GIF module*: There aren't many options out there for recording GIFs on
    the web. `gif.js` seems to be standard, and it's what we had before. I
    installed it as a devDependency in the Inspector v2, assuming that it
    will be built into the bundled package. Hope this is OK, but let me
    know.

--- packages/dev/inspector-v2/src/services/panes/toolsService.tsx
+++ packages/dev/inspector-v2/src/services/panes/toolsService.tsx
@@ -68,12 +68,6 @@ export const ToolsServiceDefinition: ServiceDefinition<[IToolsService], [IShellS
             }
         });
 
-        /**
-         * Left TODO: Implement the following sections from toolsTabComponent.tsx
-         * - GIF (consider putting in Capture Tools)
-         * - Replay (consider putting in Capture Tools)
-         */
-
         return {
             addSection: (section) => sectionsCollection.add(section),
             addSectionContent: (content) => sectionContentCollection.add(content),
