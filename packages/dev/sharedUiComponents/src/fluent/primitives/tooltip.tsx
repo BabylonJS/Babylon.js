9d06bd5ebbfade0b99daa4310d12b63f77692c74
Georgina Halpern <gehalper@microsoft.com>
2026-01-30 17:39:48 -0500

    tooltip revert

--- packages/dev/sharedUiComponents/src/fluent/primitives/tooltip.tsx
+++ packages/dev/sharedUiComponents/src/fluent/primitives/tooltip.tsx
@@ -1,26 +1,21 @@
 import type { Nullable } from "core/index";
 
-import type { ReactElement, Ref } from "react";
-import { forwardRef, cloneElement, isValidElement } from "react";
+import type { FunctionComponent, ReactElement } from "react";
 
 import { Tooltip as FluentTooltip } from "@fluentui/react-components";
 
 export type TooltipProps = { content?: Nullable<string>; children: ReactElement };
 
-export const Tooltip = forwardRef<HTMLElement, TooltipProps>((props, ref) => {
+export const Tooltip: FunctionComponent<TooltipProps> = (props) => {
     const { content, children } = props;
 
-    // When there's no tooltip content, just render children with forwarded ref
     if (!content) {
-        return ref && isValidElement(children) ? cloneElement(children, { ref } as { ref: Ref<HTMLElement> }) : children;
+        return children;
     }
 
-    // FluentTooltip handles its own ref for positioning - don't interfere with it
     return (
         <FluentTooltip relationship="description" content={content}>
             {children}
         </FluentTooltip>
     );
-});
-
-Tooltip.displayName = "Tooltip";
+};
