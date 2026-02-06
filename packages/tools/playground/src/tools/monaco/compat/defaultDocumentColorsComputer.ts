219d534f3acde2b4df8bb9e55eec03b5d3ea57f5
Vicente Cartas Espinel <vicente.cartas@outlook.com>
2026-02-06 11:14:42 -0800

    Fixing file name

--- packages/tools/playground/src/tools/monaco/compat/defaultDocumentColorsComputer.ts
+++ packages/tools/playground/src/tools/monaco/compat/defaultDocumentColorsComputer.ts
@@ -1,3 +1,5 @@
+defaultDocumentColorsComputer;
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
