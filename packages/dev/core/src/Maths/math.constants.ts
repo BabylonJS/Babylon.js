e6355d83ec944c69bead69a49ce606cd0bc0d577
David Catuhe <david@catuhe.com>
2026-02-10 15:49:46 -0800

    Restore Epsilon to let

--- packages/dev/core/src/Maths/math.constants.ts
+++ packages/dev/core/src/Maths/math.constants.ts
@@ -18,7 +18,8 @@ export const PHI = (1 + Math.sqrt(5)) / 2;
 
 /**
  * Constant used to define the minimal number value in Babylon.js
+ * Forced to "let" so the users can change it if they want to use another epsilon value
  * @ignorenaming
  */
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export const Epsilon = 0.001;
+// eslint-disable-next-line prefer-const
+export let Epsilon = 0.001;
