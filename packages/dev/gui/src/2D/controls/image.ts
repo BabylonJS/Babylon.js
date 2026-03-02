e608c5d0ebfc3fe25518a103d10d97d06c734d6a
Raanan Weber <raananw+github@gmail.com>
2026-03-02 19:47:51 +0100

    make sure the function is there before called and not after

--- packages/dev/gui/src/2D/controls/image.ts
+++ packages/dev/gui/src/2D/controls/image.ts
@@ -751,8 +751,8 @@ export class Image extends Control {
                 let elemMatrixD = 1;
                 let elemMatrixE = 0;
                 let elemMatrixF = 0;
-                const mainMatrix = elem.transform.baseVal.consolidate()!.matrix;
                 if (elem.transform && elem.transform.baseVal.consolidate()) {
+                    const mainMatrix = elem.transform.baseVal.consolidate()!.matrix;
                     elemMatrixA = mainMatrix.a;
                     elemMatrixD = mainMatrix.d;
                     elemMatrixE = mainMatrix.e;
