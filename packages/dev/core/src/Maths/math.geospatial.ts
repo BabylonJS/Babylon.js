cb8b9e2bc7a18e05114e18b0a4eb70d622200021
kircher1 <20366429+kircher1@users.noreply.github.com>
2026-03-05 22:16:24 -0800

    More geo math helpers

--- packages/dev/core/src/Maths/math.geospatial.ts
+++ packages/dev/core/src/Maths/math.geospatial.ts
@@ -6,8 +6,24 @@ export interface ILatLonLike {
     lon: number;
 }
 
-/** Latitude and longitude in radians, and altitude in meters. */
+/** Latitude and longitude in radians, and altitude, typically meters. */
 export interface ILatLonAltLike extends ILatLonLike {
-    /** The height above the surface in meters. */
+    /** The height above the surface, typically meters. */
     alt: number;
+}
+
+/**
+ * A reference ellipsoid used for geospatial functions.
+ */
+export interface IEllipsoidLike {
+    /** The larger radius for the ellipsoid. */
+    semiMajorAxis: number;
+    /** The smaller radius for the ellipsoid. */
+    semiMinorAxis: number;
+    /** The flattening for the ellipsoid. Zero for a sphere. */
+    flattening: number;
+    /** The first eccentricity, squared. */
+    firstEccentricitySquared: number;
+    /** The second eccentricity, squared. */
+    secondEccentricitySquared: number;
 }
