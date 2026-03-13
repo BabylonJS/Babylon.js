7502ed65ff35f33a72fe0d94ada89c04d56b7e13
Georgina Halpern <gehalper@microsoft.com>
2026-03-13 13:05:37 -0400

    alt

--- packages/dev/core/src/Cameras/Inputs/geospatialCameraKeyboardInput.ts
+++ packages/dev/core/src/Cameras/Inputs/geospatialCameraKeyboardInput.ts
@@ -12,6 +12,9 @@ import type { AbstractEngine } from "../../Engines/abstractEngine";
 
 /**
  * Manage the keyboard inputs to control the movement of a geospatial camera.
+ * Arrow keys + Modifier key (ctrl/alt, cmd/option on mac): rotate
+ * Arrow keys alone: pan
+ * + / - keys: zoom in/out
  * @see https://doc.babylonjs.com/features/featuresDeepDive/cameras/customizingCameraInputs
  */
 export class GeospatialCameraKeyboardInput implements ICameraInput<GeospatialCamera> {
@@ -78,7 +81,7 @@ export class GeospatialCameraKeyboardInput implements ICameraInput<GeospatialCam
     public zoomSensitivity: number = 1.0;
 
     private _keys = new Array<number>();
-    private _ctrlPressed: boolean;
+    private _modifierPressed: boolean;
     private _onCanvasBlurObserver: Nullable<Observer<AbstractEngine>>;
     private _onKeyboardObserver: Nullable<Observer<KeyboardInfo>>;
     private _engine: AbstractEngine;
@@ -115,7 +118,7 @@ export class GeospatialCameraKeyboardInput implements ICameraInput<GeospatialCam
             // Block metaKey for other keys to avoid capturing browser shortcuts (e.g., Cmd+=/Cmd+-).
             if (!evt.metaKey || isArrowKey) {
                 if (info.type === KeyboardEventTypes.KEYDOWN) {
-                    this._ctrlPressed = evt.ctrlKey || evt.metaKey || evt.shiftKey;
+                    this._modifierPressed = evt.ctrlKey || evt.metaKey || evt.altKey;
 
                     if (
                         this.keysUp.indexOf(evt.keyCode) !== -1 ||
@@ -187,7 +190,7 @@ export class GeospatialCameraKeyboardInput implements ICameraInput<GeospatialCam
 
             for (let index = 0; index < this._keys.length; index++) {
                 const keyCode = this._keys[index];
-                if (this._ctrlPressed) {
+                if (this._modifierPressed) {
                     // Rotation
                     if (this.keysLeft.indexOf(keyCode) !== -1) {
                         camera.movement.rotationAccumulatedPixels.y -= this.rotationSensitivity;
