BabylonJS/Babylon.js

commit 6139088ded9785d96b761c0a837a965a9c9160db

AI pass on Gizmos (#17982)

packages/dev/core/src/Gizmos/planeRotationGizmo.ts
@@ -34,7 +34,7 @@ export interface IPlaneRotationGizmo extends IGizmo {
     sensitivity: number;
     /**
      * Event that fires each time the gizmo snaps to a new location.
-     * * snapDistance is the change in distance
+     * * snapDistance is the change in angle
      */
     onSnapObservable: Observable<{ snapDistance: number }>;
     /** Accumulated relative angle value for rotation on the axis. */
@@ -68,7 +68,7 @@ export class PlaneRotationGizmo extends Gizmo implements IPlaneRotationGizmo {
     public snapDistance = 0;
     /**
      * Event that fires each time the gizmo snaps to a new location.
-     * * snapDistance is the change in distance
+     * * snapDistance is the change in angle
      */
     public onSnapObservable = new Observable<{ snapDistance: number }>();
 
@@ -290,7 +290,6 @@ export class PlaneRotationGizmo extends Gizmo implements IPlaneRotationGizmo {
 
         const tmpSnapEvent = { snapDistance: 0 };
         let currentSnapDragDistance = 0;
-        const tmpMatrix = new Matrix();
         const amountToRotate = new Quaternion();
         this.dragBehavior.onDragObservable.add((event) => {
             if (this.attachedNode) {
@@ -384,7 +383,7 @@ export class PlaneRotationGizmo extends Gizmo implements IPlaneRotationGizmo {
                 );
 
                 // If the meshes local scale is inverted (eg. loaded gltf file parent with z scale of -1) the rotation needs to be inverted on the y axis
-                if (tmpMatrix.determinant() > 0) {
+                if (this.attachedNode.getWorldMatrix().determinant() > 0) {
                     const tmpVector = new Vector3();
                     amountToRotate.toEulerAnglesToRef(tmpVector);
                     Quaternion.RotationYawPitchRollToRef(tmpVector.y, -tmpVector.x, -tmpVector.z, amountToRotate);

packages/dev/core/src/Gizmos/axisScaleGizmo.ts
@@ -235,10 +235,10 @@ export class AxisScaleGizmo extends Gizmo implements IAxisScaleGizmo {
                 if (Math.abs(this.snapDistance) > 0 && this.incrementalSnap) {
                     // get current scaling
                     this.attachedNode.getWorldMatrix().decompose(undefined, TmpVectors.Quaternion[0], TmpVectors.Vector3[2], Gizmo.PreserveScaling ? transformNode : undefined);
-                    // apply incrementaly, without taking care of current scaling value
+                    // apply incrementally, without taking care of current scaling value
                     tmpVector.addInPlace(this._incrementalStartupValue);
                     tmpVector.addInPlaceFromFloats(-1, -1, -1);
-                    // keep same sign or stretching close to 0 will change orientation at each drag and scaling will oscilate around 0
+                    // keep same sign or stretching close to 0 will change orientation at each drag and scaling will oscillate around 0
                     tmpVector.x = Math.abs(tmpVector.x) * (this._incrementalStartupValue.x > 0 ? 1 : -1);
                     tmpVector.y = Math.abs(tmpVector.y) * (this._incrementalStartupValue.y > 0 ? 1 : -1);
                     tmpVector.z = Math.abs(tmpVector.z) * (this._incrementalStartupValue.z > 0 ? 1 : -1);

packages/dev/core/src/Gizmos/cameraGizmo.ts
@@ -74,7 +74,7 @@ export class CameraGizmo extends Gizmo implements ICameraGizmo {
     }
     protected _camera: Nullable<Camera> = null;
 
-    /** Gets or sets a boolean indicating if frustum lines must be rendered (true by default)) */
+    /** Gets or sets a boolean indicating if frustum lines must be rendered (true by default) */
     public get displayFrustum() {
         return this._cameraLinesMesh.isEnabled();
     }

packages/dev/core/src/Gizmos/gizmo.ts
@@ -45,7 +45,7 @@ export interface GizmoAxisCache {
 export const enum GizmoAnchorPoint {
     /** The origin of the attached node */
     Origin,
-    /** The pivot point of the attached node*/
+    /** The pivot point of the attached node */
     Pivot,
 }
 
@@ -103,7 +103,7 @@ export interface IGizmo extends IDisposable {
      */
     updateScale: boolean;
     /**
-     * posture that the gizmo will be display
+     * Orientation that the gizmo will be displayed with.
      * When set null, default value will be used (Quaternion(0, 0, 0, 1))
      */
     customRotationQuaternion: Nullable<Quaternion>;
@@ -143,7 +143,7 @@ export class Gizmo implements IGizmo {
     protected _isHovered = false;
 
     /**
-     * When enabled, any gizmo operation will perserve scaling sign. Default is off.
+     * When enabled, any gizmo operation will preserve scaling sign. Default is off.
      * Only valid for TransformNode derived classes (Mesh, AbstractMesh, ...)
      */
     public static PreserveScaling = false;
@@ -323,7 +323,7 @@ export class Gizmo implements IGizmo {
     }
 
     /**
-     * posture that the gizmo will be display
+     * Orientation that the gizmo will be displayed with.
      * When set null, default value will be used (Quaternion(0, 0, 0, 1))
      */
     public get customRotationQuaternion(): Nullable<Quaternion> {
@@ -341,7 +341,7 @@ export class Gizmo implements IGizmo {
         if (this.attachedNode) {
             let effectiveNode = this.attachedNode;
             if (this.attachedMesh) {
-                effectiveNode = this.attachedMesh || this.attachedNode;
+                effectiveNode = this.attachedMesh;
             }
 
             // Position
@@ -511,7 +511,7 @@ export class Gizmo implements IGizmo {
                     const scaleMatrix = TmpVectors.Matrix[2];
                     Matrix.ScalingToRef(transform.scaling.x, transform.scaling.y, transform.scaling.z, scaleMatrix);
 
-                    const rotationMatrix = TmpVectors.Matrix[2];
+                    const rotationMatrix = TmpVectors.Matrix[7];
                     r.toRotationMatrix(rotationMatrix);
 
                     const pivotMatrix = transform.getPivotMatrix();

packages/dev/core/src/Gizmos/gizmoManager.ts
@@ -23,11 +23,11 @@ import { BoundingBoxGizmo } from "./boundingBoxGizmo";
 import type { TransformNode } from "../Meshes/transformNode";
 
 /**
- * Helps setup gizmo's in the scene to rotate/scale/position nodes
+ * Helps set up gizmos in the scene to rotate/scale/position nodes
  */
 export class GizmoManager implements IDisposable {
     /**
-     * Gizmo's created by the gizmo manager, gizmo will be null until gizmo has been enabled for the first time
+     * Gizmos created by the gizmo manager, gizmo will be null until gizmo has been enabled for the first time
      */
     public gizmos: {
         positionGizmo: Nullable<IPositionGizmo>;

packages/dev/core/src/Gizmos/positionGizmo.ts
@@ -256,7 +256,7 @@ export class PositionGizmo extends Gizmo implements IPositionGizmo {
     }
 
     /**
-     * posture that the gizmo will be display
+     * Orientation that the gizmo will be displayed with.
      * When set null, default value will be used (Quaternion(0, 0, 0, 1))
      */
     public override get customRotationQuaternion(): Nullable<Quaternion> {
@@ -412,7 +412,7 @@ export class PositionGizmo extends Gizmo implements IPositionGizmo {
      */
     public override setCustomMesh() {
         Logger.Error(
-            "Custom meshes are not supported on this gizmo, please set the custom meshes on the gizmos contained within this one (gizmo.xGizmo, gizmo.yGizmo, gizmo.zGizmo,gizmo.xPlaneGizmo, gizmo.yPlaneGizmo, gizmo.zPlaneGizmo)"
+            "Custom meshes are not supported on this gizmo, please set the custom meshes on the gizmos contained within this one (gizmo.xGizmo, gizmo.yGizmo, gizmo.zGizmo, gizmo.xPlaneGizmo, gizmo.yPlaneGizmo, gizmo.zPlaneGizmo)"
         );
     }
 }

packages/dev/core/src/Gizmos/rotationGizmo.ts
@@ -239,8 +239,8 @@ export class RotationGizmo extends Gizmo implements IRotationGizmo {
         // Relay drag events and set update scale
         const gizmos = [this.xGizmo, this.yGizmo, this.zGizmo];
         for (const gizmo of gizmos) {
-            //must set updateScale on each gizmo, as setting it on root RotationGizmo doesnt prevent individual gizmos from updating
-            //currently updateScale is a property with no getter/setter, so no good way to override behavior at runtime, so we will at least set it on startup
+            //must set updateScale on each gizmo, as setting it on root RotationGizmo doesn't prevent individual gizmos from updating
+            //set it on startup since options are only applied once at construction time
             if (options && options.updateScale != undefined) {
                 gizmo.updateScale = options.updateScale;
             }
@@ -354,7 +354,7 @@ export class RotationGizmo extends Gizmo implements IRotationGizmo {
     }
 
     /**
-     * posture that the gizmo will be display
+     * Orientation that the gizmo will be displayed with.
      * When set null, default value will be used (Quaternion(0, 0, 0, 1))
      */
     public override get customRotationQuaternion(): Nullable<Quaternion> {

packages/dev/core/src/Gizmos/scaleGizmo.ts
@@ -317,7 +317,7 @@ export class ScaleGizmo extends Gizmo implements IScaleGizmo {
     }
 
     /**
-     * posture that the gizmo will be display
+     * Orientation that the gizmo will be displayed with.
      * When set null, default value will be used (Quaternion(0, 0, 0, 1))
      */
     public override get customRotationQuaternion(): Nullable<Quaternion> {

commit 079a2ae45ed344615d34a1fe96da8cef342ecf50

fix planeRotation regression (#18105)
fix regression introduced by
https://github.com/BabylonJS/Babylon.js/pull/17982

packages/dev/core/src/Gizmos/planeRotationGizmo.ts
@@ -382,13 +382,6 @@ export class PlaneRotationGizmo extends Gizmo implements IPlaneRotationGizmo {
                     Math.cos(angle / 2)
                 );
 
-                // If the meshes local scale is inverted (eg. loaded gltf file parent with z scale of -1) the rotation needs to be inverted on the y axis
-                if (this.attachedNode.getWorldMatrix().determinant() > 0) {
-                    const tmpVector = new Vector3();
-                    amountToRotate.toEulerAnglesToRef(tmpVector);
-                    Quaternion.RotationYawPitchRollToRef(tmpVector.y, -tmpVector.x, -tmpVector.z, amountToRotate);
-                }
-
                 if (this.updateGizmoRotationToMatchAttachedMesh) {
                     // Rotate selected mesh quaternion over fixed axis
                     nodeQuaternion.multiplyToRef(amountToRotate, nodeQuaternion);