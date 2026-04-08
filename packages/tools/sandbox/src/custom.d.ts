300332fafaeada0cee416af61319600e4e82fd83
Michael Bond <mbond@adobe.com>
2026-04-08 09:38:46 -0700

    Fix build issue for sandbox

--- packages/tools/sandbox/src/custom.d.ts
+++ packages/tools/sandbox/src/custom.d.ts
@@ -2,3 +2,13 @@ declare module "*.svg" {
     const content: string;
     export default content;
 }
+
+declare module "*.scss" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: string;
+    export default content;
+}
