5559cc1635533fa37c8da0938429a7e3553eb90a
Raanan Weber <raananw+github@gmail.com>
2026-04-29 12:45:12 +0200

    fix: improve scene disposal logic and enable context loss on engine dispose

--- packages/tools/tests/test/playwright/visualizationPlaywright.utils.ts
+++ packages/tools/tests/test/playwright/visualizationPlaywright.utils.ts
@@ -222,8 +222,13 @@ export const evaluatePlaywrightVisTests = async (
 
     test.afterEach(async () => {
         await page.evaluate(() => {
-            window.engine && window.engine.dispose();
+            if (window.scene && window.scene.dispose) {
+                window.scene.dispose();
+            }
             window.scene = null;
+            if (window.engine && window.engine.dispose) {
+                window.engine.dispose();
+            }
             window.engine = null;
         });
     });
@@ -394,6 +399,7 @@ export const evaluateInitEngineForVisualization = async ({
             failIfMajorPerformanceCaveat: true,
             powerPreference: "high-performance",
             useLargeWorldRendering: useLargeWorldRendering,
+            loseContextOnDispose: true,
         });
         engine.enableOfflineSupport = false;
         engine.setDitheringState(false);
